af14a5d0068e08be35a24dced1964e99f1ea5a1e
Steve Dower <steve.dower@microsoft.com>
2016-02-13 11:57:12 -0800

    Adds execution time display when --debug passed

--- src/azure/cli/__main__.py
+++ src/azure/cli/__main__.py
@@ -1,4 +1,14 @@
+import logging
 import sys
+import time
+
+_import_time = time.perf_counter()
 
 import azure.cli.main
-sys.exit(azure.cli.main.main(sys.argv[1:]))
+try:
+    sys.exit(azure.cli.main.main(sys.argv[1:]))
+finally:
+    # Process time includes Python engine startup
+    # Script time includes azure.cli module only
+    logging.debug('Process time: %8.3fms', 1000 * time.process_time())
+    logging.debug('Script time:  %8.3fms', 1000 * (time.perf_counter() - _import_time))
