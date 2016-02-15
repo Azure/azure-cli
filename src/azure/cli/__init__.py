129d8bb0d7058ac4be0888c0a6fb22f5b86e429b
Steve Dower <steve.dower@microsoft.com>
2016-02-15 10:29:58 -0800

    Moves version/author details to __init__.py

---
+++ src/azure/cli/__init__.py
@@ -0,0 +1,8 @@
+'''The Azure Command-line tool.
+
+This tools provides a command-line interface to Azure's management and storage
+APIs.
+'''
+
+__author__ = "Microsoft Corporation <python@microsoft.com>"
+__version__ = "2016.2.4"
