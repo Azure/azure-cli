308bd4abb0cefaa68986f031ad55e0c3a457c943
Steve Dower <steve.dower@microsoft.com>
2016-02-17 08:36:20 -0800

    Adds setup.py to package

---
+++ src/setup.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+
+from __future__ import print_function
+from codecs import open
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+# If we have source, validate that our version numbers match
+# This should prevent uploading releases with mismatched versions.
+try:
+    with open('azure/cli/__init__.py', 'r', encoding='utf-8') as f:
+        content = f.read()
+except OSError:
+    pass
+else:
+    import re, sys
+    m = re.search(r'__version__\s*=\s*[\'"](.+?)[\'"]', content)
+    if not m:
+        print('Could not find __version__ in azure/cli/__init__.py')
+        sys.exit(1)
+    if m.group(1) != VERSION:
+        print('Expected __version__ = "{}"; found "{}"'.format(VERSION, m.group(1)))
+        sys.exit(1)
+
+# The full list of classifiers is available at
+# https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    #'License :: OSI Approved :: Apache Software License',
+    #'License :: OSI Approved :: MIT License',
+]
+
+# The azure-mgmt requirement should always be pinned to ensure
+# that installing a specific azure-cli version will target the
+# expected Azure API versions
+#
+# Alternatively, the more specific requirements such as
+# azure-mgmt-resource may be specified in place of the roll-up
+# packages.
+#
+# Common azure package dependencies will be pulled in by these
+# references, so do not specify azure-common or -nspkg here.
+DEPENDENCIES = [
+    'azure-mgmt==0.20.2',
+    'azure-storage==0.20.3',
+]
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    README = f.read()
+
+setup(
+    name='azure-cli',
+    version=VERSION,
+    description='Microsoft Azure Command-Line Tools',
+    long_description=README,
+    license='TBD',
+    author='Microsoft Corporation',
+    author_email='SOMEBODY@microsoft.com',
+    url='https://github.com/Azure/azure-cli',
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+    packages=[
+        'azure.cli',
+        'azure.cli.commands',
+    ],
+    install_requires=DEPENDENCIES,
+)
