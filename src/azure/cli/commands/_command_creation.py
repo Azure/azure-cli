61b7575f668a32db170cbcec385fcce7d769f530
Burt Bielicki <burtbiel@microsoft.com>
2016-02-25 15:21:29 -0800

    casing change

--- src/azure/cli/commands/_command_creation.py
+++ src/azure/cli/commands/_command_creation.py
@@ -1,8 +1,8 @@
 from .._profile import Profile
 import azure.cli._debug as _debug
 
-def get_service_client(clientType, configType):
+def get_service_client(client_type, configType):
     profile = Profile()
-    client = clientType(configType(*profile.get_login_credentials()))
+    client = client_type(configType(*profile.get_login_credentials()))
     _debug.allow_debug_connection(client)
     return client
