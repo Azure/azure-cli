78c324a65b6426df9e4da4491ac0f01c1b654aae
yugangw-msft <yugangw@microsoft.com>
2016-02-25 21:56:26 -0800

    address code analysis errors by pylints

--- src/azure/cli/commands/login.py
+++ src/azure/cli/commands/login.py
@@ -1,33 +1,36 @@
-﻿from msrestazure.azure_active_directory import UserPassCredentials
+﻿from msrest import Serializer
+from msrestazure.azure_active_directory import UserPassCredentials
 from azure.mgmt.resource.subscriptions import SubscriptionClient, \
                                               SubscriptionClientConfiguration
-
-from msrest import Serializer
 
 from .._profile import Profile
 from ..commands import command, description, option
 from .._debug import should_disable_connection_verify
+from .._locale import L
 
 CLIENT_ID = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'
 
 @command('login')
-@description(_('log in to an Azure subscription using Active Directory Organization Id'))
-@option('--username -u <username>', _('organization Id. Microsoft Account is not yet supported.'))
-@option('--password -p <password>', _('user password, will prompt if not given.'))
-def login(args, unexpected):
+@description(L('log in to an Azure subscription using Active Directory Organization Id'))
+@option('--username -u <username>', L('organization Id. Microsoft Account is not yet supported.'))
+@option('--password -p <password>', L('user password, will prompt if not given.'))
+def login(args, unexpected): #pylint: disable=unused-argument
     username = args.get('username')
 
     password = args.get('password')
     if not password:
         import getpass
-        password = getpass.getpass(_('Password: '))
+        password = getpass.getpass(L('Password: '))
 
-    credentials = UserPassCredentials(username, password, client_id=CLIENT_ID, verify=not should_disable_connection_verify())
+    credentials = UserPassCredentials(username, 
+                                      password, 
+                                      client_id=CLIENT_ID, 
+                                      verify=not should_disable_connection_verify())
     client = SubscriptionClient(SubscriptionClientConfiguration(credentials))
     subscriptions = client.subscriptions.list()
 
     if not subscriptions:
-        raise RuntimeError(_('No subscriptions found for this account.'))
+        raise RuntimeError(L('No subscriptions found for this account.'))
 
     serializable = Serializer().serialize_data(subscriptions, "[Subscription]")
 
--- src/azure/cli/commands/storage.py
+++ src/azure/cli/commands/storage.py
@@ -1,14 +1,14 @@
 from msrest import Serializer
-
-from ..main import SESSION
 from ..commands import command, description, option
 from ._command_creation import get_service_client
+from .._logging  import logger
+from .._locale import L
 
 @command('storage account list')
-@description(_('List storage accounts'))
-@option('--resource-group -g <resourceGroup>', _('the resource group name'))
-@option('--subscription -s <id>', _('the subscription id'))
-def list_accounts(args, unexpected):
+@description(L('List storage accounts'))
+@option('--resource-group -g <resourceGroup>', L('the resource group name'))
+@option('--subscription -s <id>', L('the subscription id'))
+def list_accounts(args, unexpected): #pylint: disable=unused-argument 
     from azure.mgmt.storage import StorageManagementClient, StorageManagementClientConfiguration
     from azure.mgmt.storage.models import StorageAccount
     from msrestazure.azure_active_directory import UserPassCredentials
@@ -26,10 +26,7 @@ def list_accounts(args, unexpected):
 
 @command('storage account check')
 @option('--account-name <name>')
-def checkname(args, unexpected):
+def checkname(args, unexpected): #pylint: disable=unused-argument 
     from azure.mgmt.storage import StorageManagementClient, StorageManagementClientConfiguration
-    
     smc = get_service_client(StorageManagementClient, StorageManagementClientConfiguration)
-    logger.warn(smc.storage_accounts.check_name_availability(args.account_name))
-    
-    
+    logger.warning(smc.storage_accounts.check_name_availability(args.account_name))
