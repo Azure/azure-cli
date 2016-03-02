ca64abce09d8b56b8715eb1a63cbad292f60f449
Burt Bielicki <burtbiel@microsoft.com>
2016-03-02 10:47:49 -0800

    fix pylint errors

--- src/azure/cli/commands/__init__.py
+++ src/azure/cli/commands/__init__.py
@@ -30,7 +30,8 @@ def description(description_text):
 
 def option(spec, description_text=None, required=False):
     def add_option(handler):
-        _COMMANDS.setdefault(handler, {}).setdefault('args', []).append((spec, description_text, required))
+        _COMMANDS.setdefault(handler, {}).setdefault('args', []) \
+            .append((spec, description_text, required))
         logger.debug('Added option "%s" to %s', spec, handler)
         return handler
     return add_option
@@ -41,7 +42,7 @@ def add_to_parser(parser, command_name=None):
     When `command` is specified, only commands from that module will be loaded.
     If the module is not found, all commands are loaded.
     '''
-    
+
     # Importing the modules is sufficient to invoke the decorators. Then we can
     # get all of the commands from the _COMMANDS variable.
     loaded = False
