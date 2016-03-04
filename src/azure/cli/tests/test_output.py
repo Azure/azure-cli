00c40437469c4d3dff5a8271199d142fbbefddbc
Derek Bekoe <debekoe@microsoft.com>
2016-03-03 18:04:41 -0800

    Support lists and well as dicts --- Also, fix StringIO issue with Python 2 vs. 3

--- src/azure/cli/tests/test_output.py
+++ src/azure/cli/tests/test_output.py
@@ -9,7 +9,7 @@ except ImportError:
     # Python 2
     from StringIO import StringIO
 
-from azure.cli._output import OutputProducer, OutputFormatException, format_json, format_table, format_text
+from azure.cli._output import OutputProducer, OutputFormatException, format_json, format_table, format_list, format_text
 import azure.cli._util as util
 
 class TestOutput(unittest.TestCase):
@@ -50,6 +50,71 @@ class TestOutput(unittest.TestCase):
 """active |    id   
 -------|---------
 True   | 0b1f6472
+
+"""))
+
+    def test_out_list_valid(self):
+        """
+        """
+        output_producer = OutputProducer(formatter=format_list, file=self.io)
+        output_producer.out({'active': True, 'id': '0b1f6472'})
+        self.assertEqual(util.normalize_newlines(self.io.getvalue()), util.normalize_newlines(
+"""Active : True
+Id     : 0b1f6472
+
+
+"""))
+
+    def test_out_list_valid_array_complex(self):
+        """
+        """
+        output_producer = OutputProducer(formatter=format_list, file=self.io)
+        output_producer.out([{'active': True, 'id': '783yesdf'}, {'active': False, 'id': '3hjnme32'}, {'active': False, 'id': '23hiujbs'}])
+        self.assertEqual(util.normalize_newlines(self.io.getvalue()), util.normalize_newlines(
+"""Active : True
+Id     : 783yesdf
+
+Active : False
+Id     : 3hjnme32
+
+Active : False
+Id     : 23hiujbs
+
+
+"""))
+
+    def test_out_list_valid_str_array(self):
+        """
+        """
+        output_producer = OutputProducer(formatter=format_list, file=self.io)
+        output_producer.out(['location', 'id', 'host', 'server'])
+        self.assertEqual(util.normalize_newlines(self.io.getvalue()), util.normalize_newlines(
+"""location
+
+id
+
+host
+
+server
+
+
+"""))
+
+    def test_out_list_valid_complex_array(self):
+        """
+        """
+        output_producer = OutputProducer(formatter=format_list, file=self.io)
+        output_producer.out({'active': True, 'id': '0b1f6472', 'myarray': ['1', '2', '3', '4']})
+        self.assertEqual(util.normalize_newlines(self.io.getvalue()), util.normalize_newlines(
+"""Active : True
+Id     : 0b1f6472
+
+  MYARRAY
+  1
+  2
+  3
+  4
+
 
 """))
 
