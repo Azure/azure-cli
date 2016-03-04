db2b8a21966967d2face8e9d8cf6002b96d1c2fb
Burt Bielicki <burtbiel@microsoft.com>
2016-03-04 12:00:57 -0800

    hook up msrest logging to our logger (they only log in debug level)

--- src/azure/cli/commands/_command_creation.py
+++ src/azure/cli/commands/_command_creation.py
@@ -1,10 +1,16 @@
 ﻿from .._profile import Profile
 import azure.cli._debug as _debug
 import azure.cli as cli
+from .._logging import logger
 
 def get_service_client(client_type, config_type):
     profile = Profile()
-    client = client_type(config_type(*profile.get_login_credentials()))
+    config = config_type(*profile.get_login_credentials())
+    config.log_name = 'az'
+    config.log_level = logger.level
+
+    client = client_type(config)
     _debug.allow_debug_connection(client)
     client.config.add_user_agent("AZURECLI_{}".format(cli.__version__))
+
     return client
