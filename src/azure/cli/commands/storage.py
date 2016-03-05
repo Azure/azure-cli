c0c053dced85b9226911e22f083d2ebe17bbdeec
Burt Bielicki <burtbiel@microsoft.com>
2016-03-04 16:45:13 -0800

    list blobs and create block blob

--- src/azure/cli/commands/storage.py
+++ src/azure/cli/commands/storage.py
@@ -25,8 +25,42 @@ def list_accounts(args, unexpected): #pylint: disable=unused-argument
     return serializable
 
 @command('storage account check')
-@option('--account-name <name>')
+@option('--account-name -an <name>')
 def checkname(args, unexpected): #pylint: disable=unused-argument
     from azure.mgmt.storage import StorageManagementClient, StorageManagementClientConfiguration
     smc = get_service_client(StorageManagementClient, StorageManagementClientConfiguration)
     logger.warning(smc.storage_accounts.check_name_availability(args.account_name))
+
+@command('storage blob blockblob create')
+@option('--account-name -an <name>', required=True)
+@option('--account-key -ak <key>', required=True)
+@option('--container -c <name>', required=True)
+@option('--blob-name -bn <name>', required=True)
+@option('--upload-from -uf <file>', required=True)
+@option('--content-settings.type -cs <type>')
+def create_block_blob(args, unexpected): #pylint: disable=unused-argument
+    from azure.storage.blob import BlockBlobService, PublicAccess, ContentSettings
+
+    block_blob_service = BlockBlobService(account_name=args.get('account-name'), 
+                                          account_key=args.get('account-key'))
+
+    block_blob_service.create_container(args.get('container'), 
+                                        public_access=PublicAccess.Container)
+
+    return block_blob_service.create_blob_from_path(
+        args.get('container'),
+        args.get('blob-name'),
+        args.get('upload-from'),
+        content_settings=ContentSettings(content_type=args.get('content-settings.type')))
+
+@command('storage blob list')
+@option('--account-name -an <name>', required=True)
+@option('--account-key -ak <key>', required=True)
+@option('--container -c <name>', required=True)
+def list_blobs(args, unexpected): #pylint: disable=unused-argument
+    from azure.storage.blob import BlockBlobService, PublicAccess, ContentSettings
+
+    block_blob_service = BlockBlobService(account_name=args.get('account-name'), 
+                                          account_key=args.get('account-key'))
+
+    return [b.name for b in block_blob_service.list_blobs(args.get('container'))]
