ab1e27b3b28e0463f6dd182037a265c9f69fb94f
Derek Bekoe <debekoe@microsoft.com>
2016-03-08 13:03:47 -0800

    Use service client factory method for resource group command

--- src/azure/cli/commands/resourcegroup.py
+++ src/azure/cli/commands/resourcegroup.py
@@ -1,5 +1,6 @@
 from msrest import Serializer
 from ..commands import command, description
+from ._command_creation import get_service_client
 from .._profile import Profile
 
 @command('resource group list')
@@ -13,8 +14,7 @@ def list_groups(args, unexpected): #pylint: disable=unused-argument
                                               ResourceManagementClientConfiguration
     from azure.mgmt.resource.resources.models import ResourceGroup, ResourceGroupFilter
 
-    rmc = ResourceManagementClient(
-        ResourceManagementClientConfiguration(*Profile().get_login_credentials()))
+    rmc = get_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
 
     # TODO: waiting on Python Azure SDK bug fixes
     #group_filter = ResourceGroupFilter(args.get('tag-name'), args.get('tag-value'))
