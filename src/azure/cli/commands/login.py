4c652feb16fd90aa42148ffa5415576ee5f1f438
yugangw-msft <yugangw@microsoft.com>
2016-03-10 10:11:34 -0800

    support login as a service principal

--- src/azure/cli/commands/login.py
+++ src/azure/cli/commands/login.py
@@ -1,4 +1,5 @@
-﻿from msrestazure.azure_active_directory import UserPassCredentials
+﻿from msrestazure.azure_active_directory import UserPassCredentials, \
+                                               ServicePrincipalCredentials
 from azure.mgmt.resource.subscriptions import SubscriptionClient, \
                                               SubscriptionClientConfiguration
 
@@ -11,20 +12,37 @@ CLIENT_ID = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'
 
 @command('login')
 @description(L('log in to an Azure subscription using Active Directory Organization Id'))
-@option('--username -u <username>', L('organization Id. Microsoft Account is not yet supported.'))
-@option('--password -p <password>', L('user password, will prompt if not given.'))
+@option('--username -u <username>',
+        L('organization Id or service principal. Microsoft Account is not yet supported.'))
+@option('--password -p <password>', L('user password or client secret, will prompt if not given.'))
+@option('--service-principal', L('the credential represents a service principal.'))
+@option('--tenant -t <tenant>', L('the tenant associated with the service principal.'))
 def login(args, unexpected): #pylint: disable=unused-argument
     username = args.get('username')
+    if not username:
+        raise ValueError(L('Please supply user name using "-u/--username"'))
 
     password = args.get('password')
     if not password:
         import getpass
         password = getpass.getpass(L('Password: '))
 
-    credentials = UserPassCredentials(username,
-                                      password,
-                                      client_id=CLIENT_ID,
-                                      verify=not should_disable_connection_verify())
+    cert_verify = not should_disable_connection_verify()
+    if args.get('service-principal'):
+        tenant = args.get('tenant')
+        if not tenant:
+            raise ValueError(L('Please supply tenant using "--tenant"'))
+
+        credentials = ServicePrincipalCredentials(username,
+                                                  password,
+                                                  tenant=tenant,
+                                                  verify=cert_verify)
+    else:
+        credentials = UserPassCredentials(username, #pylint: disable=redefined-variable-type
+                                          password,
+                                          client_id=CLIENT_ID,
+                                          verify=cert_verify)
+
     client = SubscriptionClient(SubscriptionClientConfiguration(credentials))
     subscriptions = client.subscriptions.list()
 
