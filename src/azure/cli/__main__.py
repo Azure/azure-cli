fe108d1c31471a92628331b3e3efb9cc37c9c86e
Derek Bekoe <debekoe@microsoft.com>
2016-03-10 17:20:13 -0800

    1. add SIGINT handler for Ctrl+C interruptions. 2. add SIGPIPE handler for BrokenPipeError

--- src/azure/cli/__main__.py
+++ src/azure/cli/__main__.py
@@ -1,5 +1,13 @@
 import sys
+import os
+import signal
 
 import azure.cli.main
+
+signal.signal(signal.SIGINT, lambda signum, frame: sys.exit(1))
+
+if os.name != 'nt':
+    # can't call Windows with SIGPIPE
+    signal.signal(signal.SIGPIPE, lambda signum, frame: sys.exit(1))
 
 sys.exit(azure.cli.main.main(sys.argv[1:]))
