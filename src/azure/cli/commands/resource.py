06cd1873ad1afd911d2861c526f0051075c98a9f
Travis Prescott <trpresco@microsoft.com>
2016-03-15 16:53:56 -0700

    Improvements to show/set/create implementation. Improve help text.

--- src/azure/cli/commands/resource.py
+++ src/azure/cli/commands/resource.py
@@ -1,6 +1,12 @@
+import json
+from json import JSONDecodeError
+
 from ..commands import command, description, option
 from ._command_creation import get_mgmt_service_client
 from .._locale import L
+    
+from azure.mgmt.resource.resources import (ResourceManagementClient,
+                                           ResourceManagementClientConfiguration)
 
 @command('resource group list')
 @description(L('List resource groups'))
@@ -9,8 +15,6 @@ from .._locale import L
 # @option('--tag-value -g <tagValue>', L('the resource group's tag value'))
 # @option('--top -g <number>', L('Top N resource groups to retrieve'))
 def list_groups(args, unexpected): #pylint: disable=unused-argument
-    from azure.mgmt.resource.resources import (ResourceManagementClient,
-                                              ResourceManagementClientConfiguration)
     from azure.mgmt.resource.resources.models import ResourceGroup, ResourceGroupFilter
 
     rmc = get_mgmt_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
@@ -25,55 +29,89 @@ def list_groups(args, unexpected): #pylint: disable=unused-argument
 @description(L('Show details of a specific resource in a resource group or subscription'))
 @option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
 @option('--name -n <name>', L('the resource name'), required=True)
-@option('--provider-namespace -p <providerNamespace>', L('the resource provider namespace'), required=True)
-@option('--resource-type -r <resourceType>', L('the resource type'), required=True)
+@option('--resource-type -r <resourceType>', L('the resource type in format: <provider-namespace>/<type>'), required=True)
 @option('--api-version -o <apiVersion>', L('the API version of the resource provider'), required=True)
-@option('--parent <parent>', L('the name of the parent resource (if needed), in path/path/path format'))
+@option('--parent <parent>', L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
 def show_resource(args, unexpected):
-    from azure.mgmt.resource.resources import (ResourceManagementClient,
-                                              ResourceManagementClientConfiguration)
-    from azure.mgmt.resource.resources.models import ResourceGroup, ResourceGroupFilter
 
     rmc = get_mgmt_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
+
+    full_type = args.get('resource-type').split('/')
 
     results = rmc.resources.get(
         resource_group_name=args.get('resource-group'),
         resource_name=args.get('name'),
-        resource_provider_namespace=args.get('provider-namespace'),
-        resource_type=args.get('resource-type'),
+        resource_provider_namespace=full_type[0],
+        resource_type=full_type[1],
         api_version=args.get('api-version'),
         parent_resource_path=args.get('parent') or ''
     )
-
-    return list(results)
+    return results
 
 @command('resource set')
 @description(L('Update a resource in a resource group'))
 @option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
 @option('--name -n <name>', L('the resource name'), required=True)
-@option('--resource-type -r <resourceType>', L('the resource type'), required=True)
+@option('--resource-type -r <resourceType>', L('the resource type in format: <provider-namespace>/<type>'), required=True)
 @option('--properties -p <properties>', L('a JSON-formatted string containing properties'), required=True)
 @option('--api-version -o <apiVersion>', L('the API version of the resource provider'), required=True)
-@option('--parent <parent>', L('the name of the parent resource (if needed), in path/path/path format'))
-@option('--tags -t <tags>', L('Tags to set to the source. Can be multiple. In the format of \'name=value\'.' \
+@option('--parent <parent>', L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
+@option('--tags -t <tags>', L('Tags to assign to the resource. Can be multiple. In the format of \'name=value\'.' \
     + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
-@option('--no-tags', L('removes all existing tags'))
-@option('--subscription <subscription>', L('the subscription identifier'))
-def set_resource(args, unexpected):
-    print('  Command: resource set (IN PROGRESS) with args: %s' % args)
-    return None
-
-#@command('resource create')
-#@description(L('Creates a resource in a resource group'))
-#@option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
-#@option('--name -n <name>', L('the resource name'), required=True)
-#@option('--resource-type -r <resourceType>', L('the resource type'), required=True)
-#@option('--location -l <location>', L('the location where the resource will be created'), required=True)
-#@option('--api-version -o <apiVersion>', L('the API version of the resource provider'), required=True)
-#@option('--parent <parent>', L('the name of the parent resource (if needed), in path/path/path format'))
-#@option('--properties -p <properties>', L('a JSON-formatted string containing properties'))
-#@option('--tags -t <tags>', L('Tags to set to the source. Can be multiple. In the format of \'name=value\'.' \
-#    + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
+# TODO: implement
+#@option('--no-tags', L('removes all existing tags'))
 #@option('--subscription <subscription>', L('the subscription identifier'))
-#def create_resource(args, unexpected):
-#    print('  Command: resource create (IN PROGRESS) with args: %s' % args)
+def set_resource(args, unexpected):
+
+    return _create_or_update(args, unexpected)
+
+@command('resource create')
+@description(L('Creates a resource in a resource group'))
+@option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
+@option('--name -n <name>', L('the resource name'), required=True)
+@option('--resource-type -r <resourceType>', L('the resource type in format: <provider-namespace>/<type>'), required=True)
+@option('--location -l <location>', L('the location where the resource will be created'), required=True)
+@option('--api-version -o <apiVersion>', L('the API version of the resource provider'), required=True)
+@option('--parent <parent>', L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
+@option('--properties -p <properties>', L('a JSON-formatted string containing properties'))
+@option('--tags -t <tags>', L('Tags to assign to the resource. Can be multiple. In the format of \'name=value\'.' \
+    + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
+# TODO: implement
+#@option('--subscription <subscription>', L('the subscription identifier'))
+def create_resource(args, unexpected):
+
+    return _create_or_update(args, unexpected)
+
+def _create_or_update(args, unexpected):
+
+    from azure.mgmt.resource.resources.models import GenericResource
+
+    rmc = get_mgmt_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
+
+    full_type = args.get('resource-type').split('/')
+
+    location = args.get('location')
+    if not location:
+        # TODO: retrieve resource and get location from there? Default for now.
+        location = 'West US'
+    
+    try:
+        properties = json.loads(args.get('properties'))
+    except JSONDecodeError as ex:
+        print(L('Invalid property format. Please use: "{\\"foo\\":\\"bar\\"}"'))
+        return None
+    
+    parameters = GenericResource(location,
+                                 tags=args.get('tags') or None,
+                                 properties=properties) 
+
+    results = rmc.resources.create_or_update(
+        resource_group_name=args.get('resource-group'),
+        resource_name=args.get('name'),
+        resource_provider_namespace=full_type[0],
+        resource_type=full_type[1],
+        api_version=args.get('api-version'),
+        parent_resource_path=args.get('parent') or '',
+        parameters=parameters
+    )
+    return results
