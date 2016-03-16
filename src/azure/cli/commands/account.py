6beafeccf41fab7a4e5b4961552bb7e92437e639
yugangw-msft <yugangw@microsoft.com>
2016-03-16 13:23:33 -0700

    support login using aad device flow

--- src/azure/cli/commands/account.py
+++ src/azure/cli/commands/account.py
@@ -12,7 +12,9 @@ def list_subscriptions(args, unexpected): #pylint: disable=unused-argument
 
 @command('account set')
 @description(L('Set the current subscription'))
-@option('--subscription-id -n <subscription-id>', L('Subscription Id, unique name also works.'))
+@option('--subscription-id -n <subscription-id>',
+        L('Subscription Id, unique name also works.'),
+        required=True)
 def set_active_subscription(args, unexpected): #pylint: disable=unused-argument
     subscription_id = args.get('subscription-id')
     if not id:
