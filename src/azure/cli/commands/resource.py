380ad2f92ae08f62442d03b53b3ae56477745ebc
Travis Prescott <trpresco@microsoft.com>
2016-03-18 10:37:28 -0700

    Fix pylint issues.

--- src/azure/cli/commands/resource.py
+++ src/azure/cli/commands/resource.py
@@ -35,10 +35,12 @@ def list_groups(args, unexpected): #pylint: disable=unused-argument
 @description(L('Show details of a specific resource in a resource group or subscription'))
 @option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
 @option('--name -n <name>', L('the resource name'), required=True)
-@option('--resource-type -r <resourceType>', L('the resource type in format: <provider-namespace>/<type>'), required=True)
+@option('--resource-type -r <resourceType>',
+        L('the resource type in format: <provider-namespace>/<type>'), required=True)
 @option('--api-version -o <apiVersion>', L('the API version of the resource provider'))
-@option('--parent <parent>', L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
-def show_resource(args, unexpected):
+@option('--parent <parent>',
+        L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
+def show_resource(args, unexpected): #pylint: disable=unused-argument
     rmc = get_mgmt_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
 
     full_type = args.get('resource-type').split('/')
@@ -47,10 +49,11 @@ def show_resource(args, unexpected):
         resource_type = full_type[1]
     except IndexError:
         raise IncorrectUsageError('Parameter --resource-type must be in <namespace>/<type> format.')
-    
+
     api_version = _resolve_api_version(args, rmc)
     if not api_version:
-        raise IncorrectUsageError(L('API version is required and could not be resolved for resource %s' % full_type))
+        raise IncorrectUsageError(
+            L('API version is required and could not be resolved for resource %s' % full_type))
 
     results = rmc.resources.get(
         resource_group_name=args.get('resource-group'),
@@ -66,16 +69,20 @@ def show_resource(args, unexpected):
 @description(L('Update a resource in a resource group'))
 @option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
 @option('--name -n <name>', L('the resource name'), required=True)
-@option('--resource-type -r <resourceType>', L('the resource type in format: <provider-namespace>/<type>'), required=True)
-@option('--properties -p <properties>', L('a JSON-formatted string containing properties'), required=True)
+@option('--resource-type -r <resourceType>',
+        L('the resource type in format: <provider-namespace>/<type>'), required=True)
+@option('--properties -p <properties>',
+        L('a JSON-formatted string containing properties'), required=True)
 @option('--api-version -o <apiVersion>', L('the API version of the resource provider'))
-@option('--parent <parent>', L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
-@option('--tags -t <tags>', L('Tags to assign to the resource. Can be multiple. In the format of \'name=value\'.' \
-    + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
+@option('--parent <parent>',
+        L('the name of the parent resource (if needed), in <parent-type>/<parent-name> format'))
+@option('--tags -t <tags>',
+        L('Tags to assign to the resource. Can be multiple. In the format of \'name=value\'.' \
+        + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
 # TODO: carried over from Node parameter list
 #@option('--no-tags', L('removes all existing tags'))
 #@option('--subscription <subscription>', L('the subscription identifier'))
-def set_resource(args, unexpected):
+def set_resource(args, unexpected): #pylint: disable=unused-argument
     from azure.mgmt.resource.resources.models import GenericResource
 
     rmc = get_mgmt_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
@@ -92,24 +99,25 @@ def set_resource(args, unexpected):
 
     api_version = _resolve_api_version(args, rmc)
     if not api_version:
-        raise ValueError(L('API version is required and could not be resolved for resource %s' % full_type))
+        raise ValueError(L('API version is required and could not be resolved for resource %s. '
+                           + 'Please specify using option --api-version or -o' % full_type))
 
     resource = rmc.resources.get(
-            resource_group_name=resource_group,
-            resource_name=resource_name,
-            resource_provider_namespace=provider_namespace,
-            resource_type=resource_type,
-            api_version=api_version,
-            parent_resource_path=parent)
+        resource_group_name=resource_group,
+        resource_name=resource_name,
+        resource_provider_namespace=provider_namespace,
+        resource_type=resource_type,
+        api_version=api_version,
+        parent_resource_path=parent)
 
     try:
         properties = json.loads(args.get('properties'))
     except JSONDecodeError as ex:
-        raise ValueError(L('Invalid JSON property format.'))
+        raise ValueError(ex.msg)
 
-    parameters = GenericResource(resource.location,
+    parameters = GenericResource(resource.location, #pylint: disable=no-member
                                  tags=args.get('tags'),
-                                 properties=properties) 
+                                 properties=properties)
 
     results = rmc.resources.create_or_update(
         resource_group_name=resource_group,
@@ -126,16 +134,20 @@ def set_resource(args, unexpected):
 @description(L('Creates a resource in a resource group'))
 @option('--resource-group -g <resourceGroup>', L('the resource group name'), required=True)
 @option('--name -n <name>', L('the resource name'), required=True)
-@option('--resource-type -r <resourceType>', L('the resource type in format: <provider-namespace>/<type>'), required=True)
-@option('--location -l <location>', L('the location where the resource will be created'), required=True)
+@option('--resource-type -r <resourceType>',
+        L('the resource type in format: <provider-namespace>/<type>'), required=True)
+@option('--location -l <location>',
+        L('the location where the resource will be created'), required=True)
 @option('--api-version -o <apiVersion>', L('the API version of the resource provider'))
-@option('--parent <parent>', L('the name of the parent resource (if needed), in <type>/<name> format'))
+@option('--parent <parent>',
+        L('the name of the parent resource (if needed), in <type>/<name> format'))
 @option('--properties -p <properties>', L('a JSON-formatted string containing properties'))
-@option('--tags -t <tags>', L('Tags to assign to the resource. Can be multiple. In the format of \'name=value\'.' \
-    + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
+@option('--tags -t <tags>',
+        L('Tags to assign to the resource. Can be multiple. In the format of \'name=value\'.' \
+          + ' Name is required and value is optional. For example, -t tag1=value1;tag2'))
 # TODO: carried over from Node parameter list
 #@option('--subscription <subscription>', L('the subscription identifier'))
-def create_resource(args, unexpected):
+def create_resource(args, unexpected): #pylint: disable=unused-argument
     from azure.mgmt.resource.resources.models import GenericResource
 
     rmc = get_mgmt_service_client(ResourceManagementClient, ResourceManagementClientConfiguration)
@@ -150,15 +162,16 @@ def create_resource(args, unexpected):
     try:
         properties = json.loads(args.get('properties'))
     except JSONDecodeError as ex:
-        raise ValueError(L('Invalid JSON property format.'))
+        raise ValueError(ex.msg)
 
     parameters = GenericResource(location,
                                  tags=args.get('tags'),
-                                 properties=properties) 
+                                 properties=properties)
 
     api_version = _resolve_api_version(args, rmc)
     if not api_version:
-        raise ValueError(L('API version is required and could not be resolved for resource %s' % full_type))
+        raise ValueError(L('API version is required and could not be resolved for resource %s. '
+                           + 'Please specify using option --api-version or -o' % full_type))
 
     results = rmc.resources.create_or_update(
         resource_group_name=args.get('resource-group'),
@@ -170,12 +183,12 @@ def create_resource(args, unexpected):
         parameters=parameters
     )
     return results
-    
+
 def _resolve_api_version(args, rmc):
     api_version = args.get('api-version')
     if api_version:
         return api_version
-    
+
     # if api-version not supplied, attempt to resolve using provider namespace
     parent = args.get('parent')
     full_type = args.get('resource-type').split('/')
@@ -190,7 +203,7 @@ def _resolve_api_version(args, rmc):
             parent_type = parent.split('/')[0]
         except IndexError:
             raise IncorrectUsageError('Parameter --parent must be in <type>/<name> format.')
-            
+
         resource_type = "%s/%s" % (parent_type, resource_type)
     else:
         resource_type = resource_type
