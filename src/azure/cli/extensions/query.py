9e03509f988f46bda12f3a5e57d48e4c2e302f4a
Johan Stenberg <johan.stenberg@microsoft.com>
2016-03-22 19:39:02 -0700

    Hook up extensions again (including output transform and jmes path querying)

--- src/azure/cli/extensions/query.py
+++ src/azure/cli/extensions/query.py
@@ -1,17 +1,19 @@
-if False: # Temporarily disabled global parameters
-    def register(event_dispatcher):
-        def handle_query_parameter(_, event_data):
-            try:
-                args = event_data['args']
-                query_index = args.index('--query')
-                query_value = args[query_index + 1]
-                del args[query_index:query_index + 1]
-            except ValueError:
-                pass
-            else:
-                def filter_output(_, event_data):
-                    import jmespath
-                    event_data['result'] = jmespath.search(query_value, event_data['result'])
+def register(event_dispatcher):
+    def register_global_parameter(self, parser):
+        # Let the program know that we are adding a parameter --query
+        parser.add_argument('--query', dest='_jmespath_query', metavar='JMESPATH', help='JMESPath query string. See http://jmespath.org/ for more information and examples.')
 
-                event_dispatcher.register(event_dispatcher.FILTER_RESULT, filter_output)
-        event_dispatcher.register(event_dispatcher.REGISTER_GLOBAL_PARAMETERS, handle_query_parameter)
+    def handle_query_parameter(_, args):
+        # Of there is a query specified on the command line, we'll take care of that!
+        query_value = args._jmespath_query
+        del(args._jmespath_query)
+
+        if query_value:
+            def filter_output(_, event_data):
+                import jmespath
+                event_data['result'] = jmespath.search(query_value, event_data['result'])
+
+            event_dispatcher.register(event_dispatcher.FILTER_RESULT, filter_output)
+
+    event_dispatcher.register('GlobalParser.Created', register_global_parameter)
+    event_dispatcher.register('CommandParser.Parsed', handle_query_parameter)
