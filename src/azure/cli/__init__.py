8674e311cae17ed77d2f4fbfe979f2c094980b1e
Derek Bekoe <debekoe@microsoft.com>
2016-03-25 16:08:37 -0700

    Fix pylint errors

--- src/azure/cli/__init__.py
+++ src/azure/cli/__init__.py
@@ -1,9 +1,9 @@
-__import__('pkg_resources').declare_namespace(__name__)
 '''The Azure Command-line tool.
 
 This tools provides a command-line interface to Azure's management and storage
 APIs.
 '''
+__import__('pkg_resources').declare_namespace(__name__)
 
 __author__ = "Microsoft Corporation <python@microsoft.com>"
 __version__ = "0.0.3.dev0"
