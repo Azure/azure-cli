f748a225286a20a1ae0c8413143cb9bb8a5446e7
Derek Bekoe <debekoe@microsoft.com>
2016-03-25 23:10:24 -0700

    Component update and update all

--- src/command_modules/azure-cli-components/azure/cli/command_modules/components/__init__.py
+++ src/command_modules/azure-cli-components/azure/cli/command_modules/components/__init__.py
@@ -7,8 +7,6 @@ from azure.cli._argparse import IncorrectUsageError
 
 COMPONENT_PREFIX = 'azure-cli-'
 
-# TODO:: Support installing/removing of a specific version
-
 @command('components list')
 @description(L('List the installed components.'))
 def list_components(args, unexpected): #pylint: disable=unused-argument
@@ -17,25 +15,24 @@ def list_components(args, unexpected): #pylint: disable=unused-argument
                    if dist.key.startswith(COMPONENT_PREFIX)])
     print('\n'.join(components))
 
-# @command('components check')
-# @description(L('Check a component for an update'))
-# @option('--name -n <name>')
-# def install_component(args, unexpected): #pylint: disable=unused-argument
-#     component_name = args.get('name')
-#     if not component_name:
-#         # Show all
-#         pip.main(['list', '--pre', '--outdated', '--isolated',
-#         '--trusted-host', '40.112.211.51',
-#         '--extra-index-url', 'http://40.112.211.51:8080/'])
-#     else:
-#         try:
-#             __import__('azure.cli.command_modules.'+component_name+'.__main__')
-#             # Check for updates
-#             pip.main(['list', '--pre', '--outdated', '--isolated',
-#             '--trusted-host', '40.112.211.51',
-#             '--find-links', 'http://40.112.211.51:8080/simple/azure-cli-'+component_name])
-#         except ImportError:
-#             raise RuntimeError("Component not installed.")
+def _install_or_update(component_name, version, link, private, upgrade=False):
+    if not component_name:
+        raise IncorrectUsageError(L('Specify a component name.'))
+    found = bool([dist for dist in pip.get_installed_distributions(local_only=False)
+                  if dist.key==COMPONENT_PREFIX+component_name])
+    if found and not upgrade:
+        raise RuntimeError("Component already installed.")
+    else:
+        version_no = '==' + version if version else ''
+        options = ['--quiet', '--isolated', '--disable-pip-version-check']
+        if upgrade:
+            options.append('--upgrade')
+        pkg_index_options = []
+        if link:
+            pkg_index_options += ['--find-links', link]
+        if private:
+            pkg_index_options += ['--extra-index-url', 'http://40.112.211.51:8080/', '--trusted-host', '40.112.211.51']
+        pip.main(['install'] + options + [COMPONENT_PREFIX + component_name+version_no] + pkg_index_options)
 
 @command('components install')
 @description(L('Install a component'))
@@ -47,43 +44,31 @@ file:// url that's a directory,then look for \
 archives in the directory listing."))
 @option('--private -p', L('Get from the project private PyPI server'))
 def install_component(args, unexpected): #pylint: disable=unused-argument
-    component_name = args.get('name')
-    version = args.get('version')
-    link = args.get('link')
-    private = args.get('private')
-    if not component_name:
-        raise IncorrectUsageError(L('Specify a component name.'))
-    if link and private:
-        raise IncorrectUsageError(L("Specify either link or private. Not both."))
-    found = bool([dist for dist in pip.get_installed_distributions(local_only=False)
-                  if dist.key==COMPONENT_PREFIX+component_name])
-    if found:
-        # TODO:: Check for updates
-        raise RuntimeError("Component already installed.")
-    else:
-        version_no = '==' + version if version else ''
-        extra_params = []
-        if link:
-            extra_params = ['--find-links', link]
-        elif private:
-            extra_params = ['--extra-index-url', 'http://40.112.211.51:8080/', '--trusted-host', '40.112.211.51']
-        pip.main(['install', '--quiet', '--isolated', '--disable-pip-version-check',
-                  COMPONENT_PREFIX + component_name+version_no] + extra_params)
+    _install_or_update(args.get('name'), args.get('version'), args.get('link'), args.get('private'), upgrade=False)
 
-# @command('components update')
-# @description(L('Update a component'))
-# @option('--name -n <name>', L('Name of component to install'), required=True)
-# def update_component(args, unexpected): #pylint: disable=unused-argument
-#     component_name = args.get('name')
-#     if not component_name:
-#         raise RuntimeError("Specify a component name.")
-#     try:
-#         __import__('azure.cli.command_modules.'+component_name+'.__main__')
-#         pip.main(['install', '--quiet', '--isolated', '--disable-pip-version-check',
-#                   '--upgrade', 'azure-cli-'+component_name,
-#         '--extra-index-url', 'http://40.112.211.51:8080/', '--trusted-host', '40.112.211.51'])
-#     except ImportError:
-#         raise RuntimeError("Component not installed.")
+@command('components update')
+@description(L('Update a component'))
+@option('--name -n <name>', L('Name of component to install'), required=True)
+@option('--link -l <url>', L("If a url or path to an html file, then parse \
+for links to archives. If a local path or \
+file:// url that's a directory,then look for \
+archives in the directory listing."))
+@option('--private -p', L('Get from the project private PyPI server'))
+def update_component(args, unexpected): #pylint: disable=unused-argument
+    _install_or_update(args.get('name'), None, args.get('link'), args.get('private'), upgrade=True)
+
+@command('components update all')
+@description(L('Update all components'))
+@option('--link -l <url>', L("If a url or path to an html file, then parse \
+for links to archives. If a local path or \
+file:// url that's a directory,then look for \
+archives in the directory listing."))
+@option('--private -p', L('Get from the project private PyPI server'))
+def update_all_components(args, unexpected): #pylint: disable=unused-argument
+    component_names = [dist.key.replace(COMPONENT_PREFIX, '') for dist in pip.get_installed_distributions(local_only=False)
+                   if dist.key.startswith(COMPONENT_PREFIX)]
+    for component_name in component_names:
+        _install_or_update(component_name, None, args.get('link'), args.get('private'), upgrade=True)
 
 @command('components remove')
 @description(L('Remove a component'))
@@ -104,3 +89,25 @@ def remove_component(args, unexpected): #pylint: disable=unused-argument
         pip.main(['uninstall', '--quiet', '--isolated', '--yes', '--disable-pip-version-check', COMPONENT_PREFIX+component_name])
     else:
         raise RuntimeError(L("Component not installed."))
+
+# @command('components check')
+# @description(L('Check a component for an update'))
+# @option('--name -n <name>')
+# def check_component(args, unexpected): #pylint: disable=unused-argument
+
+#     component_name = args.get('name')
+#     if not component_name:
+#         # Show all
+#         pip.main(['list', '--pre', '--outdated', '--isolated',
+#         '--trusted-host', '40.112.211.51',
+#         '--extra-index-url', 'http://40.112.211.51:8080/'])
+#     else:
+#         try:
+#             __import__('azure.cli.command_modules.'+component_name+'.__main__')
+#             # Check for updates
+#             pip.main(['list', '--pre', '--outdated', '--isolated',
+#             '--trusted-host', '40.112.211.51',
+#             '--find-links', 'http://40.112.211.51:8080/simple/azure-cli-'+component_name])
+#         except ImportError:
+#             raise RuntimeError("Component not installed.")
+
