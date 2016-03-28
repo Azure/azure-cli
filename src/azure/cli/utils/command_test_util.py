3348491e54165097aa3e60c8b4c740eb74ac0198
Derek Bekoe <debekoe@microsoft.com>
2016-03-28 13:13:46 -0700

    Include 'components' and 'login' by default & fix pylint issues

--- src/azure/cli/utils/command_test_util.py
+++ src/azure/cli/utils/command_test_util.py
@@ -1,15 +1,13 @@
-import os
-import sys
-import unittest
-import re
-import vcr
 import logging
+import re
 
-from six import add_metaclass, StringIO
 try:
     import unittest.mock as mock
 except ImportError:
     import mock
+
+from six import StringIO
+import vcr
 
 from azure.cli.main import main as cli
 
@@ -42,11 +40,17 @@ class CommandTestGenerator(object):
         test_functions = {}
         def gen_test(test_name, command, expected_result):
 
-            def load_subscriptions_mock(self):
-                return [{"id": "00000000-0000-0000-0000-000000000000", "user": "example@example.com", "access_token": "access_token", "state": "Enabled", "name": "Example", "active": True}]
+            def load_subscriptions_mock(self): #pylint: disable=unused-argument
+                return [{"id": "00000000-0000-0000-0000-000000000000",
+                         "user": "example@example.com",
+                         "access_token": "access_token",
+                         "state": "Enabled",
+                         "name": "Example",
+                         "active": True}]
 
             @mock.patch('azure.cli._profile.Profile.load_subscriptions', load_subscriptions_mock)
-            @self.my_vcr.use_cassette('%s.yaml'%test_name, filter_headers=CommandTestGenerator.FILTER_HEADERS)
+            @self.my_vcr.use_cassette('%s.yaml'%test_name,
+                                      filter_headers=CommandTestGenerator.FILTER_HEADERS)
             def test(self):
                 io = StringIO()
                 cli(command.split(), file=io)
@@ -57,12 +61,14 @@ class CommandTestGenerator(object):
 
         for test_spec_item in self.test_specs:
             test_name = 'test_%s' % test_spec_item['test_name']
-            test_functions[test_name] = gen_test(test_name, test_spec_item['command'], test_spec_item['expected_result'])
+            test_functions[test_name] = gen_test(test_name, test_spec_item['command'],
+                                                 test_spec_item['expected_result'])
         return test_functions
 
     @staticmethod
     def before_record_request(request):
-        request.uri = re.sub('/subscriptions/([^/]+)/', '/subscriptions/00000000-0000-0000-0000-000000000000/', request.uri)
+        request.uri = re.sub('/subscriptions/([^/]+)/',
+                             '/subscriptions/00000000-0000-0000-0000-000000000000/', request.uri)
         return request
 
     @staticmethod
