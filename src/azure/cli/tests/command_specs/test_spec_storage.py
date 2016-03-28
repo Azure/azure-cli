c1d323be47aeca58575c76504c48e9c04a1d9e86
Travis Prescott <trpresco@microsoft.com>
2016-03-28 09:07:48 -0700

    Update storage test logic to skip prompts if 'quiet' arg specified. Add more tests.

--- src/azure/cli/tests/command_specs/test_spec_storage.py
+++ src/azure/cli/tests/command_specs/test_spec_storage.py
@@ -1,11 +1,23 @@
+RESOURCE_GROUP_NAME = 'travistestresourcegroup'
+STORAGE_ACCOUNT_NAME = 'travistestresourcegr3014'
+
 definition = [
     {
         'test_name': 'storage_account_usage',
         'command': 'storage account usage',
-    }
-    ,{
+    },
+    {
         'test_name': 'storage_account_list',
         'command': 'storage account list'
+    },
+    {
+        'test_name': 'storage_account_check_name',
+        'command': 'storage account check-name-availability --name teststorageomega'
+    },
+    {
+        'test_name': 'storage_account_list_keys',
+        'command': 'storage account list-keys --rg {} --account_name {}'
+            .format(RESOURCE_GROUP_NAME, STORAGE_ACCOUNT_NAME)
     }
 ]
 
