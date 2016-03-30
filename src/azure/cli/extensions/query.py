9e96539485020a51bae29911a01a90aa77566a6b
Johan Stenberg <johan.stenberg@microsoft.com>
2016-03-30 11:39:37 -0700

    Fix line-too-long error

--- src/azure/cli/extensions/query.py
+++ src/azure/cli/extensions/query.py
@@ -12,6 +12,7 @@ def register(event_dispatcher):
         else:
             def filter_output(_, event_data):
                 import jmespath
-                event_data['result'] = jmespath.search(query_value, event_data['result'], jmespath.Options(collections.OrderedDict))
+                event_data['result'] = jmespath.search(query_value, event_data['result'],
+                                                       jmespath.Options(collections.OrderedDict))
             event_dispatcher.register(event_dispatcher.FILTER_RESULT, filter_output)
     event_dispatcher.register(event_dispatcher.REGISTER_GLOBAL_PARAMETERS, handle_query_parameter)
