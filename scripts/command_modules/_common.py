bcfafc2c163eef8ee083324702f9c686aee644ca
Derek Bekoe <debekoe@microsoft.com>
2016-03-30 16:30:28 -0700

    Modify scripts to use helper module

---
+++ scripts/command_modules/_common.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import os
+import sys
+
+from subprocess import check_call, CalledProcessError
+
+COMMAND_MODULE_PREFIX = 'azure-cli-'
+
+def get_all_command_modules():
+    # The prefix for the command module folders
+    PATH_TO_COMMAND_MODULES = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..', '..', 'src' , 'command_modules'))
+    all_command_modules = [(name, os.path.join(PATH_TO_COMMAND_MODULES, name))
+                            for name in os.listdir(PATH_TO_COMMAND_MODULES)
+                            if name.startswith(COMMAND_MODULE_PREFIX) and os.path.isdir(os.path.join(PATH_TO_COMMAND_MODULES, name))]
+    print(str(len(all_command_modules))+" command module(s) found...")
+    print([name for name, fullpath in all_command_modules])
+    return all_command_modules
+
+def exec_command(command, stdout=None):
+    '''Returns True in the command was executed successfully'''
+    try:
+        print(command)
+        check_call(command, stdout=stdout, shell=True)
+        return True
+    except CalledProcessError as err:
+        print(err, file=sys.stderr)
+        return False
+
+def print_summary(failed_modules):
+    print()
+    print("SUMMARY")
+    print("-------")
+    if failed_modules:
+        print(str(len(failed_modules))+" module(s) FAILED...", file=sys.stderr)
+        print("Failed modules: " + ', '.join(failed_modules), file=sys.stderr)
+        sys.exit(1)
+    else:
+        print("OK")
