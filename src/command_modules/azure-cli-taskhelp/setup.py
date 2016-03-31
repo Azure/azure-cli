21e48f062aec33970dd218e691ce87a0acc96fcb
Derek Bekoe <debekoe@microsoft.com>
2016-03-31 16:47:51 -0700

    Update versions of CLI and command packages

--- src/command_modules/azure-cli-taskhelp/setup.py
+++ src/command_modules/azure-cli-taskhelp/setup.py
@@ -18,7 +18,7 @@
 from codecs import open
 from setuptools import setup
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
