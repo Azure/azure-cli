0162458dda2b971621088c8118f5d3e558292a86
Derek Bekoe <debekoe@microsoft.com>
2016-04-05 16:29:47 -0700

    Remove unused ref to logger

--- src/command_modules/azure-cli-components/azure/cli/command_modules/components/__init__.py
+++ src/command_modules/azure-cli-components/azure/cli/command_modules/components/__init__.py
@@ -6,7 +6,6 @@ from six.moves import input #pylint: disable=redefined-builtin
 from azure.cli.parser import IncorrectUsageError
 from azure.cli.commands import CommandTable, COMMON_PARAMETERS
 from azure.cli._locale import L
-from azure.cli._logging import logger
 
 from azure.cli.utils.update_checker import check_for_component_update, UpdateCheckError
 
