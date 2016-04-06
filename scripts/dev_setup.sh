ed2b62d33a3fcc30333dba93b84ba141d7ad42a1
Derek Bekoe <debekoe@microsoft.com>
2016-04-06 12:34:35 -0700

    Make the script a python script so it's cross-platform

--- scripts/dev_setup.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -e
-echo "Running dev setup..."
-echo "Executing: pip install -r requirements.txt"
-pip install -r requirements.txt
-echo "Executing: pip install -e ."
-pip install -e .
-echo "python scripts/command_modules/install.py"
-python scripts/command_modules/install.py 
