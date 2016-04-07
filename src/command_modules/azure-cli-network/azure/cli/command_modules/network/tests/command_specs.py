d00414a86f3e15aedd3265a007d23c03e565bbc9
Travis Prescott <trpresco@microsoft.com>
2016-04-07 10:35:25 -0700

    Update command_test_util and start on reworking network tests.

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/tests/command_specs.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/tests/command_specs.py
@@ -1,15 +1,15 @@
-from . import TEST_DEF, load_test_definitions
+# AZURE CLI NETWORK TEST DEFINITIONS
 
-load_test_definitions(
-    package_name=locals()['__name__'],
-    definition = [
-        {
-            'test_name': 'network_usage_list',
-            'command': 'network usage list --location westus --output json'
-        },
-        {
-            'test_name': 'network_nic_list',
-            'command': 'network nic list -g travistestresourcegroup'
-        }
-    ]
-)
+ENV_VAR = {}
+
+TEST_DEF = [
+    {
+        'test_name': 'network_usage_list',
+        'command': 'network usage list --location westus --output json'
+    },
+    {
+        'test_name': 'network_nic_list',
+        'command': 'network nic list -g travistestresourcegroup'
+    }
+]
+
