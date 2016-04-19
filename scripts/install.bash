d3a0a7b6a2b554b98c4e843b118889de07f5eb15
Derek Bekoe <debekoe@microsoft.com>
2016-04-19 10:20:36 -0700

    Run pylint on install.py file and fix warnings & CR changes.
    - bash script now points to nightly build server
    - create temp file instead of temp dir
    - just run default python
    -

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,27 +2,18 @@
 # 
 # Bash script to install the Azure CLI
 #
-INSTALL_SCRIPT_URL=http://12.345.678.910/azure-cli-install.py
+INSTALL_SCRIPT_URL=http://azure-cli-nightly.cloudapp.net/install.py
 _TTY=/dev/tty
 
-temp_dir=$(mktemp -d) || exit
-echo "Created temp directory at $temp_dir"
-install_script=$temp_dir/install.py
-echo "Downloading Azure CLI install script from $INSTALL_SCRIPT_URL"
+install_script=$(mktemp -t azure_cli_install_tmp_XXXX) || exit
+echo "Downloading Azure CLI install script from $INSTALL_SCRIPT_URL to $install_script."
 curl -# $INSTALL_SCRIPT_URL > $install_script || exit
 chmod 775 $install_script
-echo "Running install script from $install_script"
-
-if which python3 > /dev/null 2>&1;
-then
-    PYTHON=python3
-else
-    PYTHON=python
-fi
+echo "Running install script."
 
 if [[ -z "$AZURE_CLI_DISABLE_PROMPTS" && -t 1 ]]; then
-    $PYTHON $install_script < $_TTY
+    $install_script < $_TTY
 else
     export AZURE_CLI_DISABLE_PROMPTS=1
-    $PYTHON $install_script
+    $install_script
 fi
