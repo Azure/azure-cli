dfab27056ed2772a80f5999a7dea42aa4bd1f1a1
Johan Stenberg <johan.stenberg@microsoft.com>
2016-04-20 17:08:53 -0700

    Respond to code review feedback

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/__init__.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/__init__.py
@@ -1,5 +1,5 @@
 from .generated import command_table as generated_command_table
-from .convenience import command_table as convenience_command_table
+from .custom import command_table as convenience_command_table
 
 command_table = generated_command_table
 command_table.update(convenience_command_table)
