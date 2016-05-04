1a407f6507c4c0fa3f2f93fb2c7080c8d23f48a9
Burt Bielicki <burtbiel@microsoft.com>
2016-05-04 15:29:34 -0700

    more workaround

--- src/azure/cli/tests/test_help.py
+++ src/azure/cli/tests/test_help.py
@@ -472,12 +472,12 @@ Global Arguments
         # there is an argparse bug on <2.7.10 where SystemExit is not thrown on missing required param
         if sys.version_info < (2, 7, 10):
             app.execute('n1 -fb a --foobar value'.split())
+            app.execute('n1 -fb a --foobar2 value --foobar3 extra'.split())
         else:
             with self.assertRaises(SystemExit):
                 app.execute('n1 -fb a --foobar value'.split())
-
-        with self.assertRaises(SystemExit):
-            app.execute('n1 -fb a --foobar2 value --foobar3 extra'.split())
+            with self.assertRaises(SystemExit):
+                app.execute('n1 -fb a --foobar2 value --foobar3 extra'.split())
 
         self.assertTrue('required' in io.getvalue()
                         and '--foobar/-fb' not in io.getvalue()
