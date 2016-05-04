0b1dc6a0a16e611fe4b7db1265c214e5077f297a
Burt Bielicki <burtbiel@microsoft.com>
2016-05-04 10:15:38 -0700

    add choices and defaults to swagger.  Since the choices and default values show in the swagger comments, I am not adding the choices to the choice list

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/mgmt/lib/models/deployment_vm.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/mgmt/lib/models/deployment_vm.py
@@ -38,7 +38,8 @@ class DeploymentVM(Model):
      used to access the Virtual Machine.
     :type dns_name_for_public_ip: str
     :param storage_account_type: Whether to use an existing storage account
-     or create a new one.
+     or create a new one. Possible values include: 'new', 'existing'. Default
+     value: "new" .
     :type storage_account_type: str
     :param os_disk_uri: URI for a custom VHD image.
     :type os_disk_uri: str
@@ -48,7 +49,8 @@ class DeploymentVM(Model):
     :param name: The VM resource name.
     :type name: str
     :param virtual_network_type: Whether to use an existing VNet or create a
-     new one.
+     new one. Possible values include: 'new', 'existing'. Default value:
+     "new" .
     :type virtual_network_type: str
     :param admin_password: Password for the Virtual Machine.  Required if SSH
      (Linux only) is not specified.
@@ -58,7 +60,10 @@ class DeploymentVM(Model):
     :param subnet_name: The subnet name.
     :type subnet_name: str
     :param os_type: Common OS choices.  Choose 'Custom' to specify an image
-     with the osPublisher, osOffer, osSKU, and osVersion parameters.
+     with the osPublisher, osOffer, osSKU, and osVersion parameters. Possible
+     values include: 'CentOS', 'CoreOS', 'Debian', 'openSUSE', 'RHEL',
+     'SLES', 'UbuntuLTS', 'Win2012R2Datacenter', 'Win2012Datacenter',
+     'Win2008R2SP1', 'Custom'. Default value: "Win2012R2Datacenter" .
     :type os_type: str
     :param admin_username: Username for the Virtual Machine.
     :type admin_username: str
@@ -71,10 +76,12 @@ class DeploymentVM(Model):
     :param os_offer: The OS Offer to install.
     :type os_offer: str
     :param public_ip_address_allocation: Dynamic or Static public IP address
-     allocation.
+     allocation. Possible values include: 'Dynamic', 'Static'. Default value:
+     "Dynamic" .
     :type public_ip_address_allocation: str
     :param authentication_type: Authentication method: password-only or add
-     ssh-keys (Linux-only).
+     ssh-keys (Linux-only). Possible values include: 'password', 'sshkey'.
+     Default value: "password" .
     :type authentication_type: str
     :param storage_account_name: Name of storage account for the VM OS disk.
     :type storage_account_name: str
@@ -83,7 +90,8 @@ class DeploymentVM(Model):
     :param size: The VM Size that should be created  (e.g. Standard_A2)
     :type size: str
     :param public_ip_address_type: Use a public IP Address for the VM Nic.
-     (new, existing or none).
+     (new, existing or none). Possible values include: 'none', 'new',
+     'existing'. Default value: "none" .
     :type public_ip_address_type: str
     :param virtual_network_ip_address_prefix: The IP address prefix.
     :type virtual_network_ip_address_prefix: str
@@ -96,12 +104,14 @@ class DeploymentVM(Model):
     :param os_publisher: The OS publisher of the OS image.
     :type os_publisher: str
     :param availability_set_type: Flag to add the VM to an existing
-     availability set.
+     availability set. Possible values include: 'none', 'existing'. Default
+     value: "none" .
     :type availability_set_type: str
     :param public_ip_address_name: Name of public IP address to use.
     :type public_ip_address_name: str
     :param dns_name_type: Associate VMs with a public IP address to a DNS
-     name (new or none).
+     name (new or none). Possible values include: 'none', 'new'. Default
+     value: "none" .
     :type dns_name_type: str
     :ivar mode: Gets or sets the deployment mode. Default value:
      "Incremental" .
@@ -161,7 +171,7 @@ class DeploymentVM(Model):
 
     mode = "Incremental"
 
-    def __init__(self, name, admin_username, content_version=None, storage_container_name=None, virtual_network_name=None, subnet_ip_address_prefix=None, private_ip_address_allocation=None, dns_name_for_public_ip=None, storage_account_type=None, os_disk_uri=None, virtual_network_type=None, admin_password=None, os_sku=None, subnet_name=None, os_type=None, os_version=None, os_disk_name=None, ssh_key_path=None, os_offer=None, public_ip_address_allocation=None, authentication_type=None, storage_account_name=None, storage_redundancy_type=None, size=None, public_ip_address_type=None, virtual_network_ip_address_prefix=None, availability_set_id=None, ssh_key_value=None, location=None, os_publisher=None, availability_set_type=None, public_ip_address_name=None, dns_name_type=None):
+    def __init__(self, name, admin_username, content_version=None, storage_container_name=None, virtual_network_name=None, subnet_ip_address_prefix=None, private_ip_address_allocation=None, dns_name_for_public_ip=None, storage_account_type="new", os_disk_uri=None, virtual_network_type="new", admin_password=None, os_sku=None, subnet_name=None, os_type="Win2012R2Datacenter", os_version=None, os_disk_name=None, ssh_key_path=None, os_offer=None, public_ip_address_allocation="Dynamic", authentication_type="password", storage_account_name=None, storage_redundancy_type=None, size=None, public_ip_address_type="none", virtual_network_ip_address_prefix=None, availability_set_id=None, ssh_key_value=None, location=None, os_publisher=None, availability_set_type="none", public_ip_address_name=None, dns_name_type="none"):
         self.content_version = content_version
         self.storage_container_name = storage_container_name
         self.virtual_network_name = virtual_network_name
