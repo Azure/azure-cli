065f365108fc0bf9ea84d0b26d4c38ec030f1135
Derek Bekoe <derek@derekbekoe.com>
2016-05-06 10:43:12 -0700

    Feature logging tracing 3 (#210)
    * Add TRACING to CLI; make --verbose and --debug flags
    
    * Handle CLI exceptions that occur during normal operation
    
    * Resolve pylint messages
    
    * Should show logging message as a warning instead of using print statement
    
    * Support colors of log output
    
    * Catch adal error on login
    
    * Support windows stream coloring
    
    * Fix pylint warnings
    
    * More tracing to long running operations and remove the dots (......)
    
    * More descriptive logger.info message when module can't be loaded.
    
    * Raise CLIError instead of RuntimeError & fix pylint errors

--- src/azure/cli/commands/_command_creation.py
+++ src/azure/cli/commands/_command_creation.py
@@ -1,11 +1,15 @@
 ﻿from .._profile import Profile
 import azure.cli._debug as _debug
 import azure.cli as cli
+import azure.cli._logging as _logging
+
+logger = _logging.get_az_logger(__name__)
 
 def get_mgmt_service_client(client_type, config_type):
+    logger.info('Getting management service client client_type=%s, config_type=%s',
+                client_type.__name__, config_type.__name__)
     profile = Profile()
     config = config_type(*profile.get_login_credentials())
-
     client = client_type(config)
     _debug.allow_debug_connection(client)
     client.config.add_user_agent("AZURECLI_{}".format(cli.__version__))
@@ -14,6 +18,7 @@ def get_mgmt_service_client(client_type, config_type):
 
 def get_data_service_client(service_type, account_name, account_key, connection_string=None,
                             sas_token=None):
+    logger.info('Getting data service client service_type=%s', service_type.__name__)
     client = service_type(account_name=account_name,
                           account_key=account_key,
                           connection_string=connection_string,
