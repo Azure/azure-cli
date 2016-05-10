58ba2374aba11dcd111fbb99e2d392eb3613ac78
Burt Bielicki <burtbiel@microsoft.com>
2016-05-10 09:41:25 -0700

    remove debug statements

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/generated.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/generated.py
@@ -260,12 +260,9 @@ def _handle_auth_types(data):
         if args.admin_password:
             raise CLIError('Admin password cannot be used with SSH authentication type')
 
-        ssh_key_file = os.path.expanduser('~/.ssh/id_rsa')
-        print('FILE: ' + ssh_key_file)
-        print('IS FILE: ' + str(os.path.isfile(ssh_key_file)))
+        ssh_key_file = os.path.join(os.path.expanduser('~'), '.ssh/id_rsa.pub')
         if os.path.isfile(ssh_key_file):
             with open(ssh_key_file) as file:
-                print('DOING IT')
                 args.ssh_key_value = file.read()
 
 Application.INSTANCE.register(Application.COMMAND_PARSER_PARSED, _handle_auth_types)
