b7089983c5674c12595a0d7162f6e62824023c90
Burt Bielicki <burtbiel@microsoft.com>
2016-05-19 15:12:36 -0700

    Create webapp command

--- src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/generated.py
+++ src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/generated.py
@@ -8,6 +8,11 @@ from azure.cli.command_modules.webapp._params import (_web_client_factory)
 from azure.cli.commands._auto_command import build_operation, CommandDefinition
 from azure.cli.commands import CommandTable, LongRunningOperation
 from azure.cli._locale import L
+from azure.cli.command_modules.webapp.mgmt_webapp.lib \
+    import (WebAppCreationClient as WebAppClient,
+            WebAppCreationClientConfiguration as WebAppClientConfig)
+from azure.cli.command_modules.webapp.mgmt_webapp.lib.operations import WebAppOperations
+
 
 command_table = CommandTable()
 
@@ -16,3 +21,19 @@ build_operation(
     [
         CommandDefinition(SitesOperations.get_sites, '[Site]'),
     ], command_table)
+
+class DeploymentOutputLongRunningOperation(LongRunningOperation): #pylint: disable=too-few-public-methods
+    def __call__(self, poller):
+        result = super(DeploymentOutputLongRunningOperation, self).__call__(poller)
+        return result.properties.outputs
+
+build_operation(
+    'webapp', 'web_app', lambda **_: get_mgmt_service_client(WebAppClient, WebAppClientConfig),
+    [
+        CommandDefinition(
+            WebAppOperations.create_or_update,
+            DeploymentOutputLongRunningOperation(L('Creating web application'),
+                                                 L('Web application created')),
+            'create')
+    ],
+    command_table, {'name': {'name': '--name -n'}})
