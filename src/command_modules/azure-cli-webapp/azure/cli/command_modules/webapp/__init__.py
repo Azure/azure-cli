646325ebb445268a462d2bd8aefa5d52b061eaed
Derek Bekoe <derek@derekbekoe.com>
2016-05-19 12:37:26 -0700

    Create webapp module with appropriate structure (#297)

---
+++ src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/__init__.py
@@ -0,0 +1,2 @@
+# pylint: disable=unused-import
+from .generated import command_table
---
+++ src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/generated.py
@@ -0,0 +1,18 @@
+﻿#pylint: disable=unused-import
+from azure.mgmt.web.operations import (
+    SitesOperations)
+
+from azure.cli.commands._command_creation import get_mgmt_service_client
+from azure.cli.command_modules.webapp.custom import ConvenienceWebCommands
+from azure.cli.command_modules.webapp._params import (_web_client_factory)
+from azure.cli.commands._auto_command import build_operation, CommandDefinition
+from azure.cli.commands import CommandTable, LongRunningOperation
+from azure.cli._locale import L
+
+command_table = CommandTable()
+
+build_operation(
+    'webapp', 'webapps', _web_client_factory,
+    [
+        CommandDefinition(SitesOperations.get_sites, '[Site]'),
+    ], command_table)
