5866ab8b28fc38cad372461076c6c1b3b7c705eb
Derek Bekoe <debekoe@microsoft.com>
2016-05-19 10:49:33 -0700

    Create webapp module with appropriate structure

---
+++ src/command_modules/azure-cli-webapp/README.rst
@@ -0,0 +1,7 @@
+Microsoft Azure CLI 'webapp' Command Module
+==================================
+
+This package is for the 'webapp' module.
+i.e. 'az webapp'
+
+This package has [not] been tested [much] with Python 2.7, 3.4 and 3.5.
---
+++ src/command_modules/azure-cli-webapp/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+#-------------------------------------------------------------------------
+# Copyright (c) Microsoft.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#--------------------------------------------------------------------------
+
+from codecs import open
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+# The full list of classifiers is available at
+# https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    #'License :: OSI Approved :: Apache Software License',
+    #'License :: OSI Approved :: MIT License',
+]
+
+DEPENDENCIES = [
+    'azure==2.0.0rc3',
+]
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    README = f.read()
+
+setup(
+    name='azure-cli-webapp',
+    version=VERSION,
+    description='Microsoft Azure Command-Line Tools',
+    long_description=README,
+    license='TBD',
+    author='Microsoft Corporation',
+    author_email='azpycli@microsoft.com',
+    url='https://github.com/Azure/azure-cli',
+    classifiers=CLASSIFIERS,
+    namespace_packages = [
+        'azure',
+        'azure.cli',
+        'azure.cli.command_modules',
+    ],
+    packages=[
+        'azure.cli.command_modules.webapp',
+    ],
+    install_requires=DEPENDENCIES,
+)
