76291ebb580ecef3358a57b476c038359cdf0bde
Johan Stenberg <johan.stenberg@microsoft.com>
2016-06-07 16:51:28 -0700

    Remove need to import and use command table when registering command.

--- src/command_modules/azure-cli-profile/azure/cli/command_modules/profile/generated.py
+++ src/command_modules/azure-cli-profile/azure/cli/command_modules/profile/generated.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 
-from azure.cli.commands import command_table, cli_command
+from azure.cli.commands import cli_command
 
 from .custom import login, logout, list_subscriptions, set_active_subscription, account_clear
 
-cli_command(command_table, 'login', login)
-cli_command(command_table, 'logout', logout)
+cli_command('login', login)
+cli_command('logout', logout)
 
-cli_command(command_table, 'account list', list_subscriptions)
-cli_command(command_table, 'account set', set_active_subscription)
-cli_command(command_table, 'account clear', account_clear)
+cli_command('account list', list_subscriptions)
+cli_command('account set', set_active_subscription)
+cli_command('account clear', account_clear)
--- src/command_modules/azure-cli-taskhelp/azure/cli/command_modules/taskhelp/generated.py
+++ src/command_modules/azure-cli-taskhelp/azure/cli/command_modules/taskhelp/generated.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
-from azure.cli.commands import cli_command, command_table
+from azure.cli.commands import cli_command
 
 from .custom import deploy_arm_template
 
-cli_command(command_table, 'taskhelp deploy-arm-template', deploy_arm_template)
+cli_command('taskhelp deploy-arm-template', deploy_arm_template)
