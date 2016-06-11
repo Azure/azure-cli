d7bec88009b73a57124dbfacc91446927328abf9
Johan Stenberg <johan.stenberg@microsoft.com>
2016-06-10 17:23:54 -0700

    Fix broken NSG create command (duplicate --name parameter)

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/custom.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/custom.py
@@ -12,13 +12,13 @@ def create_update_subnet(resource_group_name, subnet_name, virtual_network_name,
 def create_update_nsg_rule(resource_group_name, network_security_group_name, security_rule_name,
                            protocol, source_address_prefix, destination_address_prefix,
                            access, direction, source_port_range, destination_port_range,
-                           description=None, priority=None, name=None):
+                           description=None, priority=None):
     settings = SecurityRule(protocol=protocol, source_address_prefix=source_address_prefix,
                             destination_address_prefix=destination_address_prefix, access=access,
                             direction=direction,
                             description=description, source_port_range=source_port_range,
                             destination_port_range=destination_port_range, priority=priority,
-                            name=name)
+                            name=security_rule_name)
     ncf = _network_client_factory()
     return ncf.security_rules.create_or_update(
         resource_group_name, network_security_group_name, security_rule_name, settings)
