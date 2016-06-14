eb29aed57f3a65ec0721150492b292bf58d11c83
Derek Bekoe <debekoe@microsoft.com>
2016-06-14 16:10:39 -0700

    Resource completion for other resources

--- src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/_params.py
+++ src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/_params.py
@@ -1,8 +1,13 @@
 # pylint: disable=line-too-long
 import argparse
 
+from argcomplete.completers import FilesCompleter
+
 from azure.cli.commands import register_cli_argument, CliArgumentType
-from azure.cli.commands.parameters import resource_group_name_type, tag_type, tags_type
+from azure.cli.commands.parameters import (resource_group_name_type,
+                                           tag_type,
+                                           tags_type,
+                                           get_resource_group_completion_list)
 
 from ._validators import validate_resource_type, validate_parent, resolve_resource_parameters
 
@@ -21,9 +26,14 @@ register_cli_argument('resource', 'resource_type', resource_type_type)
 register_cli_argument('resource', 'parent_resource_path', CliArgumentType(help='The parent resource type in <type>/<name> format.', type=validate_parent, required=False, options_list=('--parent',)))
 register_cli_argument('resource', 'tag', tag_type)
 register_cli_argument('resource', 'tags', tags_type)
+
 register_cli_argument('resource deploy', 'mode', CliArgumentType(
     choices=['incremental', 'complete'], default='incremental',
     help='Incremental (only add resources to resource group) or Complete (remove extra resources from resource group)'))
+register_cli_argument('resource deploy', 'resource_group', completer=get_resource_group_completion_list)
+register_cli_argument('resource deploy', 'parameters_file_path', completer=FilesCompleter())
+register_cli_argument('resource deploy', 'template_file_path', completer=FilesCompleter())
+
 register_cli_argument('resource provider', 'top', CliArgumentType(help=argparse.SUPPRESS))
 register_cli_argument('resource provider', 'resource_provider_namespace', CliArgumentType(options_list=('--namespace', '-n'), help='the resource provider namespace to retrieve'))
 
@@ -32,7 +42,7 @@ register_cli_argument('resource group deployment', 'resource_group_name', arg_ty
 register_cli_argument('resource group deployment', 'deployment_name', CliArgumentType(options_list=('--name', '-n'), required=True))
 register_cli_argument('resource group export', 'include_comments', CliArgumentType(action='store_true'))
 register_cli_argument('resource group export', 'include_parameter_default_value', CliArgumentType(action='store_true'))
-register_cli_argument('resource group create', 'resource_group_name', CliArgumentType(completer=lambda _: ()))
+register_cli_argument('resource group create', 'resource_group_name', completer=None)
 
 register_cli_argument('tag', 'tag_name', CliArgumentType(options_list=('--name', '-n')))
 register_cli_argument('tag', 'tag_value', CliArgumentType(options_list=('--value',)))
