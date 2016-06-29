0543d7d0a25e65e2a9c19637446e8eddc5e5d1a2
Johan Stenberg <johan.stenberg@microsoft.com>
2016-06-28 19:18:29 -0700

    Add simple vm command parameter consistency check

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/test_vm_parameters.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/test_vm_parameters.py
@@ -43,7 +43,8 @@ class Test_ArgumentParser(unittest.TestCase):
 
         # If we use an ID as the positional parameter, we should
         # extract the resource group and name from it...
-        args = mock_echo_args('vm show', '/subscriptions/00000000-0000-0000-0000-0123456789abc/resourceGroups/thisisaresourcegroup/providers/Microsoft.Compute/virtualMachines/thisisavmname')
+        args = mock_echo_args('vm show',
+                              '/subscriptions/00000000-0000-0000-0000-0123456789abc/resourceGroups/thisisaresourcegroup/providers/Microsoft.Compute/virtualMachines/thisisavmname') # pylint: disable=line-too-long
         self.assertDictEqual({
             'api_version': '2016-03-30',
             'expand': None,
@@ -69,12 +70,36 @@ class Test_ArgumentParser(unittest.TestCase):
             'resource_group_name': None,
             }, args)
 
-        # if resource group name is specified, however, 
+        # if resource group name is specified, however,
         # it should get passed through...
         args = mock_echo_args('vm list', '-g hullo')
         self.assertDictEqual({
             'resource_group_name': 'hullo',
             }, args)
+
+    consistent_arguments = {
+        'resource_group_name': ('--resource-group', '-g'),
+        'virtual_machine_name': ('--vm-name',),
+        }
+
+    def test_command_consistency(self):
+        argv = ['vm']
+        APPLICATION.initialize(Configuration(argv))
+        command_table = APPLICATION.configuration.get_command_table()
+        vm_commands = ((vm_command, metadata) for vm_command, metadata
+                       in command_table.items() if vm_command.startswith('vm'))
+
+        for command_name, command_metadata in vm_commands:
+            for argument_name, expected_options_list in self.consistent_arguments.items():
+                try:
+                    actual_options_list = command_metadata.arguments[argument_name].options_list
+                    self.assertEqual(actual_options_list, expected_options_list,
+                                     'Argument {} of command {} has inconsistent flags'.format(
+                                         argument_name,
+                                         command_name
+                                         ))
+                except KeyError:
+                    pass
 
 if __name__ == '__main__':
     unittest.main()
