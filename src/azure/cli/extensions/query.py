c967d8da4a8269995c949777872c02b857507b3a
Derek Bekoe <derek@derekbekoe.com>
2016-06-30 11:17:47 -0700

    Use argparse 'type' to compile jmespath query instead of doing it after parsing (#480)

--- src/azure/cli/extensions/query.py
+++ src/azure/cli/extensions/query.py
@@ -1,33 +1,40 @@
 import collections
 
-from azure.cli._util import CLIError
+def jmespath_type(raw_query):
+    '''Compile the query with JMESPath and return the compiled result.
+       JMESPath raises exceptions which subclass from ValueError.
+       In addition though, JMESPath can raise a KeyError.
+       ValueErrors are caught by argparse so argument errors can be generated.
+    '''
+    from jmespath import compile as compile_jmespath
+    try:
+        return compile_jmespath(raw_query)
+    except KeyError:
+        # Raise a ValueError which argparse can handle
+        raise ValueError
 
 def _register_global_parameter(global_group):
+    # Argparse uses __name__ of the function used for 'type' when generating error message.
+    # We set __name__ for our function here.
+    jmespath_type.__name__ = 'query'
     # Let the program know that we are adding a parameter --query
     global_group.add_argument('--query', dest='_jmespath_query', metavar='JMESPATH',
-                              help='JMESPath query string. See http://jmespath.org/ for more information and examples.') # pylint: disable=line-too-long
+                              help='JMESPath query string. See http://jmespath.org/ for more'\
+                                   ' information and examples.',
+                              type=jmespath_type)
 
 def register(application):
     def handle_query_parameter(**kwargs):
-        from jmespath import compile as compile_jmespath
-        from jmespath.exceptions import ParseError
         args = kwargs['args']
-        try:
-            jmespath_query = args._jmespath_query # pylint: disable=protected-access
-            query_expression = compile_jmespath(jmespath_query) if jmespath_query else None
-            del jmespath_query
-
-            if query_expression:
-                def filter_output(**kwargs):
-                    from jmespath import search, Options
-                    kwargs['event_data']['result'] = query_expression.search(
-                        kwargs['event_data']['result'], Options(collections.OrderedDict))
-                    application.remove(application.FILTER_RESULT, filter_output)
-                application.register(application.FILTER_RESULT, filter_output)
-        except ParseError as ex:
-            raise CLIError(ex)
-        except AttributeError:
-            pass
+        query_expression = args._jmespath_query # pylint: disable=protected-access
+        del args._jmespath_query
+        if query_expression:
+            def filter_output(**kwargs):
+                from jmespath import search, Options
+                kwargs['event_data']['result'] = query_expression.search(
+                    kwargs['event_data']['result'], Options(collections.OrderedDict))
+                application.remove(application.FILTER_RESULT, filter_output)
+            application.register(application.FILTER_RESULT, filter_output)
 
     application.register(application.GLOBAL_PARSER_CREATED, _register_global_parameter)
     application.register(application.COMMAND_PARSER_PARSED, handle_query_parameter)
