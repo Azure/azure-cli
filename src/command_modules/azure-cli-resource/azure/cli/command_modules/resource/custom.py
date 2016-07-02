52414fe478b9fcd2ec81cd17ea9686bf892e0d33
Yugang Wang <yugangw@microsoft.com>
2016-07-01 19:19:55 -0700

    fix #472 #213: Group deployment create issues, vm list-ip-addresses (#486)
    is case sensitive

--- src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/custom.py
+++ src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/custom.py
@@ -135,7 +135,7 @@ def list_resources(
 
 def deploy_arm_template(
         resource_group_name, deployment_name, template_file_path,
-        parameters_file_path, mode='incremental'):
+        parameters_file_path=None, mode='incremental'):
     ''' Deploy resources with an ARM template.
         :param str resource_group_name:resource group for deployment
         :param str location:location for deployment
@@ -148,7 +148,7 @@ def deploy_arm_template(
                                      parameters_file_path, mode)
 
 def validate_arm_template(resource_group_name, template_file_path,
-                          parameters_file_path, mode='incremental'):
+                          parameters_file_path=None, mode='incremental'):
     ''' Validate an ARM template.
         :param str resource_group_name:resource group for deployment
         :param str location:location for deployment
@@ -160,11 +160,14 @@ def validate_arm_template(resource_group_name, template_file_path,
                                      parameters_file_path, mode, validate_only=True)
 
 def _deploy_arm_template_core(resource_group_name, deployment_name, template_file_path,
-                              parameters_file_path, mode, validate_only=False):
+                              parameters_file_path=None, mode='incremental', validate_only=False):
     from azure.mgmt.resource.resources.models import DeploymentProperties
 
-    parameters = _get_file_json(parameters_file_path)
-    parameters = parameters.get('parameters', parameters)
+    parameters = None
+    if parameters_file_path:
+        parameters = _get_file_json(parameters_file_path)
+        if parameters:
+            parameters = parameters.get('parameters', parameters)
 
     template = _get_file_json(template_file_path)
 
