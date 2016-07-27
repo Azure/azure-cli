144aff54e2a443c329e76ff40fdd8265f08a3394
erich-wang <erich.wang@outlook.com>
2016-07-28 07:29:00 +0800

    add sphinx extension to support markups: cligroup, clicommand, cliarg, cliexample (#550)

---
+++ doc/sphinx/cligroup/cligroup.py
@@ -0,0 +1,55 @@
+import copy
+from docutils import nodes
+from sphinx import addnodes
+from sphinx.directives import ObjectDescription
+from sphinx.util.compat import Directive
+from sphinx.util.docfields import Field
+
+cli_field_types = [
+        Field('summary', label='Summary', has_arg=False,
+                   names=('summary', 'shortdesc')),
+        Field('description', label='Description', has_arg=False,
+                   names=('description', 'desc', 'longdesc'))
+    ]
+
+class CliBaseDirective(ObjectDescription):
+    def handle_signature(self, sig, signode):
+        signode += addnodes.desc_addname(sig, sig)
+        return sig
+
+    def needs_arglist(self):
+        return False
+
+    def add_target_and_index(self, name, sig, signode):
+        signode['ids'].append(name)
+
+    def get_index_text(self, modname, name):
+        return name
+
+class CliGroupDirective(CliBaseDirective):
+    doc_field_types = cli_field_types
+
+class CliCommandDirective(CliBaseDirective):
+    doc_field_types = cli_field_types
+
+class CliArgumentDirective(CliBaseDirective):
+    doc_field_types = copy.copy(cli_field_types)
+    doc_field_types.extend([
+        Field('required', label='Required', has_arg=False,
+            names=('required')),
+        Field('values', label='Allowed values', has_arg=False,
+            names=('values', 'choices', 'options')),
+        Field('default', label='Default value', has_arg=False,
+            names=('default')),
+        Field('source', label='Values from', has_arg=False,
+            names=('source', 'sources'))
+     ])
+
+class CliExampleDirective(CliBaseDirective):
+    pass
+
+def setup(app):
+    app.add_directive('cligroup', CliGroupDirective)
+    app.add_directive('clicommand', CliCommandDirective)
+    app.add_directive('cliarg', CliArgumentDirective)
+    app.add_directive('cliexample', CliExampleDirective)
--- doc/sphinx/ind.rst
+++ doc/sphinx/ind.rst
@@ -1,23 +1,2 @@
-.. az documentation master file, created by
-   sphinx-quickstart on Fri Jul  8 14:34:03 2016.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to az's documentation!
-==============================
-
-Contents:
-
-.. toctree::
-   :maxdepth: 20
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
-
 .. azhelpgen::
+  
