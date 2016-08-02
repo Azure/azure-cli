130b05d62969d8a006e5455e8a6929f73e0b3c86
Derek Bekoe <debekoe@microsoft.com>
2016-08-01 17:35:10 -0700

    Remove warning log messages for Long Running Operations (#591)
    * Remove warning log messages
    
    * Output to stderr instead of using logging

--- src/azure/cli/commands/__init__.py
+++ src/azure/cli/commands/__init__.py
@@ -4,6 +4,7 @@
 #---------------------------------------------------------------------------------------------
 
 from __future__ import print_function
+import sys
 import json
 import time
 import traceback
@@ -89,7 +90,7 @@ class LongRunningOperation(object): #pylint: disable=too-few-public-methods
         time.sleep(self.poll_interval_ms / 1000.0)
 
     def __call__(self, poller):
-        logger.warning(self.start_msg)
+        print(self.start_msg, file=sys.stderr)
         logger.info("Starting long running operation '%s' with polling interval %s ms",
                     self.start_msg, self.poll_interval_ms)
         while not poller.done():
@@ -110,7 +111,6 @@ class LongRunningOperation(object): #pylint: disable=too-few-public-methods
 
         logger.info("Long running operation '%s' completed with result %s",
                     self.start_msg, result)
-        logger.warning(self.finish_msg)
         return result
 
 class DeploymentOutputLongRunningOperation(LongRunningOperation): #pylint: disable=too-few-public-methods
