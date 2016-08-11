b9657810c19437fe8b0d32eca61c40686e166b27
Burt Bielicki <burtbiel@microsoft.com>
2016-08-11 15:23:47 -0700

    fix bad auto-merge

--- src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/__init__.py
+++ src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/__init__.py
@@ -5,7 +5,6 @@
 
 # pylint: disable=unused-import
 
-import azure.cli.command_modules.resource._help
 import azure.cli.command_modules.resource._params
 import azure.cli.command_modules.resource.generated
 import azure.cli.command_modules.resource.custom
