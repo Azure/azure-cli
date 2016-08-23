92d04d37b8ddebeafb90c89c76f366279af48fe6
Kevin Zhao <kevinzha@microsoft.com>
2016-08-23 20:21:15 +0800

    Add commands for Azure IoT

---
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/custom.py
@@ -0,0 +1,74 @@
+#---------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+#---------------------------------------------------------------------------------------------
+# pylint: disable=no-self-use,too-many-arguments,no-member
+
+from __future__ import print_function
+from sys import stderr
+import azure.cli._logging as _logging
+from azure.cli._util import CLIError
+from azure.cli.command_modules.iot.mgmt_iot_hub.lib.models.iot_hub_description import \
+    IotHubDescription
+from azure.cli.command_modules.iot.mgmt_iot_hub.lib.models.iot_hub_sku_info import \
+    IotHubSkuInfo
+from azure.cli.command_modules.iot.mgmt_iot_hub_device.lib.iot_hub_device_client import \
+    IotHubDeviceClient
+from azure.cli.command_modules.iot.mgmt_iot_hub_device.lib.models.create_device_request import \
+    CreateDeviceRequest
+from azure.cli.command_modules.iot.sas_token_auth import SasTokenAuthentication
+from ._factory import resource_service_factory
+
+logger = _logging.get_az_logger(__name__)
+
+
+def _update_progress(current, total):
+    if total:
+        message = 'Percent complete: %'
+        percent_done = current * 100 / total
+        message += '{: >5.1f}'.format(percent_done)
+        print('\b' * len(message) + message, end='', file=stderr)
+        stderr.flush()
+        if current == total:
+            print('', file=stderr)
+
+# CUSTOM METHODS
+
+
+def iot_hub_create(client, name, resource_group, location=None, sku='F1', unit=1):
+
+    if location is None:
+        logger.info('Location is none. Use location of resource group as default.')
+        resource_group_client = resource_service_factory().resource_groups
+        group_info = resource_group_client.get(resource_group)
+        location = group_info.location
+        logger.info('Location to use: %s', location)
+
+    iot_hub_description = IotHubDescription(location=location,
+                                            sku=IotHubSkuInfo(name=sku, capacity=unit))
+    result = client.create_or_update(resource_group_name=resource_group, resource_name=name,
+                                     iot_hub_description=iot_hub_description)
+    return result
+
+
+def iot_device_create(client, resource_group, hub, device_id):
+
+    access_policies = client.list_keys(resource_group, hub)
+    if access_policies is None:
+        raise CLIError('No policy found from IoT Hub: {}.'.format(hub))
+    logger.info('Shared Access Polices: ' + str(access_policies))
+    try:
+        access_policy = next(x for x in access_policies.value
+                             if 'registrywrite' in x.rights.lower())
+    except StopIteration:
+        raise CLIError('No policy found with RegistryWrite permission.')
+
+    base_url = '{0}.azure-devices.net'.format(hub)
+    uri = '{0}/devices/{1}'.format(base_url, device_id)
+    creds = SasTokenAuthentication(uri, access_policy.key_name, access_policy.primary_key)
+
+    device_client = IotHubDeviceClient(creds, client.config.subscription_id,
+                                       base_url='https://' + base_url).iot_hub_devices
+    create_device_req = CreateDeviceRequest(device_id=device_id)
+    result = device_client.create(device_id, create_device_req)
+    return result
