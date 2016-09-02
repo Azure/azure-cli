63340bd729307ecb89e9d172ed6cd4f58202a559
Derek Bekoe <debekoe@microsoft.com>
2016-09-02 09:58:06 -0700

    Don't use shell=True and get root dir for the dev setup to work correctly (#798)

--- scripts/dev_setup.py
+++ scripts/dev_setup.py
@@ -7,17 +7,21 @@
 
 from __future__ import print_function
 import sys
+import os
 from subprocess import check_call, CalledProcessError
+
+root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', '..'))
 
 def exec_command(command):
     try:
         print('Executing: ' + command)
-        check_call(command, shell=True)
+        check_call(command.split(), cwd=root_dir)
     except CalledProcessError as err:
         print(err, file=sys.stderr)
         sys.exit(1)
 
 print('Running dev setup...')
+print('Root directory \'{}\''.format(root_dir))
 exec_command('pip install -r requirements.txt')
 exec_command('pip install -e .')
 exec_command('python scripts/command_modules/install.py')
