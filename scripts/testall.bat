8a79c0b5b1584fcffb05c9970edf8ba504967307
Travis Prescott <trpresco@microsoft.com>
2016-09-13 12:59:45 -0700

    Update various utility scripts for switch to azure-cli-core.

--- scripts/testall.bat
+++ scripts/testall.bat
@@ -1,4 +1,5 @@
 @echo off
 cls
-python -m unittest discover -s src/azure/cli/tests --buffer
+python -m unittest discover -s src/azure-cli/azure/cli/tests --buffer
+python -m unittest discover -s src/azure-cli-core/azure/cli/core/tests --buffer
 python scripts/command_modules/test.py
