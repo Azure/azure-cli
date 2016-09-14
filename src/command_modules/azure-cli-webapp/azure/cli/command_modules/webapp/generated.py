e1ecdd1f5cb85757e723bc22c8e4342e61cdc526
Yugang Wang <yugangw@microsoft.com>
2016-09-14 16:37:19 -0700

    webapp: basic support for plan, webapp, git, diagnostics (#871)

--- src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/generated.py
+++ src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/generated.py
@@ -4,24 +4,69 @@
 #---------------------------------------------------------------------------------------------
 
 #pylint: disable=unused-import
-from azure.mgmt.web.operations import SitesOperations
-
+from azure.mgmt.web.operations import SitesOperations, ServerFarmsOperations, ProviderOperations
 from azure.cli.core.commands import LongRunningOperation, cli_command
-from azure.cli.core.commands.client_factory import get_mgmt_service_client
 
-from azure.cli.command_modules.webapp._params import _web_client_factory
-from azure.cli.command_modules.webapp.mgmt_webapp.lib \
-    import (WebappCreationClient as WebAppClient)
-from azure.cli.command_modules.webapp.mgmt_webapp.lib.operations import WebappOperations
+from ._params import web_client_factory
+from .custom import (create_webapp, show_webapp,
+                     delete_webapp, stop_webapp, restart_webapp,
+                     enable_local_git, set_deployment_user,
+                     get_git_url, view_in_browser, create_app_service_plan,
+                     update_app_service_plan, config_diagnostics,
+                     get_streaming_log, download_historical_logs,
+                     create_webapp_slot, config_slot_auto_swap,
+                     get_site_configs, update_site_configs,
+                     get_app_settings, update_app_settings, delete_app_settings)
 
-class DeploymentOutputLongRunningOperation(LongRunningOperation): #pylint: disable=too-few-public-methods
-    def __call__(self, poller):
-        result = super(DeploymentOutputLongRunningOperation, self).__call__(poller)
-        return result.properties.outputs
+factory = lambda _: web_client_factory().sites
+cli_command('webapp create', create_webapp)
+cli_command('webapp list', SitesOperations.get_sites, factory)
+cli_command('webapp show', show_webapp)
+cli_command('webapp delete', delete_webapp)
+cli_command('webapp stop', stop_webapp)
+cli_command('webapp restart', restart_webapp)
 
-factory = lambda _: _web_client_factory()
-cli_command('webapp get-sites', SitesOperations.get_sites, factory)
+#TODO: finalize command interface after dicussion with webapp team
+cli_command('webapp config update', update_site_configs)
+cli_command('webapp config show', get_site_configs)
+cli_command('webapp config appsettings show', get_app_settings)
+cli_command('webapp config appsettings update', update_app_settings)
+cli_command('webapp config appsettings delete', delete_app_settings)
+cli_command('webapp show-publish-profile',
+            SitesOperations.list_site_publishing_credentials, factory)
 
-factory = lambda _: get_mgmt_service_client(WebAppClient).webapp
-cli_command('webapp create', WebappOperations.create_or_update, factory,
-            transform=DeploymentOutputLongRunningOperation('Creating webapp'))
+cli_command('webapp git enable-local', enable_local_git)
+cli_command('webapp git show-url', get_git_url)
+cli_command('webapp log tail', get_streaming_log)
+cli_command('webapp log download', download_historical_logs)
+cli_command('webapp log set', config_diagnostics)
+cli_command('webapp browse', view_in_browser)
+
+cli_command('webapp deployment slot list', SitesOperations.get_site_slots, factory)
+cli_command('webapp deployment slot auto-swap', config_slot_auto_swap)
+cli_command('webapp deployment slot swap', SitesOperations.swap_slots_slot, factory)
+cli_command('webapp deployment slot create', create_webapp_slot)
+cli_command('webapp deployment user set', set_deployment_user)
+cli_command('webapp deployment list-site-credentials',#TODO add descriptions to differentiate
+            SitesOperations.list_site_publishing_credentials, factory)
+
+factory = lambda _: web_client_factory().provider
+cli_command('webapp deployment user show', ProviderOperations.get_publishing_user, factory)
+
+factory = lambda _: web_client_factory().server_farms
+cli_command('webapp plan create', create_app_service_plan)
+cli_command('webapp plan update', update_app_service_plan)
+cli_command('webapp plan delete', ServerFarmsOperations.delete_server_farm, factory)
+cli_command('webapp plan list', ServerFarmsOperations.get_server_farms, factory)
+cli_command('webapp plan show', ServerFarmsOperations.get_server_farm, factory)
+
+#Functionalities covered by better custom commands, so not exposed for now
+#cli_command('webapp get-source-control', SitesOperations.get_site_source_control, factory)
+#cli_command('webapp source-control list', ProviderOperations.get_source_controls, factory)
+
+#Not for ignite release
+#cli_command('webapp plan update-vnet-route', ServerFarmsOperations.update_vnet_route, factory)
+#cli_command('webapp plan update-vnet-gateway',
+#             ServerFarmsOperations.update_server_farm_vnet_gateway,factory)
+#cli_command('webapp plan update-vnet-route', ServerFarmsOperations.update_vnet_route, factory)
+
