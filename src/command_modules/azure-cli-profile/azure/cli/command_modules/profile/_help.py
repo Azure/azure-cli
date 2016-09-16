358ad286d275fae6711a47653e22c25ef6231fe5
yugangw-msft <yugangw@microsoft.com>
2016-09-15 18:25:35 -0700

    minor bug fixes

--- src/command_modules/azure-cli-profile/azure/cli/command_modules/profile/_help.py
+++ src/command_modules/azure-cli-profile/azure/cli/command_modules/profile/_help.py
@@ -6,3 +6,15 @@
 from azure.cli.core.help_files import helps #pylint: disable=unused-import
 
 #pylint: disable=line-too-long
+helps['login'] = """
+            examples:
+                - name: login interactively (work for all user account types)
+                  text: >
+                    az login
+                - name: login with user name and password(doesn't work with two-factor authentication enabled accounts or Microsoft accounts, like live id)
+                  text: >
+                    az login -u johndoe@contoso.com -p VerySecret
+                - name: login with service principal
+                  text: >
+                    az login --service-principal -u http://azure-cli-2016-08-05-14-31-15 -p VerySecret --tenant contoso.onmicrosoft.com
+            """
