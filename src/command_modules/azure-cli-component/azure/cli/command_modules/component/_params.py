72d9a0a5bcce86c8beb7a7d710e1da4b7bfa6da3
Derek Bekoe <debekoe@microsoft.com>
2016-09-20 12:19:17 -0700

    Simplify 'az component' commands (#914)

--- src/command_modules/azure-cli-component/azure/cli/command_modules/component/_params.py
+++ src/command_modules/azure-cli-component/azure/cli/command_modules/component/_params.py
@@ -5,11 +5,10 @@
 
 from azure.cli.core.commands import register_cli_argument
 
-# BASIC PARAMETER CONFIGURATION
-
 # pylint: disable=line-too-long
 register_cli_argument('component', 'component_name', options_list=('--name', '-n'), help='Name of component')
-register_cli_argument('component', 'force', options_list=('--force', '-f'), help='Supress delete confirmation prompt', action='store_true')
 register_cli_argument('component', 'link', options_list=('--link', '-l'), help='If a url or path to an html file, parse for links to archives. If local path or file:// url that\'s a directory, then look for archives in the directory listing.')
-register_cli_argument('component', 'private', options_list=('--private', '-p'), help='Get from the project PyPI server', action='store_true')
-register_cli_argument('component', 'version', help='Component version (otherwise latest)')
+register_cli_argument('component', 'private', help='Include packages from a private server.', action='store_true')
+register_cli_argument('component', 'pre', help='Include pre-release versions', action='store_true')
+register_cli_argument('component', 'show_logs', help='Show logs from packaging installing/updating', action='store_true')
+register_cli_argument('component', 'additional_component', help='The name of an additional component to install')
