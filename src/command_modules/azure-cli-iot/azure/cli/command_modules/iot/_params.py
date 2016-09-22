f8cadd7cc630a3384fc59f19384b0fd94339de27
Kevin Zhao <kvn.zhl@gmail.com>
2016-09-23 01:23:50 +0800

    [IoT] add show-connection-string command for hub and device (#951)
    Per our chat, I've created issue #953 to track the outstanding items. Thanks for the PR!

--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/_params.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/_params.py
@@ -22,8 +22,20 @@ register_cli_argument('iot hub create', 'sku',
                            'Exception will be thrown if free instances exceed one.')
 register_cli_argument('iot hub create', 'unit', help='Units in your IoT Hub.', type=int)
 
+# Arguments for 'iot hub show-connection-string'
+# TODO: Add completer for iot hub name
+register_cli_argument('iot hub show-connection-string', 'name', options_list=('--name', '-n'),
+                      help='Target IoT Hub name.')
+register_cli_argument('iot hub show-connection-string', 'policy_name',
+                      help='The access policy you choose to use.')
 
 # Arguments for 'iot device create'
 register_cli_argument('iot device create', 'hub', help='Target IoT Hub name.')
 register_cli_argument('iot device create', 'device_id',
+                      options_list=('--device-id', '-d'), help='Device Id.')
+
+# Arguments for 'iot device show-connection-string'
+# TODO: Add completer for device id
+register_cli_argument('iot device show-connection-string', 'hub', help='Target IoT Hub name.')
+register_cli_argument('iot device show-connection-string', 'device_id',
                       options_list=('--device-id', '-d'), help='Device Id.')
