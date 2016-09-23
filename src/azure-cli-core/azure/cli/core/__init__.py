70e3cafaedc420892b21ed9219c21950568f7c9b
Derek Bekoe <debekoe@microsoft.com>
2016-09-23 13:51:54 -0700

    version 0.1.0b5 of all modules

--- src/azure-cli-core/azure/cli/core/__init__.py
+++ src/azure-cli-core/azure/cli/core/__init__.py
@@ -5,4 +5,4 @@
 import pkg_resources
 pkg_resources.declare_namespace(__name__)
 
-__version__ = "0.1.0b4"
+__version__ = "0.1.0b5"
--- src/command_modules/azure-cli-keyvault/setup.py
+++ src/command_modules/azure-cli-keyvault/setup.py
@@ -9,7 +9,7 @@
 from codecs import open
 from setuptools import setup
 
-VERSION = '0.1.0b4'
+VERSION = '0.1.0b5'
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
