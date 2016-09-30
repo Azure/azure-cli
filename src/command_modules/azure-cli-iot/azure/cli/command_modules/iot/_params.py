3643fcb74a60cfdcd2bbfcf7b6cea767c4336124
Kevin Zhao <kevinzha@microsoft.com>
2016-10-01 00:23:37 +0800

    refine arguments

--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/_params.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/_params.py
@@ -20,13 +20,15 @@ def get_device_id_completion_list():
             return []
     return completer
 
-hub_name_type = CliArgumentType(options_list=('--hub-name', '--name', '--hub', '-n'), help='The IoT Hub name.',
-                                completer=get_resource_name_completion_list('Microsoft.Devices/IotHubs'))
-device_id_type = CliArgumentType(options_list=('--device-id', '-d'), help='Device Id.',
-                                 completer=get_device_id_completion_list())
+
+register_cli_argument('iot', 'hub_name', options_list=('--hub-name', '--name', '--hub', '-n'),
+                      completer=get_resource_name_completion_list('Microsoft.Devices/IotHubs'),
+                      help='The IoT Hub name.')
+register_cli_argument('iot', 'device_id', options_list=('--device-id', '-d'), help='Device Id.',
+                      completer=get_device_id_completion_list())
 
 # Arguments for 'iot hub create'
-register_cli_argument('iot hub create', 'hub_name', hub_name_type)
+register_cli_argument('iot hub create', 'hub_name', completer=None)
 register_cli_argument('iot hub create', 'location', location_type,
                       help='Location of your IoT Hub. Default is the location of target resource group.')
 register_cli_argument('iot hub create', 'sku',
@@ -37,20 +39,15 @@ register_cli_argument('iot hub create', 'sku',
 register_cli_argument('iot hub create', 'unit', help='Units in your IoT Hub.', type=int)
 
 # Arguments for 'iot hub show-connection-string'
-register_cli_argument('iot hub show-connection-string', 'hub_name', hub_name_type)
 register_cli_argument('iot hub show-connection-string', 'policy_name',
                       help='The access policy you choose to use.')
 
 # Arguments for 'iot device create'
-register_cli_argument('iot device create', 'hub_name', hub_name_type)
-register_cli_argument('iot device create', 'device_id', device_id_type)
+register_cli_argument('iot device create', 'device_id', completer=None)
 
 # Arguments for 'iot device list'
-register_cli_argument('iot device list', 'hub_name', hub_name_type)
 register_cli_argument('iot device list', 'top', help='Maximum number of device identities to return.', type=int)
 
 # Arguments for 'iot device show-connection-string'
-register_cli_argument('iot device show-connection-string', 'hub_name', hub_name_type)
-register_cli_argument('iot device show-connection-string', 'device_id', device_id_type)
 register_cli_argument('iot device show-connection-string', 'top',
                       help='Maximum number of connection strings to return.', type=int)
