9773f5d956b543c174f3d5095ae8cbccd51d3242
Yugang Wang <yugangw@microsoft.com>
2016-10-03 13:56:32 -0700

    webapp: support hostname (#1024)
    * webapp:hostname add/delete/list
    
    * fix the tab completion
    
    * fix pylint errors
    
    * tab completion for hostname
    
    * fix pylint error for trailing spaces

--- src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/tests/test_webapp_commands.py
+++ src/command_modules/azure-cli-webapp/azure/cli/command_modules/webapp/tests/test_webapp_commands.py
@@ -145,6 +145,11 @@ class WebappConfigureTest(ResourceGroupVCRTestBase):
         self.assertTrue('s2' not in result)
         self.assertTrue('s3' in result)
 
+        self.cmd('appservice web config hostname list -g {} --webapp {}'.format(self.resource_group, self.webapp_name), checks=[
+            JMESPathCheck('length(@)', 1),
+            JMESPathCheck('[0].name', '{0}/{0}.azurewebsites.net'.format(self.webapp_name))
+            ])
+
 
 class WebappScaleTest(ResourceGroupVCRTestBase):
 
