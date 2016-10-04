6b04ac10fe3646ff4504f0cba81877da96f86a65
Derek Bekoe <debekoe@microsoft.com>
2016-10-04 09:45:40 -0700

    Must define a function as the entry_point. Using just the module isn't sufficient. (+1 squashed commit) (#1014)
    Squashed commits:
    [19a496b] Use setuptools entry_point instead of using our own exe

--- src/azure-cli/azure/cli/__main__.py
+++ src/azure-cli/azure/cli/__main__.py
@@ -11,29 +11,33 @@ import azure.cli.main
 from azure.cli.core.telemetry import (init_telemetry, user_agrees_to_telemetry,
                                       telemetry_flush, log_telemetry)
 
-try:
+def cli():
     try:
-        if user_agrees_to_telemetry():
-            init_telemetry()
-    except Exception: #pylint: disable=broad-except
-        pass
+        try:
+            if user_agrees_to_telemetry():
+                init_telemetry()
+        except Exception: #pylint: disable=broad-except
+            pass
 
-    args = sys.argv[1:]
+        args = sys.argv[1:]
 
-    # Check if we are in argcomplete mode - if so, we
-    # need to pick up our args from environment variables
-    if os.environ.get('_ARGCOMPLETE'):
-        comp_line = os.environ.get('COMP_LINE')
-        if comp_line:
-            args = comp_line.split()[1:]
+        # Check if we are in argcomplete mode - if so, we
+        # need to pick up our args from environment variables
+        if os.environ.get('_ARGCOMPLETE'):
+            comp_line = os.environ.get('COMP_LINE')
+            if comp_line:
+                args = comp_line.split()[1:]
 
-    sys.exit(azure.cli.main.main(args))
-except KeyboardInterrupt:
-    log_telemetry('keyboard interrupt')
-    sys.exit(1)
-finally:
-    try:
-        if user_agrees_to_telemetry():
-            telemetry_flush()
-    except Exception: #pylint: disable=broad-except
-        pass
+        sys.exit(azure.cli.main.main(args))
+    except KeyboardInterrupt:
+        log_telemetry('keyboard interrupt')
+        sys.exit(1)
+    finally:
+        try:
+            if user_agrees_to_telemetry():
+                telemetry_flush()
+        except Exception: #pylint: disable=broad-except
+            pass
+
+if __name__ == "__main__":
+    cli()
