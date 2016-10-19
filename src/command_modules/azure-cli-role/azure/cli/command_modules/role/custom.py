777c087fc66b88bf96a825bf00c9b199b6d10254
yugangw-msft <yugangw@microsoft.com>
2016-10-19 15:16:09 -0700

    remove useless import

--- src/command_modules/azure-cli-role/azure/cli/command_modules/role/custom.py
+++ src/command_modules/azure-cli-role/azure/cli/command_modules/role/custom.py
@@ -2,7 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 #---------------------------------------------------------------------------------------------
-from __future__ import print_function
 import datetime
 import json
 import re
