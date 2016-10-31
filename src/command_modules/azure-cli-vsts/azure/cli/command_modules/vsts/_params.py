1a57feb59001d95f2756293a2a9cba8901373a56
Sarthak Narang <sanar@microsoft.com>
2016-10-31 14:00:36 -0700

    Fixing merge errors

--- src/command_modules/azure-cli-vsts/azure/cli/command_modules/vsts/_params.py
+++ src/command_modules/azure-cli-vsts/azure/cli/command_modules/vsts/_params.py
@@ -15,14 +15,19 @@ registry_name = CliArgumentType(
     help='Azure container registry name. A new Azure container registry is created if omitted or does not exist.'
 )
 
-registry_resource_group = CliArgumentType(
-    options_list=('--registry-resource-group',),
-    help='Azure container registry resource group name.'
+registry_resource_id = CliArgumentType(
+    options_list=('--registry-resource-id',),
+    help='Azure container registry resource id. Specifies an existing Azure container registry. e.g. /subscriptions/{subscriptionId}/resourcegroups/{resourceGroup}/providers/Microsoft.ContainerRegistry/registries/{registryName}'
 )
 
 remote_url = CliArgumentType(
     options_list=('--remote-url', '-u'),
     help='Remote url of the GitHub or VSTS source repository that will be built and deployed. If omitted, a source repository will be searched for in the current working directory.'
+)
+
+remote_branch = CliArgumentType(		
+    options_list=('--remote-branch', '-b'),		
+    help='Remote branch of the GitHub or VSTS source repository that will be built and deployed. If omitted refs/heads/master will be selected'		
 )
 
 remote_access_token = CliArgumentType(
@@ -43,8 +48,9 @@ vsts_project_name = CliArgumentType(
 register_cli_argument('vsts release create', 'name', name_type)
 register_cli_argument('vsts release create', 'resource_group_name', resource_group_name_type)
 register_cli_argument('vsts release create', 'registry_name', registry_name)
-register_cli_argument('vsts release create', 'registry_resource_group', registry_resource_group)
+register_cli_argument('vsts release create', 'registry_resource_id', registry_resource_id)
 register_cli_argument('vsts release create', 'remote_url', remote_url)
+register_cli_argument('vsts release create', 'remote_branch', remote_branch)
 register_cli_argument('vsts release create', 'remote_access_token', remote_access_token)
 register_cli_argument('vsts release create', 'vsts_account_name', vsts_account_name)
 register_cli_argument('vsts release create', 'vsts_project_name', vsts_project_name)
@@ -52,8 +58,9 @@ register_cli_argument('vsts release create', 'vsts_project_name', vsts_project_n
 register_cli_argument('vsts build create', 'name', name_type)
 register_cli_argument('vsts build create', 'resource_group_name', resource_group_name_type)
 register_cli_argument('vsts build create', 'registry_name', registry_name)
-register_cli_argument('vsts build create', 'registry_resource_group', registry_resource_group)
+register_cli_argument('vsts build create', 'registry_resource_id', registry_resource_id)
 register_cli_argument('vsts build create', 'remote_url', remote_url)
+register_cli_argument('vsts build create', 'remote_branch', remote_branch)
 register_cli_argument('vsts build create', 'remote_access_token', remote_access_token)
 register_cli_argument('vsts build create', 'vsts_account_name', vsts_account_name)
 register_cli_argument('vsts build create', 'vsts_project_name', vsts_project_name)
