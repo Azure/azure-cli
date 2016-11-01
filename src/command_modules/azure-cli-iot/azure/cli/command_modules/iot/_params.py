18a51a5312b85be3067f1ea9a7a096ee716c0b0b
Kevin Zhao <kevinzha@microsoft.com>
2016-11-01 20:20:16 +0800

    refine parameter register

--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/_params.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/_params.py
@@ -5,7 +5,7 @@
 #pylint: disable=line-too-long
 
 from azure.cli.core.commands.parameters import \
-    (location_type, enum_choice_list, get_resource_name_completion_list)
+    (CliArgumentType, location_type, enum_choice_list, get_resource_name_completion_list)
 from azure.cli.core.commands import register_cli_argument
 from azure.mgmt.iothub.models.iot_hub_client_enums import IotHubSku
 from ._factory import iot_hub_service_factory
@@ -16,12 +16,11 @@ def get_device_id_completion_list(prefix, action, parsed_args, **kwargs):#pylint
     client = iot_hub_service_factory(kwargs)
     return [d.device_id for d in iot_device_list(client, parsed_args.hub_name, top=100)] if parsed_args.hub_name else []
 
-register_cli_argument('iot hub', 'hub_name', options_list=('--name', '-n'),
-                      completer=get_resource_name_completion_list('Microsoft.Devices/IotHubs'),
-                      help='The IoT Hub name.')
-register_cli_argument('iot device', 'hub_name', options_list=('--hub-name', '--hub'),
-                      completer=get_resource_name_completion_list('Microsoft.Devices/IotHubs'),
-                      help='The IoT Hub name.')
+hub_name_type = CliArgumentType(completer=get_resource_name_completion_list('Microsoft.Devices/IotHubs'),
+                                help='IoT Hub name.')
+
+register_cli_argument('iot hub', 'hub_name', hub_name_type, options_list=('--name', '-n'))
+register_cli_argument('iot device', 'hub_name', hub_name_type, options_list=('--hub-name', '--hub'))
 register_cli_argument('iot', 'device_id', options_list=('--device-id', '-d'), help='Device Id.',
                       completer=get_device_id_completion_list)
 
