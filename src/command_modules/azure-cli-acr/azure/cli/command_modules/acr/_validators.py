641a0bf34468a3e2a4566ca3e7a5f5856a97494b
Kevin Zhang <t-kezh@microsoft.com>
2016-11-01 18:18:58 -0700

    remove doubly defined validator

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_validators.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_validators.py
@@ -21,15 +21,3 @@ def validate_resource_group_name(namespace):
             raise CLIError(
                 'The resource group {} does not exist in the current subscription.'\
                 .format(resource_group_name))
-
-def validate_resource_group_name(namespace):
-    if namespace.resource_group_name:
-        client = get_arm_service_client()
-        resource_group_name = namespace.resource_group_name
-
-        if not client.resource_groups.check_existence(resource_group_name):
-            logger.warning('Command to create a resource group:')
-            logger.warning('  az resource group create -n <name> -l <location>')
-            raise CLIError(
-                'The resource group {} does not exist in the current subscription.'\
-                .format(resource_group_name))
