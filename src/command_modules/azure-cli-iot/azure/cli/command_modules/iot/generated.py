808513cb14c217ad62ee36197a337c63d354984c
Kevin Zhao <kvn.zhl@gmail.com>
2016-11-02 23:57:45 +0800

    [IoT] add commands `iot hub show`, `iot device show` and `iot device delete` (#1188)
    * add `iot device delete` command
    
    * add `iot hub show` command
    
    * add `iot device show` command
    
    * update test case
    
    * resolve review comments
    
    * refine parameter register
    
    * minor update

--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/generated.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/generated.py
@@ -9,8 +9,8 @@ from __future__ import print_function
 from azure.cli.core.commands import cli_command, LongRunningOperation
 from ._factory import (iot_hub_service_factory,)
 from azure.cli.command_modules.iot.custom import \
-    (iot_hub_create, iot_device_create, iot_hub_list, iot_device_list,
-     iot_hub_show_connection_string, iot_device_show_connection_string)
+    (iot_hub_create, iot_hub_get, iot_hub_list, iot_hub_show_connection_string,
+     iot_device_create, iot_device_get, iot_device_list, iot_device_delete, iot_device_show_connection_string)
 
 # iot hub commands
 factory = iot_hub_service_factory
@@ -18,12 +18,18 @@ factory = iot_hub_service_factory
 cli_command('iot hub create', iot_hub_create, factory,
             transform=LongRunningOperation('creating IoT Hub...', '', 10000.0))
 
+cli_command('iot hub show', iot_hub_get, factory)
+
 cli_command('iot hub list', iot_hub_list, factory)
 
 cli_command('iot hub show-connection-string', iot_hub_show_connection_string, factory)
 
 cli_command('iot device create', iot_device_create, factory)
 
+cli_command('iot device show', iot_device_get, factory)
+
 cli_command('iot device list', iot_device_list, factory)
+
+cli_command('iot device delete', iot_device_delete, factory)
 
 cli_command('iot device show-connection-string', iot_device_show_connection_string, factory)
--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/tests/test_iot_commands.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/tests/test_iot_commands.py
@@ -9,6 +9,7 @@ import azure.cli.core._logging as _logging
 
 logger = _logging.get_az_logger(__name__)
 
+
 class IoTHubTest(ResourceGroupVCRTestBase):
     def __init__(self, test_method):
         super(IoTHubTest, self).__init__(__file__, test_method)
@@ -34,9 +35,23 @@ class IoTHubTest(ResourceGroupVCRTestBase):
                  ])
 
         # Test 'az iot hub show-connection-string'
-        connection_string = 'HostName=iot-hub-for-testing.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=fqS8+VBHmoLe/5/juDnt2LmnBF4DUkx6I87M7r5Jj3Y='
+        connection_string = 'HostName=iot-hub-for-testing.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=cI+EYy448bmVabof54H7wiG0ntHwM9mqRL5rFOoBGj8='
         self.cmd('iot hub show-connection-string -n {0}'.format(hub), checks=[
-            JMESPathCheck('connectionString', connection_string),
+            JMESPathCheck('connectionString', connection_string)
+        ])
+
+        self.cmd('iot hub show-connection-string -g {0}'.format(rg), checks=[
+            JMESPathCheck('length([*])', 1),
+            JMESPathCheck('[0].name', hub),
+            JMESPathCheck('[0].connectionString', connection_string)
+        ])
+
+        # Test 'az iot hub show'
+        self.cmd('iot hub show -g {0} -n {1}'.format(rg, hub), checks=[
+            JMESPathCheck('resourceGroup', rg),
+            JMESPathCheck('location', location),
+            JMESPathCheck('name', hub),
+            JMESPathCheck('sku.name', 'S1')
         ])
 
         # Test 'az iot hub list'
@@ -50,7 +65,7 @@ class IoTHubTest(ResourceGroupVCRTestBase):
 
         # Test 'az iot device create'
         device_id = self.device_id
-        self.cmd('iot device create -n {0} -d {1}'.format(hub, device_id),
+        self.cmd('iot device create --hub-name {0} -d {1}'.format(hub, device_id),
                  checks=[
                      JMESPathCheck('deviceId', device_id),
                      JMESPathCheck('status', 'enabled'),
@@ -58,15 +73,31 @@ class IoTHubTest(ResourceGroupVCRTestBase):
                  ])
 
         # Test 'az iot device show-connection-string'
-        connection_string = 'HostName=iot-hub-for-testing.azure-devices.net;DeviceId=iot-device-for-testing;SharedAccessKey=qlhnBaWihYDHE/FlDZfngBTWfvwJ+BQbsmloLgl5+lU='
-        self.cmd('iot device show-connection-string -n {0} -d {1}'.format(hub, device_id), checks=[
-            JMESPathCheck('connectionString', connection_string),
+        connection_string = 'HostName=iot-hub-for-testing.azure-devices.net;DeviceId=iot-device-for-testing;SharedAccessKey=dw93dOLBIxAcczUqoEDukwh1pVi7gzIQq65ahBS6qNw='
+        self.cmd('iot device show-connection-string --hub-name {0} -d {1}'.format(hub, device_id), checks=[
+            JMESPathCheck('connectionString', connection_string)
+        ])
+
+        self.cmd('iot device show-connection-string --hub-name {0}'.format(hub), checks=[
+            JMESPathCheck('length([*])', 1),
+            JMESPathCheck('[0].deviceId', device_id),
+            JMESPathCheck('[0].connectionString', connection_string)
+        ])
+
+        # Test 'az iot device show'
+        self.cmd('iot device show --hub-name {0} -d {1}'.format(hub, device_id), checks=[
+            JMESPathCheck('deviceId', device_id),
+            JMESPathCheck('status', 'enabled'),
+            JMESPathCheck('connectionState', 'Disconnected')
         ])
 
         # Test 'az iot device list'
-        self.cmd('iot device list -n {0}'.format(hub), checks=[
+        self.cmd('iot device list --hub-name {0}'.format(hub), checks=[
             JMESPathCheck('length([*])', 1),
             JMESPathCheck('[0].deviceId', device_id),
             JMESPathCheck('[0].status', 'enabled'),
             JMESPathCheck('[0].connectionState', 'Disconnected')
         ])
+
+        # Test 'az iot device delete'
+        self.cmd('iot device delete --hub-name {0} -d {1}'.format(hub, device_id))
