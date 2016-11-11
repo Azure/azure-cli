f5060adcb107b0e1faf9c1d5d6ca9d52314e937e
Travis Prescott <trpresco@microsoft.com>
2016-11-11 15:45:06 -0800

    Fix test issues.

--- src/azure-cli-core/azure/cli/core/test_utils/vcr_test_base.py
+++ src/azure-cli-core/azure/cli/core/test_utils/vcr_test_base.py
@@ -6,6 +6,7 @@
 from __future__ import print_function
 
 import json
+import logging
 import os
 import collections
 import shlex
@@ -13,6 +14,8 @@ from random import choice
 import re
 from string import digits
 import sys
+from six.moves.urllib.parse import urlparse, parse_qs # pylint: disable=import-error
+
 import unittest
 try:
     import unittest.mock as mock
@@ -168,6 +171,30 @@ def _search_result_by_jmespath(json_data, query):
         json_val,
         jmespath.Options(collections.OrderedDict))
 
+def _custom_request_matcher(r1, r2):
+    """ Ensure method, path, and query parameters match. """
+    if r1.method != r2.method:
+        return False
+
+    url1 = urlparse(r1.uri)
+    url2 = urlparse(r2.uri)
+
+    if url1.path != url2.path:
+        return False
+
+    q1 = parse_qs(url1.query)
+    q2 = parse_qs(url2.query)
+    shared_keys = set(q1.keys()).union(set(q2.keys()))
+
+    if len(shared_keys) != len(q1) or len(shared_keys) != len(q2):
+        return False
+
+    for key in shared_keys:
+        if q1[key][0].lower() != q2[key][0].lower():
+            return False
+
+    return True
+
 # MAIN CLASS
 
 class VCRTestBase(unittest.TestCase):#pylint: disable=too-many-instance-attributes
@@ -204,11 +231,17 @@ class VCRTestBase(unittest.TestCase):#pylint: disable=too-many-instance-attribut
         if not self.playback and ('--buffer' in sys.argv) and not run_live:
             self.exception = CLIError('No recorded result provided for {}.'.format(self.test_name))
 
+        if debug:
+            logging.basicConfig()
+            vcr_log = logging.getLogger('vcr')
+            vcr_log.setLevel(logging.INFO)
         self.my_vcr = vcr.VCR(
             cassette_library_dir=self.recording_dir,
             before_record_request=VCRTestBase._before_record_request,
             before_record_response=VCRTestBase._before_record_response,
         )
+        self.my_vcr.register_matcher('custom', _custom_request_matcher)
+        self.my_vcr.match_on = ['custom']
 
     def _track_executed_commands(self, command):
         if not self.track_commands:
