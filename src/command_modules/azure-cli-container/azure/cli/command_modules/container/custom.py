fa392e08bfbcdfdde0c382c5b623aabb8741220d
Matthew <thegalah@gmail.com>
2016-11-17 17:34:08 -0800

    Added check for special case when user is in .git directory (#1354)
    * Added check for special case when user is in .git directory
    
    * aded suggestions

--- src/command_modules/azure-cli-container/azure/cli/command_modules/container/custom.py
+++ src/command_modules/azure-cli-container/azure/cli/command_modules/container/custom.py
@@ -215,10 +215,30 @@ def list_releases(target_name, target_resource_group):
     json_request = req.json()
     return json_request
 
+def _is_inside_git_directory():
+    """
+    Determines if the user is inside the .git folder of a git repo
+    """
+    try:
+        is_inside_git_dir = check_output(['git', 'rev-parse', '--is-inside-git-dir'])
+    except OSError:
+        raise CLIError('Git is not currently installed.')
+
+    git_result = is_inside_git_dir.decode('utf-8').strip()
+
+    if git_result == 'false':
+        return False
+    elif git_result == 'true':
+        return True
+    else:
+        raise CLIError('Unexpected value from git operation.')
+
 def _gitroot():
     """
     Gets the absolute path of the repository root
     """
+    if _is_inside_git_directory(): # special case need to navigate to parent
+        os.chdir('..')
     try:
         base = check_output(['git', 'rev-parse', '--show-toplevel'])
     except OSError:
