83174406f8cb837df5dadd31d8bb6ca7533a3a9e
Derek Bekoe <debekoe@microsoft.com>
2016-11-18 15:50:55 -0800

    Removed redundant show-logs flag as logs are sent to logger.debug. (#1385)
    If user wants logs, it’s expected that they should just use `—debug` instead of `—debug` and `—show-logs`

--- src/command_modules/azure-cli-component/azure/cli/command_modules/component/_params.py
+++ src/command_modules/azure-cli-component/azure/cli/command_modules/component/_params.py
@@ -10,5 +10,4 @@ register_cli_argument('component', 'component_name', options_list=('--name', '-n
 register_cli_argument('component', 'link', options_list=('--link', '-l'), help='If a url or path to an html file, parse for links to archives. If local path or file:// url that\'s a directory, then look for archives in the directory listing.')
 register_cli_argument('component', 'private', help='Include packages from a private server.', action='store_true')
 register_cli_argument('component', 'pre', help='Include pre-release versions', action='store_true')
-register_cli_argument('component', 'show_logs', help='Show logs from packaging installing/updating', action='store_true')
 register_cli_argument('component', 'additional_components', options_list=('--add',), nargs='+', help='The names of additional components to install (space separated)')
