4354da489bdf17fb8f668814edf6fcf15f4517d7
Derek Bekoe <debekoe@microsoft.com>
2016-11-22 11:17:23 -0800

    Component Discovery (#1409)
    ‘az component list-available’ command
    Check if the module is available if parsing fails due to command_package error

--- src/command_modules/azure-cli-component/azure/cli/command_modules/component/commands.py
+++ src/command_modules/azure-cli-component/azure/cli/command_modules/component/commands.py
@@ -10,3 +10,4 @@ from azure.cli.core.commands import cli_command
 cli_command(__name__, 'component list', 'azure.cli.command_modules.component.custom#list_components')
 cli_command(__name__, 'component update', 'azure.cli.command_modules.component.custom#update')
 cli_command(__name__, 'component remove', 'azure.cli.command_modules.component.custom#remove')
+cli_command(__name__, 'component list-available', 'azure.cli.command_modules.component.custom#list_available_components')
