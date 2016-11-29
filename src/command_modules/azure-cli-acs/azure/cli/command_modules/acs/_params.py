91f843403fa3bcc247918ee7b6a06b0c1481f877
Matthew Duong <maduong@microsoft.com>
2016-11-29 12:03:03 -0800

    Updated help

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_params.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_params.py
@@ -50,7 +50,7 @@ def _get_default_install_location(exe_name):
 
 name_arg_type = CliArgumentType(options_list=('--name', '-n'), metavar='NAME')
 
-register_cli_argument('acs', 'name', arg_type=name_arg_type)
+register_cli_argument('acs', 'name', arg_type=name_arg_type, help='ACS cluster name')
 register_cli_argument('acs', 'orchestrator_type', **enum_choice_list(ContainerServiceOchestratorTypes))
 #some admin names are prohibited in acs, such as root, admin, etc. Because we have no control on the orchestrators, so default to a safe name.
 register_cli_argument('acs', 'admin_username', options_list=('--admin-username',), default='azureuser', required=False)
@@ -63,7 +63,7 @@ register_extra_cli_argument('acs create', 'generate_ssh_keys', action='store_tru
 register_cli_argument('acs create', 'agent_vm_size', completer=get_vm_size_completion_list)
 
 register_cli_argument('acs', 'disable_browser', help='Do not open browser after opening a proxy to the cluster web user interface')
-register_cli_argument('acs dcos browse', 'name', name_type)
+register_cli_argument('acs dcos browse', 'name', name_arg_type)
 register_cli_argument('acs dcos browse', 'resource_group_name', resource_group_name_type)
 register_cli_argument('acs dcos install-cli', 'install_location',
                       options_list=('--install-location',),
