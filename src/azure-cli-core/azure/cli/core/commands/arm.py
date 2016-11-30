c2dd5ffa45c6deeab3384faf138c4679845fbb2d
yugangw-msft <yugangw@microsoft.com>
2016-11-29 21:46:09 -0800

    report correct error if no wait option is specified

--- src/azure-cli-core/azure/cli/core/commands/arm.py
+++ src/azure-cli-core/azure/cli/core/commands/arm.py
@@ -369,7 +369,7 @@ def cli_generic_wait_command(module_name, name, getter_op, factory=None):
         custom_condition = args.pop('custom')
         if not any([wait_for_created, wait_for_updated, wait_for_deleted,
                     wait_for_exists, custom_condition]):
-            raise CLIError("Please specify at least one flag from 'create', 'updated', 'delete', 'exits', or 'custom'")#pylint: disable=line-too-long
+            raise CLIError("incorrect usage: --created | --updated | --deleted | --exists | --custom JMESPATH")#pylint: disable=line-too-long
 
         for _ in range(0, timeout, interval):
             try:
