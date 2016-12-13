1fd5dcf561531e31aa5a0677ece3bf8de990df60
Derek Bekoe <debekoe@microsoft.com>
2016-12-13 15:38:37 -0800

    [Tab completion] Enable zsh compatibility mode for zsh shell (#1558)
    This is not full-proof as $SHELL is not always set to zsh (e.g. zsh isn’t your default shell).
    However, it will enable the compatibility mode if it is set.
    
    In the future, https://github.com/Azure/azure-cli/issues/630 will likely remove this file altogether.

--- src/azure-cli/az.completion.sh
+++ src/azure-cli/az.completion.sh
@@ -1,1 +1,11 @@
+case $SHELL in
+*/zsh)
+   echo 'Enabling ZSH compatibility mode';
+   autoload bashcompinit && bashcompinit
+   ;;
+*/bash)
+   ;;
+*)
+esac
+
 eval "$(register-python-argcomplete az)"
