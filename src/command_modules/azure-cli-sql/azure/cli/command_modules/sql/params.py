5d45ab6d1c8e510ead8e257c06994747a1eabf91
Troy Dai <troy.dai@outlook.com>
2017-01-07 14:21:25 -0800

    Add Azure SQL Server commands (#1645)
    Following commands are added:
    - az sql server
    - az sql server create
    - az sql server delete
    - az sql server update
    - az sql server show
    - az sql server list
    - az sql server usage
    - az sql server service-objective list
    - az sql server service-objective show
    - az sql server firewall create
    - az sql server firewall delete
    - az sql server firewall show
    - az sql server firewall list
    - az sql server firewall update
    - az sql database *
    - az sql elastic-pools *

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
@@ -3,3 +3,24 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from ._util import ParametersContext, patch_arg_make_required
+
+
+with ParametersContext(command='sql server create') as c:
+    from azure.mgmt.sql.models.server import Server
+
+    # About the patches:
+    #
+    # - Both administrator_login and administrator_login_password are required for server creation.
+    # However these two parameters are given default value in the create_or_update function
+    # signature, therefore, they can't be automatically converted to requirement arguments.
+    c.expand('parameters', Server, group_name='Creating', patches={
+        'administrator_login': patch_arg_make_required,
+        'administrator_login_password': patch_arg_make_required
+    })
+
+with ParametersContext(command='sql database create') as c:
+    from azure.mgmt.sql.models.database import Database
+
+    c.expand('parameters', Database, group_name='Creating')
+
