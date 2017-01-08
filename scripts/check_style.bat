dada55abfe2712d6f3ad10f29e6376357c6f610a
Troy Dai <troy.dai@outlook.com>
2017-01-07 22:00:46 -0800

    Update the shortcut script for check_style
    1. Install the automation package if it has not been not installed. This
    prevent failures from executing the scripts directly.
    2. Send arguments to the command.

--- scripts/check_style.bat
+++ scripts/check_style.bat
@@ -5,4 +5,5 @@ REM Copyright (c) Microsoft Corporation. All rights reserved.
 REM Licensed under the MIT License. See License.txt in the project root for license information.
 REM --------------------------------------------------------------------------------------------
 
-python -m automation.style.run
+pip show azure-cli-utility-automation>NUL || pip install -e %~dp0
+python -m automation.style.run %*
