5a5d561529e81187bdaac73338e021b84cc1ea2e
Vishrut Shah <vissha@microsoft.com>
2017-01-11 17:15:20 -0800

    Adding sql server show command to the tests

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/tests/test_sql_commands.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/tests/test_sql_commands.py
@@ -19,7 +19,7 @@ class SqlServerMgmtScenarioTest(ResourceGroupVCRTestBase):
 
     def __init__(self, test_method):
         super(SqlServerMgmtScenarioTest, self).__init__(__file__, test_method, resource_group='cli-test-sql-mgmt')
-        self.sql_server_name = 'cli-sql-12345-a'
+        self.sql_server_names = ['cli-sql-12345-a', 'cli-sql-12345-b']
         self.location = "westus"
         self.administrator_login = 'admin123'
         self.administrator_login_password = 'SecretPassword123'
@@ -29,22 +29,39 @@ class SqlServerMgmtScenarioTest(ResourceGroupVCRTestBase):
 
     def body(self):
         rg = self.resource_group
-        sql_server_name = self.sql_server_name
         loc = self.location
         user = self.administrator_login
         password = self.administrator_login_password
 
         # test create sql server with minimal required parameters
         self.cmd('sql server create -g {} --server-name {} -l {} --administrator-login {} --administrator-login-password {}'
-                .format(rg, sql_server_name, loc, user, password), checks=[
-            JMESPathCheck('name', sql_server_name),
-            JMESPathCheck('resourceGroup', rg)])
+                .format(rg, self.sql_server_names[0], loc, user, password), checks=[
+                    JMESPathCheck('name', self.sql_server_names[0]),
+                    JMESPathCheck('resourceGroup', rg),
+                    JMESPathCheck('administratorLogin', user)])
 
         # test list sql server should be 1
         self.cmd('sql server list -g {}'.format(rg), checks=[JMESPathCheck('length(@)', 1)])
 
+        # test create another sql server
+        self.cmd('sql server create -g {} --server-name {} -l {} --administrator-login {} --administrator-login-password {}'
+                .format(rg, self.sql_server_names[1], loc, user, password), checks=[
+                    JMESPathCheck('name', self.sql_server_names[1]),
+                    JMESPathCheck('resourceGroup', rg),
+                    JMESPathCheck('administratorLogin', user)])
+
+        # test list sql server should be 2
+        self.cmd('sql server list -g {}'.format(rg), checks=[JMESPathCheck('length(@)', 2)])
+
+        # test show sql server
+        self.cmd('sql server show -g {} --server-name {}'.format(rg, self.sql_server_names[0]), checks=[
+            JMESPathCheck('name', self.sql_server_names[0]),
+            JMESPathCheck('resourceGroup', rg),
+            JMESPathCheck('administratorLogin', user)])
+
         # test delete sql server
-        self.cmd('sql server delete -g {} --server-name {}'.format(rg, sql_server_name), checks=NoneCheck())
+        self.cmd('sql server delete -g {} --server-name {}'.format(rg, self.sql_server_names[0]), checks=NoneCheck())
+        self.cmd('sql server delete -g {} --server-name {}'.format(rg, self.sql_server_names[1]), checks=NoneCheck())
 
         # test list sql server should be 0
         self.cmd('sql server list -g {}'.format(rg), checks=[JMESPathCheck('length(@)', 0)])
