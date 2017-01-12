4d570890e3571ef98092d0576c31596c860e08d9
Troy Dai <troy.dai@outlook.com>
2017-01-12 09:24:40 -0800

    Multiple changes to automation module (#1720)
    * Update automation scripts
    
    1. Remove retired scripts testall, testall.bat, lintall, lintall.bat,
    and run_pylint.sh. They're replaced by builtin automation script
    check_style and run_tests.
    
    2. Add run_tests script to automation package.
    
    3. Remove unused code under /scripts/command_modules
    
    * Move modules installation script into automation module
    
    * Fix code styles
    
    * Move package verifier into automation module

--- scripts/automation/style/pep8.py
+++ scripts/automation/style/pep8.py
@@ -3,25 +3,26 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import sys
+
 
 def fix_p2p8(directory):
     import autopep8
     import multiprocessing
 
+    # pylint: disable=protected-access
     autopep8.fix_multiple_files([directory],
                                 options=autopep8._get_options(
-                                {
-                                    'jobs': multiprocessing.cpu_count(),
-                                    'verbose': True,
-                                    'recursive': True,
-                                    'in_place': True,
-                                    'max_line_length': 100
-                                }, False))
+                                    {
+                                        'jobs': multiprocessing.cpu_count(),
+                                        'verbose': True,
+                                        'recursive': True,
+                                        'in_place': True,
+                                        'max_line_length': 100
+                                    }, False))
 
 
 if __name__ == '__main__':
-    import sys
-
     if len(sys.argv) < 2:
         print('usage: python automation.style.pep8 <directory>')
         sys.exit(1)
--- scripts/testall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-python -m unittest discover -s src/azure-cli/azure/cli/tests --buffer
-python -m unittest discover -s src/azure-cli-core/azure/cli/core/tests --buffer
-python scripts/command_modules/test.py
