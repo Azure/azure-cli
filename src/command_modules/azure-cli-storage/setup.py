935fb0e4b26e98da8fc0b1dbfe74554c0e797c3f
Derek Bekoe <debekoe@microsoft.com>
2017-01-12 11:58:47 -0800

    Add HISTORY.rst to all modules and include in module long description
    When published to PyPI, the changelog will be shown on the module page.

--- src/command_modules/azure-cli-storage/setup.py
+++ src/command_modules/azure-cli-storage/setup.py
@@ -31,12 +31,14 @@ DEPENDENCIES = [
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     README = f.read()
+with open('HISTORY.rst', 'r', encoding='utf-8') as f:
+    HISTORY = f.read()
 
 setup(
     name='azure-cli-storage',
     version=VERSION,
     description='Microsoft Azure Command-Line Tools Storage Command Module',
-    long_description=README,
+    long_description=README + '\n\n' + HISTORY,
     license='MIT',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
