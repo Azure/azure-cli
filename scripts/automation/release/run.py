b80bfd049348865a000d424f3e0a533359374e07
Derek Bekoe <debekoe@microsoft.com>
2017-01-13 14:01:47 -0800

    0.1.0 release of GitHub bot
    Bot can now upload source code, .tar.gz and .whl assets to the GitHub release

--- scripts/automation/release/run.py
+++ scripts/automation/release/run.py
@@ -8,10 +8,18 @@ from __future__ import print_function
 import argparse
 import os
 import tempfile
+import tarfile
 from subprocess import check_call
 
 from .version_patcher import VersionPatcher
 from ..utilities.path import get_all_module_paths
+
+SOURCE_ARCHIVE_NAME = 'source.tar.gz'
+
+
+def save_source_in_archive(pkg_path, dest):
+    with tarfile.open(os.path.join(dest, SOURCE_ARCHIVE_NAME), 'w:gz') as tar:
+        tar.add(pkg_path, arcname=os.path.basename(pkg_path))
 
 
 def build(pkg_path, dest):
@@ -24,21 +32,22 @@ def build(pkg_path, dest):
 
 def release(pkg_dir, repo):
     """Release all packages in a directory"""
-    pkgs = [os.path.join(pkg_dir, f) for f in os.listdir(pkg_dir)]
+    pkgs = [os.path.join(pkg_dir, f) for f in os.listdir(pkg_dir) if f != SOURCE_ARCHIVE_NAME]
     for pkg in pkgs:
         check_call(['twine', 'register', '--repository-url', repo, '--repository', repo, pkg])
         check_call(['twine', 'upload', '--repository-url', repo, '--repository', repo, pkg])
 
 
-def run_build_release(component_name, repo, use_version_patch=True):
+def run_build_release(component_name, repo, dest=None, use_version_patch=True):
     """
     component_name - The full component name (e.g. azure-cli, azure-cli-core, azure-cli-vm, etc.)
     """
     for comp_name, comp_path in get_all_module_paths():
         if comp_name == component_name:
-            pkg_dir = tempfile.mkdtemp()
+            pkg_dir = dest or tempfile.mkdtemp()
             patcher = VersionPatcher(use_version_patch, component_name, comp_path)
             patcher.patch()
+            save_source_in_archive(comp_path, pkg_dir)
             build(comp_path, pkg_dir)
             patcher.unpatch()
             print("Built '{}' to '{}'".format(comp_name, pkg_dir))
@@ -61,11 +70,17 @@ if __name__ == '__main__':
     parser.add_argument('--repo', '-r',
                         help='Repository URL for release (e.g. https://pypi.python.org/pypi, '
                              'https://testpypi.python.org/pypi)')
+    parser.add_argument('--dest',
+                        help='Directory to store assets. By default, a temp directory is used.')
     args = parser.parse_args()
     if args.repo:
         assert os.environ.get('TWINE_USERNAME') and os.environ.get('TWINE_PASSWORD'), \
             "Set TWINE_USERNAME and TWINE_PASSWORD environment variables to authentication with " \
             "PyPI repository."
+    if args.dest:
+        args.dest = os.path.abspath(args.dest)
+        assert os.path.isdir(args.dest), "Directory '{}' does not exist".format(args.dest)
     run_build_release(args.component,
                       args.repo,
+                      args.dest,
                       args.no_version_patch)
--- scripts/github_bot/HISTORY.rst
+++ scripts/github_bot/HISTORY.rst
@@ -7,3 +7,8 @@ Release History
 ++++++++++++++++++
 
 * First release.
+* Uploads source code, .tar.gz and .whl assets to the GitHub release.
+* Bot comments on opened Release PR with checklist
+* Bot comments if release successful or not
+* Bot applies 'Release' label automatically
+* Bot creates a GitHub release
