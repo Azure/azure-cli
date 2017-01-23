ad3037b6808601b07b38e4cb95709c84174ca3af
Derek Bekoe <debekoe@microsoft.com>
2017-01-23 12:03:59 -0800

    Workaround for https://github.com/kevin1024/vcrpy/issues/293 - Do not run pylint on 3.6 as pylint doesn’t support 3.6 yet - Add Python 3.6 to package classifiers

--- scripts/build_in_travis.sh
+++ scripts/build_in_travis.sh
@@ -12,7 +12,10 @@ scripts_root=$(cd $(dirname $0); pwd)
 export PYTHONPATH=$PATHONPATH:./src
 python -m azure.cli -h
 
-check_style --ci
+# PyLint does not yet support Python 3.6 https://github.com/PyCQA/pylint/issues/1241
+if [ "$TRAVIS_PYTHON_VERSION" != "3.6" ]; then
+    check_style --ci;
+fi
 run_tests
 $scripts_root/package_verify.sh
 
