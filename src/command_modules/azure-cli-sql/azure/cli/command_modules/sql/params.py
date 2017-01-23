57baa659500012ed10c91cda554d20682e0db7d7
Vishrut Shah <vissha@microsoft.com>
2017-01-23 14:58:10 -0800

    Register sql alias --name -n with respect to most specific noun

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
@@ -6,6 +6,49 @@
 from ._util import ParametersContext, patch_arg_make_required
 
 
+with ParametersContext(command='sql db') as c:
+    c.register_alias('database_name', ('--name', '-n'))
+
+with ParametersContext(command='sql db data-warehouse') as c:
+    c.register_alias('database_name', ('--database-name',))
+
+with ParametersContext(command='sql db replication-link') as c:
+    c.register_alias('database_name', ('--database-name',))
+
+with ParametersContext(command='sql db restore-point') as c:
+    c.register_alias('database_name', ('--database-name',))
+
+with ParametersContext(command='sql db service-tier-advisor') as c:
+    c.register_alias('database_name', ('--database-name',))
+
+with ParametersContext(command='sql db transparent-data-encryption') as c:
+    c.register_alias('database_name', ('--database-name',))
+
+with ParametersContext(command='sql elastic-pools') as c:
+    c.register_alias('elastic_pool_name', ('--name', '-n'))
+
+with ParametersContext(command='sql elastic-pools db') as c:
+    c.register_alias('elastic_pool_name', ('--elastic-pool-name',))
+    c.register_alias('database_name', ('--name', '-n'))
+
+with ParametersContext(command='sql elastic-pools recommended') as c:
+    c.register_alias('recommended_elastic_pool_name', ('--name', '-n'))
+
+with ParametersContext(command='sql elastic-pools recommended db') as c:
+    c.register_alias('recommended_elastic_pool_name', ('--recommended-elastic-pool-name',))
+    c.register_alias('database_name', ('--name', '-n'))
+
+with ParametersContext(command='sql server') as c:
+    c.register_alias('server_name', ('--name', '-n'))
+
+with ParametersContext(command='sql server firewall') as c:
+    c.register_alias('server_name', ('--server-name',))
+    c.register_alias('firewall_rule_name', ('--name', '-n'))
+
+with ParametersContext(command='sql server service-objective') as c:
+    c.register_alias('server_name', ('--server-name',))
+    c.register_alias('service_objective_name', ('--name', '-n'))
+
 with ParametersContext(command='sql server create') as c:
     from azure.mgmt.sql.models.server import Server
 
