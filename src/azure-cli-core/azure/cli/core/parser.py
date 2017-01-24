3c5438dce29a9566bec4e3eaa04e025b4c828a43
Derek Bekoe <debekoe@microsoft.com>
2017-01-24 09:03:25 -0800

    Print parse error before usage statement (#1828)
    - Also, parse error now sent to logger instead of stderr directly so it is colorized also.

--- src/azure-cli-core/azure/cli/core/parser.py
+++ src/azure-cli-core/azure/cli/core/parser.py
@@ -3,14 +3,19 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import argparse
+import sys
 
+import argparse
 import argcomplete
 
 import azure.cli.core.telemetry as telemetry
 import azure.cli.core._help as _help
 from azure.cli.core._util import CLIError
 from azure.cli.core._pkg_util import handle_module_not_installed
+
+import azure.cli.core._logging as _logging
+
+logger = _logging.get_az_logger(__name__)
 
 
 class IncorrectUsageError(CLIError):
@@ -142,7 +147,11 @@ class AzCliCommandParser(argparse.ArgumentParser):
     def error(self, message):
         telemetry.set_user_fault('parse error: {}'.format(message))
         self._handle_command_package_error(message)
-        return super(AzCliCommandParser, self).error(message)
+
+        args = {'prog': self.prog, 'message': message}
+        logger.error('%(prog)s: error: %(message)s', args)
+        self.print_usage(sys.stderr)
+        self.exit(2)
 
     def format_help(self):
         is_group = self.is_group()
