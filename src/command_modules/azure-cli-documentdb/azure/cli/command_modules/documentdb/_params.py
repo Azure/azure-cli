fdaf9d5b6b1520ac0a473ebe7dc887c480ee3cf9
Digvijay Makwana <dimakwan@microsoft.com>
2017-01-30 12:18:35 -0800

    removed help for resource_group_location and added validation for ip range fitler

--- src/command_modules/azure-cli-documentdb/azure/cli/command_modules/documentdb/_params.py
+++ src/command_modules/azure-cli-documentdb/azure/cli/command_modules/documentdb/_params.py
@@ -33,6 +33,9 @@ def validate_locations(ns):
         loc_dict.append(Location(location_name=comps[0], failover_priority=int(comps[1])))
     ns.locations = loc_dict
 
+def validate_ip_range_filter(ns):
+    ns.ip_range_filter = ",".join(ns.ip_range_filter.split())
+
 register_cli_argument('documentdb', 'account_name', arg_type=name_type, help='Name of the DocumentDB Database Account', completer=get_resource_name_completion_list('Microsoft.DocumentDb/databaseAccounts'), id_part="name")
 
 register_cli_argument('documentdb regenerate-key', 'key_kind', **enum_choice_list(KeyKind))
@@ -40,9 +43,9 @@ register_cli_argument('documentdb failover-priority-change', 'failover_policies'
 
 register_cli_argument('documentdb create', 'account_name', completer=None)
 register_cli_argument('documentdb create', 'resource_group_name', help="name of the resource group")
-register_cli_argument('documentdb create', 'resource_group_location', ignore_type, help="location of the resource group")
+register_cli_argument('documentdb create', 'resource_group_location', ignore_type)
 register_cli_argument('documentdb create', 'locations', validator=validate_locations, help="space separated locations in 'regionName=failoverPriority' format. E.g \"East US\"=0. Failover priority values are 0 for write regions and greater than 0 for read regions. A failover priority value must be unique and less than the total number of regions.", nargs='+')
 register_cli_argument('documentdb create', 'default_consistency_level', help="default consistency level of the DocumentDB Database account", **enum_choice_list(DefaultConsistencyLevel))
 register_cli_argument('documentdb create', 'max_staleness_prefix', help="when used with Bounded Staleness consistency, this value represents the number of stale requests tolerated. Accepted range for this value is 1 - 2,147,483,647", type=int)
 register_cli_argument('documentdb create', 'max_interval', help="when used with Bounded Staleness consistency, this value represents the time amount of staleness (in seconds) tolerated. Accepted range for this value is 1 - 100", type=int)
-register_cli_argument('documentdb create', 'ip_range_filter', help="firewall support. Specifies the set of IP addresses or IP address ranges in CIDR form to be included as the allowed list of client IPs for a given database account. IP addresses/ranges must be comma separated and must not contain any spaces")
+register_cli_argument('documentdb create', 'ip_range_filter', validator=validate_ip_range_filter, help="firewall support. Specifies the set of IP addresses or IP address ranges in CIDR form to be included as the allowed list of client IPs for a given database account. IP addresses/ranges must be comma separated and must not contain any spaces")
