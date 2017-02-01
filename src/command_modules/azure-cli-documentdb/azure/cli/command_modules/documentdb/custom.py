e9c880771fce5d780da0601ec40d7fa0d16e89b0
Digvijay Makwana <digvijaymakwana@gmail.com>
2017-02-01 12:06:36 -0800

    DocumentDB Azure CLI support (#1815)
    * Initial documentdb commandlet
    
    * most commands completed
    
    * create command completed
    
    * Addressed PR comments
    
    * import ResourceManagementClient before using
    
    * fixed linter warnings
    
    * fixed help output
    
    * fixed additional linter errors
    
    * removed help for resource_group_location and added validation for ip range fitler
    
    * null check ip_range_filter
    
    * added multiple arguments capability to ip range filter
    
    * fixed pylint errors
    
    * perform a get on database account after create completes
    
    * use documentdb pypi package instead
    
    * cleared test

---
+++ src/command_modules/azure-cli-documentdb/azure/cli/command_modules/documentdb/custom.py
@@ -0,0 +1,52 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# --locations
+# -ipRangeFilter
+# -defaultConsistencyLevel
+# - MaxIntervalInSeconds
+# - MaxStalenessPrefix
+# - resource-group-location
+# -Kind
+
+from azure.mgmt.documentdb.models import (
+    ConsistencyPolicy,
+    DatabaseAccountCreateUpdateParameters,
+)
+
+# pylint:disable=too-many-arguments
+def cli_documentdb_create(client,
+                          resource_group_name,
+                          account_name,
+                          locations,
+                          default_consistency_level=None,
+                          max_staleness_prefix=100,
+                          max_interval=5,
+                          ip_range_filter=None):
+    # pylint:disable=line-too-long
+    """Create a new Azure DocumentDB database account.
+    """
+
+    consistency_policy = None
+    if default_consistency_level is not None:
+        consistency_policy = ConsistencyPolicy(default_consistency_level, max_staleness_prefix, max_interval)
+
+
+    from azure.mgmt.resource.resources import ResourceManagementClient
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    resource_client = get_mgmt_service_client(ResourceManagementClient)
+    rg = resource_client.resource_groups.get(resource_group_name)
+    resource_group_location = rg.location  # pylint: disable=no-member
+
+    params = DatabaseAccountCreateUpdateParameters(
+        resource_group_location,
+        locations,
+        consistency_policy=consistency_policy,
+        ip_range_filter=ip_range_filter)
+
+    async_docdb_create = client.database_accounts.create_or_update(resource_group_name, account_name, params)
+    docdb_account = async_docdb_create.result() # Will be empty, shouldn't
+    docdb_account = client.database_accounts.get(resource_group_name, account_name) # Workaround
+    return docdb_account
