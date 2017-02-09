720d832dde7eb3fd954557699c74e93583e37466
Troy Dai <troy.dai@outlook.com>
2017-02-09 14:53:02 -0800

    Make cloud module code PEP8 compliant, and enable style check in CI for it.

--- src/command_modules/azure-cli-cloud/azure/cli/command_modules/cloud/_params.py
+++ src/command_modules/azure-cli-cloud/azure/cli/command_modules/cloud/_params.py
@@ -8,27 +8,27 @@ from azure.cli.core.commands import register_cli_argument
 
 from azure.cli.core.cloud import get_clouds, get_custom_clouds
 
+
 # pylint: disable=line-too-long
 
-def get_cloud_name_completion_list(prefix, action, parsed_args, **kwargs):#pylint: disable=unused-argument
+def get_cloud_name_completion_list(prefix, action, parsed_args, **kwargs):  # pylint: disable=unused-argument
     return [c.name for c in get_clouds()]
 
-def get_custom_cloud_name_completion_list(prefix, action, parsed_args, **kwargs):#pylint: disable=unused-argument
+
+def get_custom_cloud_name_completion_list(prefix, action, parsed_args, **kwargs):  # pylint: disable=unused-argument
     return [c.name for c in get_custom_clouds()]
+
 
 register_cli_argument('cloud', 'cloud_name', options_list=('--name', '-n'),
                       help='Name of the registered cloud',
                       completer=get_cloud_name_completion_list)
 
-register_cli_argument('cloud show', 'cloud_name',
-                      help='Name of the registered cloud. If not specified, we use the current active cloud.')
-register_cli_argument('cloud update', 'cloud_name',
-                      help='Name of the registered cloud. If not specified, we use the current active cloud.')
+register_cli_argument('cloud show', 'cloud_name', help='Name of the registered cloud. If not specified, we use the current active cloud.')
+register_cli_argument('cloud update', 'cloud_name', help='Name of the registered cloud. If not specified, we use the current active cloud.')
 
 register_cli_argument('cloud register', 'cloud_name', completer=None)
 register_cli_argument('cloud register', 'cloud_config', options_list=('--cloud-config',),
                       help='JSON encoded cloud configuration. Use @{file} to load from a file.',
                       type=json.loads)
 
-register_cli_argument('cloud unregister', 'cloud_name',
-                      completer=get_custom_cloud_name_completion_list)
+register_cli_argument('cloud unregister', 'cloud_name', completer=get_custom_cloud_name_completion_list)
