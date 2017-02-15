ed9ab9dfedbb6a09568f6c963bdd4669626b7a87
Courtney Oka <oka.c@husky.neu.edu>
2017-02-15 12:32:23 -0800

    json dump

--- scripts/automation/commandlint/run.py
+++ scripts/automation/commandlint/run.py
@@ -2,11 +2,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+from __future__ import print_function
 import pkgutil
 
 import argparse
 import os
 import sys
+import json
 from importlib import import_module
 
 from automation.utilities.path import filter_user_selected_modules_with_tests
@@ -18,7 +20,8 @@ from azure.cli.core.commands import load_params, _update_command_definitions
 
 def dump_no_help(modules):
     cmd_table = APPLICATION.configuration.get_command_table()
-    exit = 0
+
+    exit_val = 0
     for cmd in cmd_table:
         cmd_table[cmd].load_arguments()
 
@@ -30,17 +33,33 @@ def dump_no_help(modules):
 
     _update_command_definitions(cmd_table)
 
+    command_list = []
+    subgroups_list = []
+    parameters = {}
     for cmd in cmd_table:
-        descrip = cmd_table[cmd].description
-        if not descrip:
-            exit = 1
-            print(cmd)
+        if not cmd_table[cmd].description:
+            command_list.append(cmd)
+            exit_val = 1
+        group_name = " ".join(cmd.split()[:-1])
+        if group_name in cmd_table and not cmd_table[group_name].description:
+            exit_val = 1
+            if group_name not in subgroups_list:
+                subgroups_list.append(group_name)
 
+        param_list = []
         for key in cmd_table[cmd].arguments:
             if cmd_table[cmd].arguments[key].type.settings.get('help') is None:
-                exit = 1
-                print(cmd + " " + str(cmd_table[cmd].arguments[key].name))
-    return exit
+                exit_val = 1
+                param_list.append(cmd_table[cmd].arguments[key].type.settings.get('name'))
+
+        parameters[cmd] = param_list
+    data = {
+        "subgroups" : subgroups_list,
+        "commands" : command_list,
+        "parameters" : parameters
+    }
+    json.dumps(data)
+    return exit_val
 
 if __name__ == '__main__':
     parse = argparse.ArgumentParser('Test tools')
@@ -64,8 +83,8 @@ if __name__ == '__main__':
                                          pkgutil.iter_modules(mods_ns_pkg.__path__)]
         except ImportError:
             pass
-        exit = dump_no_help(installed_command_modules)
+        exit_value = dump_no_help(installed_command_modules)
     else:
-        exit = dump_no_help(selected_modules)
-    sys.exit(exit)
+        exit_value = dump_no_help(selected_modules)
+    sys.exit(exit_value)
 
