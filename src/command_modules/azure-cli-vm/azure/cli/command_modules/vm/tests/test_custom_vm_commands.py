aa6a5608d662e64b3edb138e2690b0a0d5d43d36
Yugang Wang <yugangw@microsoft.com>
2017-02-17 20:35:48 +0100

    Disk encryption: Enable/Disable/Show (#2113)

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/test_custom_vm_commands.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/test_custom_vm_commands.py
@@ -5,13 +5,20 @@
 
 import unittest
 import mock
+
+from azure.cli.core._util import CLIError
+
 from azure.cli.command_modules.vm.custom import enable_boot_diagnostics, disable_boot_diagnostics
 from azure.cli.command_modules.vm.custom import (_get_access_extension_upgrade_info,
                                                  _LINUX_ACCESS_EXT,
                                                  _WINDOWS_ACCESS_EXT)
 from azure.cli.command_modules.vm.custom import \
     (attach_unmanaged_data_disk, detach_unmanaged_data_disk)
-from azure.mgmt.compute.models import NetworkProfile, StorageProfile, DataDisk
+from azure.cli.command_modules.vm.disk_encryption import enable, disable
+from azure.mgmt.compute.models import (NetworkProfile, StorageProfile, DataDisk, OSDisk,
+                                       OperatingSystemTypes, InstanceViewStatus,
+                                       VirtualMachineExtensionInstanceView,
+                                       VirtualMachineExtension, ImageReference)
 from azure.mgmt.compute.models.compute_management_client_enums import (DiskCreateOptionTypes,
                                                                        CachingTypes)
 
@@ -188,11 +195,79 @@ class Test_Vm_Custom(unittest.TestCase):
         mock_vm_set.assert_called_once_with(vm)
         self.assertEqual(len(vm.storage_profile.data_disks), 0)
 
+    # pylint: disable=line-too-long
+    @mock.patch('azure.cli.command_modules.vm.disk_encryption._compute_client_factory', autospec=True)
+    @mock.patch('azure.cli.command_modules.vm.disk_encryption._get_keyvault_key_url', autospec=True)
+    def test_enable_encryption_error_cases_handling(self, mock_get_keyvault_key_url, mock_compute_client_factory):
+        faked_keyvault = '/subscriptions/01234567-1bf0-4dda-aec3-cb9272f09590/resourceGroups/rg1/providers/Microsoft.KeyVault/vaults/v1'
+        os_disk = OSDisk(None, OperatingSystemTypes.linux)
+        existing_disk = DataDisk(lun=1, vhd='https://someuri', name='d1', create_option=DiskCreateOptionTypes.empty)
+        vm = FakedVM(None, [existing_disk], os_disk=os_disk)
+
+        compute_client_mock = mock.MagicMock()
+        compute_client_mock.virtual_machines.get.return_value = vm
+        mock_compute_client_factory.return_value = compute_client_mock
+
+        mock_get_keyvault_key_url.return_value = 'https://somevaults.vault.azure.net/'
+
+        # throw when VM has disks, but no --volume-type is specified
+        with self.assertRaises(CLIError) as context:
+            enable('rg1', 'vm1', 'client_id', faked_keyvault, 'client_secret')
+
+        self.assertTrue("supply --volume-type" in str(context.exception))
+
+        # throw when no AAD client secrets
+        with self.assertRaises(CLIError) as context:
+            enable('rg1', 'vm1', 'client_id', faked_keyvault)
+
+        self.assertTrue("--aad-client-id or --aad-client-cert-thumbprint" in str(context.exception))
+
+        # throw when the linux image does not support encryptions
+        vm.storage_profile.image_reference = ImageReference(publisher='OpenLogic', offer='centos', sku='7.1')
+        with self.assertRaises(CLIError) as context:
+            enable('rg1', 'vm1', 'client_id', faked_keyvault, 'client_secret', volume_type='DATA')
+
+        self.assertTrue("Encryption is not suppored for current VM. Supported are" in str(context.exception))
+
+    @mock.patch('azure.cli.command_modules.vm.disk_encryption.set_vm', autospec=True)
+    @mock.patch('azure.cli.command_modules.vm.disk_encryption._compute_client_factory', autospec=True)
+    def test_disable_encryption_error_cases_handling(self, mock_compute_client_factory, mock_vm_set):  # pylint: disable=unused-argument
+        os_disk = OSDisk(None, OperatingSystemTypes.linux)
+        existing_disk = DataDisk(lun=1, vhd='https://someuri', name='d1', create_option=DiskCreateOptionTypes.empty)
+        vm = FakedVM(None, [existing_disk], os_disk=os_disk)
+        vm_extension = VirtualMachineExtension('westus',
+                                               settings={'SequenceVersion': 1},
+                                               instance_view=VirtualMachineExtensionInstanceView(
+                                                   statuses=[InstanceViewStatus(message='Encryption completed successfully')],
+                                                   substatuses=[InstanceViewStatus(message='{"os":"Encrypted"}')]))
+        vm_extension.provisioning_state = 'Succeeded'
+        compute_client_mock = mock.MagicMock()
+        compute_client_mock.virtual_machines.get.return_value = vm
+        compute_client_mock.virtual_machine_extensions.get.return_value = vm_extension
+        mock_compute_client_factory.return_value = compute_client_mock
+
+        # throw on disabling encryption on OS disk of a linux VM
+        with self.assertRaises(CLIError) as context:
+            disable('rg1', 'vm1', 'OS')
+
+        self.assertTrue("Only data disk is supported to disable on Linux VM" in str(context.exception))
+
+        # throw on disabling encryption on data disk, but os disk is also encrypted
+        with self.assertRaises(CLIError) as context:
+            disable('rg1', 'vm1', 'DATA')
+
+        self.assertTrue("Disabling encryption on data disk can still cause VM unbootable" in str(context.exception))
+
+        # works fine to disable encryption on daat disk when OS disk is never encrypted
+        vm_extension.instance_view.substatuses[0].message = '{}'
+        disable('rg1', 'vm1', 'DATA')
+
 
 class FakedVM:  # pylint: disable=too-few-public-methods,old-style-class
-    def __init__(self, nics=None, disks=None):
+    def __init__(self, nics=None, disks=None, os_disk=None):
         self.network_profile = NetworkProfile(nics)
-        self.storage_profile = StorageProfile(data_disks=disks)
+        self.storage_profile = StorageProfile(data_disks=disks, os_disk=os_disk)
+        self.location = 'westus'
 
 
 class FakedAccessExtensionEntity:  # pylint: disable=too-few-public-methods,old-style-class
