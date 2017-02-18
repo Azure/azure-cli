9b487e199318b69b5a61c10761b7ea1f97da3986
davidmu1 <davidmu@microsoft.com>
2017-02-17 16:17:49 -0800

    added role, sql, and taskhelp

--- src/command_modules/azure-cli-role/azure/cli/command_modules/role/_help.py
+++ src/command_modules/azure-cli-role/azure/cli/command_modules/role/_help.py
@@ -9,25 +9,25 @@ from azure.cli.core.help_files import helps #pylint: disable=unused-import
 
 helps['ad sp create-for-rbac'] = """
             examples:
-                - name: Create with defaults
+                - name: Create role assignments with defaults.
                   text: az ad sp create-for-rbac
-                - name: Create with a custom name
+                - name: Create role assignments with a custom name.
                   text: az ad sp create-for-rbac -n "http://my-app"
-                - name: Create role assignments at the same time
+                - name: Create role assignments at the same time.
                   text: az ad sp create-for-rbac -n "http://my-app" --role contributor --scopes /subscriptions/11111111-2222-3333-4444-555555555555/resourceGroups/mygroup /subscriptions/11111111-2222-3333-4444-666666666666/resourceGroups/my-another-group
-                - name: Login with this service principal
+                - name: Login with a service principal.
                   text: az login --service-principal -u <name> -p <password> --tenant <tenant>
-                - name: Reset credentials on expiration
+                - name: Reset credentials on expiration.
                   text: az ad sp reset-credentials --name <name>
-                - name: Create role assignments
+                - name: Create role assignments.
                   text: az role assignment create --assignee <name> --role Contributor
-                - name: Revoke the service principal when done with it
+                - name: Revoke the service principal when done with it.
                   text: az ad app delete --id <name>
             """
 
 helps['role'] = """
     type: group
-    short-summary: Use role assignments to manage access to your Azure resources
+    short-summary: Use role assignments to manage access to your Azure resources.
 """
 helps['role assignment'] = """
     type: group
@@ -35,30 +35,30 @@ helps['role assignment'] = """
 """
 helps['role assignment create'] = """
     type: command
-    short-summary: Create a new role assignment
+    short-summary: Create a new role assignment.
 """
 helps['role assignment delete'] = """
     type: command
-    short-summary: Delete role assignments
+    short-summary: Delete role assignments.
 """
 helps['role assignment list'] = """
     type: command
-    short-summary: list role assignments
+    short-summary: List role assignments.
 """
 helps['role definition'] = """
     type: group
-    short-summary: Manage role definitions
+    short-summary: Manage role definitions.
 """
 
 helps['role definition create'] = """
     type: command
-    short-summary: Create a custom role definition
+    short-summary: Create a custom role definition.
     parameters: 
         - name: --role-definition
           type: string
           short-summary: 'JSON formatted string or a path to a file with such content'
     examples:
-        - name: Create a role with following definition content
+        - name: Create a role with following definition content:
           text: |
                 {
                     "Name": "Contoso On-call",
@@ -81,15 +81,15 @@ helps['role definition create'] = """
 
 helps['role definition delete'] = """
     type: command
-    short-summary: Delete a role definition
+    short-summary: Delete a role definition.
 """
 helps['role definition list'] = """
     type: command
-    short-summary: list role definitions
+    short-summary: List role definitions.
 """
 helps['role definition update'] = """
     type: command
-    short-summary: update a role definition
+    short-summary: Update a role definition.
 """
 helps['role definition create'] = """
             type: command
@@ -98,7 +98,7 @@ helps['role definition create'] = """
                   type: string
                   short-summary: 'JSON formatted string or a path to a file with such content'
             examples:
-                - name: Create a role with following definition content
+                - name: Create a role with following definition content:
                   text: |
                         {
                             "Name": "Contoso On-call",
@@ -121,21 +121,21 @@ helps['role definition create'] = """
             """
 helps['ad'] = """
     type: group
-    short-summary: Synchronize on-premises directories and manage Azure Active Directory (AAD) resources
+    short-summary: Synchronize on-premises directories and manage Azure Active Directory resources.
 """
 helps['ad app'] = """
     type: group
-    short-summary: Manages AAD applications
+    short-summary: Manage Azure Active Directory applications.
 """
 helps['ad group'] = """
     type: group
-    short-summary: Manages AAD groups
+    short-summary: Manage Azure Active Directory groups.
 """
 helps['ad sp'] = """
     type: group
-    short-summary: Manages AAD service principals for automation authentication
+    short-summary: Manage Azure Active Directory service principals for automation authentication.
 """
 helps['ad user'] = """
     type: group
-    short-summary: Manages AAD users and user authentication
+    short-summary: Manage Azure Active Directory users and user authentication.
 """
--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/help.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/help.py
@@ -9,93 +9,93 @@ from azure.cli.core.help_files import helps
 
 helps['sql'] = """
             type: group
-            short-summary: Commands to manage Azure SQL databases.
+            short-summary: Manage Azure SQL databases.
             """
 helps['sql server'] = """
             type: group
-            short-summary: Commands to manage servers.
+            short-summary: Manage servers.
             """
 helps['sql server create'] = """
             type: command
-            short-summary: Create a new Azure SQL server
+            short-summary: Create an Azure SQL server.
             """
 helps['sql server list'] = """
             type: command
-            short-summary: List the SQL servers belong to given resource group.
+            short-summary: List Azure SQL servers.
             """
 helps['sql server update'] = """
             type: command
-            short-summary: Update an Azure SQL server.
+            short-summary: Update a Azure SQL server.
             """
 helps['sql server firewall'] = """
             type: group
-            short-summary: Commands to manage Azure SQL server's firewall rules
+            short-summary: Manage Azure SQL server's firewall rules.
             """
 helps['sql server firewall create'] = """
             type: command
-            short-summary: Create an Azure SQL server firewall rule
+            short-summary: Create an Azure SQL server firewall rule.
             """
 helps['sql server firewall update'] = """
             type: command
-            short-summary: Update an Azure SQL server firewall rule
+            short-summary: Update an Azure SQL server firewall rule.
             """
 helps['sql server firewall show'] = """
             type: command
-            short-summary: Show the details of an Azure SQL server firewall rule
+            short-summary: Show the details of an Azure SQL server firewall rule.
             """
 helps['sql server firewall list'] = """
             type: command
-            short-summary: List the Azure SQL server firewall rules
+            short-summary: List the Azure SQL server firewall rules.
             """
 helps['sql server service-objective'] = """
             type: group
-            short-summary: Commands to show Azure SQL server's service objectives
+            short-summary: Show Azure SQL server's service objectives.
             """
 helps['sql db'] = """
             type: group
-            short-summary: Commands to manage databases
+            short-summary: Manage databases.
             """
 helps['sql db create'] = """
             type: command
-            short-summary: Create an Azure SQL database
+            short-summary: Create an Azure SQL database.
             """
 helps['sql db replication-link'] = """
             type: group
-            short-summary: Manage database replication links
+            short-summary: Manage database replication links.
             """
 helps['sql db data-warehouse'] = """
             type: group
-            short-summary: Manage database data warehouse
+            short-summary: Manage database data warehouse.
             """
 helps['sql db restore-point'] = """
             type: group
-            short-summary: Manage database restore points
+            short-summary: Manage database restore points.
             """
 helps['sql db transparent-data-encryption'] = """
             type: group
-            short-summary: Manage database transparent data encryption
+            short-summary: Manage database transparent data encryption.
             """
 helps['sql db service-tier-advisor'] = """
             type: group
-            short-summary: Manage database service tier advisors
+            short-summary: Manage database service tier advisors.
             """
 helps['sql elastic-pools'] = """
             type: group
-            short-summary: Commands to manage database elastic pools
+            short-summary: Manage database elastic pools.
             """
 helps['sql elastic-pools update'] = """
             type: command
-            short-summary: Update a database elastic pool
+            short-summary: Update a database elastic pool.
             """
 helps['sql elastic-pools db'] = """
             type: group
-            short-summary: Command to manage databases activities in database elastic pools
+            short-summary: Manage databases activities in database elastic pools.
             """
 helps['sql elastic-pools recommended'] = """
             type: group
-            short-summary: Commands to see information about an Azure SQL Recommended Elastic Pool
+            short-summary: Get information about an Azure SQL recommended elastic pool.
             """
 helps['sql elastic-pools recommended db'] = """
             type: group
-            short-summary: Commands to see information about an Azure SQL database inside of Recommended Elastic Pool
+            short-summary: Get information about an Azure SQL database inside of recommended elastic pool.
             """
