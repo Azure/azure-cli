9b32cf6ae75f4ecc0fcb6f84127f66cd8c63a751
Vishrut Shah <vissha@microsoft.com>
2017-02-22 10:14:17 -0800

    Implement filter for metrics-definitions list command

--- src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/custom.py
+++ src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/custom.py
@@ -2,3 +2,27 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+
+
+def list_metric_definitions(client, resource_uri, metric_names=None):
+    '''Commands to manage metric definitions.
+    :param str resource_uri: The identifier of the resource
+    :param str metric_names: The list of metric names
+    '''
+    odata_filter = _list_metric_definitions_filter_builder(metric_names)
+    metric_definitions = client.list(resource_uri, filter=odata_filter)
+    return list(metric_definitions)
+
+
+def _list_metric_definitions_filter_builder(metric_names=None):
+    '''Build up OData filter string from metric_names
+    '''
+    filters = []
+    if metric_names:
+        if "," in metric_names:
+            names = metric_names.split(",")
+            for metric_name in names:
+                filters.append("name.value eq '{}'".format(metric_name))
+        else:
+            filters.append("name.value eq '{}'".format(metric_names))
+    return ' or '.join(filters)
