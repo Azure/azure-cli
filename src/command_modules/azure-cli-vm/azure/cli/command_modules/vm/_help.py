c1e4cd81c21de7f7f466d51789a8ca052dc2e09c
Aaron Roney <twitchax@gmail.com>
2017-02-23 15:56:46 -0800

    Snapshot help update.

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_help.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_help.py
@@ -853,7 +853,7 @@ helps['disk'] = """
 
 helps['snapshot'] = """
     type: group
-    short-summary: Manage snapshots of Virtual Machines.
+    short-summary: Manage point-in-time copies of a managed disk, native blobs, or another snapshots.
 """
 
 helps['image'] = """
