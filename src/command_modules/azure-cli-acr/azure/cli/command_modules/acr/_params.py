84c1a3ba6e7f94475e40ce1ce2751f4e001e7914
Peter Hsu <shhsu@microsoft.com>
2017-02-27 10:25:16 -0800

    pylint: disable=line-too-long

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=line-too-long
+
 from azure.cli.core.commands import register_cli_argument
 from azure.cli.core.commands.parameters import (
     resource_group_name_type,
