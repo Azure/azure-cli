23a10eb94545a0fb1b418d5ae4577a0e3b4301ab
Brendan Burns <bburns@microsoft.com>
2017-03-03 22:01:09 -0800

    Add some docs for az lock, remove an unused flag, rename another.

--- src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/_params.py
+++ src/command_modules/azure-cli-resource/azure/cli/command_modules/resource/_params.py
@@ -7,6 +7,7 @@
 from argcomplete.completers import FilesCompleter
 
 from azure.mgmt.resource.resources.models import DeploymentMode
+from azure.mgmt.resource.locks.models import LockLevel
 from azure.cli.core.commands import register_cli_argument, CliArgumentType
 from azure.cli.core.commands.parameters import (ignore_type, resource_group_name_type, tag_type,
                                                 tags_type, get_resource_group_completion_list,
@@ -75,3 +76,6 @@ register_cli_argument('group create', 'resource_group_name', completer=None)
 
 register_cli_argument('tag', 'tag_name', options_list=('--name', '-n'))
 register_cli_argument('tag', 'tag_value', options_list=('--value',))
+
+register_cli_argument('lock', 'name', options_list=('--name', '-n'))
+register_cli_argument('lock create', 'level', options_list=('--lock-type', '-t'), required=True, **enum_choice_list(LockLevel))
