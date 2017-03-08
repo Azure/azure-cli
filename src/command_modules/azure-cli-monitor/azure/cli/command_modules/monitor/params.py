45fe44aae517073a70437c5f64d7808063592fdc
Vishrut Shah <vissha@microsoft.com>
2017-03-08 14:24:42 -0800

    Rename expanded params and resolve bugs

--- src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/params.py
+++ src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/params.py
@@ -13,9 +13,6 @@ with ParametersContext(command='monitor alert-rules') as c:
 with ParametersContext(command='monitor alert-rule-incidents') as c:
     c.register_alias('incident_name', ('--name', '-n'))
 
-with ParametersContext(command='monitor log-profiles') as c:
-    c.register_alias('log_profile_name', ('--name', '-n'))
-
 with ParametersContext(command='monitor autoscale-settings') as c:
     c.register_alias('name', ('--azure-resource-name',))
     c.register_alias('autoscale_setting_name', ('--name', '-n'))
@@ -28,17 +25,42 @@ with ParametersContext(command='monitor alert-rules create') as c:
                type=json.loads,
                help='JSON encoded condition configuration. Use @{file} to load from a file.')
 
-with ParametersContext(command='monitor service-diagnostic-settings create') as c:
-    from azure.mgmt.monitor.models.service_diagnostic_settings import \
-        (ServiceDiagnosticSettings)
+with ParametersContext(command='monitor service-diagnostic-settings') as c:
+    c.register_alias('resource_uri', ('--resource-id',))
 
-    c.expand('parameters', ServiceDiagnosticSettings)
+with ParametersContext(command='monitor service-diagnostic-settings create') as c:
+    from azure.mgmt.monitor.models.service_diagnostic_settings_resource import \
+        (ServiceDiagnosticSettingsResource)
+
+    c.expand('parameters', ServiceDiagnosticSettingsResource)
+    c.register_alias('resource_uri', ('--resource-id',))
+    c.register('logs', ('--logs',),
+               type=json.loads,
+               help='JSON encoded list of logs settings. Use @{file} to load from a file.')
+    c.register('metrics', ('--metrics',),
+               type=json.loads,
+               help='JSON encoded list of metric settings. Use @{file} to load from a file.')
+
+
+with ParametersContext(command='monitor log-profiles') as c:
+    c.register_alias('log_profile_name', ('--name', '-n'))
 
 with ParametersContext(command='monitor log-profiles create') as c:
-    from azure.mgmt.monitor.models.log_profile_properties import \
-        (LogProfileProperties)
+    from azure.mgmt.monitor.models.log_profile_resource import LogProfileResource
 
-    c.expand('parameters', LogProfileProperties)
+    c.register_alias('name', ('--log-profile-name',))
+    c.expand('parameters', LogProfileResource)
+    c.register('retention_policy', ('--retention-policy',),
+               type=json.loads,
+               help='JSON encoded retention policy settings. Use @{file} to load from a file.')
+    c.register_alias('name', ('--log-profile-resource-name',))
+    c.register_alias('log_profile_name', ('--name', '-n'))
+    c.argument('categories', nargs='+',
+               help="Space separated categories of the logs.Some values are: "
+                    "'Write', 'Delete', and/or 'Action.'")
+    c.argument('locations', nargs='+',
+               help="Space separated list of regions for which Activity Log events "
+                    "should be stored.")
 
 with ParametersContext(command='monitor autoscale-settings create') as c:
     c.register('parameters', ('--parameters',),
