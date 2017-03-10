feec58cbf04d45af761e16479f5f430733c3792d
begoldsm <begoldsm@users.noreply.github.com>
2017-03-09 17:08:17 -0800

    Initial ADLS and ADLA commands (#2257)
    * Initial ADLS and ADLA commands
    
    This just gets initial structure in place based on redis commands. This
    has not yet been replaced to have functional ADL commands yet.
    
    initial functional ADL commands in python
    
    Update versions and add initial custom functions
    
    Add customizations and flesh out commands.
    
    Add full help and fix up custom logic for ADL
    
    Add filesystem commands.
    
    explicitly convert to ints.
    
    * Update to fix my one test and get all the help in
    
    * Remove my debug logic.
    
    * Address PR feedback.
    
    add dashes into some command names
    commonize parameters
    fix help
    add the ability to determine location based on resource group
    
    * remove common params that are globally defined.
    
    * Add more tests, address some comments
    
    Address comments about help
    Still todo:
    Tests for job, catalog and file
    Add validators.
    
    * Add tests and fixes for filesystem.
    
    * pylint and test fixes for ADL
    
    There is one test remaining that needs to be fixed, and validators are
    required, but this covers pretty much all necessary changes.
    
    * Add validators and fix pylint and tests
    
    Add resource group validator
    Fix tests for scrubbing Job IDs
    Re-record job test
    Address remaining pylint issues.
    
    * fix help, address 2.7 pylint.
    
    * Address PR comments
    
    * add help and functionality for the generic @file
    
    * Fix pylint error.
    
    * Address final PR comments
    
    Move mock.patch to my test file
    move mock_get_uuid_str() to my test file
    move all tests to the root of azure-cli-datalake
    fix single params to work properly (missing empty second param)
    
    * Fixing tests.
    
    * Missing comma breaking tests
    
    * fix incorrect param in help.

---
+++ src/command_modules/azure-cli-datalake/README.rst
@@ -0,0 +1,7 @@
+Microsoft Azure CLI 'datalake' Command Module
+=============================================
+
+This package is for the 'datalake' module.
+i.e. 'az datalake'
+
+
---
+++ src/command_modules/azure-cli-datalake/azure/cli/command_modules/datalake/analytics/__init__.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+def load_params(_):
+    #pylint: disable=redefined-outer-name
+    import azure.cli.command_modules.datalake.analytics._params
