d79710730933432d2c8ccd722d31fbbf75845ce0
Yugang Wang <yugangw@microsoft.com>
2017-03-11 05:03:38 +0100

    core: support setting default values for common arguments like default resource group, default web, default vm (#2414)
    * wip
    
    * rename and skip a warning on conflicting with using id if defaults get applied
    
    * add a configure command for appservice
    
    * move logics of setting default RG to core
    
    * address lint errors
    
    * address review feedback
    
    * address review feedback
    
    * review feedback
    
    * minor help text change

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/_params.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/_params.py
@@ -57,7 +57,9 @@ register_cli_argument('appservice plan', 'number_of_workers', help='Number of wo
 register_cli_argument('appservice plan', 'admin_site_name', help='The name of the admin web app.')
 
 register_cli_argument('appservice web', 'slot', options_list=('--slot', '-s'), help="the name of the slot. Default to the productions slot if not specified")
-register_cli_argument('appservice web', 'name', arg_type=name_arg_type, completer=get_resource_name_completion_list('Microsoft.Web/sites'), id_part='name', help='name of the web')
+register_cli_argument('appservice web', 'name', configured_default='web',
+                      arg_type=name_arg_type, completer=get_resource_name_completion_list('Microsoft.Web/sites'), id_part='name',
+                      help="name of the web. You can configure the default using 'az configure --defaults web=<name>'")
 register_cli_argument('appservice web create', 'name', options_list=('--name', '-n'), help='name of the new webapp')
 register_cli_argument('appservice web create', 'plan', options_list=('--plan', '-p'), completer=get_resource_name_completion_list('Microsoft.Web/serverFarms'),
                       help="name or resource id of the app service plan. Use 'appservice plan create' to get one")
