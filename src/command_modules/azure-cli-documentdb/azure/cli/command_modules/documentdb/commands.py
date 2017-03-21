58ddcc2642600c4455f4f2c134b788e2d1d2976c
Derek Bekoe <debekoe@microsoft.com>
2017-03-21 09:39:55 -0700

    Lambda to function definition

--- src/command_modules/azure-cli-documentdb/azure/cli/command_modules/documentdb/commands.py
+++ src/command_modules/azure-cli-documentdb/azure/cli/command_modules/documentdb/commands.py
@@ -10,14 +10,16 @@ from azure.cli.command_modules.documentdb._client_factory import (cf_documentdb)
 
 mgmt_path = 'azure.mgmt.documentdb.operations.database_accounts_operations#{}'
 
-factory = lambda kwargs: cf_documentdb().database_accounts  # noqa: E731 lambda vs def
-cli_command(__name__, 'documentdb show', mgmt_path.format('DatabaseAccountsOperations.get'), factory)
-cli_command(__name__, 'documentdb list-keys', mgmt_path.format('DatabaseAccountsOperations.list_keys'), factory)
-cli_command(__name__, 'documentdb list-read-only-keys', mgmt_path.format('DatabaseAccountsOperations.list_read_only_keys'), factory)
-cli_command(__name__, 'documentdb regenerate-key', mgmt_path.format('DatabaseAccountsOperations.regenerate_key'), factory)
-cli_command(__name__, 'documentdb check-name-exists', mgmt_path.format('DatabaseAccountsOperations.check_name_exists'), factory)
-cli_command(__name__, 'documentdb delete', mgmt_path.format('DatabaseAccountsOperations.delete'), factory)
-cli_command(__name__, 'documentdb failover-priority-change', mgmt_path.format('DatabaseAccountsOperations.failover_priority_change'), factory)
-cli_command(__name__, 'documentdb create', 'azure.cli.command_modules.documentdb.custom#cli_documentdb_create', factory)
-cli_command(__name__, 'documentdb update', 'azure.cli.command_modules.documentdb.custom#cli_documentdb_update', factory)
-cli_command(__name__, 'documentdb list', 'azure.cli.command_modules.documentdb.custom#cli_documentdb_list', factory)
+def db_accounts_factory(_):
+    return cf_documentdb().database_accounts
+
+cli_command(__name__, 'documentdb show', mgmt_path.format('DatabaseAccountsOperations.get'), db_accounts_factory)
+cli_command(__name__, 'documentdb list-keys', mgmt_path.format('DatabaseAccountsOperations.list_keys'), db_accounts_factory)
+cli_command(__name__, 'documentdb list-read-only-keys', mgmt_path.format('DatabaseAccountsOperations.list_read_only_keys'), db_accounts_factory)
+cli_command(__name__, 'documentdb regenerate-key', mgmt_path.format('DatabaseAccountsOperations.regenerate_key'), db_accounts_factory)
+cli_command(__name__, 'documentdb check-name-exists', mgmt_path.format('DatabaseAccountsOperations.check_name_exists'), db_accounts_factory)
+cli_command(__name__, 'documentdb delete', mgmt_path.format('DatabaseAccountsOperations.delete'), db_accounts_factory)
+cli_command(__name__, 'documentdb failover-priority-change', mgmt_path.format('DatabaseAccountsOperations.failover_priority_change'), db_accounts_factory)
+cli_command(__name__, 'documentdb create', 'azure.cli.command_modules.documentdb.custom#cli_documentdb_create', db_accounts_factory)
+cli_command(__name__, 'documentdb update', 'azure.cli.command_modules.documentdb.custom#cli_documentdb_update', db_accounts_factory)
+cli_command(__name__, 'documentdb list', 'azure.cli.command_modules.documentdb.custom#cli_documentdb_list', db_accounts_factory)
