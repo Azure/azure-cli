0d4e36410c0c2b50c6fb200b3c6dfc99520fa2a9
Troy Dai <troy.dai@outlook.com>
2017-03-23 13:36:50 -0700

    Improve ScenarioTest and update storage test. (#2609)
    * Update blob upload scenario
    
    Update two storage blob test cases to prepare for LBB related scenario
    tests.
    
    * Improve the ScenarioTest to support large file download scenario
    
    1. Allow processors to modify response/request during replay.
    2. Compress large response body.
    3. Mock large response body during replay.
    
    * Make sure test passes in 2.7

--- src/azure-cli-testsdk/azure/cli/testsdk/patches.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/patches.py
@@ -53,7 +53,6 @@ def patch_long_run_operation_delay(unit_test):
     def _shortcut_long_run_operation(*args, **kwargs):  # pylint: disable=unused-argument
         return
 
-
     _mock_in_unit_test(unit_test,
                        'msrestazure.azure_operation.AzureOperationPoller._delay',
                        _shortcut_long_run_operation)
