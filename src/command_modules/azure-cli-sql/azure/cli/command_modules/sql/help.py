10852f85e0f87b321f753095519eb14922ec8be2
Jared Moore <jaredmoo@microsoft.com>
2017-03-22 18:47:00 -0700

    Fixed Import/Export examples

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/help.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/help.py
@@ -91,39 +91,29 @@ helps['sql db replica delete-link'] = """
     """
 helps['sql db export'] = """
     type: command
-    short-summary: Exports a database to a bacpac
-
+    short-summary: Exports a database to a bacpac.
     examples:
         - name: Get SAS key for use in export operation
-        text: az storage blob generate-sas --account-name myAccountName -c myContainer -n myBacpac.bacpac --permissions w --expiry 2018-01-01T00:00:00Z
-
+          text: az storage blob generate-sas --account-name myAccountName -c myContainer -n myBacpac.bacpac --permissions w --expiry 2018-01-01T00:00:00Z
         - name: Export bacpac using SAS key
-        text: az sql db import -s myserver -n mydatabase -g mygroup -p password -u login --storage-key "?sr=b&sp=rw&se=2018-01-01T00%3A00%3A00Z&sig=sdfsdfklsdjflSLIFJLSIEJFLKSDJFDd/%2wdfskdjf3%3D&sv=2015-07-08" --storage-key-type SharedAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
-
+          text: az sql db export -s myserver -n mydatabase -g mygroup -p password -u login --storage-key "?sr=b&sp=rw&se=2018-01-01T00%3A00%3A00Z&sig=mysignature&sv=2015-07-08" --storage-key-type SharedAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
         - name: Export bacpac using storage account Key
-        text: az sql db import -s myserver -n mydatabase -g mygroup -p password -u login --storage-key "storageaccountkey" --storage-key-type StorageAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
+          text: az sql db export -s myserver -n mydatabase -g mygroup -p password -u login --storage-key MYKEY== --storage-key-type StorageAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
     """
 helps['sql db import'] = """
     type: command
     short-summary: Imports a bacpac into an existing database.
-
     examples:
         - name: Get SAS key for use in import operation
-        text: az storage blob generate-sas --account-name myAccountName -c myContainer -n myBacpac.bacpac --permissions r --expiry 2018-01-01T00:00:00Z
-
+          text: az storage blob generate-sas --account-name myAccountName -c myContainer -n myBacpac.bacpac --permissions r --expiry 2018-01-01T00:00:00Z
         - name: Import bacpac into an existing database using SAS key
-        text: az sql db export -s myserver -n mydatabase -g mygroup -p password -u login --storage-key "?sr=b&sp=rw&se=2018-01-01T00%3A00%3A00Z&sig=sdfsdfklsdjflSLIFJLSIEJFLKSDJFDd/%2wdfskdjf3%3D&sv=2015-07-08" --storage-key-type SharedAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
-
+          text: az sql db import -s myserver -n mydatabase -g mygroup -p password -u login --storage-key "?sr=b&sp=rw&se=2018-01-01T00%3A00%3A00Z&sig=mysignature&sv=2015-07-08" --storage-key-type SharedAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
         - name: Import bacpac into an existing database using storage account key
-        text: az sql db export -s myserver -n mydatabase -g mygroup -p password -u login --storage-key "storageaccountkey" --storage-key-type StorageAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
+          text: az sql db import -s myserver -n mydatabase -g mygroup -p password -u login --storage-key MYKEY== --storage-key-type StorageAccessKey --storage-uri https://mystorageaccount.blob.core.windows.net/bacpacs/mybacpac.bacpac
     """
 helps['sql db restore'] = """
     type: command
     short-summary: Creates a new database by restoring from a database backup.
-    """
-helps['sql db import'] = """
-    type: command
-    short-summary: Imports a bacpac into an existing database.
     """
 helps['sql db threat-policy'] = """
     type: group
