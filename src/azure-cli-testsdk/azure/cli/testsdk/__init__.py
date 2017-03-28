38d17f9671bc48455d04973be3a205a5ed16d320
Troy Dai <troy.dai@outlook.com>
2017-03-28 13:59:50 -0700

    Update storage tests (#2637)
    * Rename blob upload tests
    
    * Add borderline cases for LBB

--- src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
@@ -7,9 +7,10 @@ from .base import ScenarioTest
 from .preparers import StorageAccountPreparer, ResourceGroupPreparer
 from .exceptions import CliTestError
 from .checkers import JMESPathCheck, JMESPathCheckExists, NoneCheck
+from .decorators import live_only
 
 __all__ = ['ScenarioTest',
            'ResourceGroupPreparer', 'StorageAccountPreparer',
            'CliTestError',
-           'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck']
+           'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck', 'live_only']
 __version__ = '0.1.0+dev'
--- src/command_modules/azure-cli-storage/tests/test_storage_blob_scenarios.py
+++ src/command_modules/azure-cli-storage/tests/test_storage_blob_scenarios.py
@@ -4,27 +4,56 @@
 # --------------------------------------------------------------------------------------------
 
 import os
-import tempfile
-import shutil
-import datetime
+import re
+from datetime import datetime, timedelta
 from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer,
                                JMESPathCheck)
 
 
-class StorageBlobTests(ScenarioTest):
+class StorageBlobUploadTests(ScenarioTest):
     @ResourceGroupPreparer()
     @StorageAccountPreparer()
     def test_storage_blob_upload_small_file(self, resource_group, storage_account):
-        self.verify_blob_upload_and_download(resource_group, storage_account, 1, 'block')
+        self.verify_blob_upload_and_download(resource_group, storage_account, 1, 'block', 0)
 
     @ResourceGroupPreparer()
     @StorageAccountPreparer()
     def test_storage_blob_upload_midsize_file(self, resource_group, storage_account):
-        # generate a 4MB file. the download test is skip for now to avoid huge recording file. the test harness will
-        # be updated later to support recording file download.
-        self.verify_blob_upload_and_download(resource_group, storage_account, 4096, 'block')
+        self.verify_blob_upload_and_download(resource_group, storage_account, 4096, 'block', 0)
 
-    def verify_blob_upload_and_download(self, group, account, file_size_kb, blob_type, skip_download=False):
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_101mb_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 101 * 1024, 'block',
+                                             26, skip_download=True)
+
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_100mb_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 100 * 1024, 'block',
+                                             25, skip_download=True)
+
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_99mb_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 99 * 1024, 'block',
+                                             25, skip_download=True)
+
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_64mb_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 64 * 1024, 'block',
+                                             16, skip_download=True)
+
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_63mb_file(self, resource_group, storage_account):
+        # 64MB is the put request size limit
+        self.verify_blob_upload_and_download(resource_group, storage_account, 63 * 1024, 'block',
+                                             skip_download=True)
+
+    def verify_blob_upload_and_download(self, group, account, file_size_kb, blob_type,
+                                        block_count=0, skip_download=False):
         container = self.create_random_name(prefix='cont', length=24)
         local_dir = self.create_temp_dir()
         local_file = self.create_temp_file(file_size_kb)
@@ -48,40 +77,61 @@ class StorageBlobTests(ScenarioTest):
         self.cmd('storage blob show -n {} -c {}'.format(blob_name, container),
                  checks=JMESPathCheck('name', blob_name))  # TODO: more checks
 
-        expiry = (datetime.datetime.utcnow() + datetime.timedelta(hours=1)).strftime('%Y-%m-%dT%H:%MZ')
+        expiry = (datetime.utcnow() + timedelta(hours=1)).strftime('%Y-%m-%dT%H:%MZ')
         sas = self.cmd('storage blob generate-sas -n {} -c {} --expiry {} '
                        '--permissions r --https-only'.format(blob_name, container, expiry)).output
         assert dict(pair.split('=') for pair in sas.split('&'))  # TODO: more checks
 
-        self.cmd('storage blob update -n {} -c {} --content-type application/test-content'.format(blob_name, container))
-        self.cmd('storage blob show -n {} -c {}'.format(blob_name, container),
-                 checks=JMESPathCheck('properties.contentSettings.contentType', 'application/test-content'))
+        self.cmd('storage blob update -n {} -c {} --content-type application/test-content'
+                 .format(blob_name, container))
 
-        self.cmd('storage blob service-properties show', checks=JMESPathCheck('hourMetrics.enabled', True))
+        self.cmd('storage blob show -n {} -c {}'.format(blob_name, container), checks=[
+            JMESPathCheck('properties.contentSettings.contentType', 'application/test-content'),
+            JMESPathCheck('properties.contentLength', file_size_kb * 1024)])
+
+        self.cmd('storage blob service-properties show',
+                 checks=JMESPathCheck('hourMetrics.enabled', True))
 
         if not skip_download:
             downloaded = os.path.join(local_dir, 'test.file')
-            self.cmd('storage blob download -n {} -c {} --file {}'.format(blob_name, container, downloaded))
+            self.cmd('storage blob download -n {} -c {} --file {}'
+                     .format(blob_name, container, downloaded))
             self.assertTrue(os.path.isfile(downloaded), 'The file is not downloaded.')
-            self.assertEqual(file_size_kb * 1024, os.stat(downloaded).st_size, 'The download file size is not right.')
+            self.assertEqual(file_size_kb * 1024, os.stat(downloaded).st_size,
+                             'The download file size is not right.')
+
+        # Verify the requests in cassette to ensure the count of the block requests is expeected
+        # This portion of validation doesn't verify anything during playback because the recording
+        # is fixed.
+        def is_block_put_req(request):
+            if request.method != 'PUT':
+                return False
+
+            if not re.search('/cont[0-9]+/blob[0-9]+', request.path):
+                return False
+
+            comp_block = False
+            has_blockid = False
+            for key, value in request.query:
+                if key == 'comp' and value == 'block':
+                    comp_block = True
+                elif key == 'blockid':
+                    has_blockid = True
+
+            return comp_block and has_blockid
+
+        requests = self.cassette.requests
+        put_blocks = [request for request in requests if is_block_put_req(request)]
+        self.assertEqual(block_count, len(put_blocks),
+                         'The expected number of block put requests is {} but the actual '
+                         'number is {}.'.format(block_count, len(put_blocks)))
 
     def get_account_key(self, group, name):
         return self.cmd('storage account keys list -n {} -g {} --query "[0].value" -otsv'
                         .format(name, group)).output
 
-    def create_temp_file(self, size_kb):
-        _, path = tempfile.mkstemp()
-        self.addCleanup(lambda: os.remove(path))
 
-        with open(path, mode='r+b') as file:
-            chunk = bytearray([0] * 1024)
-            for i in range(size_kb):
-                file.write(chunk)
+if __name__ == '__main__':
+    import unittest
 
-        return path
-
-    def create_temp_dir(self):
-        temp_dir = tempfile.mkdtemp()
-        self.addCleanup(lambda: shutil.rmtree(temp_dir, ignore_errors=True))
-
-        return temp_dir
+    unittest.main()
