57581eca9dbf76de8ea33cbd08c86b5c4c90c54b
Troy Dai <troy.dai@outlook.com>
2017-03-28 14:57:28 -0700

    Update storage queue and table

---
+++ src/command_modules/azure-cli-storage/tests/test_storage_queue_scenarios.py
@@ -0,0 +1,15 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer,
+                               JMESPathCheck)
+
+
+class StorageQueueScenarioTests(ScenarioTest):
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer(sku='Standard_RAGRS')
+    def test_storage_queue_stats(self, storage_account):
+        self.cmd('storage queue stats --account-name {}'.format(storage_account), checks=
+                 JMESPathCheck('geoReplication.status', 'live'))
