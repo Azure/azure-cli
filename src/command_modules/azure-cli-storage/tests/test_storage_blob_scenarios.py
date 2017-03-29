4ac82d59e27f149485d46861b34752f9b753a4bc
Troy Dai <troy.dai@outlook.com>
2017-03-29 10:18:00 -0700

    Adopt large block blob
    1. Maximize the upload block size and single put size.
    2. Remove the mitigation for storage python SDK issue #190.
    3. Update blob storage upload/download tests
    4. Add live only tests for large blob download
    5. Add live test base class

--- src/command_modules/azure-cli-storage/tests/test_storage_blob_scenarios.py
+++ src/command_modules/azure-cli-storage/tests/test_storage_blob_scenarios.py
@@ -5,9 +5,10 @@
 
 import os
 import re
+import unittest
 from datetime import datetime, timedelta
-from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer,
-                               JMESPathCheck)
+from azure.cli.testsdk import (ScenarioTest, LiveTest, ResourceGroupPreparer,
+                               StorageAccountPreparer, JMESPathCheck, live_only, get_sha1_hash)
 
 
 class StorageBlobUploadTests(ScenarioTest):
@@ -25,25 +26,19 @@ class StorageBlobUploadTests(ScenarioTest):
     @StorageAccountPreparer()
     def test_storage_blob_upload_101mb_file(self, resource_group, storage_account):
         self.verify_blob_upload_and_download(resource_group, storage_account, 101 * 1024, 'block',
-                                             26, skip_download=True)
+                                             0, skip_download=True)
 
     @ResourceGroupPreparer()
     @StorageAccountPreparer()
     def test_storage_blob_upload_100mb_file(self, resource_group, storage_account):
         self.verify_blob_upload_and_download(resource_group, storage_account, 100 * 1024, 'block',
-                                             25, skip_download=True)
-
-    @ResourceGroupPreparer()
-    @StorageAccountPreparer()
-    def test_storage_blob_upload_99mb_file(self, resource_group, storage_account):
-        self.verify_blob_upload_and_download(resource_group, storage_account, 99 * 1024, 'block',
-                                             25, skip_download=True)
+                                             0, skip_download=True)
 
     @ResourceGroupPreparer()
     @StorageAccountPreparer()
     def test_storage_blob_upload_64mb_file(self, resource_group, storage_account):
         self.verify_blob_upload_and_download(resource_group, storage_account, 64 * 1024, 'block',
-                                             16, skip_download=True)
+                                             0, skip_download=True)
 
     @ResourceGroupPreparer()
     @StorageAccountPreparer()
@@ -100,7 +95,7 @@ class StorageBlobUploadTests(ScenarioTest):
             self.assertEqual(file_size_kb * 1024, os.stat(downloaded).st_size,
                              'The download file size is not right.')
 
-        # Verify the requests in cassette to ensure the count of the block requests is expeected
+        # Verify the requests in cassette to ensure the count of the block requests is expected
         # This portion of validation doesn't verify anything during playback because the recording
         # is fixed.
         def is_block_put_req(request):
@@ -131,7 +126,65 @@ class StorageBlobUploadTests(ScenarioTest):
                         .format(name, group)).output
 
 
-if __name__ == '__main__':
-    import unittest
+class StorageBlobUploadLiveTests(LiveTest):
+    @live_only()
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_256mb_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 256 * 1024, 'block')
 
+    @live_only()
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_1G_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 1024 * 1024, 'block')
+
+    @live_only()
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_2G_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 2 * 1024 * 1024,
+                                             'block')
+
+    @live_only()
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload_10G_file(self, resource_group, storage_account):
+        self.verify_blob_upload_and_download(resource_group, storage_account, 10 * 1024 * 1024,
+                                             'block')
+
+    def verify_blob_upload_and_download(self, group, account, file_size_kb, blob_type):
+        container = self.create_random_name(prefix='cont', length=24)
+        local_dir = self.create_temp_dir()
+        local_file = self.create_temp_file(file_size_kb, full_random=True)
+        blob_name = self.create_random_name(prefix='blob', length=24)
+        account_key = self.cmd('storage account keys list -n {} -g {} --query "[0].value" -otsv'
+                               .format(account, group)).output
+
+        self.set_env('AZURE_STORAGE_ACCOUNT', account)
+        self.set_env('AZURE_STORAGE_KEY', account_key)
+
+        self.cmd('storage container create -n {}'.format(container))
+
+        self.cmd('storage blob exists -n {} -c {}'.format(blob_name, container),
+                 checks=JMESPathCheck('exists', False))
+
+        self.cmd('storage blob upload -c {} -f {} -n {} --type {}'
+                 .format(container, local_file, blob_name, blob_type))
+
+        self.cmd('storage blob exists -n {} -c {}'.format(blob_name, container),
+                 checks=JMESPathCheck('exists', True))
+
+        self.cmd('storage blob show -n {} -c {}'.format(blob_name, container),
+                 checks=JMESPathCheck('properties.contentLength', file_size_kb * 1024))
+
+        downloaded = os.path.join(local_dir, 'test.file')
+        self.cmd('storage blob download -n {} -c {} --file {}'
+                 .format(blob_name, container, downloaded))
+        self.assertTrue(os.path.isfile(downloaded), 'The file is not downloaded.')
+        self.assertEqual(file_size_kb * 1024, os.stat(downloaded).st_size,
+                         'The download file size is not right.')
+
+
+if __name__ == '__main__':
     unittest.main()
