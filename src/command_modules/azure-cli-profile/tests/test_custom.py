ebd78cb226bd984d7c80dd901739762cfef9fa53
yugangw-msft <yugangw@microsoft.com>
2017-03-29 11:03:45 -0700

    add more test verifications

--- src/command_modules/azure-cli-profile/tests/test_custom.py
+++ src/command_modules/azure-cli-profile/tests/test_custom.py
@@ -12,15 +12,17 @@ from azure.cli.command_modules.profile.custom import list_subscriptions
 class ProfileCommandTest(unittest.TestCase):
 
     @mock.patch('azure.cli.command_modules.profile.custom.load_subscriptions', autospec=True)
-    def test_list_only_enabled_one(self, load_subscription_mock):
+    @mock.patch('azure.cli.command_modules.profile.custom.logger', autospec=True)
+    def test_list_only_enabled_one(self, logger_mock, load_subscription_mock):
         sub1 = {'state': 'Enabled'}
         sub2 = {'state': 'Overdued'}
         load_subscription_mock.return_value = [sub1, sub2]
 
         # list all
         self.assertEqual(2, len(list_subscriptions(all=True)))
-
+        self.assertTrue(not logger_mock.warning.called)
         # list only enabled one
         result = list_subscriptions()
         self.assertEqual(1, len(result))
         self.assertEqual('Enabled', result[0]['state'])
+        logger_mock.warning.assert_called_once_with(mock.ANY)
