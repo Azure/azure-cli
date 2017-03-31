596af438a72fc25041c2c15e75e9971b6b2aa7df
Derek Bekoe <debekoe@microsoft.com>
2017-03-30 20:05:35 -0700

    fix path typo

--- packaged_releases/windows/build.cmd
+++ packaged_releases/windows/build.cmd
@@ -3,8 +3,7 @@ echo Building the Windows Installer...
 echo.
 :: Add Git to the path as this should be run through a .NET command prompt
 :: and not a Git bash shell... We also need the gnu toolchain (for curl & unzip)
-:: Also, add path for msbuild
-set PATH=%PATH%;"C:\Program Files (x86)\Git\bin;C:\Program Files (x86)\MSBuild\14.0\Bin""
+set PATH=%PATH%;"C:\Program Files (x86)\Git\bin;C:\Program Files (x86)\MSBuild\14.0\Bin"
 
 pushd %~dp0
 
