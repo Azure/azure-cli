36602356505e541cec8fd36844464f6ba05557c2
Derek Bekoe <debekoe@microsoft.com>
2017-03-31 10:37:17 -0700

    Fix msi path to include msbuild in path. (#2696)

--- packaged_releases/windows/build.cmd
+++ packaged_releases/windows/build.cmd
@@ -3,7 +3,7 @@ echo Building the Windows Installer...
 echo.
 :: Add Git to the path as this should be run through a .NET command prompt
 :: and not a Git bash shell... We also need the gnu toolchain (for curl & unzip)
-set PATH=%PATH%;"C:\Program Files (x86)\Git\bin;C:\Program Files (x86)\MSBuild\14.0\Bin"
+set "PATH=%PATH%;C:\Program Files (x86)\Git\bin;C:\Program Files (x86)\MSBuild\14.0\Bin;"
 
 pushd %~dp0
 
