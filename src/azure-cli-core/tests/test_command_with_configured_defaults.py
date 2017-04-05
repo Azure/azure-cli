655c9bda72a2ef6f469f44d771db4a99b8e94807
yugangw-msft <yugangw@microsoft.com>
2017-04-04 17:45:32 -0700

    add tests

--- src/azure-cli-core/tests/test_command_with_configured_defaults.py
+++ src/azure-cli-core/tests/test_command_with_configured_defaults.py
@@ -2,22 +2,30 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-import sys
+import os
 import logging
 import unittest
+import mock
 
 from azure.cli.core.commands import _update_command_definitions
-from azure.cli.core.commands import (
-    command_table,
-    CliArgumentType,
-    CliCommandArgument,
-    cli_command,
-    register_cli_argument,
-    register_extra_cli_argument)
+
+from azure.cli.core.commands import (command_table, CliArgumentType, cli_command,
+                                     register_cli_argument)
+from azure.cli.core.application import Application, Configuration
+from azure.cli.core._config import AzConfig
 
 
-class Test_command_registration(unittest.TestCase):
+# a dummy callback for arg-parse
+def load_params(_):
+    pass
+
+
+class TestCommandWithConfiguredDefaults(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        self.argv = None
+        self.application = None
+        super(TestCommandWithConfiguredDefaults, self).__init__(*args, **kwargs)
 
     @classmethod
     def setUpClass(cls):
@@ -30,37 +38,47 @@ class Test_command_registration(unittest.TestCase):
         logging.shutdown()
 
     @staticmethod
-    def sample_vm_get(resource_group_name, vm_name, opt_param=None, expand=None, custom_headers={},  # pylint: disable=dangerous-default-value, too-many-arguments
-                      raw=False, **operation_config):
-        pass
+    def sample_vm_list(resource_group_name):
+        return resource_group_name
 
-    def test_register_cli_argument_with_configured_defaults(self):
+    def set_up_command_table(self, required_arg=False):
         command_table.clear()
-        cli_command(None, 'test register sample-vm-get',
-                    '{}#Test_command_registration.sample_vm_get'.format(__name__))
-        register_cli_argument('test register sample-vm-get', 'vm_name', CliArgumentType(
-            options_list=('--wonky-name', '-n'), metavar='VMNAME', configured_default='vm',
-            required=False
-        ))
 
-        command_table['test register sample-vm-get'].load_arguments()
+        module_name = __name__ + '.' + self._testMethodName
+        cli_command(module_name, 'test sample-vm-list',
+                    '{}#TestCommandWithConfiguredDefaults.sample_vm_list'.format(__name__))
+
+        register_cli_argument('test sample-vm-list', 'resource_group_name',
+                              CliArgumentType(options_list=('--resource-group-name', '-g'),
+                                              configured_default='group', required=required_arg))
+
+        command_table['test sample-vm-list'].load_arguments()
         _update_command_definitions(command_table)
 
-        self.assertEqual(len(command_table), 1,
-                         'We expect exactly one command in the command table')
-        command_metadata = command_table['test register sample-vm-get']
-        self.assertEqual(len(command_metadata.arguments), 4, 'We expected exactly 4 arguments')
-        some_expected_arguments = {
-            'resource_group_name': CliArgumentType(dest='resource_group_name', required=True),
-            'vm_name': CliArgumentType(dest='vm_name', required=False),
-        }
+        self.argv = 'az test sample-vm-list'.split()
+        config = Configuration(self.argv)
+        config.get_command_table = lambda: command_table
+        self.application = Application(config)
 
-        for probe in some_expected_arguments:
-            existing = next(arg for arg in command_metadata.arguments if arg == probe)
-            self.assertDictContainsSubset(some_expected_arguments[existing].settings,
-                                          command_metadata.arguments[existing].options)
-        self.assertEqual(command_metadata.arguments['vm_name'].options_list, ('--wonky-name', '-n'))
+    @mock.patch.dict(os.environ, {AzConfig.env_var_name('defaults', 'group'): 'myRG'})
+    def test_apply_configured_defaults_on_required_arg(self):
+        self.set_up_command_table(required_arg=True)
 
+        # action
+        res = self.application.execute(self.argv[1:])
+
+        # assert
+        self.assertEqual(res.result, 'myRG')
+
+    @mock.patch.dict(os.environ, {AzConfig.env_var_name('defaults', 'group'): 'myRG'})
+    def test_apply_configured_defaults_on_optional_arg(self):
+        self.set_up_command_table(required_arg=False)
+
+        # action
+        res = self.application.execute(self.argv[1:])
+
+        # assert
+        self.assertEqual(res.result, 'myRG')
 
 
 if __name__ == '__main__':
