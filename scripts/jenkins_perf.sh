21d23695f26a9b89f522a1839402a342ec54ffb6
Troy Dai <troy.dai@outlook.com>
2017-04-12 15:32:34 -0700

    Initiate Jenkins pipeline (#2840)
    * Initiate Jenkins pipeline
    
    1. Add build task
    2. Add basic performance test task
    3. Add performance measure script

---
+++ scripts/jenkins_perf.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Run performance in jenkins build
+
+. ./env/bin/activate
+
+echo "Run performance test on $(hostname)"
+
+if [ -z %BUILD_NUMBER ]; then
+    echo "Environment variable BUILD_NUMBER is missing."
+    exit 1
+fi
+
+version=$(printf '%.8d' $BUILD_NUMBER)
+echo "Version number: $version"
+
+echo 'Before install'
+which az
+pip list
+
+build_folder=/var/build_share/$BRANCH_NAME/$version
+echo "Install build from $build_folder"
+
+python -m pip install azure-cli --find-links file://$build_folder/artifacts/build -v
+python -m pip freeze
+
+python ./scripts/performance/measure.py
