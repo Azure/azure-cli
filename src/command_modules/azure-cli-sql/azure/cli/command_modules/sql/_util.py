d66a27ce6c50b1d222f5e8e0313d8fd104f3f8a2
Jared Moore <jaredmoo@microsoft.com>
2017-04-13 17:13:43 -0700

    fixed style errors

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/_util.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/_util.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
 def get_sql_management_client(_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.mgmt.sql import SqlManagementClient
