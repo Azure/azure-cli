547f621b98d6211c981ed60628245795789f54bc
Jared Moore <jaredmoo@microsoft.com>
2017-04-13 19:01:46 -0700

    SQL - ability to connect directly to resource provider (#2832)
    * SQL RM / subid override with no authentication. For onebox.
    
    * AAD bypass logic for internal SQL testing
    
    * fixed style errors

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/_util.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/_util.py
@@ -5,9 +5,23 @@
 
 
 def get_sql_management_client(_):
-    from azure.mgmt.sql import SqlManagementClient
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    return get_mgmt_service_client(SqlManagementClient)
+    from azure.mgmt.sql import SqlManagementClient
+    from msrest.authentication import Authentication
+    from os import getenv
+
+    # Allow overriding SQL resource manager URI using environment variable
+    # for testing purposes. Subscription id is also determined by environment
+    # variable.
+    sql_rm_override = getenv('_AZURE_CLI_SQL_RM_URI')
+    if sql_rm_override:
+        return SqlManagementClient(
+            subscription_id=getenv('_AZURE_CLI_SQL_SUB_ID'),
+            base_url=sql_rm_override,
+            credentials=Authentication())  # No authentication
+    else:
+        # Normal production scenario.
+        return get_mgmt_service_client(SqlManagementClient)
 
 
 def get_sql_servers_operations(kwargs):
