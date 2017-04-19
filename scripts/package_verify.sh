f9ea9edfc70a06454c1d39f9312e5fd0aa732794
Derek Bekoe <debekoe@microsoft.com>
2017-04-19 16:21:28 -0700

    Support allowable pip dep. errors

--- scripts/package_verify.sh
+++ scripts/package_verify.sh
@@ -5,7 +5,7 @@ virtualenv package-verify-env
 . package-verify-env/bin/activate
 pip install -e scripts
 python -m automation.tests.verify_packages
-pip check
+python -m automation.tests.verify_dependencies
 deactivate
 # If we get here, all prev. commands returned 0 exit codes so we are done.
 rm -rf package-verify-env
