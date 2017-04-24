fe295b7e43114ee21136d190b75cc52a11ceba0a
Troy Dai <troy.dai@outlook.com>
2017-04-24 11:32:06 -0700

    Update storage tests
    1. Move tests from test_storage.py to other test files;
    2. Adopt ScenarioTests
    3. Retire old storage test base

--- src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
@@ -7,13 +7,12 @@ from .base import ScenarioTest, LiveTest
 from .preparers import (StorageAccountPreparer, ResourceGroupPreparer,
                         RoleBasedServicePrincipalPreparer)
 from .exceptions import CliTestError
-from .checkers import JMESPathCheck, JMESPathCheckExists, NoneCheck
+from .checkers import JMESPathCheck, JMESPathCheckExists, NoneCheck, StringCheck, StringContainCheck
 from .decorators import live_only
 from .utilities import get_sha1_hash
 
-__all__ = ['ScenarioTest', 'LiveTest',
-           'ResourceGroupPreparer', 'StorageAccountPreparer', 'RoleBasedServicePrincipalPreparer',
-           'CliTestError',
-           'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck', 'live_only',
+__all__ = ['ScenarioTest', 'LiveTest', 'ResourceGroupPreparer', 'StorageAccountPreparer',
+           'RoleBasedServicePrincipalPreparer', 'CliTestError', 'JMESPathCheck',
+           'JMESPathCheckExists', 'NoneCheck', 'live_only', 'StringCheck', 'StringContainCheck',
            'get_sha1_hash']
 __version__ = '0.1.0+dev'
