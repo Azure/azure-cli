1a77bb57266784a67db19bd8de2e2376448e6136
kostenray <xirui2006@gmail.com>
2017-04-24 12:36:24 -0700

    add the CLI for 2017-04-18 swagger file

--- src/command_modules/azure-cli-cognitiveservices/azure/cli/command_modules/cognitiveservices/commands.py
+++ src/command_modules/azure-cli-cognitiveservices/azure/cli/command_modules/cognitiveservices/commands.py
@@ -11,6 +11,7 @@ from azure.cli.command_modules.cognitiveservices._client_factory import cognitiv
 
 mgmt_path = 'azure.mgmt.cognitiveservices.operations.cognitive_services_accounts_operations#CognitiveServicesAccountsOperations.'
 custom_path = 'azure.cli.command_modules.cognitiveservices.custom#'
+skuavailabilitypath = 'azure.mgmt.cognitiveservices.operations.check_sku_availability_operations#CheckSkuAvailabilityOperations.'
 
 cli_command(__name__, 'cognitiveservices account create', custom_path + 'create', cognitiveservices_client_factory)
 cli_command(__name__, 'cognitiveservices account delete', mgmt_path + 'delete', cognitiveservices_client_factory)
@@ -20,6 +21,6 @@ cli_command(__name__, 'cognitiveservices account update', mgmt_path + 'update', 
 cli_command(__name__, 'cognitiveservices account keys regenerate', mgmt_path + 'regenerate_key', cognitiveservices_client_factory)
 cli_command(__name__, 'cognitiveservices account keys list', mgmt_path + 'list_keys', cognitiveservices_client_factory)
 cli_command(__name__, 'cognitiveservices account list-skus', mgmt_path + 'list_skus', cognitiveservices_client_factory)
-cli_command(__name__, 'cognitiveservices list-skus', mgmt_path + 'list_skus', cognitiveservices_client_factory)
+cli_command(__name__, 'cognitiveservices checkskuavailability', skuavailabilitypath + 'list', cognitiveservices_client_factory)
 
 
