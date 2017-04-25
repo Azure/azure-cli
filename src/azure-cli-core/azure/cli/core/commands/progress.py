2a9e66909c8774ee84bca80f3de6b47c2d9fb871
Courtney Oka <oka.c@husky.neu.edu>
2017-04-25 10:17:00 -0700

    repackage

--- src/azure-cli-core/azure/cli/core/commands/progress.py
+++ src/azure-cli-core/azure/cli/core/commands/progress.py
@@ -5,6 +5,46 @@
 import sys
 
 
+class StandardOut(object):
+    """ custom output for progress reporting """
+    def __init__(self, out=None):
+        self.out = out or sys.stdout
+        self.total_val = None
+
+    def start(self, message, total_value):
+        """ start reporting progress """
+        self.out.write(message)
+        self.total_val = total_value
+
+    def write(self, message, value, total_value):
+        """ writes the progress """
+        if value and total_value:
+            self.out.write(self._format_value(value, total_value) + "\n")
+        self.out.write(message + '\n')
+
+    def _format_value(self, val, total_val):
+        percent = val / total_val
+        bar_len = 100
+        completed = int(bar_len*percent)
+
+        message = '\r['
+        for i in range(bar_len):
+            if i < completed:
+                message += '#'
+            else:
+                message += ' '
+        message += ']  {:.4%}'.format(percent)
+        return message
+
+    def flush(self):
+        """ flushes the message"""
+        self.out.flush()
+
+    def end(self, message=''):
+        """ stop reporting progress """
+        self.out.write('Finished{}'.format(message))
+
+
 class ProgressReporter(object):
     """ generic progress reporter """
     def __init__(self, transform_progress=None, out=sys.stdout):
