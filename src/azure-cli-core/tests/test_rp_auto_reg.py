6e507956691fee9be479a66dabb89fb54be1b6aa
yugangw-msft <yugangw@microsoft.com>
2017-04-27 11:02:29 -0700

    update tests

--- src/azure-cli-core/tests/test_rp_auto_reg.py
+++ src/azure-cli-core/tests/test_rp_auto_reg.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import unittest
 import json
 import time
 import mock
@@ -11,6 +12,9 @@ from azure.cli.testsdk import (
     JMESPathCheck,
     ResourceGroupPreparer,
     ScenarioTest)
+
+from azure.cli.core.commands import _check_rp_not_registered_err
+
 
 # pylint: disable=line-too-long
 
@@ -33,3 +37,27 @@ class TestRPAutoRegister(ScenarioTest):
         self.cmd('provider show -n Microsoft.Sql', checks=[
             JMESPathCheck('registrationState', 'Registered')
         ])
+
+
+class TestRPErrorPolish(unittest.TestCase):
+
+    def test_rp_error_polish(self):
+        ex = mock.MagicMock()
+
+        # bad error
+        ex.response.content = b'bad error'
+        result = _check_rp_not_registered_err(ex)
+        self.assertFalse(bool(result))
+
+        # good error (but not we are interested)
+        ex.response.content = b'{"error":{"code":"c1","message":"m1"}}'
+        result = _check_rp_not_registered_err(ex)
+        self.assertFalse(bool(result))
+
+        # the error we want to polish
+        ex.response.content = (b'{"error":{"code":"MissingSubscriptionRegistration","message":'
+                               b'"The subscription is not registered to use namespace '
+                               b'\'Microsoft.Sql\'. See https://aka.ms/rps-not-found for how '
+                               b'to register subscriptions."}}')
+        result = _check_rp_not_registered_err(ex)
+        self.assertEqual(str(result), 'Microsoft.Sql')
