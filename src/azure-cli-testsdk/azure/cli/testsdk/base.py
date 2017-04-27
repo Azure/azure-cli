3ff3da4009b39b04f4e015b6230bc0b43366e43c
Troy Dai <troy.dai@outlook.com>
2017-04-26 17:23:45 -0700

    Patch away the time.sleep

--- src/azure-cli-testsdk/azure/cli/testsdk/base.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/base.py
@@ -17,7 +17,8 @@ import six
 import vcr
 
 from .patches import (patch_load_cached_subscriptions, patch_main_exception_handler,
-                      patch_retrieve_token_for_user, patch_long_run_operation_delay)
+                      patch_retrieve_token_for_user, patch_long_run_operation_delay,
+                      patch_time_sleep_api)
 from .exceptions import CliExecutionError
 from .const import (ENV_LIVE_TEST, ENV_SKIP_ASSERT, ENV_TEST_DIAGNOSE, MOCKED_SUBSCRIPTION_ID)
 from .recording_processors import (SubscriptionRecordingProcessor, OAuthRequestResponsesFilter,
@@ -151,6 +152,7 @@ class ScenarioTest(IntegrationTestBase):  # pylint: disable=too-many-instance-at
         patch_main_exception_handler(self)
 
         if not self.in_recording:
+            patch_time_sleep_api(self)
             patch_long_run_operation_delay(self)
             patch_load_cached_subscriptions(self)
             patch_retrieve_token_for_user(self)
