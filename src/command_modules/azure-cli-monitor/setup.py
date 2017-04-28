1281f59268bde6a92b1558eed9439d62ef694cc4
Derek Bekoe <debekoe@microsoft.com>
2017-04-28 14:18:30 -0700

    Release azure-cli-monitor 0.0.3 (#3068)

--- src/command_modules/azure-cli-monitor/setup.py
+++ src/command_modules/azure-cli-monitor/setup.py
@@ -12,7 +12,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = '0.0.2+dev'
+VERSION = '0.0.3+dev'
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
