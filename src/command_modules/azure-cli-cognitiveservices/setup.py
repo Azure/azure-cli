410e4909db77af81fa5b3ae518a3184a176a0675
kostenray <xirui2006@gmail.com>
2017-05-02 13:48:20 -0700

    update the setup.py

--- src/command_modules/azure-cli-cognitiveservices/setup.py
+++ src/command_modules/azure-cli-cognitiveservices/setup.py
@@ -13,6 +13,8 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
+VERSION = '0.1.0+dev'
+
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
@@ -35,10 +37,12 @@ DEPENDENCIES = [
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     README = f.read()
+with open('HISTORY.rst', 'r', encoding='utf-8') as f:
+    HISTORY = f.read()
 
 setup(
     name='azure-cli-cognitiveservices',
-    version='0.1.0',
+    version=VERSION,
     description='Microsoft Azure Command-Line Tools Cognitive Services Command Module',
     long_description=README,
     license='MIT',
