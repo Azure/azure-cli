097fb91668a11a85eb26e64912faea7f9bf9774c
David Justice <devigned@users.noreply.github.com>
2017-05-05 11:15:22 -0700

    Add support for Azure CDN (#3147)
    * initial cdn structure
    
    * endpoint create looks good
    
    * endpoint update is working with generic support
    
    * add in some help and parameter handling
    
    * add tests for most scenarios
    
    * compliance achieved \( ﾟヮﾟ)/
    
    * add release history notes
    
    * remove extra param registration
    
    * doc comment for 3 state flag
    
    * add tests for origin parsing
    
    * add headers to the test files
    
    * move doc strings into help.py
    
    * less trivial help
    
    * fix some formatting in _params
    
    * fix test difference between py 2 and 3 & help
    
    * remove unused var ex
    
    * add group help text
    
    * tighten up short-summaries
    
    * clean up whitespace
    
    * translate commands to common format

--- src/command_modules/azure-cli-cdn/setup.py
+++ src/command_modules/azure-cli-cdn/setup.py
@@ -5,7 +5,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from __future__ import print_function
+
 from codecs import open
 from setuptools import setup
 try:
@@ -15,28 +15,13 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.4+dev"
 
-# If we have source, validate that our version numbers match
-# This should prevent uploading releases with mismatched versions.
-try:
-    with open('azure/cli/__init__.py', 'r', encoding='utf-8') as f:
-        content = f.read()
-except OSError:
-    pass
-else:
-    import re
-    import sys
-    m = re.search(r'__version__\s*=\s*[\'"](.+?)[\'"]', content)
-    if not m:
-        print('Could not find __version__ in azure/cli/__init__.py')
-        sys.exit(1)
-    if m.group(1) != VERSION:
-        print('Expected __version__ = "{}"; found "{}"'.format(VERSION, m.group(1)))
-        sys.exit(1)
+VERSION = '0.0.1+dev'
 
+# The full list of classifiers is available at
+# https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
@@ -50,35 +35,8 @@ CLASSIFIERS = [
 ]
 
 DEPENDENCIES = [
-    'azure-cli-acr',
-    'azure-cli-acs',
-    'azure-cli-appservice',
-    'azure-cli-batch',
-    'azure-cli-cdn',
-    'azure-cli-cloud',
-    'azure-cli-component',
-    'azure-cli-configure',
-    'azure-cli-container',
+    'azure-mgmt-cdn==0.30.2',
     'azure-cli-core',
-    'azure-cli-dla',
-    'azure-cli-dls',
-    'azure-cli-documentdb',
-    'azure-cli-feedback',
-    'azure-cli-find',
-    'azure-cli-iot',
-    'azure-cli-keyvault',
-    'azure-cli-lab',
-    'azure-cli-monitor',
-    'azure-cli-network',
-    'azure-cli-nspkg',
-    'azure-cli-profile',
-    'azure-cli-redis',
-    'azure-cli-resource',
-    'azure-cli-role',
-    'azure-cli-sf',
-    'azure-cli-sql',
-    'azure-cli-storage',
-    'azure-cli-vm'
 ]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
@@ -87,25 +45,21 @@ with open('HISTORY.rst', 'r', encoding='utf-8') as f:
     HISTORY = f.read()
 
 setup(
-    name='azure-cli',
+    name='azure-cli-cdn',
     version=VERSION,
-    description='Microsoft Azure Command-Line Tools',
+    description='Microsoft Azure Command-Line Tools Content Delivery Network (CDN) Command Module',
     long_description=README + '\n\n' + HISTORY,
     license='MIT',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
     url='https://github.com/Azure/azure-cli',
-    zip_safe=False,
     classifiers=CLASSIFIERS,
-    scripts=[
-        'az',
-        'az.completion.sh',
-        'az.bat',
-    ],
     packages=[
         'azure',
         'azure.cli',
+        'azure.cli.command_modules',
+        'azure.cli.command_modules.cdn',
     ],
     install_requires=DEPENDENCIES,
-    cmdclass=cmdclass
+    cmdclass=cmdclass,
 )
