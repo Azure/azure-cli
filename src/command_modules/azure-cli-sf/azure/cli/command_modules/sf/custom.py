d03cb2cff2ac1596c6cb87b8651ee90f290cf77c
samedder <edsamw@gmail.com>
2017-05-07 16:13:40 -0700

    Fixing report service health

--- src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/custom.py
+++ src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/custom.py
@@ -1153,7 +1153,8 @@ def sf_report_app_health(application_id,  # pylint: disable=too-many-arguments
     sf_client.report_application_health(application_id, info, timeout)
 
 
-def sf_report_svc_health(service_id,  # pylint: disable=too-many-arguments
+def sf_report_svc_health(client,  # pylint: disable=too-many-arguments
+                         service_id,
                          source_id, health_property, health_state,
                          ttl=None, description=None, sequence_number=None,
                          remove_when_expired=None, timeout=60):
@@ -1162,21 +1163,20 @@ def sf_report_svc_health(service_id,  # pylint: disable=too-many-arguments
 
     Reports health state of the specified Service Fabric service. The
     report must contain the information about the source of the health
-    report and property on which it is reported.
-    The report is sent to a Service Fabric gateway Service, which forwards
-    to the health store.
+    report and property on which it is reported. The report is sent to a
+    Service Fabric gateway Service, which forwards to the health store.
     The report may be accepted by the gateway, but rejected by the health
-    store after extra validation.
-    For example, the health store may reject the report because of an
-    invalid parameter, like a stale sequence number.
-    To see whether the report was applied in the health store, run
-    GetServiceHealth and check that the report appears in the
-    HealthEvents section.
+    store after extra validation. For example, the health store may reject
+    the report because of an invalid parameter, like a stale sequence number.
+    To see whether the report was applied in the health store, check that the
+    report appears in the health events of the service.
 
     :param str service_id: The identity of the service. This is typically the
     full name of the service without the 'fabric:' URI scheme.
+
     :param str source_id: The source name which identifies the
     client/watchdog/system component which generated the health information.
+
     :param str health_property: The property of the health information. An
     entity can have health reports for different properties. The property is a
     string and not a fixed enumeration to allow the reporter flexibility to
@@ -1188,12 +1188,15 @@ def sf_report_svc_health(service_id,  # pylint: disable=too-many-arguments
     these reports are treated as separate health events for the specified node.
     Together with the SourceId, the property uniquely identifies the health
     information.
+
     :param str health_state: Possible values include: 'Invalid', 'Ok',
     'Warning', 'Error', 'Unknown'
-    :param int ttl: The duration, in milliseconds, for which this health report
+
+    :param str ttl: The duration, in milliseconds, for which this health report
     is valid. When clients report periodically, they should send reports with
     higher frequency than time to live. If not specified, time to live defaults
     to infinite value.
+
     :param str description: The description of the health information. It
     represents free text used to add human readable information about the
     report. The maximum string length for the description is 4096 characters.
@@ -1203,10 +1206,12 @@ def sf_report_svc_health(service_id,  # pylint: disable=too-many-arguments
     the marker indicates to users that truncation occurred. Note that when
     truncated, the description has less than 4096 characters from the original
     string.
+
     :param str sequence_number: The sequence number for this health report as a
     numeric string. The report sequence number is used by the health store to
     detect stale reports. If not specified, a sequence number is auto-generated
     by the health client when a report is added.
+
     :param bool remove_when_expired: Value that indicates whether the report is
     removed from health store when it expires. If set to true, the report is
     removed from the health store after it expires. If set to false, the report
@@ -1215,22 +1220,16 @@ def sf_report_svc_health(service_id,  # pylint: disable=too-many-arguments
     false (default). This way, is the reporter has issues (eg. deadlock) and
     can't report, the entity is evaluated at error when the health report
     expires. This flags the entity as being in Error health state.
-    :param long timeout: The server timeout for performing the operation in
-    seconds. This specifies the time duration that the client is willing to
-    wait for the requested operation to complete. The default value
-    for this parameter is 60 seconds.
     """
 
     # TODO Move common HealthInformation params to _params
 
     from azure.servicefabric.models.health_information import HealthInformation
-    from azure.cli.command_modules.sf._factory import cf_sf_client
 
     info = HealthInformation(source_id, health_property, health_state, ttl,
                              description, sequence_number, remove_when_expired)
 
-    sf_client = cf_sf_client(None)
-    sf_client.report_service_health(service_id, info, timeout)
+    client.report_service_health(service_id, info, timeout)
 
 
 def sf_report_partition_health(  # pylint: disable=too-many-arguments
