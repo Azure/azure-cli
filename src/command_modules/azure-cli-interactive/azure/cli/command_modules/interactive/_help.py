58dada05febdbd731d9a163bf6178b5167e9621b
Derek Bekoe <debekoe@microsoft.com>
2017-05-08 14:57:56 -0700

    Add link to blog in ‘az interactive —help’

--- src/command_modules/azure-cli-interactive/azure/cli/command_modules/interactive/_help.py
+++ src/command_modules/azure-cli-interactive/azure/cli/command_modules/interactive/_help.py
@@ -7,5 +7,8 @@ from azure.cli.core.help_files import helps
 
 helps['interactive'] = """
             type: command
-            short-summary: Start the interactive shell for Microsoft Azure CLI (preview)
+            short-summary: Start the interactive experience.
+            long-summary: >
+                An interactive environment that makes it easier to learn and use Azure CLI commands.
+                see https://azure.microsoft.com/en-us/blog/welcome-to-azure-cli-shell/
             """
