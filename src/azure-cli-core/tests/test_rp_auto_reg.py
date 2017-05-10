524bfa156e8ef9528959f4d997117617d2251f97
Troy Dai <troy.dai@outlook.com>
2017-05-10 15:56:35 -0700

    Exclude provider registering test from live test (#3316)
    Signed-off-by: Troy Dai <troy.dai@outlook.com>

--- src/azure-cli-core/tests/test_rp_auto_reg.py
+++ src/azure-cli-core/tests/test_rp_auto_reg.py
@@ -8,39 +8,30 @@ import json
 import time
 import mock
 
-from azure.cli.testsdk import (
-    JMESPathCheck,
-    ResourceGroupPreparer,
-    ScenarioTest)
-
+from azure.cli.testsdk import (JMESPathCheck, ResourceGroupPreparer, ScenarioTest, record_only)
 from azure.cli.core.commands import _check_rp_not_registered_err
 
 
+@record_only()
 class TestRPAutoRegister(ScenarioTest):
     def setUp(self):
         if self.in_recording:
             # note, unregistering a registered provider might take a long while(10+ mins)
-            result = self.cmd('provider show -n Microsoft.Sql '
-                              '--query "registrationState" -o tsv').output
+            result = self.cmd('provider show -n Microsoft.Sql --query "registrationState" -o tsv').output
             if result.lower().strip() != 'unregistered':
                 self.cmd('provider unregister -n Microsoft.Sql --wait')
                 time.sleep(30)  # a bit random but like to ensure unregistering went through
+
         return super(TestRPAutoRegister, self).setUp()
 
     @ResourceGroupPreparer()
     def test_rp_auto_register(self, resource_group, resource_group_location):
-        cmd = ('sql server create -g {} -n ygserver123 -l {} --admin-user sa123 '
-               '--admin-password verySecret12345')
-        self.cmd(cmd.format(resource_group, resource_group_location), checks=[
-            JMESPathCheck('name', 'ygserver123')
-        ])
-        self.cmd('provider show -n Microsoft.Sql', checks=[
-            JMESPathCheck('registrationState', 'Registered')
-        ])
+        cmd = ('sql server create -g {} -n ygserver123 -l {} --admin-user sa123 --admin-password verySecret12345')
+        self.cmd(cmd.format(resource_group, resource_group_location), checks=JMESPathCheck('name', 'ygserver123'))
+        self.cmd('provider show -n Microsoft.Sql', checks=JMESPathCheck('registrationState', 'Registered'))
 
 
 class TestRPErrorPolish(unittest.TestCase):
-
     def test_rp_error_polish(self):
         ex = mock.MagicMock()
 
--- src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
@@ -8,11 +8,10 @@ from .preparers import (StorageAccountPreparer, ResourceGroupPreparer,
                         RoleBasedServicePrincipalPreparer)
 from .exceptions import CliTestError
 from .checkers import JMESPathCheck, JMESPathCheckExists, NoneCheck, StringCheck, StringContainCheck
-from .decorators import live_only
+from .decorators import live_only, record_only
 from .utilities import get_sha1_hash
 
 __all__ = ['ScenarioTest', 'LiveTest', 'ResourceGroupPreparer', 'StorageAccountPreparer',
-           'RoleBasedServicePrincipalPreparer', 'CliTestError', 'JMESPathCheck',
-           'JMESPathCheckExists', 'NoneCheck', 'live_only', 'StringCheck', 'StringContainCheck',
-           'get_sha1_hash']
+           'RoleBasedServicePrincipalPreparer', 'CliTestError', 'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck',
+           'live_only', 'record_only', 'StringCheck', 'StringContainCheck', 'get_sha1_hash']
 __version__ = '0.1.0+dev'
