74fa351acdbe04366a089e9bbccd2b13efbe7980
Travis Prescott <trpresco@microsoft.com>
2017-05-10 12:54:55 -0700

    Update version

--- src/command_modules/azure-cli-role/setup.py
+++ src/command_modules/azure-cli-role/setup.py
@@ -34,7 +34,7 @@ DEPENDENCIES = [
     'azure-cli-core',
     'azure-mgmt-authorization==0.30.0rc6',
     'azure-graphrbac==0.30.0rc6',
-    'azure-keyvault==0.2.0',
+    'azure-keyvault==0.3.2',
     'pytz'
 ]
 
