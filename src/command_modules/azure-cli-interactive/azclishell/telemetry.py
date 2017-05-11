4e3523d2d60ae78fbdcf7432ddb820ba801a2d50
Courtney Oka <oka.c@husky.neu.edu>
2017-05-11 10:18:07 -0700

    minor

--- src/command_modules/azure-cli-interactive/azclishell/telemetry.py
+++ src/command_modules/azure-cli-interactive/azclishell/telemetry.py
@@ -31,23 +31,16 @@ class Telemetry(TelemetryClient):
 
     start_time = None
     end_time = None
-    print('init')
 
     @_user_agrees_to_telemetry
     def track_ssg(self, gesture, cmd):
         """ track shell specific gestures """
         self.track_event('Shell Specific Gesture', {gesture: cmd})
-        TELTHREAD.setDaemon = True
-        TELTHREAD.run()
 
     @_user_agrees_to_telemetry
     def track_key(self, key):
         """ tracks the special key bindings """
         self.track_event('Key Press', {"key": key})
-        TELTHREAD.daemon = True
-        print(TELTHREAD.is_alive)
-        TELTHREAD.run()
-        print(TELTHREAD.is_alive)
 
     @_user_agrees_to_telemetry
     def start(self):
@@ -60,8 +53,8 @@ class Telemetry(TelemetryClient):
         self.end_time = str(datetime.datetime.now())
         self.track_event('Run', {'start time': self.start_time,
                                  'end time': self.end_time})
-        TELTHREAD.daemon = False
-        TELTHREAD.run()
+        TELTHREAD = TelThread(self.flush)
+        TELTHREAD.start()
 
 
 class TelThread(threading.Thread):
@@ -80,7 +73,3 @@ class TelThread(threading.Thread):
 TC = Telemetry(INSTRUMENTATION_KEY)
 enable(INSTRUMENTATION_KEY)
 my_context(TC)
-print('before')
-TELTHREAD = TelThread(TC.flush)
-TELTHREAD.start()
-print('after')
