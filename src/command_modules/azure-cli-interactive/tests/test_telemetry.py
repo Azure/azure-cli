a11edd4fae04661fc050de1ced2bb489faa28a04
Courtney Oka <oka.c@husky.neu.edu>
2017-05-17 13:21:18 -0700

    test for tel

---
+++ src/command_modules/azure-cli-interactive/tests/test_telemetry.py
@@ -0,0 +1,25 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import unittest
+from azclishell.telemetry import scrub
+
+
+class TelemetryTest(unittest.TestCase):
+
+    def test_scrubbing(self):
+        """ tests the scrubbing of parameters """
+        text1 = 'this is a --secret value wut wut'
+        text2 = 'this has NO secret value'
+        text3 = 'this is -my value -is alice --bob knows'
+        scrub_val = '*****'
+        self.assertEqual(scrub(text1), 'this is a --secret {} wut wut'.format(scrub_val))
+        self.assertEqual(scrub(text2), text2)
+        self.assertEqual(scrub(text3), 'this is -my {} -is {} --bob {}'.format(
+            scrub_val, scrub_val, scrub_val))
+
+
+if __name__ == '__main__':
+    unittest.main()
