c47e33e9ae9e29627a1276830eb0e122c1d3cb9b
Ed Samson <edsamw@gmail.com>
2017-05-19 16:54:00 -0700

    Adding mock for client calls

--- src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/_factory.py
+++ src/command_modules/azure-cli-sf/azure/cli/command_modules/sf/_factory.py
@@ -7,33 +7,40 @@
 def cf_sf_client(_):
     from azure.cli.core.util import CLIError
     from azure.servicefabric import ServiceFabricClientAPIs
-    from azure.cli.command_modules.sf.custom import (
-        sf_get_cert_info, sf_get_connection_endpoint,
-        sf_get_ca_cert_info, sf_get_verify_setting
-    )
-    from azure.cli.command_modules.sf.cluster_auth import (
-        ClientCertAuthentication
-    )
-    from azure.cli.core.commands.client_factory import (
-        configure_common_settings
-    )
+    from azure.cli.command_modules.sf.custom import (sf_get_cert_info, sf_get_connection_endpoint,
+                                                     sf_get_ca_cert_info, sf_get_verify_setting)
+    from azure.cli.command_modules.sf.cluster_auth import ClientCertAuthentication
+    from azure.cli.core.commands.client_factory import configure_common_settings
+    from mock import patch
 
-    endpoint = sf_get_connection_endpoint()
-    if endpoint is None:
-        raise CLIError(
-            "Connection endpoint not specified, run 'az sf cluster "
-            "select' first."
-        )
+    def mock_get_cert_info():
+        return None
 
-    cert = sf_get_cert_info()
-    if cert is not None:
-        ca_cert = sf_get_ca_cert_info()
-    else:
-        ca_cert = None
+    def mock_get_endpoint():
+        return "http://eddertester.westus2.cloudapp.azure.com:19080"
 
-    no_verify = sf_get_verify_setting()
+    def mock_get_verify_setting():
+        return None
 
-    auth = ClientCertAuthentication(cert, ca_cert, no_verify)
-    client = ServiceFabricClientAPIs(auth, base_url=endpoint)
-    configure_common_settings(client)
-    return client
+    with patch("__main__", sf_get_cert_info=mock_get_cert_info,
+               sf_get_connection_endpoint=mock_get_endpoint,
+               sf_get_verify_setting=mock_get_verify_setting):
+        endpoint = sf_get_connection_endpoint()
+        if endpoint is None:
+            raise CLIError(
+                "Connection endpoint not specified, run 'az sf cluster "
+                "select' first."
+            )
+
+        cert = sf_get_cert_info()
+        if cert is not None:
+            ca_cert = sf_get_ca_cert_info()
+        else:
+            ca_cert = None
+
+        no_verify = sf_get_verify_setting()
+
+        auth = ClientCertAuthentication(cert, ca_cert, no_verify)
+        client = ServiceFabricClientAPIs(auth, base_url=endpoint)
+        configure_common_settings(client)
+        return client
