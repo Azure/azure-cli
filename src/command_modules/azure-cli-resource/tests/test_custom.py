7c10cae4442c3d64d5570fe4dcdf96c47dd1531a
Brendan Burns <bburns@microsoft.com>
2017-05-19 15:20:47 -0700

    Fix some problems with prompting for parameters. (#3173)
    * Fix some problems with prompting for parameters.
    
    * Fix lint.

--- src/command_modules/azure-cli-resource/tests/test_custom.py
+++ src/command_modules/azure-cli-resource/tests/test_custom.py
@@ -5,10 +5,46 @@
 
 import unittest
 
-from azure.cli.command_modules.resource.custom import _merge_parameters
+from azure.cli.command_modules.resource.custom import (
+    _merge_parameters,
+    _get_missing_parameters
+)
 
 
 class TestCustom(unittest.TestCase):
+    def test_resource_missing_parameters(self):
+        template = {
+            "parameters": {
+                "def": {
+                    "type": "string",
+                    "defaultValue": "default"
+                },
+                "present": {
+                    "type": "string",
+                },
+                "missing": {
+                    "type": "string",
+                }
+            }
+        }
+        parameters = {
+            "present": {
+                "value": "foo"
+            }
+        }
+        out_params = _get_missing_parameters(parameters, template, lambda x: {"missing": "baz"})
+
+        expected = {
+            "present": {
+                "value": "foo"
+            },
+            "missing": {
+                "value": "baz"
+            }
+        }
+
+        self.assertDictEqual(out_params, expected)
+
     def test_resource_merge_parameters(self):
         tests = [
             {
@@ -32,3 +68,7 @@ class TestCustom(unittest.TestCase):
         for test in tests:
             output = _merge_parameters(test['parameter_list'])
             self.assertEqual(output, test['expected'])
+
+
+if __name__ == '__main__':
+    unittest.main()
