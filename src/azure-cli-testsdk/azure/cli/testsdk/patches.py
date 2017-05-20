ebf183c6bfe4bfcea7234d36374d2799062eabe7
Courtney Oka <oka.c@husky.neu.edu>
2017-05-19 17:03:16 -0700

    tests;

--- src/azure-cli-testsdk/azure/cli/testsdk/patches.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/patches.py
@@ -14,10 +14,15 @@ def patch_progress_controller(unit_test):
     def _handle_progress_add(*args, **kwargs):  # pylint: disable=unused-argument
         pass
 
+    def _mock_update_progress(*args, **kwargs):  # pylint: disable=unused-argument
+        pass
+
     _mock_in_unit_test(
         unit_test, 'azure.cli.core.commands.progress.ProgressHook.update', _handle_progress_update)
     _mock_in_unit_test(
         unit_test, 'azure.cli.core.commands.progress.ProgressHook.add', _handle_progress_add)
+    _mock_in_unit_test(
+        unit_test, 'azure.cli.command_modules.storage.custom._update_progress', _mock_update_progress)
 
 
 def patch_main_exception_handler(unit_test):
