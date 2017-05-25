40c111b3daa79c4cc326f09d47e60be5826ecccc
Panda Wang <caixiaw@microsoft.com>
2017-05-25 14:03:24 -0700

    CLI commands for azure billing and consumption (#3402)

---
+++ src/command_modules/azure-cli-billing/azure/cli/command_modules/billing/__init__.py
@@ -0,0 +1,14 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import azure.cli.command_modules.billing._help  # pylint: disable=unused-import
+
+
+def load_params(_):
+    import azure.cli.command_modules.billing._params  # pylint: disable=redefined-outer-name, unused-variable
+
+
+def load_commands():
+    import azure.cli.command_modules.billing.commands  # pylint: disable=redefined-outer-name, unused-variable
