0a07bfc0c7cdb9699889e2a6d8ed513b7154e601
Jared Moore <jaredmoo@microsoft.com>
2017-05-25 11:12:26 -0700

    Reverted parameters.py change

--- src/azure-cli-core/azure/cli/core/commands/parameters.py
+++ src/azure-cli-core/azure/cli/core/commands/parameters.py
@@ -12,7 +12,7 @@ from azure.cli.core.commands import \
 from azure.cli.core.commands.validators import validate_tag, validate_tags
 from azure.cli.core.util import CLIError
 from azure.cli.core.commands.validators import generate_deployment_name
-from azure.cli.core.profiles import get_sdk, ResourceType
+from azure.cli.core.profiles import get_sdk, ResourceType, supported_api_version
 import azure.cli.core.azlogging as azlogging
 
 logger = azlogging.get_az_logger(__name__)
@@ -66,7 +66,10 @@ def get_resources_in_resource_group(resource_group_name, resource_type=None):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     rcf = get_mgmt_service_client(ResourceType.MGMT_RESOURCE_RESOURCES)
     filter_str = "resourceType eq '{}'".format(resource_type) if resource_type else None
-    return list(rcf.resource_groups.list_resources(resource_group_name, filter=filter_str))
+    if supported_api_version(ResourceType.MGMT_RESOURCE_RESOURCES, max_api='2016-09-01'):
+        return list(rcf.resource_groups.list_resources(resource_group_name, filter=filter_str))
+    else:
+        return list(rcf.resources.list_by_resource_group(resource_group_name, filter=filter_str))
 
 
 def get_resources_in_subscription(resource_type=None):
@@ -103,12 +106,6 @@ def model_choice_list(resource_type, model_name):
     return enum_choice_list(model) if model else {}
 
 
-def create_enum_type(choices):
-    """ Creates the type for a supplied enum type or list of strings. """
-    def _type(value):
-        return next((x for x in choices if x.lower() == value.lower()), value) if value else value
-
-
 def enum_choice_list(data):
     """ Creates the argparse choices and type kwargs for a supplied enum type or list of strings. """
     # transform enum types, otherwise assume list of string choices
@@ -119,9 +116,11 @@ def enum_choice_list(data):
     except AttributeError:
         choices = data
 
+    def _type(value):
+        return next((x for x in choices if x.lower() == value.lower()), value) if value else value
     params = {
         'choices': CaseInsensitiveList(choices),
-        'type': create_enum_type(choices)
+        'type': _type
     }
     return params
 
