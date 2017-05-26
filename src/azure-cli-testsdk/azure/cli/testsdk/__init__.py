5516d589fda9f2ae4d51d9148787fa532c9ed96e
Jared Moore <jaredmoo@microsoft.com>
2017-05-26 12:49:41 -0700

    Fixed testsdk style error

--- src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
@@ -13,7 +13,7 @@ from .decorators import live_only, record_only
 from .utilities import get_sha1_hash
 
 __all__ = ['ScenarioTest', 'LiveTest', 'ResourceGroupPreparer', 'StorageAccountPreparer',
-           'RoleBasedServicePrincipalPreparer', 'CliTestError', 'JMESPathCheck', 'JMESPathCheckExists','NoneCheck',
+           'RoleBasedServicePrincipalPreparer', 'CliTestError', 'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck',
            'live_only', 'record_only', 'StringCheck', 'StringContainCheck', 'get_sha1_hash', 'KeyVaultPreparer',
            'JMESPathCheckGreaterThan']
 __version__ = '0.1.0+dev'
