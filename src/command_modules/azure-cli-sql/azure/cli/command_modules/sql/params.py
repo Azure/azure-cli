21838d4cd64629e533f68622bb54b94eee58c2d5
jaczhan <jaczhan@microsoft.com>
2017-06-06 11:55:12 -0700

    Update params.py

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
@@ -601,12 +601,9 @@ with ParametersContext(command='sql server aad-admin create') as c:
 
 with ParametersContext(command='sql server aad-admin update') as c:
     c.expand('properties', ServerAzureADAdministrator)
-    c.argument('login',
-               options_list=('--user', '-u'))
-    c.argument('sid',
-               options_list=('--sid', '-s'))
-    c.argument('tenant_id',
-               options_list=('--tenant-id', '-t'))
+    c.argument('login', options_list=('--user', '-u'))
+    c.argument('sid', options_list=('--sid', '-s'))
+    c.argument('tenant_id', options_list=('--tenant-id', '-t'))
 
 #####
 #           sql server firewall-rule
