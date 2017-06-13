3baec3b9a6a286d4f6f5e683667652aeeb8c3f85
Troy Dai <troy.dai@outlook.com>
2017-06-13 14:42:36 -0700

    Release azure-cli-dla 0.0.8 (#3685)

--- src/command_modules/azure-cli-dla/setup.py
+++ src/command_modules/azure-cli-dla/setup.py
@@ -16,8 +16,7 @@ except ImportError:
     cmdclass = {}
 
 
-VERSION = '0.0.7+dev'
-
+VERSION = "0.0.8+dev"
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
