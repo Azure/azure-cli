fc6bbf21c798fd79035e2e8706cefa2f609ca2dc
Troy Dai <troy.dai@outlook.com>
2017-06-13 14:18:06 -0700

    Release azure-cli-sql 2.0.5

--- src/command_modules/azure-cli-sql/setup.py
+++ src/command_modules/azure-cli-sql/setup.py
@@ -12,8 +12,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = '2.0.4+dev'
-
+VERSION = "2.0.5+dev"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
