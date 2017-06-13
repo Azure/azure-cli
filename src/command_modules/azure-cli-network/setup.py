f287506878930953e4b46ca2f12cc65e80166e1f
Troy Dai <troy.dai@outlook.com>
2017-06-13 14:44:23 -0700

    Release azure-cli-network 2.0.8 (#3694)

--- src/command_modules/azure-cli-network/setup.py
+++ src/command_modules/azure-cli-network/setup.py
@@ -14,8 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = '2.0.7+dev'
-
+VERSION = "2.0.8+dev"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
