93f2554090485add70340648b2b02f5442e17d0a
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2017-06-19 11:42:15 -0700

    logic simplified

--- src/command_modules/azure-cli-cosmosdb/azure/cli/command_modules/cosmosdb/custom.py
+++ src/command_modules/azure-cli-cosmosdb/azure/cli/command_modules/cosmosdb/custom.py
@@ -219,7 +219,7 @@ def _populate_collection_definition(collection,
                                     partition_key_path=None,
                                     default_ttl=None,
                                     indexing_policy=None):
-    if not any(arg is not None for arg in [partition_key_path, default_ttl, indexing_policy]):
+    if all(arg is None for arg in [partition_key_path, default_ttl, indexing_policy]):
         return False
 
     if partition_key_path is not None:
