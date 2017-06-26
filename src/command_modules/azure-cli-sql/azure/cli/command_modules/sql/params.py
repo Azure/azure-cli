28fe933ce6a37695bc0c2b751e4537f7fd3f5e77
jaczhan <jaczhan@microsoft.com>
2017-06-26 11:15:09 -0700

    Update params.py

--- src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
+++ src/command_modules/azure-cli-sql/azure/cli/command_modules/sql/params.py
@@ -570,8 +570,7 @@ with ParametersContext(command='sql server') as c:
 with ParametersContext(command='sql server create') as c:
     # Both administrator_login and administrator_login_password are required for server creation.
     # However these two parameters are given default value in the create_or_update function
-    # signature, therefore, they can't be automatically converted to requirement arguments.
-    c.expand('parameters', Server, group_name='Authentication', patches={
+    # signature, therefore, they can't be automatically converted to requirement arguments.    c.expand('parameters', Server, group_name='Authentication', patches={
         'administrator_login': patch_arg_make_required,
         'administrator_login_password': patch_arg_make_required
     })
@@ -587,7 +586,6 @@ with ParametersContext(command='sql server update') as c:
 #####
 #           sql server ad-admin
 ######
-
 
 with ParametersContext(command='sql server ad-admin') as c:
     c.argument('server_name', options_list=('--server-name', '-s'),
