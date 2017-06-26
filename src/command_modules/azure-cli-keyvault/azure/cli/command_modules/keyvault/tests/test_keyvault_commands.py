0f83d6b1bd830e83ec241a00058c72805d7c4e71
Scott Schaab <sschaab@microsoft.com>
2017-06-26 13:25:33 -0700

    fix for test error when running in playback

--- src/command_modules/azure-cli-keyvault/azure/cli/command_modules/keyvault/tests/test_keyvault_commands.py
+++ src/command_modules/azure-cli-keyvault/azure/cli/command_modules/keyvault/tests/test_keyvault_commands.py
@@ -68,6 +68,14 @@ class ParseKeyVaultIdTest(ResourceGroupVCRTestBase):
     def __init__(self, test_method):
         super(ParseKeyVaultIdTest, self).__init__(__file__, test_method)
 
+    def set_up(self):
+        # overriding base set_up to avoid resource group creation as it is not needed
+        pass
+
+    def tear_down(self):
+        # overriding base set_up to avoid resource group deletion as it is not needed
+        pass
+
     def test_keyvault_id_parsing(self):
         self.execute()
 
