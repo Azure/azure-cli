42fca23d6179941c3c5ecfd0a899aef58c626cf8
azuresdkci <azuresdkci@users.noreply.github.com>
2017-07-07 16:19:15 +0000

    Release azure-cli-keyvault 2.0.8

--- src/command_modules/azure-cli-keyvault/setup.py
+++ src/command_modules/azure-cli-keyvault/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = '2.0.7+dev'
+VERSION = "2.0.8+dev"
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
