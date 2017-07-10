38d01246f2afda6f1265edf7a10e0b91827dc23b
Christof Marti <chrmarti@microsoft.com>
2017-07-10 09:55:24 -0700

    Add comment about limitation of use to VSCode extension

--- src/azure-cli-core/azure/cli/core/tooling.py
+++ src/azure-cli-core/azure/cli/core/tooling.py
@@ -1,4 +1,8 @@
-"""tooling integration"""
+"""
+Tooling Integration
+
+This module is used by the Azure CLI Tools extension for VSCode. It should not be used anywhere else and will be removed in a future release.
+"""
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
