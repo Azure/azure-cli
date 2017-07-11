9cba756f12e89e9099452c3cc777243e4360e941
Vishnu Priya Ananthu Sundaram <viananth@microsoft.com>
2017-07-11 15:29:51 -0700

    Fix Nic creation issue in stack

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/custom.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/custom.py
@@ -1,4 +1,4 @@
-# --------------------------------------------------------------------------------------------
+﻿# --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
@@ -1134,14 +1134,20 @@ def create_nic(resource_group_name, network_interface_name, subnet, location=Non
                public_ip_address=None, virtual_network_name=None, enable_accelerated_networking=None):
     client = _network_client_factory().network_interfaces
     NetworkInterface = get_sdk(ResourceType.MGMT_NETWORK, 'NetworkInterface', mod='models')
+    
+    network_interface_args = {}
+    if supported_api_version(ResourceType.MGMT_NETWORK, min_api='2016-09-01'):
+        network_interface_args['enable_accelerated_networking'] = enable_accelerated_networking
+
     NetworkInterfaceDnsSettings = get_sdk(
         ResourceType.MGMT_NETWORK,
         'NetworkInterfaceDnsSettings', mod='models')
 
     dns_settings = NetworkInterfaceDnsSettings(internal_dns_name_label=internal_dns_name_label,
                                                dns_servers=dns_servers or [])
+
     nic = NetworkInterface(location=location, tags=tags, enable_ip_forwarding=enable_ip_forwarding,
-                           dns_settings=dns_settings, enable_accelerated_networking=enable_accelerated_networking)
+                           dns_settings=dns_settings, **network_interface_args)
 
     if network_security_group:
         nic.network_security_group = NetworkSecurityGroup(id=network_security_group)
