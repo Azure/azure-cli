311a2058fc0b3dfbc2b5dbe2f93fff638d73643b
vinod953 <connectwithvinod@gmail.com>
2017-08-09 21:11:58 +0530

    Locking vsts-cd-manager to version 0.118.0

--- src/command_modules/azure-cli-appservice/setup.py
+++ src/command_modules/azure-cli-appservice/setup.py
@@ -37,7 +37,7 @@ DEPENDENCIES = [
     'urllib3[secure]>=1.18',
     'xmltodict',
     'pyOpenSSL',
-    'vsts-cd-manager',
+    'vsts-cd-manager<=0.118.0',
 ]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
