125155cba8de200a25582937270cd66c36f2c217
Derek Bekoe <debekoe@microsoft.com>
2017-08-14 16:25:08 -0700

    Remove confirm param

--- src/command_modules/azure-cli-extension/azure/cli/command_modules/extension/commands.py
+++ src/command_modules/azure-cli-extension/azure/cli/command_modules/extension/commands.py
@@ -7,9 +7,7 @@
 
 from azure.cli.core.commands import cli_command
 
-_CONFIRM_MSG = "Extensions can modify existing commands as well as add new commands.\nOnly install extensions from trusted sources.\nAre you sure you want to add this extension?"
-
-cli_command(__name__, 'extension add', 'azure.cli.command_modules.extension.custom#add_extension', confirmation=_CONFIRM_MSG)
-cli_command(__name__, 'extension remove', 'azure.cli.command_modules.extension.custom#remove_extension', confirmation=True)
+cli_command(__name__, 'extension add', 'azure.cli.command_modules.extension.custom#add_extension')
+cli_command(__name__, 'extension remove', 'azure.cli.command_modules.extension.custom#remove_extension')
 cli_command(__name__, 'extension list', 'azure.cli.command_modules.extension.custom#list_extensions')
 cli_command(__name__, 'extension show', 'azure.cli.command_modules.extension.custom#show_extension')
