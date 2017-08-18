95d64b7ea15bc818e04a5ce872749a4e8950d6e2
Yugang Wang <yugangw@microsoft.com>
2017-08-18 10:06:54 -0700

    webapp: bug fixes on output, logging, etc (#4234)

--- src/command_modules/azure-cli-appservice/HISTORY.rst
+++ src/command_modules/azure-cli-appservice/HISTORY.rst
@@ -2,6 +2,14 @@
 
 Release History
 ===============
+Unreleased
+++++++++++++++++++
+Breaking Change:webapp: fix inconsistencies in the output of "az webapp config appsettings delete/set"
+webapp: add a new alias of '-i' for "az webapp config container set --docker-custom-image-name"
+webapp: expose 'az webapp log show'
+webapp: expose new arguments from 'az webapp delete' to retain app service plan, metrics or dns registration. 
+webapp: detect a slot setting correctly 
+
 0.1.13 (2017-08-15)
 +++++++++++++++++++
 webapp: fix an exception when create a new git based linux webapp
--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
@@ -134,8 +134,18 @@ def _rename_server_farm_props(webapp):
     return webapp
 
 
-def delete_webapp(resource_group_name, name, slot=None):
+def delete_function_app(resource_group_name, name, slot=None):
     return _generic_site_operation(resource_group_name, name, 'delete', slot)
+
+
+def delete_webapp(resource_group_name, name, keep_metrics=None, keep_empty_plan=None,
+                  keep_dns_registration=None, slot=None):
+    client = web_client_factory()
+    delete_method = getattr(client.web_apps, 'delete' if slot is None else 'delete_slot')
+    delete_method(resource_group_name, name,
+                  delete_metrics=False if keep_metrics else None,
+                  delete_empty_server_farm=False if keep_empty_plan else None,
+                  skip_dns_registration=False if keep_dns_registration else None)
 
 
 def stop_webapp(resource_group_name, name, slot=None):
@@ -157,12 +167,8 @@ def get_site_configs(resource_group_name, name, slot=None):
 def get_app_settings(resource_group_name, name, slot=None):
     result = _generic_site_operation(resource_group_name, name, 'list_application_settings', slot)
     client = web_client_factory()
-    slot_constr_names = client.web_apps.list_slot_configuration_names(resource_group_name, name) \
-                              .connection_string_names or []
-    result = [{'name': p,
-               'value': result.properties[p],
-               'slotSetting': p in slot_constr_names} for p in _mask_creds_related_appsettings(result.properties)]
-    return result
+    slot_app_setting_names = client.web_apps.list_slot_configuration_names(resource_group_name, name).app_setting_names
+    return _build_app_settings_output(result.properties, slot_app_setting_names)
 
 
 def get_connection_strings(resource_group_name, name, slot=None):
@@ -251,16 +257,17 @@ def update_app_settings(resource_group_name, name, settings=None, slot=None, slo
 
     result = _generic_site_operation(resource_group_name, name, 'update_application_settings',
                                      slot, app_settings)
-
+    app_settings_slot_cfg_names = []
     if slot_settings:
         client = web_client_factory()
         new_slot_setting_names = [n.split('=', 1)[0] for n in slot_settings]
         slot_cfg_names = client.web_apps.list_slot_configuration_names(resource_group_name, name)
         slot_cfg_names.app_setting_names = slot_cfg_names.app_setting_names or []
         slot_cfg_names.app_setting_names += new_slot_setting_names
+        app_settings_slot_cfg_names = slot_cfg_names.app_setting_names
         client.web_apps.update_slot_configuration_names(resource_group_name, name, slot_cfg_names)
 
-    return _mask_creds_related_appsettings(result.properties)
+    return _build_app_settings_output(result.properties, app_settings_slot_cfg_names)
 
 
 def delete_app_settings(resource_group_name, name, setting_names, slot=None):
@@ -277,9 +284,16 @@ def delete_app_settings(resource_group_name, name, setting_names, slot=None):
 
     if is_slot_settings:
         client.web_apps.update_slot_configuration_names(resource_group_name, name, slot_cfg_names)
-    return _mask_creds_related_appsettings(_generic_site_operation(resource_group_name, name,
-                                                                   'update_application_settings',
-                                                                   slot, app_settings).properties)
+    result = _generic_site_operation(resource_group_name, name,
+                                     'update_application_settings', slot, app_settings)
+    return _build_app_settings_output(result.properties, slot_cfg_names.app_setting_names)
+
+
+def _build_app_settings_output(app_settings, slot_cfg_names):
+    slot_cfg_names = slot_cfg_names or []
+    return [{'name': p,
+             'value': app_settings[p],
+             'slotSetting': p in slot_cfg_names} for p in _mask_creds_related_appsettings(app_settings)]
 
 
 def update_connection_strings(resource_group_name, name, connection_string_type,
@@ -974,6 +988,10 @@ def config_diagnostics(resource_group_name, name, level=None,
 
     return _generic_site_operation(resource_group_name, name, 'update_diagnostic_logs_config',
                                    slot, site_log_config)
+
+
+def show_diagnostic_settings(resource_group_name, name, slot=None):
+    return _generic_site_operation(resource_group_name, name, 'get_diagnostic_logs_configuration', slot)
 
 
 def config_slot_auto_swap(resource_group_name, webapp, slot, auto_swap_slot=None, disable=None):
