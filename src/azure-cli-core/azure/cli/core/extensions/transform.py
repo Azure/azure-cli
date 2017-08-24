3dc9bc0e38dd6a271e766345c3c5dbdbcdfaf69b
Jingtao Ren <jiren@microsoft.com>
2017-08-24 13:48:48 -0700

    make _parse_id case insensitive

--- src/azure-cli-core/azure/cli/core/extensions/transform.py
+++ src/azure-cli-core/azure/cli/core/extensions/transform.py
@@ -16,7 +16,7 @@ def register(application):
 def _parse_id(strid):
     parsed = {}
     parts = re.split('/', strid)
-    if parts[3] != 'resourceGroups':
+    if not re.match('resourcegroups', parts[3], re.IGNORECASE):
         raise KeyError()
 
     parsed['resource-group'] = parts[4]
