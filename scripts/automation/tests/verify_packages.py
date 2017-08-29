04a74dc39e2b5fddc605a4fb1cf1990aa9e2dbe7
Derek Bekoe <debekoe@microsoft.com>
2017-08-29 13:37:50 -0700

    In package verification, check for missing MANIFEST.in. (#4339)
    * In package verification, check for missing MANIFEST.in.
    
    * Add missing MANIFEST.in

--- scripts/automation/tests/verify_packages.py
+++ scripts/automation/tests/verify_packages.py
@@ -105,6 +105,12 @@ def verify_packages():
     all_modules = automation_path.get_all_module_paths()
     all_command_modules = automation_path.get_command_modules_paths(include_prefix=True)
 
+    modules_missing_manifest_in = [name for name, path in all_modules if not os.path.isfile(os.path.join(path, 'MANIFEST.in'))]
+    if modules_missing_manifest_in:
+        print_heading('Error: The following modules are missing the MANIFEST.in file.')
+        print(modules_missing_manifest_in)
+        sys.exit(1)
+
     # STEP 1:: Build the packages
     for name, path in all_modules:
         build_package(path, built_packages_dir)
