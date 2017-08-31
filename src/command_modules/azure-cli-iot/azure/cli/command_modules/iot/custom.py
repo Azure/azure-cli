fbaf6b9e8e113e80a623882e2adb713370e8dc59
Paymaun Heidari <Paymaunh@microsoft.com>
2017-08-30 19:02:25 -0700

    With justification, disabled pylint errors that don't make sense in this particular use case.
    * For example, IoT SDK is a compiled binary. 'no-name-in-module' errors
    would output for every import although the import is working fine.
    * Another example is the 'unused-argument' warning. The IoT SDK
    actually throws exceptions if the signature does not match regardless
    of if you use the parameters or not.

--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/custom.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/custom.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# pylint: disable=no-self-use,no-member,line-too-long,too-few-public-methods
+# pylint: disable=no-self-use,no-member,line-too-long,too-few-public-methods,no-name-in-module
 
 from os.path import exists
 from enum import Enum
--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/iot_sdk/device_manager.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/iot_sdk/device_manager.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for
 # full license information.
+# pylint: disable=no-self-use,unused-argument,no-name-in-module,too-many-instance-attributes
 
 import time
 import os
