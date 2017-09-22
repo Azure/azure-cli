8ae54c81a8017c9dbef59fc8758259ac554eb7f1
azuresdkci <azuresdkci@users.noreply.github.com>
2017-09-22 23:22:44 +0000

    Release azure-cli-storage 2.0.16

--- src/command_modules/azure-cli-storage/setup.py
+++ src/command_modules/azure-cli-storage/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.15+dev"
+VERSION = "2.0.16+dev"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
