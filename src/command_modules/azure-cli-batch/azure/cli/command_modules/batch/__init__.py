9bdd2adc55e4e8807ed5f0297bba723f721d1b61
annatisch <antisch@microsoft.com>
2017-10-05 11:29:40 -0700

    [Batch] Updates for new extension model (#4607)
    * Updates for new extension model
    
    * Updated release notes

--- src/command_modules/azure-cli-batch/azure/cli/command_modules/batch/__init__.py
+++ src/command_modules/azure-cli-batch/azure/cli/command_modules/batch/__init__.py
@@ -12,19 +12,7 @@ logger = azlogging.get_az_logger(__name__)
 
 def load_params(_):
     import azure.cli.command_modules.batch._params  # pylint: disable=redefined-outer-name, unused-variable
-    try:
-        import azure.cli.command_modules.batch_extensions._params
-    except ImportError as exp:
-        # Optional Batch Extensions not installed
-        logger.debug("Did not load Batch Extensions module")
-        logger.debug(str(exp))
 
 
 def load_commands():
     import azure.cli.command_modules.batch.commands  # pylint: disable=redefined-outer-name, unused-variable
-    try:
-        import azure.cli.command_modules.batch_extensions.commands
-    except ImportError as exp:
-        # Optional Batch Extensions not installed
-        logger.debug("Did not load Batch Extensions module")
-        logger.debug(str(exp))
