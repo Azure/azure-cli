d4de91c63abd05f7ec460c61bc41c74b98696357
Willie Xu <wilx@microsoft.com>
2017-10-12 09:48:20 -0700

    rebase

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/tests/test_webapp_commands.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/tests/test_webapp_commands.py
@@ -714,29 +714,6 @@ class FunctionAppWithPlanE2ETest(ResourceGroupVCRTestBase):
         self.cmd('functionapp delete -g {} -n {}'.format(self.resource_group, functionapp_name))
 
 
-<<<<<<< HEAD
-class FunctionAppWithConsumptionPlanE2ETest(ResourceGroupVCRTestBase):
-
-    def __init__(self, test_method):
-        super(FunctionAppWithConsumptionPlanE2ETest, self).__init__(__file__, test_method, resource_group='azurecli-functionapp-c-e2e')
-
-    def test_functionapp_consumption_e2e(self):
-        self.execute()
-
-    def body(self):
-        functionapp_name = 'functionappconsumption1'
-        location = 'westus'
-        storage = 'functionaconstorage1'
-
-        self.cmd('storage account create --name {} -g {} -l {} --sku Standard_LRS'.format(storage, self.resource_group, location))
-        self.cmd('functionapp create -g {} -n {} -c {} -s {}'.format(self.resource_group, functionapp_name, location, storage), checks=[
-            JMESPathCheck('state', 'Running'),
-            JMESPathCheck('name', functionapp_name),
-            JMESPathCheck('hostNames[0]', functionapp_name + '.azurewebsites.net')
-        ])
-
-        self.cmd('functionapp delete -g {} -n {}'.format(self.resource_group, functionapp_name))
-=======
 class FunctionAppWithConsumptionPlanE2ETest(ScenarioTest):
     @ResourceGroupPreparer(name_prefix='azurecli-functionapp-c-e2e', location='westus')
     @StorageAccountPreparer()
@@ -750,7 +727,6 @@ class FunctionAppWithConsumptionPlanE2ETest(ScenarioTest):
                      JMESPathCheckV2('hostNames[0]', functionapp_name + '.azurewebsites.net')])
 
         self.cmd('functionapp delete -g {} -n {}'.format(resource_group, functionapp_name))
->>>>>>> fixed function app test by randomizing name and turned to scenariotest
 
 
 class WebappAuthenticationTest(ScenarioTest):
