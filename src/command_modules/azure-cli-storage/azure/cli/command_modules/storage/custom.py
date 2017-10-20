9590cf1cd346e097ac2bd30f2351b24c011202f0
Willie Xu <wx44@cornell.edu>
2017-10-20 10:20:04 -0700

    used skuname enum from sdk

--- src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/custom.py
+++ src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/custom.py
@@ -39,7 +39,7 @@ def _update_progress(current, total):
 
 # CUSTOM METHODS
 
-def create_storage_account(resource_group_name, account_name, sku='Standard_RAGRS', location=None, kind=None,
+def create_storage_account(resource_group_name, account_name, sku=None, location=None, kind=None,
                            tags=None, custom_domain=None, encryption_services=None, access_tier=None, https_only=None,
                            bypass=None, default_action=None, assign_identity=False):
     StorageAccountCreateParameters, Kind, Sku, CustomDomain, AccessTier, Identity, Encryption, NetworkRuleSet = get_sdk(
