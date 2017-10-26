322a35bd1b7a65b1eba7c5312eb9fec4d8842702
Willie Xu <wx44@cornell.edu>
2017-10-26 13:42:43 -0700

    Fixed versions from pr #4704 (#4769)
    * fixed versions from pr #4704
    
    * Capitalized sentences
    
    * fixversions

--- src/command_modules/azure-cli-lab/setup.py
+++ src/command_modules/azure-cli-lab/setup.py
@@ -12,7 +12,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.0.12"
+VERSION = "0.0.13"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
