3abcc80788f3e21d00d40c60fda43c3ff6754e5c
Willie Xu <wx44@cornell.edu>
2017-10-31 15:20:13 -0700

    better error messages for `deployment user set` (#4794)
    * usererrors
    
    * used yugang's exception handler
    
    * throw exception instead of logging
    
    * refactored handler to command instead of custom method

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
@@ -958,8 +958,7 @@ def set_deployment_user(user_name, password=None):
             raise CLIError('Please specify both username and password in non-interactive mode.')
 
     user.publishing_password = password
-    result = client.update_publishing_user(user)
-    return result
+    return client.update_publishing_user(user)
 
 
 def list_publish_profiles(resource_group_name, name, slot=None):
