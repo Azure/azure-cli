187be20a038ead8f5a93475928f01aaff9c60e07
Derek Bekoe <debekoe@microsoft.com>
2017-11-01 09:47:49 -0700

    ‘azdev build’ for Debian, RPM, Docker, Python wheels, Homebrew (#4750)
    * ‘azdev build’ for Debian, RPM, Docker, Python wheels, Homebrew
    
    Use entry points instead of custom scripts
    
    * Fix pylint

--- tools/scripts/azdev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM --------------------------------------------------------------------------------------------
-REM Copyright (c) Microsoft Corporation. All rights reserved.
-REM Licensed under the MIT License. See License.txt in the project root for license information.
-REM --------------------------------------------------------------------------------------------
-
-pip show azure-cli-dev-tools>NUL || pip install -e %~dp0
-python -m automation %*
