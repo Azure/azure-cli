4b507718fb3a1ba1aa003a32cacc6aa9bdcd48f1
yugangw-msft <yugangw@microsoft.com>
2017-11-03 12:15:57 -0700

    msi: support explicit identity on vm/vmss

--- src/command_modules/azure-cli-vm/setup.py
+++ src/command_modules/azure-cli-vm/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     cmdclass = {}
 
 
-VERSION = "2.0.21"
+VERSION = "2.0.20"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
@@ -31,6 +31,7 @@ CLASSIFIERS = [
 ]
 
 DEPENDENCIES = [
+    'azure-mgmt-msi==0.1.0',
     'azure-mgmt-authorization==0.30.0',
     'azure-mgmt-compute==3.1.0rc1',
     'azure-mgmt-keyvault==0.40.0',
