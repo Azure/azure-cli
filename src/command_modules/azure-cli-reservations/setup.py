1378f8d43a4734a67adf5204d3eec00055b8fe5a
Sean Oh <hyo@microsoft.com>
2017-11-07 10:57:55 -0800

    setup and README corrected

--- src/command_modules/azure-cli-reservations/setup.py
+++ src/command_modules/azure-cli-reservations/setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
@@ -27,6 +29,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'License :: OSI Approved :: MIT License',
 ]
 
 DEPENDENCIES = [
