b3070fcdc852a72b349e45e20610eb14c8b29769
Prasanna Padmanabhan <prasank@microsoft.com>
2017-11-15 16:13:32 -0800

    More updates.

--- src/command_modules/azure-cli-advisor/azure/cli/command_modules/advisor/tests/test_advisor_commands.py
+++ src/command_modules/azure-cli-advisor/azure/cli/command_modules/advisor/tests/test_advisor_commands.py
@@ -11,7 +11,21 @@ class AzureAdvisorServiceScenarioTest(ScenarioTest):
 
     def test_generate_recommendations(self):
         output = self.cmd('advisor recommendation generate').get_output_in_json()
-        self.assertEqual(output, 'Recommendations successfully generated.')
+        self.assertEqual(output['Status'], 204)
+
+    def test_list_disable_enable_recommendations(self):
+        output = self.cmd('advisor recommendation list').get_output_in_json()
+        self.assertTrue(len(output) > 1)
+        output = self.cmd('advisor recommendation list --category cost').get_output_in_json()
+        self.assertEqual(len(output), 1)
+        disableCmd = 'advisor recommendation disable --ids {} --days 1'.format(output[0]['id'])
+        enableCmd = 'advisor recommendation enable --ids {}'.format(output[0]['id'])
+        output = self.cmd(disableCmd).get_output_in_json()
+        self.assertEqual(output[0]['ttl'], '1.00:00:00')
+        self.cmd(enableCmd)
+        output = self.cmd('advisor recommendation list --category cost').get_output_in_json()
+        self.assertEqual(len(output), 1)
+        self.assertEqual(output[0]['suppressionIds'], None)
 
 
 if __name__ == '__main__':
