dc96afcd828d0613bbe3cecc2798593b0ec77ecd
Sandeep N L <sandeepiyengar@gmail.com>
2017-11-16 15:47:33 -0800

    Changing CLI for azure-consumption reflecting the new api-version (#4900)
    * Changing CLI for azure-consumption reflecting the new api-version
    
    * Modifying Setup.py and history.rst
    
    * Implementing review comments

--- src/command_modules/azure-cli-consumption/azure/cli/command_modules/consumption/tests/test_consumption_commands.py
+++ src/command_modules/azure-cli-consumption/azure/cli/command_modules/consumption/tests/test_consumption_commands.py
@@ -2,8 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-from azure.cli.testsdk import ScenarioTest, JMESPathCheck
+from datetime import datetime
+from azure.cli.testsdk import ScenarioTest
 
 
 class AzureConsumptionServiceScenarioTest(ScenarioTest):
@@ -25,31 +25,26 @@ class AzureConsumptionServiceScenarioTest(ScenarioTest):
             self.assertIsNone(usage['meterDetails'])
 
     def test_list_usages_billing_period(self):
-        billing_period = '201705-1'
-        usages_list = self.cmd('consumption usage list -p {} -s 2017-04-02 -t 5'.format(billing_period)).get_output_in_json()
+        billing_period = '201710'
+        usages_list = self.cmd('consumption usage list -p {} -t 5'.format(billing_period)).get_output_in_json()
         self.assertTrue(usages_list)
         self.assertTrue(len(usages_list) <= 5)
         self._validate_usage(usages_list[0], False)
-        self.assertTrue(usages_list[0]['billingPeriodId'].endswith(billing_period))
 
     def test_list_usages_billing_period_expand(self):
-        billing_period = '201705-1'
+        billing_period = '201710'
         usages_list = self.cmd('consumption usage list -p {} -a -m -t 5'.format(billing_period)).get_output_in_json()
         self.assertTrue(usages_list)
         self.assertTrue(len(usages_list) <= 5)
         self._validate_usage(usages_list[0], True)
-        self.assertTrue(usages_list[0]['billingPeriodId'].endswith(billing_period))
-
-    def test_list_usages_invoice(self):
-        invoice_name = '201705-217089190378988'
-        usages_list = self.cmd('consumption usage list -i {} -s 2017-03-03 -e 2017-04-04 -m -t 5'.format(invoice_name)).get_output_in_json()
-        self.assertTrue(usages_list)
-        self.assertTrue(len(usages_list) <= 5)
-        self._validate_usage(usages_list[0], True)
-        self.assertTrue(usages_list[0]['invoiceId'].endswith(invoice_name))
 
     def test_list_usages_subscription(self):
-        usages_list = self.cmd('consumption usage list -e 2017-04-01 -t 5 -a').get_output_in_json()
+        usages_list = self.cmd('consumption usage list -t 5 -a').get_output_in_json()
         self.assertTrue(usages_list)
         self.assertTrue(len(usages_list) <= 5)
         self._validate_usage(usages_list[0], False)
+
+    def test_list_usages_subscription_custom_date_range(self):
+        usages_list = self.cmd('consumption usage list -s 2017-10-11T23:59:59Z -e 2017-10-12T03:59:59Z').get_output_in_json()
+        self.assertTrue(usages_list)
+        self.assertTrue(all((datetime.strptime(usage_date['usageEnd'], "%Y-%m-%dT%H:%M:%SZ") >= datetime.strptime('2017-10-11T23:59:59Z', "%Y-%m-%dT%H:%M:%SZ") and datetime.strptime(usage_date['usageEnd'], "%Y-%m-%dT%H:%M:%SZ") <= datetime.strptime('2017-10-12T23:59:59Z', "%Y-%m-%dT%H:%M:%SZ")) for usage_date in usages_list))
