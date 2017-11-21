8b191c9c67e2cb45888df84b4621334ca7893a73
chshou <shou3301@outlook.com>
2017-11-21 10:57:34 -0800

    bump up version

--- src/command_modules/azure-cli-container/setup.py
+++ src/command_modules/azure-cli-container/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.13"
+VERSION = "0.1.14"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
