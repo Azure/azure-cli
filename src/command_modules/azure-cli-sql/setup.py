80a5d694401b3463d781b8c7ba06c8459e9bdee3
Jared Moore <jaredmoo@microsoft.com>
2017-11-21 15:06:09 -0800

    Add az sql show-usage/list-usages (#4883)
    * Add sql show/list-usages commands
    
    * Added release notes
    
    * Updated sql dependency
    
    * Fixed history
    
    * Fixed version num in setup.py

--- src/command_modules/azure-cli-sql/setup.py
+++ src/command_modules/azure-cli-sql/setup.py
@@ -12,7 +12,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.15"
+VERSION = "2.0.16"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
@@ -29,7 +29,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-cli-core',
-    'azure-mgmt-sql==0.8.3',
+    'azure-mgmt-sql==0.8.4',
     'azure-mgmt-storage==1.4.0',
     'six'
 ]
