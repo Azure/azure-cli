6a7d2c2df2f71bc947e2c3ad772a4632b2e76187
Derek Bekoe <debekoe@microsoft.com>
2017-11-22 10:45:18 -0800

    Add advisor to default install (#4956)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -51,6 +51,7 @@ CLASSIFIERS = [
 DEPENDENCIES = [
     'azure-cli-acr',
     'azure-cli-acs',
+    'azure-cli-advisor',
     'azure-cli-appservice',
     'azure-cli-batch',
     'azure-cli-batchai',
