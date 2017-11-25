9968af953f911a93e866167ecca5b21431808eac
Troy Dai <troy.dai@outlook.com>
2017-11-25 13:57:39 -0800

    Update storage command module to new multiapi SDK

--- src/command_modules/azure-cli-storage/setup.py
+++ src/command_modules/azure-cli-storage/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.19"
+VERSION = "2.0.20"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
@@ -30,7 +30,7 @@ CLASSIFIERS = [
 ]
 
 DEPENDENCIES = [
-    'azure-multiapi-storage==0.1.6',
+    'azure-multiapi-storage==0.1.7',
     'azure-mgmt-storage==1.4.0',
     'azure-cli-core',
 ]
