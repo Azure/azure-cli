33aa1df22a0f84091e095330ae6bed01452df17f
Sandeep Nuggehalli Lakshminarayana <sandeepn@microsoft.com>
2017-11-27 15:49:46 -0800

    Editing the help file for az consumption

--- src/command_modules/azure-cli-consumption/azure/cli/command_modules/consumption/_help.py
+++ src/command_modules/azure-cli-consumption/azure/cli/command_modules/consumption/_help.py
@@ -7,15 +7,10 @@ from azure.cli.core.help_files import helps
 
 helps['consumption'] = """
     type: group
-    short-summary: Manage consumption of Azure resources.
-"""
-
-helps['consumption usage'] = """
-    type: group
-    short-summary: Inspect the usage of Azure resources.
+    short-summary: View consumption of Azure resources.
 """
 
 helps['consumption usage list'] = """
     type: command
-    short-summary: Show the details of Azure resource consumption, either as an invoice or within a billing period.
+    short-summary: Shows the usage details of Azure resource consumption, either with current biling period or within a specific billing period.
 """
