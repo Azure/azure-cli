10ec91fafbdff2bd95916fad3ce4ff17ed31ba24
Troy Dai <troy.dai@outlook.com>
2017-11-27 13:59:27 -0800

    Update storage command module to new multiapi SDK (#4977)

--- src/command_modules/azure-cli-vm/setup.py
+++ src/command_modules/azure-cli-vm/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     cmdclass = {}
 
 
-VERSION = "2.0.18"
+VERSION = "2.0.19"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
@@ -37,7 +37,7 @@ DEPENDENCIES = [
     'azure-keyvault==0.3.7',
     'azure-mgmt-network==1.5.0rc3',
     'azure-mgmt-resource==1.2.1',
-    'azure-multiapi-storage==0.1.6',
+    'azure-multiapi-storage==0.1.7',
     'azure-cli-core'
 ]
 
