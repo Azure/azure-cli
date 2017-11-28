5b5f1901ee9290a054a4560791766434254eda38
Troy Dai <troy.dai@outlook.com>
2017-11-27 18:03:35 -0800

    Upgrade SDK reference and update metric commands
    1. Remove azure-monitor SDK reference
    2. Upgrade azure-mgmt-monitor reference to 0.4.0
    3. Update diagnostics settings
    4. Update and refresh monitor action-group commands
    5. Refresh the monitor activity-log list command and tests
    6. Refresh the metric alert command and tests
    7. Refresh the autoscale command
    8. Refresh the diagnostic-settings command
    9. Refresh the log-profiles command
    10.Move custom commands under operations module
    11.BREAKING CHANGE: Update metrics command to support multi-dimension
    metrics
    12.Improve metric list table output

--- src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/transformers.py
+++ src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/transformers.py
@@ -28,6 +28,13 @@ def _item_to_ordered_dict(item, *properties):
     return result
 
 
+def _generic_table_convert(source, row_convert_fn):
+    if not isinstance(source, list):
+        source = [source]
+
+    return [row_convert_fn(each) for each in source]
+
+
 def action_group_list_table(results):
     if not isinstance(results, list):
         results = [results]
@@ -35,10 +42,56 @@ def action_group_list_table(results):
     output_results = []
     for result in results:
         data = _item_to_ordered_dict(result, 'name', 'resourceGroup', 'groupShortName', 'enabled', 'location',
-                                     ('emailReceivers', 'email'),
-                                     ('smsReceivers', 'sms'),
+                                     ('emailReceivers', 'email'), ('smsReceivers', 'sms'),
                                      ('webhookReceivers', 'webhook'))
 
         output_results.append(data)
 
     return output_results
+
+
+def metrics_definitions_table(results):
+    def row_convert(item):
+        from collections import OrderedDict
+        result = OrderedDict()
+        result['Display Name'] = item['name']['localizedValue']
+        result['Metric Name'] = item['name']['value']
+        result['Unit'] = item['unit']
+        result['Type'] = item['primaryAggregationType']
+        result['Dimension Required'] = 'True' if item['isDimensionRequired'] else 'False'
+        result['Dimensions'] = ', '.join(d['value'] for d in item.get('dimensions', []) or [])
+
+        return result
+
+    return _generic_table_convert(results, row_convert)
+
+
+def metrics_table(results):
+    from collections import OrderedDict
+
+    def from_time(time_string):
+        from datetime import datetime
+        try:
+            return datetime.strptime(time_string, '%Y-%m-%dT%H:%M:%S+00:00').strftime('%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            return time_string
+
+    retval = []
+    for value_group in results['value']:
+        name = value_group['name']['localizedValue']
+        for series in value_group['timeseries']:
+            metadata = dict((m['name']['localizedValue'], m['value']) for m in series['metadatavalues'])
+
+            for data in series['data']:
+                row = OrderedDict()
+                row['Timestamp'] = from_time(data['timeStamp'])
+                row['Name'] = name
+                for metadata_name, metadata_value in metadata.items():
+                    row[metadata_name] = metadata_value
+
+                for field in data:
+                    if field == 'timeStamp':
+                        continue
+                    row[field] = data[field]
+                retval.append(row)
+    return retval
