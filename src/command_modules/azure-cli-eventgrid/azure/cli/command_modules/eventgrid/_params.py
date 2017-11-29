7916715a8919e25b6fbefc5a012e73dfd282c5d6
Derek Bekoe <debekoe@microsoft.com>
2017-11-29 14:56:11 -0800

    Use argtype kwarg

--- src/command_modules/azure-cli-eventgrid/azure/cli/command_modules/eventgrid/_params.py
+++ src/command_modules/azure-cli-eventgrid/azure/cli/command_modules/eventgrid/_params.py
@@ -29,12 +29,12 @@ labels_type = CLIArgumentType(
 
 def load_arguments(self, _):
     with self.argument_context('eventgrid') as c:
-        c.argument('resource_group_name', resource_group_name_type)
-        c.argument('location', get_location_type(self.cli_ctx))
-        c.argument('tags', tags_type)
-        c.argument('included_event_types', included_event_types_type)
-        c.argument('labels', labels_type)
-        c.argument('endpoint_type', get_enum_type(['webhook', 'eventhub'], default='webhook'))
+        c.argument('resource_group_name', arg_type=resource_group_name_type)
+        c.argument('location', arg_type=get_location_type(self.cli_ctx))
+        c.argument('tags', arg_type=tags_type)
+        c.argument('included_event_types', arg_type=included_event_types_type)
+        c.argument('labels', arg_type=labels_type)
+        c.argument('endpoint_type', arg_type=get_enum_type(['webhook', 'eventhub'], default='webhook'))
         c.argument('provider_namespace', help="Namespace of the provider owning the resource.")
         c.argument('resource_type', help="Type of the resource.")
         c.argument('resource_name', help="Name of the resource whose event subscription needs to be managed.")
@@ -43,18 +43,18 @@ def load_arguments(self, _):
         c.argument('subject_begins_with', help="An optional string to filter events for an event subscription based on a prefix. Wildcard characters are not supported.")
         c.argument('subject_ends_with', help="An optional string to filter events for an event subscription based on a suffix. Wildcard characters are not supported.")
         c.argument('topic_type_name', help="Name of the topic type.")
-        c.argument('is_subject_case_sensitive', get_three_state_flag(), options_list=['--subject-case-sensitive'], help="Specify to indicate whether the subject fields should be compared in a case sensitive manner. True if flag present.", )
+        c.argument('is_subject_case_sensitive', arg_type=get_three_state_flag(), options_list=['--subject-case-sensitive'], help="Specify to indicate whether the subject fields should be compared in a case sensitive manner. True if flag present.", )
 
     with self.argument_context('eventgrid topic') as c:
-        c.argument('topic_name', name_type, help='Name of the topic', id_part='name')
+        c.argument('topic_name', arg_type=name_type, help='Name of the topic', id_part='name')
 
     with self.argument_context('eventgrid topic event-subscription') as c:
         c.argument('topic_name', options_list=['--topic-name'])
-        c.argument('event_subscription_name', name_type, help='Name of the event subscription')
+        c.argument('event_subscription_name', arg_type=name_type, help='Name of the event subscription')
     with self.argument_context('eventgrid event-subscription') as c:
-        c.argument('event_subscription_name', name_type, help='Name of the event subscription')
+        c.argument('event_subscription_name', arg_type=name_type, help='Name of the event subscription')
     with self.argument_context('eventgrid resource event-subscription') as c:
-        c.argument('event_subscription_name', name_type, help='Name of the event subscription')
+        c.argument('event_subscription_name', arg_type=name_type, help='Name of the event subscription')
 
     with self.argument_context('eventgrid topic-type') as c:
-        c.argument('topic_type_name', name_type, help="Name of the topic type.")
+        c.argument('topic_type_name', arg_type=name_type, help="Name of the topic type.")
