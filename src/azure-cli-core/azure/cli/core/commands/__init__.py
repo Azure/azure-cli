62b87202aba37de038d9197b60171503ffd6bc24
Troy Dai <troy.dai@outlook.com>
2017-12-04 11:35:17 -0800

    Update azdev to list commands information

--- src/azure-cli-core/azure/cli/core/commands/__init__.py
+++ src/azure-cli-core/azure/cli/core/commands/__init__.py
@@ -605,6 +605,9 @@ class CliCommandType(object):
         self.settings = {}
         self.update(overrides, **kwargs)
 
+    def __repr__(self):
+        return str(vars(self))
+
     def update(self, other=None, **kwargs):
         if other:
             self.settings.update(**other.settings)
--- tools/automation/verify/verify_commands.py
+++ tools/automation/verify/verify_commands.py
@@ -11,10 +11,14 @@ import subprocess
 
 def init(root):
     parser = root.add_parser('commands', help='Run all the commands with "-h" parameter.')
+    parser.add_argument('--prefix', dest='prefix',
+                        help='Select the commands with the given prefix. If omit select all the commands.')
+    parser.add_argument('--list-only', dest='list_only', action='store_true',
+                        help='List the commands\' information instead of execute them in separate processes.')
     parser.set_defaults(func=run_commands)
 
 
-def run_commands(_):
+def run_commands(args):
     from knack.cli import CLI
     from azure.cli.core import MainCommandsLoader, logger
     from azure.cli.core._config import GLOBAL_CONFIG_DIR, ENV_VAR_PREFIX
@@ -32,6 +36,15 @@ def run_commands(_):
 
     loader = MainCommandsLoader(MockCLI())
     table = loader.load_command_table('')
+
+    if args.prefix:
+        table = dict((each, table[each]) for each in table if each.startswith(args.prefix))
+
+    if args.list_only:
+        for each in sorted(table):
+            print_command_info(table[each])
+        sys.exit(0)
+
     commands = ['az {} --help'.format(each) for each in table]
     results = []
 
@@ -48,6 +61,31 @@ def run_commands(_):
         sys.exit(1)
 
     print('Done')
+
+
+def print_command_info(command):
+    from tabulate import tabulate
+
+    command.load_arguments()
+
+    print('\n{}\n'.format(command.name.upper()))
+
+    attributes = []
+    for attr in dir(command):
+        if attr.startswith('__'):
+            continue
+
+        value = getattr(command, attr)
+        if isinstance(value, dict):
+            attributes.append({'name': '{}'.format(attr), 'value': ''})
+            for k in value:
+                attributes.append({'name': '...{}'.format(str(k)), 'value': value[k]})
+        else:
+            value = str(value)
+            attributes.append({'name': attr, 'value': value})
+
+    print(tabulate(attributes, tablefmt='plain'))
+    print('<<<<')
 
 
 def run_single_command(command):
