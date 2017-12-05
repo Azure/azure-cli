954c645e437952fbce7e474b1f4e14ce577031b0
williexu <wx44@cornell.edu>
2017-12-05 15:22:18 -0800

    finished knack conversion for reservations

--- src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/_client_factory.py
+++ src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/_client_factory.py
@@ -4,15 +4,15 @@
 # --------------------------------------------------------------------------------------------
 
 
-def cf_reservations(**_):
+def cf_reservations(cli_ctx, **_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.mgmt.reservations.azure_reservation_api import AzureReservationAPI
-    return get_mgmt_service_client(AzureReservationAPI, subscription_bound=False)
+    return get_mgmt_service_client(cli_ctx, AzureReservationAPI, subscription_bound=False)
 
 
-def reservation_mgmt_client_factory(kwargs):
-    return cf_reservations(**kwargs).reservation
+def reservation_mgmt_client_factory(cli_ctx, kwargs):
+    return cf_reservations(cli_ctx, **kwargs).reservation
 
 
-def reservation_order_mgmt_client_factory(kwargs):
-    return cf_reservations(**kwargs).reservation_order
+def reservation_order_mgmt_client_factory(cli_ctx, kwargs):
+    return cf_reservations(cli_ctx, **kwargs).reservation_order
