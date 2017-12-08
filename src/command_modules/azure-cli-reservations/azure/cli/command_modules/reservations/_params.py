6e5ac6d3730a963aa57502089f15c4cda76f56e4
Willie Xu <wx44@cornell.edu>
2017-12-08 00:57:42 -0800

    rm uneccessary required true kwargs

--- src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/_params.py
+++ src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/_params.py
@@ -7,13 +7,13 @@
 # pylint: disable=line-too-long
 def load_arguments(self, _):
     with self.argument_context('reservations reservation update') as c:
-        c.argument('applied_scope_type', options_list=['--applied-scope-type', '-t'], required=True)
+        c.argument('applied_scope_type', options_list=['--applied-scope-type', '-t'])
         c.argument('applied_scopes', options_list=['--applied-scopes', '-s'])
 
     with self.argument_context('reservations reservation split') as c:
-        c.argument('quantity_1', options_list=['--quantity-1', '-1'], required=True)
-        c.argument('quantity_2', options_list=['--quantity-2', '-2'], required=True)
+        c.argument('quantity_1', options_list=['--quantity-1', '-1'])
+        c.argument('quantity_2', options_list=['--quantity-2', '-2'])
 
     with self.argument_context('reservations reservation merge') as c:
-        c.argument('reservation_id_1', options_list=['--reservation-id-1', '-1'], required=True)
-        c.argument('reservation_id_2', options_list=['--reservation-id-2', '-2'], required=True)
+        c.argument('reservation_id_1', options_list=['--reservation-id-1', '-1'])
+        c.argument('reservation_id_2', options_list=['--reservation-id-2', '-2'])
