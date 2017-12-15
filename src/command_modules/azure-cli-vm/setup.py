199c3877eda4bfa4e19ee7f32881e8174a42b705
Travis Prescott <tjprescott@users.noreply.github.com>
2017-12-15 07:15:51 -0800

    [Network] PublicIP Tags and Network API 2017-11-01 (#5104)
    * Public IP tags and re-record tests.
    
    * Re-recording tests!!!! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/command_modules/azure-cli-vm/setup.py
+++ src/command_modules/azure-cli-vm/setup.py
@@ -36,7 +36,7 @@ DEPENDENCIES = [
     'azure-mgmt-compute==3.1.0rc2',
     'azure-mgmt-keyvault==0.40.0',
     'azure-keyvault==0.3.7',
-    'azure-mgmt-network==1.6.0',
+    'azure-mgmt-network==1.7.0',
     'azure-mgmt-resource==1.2.1',
     'azure-multiapi-storage==0.1.7',
     'azure-cli-core'
