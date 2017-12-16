210fe8ce025e676f0c086bac2ec893ceda6e8ca5
wilco <wilcob@microsoft.com>
2017-12-15 17:20:21 -0800

    Code style fix.

--- src/command_modules/azure-cli-subscription/azure/cli/command_modules/subscription/_client_factory.py
+++ src/command_modules/azure-cli-subscription/azure/cli/command_modules/subscription/_client_factory.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
 def cf_subscription_definitions(_):
     from azure.cli.core.commands.client_factory import _get_mgmt_service_client
     from azure.mgmt.subscription import SubscriptionClient
