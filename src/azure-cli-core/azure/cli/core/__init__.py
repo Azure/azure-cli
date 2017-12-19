a45d9dcb761d2fc045d20a21907963fa6f5b13ed
Derek Bekoe <debekoe@microsoft.com>
2017-12-19 14:53:54 -0800

    Update versions for CI

--- src/azure-cli-core/azure/cli/core/__init__.py
+++ src/azure-cli-core/azure/cli/core/__init__.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------
 from __future__ import print_function
 
-__version__ = "2.0.22"
+__version__ = "2.0.24"
 
 import os
 import sys
