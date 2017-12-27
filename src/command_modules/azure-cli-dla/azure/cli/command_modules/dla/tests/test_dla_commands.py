cfe396a497da7c79cc62db6a2a30a2ae865693df
Travis Prescott <trpresco@microsoft.com>
2017-12-27 11:36:45 -0600

    Fix DLA recording processor test issue.

--- src/command_modules/azure-cli-dla/azure/cli/command_modules/dla/tests/test_dla_commands.py
+++ src/command_modules/azure-cli-dla/azure/cli/command_modules/dla/tests/test_dla_commands.py
@@ -13,46 +13,14 @@ except ImportError:
 
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 
-from azure_devtools.scenario_tests import RecordingProcessor, mock_in_unit_test
-
 from knack.util import CLIError
-
-
-MOCK_JOB_ID = '00000000-0000-0000-0000-000000000000'
-
-
-class JobIdReplacer(RecordingProcessor):
-
-    """Replace the random job id with a fixed mock name."""
-    def process_request(self, request):
-        import re
-        request.uri = re.sub('/Jobs/([^/?]+)', '/Jobs/{}'.format(MOCK_JOB_ID), request.uri)
-        return request
-
-    def process_response(self, response):
-        if response['body']['string']:
-            response['body']['string'] = self._replace_job_id(response['body']['string'])
-        return response
-
-    def _replace_job_id(self, val):
-        import re
-        if 'jobId' in val:
-            return re.sub(r'"jobId":"([^"]+)"', r'"jobId":"{}"'.format(MOCK_JOB_ID), val, flags=re.IGNORECASE)
-        return val
-
-
-def patch_uuid_str(unit_test):
-
-    def _mock_get_uuid_str(*args, **kwargs):  # pylint: disable=unused-argument
-        return MOCK_JOB_ID
-
-    mock_in_unit_test(unit_test, 'azure.cli.command_modules.dla.custom._get_uuid_str', _mock_get_uuid_str)
 
 
 # pylint: disable=too-many-instance-attributes
 class DataLakeAnalyticsScenarioTest(ScenarioTest):
 
     def __init__(self, method_name):
+        from azure.cli.command_modules.dla.tests.recording_processors import JobIdReplacer, patch_uuid_str
         return super(DataLakeAnalyticsScenarioTest, self).__init__(method_name,
                                                                    recording_processors=JobIdReplacer(),
                                                                    replay_patches=patch_uuid_str)
