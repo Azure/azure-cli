4b9c11a1e8c580ed224fcbde77ce90bf752efecb
Troy Dai <troy.dai@outlook.com>
2018-01-02 23:28:19 -0800

    Update the droid build script
    1. Support build droid image locally and publish end to end
    2. Add script assisting login Azure Container Registry
    3. Add a travis yaml prototype for building droid container.
    4. Update the py36 dockerfile to derive from a stable base image
    5. Update the version.sh to support timestamp as version.

--- scripts/ci/build_droid.sh
+++ scripts/ci/build_droid.sh
@@ -4,18 +4,30 @@ set -e
 
 dp0=`cd $(dirname $0); pwd`
 
+# Clean up artifacts
+if [ -d artifacts ]; then rm -r artifacts; fi
+
 # Build the whl files first
 . $dp0/build.sh
 
 # Move dockerfile
 cp $dp0/a01/Dockerfile.py36 artifacts/
 
-image_name=azurecli-test:python3.6-$version
+# for travis repo slug, remove the suffix to reveal the owner
+# - the offical repo will generate image: azurecli-test-Azure
+# - the fork repo will generate image: azurecli-test-johnongithub
+# for local private build uses local user name.
+# - eg. azurecli-test-private-john
+image_owner=${TRAVIS_REPO_SLUG%/azure-cli} 
+image_owner=${image_owner:="private-${USER}"}
+
+image_name=azurecli-test-$image_owner:python3.6-$version
+
 docker build -t $image_name -f artifacts/Dockerfile.py36 artifacts
 
-if [ $A01_ACR ]; then
-    az acr login -n $A01_ACR
-    docker tag $image_name $A01_ACR.azurecr.io/$image_name
-    docker push $A01_ACR.azurecr.io/$image_name
+if [ $AZURE_CLI_BUILD_CR ]; then
+    if [ -z $TRAVIS ]; then $dp0/login_cr.sh; fi
+    docker tag $image_name $AZURE_CLI_BUILD_CR/$image_name
+    docker push $AZURE_CLI_BUILD_CR/$image_name
 fi
 
