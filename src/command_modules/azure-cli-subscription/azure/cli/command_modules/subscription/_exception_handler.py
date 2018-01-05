86167031deb9632326c4e0751a492677da599e68
wilco <wilcob@microsoft.com>
2018-01-04 18:26:50 -0800

    Add Subscription RP commands + tests.

---
+++ src/command_modules/azure-cli-subscription/azure/cli/command_modules/subscription/_exception_handler.py
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core.util import CLIError
+
+
+def subscription_definition_exception_handler(ex):
+    from azure.mgmt.subscription.models import ErrorResponseException
+    if isinstance(ex, ErrorResponseException):
+        message = ex.error.error.message
+        raise CLIError(message)
+    else:
+        import sys
+        from sys import reraise
+        reraise(*sys.exc_info())
