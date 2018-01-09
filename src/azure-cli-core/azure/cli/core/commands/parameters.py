f189dd0d9d6b9bea8c39176c9912675f8d0d97a7
Travis Prescott <trpresco@microsoft.com>
2018-01-09 09:40:41 -0800

    Fix #5245.

--- src/azure-cli-core/azure/cli/core/commands/parameters.py
+++ src/azure-cli-core/azure/cli/core/commands/parameters.py
@@ -68,9 +68,11 @@ def get_resource_group_completion_list(cmd, prefix, namespace, **kwargs):  # pyl
 
 def get_resources_in_resource_group(cli_ctx, resource_group_name, resource_type=None):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.cli.core.profiles import supported_api_version
+
     rcf = get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
     filter_str = "resourceType eq '{}'".format(resource_type) if resource_type else None
-    if cli_ctx.cloud.supported_api_version(ResourceType.MGMT_RESOURCE_RESOURCES, max_api='2016-09-01'):
+    if supported_api_version(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES, max_api='2016-09-01'):
         return list(rcf.resource_groups.list_resources(resource_group_name, filter=filter_str))
     return list(rcf.resources.list_by_resource_group(resource_group_name, filter=filter_str))
 
