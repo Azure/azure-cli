20e4a1ab857555d3bd3377492b207b96b730bf63
yugangw-msft <yugangw@microsoft.com>
2018-01-17 16:34:53 -0800

    update history

--- src/command_modules/azure-cli-appservice/setup.py
+++ src/command_modules/azure-cli-appservice/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.24"
+VERSION = "0.1.25"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
