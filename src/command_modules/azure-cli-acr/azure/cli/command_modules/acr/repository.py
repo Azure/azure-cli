55cdf5f9c5dd7c637bf7da9a9bbe6ad8b2e12d1e
Troy Dai <troy.dai@outlook.com>
2018-01-23 15:49:43 -0800

    Fix telemetry issue (#5382)

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/repository.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/repository.py
@@ -79,7 +79,7 @@ def _delete_data_from_registry(login_server, path, username, password, retry_tim
         raise CLIError(errorMessage)
 
 
-def _get_manifest_digest(login_server, path, username, password, retry_times=3, retry_interval=5):
+def _get_manifest_digest(login_server, path, username, password, retry_times=3, retry_interval=5):  # pylint: disable=inconsistent-return-statements
     for i in range(0, retry_times):
         errorMessage = None
         try:
@@ -104,6 +104,7 @@ def _get_manifest_digest(login_server, path, username, password, retry_times=3, 
             errorMessage = str(e)
             logger.debug('Retrying %s with exception %s', i + 1, errorMessage)
             time.sleep(retry_interval)
+
     if errorMessage:
         raise CLIError(errorMessage)
 
