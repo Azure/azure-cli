b8ebb5fec114ffb2950ad78fcd08b5022ef81080
Digvijay Makwana <digvijaymakwana@gmail.com>
2018-01-24 09:07:40 -0800

    Fixed parameter description for failover policies (#5367)
    * fixed parameter description for failover policies
    
    * fixed failover parameter for locations parameter
    
    * update history.rst
    
    * updated version in setup.py

--- src/command_modules/azure-cli-cosmosdb/azure/cli/command_modules/cosmosdb/_params.py
+++ src/command_modules/azure-cli-cosmosdb/azure/cli/command_modules/cosmosdb/_params.py
@@ -26,7 +26,7 @@ def load_arguments(self, _):
         with self.argument_context(scope) as c:
             c.argument('account_name', completer=None)
             c.ignore('resource_group_location')
-            c.argument('locations', nargs='+', validator=validate_locations, help="space separated locations in 'regionName=failoverPriority' format. E.g \"East US\"=0 \"West US\"=1. Failover priority values are 0 for write regions and greater than 0 for read regions. A failover priority value must be unique and less than the total number of regions. Default: single region account in the location of the specified resource group.")
+            c.argument('locations', nargs='+', validator=validate_locations, help="space separated locations in 'regionName=failoverPriority' format. E.g eastus=0 westus=1. Failover priority values are 0 for write regions and greater than 0 for read regions. A failover priority value must be unique and less than the total number of regions. Default: single region account in the location of the specified resource group.")
             c.argument('default_consistency_level', arg_type=get_enum_type(DefaultConsistencyLevel), help="default consistency level of the Cosmos DB database account")
             c.argument('max_staleness_prefix', type=int, help="when used with Bounded Staleness consistency, this value represents the number of stale requests tolerated. Accepted range for this value is 1 - 2,147,483,647")
             c.argument('max_interval', type=int, help="when used with Bounded Staleness consistency, this value represents the time amount of staleness (in seconds) tolerated. Accepted range for this value is 1 - 100")
@@ -38,7 +38,7 @@ def load_arguments(self, _):
         c.argument('key_kind', arg_type=get_enum_type(KeyKind))
 
     with self.argument_context('cosmosdb failover-priority-change') as c:
-        c.argument('failover_policies', validator=validate_failover_policies, help="space separated failover policies in 'regionName=failoverPriority' format. E.g \"East US\"=0 \"West US\"=1", nargs='+')
+        c.argument('failover_policies', validator=validate_failover_policies, help="space separated failover policies in 'regionName=failoverPriority' format. E.g eastus=0 westus=1", nargs='+')
 
     with self.argument_context('cosmosdb collection') as c:
         c.argument('collection_id', options_list=['--collection-name', '-c'], help='Collection Name')
