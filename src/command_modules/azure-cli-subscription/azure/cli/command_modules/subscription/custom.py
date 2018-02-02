3b9e493785e2da75e04ae5b8c83e255c891981e0
wilco <wilcob@microsoft.com>
2018-02-02 14:51:40 -0800

    Fix how we handle subscription_definition_name param.

--- src/command_modules/azure-cli-subscription/azure/cli/command_modules/subscription/custom.py
+++ src/command_modules/azure-cli-subscription/azure/cli/command_modules/subscription/custom.py
@@ -6,9 +6,9 @@
 from azure.mgmt.subscription.models import SubscriptionDefinition
 
 
-def cli_subscription_create_subscription_definition(client, name, offer_type, subscription_display_name=None):
+def cli_subscription_create_subscription_definition(client, subscription_definition_name, offer_type, subscription_display_name=None):
     new_def = SubscriptionDefinition(
-        subscription_display_name=subscription_display_name if subscription_display_name else name,
+        subscription_display_name=subscription_display_name if subscription_display_name else subscription_definition_name,
         offer_type=offer_type)
 
-    return client.create(name, new_def)
+    return client.create(subscription_definition_name, new_def)
