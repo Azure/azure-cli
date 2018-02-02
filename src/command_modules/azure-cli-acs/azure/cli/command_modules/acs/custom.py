0ff5d7001e426f76c525664674e1de341a4a2e9b
Matt Boersma <Matt.Boersma@microsoft.com>
2018-02-02 14:53:40 -0800

    [AKS] fix `az aks browse` if dashboard isn't prefixed by "pods/" (#5462)
    * [AKS] fix `az aks browse` if dashboard isn't prefixed by "pods/"
    
    * Update HISTORY.rst

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
@@ -1236,8 +1236,8 @@ def aks_browse(cmd, client, resource_group_name, name, disable_browser=False):
     except subprocess.CalledProcessError as err:
         raise CLIError('Could not find dashboard pod: {}'.format(err))
     if dashboard_pod:
-        # remove the "pods/" prefix from the name
-        dashboard_pod = str(dashboard_pod)[5:].strip()
+        # remove any "pods/" or "pod/" prefix from the name
+        dashboard_pod = str(dashboard_pod).split('/')[-1].strip()
     else:
         raise CLIError("Couldn't find the Kubernetes dashboard pod.")
     # launch kubectl port-forward locally to access the remote dashboard
