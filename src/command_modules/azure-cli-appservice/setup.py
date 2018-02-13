743bd788800bc32910d055c57640d8920ac5bd0e
Sisira Panchagnula <panchagnula@users.noreply.github.com>
2018-02-13 13:06:44 -0800

    Fixes#5538: list_locations supports 'Free' and 'shared' as valid parameters (#5552)

--- src/command_modules/azure-cli-appservice/setup.py
+++ src/command_modules/azure-cli-appservice/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.26"
+VERSION = "0.1.27"
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
