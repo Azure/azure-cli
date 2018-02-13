c0e43ce251e89fc604013f43b7e78cda565be64d
Shaheen Bhatia <shbha@microsoft.com>
2018-02-12 16:39:00 -0800

    updated query to limit the response size.

--- src/command_modules/azure-cli-consumption/azure/cli/command_modules/consumption/tests/latest/test_consumption_commands.py
+++ src/command_modules/azure-cli-consumption/azure/cli/command_modules/consumption/tests/latest/test_consumption_commands.py
@@ -26,35 +26,6 @@ class AzureConsumptionServiceScenarioTest(ScenarioTest):
         else:
             self.assertIsNone(usage['meterDetails'])
 
-    def _validate_reservation_summaries(self, reservationsummaries):
-        self.assertIsNotNone(reservationsummaries)
-        self.assertTrue(reservationsummaries['id'])
-        self.assertTrue(reservationsummaries['name'])
-        self.assertEqual(reservationsummaries['type'], 'Microsoft.Consumption/reservationSummaries')
-        self.assertTrue(reservationsummaries['avgUtilizationPercentage'])
-        self.assertTrue(reservationsummaries['maxUtilizationPercentage'])
-        self.assertTrue(reservationsummaries['minUtilizationPercentage'])
-        self.assertTrue(reservationsummaries['reservationId'])
-        self.assertTrue(reservationsummaries['reservationOrderId'])
-        self.assertTrue(reservationsummaries['reservedHours'])
-        self.assertTrue(reservationsummaries['skuName'])
-        self.assertTrue(reservationsummaries['usageDate'])
-        self.assertTrue(reservationsummaries['usedHours'])
-
-    def _validate_reservation_details(self, reservationdetails):
-        self.assertIsNotNone(reservationdetails)
-        self.assertTrue(reservationdetails['id'])
-        self.assertTrue(reservationdetails['name'])
-        self.assertEqual(reservationdetails['type'], 'Microsoft.Consumption/reservationDetails')
-        self.assertTrue(reservationdetails['instanceId'])
-        self.assertTrue(reservationdetails['reservationId'])
-        self.assertTrue(reservationdetails['reservationOrderId'])
-        self.assertTrue(reservationdetails['reservedHours'])
-        self.assertTrue(reservationdetails['skuName'])
-        self.assertTrue(reservationdetails['totalReservedQuantity'])
-        self.assertTrue(reservationdetails['usageDate'])
-        self.assertTrue(reservationdetails['usedHours'])
-
     def _validate_pricesheet(self, pricesheet, includeMeterDetails=False):
         self.assertIsNotNone(pricesheet)
         self.assertEqual(pricesheet['type'], 'Microsoft.Consumption/pricesheets')
@@ -86,73 +57,12 @@ class AzureConsumptionServiceScenarioTest(ScenarioTest):
         self.assertTrue(len(usages_list) <= 5)
         self._validate_usage(usages_list[0], False)
 
-    def test_consumption_usage_list_properties(self):
-        usages_list = self.cmd('consumption usage list -t 5 -a').get_output_in_json()
-        self.assertTrue(usages_list)
-        self.assertTrue(len(usages_list) <= 5)
-        self._validate_usage(usages_list[0], False)
-
-    def test_consumption_usage_by_billing_period(self):
-        usages_list = self.cmd('consumption usage billing period list -p 201710 -t 5').get_output_in_json()
-        self.assertTrue(usages_list)
-        self.assertTrue(len(usages_list) <= 5)
-        self._validate_usage(usages_list[0], False)
-
-    def test_consumption_usage_list_expand(self):
-        usages_list = self.cmd('consumption usage list -a -m -t 5').get_output_in_json()
-        self.assertTrue(usages_list)
-        self.assertTrue(len(usages_list) <= 5)
-        self._validate_usage(usages_list[0], True)
-
-    def test_consumption_usage_by_billing_period_expand(self):
-        usages_list = self.cmd('consumption usage billing period list -p 201710 -a -m -t 5').get_output_in_json()
-        self.assertTrue(usages_list)
-        self.assertTrue(len(usages_list) <= 5)
-        self._validate_usage(usages_list[0], True)
-
     def test_list_usages_subscription_custom_date_range(self):
         self.kwargs.update({
-            'start_date': '2017-10-11T23:59:59Z',
-            'end_date': '2017-10-12T03:59:59Z'
+            'start_date': '2017-09-11T23:59:59Z',
+            'end_date': '2017-09-12T03:59:59Z'
         })
 
-        usages_list = self.cmd('consumption usage list -s {start_date} -e {end_date}').get_output_in_json()
+        usages_list = self.cmd('consumption usage list -s {start_date} -e {end_date} -t 5').get_output_in_json()
         self.assertTrue(usages_list)
 
-        def usage_date_check(usage_date):
-            check_after = datetime.strptime(usage_date['usageEnd'], "%Y-%m-%dT%H:%M:%SZ") >= \
-                datetime.strptime('2017-10-11T23:59:59Z', "%Y-%m-%dT%H:%M:%SZ")
-            check_before = datetime.strptime(usage_date['usageEnd'], "%Y-%m-%dT%H:%M:%SZ") <= \
-                datetime.strptime('2017-10-12T23:59:59Z', "%Y-%m-%dT%H:%M:%SZ")
-            return check_after and check_before
-        self.assertTrue(all(usage_date_check(usage_date) for usage_date in usages_list))
-
-    def test_list_reservations_summaries_monthly(self):
-        reservations_summaries_monthly_list = self.cmd('consumption reservations summaries list -g ''monthly'' -r ca69259e-bd4f-45c3-bf28-3f353f9cce9b').get_output_in_json()
-        self.assertTrue(reservations_summaries_monthly_list)
-        self._validate_reservation_summaries(reservations_summaries_monthly_list[0])
-
-    def test_list_reservations_summaries_monthly_with_reservationid(self):
-        reservations_summaries_monthly_withreservationid_list = self.cmd('consumption reservations summaries reservation id list -g ''monthly'' -r ca69259e-bd4f-45c3-bf28-3f353f9cce9b -i f37f4b70-52ba-4344-a8bd-28abfd21d640').get_output_in_json()
-        self.assertTrue(reservations_summaries_monthly_withreservationid_list)
-        self._validate_reservation_summaries(reservations_summaries_monthly_withreservationid_list[0])
-
-    def test_list_reservations_summaries_daily(self):
-        reservations_summaries_daily_list = self.cmd('consumption reservations summaries list -g ''daily'' -r ca69259e-bd4f-45c3-bf28-3f353f9cce9b -s ''2017-12-01'' -e ''2017-12-07''').get_output_in_json()
-        self.assertTrue(reservations_summaries_daily_list)
-        self._validate_reservation_summaries(reservations_summaries_daily_list[0])
-
-    def test_list_reservations_summaries_daily_with_reservationid(self):
-        reservations_summaries_daily_withreservationid_list = self.cmd('consumption reservations summaries reservation id list -g ''daily'' -r ca69259e-bd4f-45c3-bf28-3f353f9cce9b -i f37f4b70-52ba-4344-a8bd-28abfd21d640 -s ''2017-12-01'' -e ''2017-12-07''').get_output_in_json()
-        self.assertTrue(reservations_summaries_daily_withreservationid_list)
-        self._validate_reservation_summaries(reservations_summaries_daily_withreservationid_list[0])
-
-    def test_list_reservations_details(self):
-        reservations_details_list = self.cmd('consumption reservations details list -r ca69259e-bd4f-45c3-bf28-3f353f9cce9b -s ''2017-12-01'' -e ''2017-12-07''').get_output_in_json()
-        self.assertTrue(reservations_details_list)
-        self._validate_reservation_details(reservations_details_list[0])
-
-    def test_list_reservations_details_with_reservationid(self):
-        reservations_details_list = self.cmd('consumption reservations details reservation id list -r ca69259e-bd4f-45c3-bf28-3f353f9cce9b -i f37f4b70-52ba-4344-a8bd-28abfd21d640 -s ''2017-12-01'' -e ''2017-12-07''').get_output_in_json()
-        self.assertTrue(reservations_details_list)
-        self._validate_reservation_details(reservations_details_list[0])
