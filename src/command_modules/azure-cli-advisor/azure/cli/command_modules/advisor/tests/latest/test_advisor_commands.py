6e17e92e2cab995d28b191f83ba741e7ed1ef7ca
Prasanna-Padmanabhan <31525198+Prasanna-Padmanabhan@users.noreply.github.com>
2018-02-16 09:01:23 -0800

    Fix indentation errors.

--- src/command_modules/azure-cli-advisor/azure/cli/command_modules/advisor/tests/latest/test_advisor_commands.py
+++ src/command_modules/azure-cli-advisor/azure/cli/command_modules/advisor/tests/latest/test_advisor_commands.py
@@ -23,14 +23,14 @@ class AzureAdvisorUnitTest(unittest.TestCase):
 
         self.assertEqual(
             _cli_advisor_build_filter_string(),
-             None)
+            None)
         self.assertEqual(
             _cli_advisor_build_filter_string(ids=ids),
             "ResourceId eq 'a' or ResourceId eq 'b' or ResourceId eq 'c'")
         self.assertEqual(
             _cli_advisor_build_filter_string(
                 resource_group_name=resource_group_name),
-             "ResourceGroup eq 'r'")
+            "ResourceGroup eq 'r'")
         self.assertEqual(
             _cli_advisor_build_filter_string(
                 category=category),
@@ -48,21 +48,21 @@ class AzureAdvisorUnitTest(unittest.TestCase):
 
     def test__cli_advisor_parse_operation_id(self):
         location = ("https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/"
-                   "providers/Microsoft.Advisor/generateRecommendations/a9544ca5-5837-4cb4-94d6-bad2b6e76320"
-                   "?api-version=2017-04-19")
+                    "providers/Microsoft.Advisor/generateRecommendations/a9544ca5-5837-4cb4-94d6-bad2b6e76320"
+                    "?api-version=2017-04-19")
         operation_id = _cli_advisor_parse_operation_id(location)
         self.assertEqual(operation_id, 'a9544ca5-5837-4cb4-94d6-bad2b6e76320')
 
     def test__cli_advisor_parse_recommendation_uri(self):
         recommendation_uri = ("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo"
-                             "/providers/Microsoft.Network/expressRouteCircuits/test/"
-                             "providers/Microsoft.Advisor/recommendations/c4deb869-ea38-f90d-331f-91770021d425"
-                             "/suppressions/5c9c3fce-c1b2-7e45-106c-152ce3c04be5")
+                              "/providers/Microsoft.Network/expressRouteCircuits/test/"
+                              "providers/Microsoft.Advisor/recommendations/c4deb869-ea38-f90d-331f-91770021d425"
+                              "/suppressions/5c9c3fce-c1b2-7e45-106c-152ce3c04be5")
         result = _cli_advisor_parse_recommendation_uri(recommendation_uri)
         self.assertEqual(
             result['resourceUri'],
             ("/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/foo"
-            "/providers/Microsoft.Network/expressRouteCircuits/test"))
+             "/providers/Microsoft.Network/expressRouteCircuits/test"))
         self.assertEqual(result['recommendationId'], 'c4deb869-ea38-f90d-331f-91770021d425')
 
 
@@ -86,29 +86,29 @@ class AzureAdvisorLiveScenarioTest(LiveScenarioTest):
 
         # Disable with specified duration should create a suppression
         self.cmd('advisor recommendation disable -n {name} -g {group} --days 1',
-                 checks = [self.check("name", name),
-                           self.check("resourceGroup", group),
-                           self.exists("suppressionIds")])
+                 checks=[self.check("name", name),
+                         self.check("resourceGroup", group),
+                         self.exists("suppressionIds")])
 
         # Disable again should create another suppression
         self.cmd('advisor recommendation disable -n {name} -g {group}',
-                 checks = [self.check("name", name),
-                           self.check("resourceGroup", group),
-                           self.greater_than("length(suppressionIds)", 1)])
+                 checks=[self.check("name", name),
+                         self.check("resourceGroup", group),
+                         self.greater_than("length(suppressionIds)", 1)])
 
         # Enable should remove all suppressions
         self.cmd('advisor recommendation enable -n {name} -g {group}')
         self.cmd('advisor recommendation list --ids {ids}',
-                 checks = [self.check("[0].name", name),
-                           self.check("[0].resourceGroup", group),
-                           self.check("[0].suppressionIds", None)])
+                 checks=[self.check("[0].name", name),
+                         self.check("[0].resourceGroup", group),
+                         self.check("[0].suppressionIds", None)])
 
         # Enable again should be a no op
         self.cmd('advisor recommendation enable -n {name} -g {group}')
         self.cmd('advisor recommendation list --ids {ids}',
-                 checks = [self.check("[0].name", name),
-                           self.check("[0].resourceGroup", group),
-                           self.check("[0].suppressionIds", None)])
+                 checks=[self.check("[0].name", name),
+                         self.check("[0].resourceGroup", group),
+                         self.check("[0].suppressionIds", None)])
 
         # List should return at least one recommendation without any filters
         self.cmd('advisor recommendation list --refresh',
@@ -122,45 +122,45 @@ class AzureAdvisorScenarioTest(ScenarioTest):
 
         # Show should always return a default
         self.cmd('advisor configuration show',
-                 checks = [self.check("name", subscriptionId)])
+                 checks=[self.check("name", subscriptionId)])
 
         # Show should reflect the changes made by Update
         self.cmd('advisor configuration update --low-cpu-threshold 15 --exclude')
         self.cmd('advisor configuration show',
-                 checks = [self.check("name", subscriptionId),
-                           self.check("properties.lowCpuThreshold", "15"),
-                           self.check("properties.exclude", True)])
+                 checks=[self.check("name", subscriptionId),
+                         self.check("properties.lowCpuThreshold", "15"),
+                         self.check("properties.exclude", True)])
 
         # Show should reflect the changes made by Update
         self.cmd('advisor configuration update --low-cpu-threshold 5 --include')
         self.cmd('advisor configuration show',
-                 checks = [self.check("name", subscriptionId),
-                           self.check("properties.lowCpuThreshold", "5"),
-                           self.check("properties.exclude", False)])
+                 checks=[self.check("name", subscriptionId),
+                         self.check("properties.lowCpuThreshold", "5"),
+                         self.check("properties.exclude", False)])
 
         # List should reflect the changes made by Update
         self.cmd('advisor configuration list',
-                 checks = [self.check("[0].name", subscriptionId),
-                           self.check("[0].properties.lowCpuThreshold", "5"),
-                           self.check("[0].properties.exclude", False)])
+                 checks=[self.check("[0].name", subscriptionId),
+                         self.check("[0].properties.lowCpuThreshold", "5"),
+                         self.check("[0].properties.exclude", False)])
 
     @ResourceGroupPreparer(name_prefix='cli_test_advisor')
     def test_configurations_subscription(self, resource_group):
         # Show should always return a default even for a brand new resource group
         self.cmd('advisor configuration show -g {rg}',
-                 checks = [self.check("resourceGroup", resource_group)])
+                 checks=[self.check("resourceGroup", resource_group)])
 
         # Show should reflect the changes made by Update
         self.cmd('advisor configuration update --exclude -g {rg}')
         self.cmd('advisor configuration show -g {rg}',
-                 checks = [self.check("resourceGroup", resource_group),
-                           self.check("properties.exclude", True)])
+                 checks=[self.check("resourceGroup", resource_group),
+                         self.check("properties.exclude", True)])
 
         # Show should reflect the changes made by Update
         self.cmd('advisor configuration update --include -g {rg}')
         self.cmd('advisor configuration show -g {rg}',
-                 checks = [self.check("resourceGroup", resource_group),
-                           self.check("properties.exclude", False)])
+                 checks=[self.check("resourceGroup", resource_group),
+                         self.check("properties.exclude", False)])
 
 
 if __name__ == '__main__':
