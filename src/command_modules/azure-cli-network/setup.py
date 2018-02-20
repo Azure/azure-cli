520a2694ffd73813ffd6d5c469cb2eca0af3741b
Travis Prescott <tjprescott@users.noreply.github.com>
2018-02-20 13:45:36 -0800

    Fixes #5559. Remove api_constraints and tests which don't apply to latest profile. (#5570)

--- src/command_modules/azure-cli-network/setup.py
+++ src/command_modules/azure-cli-network/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.23"
+VERSION = "2.0.24"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
