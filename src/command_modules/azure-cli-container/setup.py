bc9561cae6e9c801dd53460a1f768c28ef93fc5a
Paul Meyer <paul.meyer@outlook.com>
2018-02-20 10:15:45 -0800

    [Container]  Add secrets for container instances (#5578)
    * Add secrets for container instances
    
    Adds --secrets and --secrets-mount-path to add secrets for an Azure
    Container Instance.
    
    * Version 0.1.19
    
    * fixup! Add secrets for container instances
    
    * fixup! Version 0.1.19
    
    * fixup! Add secrets for container instances
    
    * fixup! Add secrets for container instances
    
    * Do not allow empty array of secrets

--- src/command_modules/azure-cli-container/setup.py
+++ src/command_modules/azure-cli-container/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.18"
+VERSION = "0.1.19"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
