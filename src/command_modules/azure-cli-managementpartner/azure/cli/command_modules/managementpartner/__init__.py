aa1a07fe952fd84ed721752761b45ccf100d0e96
jeffrey-ACE <jefl@Microsoft.com>
2018-02-21 18:05:37 -0800

    remove unexpected spaces

--- src/command_modules/azure-cli-managementpartner/azure/cli/command_modules/managementpartner/__init__.py
+++ src/command_modules/azure-cli-managementpartner/azure/cli/command_modules/managementpartner/__init__.py
@@ -16,9 +16,9 @@ class ManagementpartnerCommandsLoader(AzCommandsLoader):
     def __init__(self, cli_ctx=None):
         from azure.cli.core.commands import CliCommandType
         managementpartner_custom = CliCommandType(
-            operations_tmpl = 'azure.cli.command_modules.managementpartner.custom#{}',
-            client_factory = managementpartner_partner_client_factory,
-            exception_handler = managementpartner_exception_handler
+            operations_tmpl='azure.cli.command_modules.managementpartner.custom#{}',
+            client_factory=managementpartner_partner_client_factory,
+            exception_handler=managementpartner_exception_handler
         )
 
         super(ManagementpartnerCommandsLoader, self).__init__(cli_ctx=cli_ctx, custom_command_type=managementpartner_custom)
