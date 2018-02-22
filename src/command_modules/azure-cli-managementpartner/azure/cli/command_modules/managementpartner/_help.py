4446945a300598a6acef00a3bf5b1804e28489f6
jeffrey-ACE <jefl@Microsoft.com>
2018-02-22 14:28:03 -0800

    On-board Management Partner

---
+++ src/command_modules/azure-cli-managementpartner/azure/cli/command_modules/managementpartner/_help.py
@@ -0,0 +1,32 @@
+﻿# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+from knack.help_files import helps
+
+helps['managementpartner'] = """
+    type: group
+    short-summary: Allows the partners to associate a Microsoft Partner Network(MPN) ID to a user or service principal in the customer's Azure directory.
+"""
+
+helps['managementpartner create'] = """
+    type: command
+    short-summary: Associates a Microsoft Partner Network(MPN) ID to the current authenticated user or service principal.
+"""
+
+helps['managementpartner show'] = """
+    type: command
+    short-summary: Gets the Microsoft Partner Network(MPN) ID of the current authenticated user or service principal.
+"""
+
+helps['managementpartner update'] = """
+    type: command
+    short-summary: Updates the Microsoft Partner Network(MPN) ID of the current authenticated user or service principal.
+"""
+
+helps['managementpartner delete'] = """
+    type: command
+    short-summary: Delete the Microsoft Partner Network(MPN) ID of the current authenticated user or service principal.
+"""
---
+++ src/command_modules/azure-cli-managementpartner/azure/cli/command_modules/managementpartner/commands.py
@@ -0,0 +1,25 @@
+﻿# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+from azure.cli.core.commands import CliCommandType
+from azure.cli.command_modules.managementpartner._client_factory import managementpartner_partner_client_factory
+from ._exception_handler import managementpartner_exception_handler
+
+
+def load_command_table(self, _):
+    def managementpartner_type(*args, **kwargs):
+        return CliCommandType(*args, exception_handler=managementpartner_exception_handler, **kwargs)
+
+    managementpartner_partner_sdk = managementpartner_type(
+        operations_tmpl='azure.mgmt.managementpartner.operations.partner_operations#PartnerOperations.{}',
+        client_factory=managementpartner_partner_client_factory
+    )
+
+    with self.command_group('managementpartner', managementpartner_partner_sdk) as g:
+        g.command('show', 'get')
+        g.command('create', 'create')
+        g.command('update', 'update')
+        g.command('delete', 'delete')
