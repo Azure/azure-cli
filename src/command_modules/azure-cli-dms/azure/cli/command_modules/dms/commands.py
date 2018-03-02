c01cc4fd6784f4d6c0271629078a0a0d71da6826
Chuck Skelton <chskelt@microsoft.com>
2018-03-01 20:32:47 -0800

    Sync

--- src/command_modules/azure-cli-dms/azure/cli/command_modules/dms/commands.py
+++ src/command_modules/azure-cli-dms/azure/cli/command_modules/dms/commands.py
@@ -3,7 +3,23 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
+from azure.cli.command_modules.dms._client_factory import dms_client_factory
+
 from azure.cli.core.commands import CliCommandType
 
 def load_command_table(self, _):
-    pass
+    
+    dms_tasks_sdk = CliCommandType(
+        operations_tmpl='azure.mgmt.datamigration.tasks_operations#TasksOperations.{}',
+        client_factory=dms_client_factory
+    )
+
+    with self.command_group('dms task', dms_tasks_sdk) as g:
+        g.command('list', 'list')
+        g.command('create', 'create_or_update')
+        g.command('set', 'create_or_update')
+        g.command('show', 'get')
+        g.command('delete', 'delete')
+        g.command('update', 'update')
+        g.command('cancel', 'cancel')
