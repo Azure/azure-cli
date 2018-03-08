7cfdc9d5bd3dc6657b857f201ec3c7df8d2ac2c0
Yugang Wang <yugangw@microsoft.com>
2018-03-07 20:05:22 -0800

    msi: terminology update to identity (#5744)

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/commands.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/commands.py
@@ -159,8 +159,15 @@ def load_command_table(self, _):
         g.generic_update_command('update', custom_func_name='update_snapshot', setter_arg_name='snapshot')
 
     with self.command_group('vm', compute_vm_sdk) as g:
-        g.custom_command('assign-identity', 'assign_vm_identity', validator=process_assign_identity_namespace)
-        g.custom_command('remove-identity', 'remove_vm_identity', validator=process_remove_identity_namespace, min_api='2017-12-01')
+
+        g.custom_command('assign-identity', 'assign_vm_identity', validator=process_assign_identity_namespace,
+                         deprecate_info='az vm identity assign')
+        g.custom_command('remove-identity', 'remove_vm_identity', validator=process_remove_identity_namespace, min_api='2017-12-01',
+                         deprecate_info='az vm identity remove')
+        g.custom_command('identity assign', 'assign_vm_identity', validator=process_assign_identity_namespace)
+        g.custom_command('identity remove', 'remove_vm_identity', validator=process_remove_identity_namespace, min_api='2017-12-01')
+        g.custom_command('identity show', 'show_vm_identity')
+
         g.custom_command('capture', 'capture_vm')
         g.custom_command('create', 'create_vm', transform=transform_vm_create_output, supports_no_wait=True, table_transformer=deployment_validate_table_format, validator=process_vm_create_namespace)
         g.command('convert', 'convert_to_managed_disks', min_api='2016-04-30-preview')
@@ -262,8 +269,12 @@ def load_command_table(self, _):
         g.custom_command('reset-ssh', 'reset_linux_ssh')
 
     with self.command_group('vmss', compute_vmss_sdk, operation_group='virtual_machine_scale_sets') as g:
-        g.custom_command('assign-identity', 'assign_vmss_identity', validator=process_assign_identity_namespace)
-        g.custom_command('remove-identity', 'remove_vmss_identity', validator=process_remove_identity_namespace, min_api='2017-12-01')
+        g.custom_command('assign-identity', 'assign_vmss_identity', validator=process_assign_identity_namespace, deprecate_info='az vmss identity assign')
+        g.custom_command('remove-identity', 'remove_vmss_identity', validator=process_remove_identity_namespace, min_api='2017-12-01',
+                         deprecate_info='az vmss identity remove')
+        g.custom_command('identity assign', 'assign_vmss_identity', validator=process_assign_identity_namespace)
+        g.custom_command('identity remove', 'remove_vmss_identity', validator=process_remove_identity_namespace, min_api='2017-12-01')
+        g.custom_command('identity show', 'show_vmss_identity')
         g.custom_command('create', 'create_vmss', transform=DeploymentOutputLongRunningOperation(self.cli_ctx, 'Starting vmss create'), supports_no_wait=True, table_transformer=deployment_validate_table_format, validator=process_vmss_create_namespace)
         g.custom_command('deallocate', 'deallocate_vmss', supports_no_wait=True)
         g.command('delete', 'delete', supports_no_wait=True)
