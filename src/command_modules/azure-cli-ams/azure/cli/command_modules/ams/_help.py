146de69b9388953820ecb81655dabb68b818ad81
damatojuan <juanignaciodamato@gmail.com>
2018-03-09 13:30:03 -0300

    [AMS] Improve help documentation wording.

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_help.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_help.py
@@ -5,53 +5,60 @@
 
 from knack.help_files import helps
 
+# pylint: disable=line-too-long, too-many-lines
+
 helps['ams'] = """
     type: group
-    short-summary: Manage Azure Media Services.
+    short-summary: Manage Azure Media Services resources.
 """
 
 helps['ams create'] = """
     type: command
-    short-summary: Create an Azure Media Service.
+    short-summary: Create an Azure Media Services account.
 """
 
 helps['ams list'] = """
     type: command
-    short-summary: List Azure Media Services for the entire subscription.
+    short-summary: List Azure Media Services accounts for the entire subscription.
 """
 
 helps['ams show'] = """
     type: command
-    short-summary: Show the details of an Azure Media Service.
+    short-summary: Show the details of an Azure Media Services account.
 """
 
 helps['ams storage'] = """
     type: group
-    short-summary: Manage secondary storage for an Azure Media Service.
+    short-summary: Manage secondary storage for an Azure Media Services account.
 """
 
 helps['ams storage add'] = """
     type: command
-    short-summary: Attach a secondary storage to an Azure Media Service.
+    short-summary: Attach a secondary storage to an Azure Media Services account.
 """
 
 helps['ams storage remove'] = """
     type: command
-    short-summary: Detach a secondary storage from an Azure Media Service.
+    short-summary: Detach a secondary storage from an Azure Media Services account. 
+"""
+
+helps['ams sp'] = """
+    type: group
+    short-summary: Manage service principal and role based access for an Azure Media Services account.
 """
 
 helps['ams sp create'] = """
     type: command
-    short-summary: Create a service principal and configure its access to an Azure Media Service account.
+    short-summary: Create a service principal and configure its access to an Azure Media Services account.
     parameters:
         - name: --sp-name
-          short-summary: Name or app URI to associate the RBAC with. If not present, a name will be generated.
+          short-summary: Name or app URI to associate the RBAC with. If not present, a name like '{amsaccountname}-access-sp' will be generated.
         - name: --sp-password
-          short-summary: The password used to log in.. If not present, a random password will be generated.
+          short-summary: The password used to log in. If not present, a random password will be generated.
         - name: --role
           short-summary: Role of the service principal.
     examples:
-        - name: Create a service principal with password and configure its access to an Azure Media Service account.
+        - name: Create a service principal with password and configure its access to an Azure Media Services account.
           text: >
             az ams sp create -n {myamsaccount} -g {myresourcegroup} --sp-name {mySpName} --sp-password {mySpPassword} --role {rol}
     """
--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
@@ -15,17 +15,17 @@ def load_arguments(self, _):
     storage_account_arg_type = CLIArgumentType(options_list=('--storage-account'), metavar='STORAGE_NAME')
 
     with self.argument_context('ams') as c:
-        c.argument('account_name', name_arg_type, help='The name of the media service account within the resource group.')
+        c.argument('account_name', name_arg_type, help='The name of the Azure Media Services account within the resource group.')
         c.argument('location', arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)
 
     with self.argument_context('ams create') as c:
-        c.argument('storage_account', storage_account_arg_type, help='The name of the primary storage account to attach to the media service account.')
+        c.argument('storage_account', storage_account_arg_type, help='The name of the primary storage account to attach to the Azure Media Services account.')
 
     with self.argument_context('ams storage add') as c:
-        c.argument('storage_account', storage_account_arg_type, help='The name of the secondary storage account to attach to the media service account.')
+        c.argument('storage_account', storage_account_arg_type, help='The name of the secondary storage account to attach to the Azure Media Services account.')
 
     with self.argument_context('ams storage remove') as c:
-        c.argument('storage_account', storage_account_arg_type, help='The name of the secondary storage account to detach from the media service account.')
+        c.argument('storage_account', storage_account_arg_type, help='The name of the secondary storage account to detach from the Azure Media Services account.')
 
     with self.argument_context('ams sp create') as c:
         c.argument('sp_name', help='The name or app URI to associate the RBAC with. If not present, a name will be generated.')
