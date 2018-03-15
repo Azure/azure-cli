876420c345b14ce9a859bdf7346e312ad2e4fd45
Muhammad Waqar <waqar.hameed08@gmail.com>
2018-03-15 09:02:20 +0500

    Disabling line-too-long in __init__.py

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/__init__.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/__init__.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=line-too-long
+
 from azure.cli.core import AzCommandsLoader
 from azure.cli.core.profiles import ResourceType
 
