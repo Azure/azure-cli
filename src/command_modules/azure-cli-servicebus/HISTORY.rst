ccf259c52eca3561f0d0233b40c23cf7bedb5bc9
Derek Bekoe <debekoe@microsoft.com>
2018-03-19 13:59:46 -0700

    Fix service bus and event hub command loaders (#5836)
    * Fix service bus and event hub command loaders
    
    * Update versions

--- src/command_modules/azure-cli-servicebus/HISTORY.rst
+++ src/command_modules/azure-cli-servicebus/HISTORY.rst
@@ -3,8 +3,13 @@
 Release History
 ===============
 
+0.1.1
++++++
+* Suppress the service bus extension from being loaded now.
+
+
 0.1.0
-++++++++++++++++++
++++++
 
 * Initial release.
 
--- src/command_modules/azure-cli-servicebus/azure/cli/command_modules/servicebus/__init__.py
+++ src/command_modules/azure-cli-servicebus/azure/cli/command_modules/servicebus/__init__.py
@@ -14,13 +14,13 @@ class ServicebusCommandsLoader(AzCommandsLoader):
 
     def __init__(self, cli_ctx=None):
         from azure.cli.core import ModExtensionSuppress
-        # Suppress eventhubs up to and including version 0.0.1
-        super(ServicebusCommandsLoader, self).__init__(cli_ctx=cli_ctx,
-                                                       suppress_extension=ModExtensionSuppress(__name__, 'servicebus', '0.0.1', reason='These commands are now in the CLI.', recommend_remove=True))
         from azure.cli.core.commands import CliCommandType
         servicebus_custom = CliCommandType(operations_tmpl='azure.cli.command_modules.servicebus.custom#{}')
         super(ServicebusCommandsLoader, self).__init__(cli_ctx=cli_ctx, custom_command_type=servicebus_custom,
-                                                       min_profile="2017-03-10-profile")
+                                                       min_profile='2017-03-10-profile',
+                                                       suppress_extension=ModExtensionSuppress(__name__, 'servicebus', '0.0.1',
+                                                                                               reason='These commands are now in the CLI.',
+                                                                                               recommend_remove=True))
 
     def load_command_table(self, args):
         from azure.cli.command_modules.servicebus.commands import load_command_table
