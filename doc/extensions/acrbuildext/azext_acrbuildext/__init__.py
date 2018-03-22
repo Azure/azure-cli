7a63059e205e87f2b614448759b1cf9d42e4f032
ankurkhemani <ankheman@microsoft.com>
2018-03-21 18:37:53 -0700

    copy build-task commands to build extension

--- doc/extensions/acrbuildext/azext_acrbuildext/__init__.py
+++ doc/extensions/acrbuildext/azext_acrbuildext/__init__.py
@@ -8,9 +8,10 @@ from azure.cli.core.commands.parameters import (
     get_resource_name_completion_list
 )
 from ._constants import (
-    REGISTRY_RESOURCE_TYPE
+    REGISTRY_RESOURCE_TYPE,
+    BUILD_TASK_RESOURCE_TYPE
 )
-from ._client_factory import cf_acr_builds
+from ._client_factory import cf_acr_builds, cf_acr_build_tasks
 
 class AcrBuildCommandsLoader(AzCommandsLoader):
 
@@ -25,9 +26,21 @@ class AcrBuildCommandsLoader(AzCommandsLoader):
             client_factory=cf_acr_builds
         )
 
+        acr_build_task_util = CliCommandType(
+            operations_tmpl='azure.cli.command_modules.acr.build_task#{}',    
+            client_factory=cf_acr_build_tasks
+        )
+
         with self.command_group('acr build', acr_build_util) as g:
             g.command('show-logs', 'acr_build_show_logs')
             g.command('', 'acr_queue') # TODO: it should be moved to acr command group once we can integrate the full sdk.
+        return self.command_table
+
+        with self.command_group('acr build-task', acr_build_task_util) as g:
+            g.command('create', 'acr_build_task_create')
+            g.command('show', 'acr_build_task_show')
+            g.command('list', 'acr_build_task_list')
+            g.command('delete', 'acr_build_task_delete')
         return self.command_table
 
     def load_arguments(self, _):
@@ -41,5 +54,20 @@ class AcrBuildCommandsLoader(AzCommandsLoader):
             c.argument('secret_build_args', nargs='+', help='The space-separated secret build arguments in a format of <name>=<value>.')
             c.argument('no_logs', options_list=['--no-logs'], action='store_true', help='Do not show logs after successfully queuing the build.') 
         return self.argument_context
+
+        with self.argument_context('acr build-task') as c:
+            c.argument('registry_name', options_list=['--registry', '-r'], help='The name of the container registry. You can configure the default registry name using `az configure --defaults acr=<registry name>`', completer=get_resource_name_completion_list(REGISTRY_RESOURCE_TYPE), configured_default='acr')
+            c.argument('build_task_name', options_list=['--name', '-n'], help='The name of the build task', completer=get_resource_name_completion_list(BUILD_TASK_RESOURCE_TYPE))
+            c.argument('image_name', options_list=['--image', '-t'], help="The image repository and optionally a tag in the 'repository:tag' format.")
+            c.argument('context', options_list=['--context'], help="The URL to a git repository.")
+            c.argument('source_branch', options_list=['--branch'], help="The source control branch name")
+            c.argument('docker_file_path', options_list=['--file', '-f'], help="The relative path of the the docker file to the source code root folder.")
+            c.argument('git_access_token', help="The git access token for configuring webhook.")
+            c.argument('os_type', help="The platform OS that has to be used for build task.")
+            c.argument('cpu', help="The number of cpu cores to use for running builds.")
+
+
+        with self.argument_context('acr build-task create') as c:
+            c.argument('build_task_name', completer=None)
 
 COMMAND_LOADER_CLS = AcrBuildCommandsLoader
---
+++ doc/extensions/acrbuildext/azext_acrbuildext/build_task.py
@@ -0,0 +1,79 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core.commands import LongRunningOperation
+from ._utils import (
+    arm_deploy_template_build_task_create,
+    validate_managed_registry
+)
+from .build import _check_image_name
+
+BUILD_TASKS_NOT_SUPPORTED = 'Build Tasks are only supported for managed registries.'
+
+
+def acr_build_task_create(
+    cmd,
+    client,
+    build_task_name,
+    registry_name,
+    context,            
+    source_branch,  
+    image_name,
+    git_access_token,
+    resource_group_name=None,
+    docker_file_path='Dockerfile',
+    os_type="Linux",
+    cpu=1):
+    
+    registry, resource_group_name = validate_managed_registry(
+        cmd.cli_ctx, registry_name, resource_group_name, BUILD_TASKS_NOT_SUPPORTED)
+
+    LongRunningOperation(cmd.cli_ctx)(
+            arm_deploy_template_build_task_create(
+                cmd.cli_ctx,
+                build_task_name,
+                registry_name,
+                registry.location,
+                resource_group_name,
+                context,
+                source_branch,              
+                _check_image_name(image_name),
+                docker_file_path,
+                git_access_token,
+                os_type,
+                cpu)
+        )
+    return client.get(resource_group_name, registry_name, build_task_name)
+
+def acr_build_task_show(
+    cmd,
+    client,
+    build_task_name,
+    registry_name,
+    resource_group_name=None):
+    
+    _, resource_group_name = validate_managed_registry(
+        cmd.cli_ctx, registry_name, resource_group_name, BUILD_TASKS_NOT_SUPPORTED)
+    return client.get(resource_group_name, registry_name, build_task_name)
+
+def acr_build_task_list(
+    cmd,
+    client,
+    registry_name,
+    resource_group_name=None):
+    
+    _, resource_group_name = validate_managed_registry(
+        cmd.cli_ctx, registry_name, resource_group_name, BUILD_TASKS_NOT_SUPPORTED)
+    return client.list(resource_group_name, registry_name)
+
+def acr_build_task_delete(
+    cmd,
+    client,
+    build_task_name,
+    registry_name,
+    resource_group_name=None):
+    _, resource_group_name = validate_managed_registry(
+        cmd.cli_ctx, registry_name, resource_group_name, BUILD_TASKS_NOT_SUPPORTED)
+    return client.delete(resource_group_name, registry_name, build_task_name)
