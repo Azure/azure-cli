f1555c672300884253f99d0490ebbde859198d2b
Lucas Marambio <lucas.marambio@southworks.com>
2018-04-03 19:40:00 +0000

    [AMS] Remove --location parameter from transform create operation.

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
@@ -60,8 +60,6 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                    help='The name of the transform.')
 
     with self.argument_context('ams transform create') as c:
-        c.argument('location', arg_type=get_location_type(self.cli_ctx),
-                   validator=get_default_location_from_resource_group)
         c.argument('preset_name', arg_type=get_enum_type(EncoderNamedPreset),
                    help='The name of the built in preset to use.')
         c.argument('tags', arg_type=tags_type)
--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/transform.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/transform.py
@@ -4,12 +4,12 @@
 # --------------------------------------------------------------------------------------------
 
 
-def create_transform(client, account_name, resource_group_name, transform_name, preset_name, location=None, description=None, tags=None):
+def create_transform(client, account_name, resource_group_name, transform_name, preset_name, description=None, tags=None):
     from azure.mediav3.models import (Transform, TransformOutput, BuiltInStandardEncoderPreset)
 
     transform_preset = BuiltInStandardEncoderPreset(preset_name=preset_name)
     transform_output = TransformOutput(preset=transform_preset)
-    transform_parameters = Transform(outputs=[transform_output], location=location, description=description, tags=tags)
+    transform_parameters = Transform(outputs=[transform_output], location='westus2', description=description, tags=tags)
 
     return client.create_or_update(resource_group_name, account_name, transform_name, transform_parameters)
 
