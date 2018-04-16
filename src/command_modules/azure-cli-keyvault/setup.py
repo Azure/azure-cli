6dcca64b0a825b5ae22dc5864c360301189a6ecc
Scott Schaab <sschaab@microsoft.com>
2018-04-16 16:01:28 -0700

    updating command module versions and history

--- src/command_modules/azure-cli-keyvault/setup.py
+++ src/command_modules/azure-cli-keyvault/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.21"
+VERSION = "2.0.22"
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
