a5b77828a260ab1b29dbcd457ddb170fe75be3f6
yugangw-msft <yugangw@microsoft.com>
2018-04-23 09:00:56 -0700

    webapp: remove '--runtime-version' from 'az webapp auth update'

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
@@ -272,7 +272,7 @@ def get_auth_settings(cmd, resource_group_name, name, slot=None):
 
 def update_auth_settings(cmd, resource_group_name, name, enabled=None, action=None,  # pylint: disable=unused-argument
                          client_id=None, token_store_enabled=None,  # pylint: disable=unused-argument
-                         runtime_version=None, token_refresh_extension_hours=None,  # pylint: disable=unused-argument
+                         token_refresh_extension_hours=None,  # pylint: disable=unused-argument
                          allowed_external_redirect_urls=None, client_secret=None,  # pylint: disable=unused-argument
                          allowed_audiences=None, issuer=None, facebook_app_id=None,  # pylint: disable=unused-argument
                          facebook_app_secret=None, facebook_oauth_scopes=None,  # pylint: disable=unused-argument
