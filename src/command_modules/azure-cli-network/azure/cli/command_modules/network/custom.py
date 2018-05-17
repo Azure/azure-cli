a51498a46f2c53db07e9180ad0279efefd55cc19
yugangw-msft <yugangw@microsoft.com>
2018-05-17 09:39:32 -0700

    vnet peering

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/custom.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/custom.py
@@ -2918,9 +2918,19 @@ def create_vnet_peering(cmd, resource_group_name, virtual_network_name, virtual_
         allow_gateway_transit=allow_gateway_transit,
         allow_forwarded_traffic=allow_forwarded_traffic,
         use_remote_gateways=use_remote_gateways)
-    ncf = network_client_factory(cmd.cli_ctx)
+    aux_subscription = parse_resource_id(remote_virtual_network)['subscription']
+    ncf = network_client_factory(cmd.cli_ctx, aux_subscriptions=[aux_subscription])
     return ncf.virtual_network_peerings.create_or_update(
         resource_group_name, virtual_network_name, virtual_network_peering_name, peering)
+
+
+def update_vnet_peering(cmd, resource_group_name, virtual_network_name, virtual_network_peering_name, **kwargs):
+    peering = kwargs['parameters']
+    aux_subscription = parse_resource_id(peering.remote_virtual_network.id)['subscription']
+    ncf = network_client_factory(cmd.cli_ctx, aux_subscriptions=[aux_subscription])
+    return ncf.virtual_network_peerings.create_or_update(
+        resource_group_name, virtual_network_name, virtual_network_peering_name, peering)
+
 # endregion
 
 
