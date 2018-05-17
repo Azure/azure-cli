caf23af9cc19746910754f21bdd5d91a25f74504
Bulent Elmaci <bulente@microsoft.com>
2018-05-17 16:17:02 -0700

    Requested changes

--- src/command_modules/azure-cli-policyinsights/azure/cli/command_modules/policyinsights/_help.py
+++ src/command_modules/azure-cli-policyinsights/azure/cli/command_modules/policyinsights/_help.py
@@ -12,48 +12,6 @@ helps['policy event'] = """
 helps['policy event list'] = """
     type: command
     short-summary: List policy events.
-    parameters:
-        - name: --management-group-name -m
-          type: string
-
-        - name: --subscription-id -s
-          type: string
-
-        - name: --resource-group-name -g
-          type: string
-
-        - name: --resource-id -r
-          type: string
-
-        - name: --policy-set-definition-name -ps
-          type: string
-
-        - name: --policy-definition-name -pd
-          type: string
-
-        - name: --policy-assignment-name -pa
-          type: string
-
-        - name: --from
-          type: datetime
-
-        - name: --to
-          type: datetime
-
-        - name: --top
-          type: long
-
-        - name: --order-by
-          type: string
-
-        - name: --select
-          type: string
-
-        - name: --filter
-          type: string
-
-        - name: --apply
-          type: string
     examples:
         - name: Get policy events at current subscription scope created in the last day.
           text: >
@@ -72,16 +30,16 @@ helps['policy event list'] = """
               az policy event list -r "/subscriptions/fff10b27-fff3-fff5-fff8-fffbe01e86a5/resourceGroups/myResourceGroup /providers/Microsoft.EventHub/namespaces/myns1/eventhubs/eh1/consumergroups/cg1"
         - name: Get policy events for a policy set definition in current subscription.
           text: >
-              az policy event list -ps "fff58873-fff8-fff5-fffc-fffbe7c9d697"
+              az policy event list -p "fff58873-fff8-fff5-fffc-fffbe7c9d697"
         - name: Get policy events for a policy definition in current subscription.
           text: >
-              az policy event list -pd "fff69973-fff8-fff5-fffc-fffbe7c9d698"
+              az policy event list -d "fff69973-fff8-fff5-fffc-fffbe7c9d698"
         - name: Get policy events for a policy assignment in current subscription.
           text: >
-              az policy event list -pa "ddd8ef92e3714a5ea3d208c1"
+              az policy event list -a "ddd8ef92e3714a5ea3d208c1"
         - name: Get policy events for a policy assignment in the specified resource group in current subscription.
           text: >
-              az policy event list -g "myRg" -pa "ddd8ef92e3714a5ea3d208c1"
+              az policy event list -g "myRg" -a "ddd8ef92e3714a5ea3d208c1"
         - name: Get top 5 policy events in current subscription, selecting a subset of properties and customizing ordering.
           text: >
               az policy event list --top 5 --order-by "timestamp desc, policyAssignmentName asc" --select "timestamp, resourceId, policyAssignmentId, policySetDefinitionId, policyDefinitionId"
@@ -111,48 +69,6 @@ helps['policy state'] = """
 helps['policy state list'] = """
     type: command
     short-summary: List policy compliance states.
-    parameters:
-        - name: --management-group-name -m
-          type: string
-
-        - name: --subscription-id -s
-          type: string
-
-        - name: --resource-group-name -g
-          type: string
-
-        - name: --resource-id -r
-          type: string
-
-        - name: --policy-set-definition-name -ps
-          type: string
-
-        - name: --policy-definition-name -pd
-          type: string
-
-        - name: --policy-assignment-name -pa
-          type: string
-
-        - name: --from
-          type: datetime
-
-        - name: --to
-          type: datetime
-
-        - name: --top
-          type: long
-
-        - name: --order-by
-          type: string
-
-        - name: --select
-          type: string
-
-        - name: --filter
-          type: string
-
-        - name: --apply
-          type: string
     examples:
         - name: Get latest policy states at current subscription scope.
           text: >
@@ -174,16 +90,16 @@ helps['policy state list'] = """
               az policy state list -r "/subscriptions/fff10b27-fff3-fff5-fff8-fffbe01e86a5/resourceGroups/myResourceGroup /providers/Microsoft.EventHub/namespaces/myns1/eventhubs/eh1/consumergroups/cg1"
         - name: Get latest policy states for a policy set definition in current subscription.
           text: >
-              az policy state list -ps "fff58873-fff8-fff5-fffc-fffbe7c9d697"
+              az policy state list -p "fff58873-fff8-fff5-fffc-fffbe7c9d697"
         - name: Get latest policy states for a policy definition in current subscription.
           text: >
-              az policy state list -pd "fff69973-fff8-fff5-fffc-fffbe7c9d698"
+              az policy state list -d "fff69973-fff8-fff5-fffc-fffbe7c9d698"
         - name: Get latest policy states for a policy assignment in current subscription.
           text: >
-              az policy state list -pa "ddd8ef92e3714a5ea3d208c1"
+              az policy state list -a "ddd8ef92e3714a5ea3d208c1"
         - name: Get latest policy states for a policy assignment in the specified resource group in current subscription.
           text: >
-              az policy state list -g "myRg" -pa "ddd8ef92e3714a5ea3d208c1"
+              az policy state list -g "myRg" -a "ddd8ef92e3714a5ea3d208c1"
         - name: Get top 5 latest policy states in current subscription, selecting a subset of properties and customizing ordering.
           text: >
               az policy state list --top 5 --order-by "timestamp desc, policyAssignmentName asc" --select "timestamp, resourceId, policyAssignmentId, policySetDefinitionId, policyDefinitionId"
@@ -209,39 +125,6 @@ helps['policy state list'] = """
 helps['policy state summarize'] = """
     type: command
     short-summary: Summarize policy compliance states.
-    parameters:
-        - name: --management-group-name -m
-          type: string
-
-        - name: --subscription-id -s
-          type: string
-
-        - name: --resource-group-name -g
-          type: string
-
-        - name: --resource-id -r
-          type: string
-
-        - name: --policy-set-definition-name -ps
-          type: string
-
-        - name: --policy-definition-name -pd
-          type: string
-
-        - name: --policy-assignment-name -pa
-          type: string
-
-        - name: --from
-          type: datetime
-
-        - name: --to
-          type: datetime
-
-        - name: --top
-          type: long
-
-        - name: --filter
-          type: string
     examples:
         - name: Get latest non-compliant policy states summary at current subscription scope.
           text: >
@@ -260,16 +143,16 @@ helps['policy state summarize'] = """
               az policy state summarize -r "/subscriptions/fff10b27-fff3-fff5-fff8-fffbe01e86a5/resourceGroups/myResourceGroup /providers/Microsoft.EventHub/namespaces/myns1/eventhubs/eh1/consumergroups/cg1"
         - name: Get latest non-compliant policy states summary for a policy set definition in current subscription.
           text: >
-              az policy state summarize -ps "fff58873-fff8-fff5-fffc-fffbe7c9d697"
+              az policy state summarize -p "fff58873-fff8-fff5-fffc-fffbe7c9d697"
         - name: Get latest non-compliant policy states summary for a policy definition in current subscription.
           text: >
-              az policy state summarize -pd "fff69973-fff8-fff5-fffc-fffbe7c9d698"
+              az policy state summarize -d "fff69973-fff8-fff5-fffc-fffbe7c9d698"
         - name: Get latest non-compliant policy states summary for a policy assignment in current subscription.
           text: >
-              az policy state summarize -pa "ddd8ef92e3714a5ea3d208c1"
+              az policy state summarize -a "ddd8ef92e3714a5ea3d208c1"
         - name: Get latest non-compliant policy states summary for a policy assignment in the specified resource group in current subscription.
           text: >
-              az policy state summarize -g "myRg" -pa "ddd8ef92e3714a5ea3d208c1"
+              az policy state summarize -g "myRg" -a "ddd8ef92e3714a5ea3d208c1"
         - name: Get latest non-compliant policy states summary in current subscription, limiting the assignments summary to top 5.
           text: >
               az policy state summarize --top 5
--- src/command_modules/azure-cli-policyinsights/azure/cli/command_modules/policyinsights/_params.py
+++ src/command_modules/azure-cli-policyinsights/azure/cli/command_modules/policyinsights/_params.py
@@ -13,141 +13,77 @@ from azure.cli.command_modules.resource._completers import (
 
 
 def load_arguments(self, _):
-    with self.argument_context('policy event') as c:
-        c.argument(
-            'management_group_name',
-            options_list=('--management-group-name', '-m'),
-            help='Management group name.')
-        c.argument(
-            'subscription_id',
-            options_list=('--subscription-id', '-s'),
-            help='Subscription id.')
-        c.argument(
-            'resource_group_name',
-            options_list=('--resource-group-name', '-g'),
-            arg_type=resource_group_name_type,
-            completer=get_resource_group_completion_list,
-            help='Resource group name.')
-        c.argument(
-            'resource_id',
-            options_list=('--resource-id', '-r'),
-            help='Resource id.')
-        c.argument(
-            'policy_set_definition_name',
-            options_list=('--policy-set-definition-name', '-ps'),
-            completer=get_policy_set_completion_list,
-            help='Policy set definition name.')
-        c.argument(
-            'policy_definition_name',
-            options_list=('--policy-definition-name', '-pd'),
-            completer=get_policy_completion_list,
-            help='Policy definition name.')
-        c.argument(
-            'policy_assignment_name',
-            options_list=('--policy-assignment-name', '-pa'),
-            completer=get_policy_assignment_completion_list,
-            help='Policy assignment name.')
-        c.argument(
-            'from_value',
-            options_list=('--from'),
-            help='Timestamp specifying the start time of the interval to query.')
-        c.argument(
-            'to_value',
-            options_list=('--to'),
-            help='Timestamp specifying the end time of the interval to query.')
-        c.argument(
-            'top_value',
-            options_list=('--top'),
-            type=int,
-            help='Maximum number of records to return.')
-        c.argument(
-            'order_by_clause',
-            options_list=('--order-by'),
-            help='Ordering expression using OData notation.')
-        c.argument(
-            'select_clause',
-            options_list=('--select'),
-            help='Select expression using OData notation.')
-        c.argument(
-            'filter_clause',
-            options_list=('--filter'),
-            help='Filter expression using OData notation.')
-        c.argument(
-            'apply_clause',
-            options_list=('--apply'),
-            help='Apply expression for aggregations using OData notation.')
+    for scope in ['state', 'event']:
+        with self.argument_context('policy {}'.format(scope)) as c:
+            c.argument(
+                'management_group_name',
+                options_list=['--management-group-name', '-m'],
+                help='Management group name.')
+            c.argument(
+                'subscription_id',
+                options_list=['--subscription-id', '-s'],
+                help='Subscription id.')
+            c.argument(
+                'resource_group_name',
+                options_list=['--resource-group-name', '-g'],
+                arg_type=resource_group_name_type,
+                completer=get_resource_group_completion_list,
+                help='Resource group name.')
+            c.argument(
+                'resource_id',
+                options_list=['--resource-id', '-r'],
+                help='Resource id.')
+            c.argument(
+                'policy_set_definition_name',
+                options_list=['--policy-set-definition-name', '-p'],
+                completer=get_policy_set_completion_list,
+                help='Policy set definition name.')
+            c.argument(
+                'policy_definition_name',
+                options_list=['--policy-definition-name', '-d'],
+                completer=get_policy_completion_list,
+                help='Policy definition name.')
+            c.argument(
+                'policy_assignment_name',
+                options_list=['--policy-assignment-name', '-a'],
+                completer=get_policy_assignment_completion_list,
+                help='Policy assignment name.')
+            c.argument(
+                'from_value',
+                options_list=['--from'],
+                help='ISO 8601 formatted timestamp specifying the start time of the interval to query.')
+            c.argument(
+                'to_value',
+                options_list=['--to'],
+                help='ISO 8601 formatted timestamp specifying the end time of the interval to query.')
+            c.argument(
+                'top_value',
+                options_list=['--top'],
+                type=int,
+                help='Maximum number of records to return.')
+            c.argument(
+                'order_by_clause',
+                options_list=['--order-by'],
+                help='Ordering expression using OData notation.')
+            c.argument(
+                'select_clause',
+                options_list=['--select'],
+                help='Select expression using OData notation.')
+            c.argument(
+                'filter_clause',
+                options_list=['--filter'],
+                help='Filter expression using OData notation.')
+            c.argument(
+                'apply_clause',
+                options_list=['--apply'],
+                help='Apply expression for aggregations using OData notation.')
 
     with self.argument_context('policy state') as c:
         c.argument(
             'all_results',
-            options_list=('--all'),
+            options_list=['--all'],
             arg_type=get_three_state_flag(),
             help='Within the specified time interval, get all policy states instead of the latest only.')
-        c.argument(
-            'management_group_name',
-            options_list=('--management-group-name', '-m'),
-            help='Management group name.')
-        c.argument(
-            'subscription_id',
-            options_list=('--subscription-id', '-s'),
-            help='Subscription id.')
-        c.argument(
-            'resource_group_name',
-            options_list=('--resource-group-name', '-g'),
-            arg_type=resource_group_name_type,
-            completer=get_resource_group_completion_list,
-            help='Resource group name.')
-        c.argument(
-            'resource_id',
-            options_list=('--resource-id', '-r'),
-            help='Resource id.')
-        c.argument(
-            'policy_set_definition_name',
-            options_list=('--policy-set-definition-name', '-ps'),
-            completer=get_policy_set_completion_list,
-            help='Policy set definition name.')
-        c.argument(
-            'policy_definition_name',
-            options_list=('--policy-definition-name', '-pd'),
-            completer=get_policy_completion_list,
-            help='Policy definition name.')
-        c.argument(
-            'policy_assignment_name',
-            options_list=('--policy-assignment-name', '-pa'),
-            completer=get_policy_assignment_completion_list,
-            help='Policy assignment name.')
-        c.argument(
-            'from_value',
-            options_list=('--from'),
-            help='Timestamp specifying the start time of the interval to query.')
-        c.argument(
-            'to_value',
-            options_list=('--to'),
-            help='Timestamp specifying the end time of the interval to query.')
-        c.argument(
-            'top_value',
-            options_list=('--top'),
-            type=int,
-            help='Maximum number of records to return.')
-        c.argument(
-            'order_by_clause',
-            options_list=('--order-by'),
-            help='Ordering expression using OData notation.')
-        c.argument(
-            'select_clause',
-            options_list=('--select'),
-            help='Select expression using OData notation.')
-        c.argument(
-            'filter_clause',
-            options_list=('--filter'),
-            help='Filter expression using OData notation.')
-        c.argument(
-            'apply_clause',
-            options_list=('--apply'),
-            help='Apply expression for aggregations using OData notation.')
 
     with self.argument_context('policy state summarize') as c:
-        c.ignore('all_results')
-        c.ignore('order_by_clause')
-        c.ignore('select_clause')
-        c.ignore('apply_clause')
+        c.ignore('all_results', 'order_by_clause', 'select_clause', 'apply_clause')
