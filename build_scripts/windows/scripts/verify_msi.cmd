2365e538359875d20460d2d1a40f009f7a48a8a8
Yugang Wang <yugangw@microsoft.com>
2018-05-21 11:09:51 -0700

    output module path

--- build_scripts/windows/scripts/verify_msi.cmd
+++ build_scripts/windows/scripts/verify_msi.cmd
@@ -10,20 +10,12 @@ pip install -e %REPO_DIR%\tools
 pip install -e %REPO_DIR%\src\azure-cli-testsdk
 
 set PYTHONPATH=%ProgramFiles(x86)%\Microsoft SDKs\Azure\CLI2\Lib\site-packages
-
 for /D %%i in (%REPO_DIR%\src\command_modules\*) do (
     set m=%%~ni
     set module=!m:~10!
     azdev test !module!
-	if !errorlevel! neq 0 goto ERROR
+	if !errorlevel! neq 0 set ERR_MODULES=!ERR_MODULES!;!module!
 )
 azdev test core
-if %errorlevel% neq 0 goto ERROR
-goto END
-:ERROR
-echo Error occurred, please check the output for details.
-exit /b 1
-
-:END
-exit /b 0
-popd
+if %errorlevel% neq 0 set ERR_MODULES=%ERR_MODULES%;core
+echo Module with test errors: %ERR_MODULES%
