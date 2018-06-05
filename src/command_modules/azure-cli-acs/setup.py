d964d62d0b31e8a92b2f79e01699445c83abc00b
Matt Boersma <Matt.Boersma@microsoft.com>
2018-06-05 11:47:54 -0600

    Update HISTORY.rst

--- src/command_modules/azure-cli-acs/setup.py
+++ src/command_modules/azure-cli-acs/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "2.0.34"
+VERSION = "2.0.35"
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
