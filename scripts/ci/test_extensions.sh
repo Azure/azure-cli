8deb1275726a833909bb2999a7c209770b1018e6
Willie Xu <wx44@cornell.edu>
2018-06-05 15:19:38 -0700

    add azdev verify script for loading all commands/args and extension CI check

---
+++ scripts/ci/test_extensions.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+. $(cd $(dirname $0); pwd)/artifacts.sh
+
+ls -la $share_folder/build
+
+ALL_MODULES=`find $share_folder/build/ -name "*.whl"`
+
+pip install -e ./tools
+[ -d privates ] && pip install -qqq privates/*.whl
+pip install $ALL_MODULES
+
+echo "Listing Available Extensions:"
+az extension list-available -otable
+output=$(az extension list-available --query [].name -otsv)
+exit_code=0
+
+for ext in $output; do
+    echo
+    echo "Verifying extension:" $ext
+    az extension add -n $ext
+    azdev verify load_all
+    if [ $? != 0 ]; then exit_code=1; fi
+    az extension remove -n $ext
+    echo $ext "extension has been removed."
+done
+
+exit $exit_code
