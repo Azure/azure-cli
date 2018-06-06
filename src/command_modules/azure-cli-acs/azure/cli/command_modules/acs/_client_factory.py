d5a4aea01d4ec9f71683fa6995fd6cfe62f4a51b
yugangw-msft <yugangw@microsoft.com>
2018-06-05 17:13:40 -0700

    minor fixes

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_client_factory.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_client_factory.py
@@ -26,7 +26,6 @@ def cf_resource_groups(cli_ctx, *_):
 def get_auth_management_client(cli_ctx, scope=None, **_):
     import re
     from azure.cli.core.profiles import ResourceType
-    from azure.mgmt.authorization import AuthorizationManagementClient
 
     subscription_id = None
     if scope:
