06b860454349a9bd8708fd6b969024f22e902597
Saurabh Sarkar <saurabsa@microsoft.com>
2018-06-11 15:30:26 -0700

    Updating options/params of use-dev-spaces command under aks module (#6509)

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_params.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/_params.py
@@ -198,8 +198,9 @@ def load_arguments(self, _):
         c.argument('service_principal')
 
     with self.argument_context('aks use-dev-spaces') as c:
+        c.argument('update', options_list=['--update'], action='store_true')
         c.argument('space_name', options_list=['--space', '-s'])
-        c.argument('parent_space_name', options_list=['--parent-space', '-p'])
+        c.argument('prompt', options_list=['--yes', '-y'], action='store_true', help='Do not prompt for confirmation. Requires --space.')
 
     with self.argument_context('aks remove-dev-spaces') as c:
         c.argument('prompt', options_list=['--yes', '-y'], action='store_true', help='Do not prompt for confirmation')
