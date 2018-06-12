e3eb5d0db1839cc7a34b67d6867116db86d91ca0
Troy Dai <troy.dai@outlook.com>
2018-06-12 14:35:21 -0700

    Bump azure-cli and azure-cli-core versions

--- src/azure-cli-core/azure/cli/core/__init__.py
+++ src/azure-cli-core/azure/cli/core/__init__.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------
 from __future__ import print_function
 
-__version__ = "2.0.37"
+__version__ = "2.0.38"
 
 import os
 import sys
