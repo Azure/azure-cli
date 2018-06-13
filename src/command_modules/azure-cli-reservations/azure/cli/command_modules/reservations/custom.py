a9aeee552b205481634d383c2cced71646d6ef68
REDMOND\juhle <juhle@microsoft.com>
2018-06-12 17:42:50 -0700

    fixing pylint line too long issue

--- src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/custom.py
+++ src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/custom.py
@@ -7,11 +7,15 @@ from azure.mgmt.reservations.models.patch import Patch
 
 
 def cli_reservation_update_reservation(client, reservation_order_id, reservation_id,
-                                       applied_scope_type, applied_scopes=None, instance_flexibility=None):
+                                       applied_scope_type, applied_scopes=None,
+                                       instance_flexibility=None):
     if applied_scopes:
-        patch = Patch(applied_scope_type=applied_scope_type, applied_scopes=[applied_scopes], instance_flexibility=instance_flexibility)
+        patch = Patch(applied_scope_type=applied_scope_type,
+                      applied_scopes=[applied_scopes],
+                      instance_flexibility=instance_flexibility)
     else:
-        patch = Patch(applied_scope_type=applied_scope_type, instance_flexibility=instance_flexibility)
+        patch = Patch(applied_scope_type=applied_scope_type,
+                      instance_flexibility=instance_flexibility)
     return client.update(reservation_order_id, reservation_id, patch)
 
 
@@ -20,12 +24,14 @@ def create_resource_id(reservation_order_id, reservation_id):
     return template.format(reservation_order_id, reservation_id)
 
 
-def cli_reservation_split_reservation(client, reservation_order_id, reservation_id, quantity_1, quantity_2):
+def cli_reservation_split_reservation(client, reservation_order_id,
+                                      reservation_id, quantity_1, quantity_2):
     reservationToSplit = create_resource_id(reservation_order_id, reservation_id)
     return client.split(reservation_order_id, [quantity_1, quantity_2], reservationToSplit)
 
 
-def cli_reservation_merge_reservation(client, reservation_order_id, reservation_id_1, reservation_id_2):
+def cli_reservation_merge_reservation(client, reservation_order_id,
+                                      reservation_id_1, reservation_id_2):
     return client.merge(reservation_order_id,
                         [create_resource_id(reservation_order_id, reservation_id_1),
                          create_resource_id(reservation_order_id, reservation_id_2)])
