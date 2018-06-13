072963629caf082f2b27972692a851c66b8c6ca1
Ju Hee Lee <juhee0202@gmail.com>
2018-06-12 20:01:25 -0700

    Reservations RP 2018-06-01 api version (#6558)
    * updating cli commands for the new api version 2018-06-01
    
    * fixing pylint and version mismatch build errors
    
    * updating history.rst and recorded all tests again
    
    * replacing enums with models
    
    * fixing pylint line too long issue

--- src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/custom.py
+++ src/command_modules/azure-cli-reservations/azure/cli/command_modules/reservations/custom.py
@@ -4,16 +4,18 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.mgmt.reservations.models.patch import Patch
-from azure.mgmt.reservations.models.split_request import SplitRequest
-from azure.mgmt.reservations.models.merge_request import MergeRequest
 
 
 def cli_reservation_update_reservation(client, reservation_order_id, reservation_id,
-                                       applied_scope_type, applied_scopes=None):
+                                       applied_scope_type, applied_scopes=None,
+                                       instance_flexibility=None):
     if applied_scopes:
-        patch = Patch(applied_scope_type, [applied_scopes])
+        patch = Patch(applied_scope_type=applied_scope_type,
+                      applied_scopes=[applied_scopes],
+                      instance_flexibility=instance_flexibility)
     else:
-        patch = Patch(applied_scope_type)
+        patch = Patch(applied_scope_type=applied_scope_type,
+                      instance_flexibility=instance_flexibility)
     return client.update(reservation_order_id, reservation_id, patch)
 
 
@@ -22,12 +24,14 @@ def create_resource_id(reservation_order_id, reservation_id):
     return template.format(reservation_order_id, reservation_id)
 
 
-def cli_reservation_split_reservation(client, reservation_order_id, reservation_id, quantity_1, quantity_2):
-    split = SplitRequest([quantity_1, quantity_2], create_resource_id(reservation_order_id, reservation_id))
-    return client.split(reservation_order_id, split)
+def cli_reservation_split_reservation(client, reservation_order_id,
+                                      reservation_id, quantity_1, quantity_2):
+    reservationToSplit = create_resource_id(reservation_order_id, reservation_id)
+    return client.split(reservation_order_id, [quantity_1, quantity_2], reservationToSplit)
 
 
-def cli_reservation_merge_reservation(client, reservation_order_id, reservation_id_1, reservation_id_2):
-    merge = MergeRequest([create_resource_id(reservation_order_id, reservation_id_1),
-                          create_resource_id(reservation_order_id, reservation_id_2)])
-    return client.merge(reservation_order_id, merge)
+def cli_reservation_merge_reservation(client, reservation_order_id,
+                                      reservation_id_1, reservation_id_2):
+    return client.merge(reservation_order_id,
+                        [create_resource_id(reservation_order_id, reservation_id_1),
+                         create_resource_id(reservation_order_id, reservation_id_2)])
--- src/command_modules/azure-cli-reservations/setup.py
+++ src/command_modules/azure-cli-reservations/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.2"
+VERSION = "0.2.0"
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
@@ -33,7 +33,7 @@ CLASSIFIERS = [
 ]
 
 DEPENDENCIES = [
-    'azure-mgmt-reservations==0.1.0',
+    'azure-mgmt-reservations==0.2.0',
     'azure-cli-core'
 ]
 
