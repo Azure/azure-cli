0ef78eb766e8bed87dee8efb8367040c7367d148
Tejaswini Duggaraju <naduggar@microsoft.com>
2018-06-27 13:21:00 -0700

    Removing the supported OS types and other comments from PR are addressed

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/build.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/build.py
@@ -28,8 +28,6 @@ from azure.mgmt.containerregistry.v2018_02_01_preview.models import (
     PlatformProperties
 )
 
-from ._constants import SUPPORTED_OS_TYPES
-
 from ._utils import validate_managed_registry
 from ._client_factory import cf_acr_registries
 from ._build_polling import get_build_with_polling
@@ -264,12 +262,9 @@ def acr_build(cmd,
               docker_file_path='Dockerfile',
               no_push=False,
               no_logs=False,
-              os_type=None):
+              os_type='Linux'):
     _, resource_group_name = validate_managed_registry(
         cmd.cli_ctx, registry_name, resource_group_name, BUILD_NOT_SUPPORTED)
-
-    if os_type and os_type.lower() not in SUPPORTED_OS_TYPES:
-        raise CLIError("The os type is invalid. Supported values are '{!s}'".format(SUPPORTED_OS_TYPES))
 
     client_registries = cf_acr_registries(cmd.cli_ctx)
 
@@ -315,14 +310,9 @@ def acr_build(cmd,
             is_push_enabled = False
             logger.warning("'--image -t' is not provided. Skipping image push after build.")
 
-    if os_type:
-        platform = PlatformProperties(os_type=os_type)
-    else:
-        platform = PlatformProperties(os_type='Linux')
-
     build_request = QuickBuildRequest(
         source_location=source_location,
-        platform=platform,
+        platform=PlatformProperties(os_type=os_type),
         docker_file_path=docker_file_path,
         image_names=image_names,
         is_push_enabled=is_push_enabled,
