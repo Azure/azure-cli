907505484b56b928dacbd6008e591644d5783cad
Troy Dai <troy.dai@outlook.com>
2018-07-13 09:57:12 -0700

    Bump CLI and ACS module version

--- src/azure-cli-core/azure/cli/core/__init__.py
+++ src/azure-cli-core/azure/cli/core/__init__.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------
 from __future__ import print_function
 
-__version__ = "2.0.42"
+__version__ = "2.0.43"
 
 import os
 import sys
