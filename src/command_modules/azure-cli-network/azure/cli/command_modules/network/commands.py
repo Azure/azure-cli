1e5a5eb815685c35c135332c710a4de8783cc41b
seyadava@microsoft.com <seyadava@microsoft.com>
2018-07-17 16:54:45 -0700

    fixed caa show comment issue

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/commands.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/commands.py
@@ -347,7 +347,7 @@ def load_command_table(self, _):
             g.generic_update_command('update', custom_func_name='update_dns_record_set', transform=transform_dns_record_set_output)
 
     with self.command_group('network dns record-set caa', network_dns_record_set_sdk) as g:
-        g.show_command('show', 'get', min_api='2018-03-01', transform=transform_dns_record_set_output)
+        g.show_command('show', 'get', min_api='2018-02-01', transform=transform_dns_record_set_output)
         g.command('delete', 'delete', min_api='2018-02-01', confirmation=True)
         g.custom_command('list', 'list_dns_record_set', min_api='2018-02-01', client_factory=cf_dns_mgmt_record_sets, transform=transform_dns_record_set_output, table_transformer=transform_dns_record_set_table_output)
         g.custom_command('create', 'create_dns_record_set', min_api='2018-02-01', transform=transform_dns_record_set_output, doc_string_source=dns_doc_string)
