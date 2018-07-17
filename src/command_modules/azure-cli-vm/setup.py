5b5d11e0c558f77914587df4c5699f2c211770bd
Willie Xu <wx44@cornell.edu>
2018-07-17 15:04:13 -0700

    use multiapi 0.2.2 (#6822)
    * use multiapi 0.2.2
    
    * version bump
    
    * history edit

--- src/command_modules/azure-cli-vm/setup.py
+++ src/command_modules/azure-cli-vm/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     cmdclass = {}
 
 
-VERSION = "2.1.0"
+VERSION = "2.1.1"
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
@@ -39,7 +39,7 @@ DEPENDENCIES = [
     'azure-keyvault==0.3.7',
     'azure-mgmt-network==2.0.0rc3',
     'azure-mgmt-resource==2.0.0rc2',
-    'azure-multiapi-storage==0.2.1',
+    'azure-multiapi-storage==0.2.2',
     'azure-mgmt-marketplaceordering==0.1.0',
     'azure-cli-core'
 ]
