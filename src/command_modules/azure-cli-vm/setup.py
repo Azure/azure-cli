55fbbbe4408b2020deae463080517cea96b5aff1
Yugang Wang <yugangw@microsoft.com>
2018-07-20 17:17:23 -0700

    update to GA version

--- src/command_modules/azure-cli-vm/setup.py
+++ src/command_modules/azure-cli-vm/setup.py
@@ -33,17 +33,12 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-mgmt-msi==0.1.0',
-<<<<<<< HEAD
     'azure-mgmt-authorization==0.50.0',
-    'azure-mgmt-compute==4.0.0rc2',
-=======
-    'azure-mgmt-authorization==0.40.0',
-    'azure-mgmt-compute==4.0.0rc3',
->>>>>>> MSI: use new patch support to update identity
+    'azure-mgmt-compute==4.0.0',
     'azure-mgmt-keyvault==0.40.0',
     'azure-keyvault==0.3.7',
     'azure-mgmt-network==2.0.0rc3',
-    'azure-mgmt-resource==2.0.0rc3',
+    'azure-mgmt-resource==2.0.0',
     'azure-multiapi-storage==0.2.2',
     'azure-mgmt-marketplaceordering==0.1.0',
     'azure-cli-core'
