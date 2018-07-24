4246491889d1a2efafdda4a5c60bef535e28a71a
damatojuan <juanignaciodamato@gmail.com>
2018-07-24 16:20:55 -0300

    Refactor custom preset logic & add support for aacaudio codec

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_sdk_utils.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_sdk_utils.py
@@ -5,12 +5,23 @@
 
 import importlib
 
+
 models_module = importlib.import_module('azure.mgmt.media.models')
 
-sdk_mes_formats_mapper = {'MP4Format':'Mp4Format',
-                          'JpgFormat':'JpgFormat',
-                          'PngFormat':'PngFormat',
-                          'TransportStreamFormat':'TransportStreamFormat'}
+
+sdk_mes_formats_mapper = {'MP4Format':'Mp4Format'}
+
 
 def map_format_type(format_type):
     return sdk_mes_formats_mapper.get(format_type, format_type)
+
+
+sdk_mes_codecs_mapper = {'AACAudio':'AacAudio'}
+
+
+def map_codec_type(codec_type):
+    return sdk_mes_codecs_mapper.get(codec_type, codec_type)
+
+
+def get_sdk_model_class(class_name):
+    return getattr(models_module, class_name)
--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_utils.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_utils.py
@@ -3,7 +3,9 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import uuid
+import uuid, re, isodate
+
+from datetime import datetime, timedelta
 
 
 def _gen_guid():
@@ -17,3 +19,18 @@ def _is_guid(guid):
     except ValueError:
         pass
     return False
+
+
+def parse_iso_duration(str_duration):
+    iso_duration_format_value = None
+    if str_duration:
+        datetime_duration = datetime.strptime(str_duration,'%H:%M:%S')
+        iso_duration_format_value = isodate.duration_isoformat(timedelta(hours=datetime_duration.hour,
+                                                                         minutes=datetime_duration.minute,
+                                                                         seconds=datetime_duration.second))
+    return iso_duration_format_value
+
+
+def camel_to_snake(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
