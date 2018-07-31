1789f1762b85a9eddfca90ea7a256870d9752253
Nahuel Berg <nahuel.berg@southworks.com>
2018-07-31 18:41:48 -0300

    Added missing input parameters.

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/live_event.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/live_event.py
@@ -5,20 +5,21 @@
 
 
 def create(client, resource_group_name, account_name, live_event_name, streaming_protocol, location,
-           auto_start=False, encoding_type=None, preset_name=None, tags=None, description=None):
+           auto_start=False, encoding_type=None, preset_name=None, tags=None, description=None,
+           key_frame_interval_duration=None, access_token=None):
     from azure.mgmt.media.models import LiveEventInputProtocol
     from azure.mgmt.media.models import LiveEventInput
     from azure.mgmt.media.models import LiveEvent
-    from azure.mgmt.media.models import LiveEventEncodingType
     from azure.mgmt.media.models import LiveEventEncoding
 
     protocol = LiveEventInputProtocol(streaming_protocol)
-    liveeventinput = LiveEventInput(streaming_protocol=protocol)
-    encodingtypeenum = LiveEventEncodingType(encoding_type)
-    liveeventencoding = LiveEventEncoding(encoding_type=encodingtypeenum, preset_name=preset_name)
+    liveeventinput = LiveEventInput(streaming_protocol=protocol,
+                                    key_frame_interval_duration=key_frame_interval_duration, access_token=access_token)
+    liveeventencoding = LiveEventEncoding(encoding_type=encoding_type, preset_name=preset_name)
 
     live_event = LiveEvent(input=liveeventinput, location=location, encoding=liveeventencoding)
 
     return client.create(resource_group_name=resource_group_name, account_name=account_name,
                          live_event_name=live_event_name, parameters=live_event, auto_start=auto_start,
-                         description=description, tags=tags)
+                         description=description, tags=tags, key_frame_interval_duration=key_frame_interval_duration,
+                         access_token=access_token)
