16b9a5548bb85f24ec7fc9890f4b9f9850df87c4
Marsh Macy <marsma@microsoft.com>
2018-08-07 10:43:41 -0700

    [batchai] update HISTORY.rst and setup.py

--- src/command_modules/azure-cli-batchai/setup.py
+++ src/command_modules/azure-cli-batchai/setup.py
@@ -13,7 +13,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.4.0"
+VERSION = "0.4.1"
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
