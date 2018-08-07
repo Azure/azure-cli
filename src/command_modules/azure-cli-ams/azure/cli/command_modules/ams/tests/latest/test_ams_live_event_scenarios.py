a4292effe8221c3779d214d4bc8fdfc8b1e7c03b
damatojuan <juanignaciodamato@gmail.com>
2018-08-07 19:33:49 -0300

    Update test for live event create command

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/tests/latest/test_ams_live_event_scenarios.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/tests/latest/test_ams_live_event_scenarios.py
@@ -3,11 +3,19 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import os
+
 from azure.cli.core.util import CLIError
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer
+from azure.cli.command_modules.ams._utils import _gen_guid
 
 
 class AmsLiveEventTests(ScenarioTest):
+    def _get_test_data_file(self, filename):
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', filename)
+        self.assertTrue(os.path.isfile(filepath), 'File {} does not exist.'.format(filepath))
+        return filepath
+
     @ResourceGroupPreparer()
     @StorageAccountPreparer(parameter_name='storage_account_for_create')
     def test_live_event_create(self, storage_account_for_create):
@@ -18,9 +26,16 @@ class AmsLiveEventTests(ScenarioTest):
             'amsname': amsname,
             'storageAccount': storage_account_for_create,
             'location': 'westus2',
-            'streaming_protocol': 'FragmentedMP4',
+            'streamingProtocol': 'RTMP',
             'liveEventName': live_event_name,
-            'encodingType': 'Basic'
+            'encodingType': 'Basic',
+            'tags': 'key=value',
+            'previewLocator': _gen_guid(),
+            'keyFrameIntervalDuration': 'PT2S',
+            'description': 'asd',
+            'accessToken': '0abf356884d74b4aacbd7b1ebd3da0f7',
+            'clientAccessPolicy': self._get_test_data_file('clientAccessPolicy.xml'),
+            'crossDomainPolicy': self._get_test_data_file('crossDomainPolicy.xml')
         })
 
         self.cmd('az ams account create -n {amsname} -g {rg} --storage-account {storageAccount} -l {location}', checks=[
@@ -28,13 +43,22 @@ class AmsLiveEventTests(ScenarioTest):
             self.check('location', 'West US 2')
         ])
 
-        live_event = self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --streaming-protocol {streaming_protocol} --auto-start --encoding-type {encodingType} --preset-name Default720p --tags key=value', checks=[
+        live_event = self.cmd('az ams live event create -a {amsname} -l {location} -n {liveEventName} -g {rg} --auto-start --streaming-protocol {streamingProtocol} --encoding-type {encodingType} --tags {tags} --stream-options Default LowLatency --preview-locator {previewLocator} --ips 1.1.1.1 0.0.0.0 --key-frame-interval-duration {keyFrameIntervalDuration} --access-token {accessToken} --description {description} --client-access-policy "{clientAccessPolicy}" --cross-domain-policy "{crossDomainPolicy}" --vanity-url', checks=[
             self.check('name', '{liveEventName}'),
             self.check('location', 'West US 2'),
-            self.check('input.streamingProtocol', 'FragmentedMP4'),
-            self.check('encoding.encodingType', 'Basic'),
-            self.check('presetName', 'Default720p')
+            self.check('input.streamingProtocol', '{streamingProtocol}'),
+            self.check('encoding.encodingType', '{encodingType}'),
+            self.check('length(preview.accessControl.ip.allow)', 2),
+            self.check('preview.previewLocator', '{previewLocator}'),
+            self.check('input.keyFrameIntervalDuration', '{keyFrameIntervalDuration}'),
+            self.check('length(streamOptions)', 2),
+            self.check('description', '{description}'),
+            self.check('input.accessToken', '{accessToken}'),
+            self.check('vanityUrl', True)
         ]).get_output_in_json()
+
+        self.assertIsNotNone(live_event['crossSiteAccessPolicies']['crossDomainPolicy'])
+        self.assertIsNotNone(live_event['crossSiteAccessPolicies']['clientAccessPolicy'])
 
         self.assertNotEquals('Stopping', live_event['resourceState'])
         self.assertNotEquals('Stopped', live_event['resourceState'])
