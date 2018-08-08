6d66651d24e329812929c09335cf28ff42c189b1
Troy Dai <troy.dai@outlook.com>
2018-08-07 23:18:15 -0700

    Explicitly declare azure-mgmt-resource dependencies (#6983)

--- src/command_modules/azure-cli-search/setup.py
+++ src/command_modules/azure-cli-search/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -33,6 +33,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-cli-core',
+    'azure-mgmt-resource==2.0.0',
     'azure-mgmt-search==2.0.0',
 ]
 
