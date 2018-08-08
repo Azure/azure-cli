9f7b5fe4ddd31e03a096cb0b8176e0306ac9485a
Justin Emgarten <justin@emgarten.com>
2018-08-08 14:09:28 -0700

    Update IotCentral dependency to 0.2.0 (#6997)
    * Update azure-mgmt-iotcentral to 0.2.0 to support the GA version 2018-09-01
    * Remove PREVIEW prefix from iotcentral command summary
    * Update test recording data for 2018-09-01

--- src/command_modules/azure-cli-iotcentral/setup.py
+++ src/command_modules/azure-cli-iotcentral/setup.py
@@ -33,7 +33,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-cli-core',
-    'azure-mgmt-iotcentral==0.1.0'
+    'azure-mgmt-iotcentral==0.2.0'
 ]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
