209111efc9f218ad73bbb06e9c1f432a8ab38aea
xingwu1 <xingwu1@yahoo.com>
2018-08-10 14:30:23 -0700

    Update Batch Management SDK dependency

--- src/command_modules/azure-cli-batch/HISTORY.rst
+++ src/command_modules/azure-cli-batch/HISTORY.rst
@@ -3,6 +3,10 @@
 Release History
 ===============
 
+3.3.3
++++++
+* Update Batch Management SDK dependency
+
 3.3.2
 +++++
 * Update Key Vault SDK dependency
--- src/command_modules/azure-cli-batch/setup.py
+++ src/command_modules/azure-cli-batch/setup.py
@@ -15,7 +15,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "3.3.2"
+VERSION = "3.3.3"
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
@@ -34,7 +34,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-batch==4.1.3',
-    'azure-mgmt-batch==4.1.0',
+    'azure-mgmt-batch==5.0.1',
     'azure-mgmt-keyvault==1.1.0',
     'azure-cli-core'
 ]
