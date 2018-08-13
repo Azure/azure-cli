52601daab5526f9971c3faa6745f0ff8e8b57b15
Troy Dai <troy.dai@outlook.com>
2018-08-13 10:12:42 -0700

    [CDN] Update CDN sdk version (#7024)

--- src/command_modules/azure-cli-cdn/azure/cli/command_modules/cdn/_client_factory.py
+++ src/command_modules/azure-cli-cdn/azure/cli/command_modules/cdn/_client_factory.py
@@ -4,7 +4,31 @@
 # --------------------------------------------------------------------------------------------
 
 
-def cf_cdn(cli_ctx, _):
+def cf_cdn(cli_ctx, *kwargs):  # pylint: disable=unused-argument
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.mgmt.cdn import CdnManagementClient
     return get_mgmt_service_client(cli_ctx, CdnManagementClient)
+
+
+def cf_custom_domain(cli_ctx, _):
+    return cf_cdn(cli_ctx).custom_domains
+
+
+def cf_endpoints(cli_ctx, _):
+    return cf_cdn(cli_ctx).endpoints
+
+
+def cf_profiles(cli_ctx, _):
+    return cf_cdn(cli_ctx).profiles
+
+
+def cf_origins(cli_ctx, _):
+    return cf_cdn(cli_ctx).origins
+
+
+def cf_resource_usage(cli_ctx, _):
+    return cf_cdn(cli_ctx).resource_usage
+
+
+def cf_edge_nodes(cli_ctx, _):
+    return cf_cdn(cli_ctx).edge_nodes
