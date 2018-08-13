7a497c3de1187dec824ad7d2f82becc28ccf0d3d
williexu <wx44@cornell.edu>
2018-08-13 10:41:34 -0700

    version bump

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -53,7 +53,7 @@ CLASSIFIERS = [
 
 # TODO These dependencies should be updated to reflect only what this package needs
 DEPENDENCIES = [
-    'adal>=1.0.0',
+    'adal>=1.0.2',
     'argcomplete>=1.8.0',
     'azure-cli-telemetry',
     'colorama>=0.3.9',
