a4855c3fc3743fe656f409a79391bb6d4de2a9bd
Nahuel Berg <nahuel.berg@southworks.com>
2018-08-16 18:31:36 -0300

    Initial commit.

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_endpoint.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_endpoint.py
@@ -64,6 +64,36 @@ def remove_akamai_access_control(client, account_name, resource_group_name, stre
     return client.update(resource_group_name, account_name, streaming_endpoint_name, streaming_endpoint)
 
 
+def update_streaming_endpoint(client, resource_group_name, account_name, streaming_endpoint_name, location,  # pylint: disable=too-many-locals
+                              auto_start=None, tags=None, cross_domain_policy=None, ips=None,
+                              description=None, scale_units=None, availability_set_name=None, max_cache_age=None,
+                              cdn_provider=None, cdn_profile=None, custom_host_names=None, client_access_policy=None):
+    from azure.mgmt.media.models import (StreamingEndpoint, IPAccessControl, StreamingEndpointAccessControl)
+
+    allow_list = []
+    if ips is not None:
+        for ip in ips:
+            allow_list.append(create_ip_range(streaming_endpoint_name, ip))
+
+    streaming_endpoint_access_control = None
+    if ips is not None:
+        streaming_endpoint_access_control = StreamingEndpointAccessControl(ip=IPAccessControl(allow=allow_list))
+
+    policies = create_cross_site_access_policies(client_access_policy, cross_domain_policy)
+
+    cdn_enabled = cdn_profile is not None or cdn_provider is not None
+
+    streaming_endpoint = StreamingEndpoint(max_cache_age=max_cache_age, tags=tags, location=location,
+                                           description=description, custom_host_names=custom_host_names,
+                                           scale_units=scale_units, cdn_profile=cdn_profile,
+                                           availability_set_name=availability_set_name, cdn_enabled=cdn_enabled,
+                                           cdn_provider=cdn_provider, cross_site_access_policies=policies,
+                                           access_control=streaming_endpoint_access_control)
+
+    return client.create(resource_group_name=resource_group_name, account_name=account_name, auto_start=auto_start,
+                         streaming_endpoint_name=streaming_endpoint_name, parameters=streaming_endpoint)
+
+
 def create_cross_site_access_policies(client_access_policy, cross_domain_policy):
     from azure.mgmt.media.models import CrossSiteAccessPolicies
 
