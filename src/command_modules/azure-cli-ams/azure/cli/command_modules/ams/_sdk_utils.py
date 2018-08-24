487ccd7e4d1283dbf6c9b73aa8a06068c895ecc9
Fernando Besteiro <fernando.besteiro@southworks.com>
2018-08-24 16:08:48 -0300

    Added help descrpition for streaming policy name on streaming locator create help

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_sdk_utils.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_sdk_utils.py
@@ -28,3 +28,8 @@ def get_stand_alone_presets():
 
 def get_cdn_providers():
     return ['StandardVerizon', 'PremiumVerizon', 'StandardAkamai']
+
+
+def get_default_streaming_policies():
+    return ['Predefined_DownloadOnly', 'Predefined_ClearStreamingOnly', 'Predefined_DownloadAndClearStreaming',
+            'Predefined_ClearKey', 'Predefined_SecureStreaming', 'Predefined_SecureStreamingWithFairPlay']
