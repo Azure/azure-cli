8bcde1bf1f805f6c2816cef176f81f765eb322de
Ray Wu <lewu@microsoft.com>
2018-08-24 15:28:58 -0700

    fix CI

--- src/command_modules/azure-cli-dls/azure/cli/command_modules/dls/_help.py
+++ src/command_modules/azure-cli-dls/azure/cli/command_modules/dls/_help.py
@@ -125,13 +125,6 @@ helps['dls account network-rule create'] = """
 helps['dls account network-rule update'] = """
     type: command
     short-summary: Updates a virtual network rule in a Data Lake Store account.
-    parameters:
-        - name: --subnet
-          type: string
-          short-summary: 'The subnet name or id for the virtual network rule.'
-        - name: --vnet-name
-          type: string
-          short-summary: 'The name of the virtual network rule.'
 """
 
 helps['dls account network-rule show'] = """
