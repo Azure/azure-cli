041dcce4a16137eb5c8725c672aba3458f53b781
Fernando Besteiro <fernando.besteiro@southworks.com>
2018-08-27 17:30:51 -0300

    Dummy command is now working

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_params.py
@@ -131,6 +131,11 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
     with self.argument_context('ams job cancel') as c:
         c.argument('delete', action='store_true', help='Delete the job being cancelled.')
 
+    with self.argument_context('ams content-key-policy') as c:
+        c.argument('account_name', account_name_arg_type)
+        c.argument('content_key_policy_name', name_arg_type)
+        c.argument('description', help='The content key policy description.')
+
     with self.argument_context('ams streaming') as c:
         c.argument('account_name', account_name_arg_type)
         c.argument('default_content_key_policy_name', default_policy_name_arg_type)
--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/content_key_policy.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/content_key_policy.py
@@ -1,2 +1,17 @@
-def create_content_key_policy():
-    print('hi')
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+def create_content_key_policy(client, resource_group_name, account_name,
+                              content_key_policy_name, description=None):
+    from azure.mgmt.media.models import (ContentKeyPolicyOption, ContentKeyPolicyClearKeyConfiguration,
+                                         ContentKeyPolicyOpenRestriction)
+
+    options = [ContentKeyPolicyOption(name='test',
+                                      configuration=ContentKeyPolicyClearKeyConfiguration(),
+                                      restriction=ContentKeyPolicyOpenRestriction())]
+
+    return client.create_or_update(resource_group_name, account_name,
+                                   content_key_policy_name, options, description)
