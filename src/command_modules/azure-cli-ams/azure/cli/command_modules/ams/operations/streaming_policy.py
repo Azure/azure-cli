de2acabcf04ca0865407befe58eb6869f1c3f3d8
damatojuan <juanignaciodamato@gmail.com>
2018-08-28 13:45:03 -0300

    Add streaming locator get-content-keys command. Separate streaming operation files

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_policy.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/streaming_policy.py
@@ -4,22 +4,6 @@
 # --------------------------------------------------------------------------------------------
 
 
-def create_streaming_locator(client, resource_group_name, account_name,
-                             streaming_locator_name, streaming_policy_name,
-                             asset_name, start_time=None, default_content_key_policy_name=None,
-                             end_time=None, streaming_locator_id=None, alternative_media_id=None):
-    from azure.mgmt.media.models import StreamingLocator
-
-    streaming_locator = StreamingLocator(asset_name=asset_name,
-                                         start_time=start_time, end_time=end_time,
-                                         streaming_policy_name=streaming_policy_name,
-                                         default_content_key_policy_name=default_content_key_policy_name,
-                                         streaming_locator_id=streaming_locator_id,
-                                         alternative_media_id=alternative_media_id)
-
-    return client.create(resource_group_name, account_name, streaming_locator_name, streaming_locator)
-
-
 def create_streaming_policy(cmd, resource_group_name, account_name,
                             streaming_policy_name,
                             download=False, dash=False, hls=False, smooth_streaming=False,
