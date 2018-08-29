f3531af6e8af6e46cdde93b567858d70935a6e97
Chip Locke <wd@teleri.net>
2018-08-28 20:42:30 -0700

    Reduce line lengths.

--- src/azure-cli-core/azure/cli/core/_session.py
+++ src/azure-cli-core/azure/cli/core/_session.py
@@ -48,14 +48,15 @@ class Session(collections.MutableMapping):
             with codecs_open(self.filename, 'r', encoding=self._encoding) as f:
                 self.data = json.load(f)
         except (OSError, IOError, t_JSONDecodeError) as load_exception:
-            # OSError / IOError should imply file not found issues which are expected on fresh runs (e.g. on build agents
-            # or new systems). A parse error indicates invalid/bad data in the file. We do not wish to warn on missing
-            # files since we expect that, but do if the data isn't as expected.
+            # OSError / IOError should imply file not found issues which are expected on fresh runs (e.g. on build
+            # agents or new systems). A parse error indicates invalid/bad data in the file. We do not wish to warn
+            # on missing files since we expect that, but do if the data isn't parsing as expected.
             log_level = logging.INFO
             if isinstance(load_exception, t_JSONDecodeError):
                 log_level = logging.WARNING
 
-            get_logger(__name__).log(log_level, "Failed to load or parse file %s. It will be overridden by default settings.",
+            get_logger(__name__).log(log_level,
+                                     "Failed to load or parse file %s. It will be overridden by default settings.",
                                      self.filename)
             self.save()
 
