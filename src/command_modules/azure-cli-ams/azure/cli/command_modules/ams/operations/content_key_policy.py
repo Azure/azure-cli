5a795bd226c5de3823b404044146a07a40b2789e
Fernando Besteiro <fernando.besteiro@southworks.com>
2018-08-30 15:54:44 -0300

    Added parameters for add to allow token restriction

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/content_key_policy.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/content_key_policy.py
@@ -25,7 +25,14 @@ def create_content_key_policy(client, resource_group_name, account_name, content
     return client.create_or_update(resource_group_name, account_name,
                                    content_key_policy_name, options, description)
 
-def add_content_key_policy_option(client):
+def add_content_key_policy_option(client, resource_group_name, account_name, content_key_policy_name,
+                                  clear_key_configuration=False, open_restriction=False, 
+                                  issuer=None, audience=None, symmetric_token_key=None,
+                                  rsa_token_key_exponent=None, rsa_token_key_modulus=None,
+                                  x509_certificate_token_key=None, alt_symmetric_token_key=None,
+                                  alt_rsa_token_key_exponent=None, alt_rsa_token_key_modulus=None,
+                                  alt_x509_certificate_token_key=None, token_claims=None,
+                                  restriction_token_type=None, open_id_connect_discovery_document=None):
     print(' ok')
 
 def remove_content_key_policy_option(client):
