da2168fef826ffcdb38d90a488be545cad4d1bea
williexu <wx44@cornell.edu>
2018-09-06 17:17:31 -0700

    fixes to allow for all properties to be generically updated

--- src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/operations/blob.py
+++ src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/operations/blob.py
@@ -48,16 +48,11 @@ def set_delete_policy(client, enable=None, days_retained=None):
 def set_service_properties(client, parameters, delete_retention=None, days_retained=None, static_website=None,
                            index_document=None, error_document_404_path=None):
     # update
-    kwargs = {}
-    if any([delete_retention, days_retained]):
-        kwargs['delete_retention_policy'] = parameters.delete_retention_policy
     if delete_retention is not None:
         parameters.delete_retention_policy.enabled = delete_retention
     if days_retained is not None:
         parameters.delete_retention_policy.days = days_retained
 
-    if any([static_website, index_document, error_document_404_path]):
-        kwargs['static_website'] = parameters.static_website
     if static_website is not None:
         parameters.static_website.enabled = static_website
     if index_document is not None:
@@ -70,6 +65,10 @@ def set_service_properties(client, parameters, delete_retention=None, days_retai
     if policy.enabled and not policy.days:
         from knack.util import CLIError
         raise CLIError("must specify days-retained")
+
+    kwargs = {}
+    for parameter in parameters.__dict__:
+        kwargs[parameter] = getattr(parameters, parameter)
 
     client.set_blob_service_properties(**kwargs)
     return client.get_blob_service_properties()
