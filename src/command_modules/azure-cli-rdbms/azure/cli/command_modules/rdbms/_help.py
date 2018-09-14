a02e740539941623fe1fc366c74365e2e8f3ece4
Shuode Li <shuodl@microsoft.com>
2018-09-13 20:48:29 -0700

    Add MariaDB service in module rdbms

--- src/command_modules/azure-cli-rdbms/azure/cli/command_modules/rdbms/_help.py
+++ src/command_modules/azure-cli-rdbms/azure/cli/command_modules/rdbms/_help.py
@@ -118,23 +118,25 @@ def add_helps(command_group, server_type):
                 type: command
                 short-summary: List all firewall rules for a server.
                 """
-    helps['{} server vnet-rule'.format(command_group)] = """
-                type: group
-                short-summary: Manage a server's virtual network rules.
-                """
-    helps['{} server vnet-rule update'.format(command_group)] = """
-                type: command
-                short-summary: Update a virtual network rule.
-                """
-    helps['{} server vnet-rule create'.format(command_group)] = """
-                type: command
-                short-summary: Create a virtual network rule to allows access to a {} server.
-                examples:
-                    - name: Create a virtual network rule by providing the subnet id.
-                      text: az {} server vnet-rule create -g testgroup -s testsvr -n vnetRuleName --subnet /subscriptions/{{SubID}}/resourceGroups/{{ResourceGroup}}/providers/Microsoft.Network/virtualNetworks/vnetName/subnets/subnetName
-                    - name: Create a vnet rule by providing the vnet and subnet name. The subnet id is created by taking the resource group name and subscription id of the server.
-                      text: az {} server vnet-rule create -g testgroup -s testsvr -n vnetRuleName --subnet subnetName --vnet-name vnetName
-                """.format(server_type, command_group, command_group)
+    if server_type != "MariaDB":
+        helps['{} server vnet-rule'.format(command_group)] = """
+                    type: group
+                    short-summary: Manage a server's virtual network rules.
+                    """
+        helps['{} server vnet-rule update'.format(command_group)] = """
+                    type: command
+                    short-summary: Update a virtual network rule.
+                    """
+        helps['{} server vnet-rule create'.format(command_group)] = """
+                    type: command
+                    short-summary: Create a virtual network rule to allows access to a {} server.
+                    examples:
+                        - name: Create a virtual network rule by providing the subnet id.
+                          text: az {} server vnet-rule create -g testgroup -s testsvr -n vnetRuleName --subnet /subscriptions/{{SubID}}/resourceGroups/{{ResourceGroup}}/providers/Microsoft.Network/virtualNetworks/vnetName/subnets/subnetName
+                        - name: Create a vnet rule by providing the vnet and subnet name. The subnet id is created by taking the resource group name and subscription id of the server.
+                          text: az {} server vnet-rule create -g testgroup -s testsvr -n vnetRuleName --subnet subnetName --vnet-name vnetName
+                    """.format(server_type, command_group, command_group)
+
     helps['{} server configuration'.format(command_group)] = """
                 type: group
                 short-summary: Manage configuration values for a server.
@@ -214,5 +216,6 @@ def add_helps(command_group, server_type):
                 """.format(command_group)
 
 
+add_helps("mariadb", "MariaDB")
 add_helps("mysql", "MySQL")
 add_helps("postgres", "PostgreSQL")
