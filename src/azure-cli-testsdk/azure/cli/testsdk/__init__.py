eeee1a654df901b6e01e697b8b2b02c05a9ed5fc
Julien Stroheker <julienstroheker@gmail.com>
2018-09-27 18:10:19 -0400

    lint

--- src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
@@ -15,9 +15,10 @@ from .decorators import api_version_constraint
 from .utilities import create_random_name
 
 __all__ = ['ScenarioTest', 'LiveScenarioTest', 'ResourceGroupPreparer', 'StorageAccountPreparer',
-           'RoleBasedServicePrincipalPreparer', 'ManagedApplicationPreparer', 'CliTestError', 'JMESPathCheck', 'JMESPathCheckExists', 'NoneCheck',
-           'live_only', 'record_only', 'StringCheck', 'StringContainCheck', 'get_sha1_hash', 'KeyVaultPreparer',
-           'JMESPathCheckGreaterThan', 'api_version_constraint', 'create_random_name']
+           'RoleBasedServicePrincipalPreparer', 'ManagedApplicationPreparer', 'CliTestError', 'JMESPathCheck',
+           'JMESPathCheckExists', 'NoneCheck', 'live_only', 'record_only', 'StringCheck', 'StringContainCheck',
+           'get_sha1_hash', 'KeyVaultPreparer', 'JMESPathCheckGreaterThan', 'api_version_constraint',
+           'create_random_name']
 
 
 __version__ = '0.1.0'
