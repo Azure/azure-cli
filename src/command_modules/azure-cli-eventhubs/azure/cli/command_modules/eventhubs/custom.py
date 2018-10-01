bc7fafd156549923b7bb2f28826fa884e15b9469
Ajit Navasare <v-ajnava@microsoft.com>
2018-10-01 09:12:09 -0700

    Eventhub : implemented exception handler (#7253)
    * exception handler
    
    * updated version and history
    
    * exception handlers in one file
    
    * fixed issue found in eventhub update command
    
    * updated history
    
    * Update test_eventhub_commands.py
    
    * removed exception handler for bad request
    
    * fix lint errors
    
    * updated tests
    
    * updated failing test
    
    * updated history and custom
    
    * fix pep8 error
    
    * updated history for breaking change as removed the list 404 exception handler
    
    * updated setup version

--- src/command_modules/azure-cli-eventhubs/azure/cli/command_modules/eventhubs/custom.py
+++ src/command_modules/azure-cli-eventhubs/azure/cli/command_modules/eventhubs/custom.py
@@ -94,7 +94,7 @@ def cli_eheventhub_update(instance, message_retention_in_days=None, partition_co
                           enabled=None, capture_interval_seconds=None,
                           capture_size_limit_bytes=None, destination_name=None, storage_account_resource_id=None,
                           blob_container=None, archive_name_format=None):
-
+    from azure.mgmt.eventhub.models import CaptureDescription, Destination, EncodingCaptureDescription
     if message_retention_in_days:
         instance.message_retention_in_days = message_retention_in_days
 
@@ -104,28 +104,25 @@ def cli_eheventhub_update(instance, message_retention_in_days=None, partition_co
     if status:
         instance.status = status
 
-    if instance.enabled is True or enabled is True:
-        if enabled:
-            instance.capture_description.enabled = enabled
+    if enabled:
+        instance.capture_description.enabled = enabled
+
+        if not instance.capture_description:
+            instance.capture_description = CaptureDescription()
+            instance.capture_description.destination = Destination()
+            instance.capture_description.encoding = EncodingCaptureDescription.avro
+
         if capture_interval_seconds:
             instance.interval_in_seconds = capture_interval_seconds
         if capture_size_limit_bytes:
             instance.size_limit_in_bytes = capture_size_limit_bytes
         if destination_name:
-            instance.destination = destination_name
+            instance.capture_description.destination.name = destination_name
         if storage_account_resource_id:
-            instance.storage_account_resource_id = storage_account_resource_id
+            instance.capture_description.destination.storage_account_resource_id = storage_account_resource_id
         if blob_container:
-            instance.blob_container = blob_container
+            instance.capture_description.destination.blob_container = blob_container
         if archive_name_format:
-            instance.archive_name_format = archive_name_format
+            instance.capture_description.destination.archive_name_format = archive_name_format
 
     return instance
-
-
-# pylint: disable=inconsistent-return-statements
-def empty_on_404(ex):
-    from azure.mgmt.eventhub.models import ErrorResponseException
-    if isinstance(ex, ErrorResponseException) and ex.response.status_code == 404:
-        return None
-    raise ex
