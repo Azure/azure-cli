0b5b1df7a4d05de304912de131eefd431c3f3f2b
yugangw-msft <yugangw@microsoft.com>
2018-10-05 15:32:46 -0700

    vm/vmss: support cross tenant image reference

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_params.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_params.py
@@ -326,6 +326,7 @@ def load_arguments(self, _):
         c.argument('eviction_policy', resource_type=ResourceType.MGMT_COMPUTE, min_api='2017-12-01', arg_type=get_enum_type(VirtualMachineEvictionPolicyTypes, default=None),
                    help="(PREVIEW) The eviction policy for virtual machines in a low priority scale set.")
         c.argument('application_security_groups', resource_type=ResourceType.MGMT_COMPUTE, min_api='2018-06-01', nargs='+', options_list=['--asgs'], help='Space-separated list of existing application security groups to associate with the VM.', arg_group='Network', validator=validate_asg_names_or_ids)
+        c.ignore('aux_subscriptions') 
 
     with self.argument_context('vmss create', arg_group='Network Balancer') as c:
         LoadBalancerSkuName = self.get_models('LoadBalancerSkuName', resource_type=ResourceType.MGMT_NETWORK)
