211d33e06cd5fca92d9e0f6f98f4b459319d0deb
damatojuan <juanignaciodamato@gmail.com>
2018-10-10 11:53:38 -0300

    Fix CI errors

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_exception_handler.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/_exception_handler.py
@@ -3,14 +3,18 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import sys
+
 
 def ams_exception_handler(ex):
-    from azure.mgmt.media.models.api_error_py3 import ApiErrorException as ApiErrorExceptionPy3
-    from azure.mgmt.media.models.api_error import ApiErrorException
+    if sys.version_info.major < 3:
+        from azure.mgmt.media.models.api_error import ApiErrorException
+    else:
+        from azure.mgmt.media.models.api_error_py3 import ApiErrorException
     from msrest.exceptions import ValidationError
     from knack.util import CLIError
 
-    if isinstance(ex, (ApiErrorException, ApiErrorExceptionPy3)) and ex.message:
+    if isinstance(ex, ApiErrorException) and ex.message:
         raise CLIError(ex.message)
     if isinstance(ex, (ValidationError, IOError, ValueError)):
         raise CLIError(ex)
