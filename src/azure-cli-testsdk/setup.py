b3763c4a31014e9b4915a094b90d24c2304580c3
Yugang Wang <yugangw@microsoft.com>
2018-10-12 14:15:58 -0700

    [test]: introduce new replacer to scrub away the storage keys (#7547)

--- src/azure-cli-testsdk/setup.py
+++ src/azure-cli-testsdk/setup.py
@@ -14,7 +14,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.2.1"
+VERSION = "0.2.2"
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
