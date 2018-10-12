26faf83340f87dde6a55c2eca1dd79d3df4179a4
Travis Prescott <tjprescott@users.noreply.github.com>
2018-10-12 10:30:19 -0700

    Closes #7552. (#7553)

--- src/azure-cli-core/azure/cli/core/commands/arm.py
+++ src/azure-cli-core/azure/cli/core/commands/arm.py
@@ -330,14 +330,13 @@ remove_usage = '--remove property.list <indexToRemove> OR --remove propertyToRem
 
 
 def _get_child(parent, collection_name, item_name, collection_key):
+    if not item_name:
+        raise CLIError("Name property for collection '{}' not provided. Check your input.".format(collection_name))
     items = getattr(parent, collection_name)
-    result = next((x for x in items if getattr(x, collection_key, '').lower() ==
-                   item_name.lower()), None)
+    result = next((x for x in items if getattr(x, collection_key, '').lower() == item_name.lower()), None)
     if not result:
-        raise CLIError("Property '{}' does not exist for key '{}'.".format(
-            item_name, collection_key))
-    else:
-        return result
+        raise CLIError("Property '{}' does not exist for key '{}'.".format(item_name, collection_key))
+    return result
 
 
 def _get_operations_tmpl(cmd, custom_command=False):
