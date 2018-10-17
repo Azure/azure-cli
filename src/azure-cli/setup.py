8cab4b0d674e914bca62df4959d3b0b1b793f2b2
yugangw-msft <yugangw@microsoft.com>
2018-10-17 13:08:12 -0700

    cap the urllib3 version to workaround a known issue at requests

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -100,6 +100,8 @@ DEPENDENCIES = [
     'azure-cli-sql',
     'azure-cli-storage',
     'azure-cli-vm'
+    'urllib3==1.23',
+    'urllib3[secure]==1.23'
 ]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
