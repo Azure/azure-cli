b59fddc1c763f9afa3a7a0288a83dd77793da43b
Sam Kreter <samkreter@gmail.com>
2018-10-18 12:29:10 -0700

    Removing Warnings for Cloudshell Proxy (#7605)
    * removing warnings for cloudshell proxy
    
    * updating code owners for the container modules
    
    * Updating how env var is accessed
    
    * fixing formatting error in string message
    
    * removing unused import

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
@@ -31,7 +31,6 @@ import dateutil.parser
 from dateutil.relativedelta import relativedelta
 from knack.log import get_logger
 from knack.util import CLIError
-from knack.prompting import prompt_y_n
 from msrestazure.azure_exceptions import CloudError
 import requests
 
@@ -1357,16 +1356,14 @@ def aks_browse(cmd, client, resource_group_name, name, disable_browser=False, li
         raise CLIError("Couldn't find the Kubernetes dashboard pod.")
     # launch kubectl port-forward locally to access the remote dashboard
     if in_cloud_console() and enable_cloud_console_aks_browse:
-        logger.warning('***WARNING***')
-        logger.warning('Browsing the kubernetes dashboard in Cloud Shell is an alpha feature.')
-        logger.warning('The browse URL is currently obfuscated but not authenticated.')
-        logger.warning('Do not share this URL.')
-        if not prompt_y_n('Proceed?'):
-            raise CLIError("Browse aborted.")
         # TODO: better error handling here.
         response = requests.post('http://localhost:8888/openport/8001')
         result = json.loads(response.text)
-        logger.warning('To view the console, please open % in a new tab', result['url'])
+        term_id = os.environ.get('ACC_TERM_ID')
+        if term_id:
+            response = requests.post('http://localhost:8888/openLink/{}'.format(term_id),
+                                     json={"url": result['url']})
+        logger.warning('To view the console, please open %s in a new tab', result['url'])
     else:
         logger.warning('Proxy running on %s', proxy_url)
 
