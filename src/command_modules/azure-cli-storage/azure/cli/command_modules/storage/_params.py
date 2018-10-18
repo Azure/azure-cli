aa9e358162f723b90a50c9ac1aeca1ce6c8d442c
Willie Xu <wx44@cornell.edu>
2018-10-17 18:34:01 -0700

    [Storage] allow deletion of containers with immutability policy after service bug fix (#7569)
    * delete container to custom command
    
    * finished up commands
    
    * added test
    
    * history and pep8
    
    * linter, static
    
    * new recording after scrubbing fix
    
    * comment typo

--- src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/_params.py
+++ src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/_params.py
@@ -370,6 +370,14 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
 
     with self.argument_context('storage container delete') as c:
         c.argument('fail_not_exist', help='Throw an exception if the container does not exist.')
+        c.argument('bypass_immutability_policy', action='store_true', help='Bypasses upcoming service behavior that '
+                   'will block a container from being deleted if it has a immutability-policy. Specifying this will '
+                   'ignore arguments aside from those used to identify the container ("--name", "--account-name").')
+        c.argument('lease_id', help="If specified, delete_container only succeeds if the container's lease is active "
+                                    "and matches this ID. Required if the container has an active lease.")
+        c.ignore('processed_resource_group')
+        c.ignore('processed_account_name')
+        c.ignore('mgmt_client')
 
     with self.argument_context('storage container exists') as c:
         c.ignore('blob_name', 'snapshot')
