43a036f610175e8aed2d80fffacadcc0ce359f97
Oluwatosin Adewale <oluwatosin.adewale@microsoft.com>
2018-10-19 14:30:35 -0700

    Help conversion script uses ruamel.yaml. Output is ordered and visually appealing. '>' and '|' (part of yaml syntax) are preserved in values.

--- scripts/help_convert.py
+++ scripts/help_convert.py
@@ -6,7 +6,13 @@
 import sys
 from importlib import  import_module
 import os
-import yaml
+
+try:
+    from ruamel.yaml import YAML
+    yaml = YAML()
+except ImportError as e:
+    msg = "{}\npip install ruamel.Yaml to use this script.".format(e)
+    exit(msg)
 
 PACKAGE_PREFIX = "azure.cli.command_modules"
 
@@ -19,7 +25,7 @@ def convert(target_mod):
     out_file = os.path.join(os.path.dirname(loader_file_path),"help.yaml")
 
     with open(out_file, "w") as f:
-        yaml.dump(result, f, default_flow_style=False)
+        yaml.dump(result, f)
 
 def _get_new_yaml_dict(help_dict):
 
@@ -34,18 +40,48 @@ def _get_new_yaml_dict(help_dict):
         elem = {type: dict(name=command_or_group)}
         elem_content = elem[type]
 
-        if "short-summary" in help_dict:
-            elem_content["summary"] = help_dict["short-summary"]
+        _convert_summaries(old_dict=help_dict, new_dict=elem_content)
 
-        if "long-summary" in help_dict:
-            elem_content["description"] = help_dict["long-summary"]
+        if "examples" in help_dict:
+            elem_examples = []
+            for ex in help_dict["examples"]:
+                new_ex = dict()
+                if "name" in ex:
+                    new_ex["summary"] = ex["name"]
+                if "text" in ex:
+                    new_ex["command"] = ex["text"]
+                if "min_profile" in ex:
+                    new_ex["min_profile"] = ex["min_profile"]
+                if "max_profile" in ex:
+                    new_ex["max_profile"] = ex["max_profile"]
+                elem_examples.append(new_ex)
+            elem_content["examples"] = elem_examples
 
-
+        if "parameters" in help_dict:
+            parameters = []
+            for param in help_dict["parameters"]:
+                new_param = dict()
+                if "name" in param:
+                    options = param["name"].split()
+                    option = max(options, key = lambda x: len(x))
+                    new_param["name"] = option.lstrip('-')
+                if "populator-commands" in param:
+                    new_param["value-source"] = []
+                    for item in param["populator-commands"]:
+                        new_param["value-source"].append(dict(string=item))
+                _convert_summaries(old_dict=param, new_dict=new_param)
+                parameters.append(new_param)
+            help_dict["parameters"] = parameters
 
         content.append(elem)
 
     return result
 
+def _convert_summaries(old_dict, new_dict):
+    if "short-summary" in old_dict:
+        new_dict["summary"] = old_dict["short-summary"]
+    if "long-summary" in old_dict:
+        new_dict["description"] = old_dict["long-summary"]
 
 if __name__ == "__main__":
     args = sys.argv[1:]
