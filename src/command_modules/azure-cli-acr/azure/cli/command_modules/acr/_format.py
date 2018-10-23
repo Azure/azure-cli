52180d1b522a0692652f95393e0b5b4b0ebecb05
Jeanine Burke <41650461+jaysterp@users.noreply.github.com>
2018-10-23 15:57:57 -0700

    ACR: Add Pull Request Event to Task Source Trigger (#7526)
    * initial work on the PR trigger
    
    * task create and update with PR trigger
    
    * use one flag to enable commit and pr trigger events
    
    * nit fixes
    
    * Two event flags and only support one source trigger per task
    
    * PR fixes
    
    * allow empty event list when disabling all trigger events
    
    * format fix
    
    * do not clear event list in update, instead status disabled
    
    * update task table
    
    * nit fix
    
    * bump cli event version
    
    * nit fixes
    
    * update ACR sdk version
    
    * version fix
    
    * update appservice version
    
    * pylint fixes
    
    * pylint fixes
    
    * nit
    
    * pylint fix
    
    * respond to PR comments
    
    * change from null to empty check

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_format.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_format.py
@@ -199,7 +199,7 @@ def _task_format_group(item):
         ('Name', _get_value(item, 'name')),
         ('PLATFORM', _get_value(item, 'platform', 'os')),
         ('STATUS', _get_value(item, 'status')),
-        ('COMMIT TRIGGER', _get_value(item, 'trigger', 'sourceTriggers', 0, 'status')),
+        ('SOURCE TRIGGER', _get_value(item, 'trigger', 'sourceTriggers', 0, 'status')),
         ('SOURCE REPOSITORY', _get_value(item, 'step', 'contextPath')),
         ('BRANCH', _get_value(item, 'trigger', 'sourceTriggers', 0, 'sourceRepository', 'branch')),
         ('BASE IMAGE TRIGGER', _get_value(item, 'trigger', 'baseImageTrigger', 'baseImageTriggerType')),
@@ -214,7 +214,8 @@ def _build_format_group(item):
         ('TASK', _get_value(item, 'buildTask')),
         ('PLATFORM', _get_value(item, 'platform', 'osType')),
         ('STATUS', _get_value(item, 'status')),
-        ("TRIGGER", _get_build_trigger(_get_value(item, 'imageUpdateTrigger'), _get_value(item, 'gitCommitTrigger'))),
+        ("TRIGGER", _get_build_trigger(_get_value(item, 'imageUpdateTrigger'),
+                                       _get_value(item, 'sourceTrigger', 'eventType'))),
         ('STARTED', _format_datetime(_get_value(item, 'startTime'))),
         ('DURATION', _get_duration(_get_value(item, 'startTime'), _get_value(item, 'finishTime')))
     ])
@@ -226,7 +227,8 @@ def _run_format_group(item):
         ('TASK', _get_value(item, 'task')),
         ('PLATFORM', _get_value(item, 'platform', 'os')),
         ('STATUS', _get_value(item, 'status')),
-        ("TRIGGER", _get_build_trigger(_get_value(item, 'imageUpdateTrigger'), _get_value(item, 'sourceTrigger'))),
+        ("TRIGGER", _get_build_trigger(_get_value(item, 'imageUpdateTrigger'),
+                                       _get_value(item, 'sourceTrigger', 'eventType'))),
         ('STARTED', _format_datetime(_get_value(item, 'startTime'))),
         ('DURATION', _get_duration(_get_value(item, 'startTime'), _get_value(item, 'finishTime')))
     ])
@@ -257,9 +259,9 @@ def _get_value(item, *args):
         return ' '
 
 
-def _get_build_trigger(image_update_trigger, git_commit_trigger):
-    if git_commit_trigger.strip():
-        return 'Git Commit'
+def _get_build_trigger(image_update_trigger, git_source_trigger):
+    if git_source_trigger.strip():
+        return git_source_trigger
     if image_update_trigger.strip():
         return 'Image Update'
     return 'Manual'
--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
@@ -207,6 +207,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-statements
         # Source Trigger parameters
         c.argument('source_trigger_name', help="The name of the source trigger.")
         c.argument('commit_trigger_enabled', help="Indicates whether the source control commit trigger is enabled.", arg_type=get_three_state_flag())
+        c.argument('pull_request_trigger_enabled', help="Indicates whether the source control pull request trigger is enabled.", arg_type=get_three_state_flag())
         c.argument('git_access_token', help="The access token used to access the source control provider.")
         c.argument('branch', help="The source control branch name.")
         c.argument('base_image_trigger_name', help="The name of the base image trigger.")
