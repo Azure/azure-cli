4ddab33d835c74421f4a87d6223112eb29c17ec2
adewaleo <oladewal@microsoft.com>
2018-10-24 12:33:34 -0700

    Fixed bug with update --remove --ids (#7643)
    * Fixed bug where if update --remove is called with --ids, the correct params aren't passed in subsequent iterations of the update command.
    
    * Fixed Pep8 issues
    
    * Fixed bug by changing generic update logic instead of copying CLI params.

--- src/azure-cli-core/azure/cli/core/commands/arm.py
+++ src/azure-cli-core/azure/cli/core/commands/arm.py
@@ -794,6 +794,7 @@ def set_properties(instance, expression, force_string):
 
 def add_properties(instance, argument_values, force_string):
     # The first argument indicates the path to the collection to add to.
+    argument_values = list(argument_values)
     list_attribute_path = _get_internal_path(argument_values.pop(0))
     list_to_add_to = _find_property(instance, list_attribute_path)
 
@@ -833,8 +834,8 @@ def add_properties(instance, argument_values, force_string):
 
 
 def remove_properties(instance, argument_values):
-    # The first argument indicates the path to the collection to add to.
-    argument_values = argument_values if isinstance(argument_values, list) else [argument_values]
+    # The first argument indicates the path to the collection to remove from.
+    argument_values = list(argument_values) if isinstance(argument_values, list) else [argument_values]
 
     list_attribute_path = _get_internal_path(argument_values.pop(0))
     list_index = None
