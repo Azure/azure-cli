c5b63ceafd0c5800ee2918d0158785ac54670aed
Suna Liu <sunl@microsoft.com>
2018-10-26 23:33:25 +0800

    Add mysql replica commands (#7540)
    * Add mysql replica commands
    
    * Fix comment: Catch CloudError instead of Exception for get server
    
    * change rdbms python sdk to 1.4.1

--- src/command_modules/azure-cli-rdbms/azure/cli/command_modules/rdbms/_client_factory.py
+++ src/command_modules/azure-cli-rdbms/azure/cli/command_modules/rdbms/_client_factory.py
@@ -167,3 +167,7 @@ def cf_mysql_virtual_network_rules_operations(cli_ctx, _):
 
 def cf_postgres_virtual_network_rules_operations(cli_ctx, _):
     return get_postgresql_management_client(cli_ctx).virtual_network_rules
+
+
+def cf_mysql_replica(cli_ctx, _):
+    return get_mysql_management_client(cli_ctx).replicas
