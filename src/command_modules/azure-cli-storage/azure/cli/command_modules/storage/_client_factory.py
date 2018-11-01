e88e0e0120e8af160071d973a31020201fda1930
Willie Xu <wx44@cornell.edu>
2018-10-31 18:04:44 -0700

    update msg (#7735)

--- src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/_client_factory.py
+++ src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/_client_factory.py
@@ -12,10 +12,12 @@ NO_CREDENTIALS_ERROR_MESSAGE = """
 No credentials specified to access storage service. Please provide any of the following:
     (1) account name and key (--account-name and --account-key options or
         set AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY environment variables)
-    (2) connection string (--connection-string option or
-        set AZURE_STORAGE_CONNECTION_STRING environment variable)
-    (3) account name and SAS token (--sas-token option used with either the --account-name
+    (2) account name and SAS token (--sas-token option used with either the --account-name
         option or AZURE_STORAGE_ACCOUNT environment variable)
+    (3) account name (--account-name option or AZURE_STORAGE_ACCOUNT environment variable;
+        this will make calls to query for a storage account key using login credentials)
+    (4) connection string (--connection-string option or
+        set AZURE_STORAGE_CONNECTION_STRING environment variable)
 """
 
 
