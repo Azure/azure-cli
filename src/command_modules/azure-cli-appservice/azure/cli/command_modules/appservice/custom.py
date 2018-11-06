c5622ad43ea55aec44b997b2e6b6fbc0092395bb
Patrick <31744877+patricklee2@users.noreply.github.com>
2018-11-05 18:06:37 -0800

    fix get_fx_version returning null (#7762)

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/custom.py
@@ -576,7 +576,7 @@ def _delete_linux_fx_version(cmd, resource_group_name, name, slot=None):
 
 def _get_fx_version(cmd, resource_group_name, name, slot=None):
     site_config = get_site_configs(cmd, resource_group_name, name, slot)
-    return site_config.linux_fx_version or site_config.windows_fx_version
+    return site_config.linux_fx_version or site_config.windows_fx_version or ''
 
 
 def url_validator(url):
