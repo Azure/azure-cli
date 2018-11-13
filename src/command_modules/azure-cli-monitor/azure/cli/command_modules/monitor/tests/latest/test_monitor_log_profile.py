b1a184ac4546992d2aded4d10a395585e79ea09b
Travis Prescott <trpresco@microsoft.com>
2018-11-13 10:19:25 -0800

    Monitor live-test fixes.

--- src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/tests/latest/test_monitor_log_profile.py
+++ src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/tests/latest/test_monitor_log_profile.py
@@ -10,16 +10,13 @@ class TestLogProfileScenarios(ScenarioTest):
     @ResourceGroupPreparer(location='southcentralus')
     @StorageAccountPreparer(location='southcentralus')
     def test_monitor_create_log_profile(self, resource_group, storage_account):
-        self.kwargs['name'] = self.create_random_name('clitest', 20)
+        self.kwargs.update({
+            'name': self.create_random_name('clitest', 20)
+        })
         self.kwargs['storage'] = self.cmd('storage account show -n {sa} -g {rg} --query id -otsv').output.strip()
-
-        cmd = "monitor log-profiles create --categories 'Write' --enabled false --days 1095 --location southcentralus" \
-              " --locations westus southcentralus --name {name} --storage-account-id {storage} -ojson"
-
-        created = self.cmd(cmd).get_output_in_json()
-
+        self.cmd("monitor log-profiles create --categories 'Write' --enabled false --days 1095 --location southcentralus --locations westus southcentralus --name {name} --storage-account-id {storage}", checks=[
+            self.check('storageAccountId', '{storage}'),
+            self.check('serviceBusRuleId', None),
+            self.check('retentionPolicy.enabled', 'False')
+        ])
         self.cmd("monitor log-profiles delete -n {name}")
-
-        self.assertEqual(created['storageAccountId'], self.kwargs['storage'])
-        self.assertIsNone(created['serviceBusRuleId'])
-        self.assertFalse(created['retentionPolicy']['enabled'])
