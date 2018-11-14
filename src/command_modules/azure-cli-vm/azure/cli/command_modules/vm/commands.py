f50cc58b2e123079fa7aa4d770872d9945461c0f
Travis Prescott <tjprescott@users.noreply.github.com>
2018-11-14 10:35:09 -0800

    Add wait/no-wait support for snapshots. (#7836)

--- src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/commands.py
+++ src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/commands.py
@@ -163,13 +163,14 @@ def load_command_table(self, _):
         g.command('delete', 'delete')
 
     with self.command_group('snapshot', compute_snapshot_sdk, operation_group='snapshots', min_api='2016-04-30-preview') as g:
-        g.custom_command('create', 'create_snapshot', validator=process_disk_or_snapshot_create_namespace)
+        g.custom_command('create', 'create_snapshot', validator=process_disk_or_snapshot_create_namespace, supports_no_wait=True)
         g.command('delete', 'delete')
         g.custom_command('grant-access', 'grant_snapshot_access')
         g.custom_command('list', 'list_snapshots')
         g.command('revoke-access', 'revoke_access')
         g.show_command('show', 'get')
-        g.generic_update_command('update', custom_func_name='update_snapshot', setter_arg_name='snapshot')
+        g.generic_update_command('update', custom_func_name='update_snapshot', setter_arg_name='snapshot', supports_no_wait=True)
+        g.wait_command('wait')
 
     with self.command_group('vm', compute_vm_sdk) as g:
         g.custom_command('identity assign', 'assign_vm_identity', validator=process_assign_identity_namespace)
