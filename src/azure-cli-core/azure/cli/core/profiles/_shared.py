0d0c5c4e5b99419cb19ba830e914ab35fd4f6da7
Willie Xu <wx44@cornell.edu>
2018-11-21 21:51:58 -0500

    [Storage] bump mgmt sdk version to 3.1.0 and set new api-version to use. (#7869)
    * changed api-version in recordings and set to new ver of sdk
    
    * version bump and history
    
    * fix immutability test
    
    * re-record all
    
    * fixed recordings
    
    fix recording
    
    * bumped version after release

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -79,7 +79,7 @@ class SDKProfile(object):  # pylint: disable=too-few-public-methods
 
 AZURE_API_PROFILES = {
     'latest': {
-        ResourceType.MGMT_STORAGE: '2018-03-01-preview',
+        ResourceType.MGMT_STORAGE: '2018-07-01',
         ResourceType.MGMT_NETWORK: '2018-08-01',
         ResourceType.MGMT_COMPUTE: SDKProfile('2018-10-01', {
             'resource_skus': '2017-09-01',
--- src/command_modules/azure-cli-servicefabric/setup.py
+++ src/command_modules/azure-cli-servicefabric/setup.py
@@ -16,7 +16,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.8"
+VERSION = "0.1.9"
 
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -39,7 +39,7 @@ DEPENDENCIES = [
     'azure-keyvault==1.1.0',
     'azure-mgmt-network==2.3.0',
     'azure-mgmt-compute==4.3.1',
-    'azure-mgmt-storage==2.0.0rc4',
+    'azure-mgmt-storage==3.1.0',
     'azure-mgmt-servicefabric==0.2.0',
     'azure-mgmt-keyvault==1.1.0',
     'azure-cli-core',
