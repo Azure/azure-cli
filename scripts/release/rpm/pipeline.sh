75191f82ebb762e9cb7d8ffc5f8d9f972a4e882b
Martin Strobel <marstr@microsoft.com>
2018-12-05 12:14:51 -0800

    Adding CentOS 7 Dockerfile

--- scripts/release/rpm/pipeline.sh
+++ scripts/release/rpm/pipeline.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
-# Build APT package in an Azure Container Instances
-# This script assumes the Azure CLI is installed and logged in.
+# Build an RPM containing the Azure CLI at a paritcular
 
 set -exv
 
 CLI_VERSION=`cat src/azure-cli/azure/cli/__init__.py | grep __version__ | sed s/' '//g | sed s/'__version__='// |  sed s/\"//g`
 
-docker run --rm \
-           -v "$BUILD_SOURCESDIRECTORY":/mnt/repo \
-           -v "$BUILD_STAGINGDIRECTORY":/mnt/output \
-           -e CLI_VERSION=$CLI_VERSION \
-           centos:7 \
-           /mnt/repo/scripts/release/rpm/build.sh
+docker build \
+    --target build-env \
+    --build-arg cli_version=${CLI_VERSION} \
+    -f ./scripts/release/rpm/Dockerfile \
+    -t microsoft/azure-cli:centos7-builder \
+    .
+
+docker run \
+    microsoft/azure-cli:centos7-builder \
+    cat /root/rpmbuild/RPMS/x86_64/azure-cli-${CLI_VERSION}-1.el7.x86_64.rpm \
+    > ${BUILD_STAGINDIRECTORY}/azure-cli-${CLI_VERSION}-1.el7.x86_64.rpm
