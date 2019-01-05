433859e1ab2bdff899a5ef013b2d105a930cdd04
Sisira Panchagnula <sisirap@microsoft.com>
2019-01-04 18:42:05 -0800

    Fixing the issue where on deployment status, deployment taking too long was showing up event the deployment actually failed & making the default SKU for linux to be P1V1, when used by up command
    Updating HISTORY
    
    Fixing static check failure
    
    Updating as a CLIError
    
    Updating history.rst
    
    Adding to the correct release
    
    updating value for while loop

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/_constants.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/_constants.py
@@ -16,3 +16,4 @@ STATIC_RUNTIME_NAME = "static"  # not an oficial supported runtime but used for 
 NODE_VERSIONS = ['4.4', '4.5', '6.2', '6.6', '6.9', '6.11', '8.0', '8.1', '8.9', '8.11']
 NETCORE_VERSIONS = ['1.0', '1.1', '2.0']
 DOTNET_VERSIONS = ['3.5', '4.7']
+LINUX_SKU_DEFAULT = "P1V2"
