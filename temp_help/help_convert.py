fceb34996bf6b9bbf7a61b825f35d149379eb2f9
Oluwatosin Adewale <oluwatosin.adewale@microsoft.com>
2019-01-18 19:36:29 -0800

    Optimize help conversion script. Added logic to convert all core modules.

--- temp_help/help_convert.py
+++ temp_help/help_convert.py
@@ -10,8 +10,11 @@ import subprocess
 import difflib
 from pprint import pprint
 
-from azure.cli.core import get_default_cli
-from azure.cli.core.file_util import get_all_help, create_invoker_and_load_cmds_and_args
+from knack.util import CLIError
+from azure.cli.core.mock import DummyCli
+from azure.cli.core.util import get_installed_cli_distributions
+from azure.cli.core._help import CliCommandHelpFile, CliGroupHelpFile
+from azure.cli.core.file_util import _store_parsers, _is_group
 
 try:
     from ruamel.yaml import YAML
@@ -21,23 +24,123 @@ except ImportError as e:
     exit(msg)
 
 PACKAGE_PREFIX = "azure.cli.command_modules"
+CLI_PACKAGE_NAME = 'azure-cli'
+COMPONENT_PREFIX = 'azure-cli-'
 
 failed = 0
 
+loaded_helps = {}
+
+
+def get_all_help(cli_ctx):
+
+    invoker = cli_ctx.invocation
+    help_ctx = cli_ctx.help_cls(cli_ctx)
+    if not invoker:
+        raise CLIError('CLI context does not contain invocation.')
+
+    parser_keys = []
+    parser_values = []
+    sub_parser_keys = []
+    sub_parser_values = []
+    _store_parsers(invoker.parser, parser_keys, parser_values, sub_parser_keys, sub_parser_values)
+    for cmd, parser in zip(parser_keys, parser_values):
+        if cmd not in sub_parser_keys:
+            sub_parser_keys.append(cmd)
+            sub_parser_values.append(parser)
+    help_files = []
+    for cmd, parser in zip(sub_parser_keys, sub_parser_values):
+        if cmd in loaded_helps:
+            try:
+                help_file = CliGroupHelpFile(help_ctx, cmd, parser) if _is_group(parser) \
+                    else CliCommandHelpFile(help_ctx, cmd, parser)
+                help_file.load(parser)
+                help_files.append(help_file)
+            except Exception as ex:  # pylint: disable=broad-except
+                print("Skipped '{}' due to '{}'".format(cmd, ex))
+    help_files = sorted(help_files, key=lambda x: x.command)
+    assert {help_file.command for help_file in help_files} == set(loaded_helps.keys())
+    print("Loaded {} help files".format(len(help_files)))
+    return help_files
+
+
+def create_invoker_and_load_cmds_and_args(cli_ctx):
+    global loaded_helps
+    from knack import events
+    from azure.cli.core.commands.arm import register_global_subscription_argument, register_ids_argument
+
+    invoker = cli_ctx.invocation_cls(cli_ctx=cli_ctx, commands_loader_cls=cli_ctx.commands_loader_cls,
+                                     parser_cls=cli_ctx.parser_cls, help_cls=cli_ctx.help_cls)
+    cli_ctx.invocation = invoker
+    invoker.commands_loader.skip_applicability = True
+    invoker.commands_loader.load_command_table(None)
+
+    # turn off applicability check for applicable loaders
+    new_cmd_to_loader_map = {}
+    new_command_group_table = {}
+    new_command_table = {}
+    for cmd in loaded_helps.keys():
+        if cmd in invoker.commands_loader.cmd_to_loader_map: # if a command from help, then update commands_loader
+            new_cmd_to_loader_map[cmd] = invoker.commands_loader.cmd_to_loader_map[cmd]
+            new_command_table[cmd] = invoker.commands_loader.command_table[cmd]
+        else: # else a group then update
+            new_command_group_table[cmd] = invoker.commands_loader.command_group_table[cmd]
+
+    # include commands of groups in help.py, even though the commands themselves might not be in help.py, so their subparsers can be added.
+    for cmd in new_command_group_table.keys():
+        all_cmds = invoker.commands_loader.cmd_to_loader_map.keys()
+        for old_cmd in all_cmds:
+            if old_cmd.startswith(cmd) and old_cmd != cmd:
+                new_cmd_to_loader_map[old_cmd] = invoker.commands_loader.cmd_to_loader_map[old_cmd]
+                new_command_table[old_cmd] = invoker.commands_loader.command_table[old_cmd]
+
+    invoker.commands_loader.cmd_to_loader_map = new_cmd_to_loader_map
+    invoker.commands_loader.command_table = new_command_table
+    invoker.commands_loader.command_group_table = new_command_group_table
+
+    for loaders in invoker.commands_loader.cmd_to_loader_map.values():
+        for loader in loaders:
+            loader.skip_applicability = True
+
+    for command in invoker.commands_loader.command_table:
+        invoker.commands_loader.load_arguments(command)
+
+    assert len(new_command_table) == len(new_cmd_to_loader_map)
+    assert set(list(new_command_table.keys()) + list(new_command_group_table.keys())) >= set(loaded_helps.keys())
+
+    register_global_subscription_argument(cli_ctx)
+    register_ids_argument(cli_ctx)  # global subscription must be registered first!
+    cli_ctx.raise_event(events.EVENT_INVOKER_POST_CMD_TBL_CREATE, commands_loader=invoker.commands_loader)
+    invoker.parser.load_command_table(invoker.commands_loader)
 
 # this must be called before loading any command modules. Otherwise helps object will have every help.py file's contents
-def convert(target_mod, mod_name, test=False):
+def convert(target_mod_file, mod_name, test=False):
+    global loaded_helps
+    target_mod = import_module(target_mod_file)
     help_dict = target_mod.helps
     loader_file_path = os.path.abspath(target_mod.__file__)
 
     out_file = os.path.join(os.path.dirname(loader_file_path), "help.yaml")
     if test and os.path.exists(out_file):
-        print("{}/help.yaml already exists.\nPlease remove: {}\nand re-run this command.\n".format(mod_name, out_file))
-        exit(1)
+        print("{}/help.yaml already exists.\nwill remove and rewrite: {}\n".format(mod_name, out_file))
+        os.remove(out_file)
 
-    result = _get_new_yaml_dict(help_dict)
+    # the modules keys are keys added to helps object from fresh import....
+    mod_help = {k: v for k, v in help_dict.items() if k not in loaded_helps}
+
+    result = _get_new_yaml_dict(mod_help)
+
+    # clear modules help from knack.helps, store help.py info
+    for key, value in mod_help.items():
+        loaded_helps[key] = value
 
     return out_file, result
+
+
+def get_all_mod_names():
+    installed_dists = get_installed_cli_distributions()
+    mod_name_list = list(sorted(dist.key.replace(COMPONENT_PREFIX, '') for dist in installed_dists if dist.key.startswith(COMPONENT_PREFIX)))
+    return mod_name_list
 
 
 def _get_new_yaml_dict(help_dict):
@@ -177,7 +280,7 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     test = False
 
-    msg = 'Usage: python help_convert.py (MOD | MOD "TEST")\n'
+    msg = 'Usage: python help_convert.py (MOD | --test | MOD "--test")\n'
 
     if "--help" in args or "-h" in args:
         print(msg)
@@ -187,45 +290,60 @@ if __name__ == "__main__":
         exit(msg)
 
     if len(args) == 2:
-        if args[1].lower() != "test":
+        if args[1].lower() != "--test":
             exit(msg)
         else:
             test = True
 
-    # attempt to find and load the desired module.
-    MOD_NAME = "{}.{}._help".format(PACKAGE_PREFIX, args[0])
-    target_mod = import_module(MOD_NAME)
+    target_mods = None
+    if args[0].lower() == "--test":
+        # convert all modules and test
+        mod_names = get_all_mod_names()
+        target_mods = ["{}.{}._help".format(PACKAGE_PREFIX, mod) for mod in mod_names]
+    else:
+        mod_names = [args[0]]
+        # attempt to find and load the desired module.
+        target_mods = ["{}.{}._help".format(PACKAGE_PREFIX, args[0])]
 
     if test:
-        # setup CLI to enable command loader
-        az_cli = get_default_cli()
-
         # convert _help.py contents to help.yaml. Write out help.yaml
         print("Generating new help.yaml file contents. Holding off on writing contents...")
-        out_file, result = convert(target_mod, args[0], test=True)
+        file_to_help = {}
+
+        for mod, mod_name in zip(target_mods, mod_names):
+            file, help_dict = convert(mod, mod_name, test=True)
+            file_to_help[file] = help_dict
 
         print("Loading Commands...")
-        # load commands, args, and help
+        # setup CLI
+        az_cli = DummyCli()
         create_invoker_and_load_cmds_and_args(az_cli)
 
-        # format loaded help
         print("Loading all old help...")
-        old_loaded_help = {data.command: data for data in get_all_help(az_cli) if data.command}
+        all_cli_help = get_all_help(az_cli)
+        old_loaded_help = {data.command: data for data in all_cli_help}
 
         print("Now writing out new help.yaml file contents...")
-        with open(out_file, "w") as f:
-            yaml.dump(result, f)
+        for file, help_dict in file_to_help.items():
+            print("Writing {}...".format(file))
+            with open(file, "w") as f:
+                yaml.dump(help_dict, f)
 
         print("Loading all help again...")
-        new_loaded_help = {data.command: data for data in get_all_help(az_cli) if data.command}
+        all_cli_help = get_all_help(az_cli)
+        new_loaded_help = {data.command: data for data in all_cli_help}
+
+        assert len(old_loaded_help) == len(new_loaded_help)
 
         diff_dict = {}
         for command in old_loaded_help:
-            if command.startswith(args[0]):
-                diff_dict[command] = (old_loaded_help[command], new_loaded_help[command])
+            diff_dict[command] = (old_loaded_help[command], new_loaded_help[command])
 
         print("Verifying that help objects are the same for {0}/_help.py and {0}/help.yaml.".format(args[0]))
         # verify that contents the same
+        print(len(diff_dict))
+        print(len(loaded_helps))
+        assert len(diff_dict) == len(loaded_helps)
         for old, new in diff_dict.values():
             assert_help_objs_equal(old, new)
 
@@ -247,7 +365,7 @@ if __name__ == "__main__":
 
     else:
         print("Generating help.yaml file...")
-        out_file, result = convert(target_mod, args[0])
+        out_file, result = convert(target_mods[0], args[0])
         with open(out_file, "w") as f:
             yaml.dump(result, f)
         print("Done! Successfully generated {0}/help.yaml in {0} module.".format(args[0]))
