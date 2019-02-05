33061c9f3774af3d4e7f9e7064adc257db88fb8a
Dongjiang You <djyou@users.noreply.github.com>
2019-02-05 12:38:16 -0800

    [ACR] Remove hard-coded SDK import (#8417)
    * Remove hard-coded SDK import
    
    * Fix CI
    
    * Fix tests
    
    * update history
    
    * fix history format

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_constants.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_constants.py
@@ -3,24 +3,42 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from azure.mgmt.containerregistry.v2018_09_01.models import SkuName
-from azure.mgmt.containerregistry.v2018_09_01.models import OS, Architecture, Variant
-
 STORAGE_RESOURCE_TYPE = 'Microsoft.Storage/storageAccounts'
 
 ACR_RESOURCE_PROVIDER = 'Microsoft.ContainerRegistry'
 REGISTRY_RESOURCE_TYPE = ACR_RESOURCE_PROVIDER + '/registries'
 WEBHOOK_RESOURCE_TYPE = REGISTRY_RESOURCE_TYPE + '/webhooks'
 REPLICATION_RESOURCE_TYPE = REGISTRY_RESOURCE_TYPE + '/replications'
-BUILD_TASK_RESOURCE_TYPE = REGISTRY_RESOURCE_TYPE + '/buildTasks'
-BUILD_STEP_RESOURCE_TYPE = BUILD_TASK_RESOURCE_TYPE + '/steps'
 
 TASK_RESOURCE_TYPE = REGISTRY_RESOURCE_TYPE + '/tasks'
 TASK_VALID_VSTS_URLS = ['visualstudio.com', 'dev.azure.com']
 
-CLASSIC_REGISTRY_SKU = [SkuName.classic.value]
-MANAGED_REGISTRY_SKU = [SkuName.basic.value, SkuName.standard.value, SkuName.premium.value]
 
-VALID_OS = [item.value.title() for item in OS]
-VALID_ARCH = [item.value.title() for item in Architecture]
-VALID_VARIANT = [item.value for item in Variant]
+def get_classic_sku(cmd):
+    SkuName = cmd.get_models('SkuName')
+    return [SkuName.classic.value]
+
+
+def get_managed_sku(cmd):
+    SkuName = cmd.get_models('SkuName')
+    return [SkuName.basic.value, SkuName.standard.value, SkuName.premium.value]
+
+
+def get_premium_sku(cmd):
+    SkuName = cmd.get_models('SkuName')
+    return [SkuName.premium.value]
+
+
+def get_valid_os(cmd):
+    OS = cmd.get_models('OS')
+    return [item.value.lower() for item in OS]
+
+
+def get_valid_architecture(cmd):
+    Architecture = cmd.get_models('Architecture')
+    return [item.value.lower() for item in Architecture]
+
+
+def get_valid_variant(cmd):
+    Variant = cmd.get_models('Variant')
+    return [item.value.lower() for item in Variant]
