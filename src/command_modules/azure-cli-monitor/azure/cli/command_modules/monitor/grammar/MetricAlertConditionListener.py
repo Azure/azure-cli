2cfa7d8b81753540a1416183b14b71234707b31f
Travis Prescott <tjprescott@users.noreply.github.com>
2019-02-05 13:52:30 -0800

    Fix #8377. (#8433)

--- src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/grammar/MetricAlertConditionListener.py
+++ src/command_modules/azure-cli-monitor/azure/cli/command_modules/monitor/grammar/MetricAlertConditionListener.py
@@ -5,7 +5,7 @@
 
 # pylint: disable=all
 
-# Generated from MetricAlertCondition.g4 by ANTLR 4.7.2
+# Generated from MetricAlertCondition.g4 by ANTLR 4.7.1
 from antlr4 import *
 
 # This class defines a complete listener for a parse tree produced by MetricAlertConditionParser.
