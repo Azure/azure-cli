c850423126acdf14a3cf67db5e806304db19f90d
Alfan T P <iamalfan@gmail.com>
2019-02-06 14:30:14 -0800

    Adding new redis commands, tests, and help (#8217)
    * adding ability to scale for redis cache
    
    * adding tests for redis cache, adding change in history.rst
    
    * fixing test failures
    
    * fixing build errors
    
    * removing sku-capacity and sku-family and adding vm-size as variable. Adding 'getting deprecated' message for update-settings command
    
    * fixing build
    
    * incorporating CR comments
    
    * Adding 8 additional commands to azure redis CLI, support availability zones in create, deprecating import-method command, removing update-settings command
    
    * fixing an issue with import-method parameters
    
    * updating redis cache live tests
    
    * making setup.py in sync with version in history
    
    * addressing CR feedback
    
    * incorporating CR feedback
    
    * adding tests, fixing commands
    
    * resolving style issues
    
    * adding minimum tls version parameter. Fixing input validation for zones and tags, adding help for tags and minimum-tls-version, adding more parameters in test for create
    
    * fixing style issues
    
    * fixing more style and build issues
    
    * fixing style issues
    
    * removing id from list commands
    
    * fixing some of the reported issues for redis cache cli
    
    * incorporating CR feedback, adding more tests
    
    * incorporating CR feedback
    
    * fixing tests and styling issues

--- src/command_modules/azure-cli-redis/azure/cli/command_modules/redis/commands.py
+++ src/command_modules/azure-cli-redis/azure/cli/command_modules/redis/commands.py
@@ -5,11 +5,11 @@
 
 from azure.cli.core.commands import CliCommandType
 
+# pylint: disable=line-too-long
+from azure.cli.command_modules.redis._client_factory import cf_redis, cf_patch_schedules, cf_firewall_rule, cf_linked_server
+
 
 def load_command_table(self, _):
-    from azure.cli.command_modules.redis._client_factory import cf_redis, cf_patch_schedules
-    from azure.cli.command_modules.redis.custom import wrong_vmsize_argument_exception_handler
-
     redis_sdk = CliCommandType(
         operations_tmpl='azure.mgmt.redis.operations.redis_operations#RedisOperations.{}',
         client_factory=cf_redis)
@@ -18,26 +18,42 @@ def load_command_table(self, _):
         operations_tmpl='azure.mgmt.redis.operations.patch_schedules_operations#PatchSchedulesOperations.{}',
         client_factory=cf_patch_schedules)
 
+    redis_firewall_rules = CliCommandType(
+        operations_tmpl='azure.mgmt.redis.operations.firewall_rules_operations#FirewallRulesOperations.{}',
+        client_factory=cf_firewall_rule)
+
+    redis_linked_server = CliCommandType(
+        operations_tmpl='azure.mgmt.redis.operations.linked_server_operations#LinkedServerOperations.{}',
+        client_factory=cf_linked_server)
+
     with self.command_group('redis', redis_sdk) as g:
-        g.custom_command('create', 'cli_redis_create', client_factory=cf_redis,
-                         exception_handler=wrong_vmsize_argument_exception_handler)
-        g.command('delete', 'delete')
+        g.custom_command('create', 'cli_redis_create', client_factory=cf_redis)
+        g.command('delete', 'delete', confirmation=True)
         g.custom_command('export', 'cli_redis_export')
         g.command('force-reboot', 'force_reboot')
-        g.custom_command('import-method', 'cli_redis_import_method',
-                         deprecate_info=g.deprecate(redirect='redis import', hide='2.0.34'))
-        g.custom_command('import', 'cli_redis_import_method')
-        g.custom_command('list', 'cli_redis_list')
-        g.command('list-all', 'list', deprecate_info=g.deprecate(redirect='redis list', hide='2.0.34'))
+        g.command('import-method', 'import_data', deprecate_info=g.deprecate(redirect='redis import', hide=True))
+        g.command('import', 'import_data')
+        g.custom_command('list', 'cli_redis_list_cache')
         g.command('list-keys', 'list_keys')
         g.command('regenerate-keys', 'regenerate_key')
-        g.show_command('show', 'get')
-        g.custom_command('update-settings', 'cli_redis_update_settings',
-                         deprecate_info=g.deprecate(redirect='redis update', hide='2.0.34'))
-        g.generic_update_command('update', exception_handler=wrong_vmsize_argument_exception_handler,
-                                 setter_name='update', custom_func_name='cli_redis_update')
+        g.command('show', 'get')
+        g.generic_update_command('update', setter_name='update', custom_func_name='cli_redis_update')
 
     with self.command_group('redis patch-schedule', redis_patch) as g:
-        g.command('set', 'create_or_update')
+        g.command('create', 'create_or_update')
+        g.command('update', 'create_or_update')
         g.command('delete', 'delete')
-        g.show_command('show', 'get')
+        g.command('show', 'get')
+
+    with self.command_group('redis firewall-rules', redis_firewall_rules) as g:
+        g.command('create', 'create_or_update')
+        g.command('update', 'create_or_update')
+        g.command('delete', 'delete')
+        g.command('show', 'get')
+        g.command('list', 'list_by_redis_resource')
+
+    with self.command_group('redis server-link', redis_linked_server) as g:
+        g.custom_command('create', 'cli_redis_create_server_link', client_factory=cf_linked_server)
+        g.command('delete', 'delete')
+        g.command('show', 'get')
+        g.command('list', 'list')
