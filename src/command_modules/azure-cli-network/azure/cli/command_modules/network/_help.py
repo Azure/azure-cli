21cee3e76c5701beb9fa21d070e9032ecdc4e796
Travis Prescott <tjprescott@users.noreply.github.com>
2019-02-15 09:35:24 -0800

    [Network] Express Route commands and features (#8536)
    * Initial work.
    
    * Add --allow-classic-operations.
    
    * Add --legacy-mode to express-route peering create/update.
    
    * CI fixes.

--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/_help.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/_help.py
@@ -1932,6 +1932,87 @@ helps['network express-route auth show'] = """
 """
 # endregion
 
+# region Express Route Gateway
+helps['network express-route gateway'] = """
+    type: group
+    short-summary: Manage ExpressRoute gateways.
+"""
+
+helps['network express-route gateway create'] = """
+    type: command
+    short-summary: Create an ExpressRoute gateway.
+"""
+
+helps['network express-route gateway delete'] = """
+    type: command
+    short-summary: Delete an ExpressRoute gateway.
+"""
+
+helps['network express-route gateway list'] = """
+    type: command
+    short-summary: List ExpressRoute gateways.
+"""
+
+helps['network express-route gateway show'] = """
+    type: command
+    short-summary: Get the details of an ExpressRoute gateway.
+"""
+
+helps['network express-route gateway update'] = """
+    type: command
+    short-summary: Update settings of an ExpressRoute gateway.
+"""
+# endregion
+
+# region Express Route gateway connection
+helps['network express-route gateway connection'] = """
+    type: group
+    short-summary: Manage ExpressRoute gateway connections.
+"""
+
+helps['network express-route gateway connection create'] = """
+    type: command
+    short-summary: Create an ExpressRoute gateway connection.
+"""
+
+helps['network express-route gateway connection delete'] = """
+    type: command
+    short-summary: Delete an ExpressRoute gateway connection.
+"""
+
+helps['network express-route gateway connection list'] = """
+    type: command
+    short-summary: List ExpressRoute gateway connections.
+"""
+
+helps['network express-route gateway connection show'] = """
+    type: command
+    short-summary: Get the details of an ExpressRoute gateway connection.
+"""
+
+helps['network express-route gateway connection update'] = """
+    type: command
+    short-summary: Update an ExpressRoute gateway connection.
+"""
+# endregion
+
+# region Express Route Link
+helps['network express-route port link'] = """
+    type: group
+    short-summary: View ExpressRoute links.
+"""
+
+helps['network express-route port link list'] = """
+    type: command
+    short-summary: List ExpressRoute links.
+"""
+
+helps['network express-route port link show'] = """
+    type: command
+    short-summary: Get the details of an ExpressRoute link.
+"""
+# endregion
+
 # region Express Route peering
 helps['network express-route peering'] = """
     type: group
@@ -2013,6 +2094,55 @@ helps['network express-route peering connection delete'] = """
 helps['network express-route peering connection show'] = """
     type: command
     short-summary: Get the details of an ExpressRoute circuit connection.
+"""
+# endregion
+
+# region Express Route Port
+helps['network express-route port'] = """
+    type: group
+    short-summary: Manage ExpressRoute ports.
+"""
+
+helps['network express-route port create'] = """
+    type: command
+    short-summary: Create an ExpressRoute port.
+"""
+
+helps['network express-route port delete'] = """
+    type: command
+    short-summary: Delete an ExpressRoute port.
+"""
+
+helps['network express-route port list'] = """
+    type: command
+    short-summary: List ExpressRoute ports.
+"""
+
+helps['network express-route port show'] = """
+    type: command
+    short-summary: Get the details of an ExpressRoute port.
+"""
+
+helps['network express-route port update'] = """
+    type: command
+    short-summary: Update settings of an ExpressRoute port.
+"""
+# endregion
+
+# region Express Route Port Locations
+helps['network express-route port location'] = """
+    type: group
+    short-summary: View ExpressRoute port location information.
+"""
+
+helps['network express-route port location list'] = """
+    type: command
+    short-summary: List ExpressRoute port locations.
+"""
+
+helps['network express-route port location show'] = """
+    type: command
+    short-summary: Get the details of an ExpressRoute port location.
 """
 # endregion
 
--- src/command_modules/azure-cli-network/azure/cli/command_modules/network/_validators.py
+++ src/command_modules/azure-cli-network/azure/cli/command_modules/network/_validators.py
@@ -3,12 +3,15 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=too-many-lines
+
 import argparse
 import base64
 import socket
 import os
 
 from knack.util import CLIError
+from knack.log import get_logger
 
 from azure.cli.core.commands.validators import \
     (validate_tags, get_default_location_from_resource_group)
@@ -17,8 +20,8 @@ from azure.cli.core.commands.client_factory import get_subscription_id, get_mgmt
 from azure.cli.core.commands.validators import validate_parameter_set
 from azure.cli.core.profiles import ResourceType
 
-# PARAMETER VALIDATORS
-# pylint: disable=too-many-lines
+
+logger = get_logger(__name__)
 
 
 def get_asg_validator(loader, dest):
@@ -247,6 +250,93 @@ def validate_dns_record_type(namespace):
             else:
                 namespace.record_set_type = token
             return
+
+
+def validate_express_route_peering(cmd, namespace):
+    from msrestazure.tools import is_valid_resource_id, resource_id
+    circuit = namespace.circuit_name
+    peering = namespace.peering
+
+    if not circuit and not peering:
+        return
+
+    usage_error = CLIError('usage error: --peering ID | --peering NAME --circuit-name CIRCUIT')
+    if not is_valid_resource_id(peering):
+        namespace.peering = resource_id(
+            subscription=get_subscription_id(cmd.cli_ctx),
+            resource_group=namespace.resource_group_name,
+            namespace='Microsoft.Network',
+            type='expressRouteCircuits',
+            name=circuit,
+            child_type_1='peerings',
+            child_name_1=peering
+        )
+    elif circuit:
+        raise usage_error
+
+
+def validate_express_route_port(cmd, namespace):
+    from msrestazure.tools import is_valid_resource_id, resource_id
+    if namespace.express_route_port and not is_valid_resource_id(namespace.express_route_port):
+        namespace.express_route_port = resource_id(
+            subscription=get_subscription_id(cmd.cli_ctx),
+            resource_group=namespace.resource_group_name,
+            namespace='Microsoft.Network',
+            type='expressRoutePorts',
+            name=namespace.express_route_port
+        )
+
+
+def validate_virtual_hub(cmd, namespace):
+    from msrestazure.tools import is_valid_resource_id, resource_id
+    if namespace.virtual_hub and not is_valid_resource_id(namespace.virtual_hub):
+        namespace.virtual_hub = resource_id(
+            subscription=get_subscription_id(cmd.cli_ctx),
+            resource_group=namespace.resource_group_name,
+            namespace='Microsoft.Network',
+            type='virtualHubs',
+            name=namespace.virtual_hub
+        )
+
+
+def bandwidth_validator_factory(mbps=True):
+    def validator(namespace):
+        return validate_circuit_bandwidth(namespace, mbps=mbps)
+    return validator
+
+
+def validate_circuit_bandwidth(namespace, mbps=True):
+    # use gbps if mbps is False
+    unit = 'mbps' if mbps else 'gbps'
+    bandwidth = None
+    bandwidth = getattr(namespace, 'bandwidth_in_{}'.format(unit), None)
+    if bandwidth is None:
+        return
+
+    if len(bandwidth) == 1:
+        bandwidth_comps = bandwidth[0].split(' ')
+    else:
+        bandwidth_comps = bandwidth
+
+    usage_error = CLIError('usage error: --bandwidth INT {Mbps,Gbps}')
+    if len(bandwidth_comps) == 1:
+        logger.warning('interpretting --bandwidth as %s. Consider being explicit: Mbps, Gbps', unit)
+        setattr(namespace, 'bandwidth_in_{}'.format(unit), float(bandwidth_comps[0]))
+        return
+    if len(bandwidth_comps) > 2:
+        raise usage_error
+
+    if float(bandwidth_comps[0]) and bandwidth_comps[1].lower() in ['mbps', 'gbps']:
+        input_unit = bandwidth_comps[1].lower()
+        if input_unit == unit:
+            converted_bandwidth = float(bandwidth_comps[0])
+        elif input_unit == 'gbps':
+            converted_bandwidth = float(bandwidth_comps[0]) * 1000
+        else:
+            converted_bandwidth = float(bandwidth_comps[0]) / 1000
+        setattr(namespace, 'bandwidth_in_{}'.format(unit), converted_bandwidth)
+    else:
+        raise usage_error
 
 
 def validate_er_peer_circuit(cmd, namespace):
