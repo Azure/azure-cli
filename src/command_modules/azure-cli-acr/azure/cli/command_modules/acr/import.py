230f541e829a6ae7dbb874a1933691ba77e02613
NatalieYuGong <43596342+NatalieYuGong@users.noreply.github.com>
2019-02-28 15:17:52 -0800

    rephrase error messages

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/import.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/import.py
@@ -11,13 +11,14 @@ from ._utils import (
     get_registry_from_name_or_login_server
 )
 
-SOURCE_REGISTRY_NOT_FOUND = "The source container registry can not be found. " \
-                            "Please provide a valid address or credentials and try again."
+SOURCE_REGISTRY_NOT_FOUND = "The source container registry can not be found in the current subscription. " \
+                            "Please provide a valid address and/or credentials."
 IMPORT_NOT_SUPPORTED = "Imports are only supported for managed registries."
 SOURCE_NOT_FOUND = "Source cannot be found. " \
                    "Please provide a valid image and source registry or a fully qualified source."
 LOGIN_SERVER_NOT_VALID = "Login server of the registry is not valid" \
-                         "because it violates the naming policy."
+                         "because it is not a fully qualified domain name."
+CREDENTIALS_INVALID = "Authentication failed. Please provide password."
 
 
 def acr_import(cmd,
@@ -31,45 +32,44 @@ def acr_import(cmd,
                resource_group_name=None,
                repository=None,
                force=False):
+    
+    if source_registry_username and not source_registry_password:
+        raise CLIError(CREDENTIALS_INVALID)
 
     _, resource_group_name = validate_managed_registry(
         cmd, registry_name, resource_group_name, IMPORT_NOT_SUPPORTED)
-
-    if not source:
-        raise CLIError(SOURCE_NOT_FOUND)
-    source_image = source
-
+    
     ImportImageParameters, ImportSource, ImportMode = cmd.get_models(
         'ImportImageParameters', 'ImportSource', 'ImportMode')
 
     if source_registry:
         if is_valid_resource_id(source_registry):
-            source = ImportSource(resource_id=source_registry, source_image=source_image)
+            source = ImportSource(resource_id=source_registry, source_image=source)
         else:
             registry = get_registry_from_name_or_login_server(cmd.cli_ctx, source_registry, source_registry)
             if registry:
-                source = ImportSource(resource_id=registry.id, source_image=source_image)
+                source = ImportSource(resource_id=registry.id, source_image=source)
             else:
                 raise CLIError(SOURCE_REGISTRY_NOT_FOUND)
     else:
-        slash = source_image.find('/')
+        slash = source.find('/')
         if slash > 0:
-            registry_uri = source_image[:slash]
+            registry_uri = source[:slash]
             dot = registry_uri.find('.')
             if dot > 0:
-                source_image = source_image[slash + 1:]
+                source = source[slash + 1:]
                 if source_registry_password:
                     ImportSourceCredentials = cmd.get_models('ImportSourceCredentials')
                     source = ImportSource(registry_uri=registry_uri,
-                                          source_image=source_image,
+                                          source_image=source,
                                           credentials=ImportSourceCredentials(password=source_registry_password,
                                                                               username=source_registry_username))
                 else:
                     registry = get_registry_from_name_or_login_server(cmd.cli_ctx, registry_uri)
                     if registry:
-                        source = ImportSource(resource_id=registry.id, source_image=source_image)
+                        source = ImportSource(resource_id=registry.id, source_image=source)
                     else:
-                        source = ImportSource(registry_uri=registry_uri, source_image=source_image)
+                        source = ImportSource(registry_uri=registry_uri, source_image=source)
             else:
                 raise CLIError(LOGIN_SERVER_NOT_VALID)
         else:
