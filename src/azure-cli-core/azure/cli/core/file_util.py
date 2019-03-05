1e388106a8cfa4e3034a035cd6db17f12dd4ee7b
adewaleo <oladewal@microsoft.com>
2019-03-05 12:44:55 -0800

    [core] Optimize help loader v1. (#8535)
    * AzCliHelp now updates help loaders with applicable file contents before calling load. Update get_all_help in file_util. Improved efficiency. Updates to test.
    
    * Parse Yaml when caching file contents. Yaml parsing is expensive. Update tests. Remove help.yaml
    
    * Minor style fixes.
    
    * Fix bad python 2 super call.

--- src/azure-cli-core/azure/cli/core/file_util.py
+++ src/azure-cli-core/azure/cli/core/file_util.py
@@ -27,6 +27,7 @@ def get_all_help(cli_ctx):
     help_files = []
     for cmd, parser in zip(sub_parser_keys, sub_parser_values):
         try:
+            help_ctx.update_loaders_with_help_file_contents(cmd.split())
             help_file = CliGroupHelpFile(help_ctx, cmd, parser) if _is_group(parser) \
                 else CliCommandHelpFile(help_ctx, cmd, parser)
             help_file.load(parser)
