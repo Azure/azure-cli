8f314c7891e9e74503f084d4bbc4788f17534873
Sisira Panchagnula <panchagnula@users.noreply.github.com>
2019-03-06 21:11:11 -0800

    web app: Updates to az webapp up command - removes preview tag & other changes (#8693)

--- src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/_help.py
+++ src/command_modules/azure-cli-appservice/azure/cli/command_modules/appservice/_help.py
@@ -1166,7 +1166,7 @@ short-summary: Display the current distribution of traffic across slots.
 
 helps['webapp up'] = """
 type: command
-short-summary: (Preview) Create and deploy existing local code to the web app, by running the command from the folder where the code is present. Supports running the command in preview mode using --dryrun parameter. Current supports includes Node, Python,.NET Core, ASP.NET, staticHtml. Node, Python apps are created as Linux apps. .Net Core, ASP.NET and static HTML apps are created as Windows apps. If command is run from an empty folder, an empty windows web app is created.
+short-summary: Create and deploy existing local code to the web app, by running the command from the folder where the code is present. Supports running the command in preview mode using --dryrun parameter. Current supports includes Node, Python,.NET Core, ASP.NET, staticHtml. Node, Python apps are created as Linux apps. .Net Core, ASP.NET and static HTML apps are created as Windows apps. If command is run from an empty folder, an empty windows web app is created.
 examples:
   - name: View the details of the app that will be created, without actually running the operation
     text: >
