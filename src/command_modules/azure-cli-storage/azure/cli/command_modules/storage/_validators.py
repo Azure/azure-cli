04c3f9fb85bd25ebee49d170427d5669a5627a5a
williexu <wx44@cornell.edu>
2019-03-08 14:07:09 -0800

    fix faulty encryption parameter update logic for storage account update

--- src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/_validators.py
+++ src/command_modules/azure-cli-storage/azure/cli/command_modules/storage/_validators.py
@@ -426,9 +426,6 @@ def validate_encryption_source(cmd, namespace):
                          'when --encryption-key-source=Microsoft.Keyvault is specified.')
 
     if key_name or key_version or key_vault_uri:
-        if namespace.encryption_key_source != 'Microsoft.Keyvault':
-            raise ValueError('--encryption-key-name, --encryption-key-vault, and --encryption-key-version are not '
-                             'applicable when --encryption-key-source=Microsoft.Keyvault is not specified.')
         KeyVaultProperties = get_sdk(cmd.cli_ctx, ResourceType.MGMT_STORAGE, 'KeyVaultProperties',
                                      mod='models')
         if not KeyVaultProperties:
