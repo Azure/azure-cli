a6fc6eef009b3cf6f49def51c7268fd16cfd798c
yugangw-msft <yugangw@microsoft.com>
2019-03-10 21:51:51 -0700

    fix more

--- src/command_modules/azure-cli-backup/azure/cli/command_modules/backup/commands.py
+++ src/command_modules/azure-cli-backup/azure/cli/command_modules/backup/commands.py
@@ -22,15 +22,15 @@ def load_command_table(self, _):
         operations_tmpl='azure.mgmt.recoveryservices.operations#VaultsOperations.{}',
         client_factory=vaults_cf)
 
-    backup_storage_config_sdk = CliCommandType(
-        operations_tmpl='azure.mgmt.recoveryservices.operations#BackupStorageConfigsOperations.{}',
-        client_factory=vaults_cf)
+    # backup_storage_config_sdk = CliCommandType(
+    #     operations_tmpl='azure.mgmt.recoveryservices.operations#BackupStorageConfigsOperations.{}',
+    #     client_factory=vaults_cf)
 
     with self.command_group('backup vault', backup_vaults_sdk, client_factory=vaults_cf) as g:
         g.custom_command('create', 'create_vault')
         g.show_command('show', 'get')
         g.custom_command('list', 'list_vaults')
-        g.show_command('backup-properties show', 'get', command_type=backup_storage_config_sdk)
+        # g.show_command('backup-properties show', 'get', command_type=backup_storage_config_sdk)
         g.custom_command('backup-properties set', 'set_backup_properties', client_factory=backup_storage_configs_cf)
         g.custom_command('delete', 'delete_vault', confirmation=True)
 
