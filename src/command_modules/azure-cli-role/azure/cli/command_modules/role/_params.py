2e7a486f541732b5d5c1e9ee7f26fe3297c9f4a1
Yugang Wang <yugangw@microsoft.com>
2019-03-20 14:55:50 -0700

    create-for-rbac: remove the deprecation version on --password (#8824)

--- src/command_modules/azure-cli-role/azure/cli/command_modules/role/_params.py
+++ src/command_modules/azure-cli-role/azure/cli/command_modules/role/_params.py
@@ -97,7 +97,7 @@ def load_arguments(self, _):
 
     with self.argument_context('ad sp create-for-rbac') as c:
         c.argument('password', options_list=['--password', '-p'], arg_group='Credential',
-                   deprecate_info=c.deprecate(expiration='2.1.0', hide=False), help="If missing, CLI will generate a strong password")
+                   deprecate_info=c.deprecate(hide=False), help="If missing, CLI will generate a strong password")
 
     with self.argument_context('ad sp credential reset') as c:
         c.argument('password', options_list=['--password', '-p'], arg_group='Credential',
