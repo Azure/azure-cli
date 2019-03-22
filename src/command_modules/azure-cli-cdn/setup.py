9e6707a7132f17d693514b4d09739bed5306644d
David Justice <devigned@users.noreply.github.com>
2019-03-22 16:38:14 -0400

    Add support for updated CDN SKUs (#8855)
    * fixes #7635 and adds support for Standard_ChinaCdn
    
    * allow any higher version of pip
    
    * add history.rst entry

--- src/command_modules/azure-cli-cdn/setup.py
+++ src/command_modules/azure-cli-cdn/setup.py
@@ -16,7 +16,7 @@ except ImportError:
     cmdclass = {}
 
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 # The full list of classifiers is available at
 # https://pypi.python.org/pypi?%3Aaction=list_classifiers
 CLASSIFIERS = [
@@ -34,7 +34,7 @@ CLASSIFIERS = [
 ]
 
 DEPENDENCIES = [
-    'azure-mgmt-cdn==3.0.0',
+    'azure-mgmt-cdn==3.1.0',
     'azure-cli-core',
 ]
 
