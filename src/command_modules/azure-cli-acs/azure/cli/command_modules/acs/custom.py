5d566912a8596951e3f9632c20b77c43ef3649ed
Ganesha <ganesha.ashoka@gmail.com>
2019-03-26 09:39:29 -0700

    Re-allow adv networking clusters with only a vnetsubnet (#8844)

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
@@ -1517,7 +1517,7 @@ def aks_create(cmd, client, resource_group_name, name, ssh_key_value,  # pylint:
                            'Are you an Owner on this subscription?')
 
     network_profile = None
-    if any([pod_cidr, service_cidr, dns_service_ip, docker_bridge_address, network_policy]):
+    if any([network_plugin, pod_cidr, service_cidr, dns_service_ip, docker_bridge_address, network_policy]):
         if not network_plugin:
             raise CLIError('Please explicitly specify the network plugin type')
         if pod_cidr and network_plugin == "azure":
