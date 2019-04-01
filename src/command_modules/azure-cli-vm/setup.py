62a66f3855f77dff53f32e37ed2ddccc3b4fb2d9
Yugang Wang <yugangw@microsoft.com>
2019-04-01 11:55:12 -0700

    compute: support large  disk (#8876)

--- src/command_modules/azure-cli-vm/setup.py
+++ src/command_modules/azure-cli-vm/setup.py
@@ -34,7 +34,7 @@ CLASSIFIERS = [
 DEPENDENCIES = [
     'azure-mgmt-msi==0.2.0',
     'azure-mgmt-authorization==0.50.0',
-    'azure-mgmt-compute==4.4.0',
+    'azure-mgmt-compute==4.5.1',
     'azure-mgmt-keyvault==1.1.0',
     'azure-keyvault==1.1.0',
     'azure-mgmt-network==2.6.0',
