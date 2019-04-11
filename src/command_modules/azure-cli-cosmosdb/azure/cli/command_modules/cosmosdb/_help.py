75c6025e8831c827c7cf30170e919cd995a59b69
Spandan Garg <spandan.garg1706@gmail.com>
2019-04-11 11:21:47 -0700

    Sort commands in cosmosdb _help.py 	- Sort commands in help files for easier diffs in the future.

--- src/command_modules/azure-cli-cosmosdb/azure/cli/command_modules/cosmosdb/_help.py
+++ src/command_modules/azure-cli-cosmosdb/azure/cli/command_modules/cosmosdb/_help.py
@@ -4,80 +4,80 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from knack.help_files import helps
-
+from knack.help_files import helps  # pylint: disable=unused-import
+# pylint: disable=line-too-long, too-many-lines
 
 helps['cosmosdb'] = """
-    type: group
-    short-summary: Manage Azure Cosmos DB database accounts.
-"""
-
-helps['cosmosdb database'] = """
-    type: group
-    short-summary: Manage Azure Cosmos DB databases.
-"""
-
-helps['cosmosdb collection'] = """
-    type: group
-    short-summary: Manage Azure Cosmos DB collections.
-"""
-
-helps['cosmosdb network-rule'] = """
-    type: group
-    short-summary: Manage Azure Comsos DB network rules.
+type: group
+short-summary: Manage Azure Cosmos DB database accounts.
 """
 
 helps['cosmosdb check-name-exists'] = """
-    type: command
-    short-summary: Checks if an Azure Cosmos DB account name exists.
+type: command
+short-summary: Checks if an Azure Cosmos DB account name exists.
+"""
+
+helps['cosmosdb collection'] = """
+type: group
+short-summary: Manage Azure Cosmos DB collections.
 """
 
 helps['cosmosdb create'] = """
-    type: command
-    short-summary: Creates a new Azure Cosmos DB database account.
+type: command
+short-summary: Creates a new Azure Cosmos DB database account.
+"""
+
+helps['cosmosdb database'] = """
+type: group
+short-summary: Manage Azure Cosmos DB databases.
 """
 
 helps['cosmosdb delete'] = """
-    type: command
-    short-summary: Deletes an Azure Cosmos DB database account.
+type: command
+short-summary: Deletes an Azure Cosmos DB database account.
 """
 
 helps['cosmosdb failover-priority-change'] = """
-    type: command
-    short-summary: Changes the failover priority for the Azure Cosmos DB database account.
+type: command
+short-summary: Changes the failover priority for the Azure Cosmos DB database account.
 """
 
 helps['cosmosdb list'] = """
-    type: command
-    short-summary: List Azure Cosmos DB database accounts.
+type: command
+short-summary: List Azure Cosmos DB database accounts.
 """
 
 helps['cosmosdb list-connection-strings'] = """
-    type: command
-    short-summary: List the connection strings for a Azure Cosmos DB database account.
+type: command
+short-summary: List the connection strings for a Azure Cosmos DB database account.
 """
 
 helps['cosmosdb list-keys'] = """
-    type: command
-    short-summary: List the access keys for a Azure Cosmos DB database account.
+type: command
+short-summary: List the access keys for a Azure Cosmos DB database account.
 """
 
 helps['cosmosdb list-read-only-keys'] = """
-    type: command
-    short-summary: List the read-only access keys for a Azure Cosmos DB database account.
+type: command
+short-summary: List the read-only access keys for a Azure Cosmos DB database account.
+"""
+
+helps['cosmosdb network-rule'] = """
+type: group
+short-summary: Manage Azure Comsos DB network rules.
 """
 
 helps['cosmosdb regenerate-key'] = """
-    type: command
-    short-summary: Regenerate an access key for a Azure Cosmos DB database account.
+type: command
+short-summary: Regenerate an access key for a Azure Cosmos DB database account.
 """
 
 helps['cosmosdb show'] = """
-    type: command
-    short-summary: Get the details of an Azure Cosmos DB database account.
+type: command
+short-summary: Get the details of an Azure Cosmos DB database account.
 """
 
 helps['cosmosdb update'] = """
-    type: command
-    short-summary: Update an Azure Cosmos DB database account.
+type: command
+short-summary: Update an Azure Cosmos DB database account.
 """
