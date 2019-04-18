2da3157d995623d9f5ea3f984cde8a2400b7edb2
Jin Soon Lim <jilim@microsoft.com>
2019-04-18 14:52:10 -0700

    Add --expand parameter to 'az policy state list'

--- src/command_modules/azure-cli-policyinsights/azure/cli/command_modules/policyinsights/_validators.py
+++ src/command_modules/azure-cli-policyinsights/azure/cli/command_modules/policyinsights/_validators.py
@@ -14,3 +14,13 @@ def validate_resource(cmd, namespace):  # pylint: disable=unused-argument
                 raise CLIError('--namespace is required if --resource is not a resource ID.')
             if not namespace.resource_type:
                 raise CLIError('--resource-type is required if --resource is not a resource ID.')
+
+def validate_resourceScopeRequired(cmd, namespace):
+    if not namespace.resource:
+        raise CLIError('--resource is required for the operation.')
+    else:
+        validate_resource(cmd, namespace)
+
+def validate_expand(cmd, namespace):
+    if namespace.expand_clause is not None:
+        validate_resourceScopeRequired(cmd, namespace)
