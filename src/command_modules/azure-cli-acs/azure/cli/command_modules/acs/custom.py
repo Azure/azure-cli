38a48b7e541c66552e03363ba23d72710ce4c5d0
Jack <JackQuincy@users.noreply.github.com>
2019-04-24 10:08:24 -0700

    Osa cluster admin (#9058)
    * adding cluster admin option to osa create
    
    * adding direcctory read permissions to the aad app (#1)
    
    * adding cluster admin option to osa create
    
    * adding direcctory read permissions to the aad app
    
    * making active directory app always require directory permissions
    
    * infra node to 3
    
    * adding new version of python sdk (#3)
    
    * adding cluster admin option to osa create
    
    * adding direcctory read permissions to the aad app
    
    * making app always require directory permissions
    
    * clean + infra node to 3
    
    * fixing typo
    
    * using new version of acs python sdk
    
    * updating history.rst
    
    * Change type from scope to role
    
    * apply review
    
    * pep fix
    
    * fix merge conflict
    
    * update history and help
    
    * update recorded aks tests
    
    * Revert "update recorded aks tests"
    
    This reverts commit 8643736dce37871c546b64631cfa51075fa67262.
    
    * change api version for tests

--- src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
+++ src/command_modules/azure-cli-acs/azure/cli/command_modules/acs/custom.py
@@ -2255,7 +2255,8 @@ def _ensure_osa_aad(cli_ctx,
                     aad_client_app_secret=None,
                     aad_tenant_id=None,
                     identifier=None,
-                    name=None, update=False):
+                    name=None, update=False,
+                    customer_admin_group_id=None):
     rbac_client = get_graph_rbac_management_client(cli_ctx)
     if not aad_client_app_id:
         if not aad_client_app_secret and update:
@@ -2265,9 +2266,14 @@ def _ensure_osa_aad(cli_ctx,
         # Delegate Sign In and Read User Profile permissions on Windows Azure Active Directory API
         resource_access = ResourceAccess(id="311a71cc-e848-46a1-bdf8-97ff7156d8e6",
                                          additional_properties=None, type="Scope")
-        required_osa_aad_access = RequiredResourceAccess(resource_access=[resource_access],
+        # Read directory permissions on Windows Azure Active Directory API
+        directory_access = ResourceAccess(id="5778995a-e1bf-45b8-affa-663a9f3f4d04",
+                                          additional_properties=None, type="Role")
+
+        required_osa_aad_access = RequiredResourceAccess(resource_access=[resource_access, directory_access],
                                                          additional_properties=None,
                                                          resource_app_id="00000002-0000-0000-c000-000000000000")
+
         list_aad_filtered = list(rbac_client.applications.list(filter="identifierUris/any(s:s eq '{}')"
                                                                .format(reply_url)))
         if update:
@@ -2303,7 +2309,8 @@ def _ensure_osa_aad(cli_ctx,
         client_id=aad_client_app_id,
         secret=aad_client_app_secret,
         tenant_id=aad_tenant_id,
-        kind='AADIdentityProvider')
+        kind='AADIdentityProvider',
+        customer_admin_group_id=customer_admin_group_id)
 
 
 def _ensure_service_principal(cli_ctx,
@@ -2495,7 +2502,8 @@ def openshift_create(cmd, client, resource_group_name, name,  # pylint: disable=
                      subnet_prefix="10.0.0.0/24",
                      vnet_peer=None,
                      tags=None,
-                     no_wait=False):
+                     no_wait=False,
+                     customer_admin_group_id=None):
 
     if location is None:
         location = _get_rg_location(cmd.cli_ctx, resource_group_name)
@@ -2511,7 +2519,7 @@ def openshift_create(cmd, client, resource_group_name, name,  # pylint: disable=
 
     agent_infra_pool_profile = OpenShiftManagedClusterAgentPoolProfile(
         name='infra',  # Must be 12 chars or less before ACS RP adds to it
-        count=int(2),
+        count=int(3),
         vm_size="Standard_D4s_v3",
         os_type="Linux",
         role=OpenShiftAgentPoolProfileRole.infra,
@@ -2540,7 +2548,8 @@ def openshift_create(cmd, client, resource_group_name, name,  # pylint: disable=
                                        aad_client_app_id=aad_client_app_id,
                                        aad_client_app_secret=aad_client_app_secret,
                                        aad_tenant_id=aad_tenant_id, identifier=fqdn,
-                                       name=name, update=update_aad_secret)
+                                       name=name, update=update_aad_secret,
+                                       customer_admin_group_id=customer_admin_group_id)
     identity_providers.append(
         OpenShiftManagedClusterIdentityProvider(
             name='Azure AD',
