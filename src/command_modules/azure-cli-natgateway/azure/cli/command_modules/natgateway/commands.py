657ba664f466b2b3dd5bacd52fb03f99c0348514
khannarheams <47536412+khannarheams@users.noreply.github.com>
2019-05-02 08:54:24 -0700

    Nat Gateway Resource (#9147)
    * Azure CLI Nat Gateway contract changes
    
    * Update help text to nat gateway sku
    
    * Update setup files
    
    * Add Get,Create,Remove commands for Nat Gateway and Test for Create,Remove NatGateway
    
    * Update PR comments and tests
    
    * Update Recordings

--- src/command_modules/azure-cli-natgateway/azure/cli/command_modules/natgateway/commands.py
+++ src/command_modules/azure-cli-natgateway/azure/cli/command_modules/natgateway/commands.py
@@ -10,11 +10,13 @@ from azure.cli.command_modules.natgateway._client_factory import cf_nat_gateways
 def load_command_table(self, _):
 
     nat_gateway_sdk = CliCommandType(
-        operations_tmpl='azure.mgmt.network.operations#NatGatewayOperations.{}',
+        operations_tmpl='azure.mgmt.network.operations#NatGatewaysOperations.{}',
         client_factory=cf_nat_gateways)
 
     with self.command_group('network nat-gateway', nat_gateway_sdk) as g:
-        # g.show_command('show', 'get')
-        g.custom_command('list', 'list_nat_gateways')
-        g.custom_command('create', 'create_nat_gateway')
-        # g.command('delete', 'delete')
+        g.show_command('show', 'get')
+        g.generic_update_command('update', supports_no_wait=True)
+        g.custom_command('create', 'create_nat_gateway', supports_no_wait=True)
+        g.custom_command('list', 'list_nat_gateway')
+        g.command('delete', 'delete')
+        g.wait_command('wait')
