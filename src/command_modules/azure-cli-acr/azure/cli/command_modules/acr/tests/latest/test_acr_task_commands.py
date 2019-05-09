7599a4046b9235d9bd85833189514a975d15d169
Dor Fire <dorfire@microsoft.com>
2019-05-09 15:45:51 -0700

    Added acr pack test

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/tests/latest/test_acr_task_commands.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/tests/latest/test_acr_task_commands.py
@@ -128,3 +128,21 @@ class AcrTaskCommandsTests(LiveScenarioTest):
                          self.check('provisioningState', 'Succeeded')])
         # Run a contextless command
         self.cmd('acr run -r {registry_name} --timeout {timeout} --cmd {command} {source_location}')
+
+    @ResourceGroupPreparer()
+    def test_acr_pack(self, resource_group):
+        self.kwargs.update({
+            'rg_loc': 'westcentralus',
+            'sku': 'Standard',
+            'registry_name': self.create_random_name('runreg', 20),
+            'timeout': 400,
+            'source_location': '/dev/null'
+        })
+        self.cmd('acr create -n {registry_name} -g {rg} -l {rg_loc} --sku {sku}',
+                 checks=[self.check('name', '{registry_name}'),
+                         self.check('location', '{rg_loc}'),
+                         self.check('sku.name', 'Standard'),
+                         self.check('sku.tier', 'Standard'),
+                         self.check('provisioningState', 'Succeeded')])
+        # Run a contextless command
+        self.cmd('acr run -r {registry_name} --timeout {timeout} {source_location}')
