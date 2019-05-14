30cfb239bff2f59a8070d555bdff510e26c32a11
yugangw-msft <yugangw@microsoft.com>
2019-05-14 12:29:00 -0700

    fix ci

--- src/command_modules/azure-cli-role/azure/cli/command_modules/role/_validators.py
+++ src/command_modules/azure-cli-role/azure/cli/command_modules/role/_validators.py
@@ -85,7 +85,7 @@ def validate_cert(namespace):
         namespace.cert = x509
 
 
-def process_assignment_namespace(_, namespace):
+def process_assignment_namespace(cmd, namespace):  # pylint: disable=unused-argument
     resource_group = namespace.resource_group_name
     if namespace.scope and resource_group and getattr(resource_group, 'is_default', None):
         namespace.resource_group_name = None  # drop configured defaults
