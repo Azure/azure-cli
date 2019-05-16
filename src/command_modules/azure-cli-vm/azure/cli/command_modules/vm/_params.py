Azure/azure-cli

commit ae176675999b233affd194977c1cd7a32c09db2b

[compute] Compute cli updates (#9306)
* expose --computer-name for vm create.

* Minor changes. Re-record test_vm_create_ubuntu.

* Add additional example to snapshot create, in order to close issue #9026.

* Remove preview prefix from vmss encryption, vmss rolling-upgrade.

* Support supplying multiple ssh-key-values through --ssh-key-value. Added tests and updated documentation.

* Rename `--ssh-key-value`  to `--ssh-key-values` in vm/vmss create.

* Remove preview from vmss create --health-probe.

* Update tests to use ssh_key_value list.

Handle edge case where no ssh key value supplied.

* Properly handle when ssh_key_value is none

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_params.py
@@ -334,7 +334,7 @@ def load_arguments(self, _):
         c.argument('instance_count', help='Number of VMs in the scale set.', type=int)
         c.argument('disable_overprovision', help='Overprovision option (see https://azure.microsoft.com/en-us/documentation/articles/virtual-machine-scale-sets-overview/ for details).', action='store_true')
         c.argument('upgrade_policy_mode', help=None, arg_type=get_enum_type(UpgradeMode))
-        c.argument('health_probe', help='(Preview) probe name from the existing load balancer, mainly used for rolling upgrade')
+        c.argument('health_probe', help='Probe name from the existing load balancer, mainly used for rolling upgrade')
         c.argument('vm_sku', help='Size of VMs in the scale set. Default to "Standard_DS1_v2". See https://azure.microsoft.com/en-us/pricing/details/virtual-machines/ for size info.')
         c.argument('nsg', help='Name or ID of an existing Network Security Group.', arg_group='Network')
         c.argument('priority', resource_type=ResourceType.MGMT_COMPUTE, min_api='2017-12-01', arg_type=get_enum_type(VMPriorityTypes, default=None),
@@ -459,8 +459,8 @@ def load_arguments(self, _):
             c.argument('generate_ssh_keys', action='store_true', help='Generate SSH public and private key files if missing. The keys will be stored in the ~/.ssh directory')
             c.argument('admin_username', help='Username for the VM.', default=get_default_admin_username())
             c.argument('admin_password', help="Password for the VM if authentication type is 'Password'.")
-            c.argument('ssh_key_value', help='SSH public key or public key file path.', completer=FilesCompleter(), type=file_type)
-            c.argument('ssh_dest_key_path', help='Destination file path on the VM for the SSH key.')
+            c.argument('ssh_key_value', options_list=['--ssh-key-values'], completer=FilesCompleter(), type=file_type, nargs='*')
+            c.argument('ssh_dest_key_path', help='Destination file path on the VM for the SSH key. If the file already exists, the specified key(s) are appended to the file.')
             c.argument('authentication_type', help='Type of authentication to use with the VM. Defaults to password for Windows and SSH public key for Linux. "all" enables both ssh and password authentication. ', arg_type=get_enum_type(['ssh', 'password', 'all']))
 
         with self.argument_context(scope, arg_group='Storage') as c:

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_validators.py
@@ -944,7 +944,22 @@ def _validate_admin_password(password, os_type):
 
 
 def validate_ssh_key(namespace):
-    string_or_file = (namespace.ssh_key_value or
+    if namespace.ssh_key_value:
+        if namespace.generate_ssh_keys and len(namespace.ssh_key_value) > 1:
+            logger.warning("Ignoring --generate-ssh-keys as multiple ssh key values have been specified.")
+            namespace.generate_ssh_keys = False
+
+        processed_ssh_key_values = []
+        for ssh_key_value in namespace.ssh_key_value:
+            processed_ssh_key_values.append(_validate_ssh_key_helper(ssh_key_value, namespace.generate_ssh_keys))
+        namespace.ssh_key_value = processed_ssh_key_values
+    # if no ssh keys processed, try to generate new key / use existing at root.
+    else:
+        namespace.ssh_key_value = _validate_ssh_key_helper("", namespace.generate_ssh_keys)
+
+
+def _validate_ssh_key_helper(ssh_key_value, should_generate_ssh_keys):
+    string_or_file = (ssh_key_value or
                       os.path.join(os.path.expanduser('~'), '.ssh', 'id_rsa.pub'))
     content = string_or_file
     if os.path.exists(string_or_file):
@@ -952,7 +967,7 @@ def validate_ssh_key(namespace):
         with open(string_or_file, 'r') as f:
             content = f.read()
     elif not keys.is_valid_ssh_rsa_public_key(content):
-        if namespace.generate_ssh_keys:
+        if should_generate_ssh_keys:
             # figure out appropriate file names:
             # 'base_name'(with private keys), and 'base_name.pub'(with public keys)
             public_key_filepath = string_or_file
@@ -968,7 +983,7 @@ def validate_ssh_key(namespace):
         else:
             raise CLIError('An RSA key file or key value must be supplied to SSH Key Value. '
                            'You can use --generate-ssh-keys to let CLI generate one for you')
-    namespace.ssh_key_value = content
+    return content
 
 
 def _validate_vm_vmss_msi(cmd, namespace, from_set_command=False):

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_help.py
@@ -176,7 +176,7 @@ short-summary: create a new version from an image defintion
 
 helps['sig image-version create'] = """
 type: command
-short-summary: creat a new image version
+short-summary: create a new image version
 long-summary: this operation might take a long time depending on the replicate region number. Use "--no-wait" is advised.
 examples:
   - name: Add a new image version
@@ -243,12 +243,13 @@ type: command
 short-summary: Create a snapshot.
 examples:
   - name: Create a snapshot by importing from a blob uri.
-    text: >
-        az snapshot create -g MyResourceGroup -n MySnapshot --source https://vhd1234.blob.core.windows.net/vhds/osdisk1234.vhd
+    text: az snapshot create -g MyResourceGroup -n MySnapshot --source https://vhd1234.blob.core.windows.net/vhds/osdisk1234.vhd
   - name: Create an empty snapshot.
     text: az snapshot create -g MyResourceGroup -n MySnapshot --size-gb 10
   - name: Create a snapshot by copying an existing disk in the same resource group.
     text: az snapshot create -g MyResourceGroup -n MySnapshot2 --source MyDisk
+  - name: Create a snapshot from an existing disk in another resource group.
+    text: az snapshot create -g MyResourceGroup -n MySnapshot2 --source "/subscriptions/00000/resourceGroups/AnotherResourceGroup/providers/Microsoft.Compute/disks/MyDisk"
 """
 
 helps['snapshot grant-access'] = """
@@ -467,8 +468,10 @@ parameters:
     populator-commands:
       - az vm image list
       - az vm image show
-  - name: --ssh-key-value
-    short-summary: The SSH public key or public key file path.
+  - name: --ssh-key-values
+    short-summary: Space-separated list of SSH public keys or public key file paths.
+  - name: --computer-name
+    short-summary: The host OS name of the virtual machine. Defaults to the name of the VM.
 examples:
   - name: Create a default Ubuntu VM with automatic SSH authentication.
     text: >
@@ -1478,6 +1481,8 @@ parameters:
     populator-commands:
       - az vm image list
       - az vm image show
+  - name: --ssh-key-values
+    short-summary: Space-separated list of SSH public keys or public key file paths.
 examples:
   - name: Create a Windows VM scale set with 5 instances, a load balancer, a public IP address, and a 2GB data disk.
     text: >
@@ -1591,7 +1596,7 @@ examples:
 
 helps['vmss encryption'] = """
 type: group
-short-summary: "(PREVIEW) Manage encryption of VMSS."
+short-summary: "Manage encryption of VMSS."
 long-summary: "For more information, see: https://docs.microsoft.com/en-us/azure/security/azure-security-disk-encryption-overview"
 """
 
@@ -1792,7 +1797,7 @@ examples:
 
 helps['vmss rolling-upgrade'] = """
 type: group
-short-summary: (PREVIEW) Manage rolling upgrades.
+short-summary: Manage rolling upgrades.
 """
 
 helps['vmss run-command'] = """

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_template_builder.py
@@ -243,18 +243,19 @@ def build_msi_role_assignment(vm_vmss_name, vm_vmss_resource_id, role_definition
 
 def build_vm_resource(  # pylint: disable=too-many-locals
         cmd, name, location, tags, size, storage_profile, nics, admin_username,
-        availability_set_id=None, admin_password=None, ssh_key_value=None, ssh_key_path=None,
+        availability_set_id=None, admin_password=None, ssh_key_values=None, ssh_key_path=None,
         image_reference=None, os_disk_name=None, custom_image_os_type=None, authentication_type=None,
         os_publisher=None, os_offer=None, os_sku=None, os_version=None, os_vhd_uri=None,
         attach_os_disk=None, os_disk_size_gb=None, custom_data=None, secrets=None, license_type=None, zone=None,
-        disk_info=None, boot_diagnostics_storage_uri=None, ultra_ssd_enabled=None, proximity_placement_group=None):
+        disk_info=None, boot_diagnostics_storage_uri=None, ultra_ssd_enabled=None, proximity_placement_group=None,
+        computer_name=None):
 
     os_caching = disk_info['os'].get('caching')
 
     def _build_os_profile():
 
         os_profile = {
-            'computerName': name,
+            'computerName': computer_name or name,
             'adminUsername': admin_username
         }
 
@@ -264,7 +265,7 @@ def build_vm_resource(  # pylint: disable=too-many-locals
         if custom_data:
             os_profile['customData'] = b64encode(custom_data)
 
-        if ssh_key_value and ssh_key_path:
+        if ssh_key_values and ssh_key_path:
             os_profile['linuxConfiguration'] = {
                 'disablePasswordAuthentication': authentication_type == 'ssh',
                 'ssh': {
@@ -272,7 +273,7 @@ def build_vm_resource(  # pylint: disable=too-many-locals
                         {
                             'keyData': ssh_key_value,
                             'path': ssh_key_path
-                        }
+                        } for ssh_key_value in ssh_key_values
                     ]
                 }
             }
@@ -614,7 +615,7 @@ def build_vmss_resource(cmd, name, naming_prefix, location, tags, overprovision,
                         vm_sku, instance_count, ip_config_name, nic_name, subnet_id,
                         public_ip_per_vm, vm_domain_name, dns_servers, nsg, accelerated_networking,
                         admin_username, authentication_type, storage_profile, os_disk_name, disk_info,
-                        os_type, image=None, admin_password=None, ssh_key_value=None,
+                        os_type, image=None, admin_password=None, ssh_key_values=None,
                         ssh_key_path=None, os_publisher=None, os_offer=None, os_sku=None, os_version=None,
                         backend_address_pool_id=None, inbound_nat_pool_id=None, health_probe=None,
                         single_placement_group=None, platform_fault_domain_count=None, custom_data=None,
@@ -710,7 +711,7 @@ def build_vmss_resource(cmd, name, naming_prefix, location, tags, overprovision,
     if admin_password:
         os_profile['adminPassword'] = "[parameters('adminPassword')]"
 
-    if ssh_key_value and ssh_key_path:
+    if ssh_key_values and ssh_key_path:
         os_profile['linuxConfiguration'] = {
             'disablePasswordAuthentication': authentication_type == 'ssh',
             'ssh': {
@@ -718,7 +719,7 @@ def build_vmss_resource(cmd, name, naming_prefix, location, tags, overprovision,
                     {
                         'path': ssh_key_path,
                         'keyData': ssh_key_value
-                    }
+                    } for ssh_key_value in ssh_key_values
                 ]
             }
         }

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/custom.py
@@ -486,6 +486,7 @@ def assign_vm_identity(cmd, resource_group_name, vm_name, assign_identity=None, 
 # endregion
 
 
+# region VirtualMachines
 def capture_vm(cmd, resource_group_name, vm_name, vhd_name_prefix,
                storage_container='vhds', overwrite=True):
     VirtualMachineCaptureParameters = cmd.get_models('VirtualMachineCaptureParameters')
@@ -501,7 +502,7 @@ def capture_vm(cmd, resource_group_name, vm_name, vhd_name_prefix,
 
 # pylint: disable=too-many-locals, unused-argument, too-many-statements, too-many-branches
 def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_v2', location=None, tags=None,
-              no_wait=False, authentication_type=None, admin_password=None,
+              no_wait=False, authentication_type=None, admin_password=None, computer_name=None,
               admin_username=None, ssh_dest_key_path=None, ssh_key_value=None, generate_ssh_keys=False,
               availability_set=None, nics=None, nsg=None, nsg_rule=None, accelerated_networking=None,
               private_ip_address=None, public_ip_address=None, public_ip_address_allocation='dynamic',
@@ -630,13 +631,13 @@ def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_
     vm_resource = build_vm_resource(
         cmd=cmd, name=vm_name, location=location, tags=tags, size=size, storage_profile=storage_profile, nics=nics,
         admin_username=admin_username, availability_set_id=availability_set, admin_password=admin_password,
-        ssh_key_value=ssh_key_value, ssh_key_path=ssh_dest_key_path, image_reference=image,
+        ssh_key_values=ssh_key_value, ssh_key_path=ssh_dest_key_path, image_reference=image,
         os_disk_name=os_disk_name, custom_image_os_type=os_type, authentication_type=authentication_type,
         os_publisher=os_publisher, os_offer=os_offer, os_sku=os_sku, os_version=os_version, os_vhd_uri=os_vhd_uri,
         attach_os_disk=attach_os_disk, os_disk_size_gb=os_disk_size_gb, custom_data=custom_data, secrets=secrets,
         license_type=license_type, zone=zone, disk_info=disk_info,
         boot_diagnostics_storage_uri=boot_diagnostics_storage, ultra_ssd_enabled=ultra_ssd_enabled,
-        proximity_placement_group=proximity_placement_group)
+        proximity_placement_group=proximity_placement_group, computer_name=computer_name)
 
     vm_resource['dependsOn'] = vm_dependencies
 
@@ -2055,7 +2056,7 @@ def create_vmss(cmd, vmss_name, resource_group_name, image,
         accelerated_networking=accelerated_networking, admin_username=admin_username,
         authentication_type=authentication_type, storage_profile=storage_profile, os_disk_name=os_disk_name,
         disk_info=disk_info, os_type=os_type, image=image, admin_password=admin_password,
-        ssh_key_value=ssh_key_value, ssh_key_path=ssh_dest_key_path, os_publisher=os_publisher, os_offer=os_offer,
+        ssh_key_values=ssh_key_value, ssh_key_path=ssh_dest_key_path, os_publisher=os_publisher, os_offer=os_offer,
         os_sku=os_sku, os_version=os_version, backend_address_pool_id=backend_address_pool_id,
         inbound_nat_pool_id=inbound_nat_pool_id, health_probe=health_probe,
         single_placement_group=single_placement_group, platform_fault_domain_count=platform_fault_domain_count,
@@ -2523,9 +2524,8 @@ def remove_vmss_identity(cmd, resource_group_name, vmss_name, identities=None):
                               _set_vmss)
 # endregion
 
+
 # region image galleries
-
-
 def list_image_galleries(cmd, resource_group_name=None):
     client = _compute_client_factory(cmd.cli_ctx)
     if resource_group_name:

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/hybrid_2018_03_01/test_vm_actions.py
@@ -39,13 +39,13 @@ class TestActions(unittest.TestCase):
         os.remove(private_key_file)
 
         args = mock.MagicMock()
-        args.ssh_key_value = public_key_file
+        args.ssh_key_value = [public_key_file]
         args.generate_ssh_keys = True
 
         # 1 verify we generate key files if not existing
         validate_ssh_key(args)
 
-        generated_public_key_string = args.ssh_key_value
+        generated_public_key_string = args.ssh_key_value[0]
         self.assertTrue(bool(args.ssh_key_value))
         self.assertTrue(is_valid_ssh_rsa_public_key(generated_public_key_string))
         self.assertTrue(os.path.isfile(private_key_file))
@@ -53,17 +53,17 @@ class TestActions(unittest.TestCase):
         # 2 verify we load existing key files
         # for convinience we will reuse the generated file in the previous step
         args2 = mock.MagicMock()
-        args2.ssh_key_value = generated_public_key_string
+        args2.ssh_key_value = [generated_public_key_string]
         args2.generate_ssh_keys = False
         validate_ssh_key(args2)
         # we didn't regenerate
-        self.assertEqual(generated_public_key_string, args.ssh_key_value)
+        self.assertEqual(generated_public_key_string, args.ssh_key_value[0])
 
         # 3 verify we do not generate unless told so
         _, private_key_file2 = tempfile.mkstemp(dir=temp_dir_name)
         public_key_file2 = private_key_file2 + '.pub'
         args3 = mock.MagicMock()
-        args3.ssh_key_value = public_key_file2
+        args3.ssh_key_value = [public_key_file2]
         args3.generate_ssh_keys = False
         with self.assertRaises(CLIError):
             validate_ssh_key(args3)
@@ -72,7 +72,7 @@ class TestActions(unittest.TestCase):
         _, public_key_file4 = tempfile.mkstemp(dir=temp_dir_name)
         public_key_file4 += '1'  # make it nonexisting
         args4 = mock.MagicMock()
-        args4.ssh_key_value = public_key_file4
+        args4.ssh_key_value = [public_key_file4]
         args4.generate_ssh_keys = True
         validate_ssh_key(args4)
         self.assertTrue(os.path.isfile(public_key_file4 + '.private'))

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/hybrid_2018_03_01/test_vm_defaults.py
@@ -264,6 +264,7 @@ class TestVMDefaultAuthType(unittest.TestCase):
         ns.ssh_key_value = None
         ns.ssh_dest_key_path = None
         ns.admin_password = None
+        ns.generate_ssh_keys = None
         return ns
 
     def test_default_windows(self):
@@ -282,7 +283,7 @@ class TestVMDefaultAuthType(unittest.TestCase):
         test_user = 'user12345'
         ns.admin_username = test_user
         ns.admin_password = None
-        ns.ssh_key_value = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCbIg1guRHbI0lV11wWDt1r2cUdcNd27CJsg+SfgC7miZeubtwUhbsPdhMQsfDyhOWHq1+ZL0M+nJZV63d/1dhmhtgyOqejUwrPlzKhydsbrsdUor+JmNJDdW01v7BXHyuymT8G4s09jCasNOwiufbP/qp72ruu0bIA1nySsvlf9pCQAuFkAnVnf/rFhUlOkhtRpwcq8SUNY2zRHR/EKb/4NWY1JzR4sa3q2fWIJdrrX0DvLoa5g9bIEd4Df79ba7v+yiUBOS0zT2ll+z4g9izHK3EO5d8hL4jYxcjKs+wcslSYRWrascfscLgMlMGh0CdKeNTDjHpGPncaf3Z+FwwwjWeuiNBxv7bJo13/8B/098KlVDl4GZqsoBCEjPyJfV6hO0y/LkRGkk7oHWKgeWAfKtfLItRp00eZ4fcJNK9kCaSMmEugoZWcI7NGbZXzqFWqbpRI7NcDP9+WIQ+i9U5vqWsqd/zng4kbuAJ6UuKqIzB0upYrLShfQE3SAck8oaLhJqqq56VfDuASNpJKidV+zq27HfSBmbXnkR/5AK337dc3MXKJypoK/QPMLKUAP5XLPbs+NddJQV7EZXd29DLgp+fRIg3edpKdO7ZErWhv7d+3Kws+e1Y+ypmR2WIVSwVyBEUfgv2C8Ts9gnTF4pNcEY/S2aBicz5Ew2+jdyGNQQ== test@example.com\n'
+        ns.ssh_key_value = ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCbIg1guRHbI0lV11wWDt1r2cUdcNd27CJsg+SfgC7miZeubtwUhbsPdhMQsfDyhOWHq1+ZL0M+nJZV63d/1dhmhtgyOqejUwrPlzKhydsbrsdUor+JmNJDdW01v7BXHyuymT8G4s09jCasNOwiufbP/qp72ruu0bIA1nySsvlf9pCQAuFkAnVnf/rFhUlOkhtRpwcq8SUNY2zRHR/EKb/4NWY1JzR4sa3q2fWIJdrrX0DvLoa5g9bIEd4Df79ba7v+yiUBOS0zT2ll+z4g9izHK3EO5d8hL4jYxcjKs+wcslSYRWrascfscLgMlMGh0CdKeNTDjHpGPncaf3Z+FwwwjWeuiNBxv7bJo13/8B/098KlVDl4GZqsoBCEjPyJfV6hO0y/LkRGkk7oHWKgeWAfKtfLItRp00eZ4fcJNK9kCaSMmEugoZWcI7NGbZXzqFWqbpRI7NcDP9+WIQ+i9U5vqWsqd/zng4kbuAJ6UuKqIzB0upYrLShfQE3SAck8oaLhJqqq56VfDuASNpJKidV+zq27HfSBmbXnkR/5AK337dc3MXKJypoK/QPMLKUAP5XLPbs+NddJQV7EZXd29DLgp+fRIg3edpKdO7ZErWhv7d+3Kws+e1Y+ypmR2WIVSwVyBEUfgv2C8Ts9gnTF4pNcEY/S2aBicz5Ew2+jdyGNQQ== test@example.com\n']
         _validate_vm_vmss_create_auth(ns)
         self.assertEqual(ns.authentication_type, 'ssh')
         self.assertEqual(ns.ssh_dest_key_path, '/home/{}/.ssh/authorized_keys'.format(test_user))
@@ -298,7 +299,7 @@ class TestVMDefaultAuthType(unittest.TestCase):
         self.assertEqual(ns.authentication_type, 'password')
 
         # throw when conflict with ssh key value
-        ns.ssh_key_value = 'junk but does not matter'
+        ns.ssh_key_value = ['junk but does not matter']
         with self.assertRaises(CLIError) as context:
             _validate_vm_vmss_create_auth(ns)
             self.assertTrue("SSH key cannot be used with password authentication type." in str(context.exception))

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/tests/hybrid_2019_03_01/test_vm_actions.py
@@ -39,13 +39,13 @@ class TestActions(unittest.TestCase):
         os.remove(private_key_file)
 
         args = mock.MagicMock()
-        args.ssh_key_value = public_key_file
+        args.ssh_key_value = [public_key_file]
         args.generate_ssh_keys = True
 
         # 1 verify we generate key files if not existing
         validate_ssh_key(args)
 
-        generated_public_key_string = args.ssh_key_value
+        generated_public_key_string = args.ssh_key_value[0]
         self.assertTrue(bool(args.ssh_key_value))
         self.assertTrue(is_valid_ssh_rsa_public_key(generated_public_key_string))
         self.assertTrue(os.path.isfile(private_key_file))
@@ -53,18 +53,18 @@ class TestActions(unittest.TestCase):
         # 2 verify we load existing key files
         # for convinience we will reuse the generated file in the previous step
         args2 = mock.MagicMock()
-        args2.ssh_key_value = generated_public_key_string
+        args2.ssh_key_value = [generated_public_key_string]
         args2.generate_ssh_keys = False
         validate_ssh_key(args2)
         # we didn't regenerate
-        self.assertEqual(generated_public_key_string, args.ssh_key_value)
+        self.assertEqual(generated_public_key_string, args.ssh_key_value[0])
 
         # 3 verify we do not generate unless told so
         fd, private_key_file2 = tempfile.mkstemp(dir=temp_dir_name)
         os.close(fd)
         public_key_file2 = private_key_file2 + '.pub'
         args3 = mock.MagicMock()
-        args3.ssh_key_value = public_key_file2
+        args3.ssh_key_value = [public_key_file2]
         args3.generate_ssh_keys = False
         with self.assertRaises(CLIError):
             validate_ssh_key(args3)
@@ -74,7 +74,7 @@ class TestActions(unittest.TestCase):
         os.close(fd)
         public_key_file4 += '1'  # make it nonexisting
         args4 = mock.MagicMock()
-        args4.ssh_key_value = public_key_file4
+        args4.ssh_key_value = [public_key_file4]
         args4.generate_ssh_keys = True
         validate_ssh_key(args4)
         self.assertTrue(os.path.isfile(public_key_file4 + '.private'))

commit 9623e40c9805b7e68bfb5f5bbc2c81ae6a6b1eff

Fix bug introduced in #9306. Now if an ssh key value is not specified, it is properly found from id_rsa.pub or generated.

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_params.py
@@ -459,7 +459,7 @@ def load_arguments(self, _):
             c.argument('generate_ssh_keys', action='store_true', help='Generate SSH public and private key files if missing. The keys will be stored in the ~/.ssh directory')
             c.argument('admin_username', help='Username for the VM.', default=get_default_admin_username())
             c.argument('admin_password', help="Password for the VM if authentication type is 'Password'.")
-            c.argument('ssh_key_value', options_list=['--ssh-key-values'], completer=FilesCompleter(), type=file_type, nargs='*')
+            c.argument('ssh_key_value', options_list=['--ssh-key-values'], completer=FilesCompleter(), type=file_type, nargs='+')
             c.argument('ssh_dest_key_path', help='Destination file path on the VM for the SSH key. If the file already exists, the specified key(s) are appended to the file.')
             c.argument('authentication_type', help='Type of authentication to use with the VM. Defaults to password for Windows and SSH public key for Linux. "all" enables both ssh and password authentication. ', arg_type=get_enum_type(['ssh', 'password', 'all']))
 

src/command_modules/azure-cli-vm/azure/cli/command_modules/vm/_validators.py
@@ -955,7 +955,7 @@ def validate_ssh_key(namespace):
         namespace.ssh_key_value = processed_ssh_key_values
     # if no ssh keys processed, try to generate new key / use existing at root.
     else:
-        namespace.ssh_key_value = _validate_ssh_key_helper("", namespace.generate_ssh_keys)
+        namespace.ssh_key_value = [_validate_ssh_key_helper("", namespace.generate_ssh_keys)]
 
 
 def _validate_ssh_key_helper(ssh_key_value, should_generate_ssh_keys):