d75026a795ca2a8529474eed58497456efddf34b
Oluwatosin Adewale <oluwatosin.adewale@microsoft.com>
2019-05-16 14:40:31 -0700

    Add comment explaining yaml formatter error handling logic.

--- src/azure-cli-core/azure/cli/core/_output.py
+++ src/azure-cli-core/azure/cli/core/_output.py
@@ -23,6 +23,7 @@ class AzOutputProducer(knack.output.OutputProducer):
         try:
             return safe_dump(obj.result, default_flow_style=False)
         except representer.RepresenterError:
+            # yaml.safe_dump fails when obj.result is an OrderedDict. knack's --query implementation converts the result to an OrderedDict. https://github.com/microsoft/knack/blob/af674bfea793ff42ae31a381a21478bae4b71d7f/knack/query.py#L46. # pylint: disable=line-too-long
             return safe_dump(json.loads(json.dumps(obj.result)), default_flow_style=False)
 
     @staticmethod
