781320f27cbfcb7f3379742f0b7a19b2423dbc40
adewaleo <tosyn.vic@gmail.com>
2019-05-19 21:10:19 -0700

    [core] Fix bug where --query cannot be used with --output yaml (#9403)
    * [core] Fix bug where --query cannot be used with --output yaml
    
    * Add comment explaining yaml formatter error handling logic.

--- src/azure-cli-core/azure/cli/core/_output.py
+++ src/azure-cli-core/azure/cli/core/_output.py
@@ -17,8 +17,14 @@ class AzOutputProducer(knack.output.OutputProducer):
 
     @staticmethod
     def format_yaml(obj):
-        import yaml
-        return yaml.safe_dump(obj.result, default_flow_style=False)
+        from yaml import (safe_dump, representer)
+        import json
+
+        try:
+            return safe_dump(obj.result, default_flow_style=False)
+        except representer.RepresenterError:
+            # yaml.safe_dump fails when obj.result is an OrderedDict. knack's --query implementation converts the result to an OrderedDict. https://github.com/microsoft/knack/blob/af674bfea793ff42ae31a381a21478bae4b71d7f/knack/query.py#L46. # pylint: disable=line-too-long
+            return safe_dump(json.loads(json.dumps(obj.result)), default_flow_style=False)
 
     @staticmethod
     def format_none(_):
