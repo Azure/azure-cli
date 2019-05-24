4e2397e7f6a7a72a48fe7f4400e9f199a1495b9d
Rhea Khanna <rheak@microsoft.com>
2019-05-24 15:52:07 -0700

    Update zone for nat gateway

--- src/command_modules/azure-cli-natgateway/azure/cli/command_modules/natgateway/custom.py
+++ src/command_modules/azure-cli-natgateway/azure/cli/command_modules/natgateway/custom.py
@@ -39,7 +39,7 @@ def create_nat_gateway(cmd, nat_gateway_name, resource_group_name,
 
 
 def update_nat_gateway(instance, cmd, public_ip_addresses=None,
-                       public_ip_prefixes=None, idle_timeout=None):
+                       public_ip_prefixes=None, idle_timeout=None, zone=None):
 
     with cmd.update_context(instance) as c:
         c.set_param('idle_timeout_in_minutes', idle_timeout)
