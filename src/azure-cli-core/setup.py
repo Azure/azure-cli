a61f1800a16caaa1537146fe6275aa4bd4aa3098
Travis Prescott <tjprescott@users.noreply.github.com>
2019-05-31 07:23:33 -0700

    Update CLI to use Knack 0.6.2 (preview mechanism) (#9456)
    * Initial work.
    
    * Update preview references.
    
    * Additional work to wire up preview arguments.
    
    * Update for knack changes.
    
    * Update dependencies. Remove command suggestion code. Deprecate BatchAI. Hide ACS.
    
    * Restore CLI-specific spellchecker.
    
    * Update history entries.

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -59,7 +59,7 @@ DEPENDENCIES = [
     'colorama>=0.3.9',
     'humanfriendly>=4.7',
     'jmespath',
-    'knack~=0.6.1',
+    'knack~=0.6.2',
     'msrest>=0.4.4',
     'msrestazure>=0.4.25',
     'paramiko>=2.0.8',
