6dd39e7ecb2e9b3534397e74a222fea17d9ed283
Yadi Reyes <36422356+yareyes@users.noreply.github.com>
2019-06-10 09:18:28 -0700

    Adding --image-sku as a parameter for sql vm create and update command. (#9556)
    * Adding support for image sku.
    
    * Added test run
    
    * Re-running all tests
    
    * Made sql license required parameter.
    
    * Addressing comments on review

--- src/command_modules/azure-cli-sqlvm/setup.py
+++ src/command_modules/azure-cli-sqlvm/setup.py
@@ -12,7 +12,7 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.1.1"
+VERSION = "0.2.0"
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -31,7 +31,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'azure-cli-core',
-    'azure-mgmt-sqlvirtualmachine==0.2.0'
+    'azure-mgmt-sqlvirtualmachine==0.3.0'
 ]
 
 with open('README.rst', 'r', encoding='utf-8') as f:
