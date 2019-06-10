7bd80258e9f758a38cb393125755ab6d6c1a9df0
Sapan Saxena <v-sapsax@microsoft.com>
2019-06-10 16:43:05 -0700

    Resolved PR comment

--- src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/tests/latest/test_iot_digitaltwin_commands.py
+++ src/command_modules/azure-cli-iot/azure/cli/command_modules/iot/tests/latest/test_iot_digitaltwin_commands.py
@@ -24,15 +24,20 @@ class IoTDigitalTwinTest(ScenarioTest):
                                 self.exists('provisioningState'),
                                 self.check('status', 'Provisioned')]).get_output_in_json()
 
+        self.kwargs.update({
+            'id': repo['id'],
+            'provisioningState': repo['provisioningState']
+        })
+
         # Test 'az iot digitaltwin repository get-provision-status'
-        self.cmd('iot digitaltwin repository get-provision-status -e "{endpoint}" -r {} -s {}'.format(repo['id'], repo['provisioningState'], **self.kwargs),
+        self.cmd('iot digitaltwin repository get-provision-status -e "{endpoint}" -r {id} -s {provisioningState}',
                  checks=[self.exists('id'),
                          self.exists('provisioningState'),
                          self.exists('status'),
-                         self.check('provisioningState', repo['provisioningState'])])
+                         self.check('provisioningState', '{provisioningState}')])
 
         # Test 'az iot digitaltwin repository update'
-        self.cmd('iot digitaltwin repository update -e "{endpoint}" -r {} -n {name2}'.format(repo['id'], **self.kwargs),
+        self.cmd('iot digitaltwin repository update -e "{endpoint}" -r {id} -n {name2}',
                  checks=[self.exists('id'),
                          self.exists('provisioningState'),
                          self.check('status', 'Provisioned')])
@@ -42,18 +47,18 @@ class IoTDigitalTwinTest(ScenarioTest):
                  checks=[self.greater_than('length([*])', 0)])
 
         # Test 'az iot digitaltwin repository show'
-        self.cmd('iot digitaltwin repository show -e "{endpoint}" -r {}'.format(repo['id'], **self.kwargs),
+        self.cmd('iot digitaltwin repository show -e "{endpoint}" -r {id}',
                  checks=[self.check('name', '{name2}'),
                          self.check('properties.kind', 'Repository'),
-                         self.check('id', repo['id'])])
+                         self.check('id', '{id}')])
 
         # Test 'az iot digitaltwin key list'
-        self.cmd('iot digitaltwin key list -e "{endpoint}" -r {}'.format(repo['id'], **self.kwargs),
+        self.cmd('iot digitaltwin key list -e "{endpoint}" -r {id}',
                  checks=[self.check('length([*])', 0)])
 
         # Test 'az iot digitaltwin key create'
-        key = self.cmd('iot digitaltwin key create -e "{endpoint}" -r {} --role {user_role}'.format(repo['id'], **self.kwargs),
-                       checks=[self.check('repositoryId', repo['id']),
+        key = self.cmd('iot digitaltwin key create -e "{endpoint}" -r {id} --role {user_role}',
+                       checks=[self.check('repositoryId', '{id}'),
                                self.check('userRole', '{user_role}'),
                                self.exists('connectionString'),
                                self.exists('secret'),
@@ -62,12 +67,16 @@ class IoTDigitalTwinTest(ScenarioTest):
                                self.exists('tenantName'),
                                self.exists('id')]).get_output_in_json()
 
+        self.kwargs.update({
+            'keyId': key['id']
+        })
+
         # Test 'az iot digitaltwin key list'
-        self.cmd('iot digitaltwin key list -e "{endpoint}" -r {}'.format(repo['id'], **self.kwargs),
+        self.cmd('iot digitaltwin key list -e "{endpoint}" -r {id}',
                  checks=[self.check('length([*])', 1)])
 
         # Test 'az iot digitaltwin key show'
-        self.cmd('iot digitaltwin key show -e "{endpoint}" -r {} -k {}'.format(repo['id'], key['id'], **self.kwargs),
+        self.cmd('iot digitaltwin key show -e "{endpoint}" -r {id} -k {keyId}',
                  checks=[self.check('repositoryId', repo['id']),
                          self.check('userRole', '{user_role}'),
                          self.exists('connectionString'),
@@ -78,8 +87,8 @@ class IoTDigitalTwinTest(ScenarioTest):
                          self.exists('id')])
 
         # Test 'az iot digitaltwin key update'
-        self.cmd('iot digitaltwin key update -e "{endpoint}" -r {} -k {} --role {user_role2}'.format(repo['id'], key['id'], **self.kwargs),
-                 checks=[self.check('repositoryId', repo['id']),
+        self.cmd('iot digitaltwin key update -e "{endpoint}" -r {id} -k {keyId} --role {user_role2}',
+                 checks=[self.check('repositoryId', '{id}'),
                          self.check('userRole', '{user_role2}'),
                          self.exists('connectionString'),
                          self.exists('secret'),
@@ -89,15 +98,15 @@ class IoTDigitalTwinTest(ScenarioTest):
                          self.exists('id')])
 
         # Test 'az iot digitaltwin key delete'
-        self.cmd('iot digitaltwin key delete -e "{endpoint}" -r {} -k {}'.format(repo['id'], key['id'], **self.kwargs),
+        self.cmd('iot digitaltwin key delete -e "{endpoint}" -r {id} -k {keyId}',
                  checks=self.is_empty())
 
         # Test 'az iot digitaltwin key list'
-        self.cmd('iot digitaltwin key list -e "{endpoint}" -r {}'.format(repo['id'], **self.kwargs),
+        self.cmd('iot digitaltwin key list -e "{endpoint}" -r {id}',
                  checks=[self.check('length([*])', 0)])
 
         # Test 'az iot digitaltwin repository delete'
-        self.cmd('iot digitaltwin repository delete -e "{endpoint}" -r {}'.format(repo['id'], **self.kwargs),
+        self.cmd('iot digitaltwin repository delete -e "{endpoint}" -r {id}',
                  checks=[self.exists('id'),
                          self.exists('provisioningState'),
                          self.exists('status'),
