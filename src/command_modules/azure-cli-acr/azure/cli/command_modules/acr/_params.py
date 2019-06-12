4e72b9721d5fbc8a8a873afd8fbd4d1fcd0a9c12
Jason R. Shaver <jasonrshaver@gmail.com>
2019-06-11 22:40:47 -0700

    Updated pack command help

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_params.py
@@ -136,7 +136,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-statements
 
     with self.argument_context('acr pack') as c:
         c.argument('registry_name', options_list=['--registry', '-r'])
-        c.argument('image_name', options_list=['--image', '-t'], help="The name and tag of the image using the format: '-t repo/image:tag'.")
+        c.argument('image_name', options_list=['--image', '-t'], help="The name and tag of the image, in lowercase, using the format: '-t repo/image:tag'.")
         c.argument('builder', options_list=['--builder', '-b'], help="The name and tag of a Buildpack builder image.")
         c.argument('pull', options_list=['--pull'], help="Pull the latest builder and run images before use.", action='store_true')
         c.positional('source_location', help="The local source code directory path (e.g., './src') or the URL to a git repository (e.g., 'https://github.com/Azure-Samples/acr-build-helloworld-node.git') or a remote tarball (e.g., 'http://server/context.tar.gz').", completer=FilesCompleter())
