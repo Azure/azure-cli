030923570a7719cf3bbf7636f7034505dced1261
Limin Gu <48228819+limingu@users.noreply.github.com>
2019-06-19 13:12:21 -0700

    Storage API 2019-04-01 (#9687)
    * Storage API 2019-04-01
    
    * Fix tests
    
    * Fix tests
    
    * Fix test

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -116,7 +116,7 @@ class SDKProfile(object):  # pylint: disable=too-few-public-methods
 
 AZURE_API_PROFILES = {
     'latest': {
-        ResourceType.MGMT_STORAGE: '2018-11-01',
+        ResourceType.MGMT_STORAGE: '2019-04-01',
         ResourceType.MGMT_NETWORK: '2019-04-01',
         ResourceType.MGMT_COMPUTE: SDKProfile('2019-03-01', {
             'resource_skus': '2017-09-01',
