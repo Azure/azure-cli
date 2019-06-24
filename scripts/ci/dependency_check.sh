bb4cfb46e9a33ed4f99356396ca3daa0b8bd7553
Martin Strobel <marstr@microsoft.com>
2019-06-24 16:04:21 -0700

    Add az invocations on dependency check to further ensure all is well

--- scripts/ci/dependency_check.sh
+++ scripts/ci/dependency_check.sh
@@ -10,3 +10,7 @@ find ${REPO_ROOT}/src -name setup.py -type f | xargs dirname | xargs pip3 instal
 pip3 install -r ${REPO_ROOT}/src/azure-cli/requirements.py3.linux.txt
 
 pip3 check
+
+python3 -m azure.cli --version
+
+python3 -m azure.cli self-test
