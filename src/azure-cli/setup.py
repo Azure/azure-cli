208b73469a365c1a1f104c9ec0b445e088240e4a
Leonard Francis <leonard@greenqloud.com>
2019-06-26 10:42:05 +0000

    NFSAAS-2316 R4 update and structure change (#33)
    * NFSAAS-2316 update to R4 RP standard
    
    * NFSAAS-2316 update to R4 RP standard
    
    * NFSAAS-2316 correction to path in R4 update
    
    * NFSAAS-2316 R4 update and structure change

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -101,6 +101,7 @@ DEPENDENCIES = [
     'azure-mgmt-monitor~=0.5.2',
     'azure-mgmt-msi~=0.2',
     'azure-mgmt-network~=3.0',
+    'azure-mgmt-netapp~=0.4.0',
     'azure-mgmt-policyinsights~=0.3.1',
     'azure-mgmt-privatedns~=0.1.0',
     'azure-mgmt-rdbms~=1.8',
