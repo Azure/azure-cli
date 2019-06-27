6354cce02b013bb7e25ab7fee8cba19124a087d3
Martin Strobel <marstr@microsoft.com>
2019-06-27 16:55:08 -0700

    Update Linux Installers to use requirements.py*.Linux.txt (#9820)
    * Add script for installing all locally provided, then all transitive dependencies.
    
    * Update RPM spec to use requirements.py*.Linux.txt
    
    * Use install_full script for Dockerfile
    
    * Switch deb install mechanism
    
    * Enable Xenial build for testing
    
    * Fix path
    
    * Add Python and Pip aliases for build machine
    
    * set aliases in sub-process instead of actual one
    
    * Add diagnostics
    
    * Avoid the games, just embed what I need to install these scripts
    
    * make absolute paths
    
    * Fix dependency check requirements.txt location.
    
    * Add MIT license header
    
    * Revert "Enable Xenial build for testing"
    
    This reverts commit d2f353ef9de9abff6ddbabce3269d3dff75f5801.

--- scripts/ci/dependency_check.sh
+++ scripts/ci/dependency_check.sh
@@ -13,7 +13,7 @@ pip3 check
 # Install everything from our repository first.
 find ${REPO_ROOT}/src -name setup.py -type f | xargs dirname | xargs pip3 install --no-deps
 
-pip3 install -r ${REPO_ROOT}/src/azure-cli/requirements.py3.linux.txt
+pip3 install -r ${REPO_ROOT}/src/azure-cli/requirements.py3.Linux.txt
 
 pip3 check
 
