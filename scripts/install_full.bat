646e133511d289468ad267ceead713de24a7d96d
Martin Strobel <marstr@microsoft.com>
2019-06-28 16:15:11 -0700

    Adding windows requirements.txt based installation script.

---
+++ scripts/install_full.bat
@@ -0,0 +1,13 @@
+pushd %~dp0..
+
+set cmd="dir .\src\ /b | findstr /v azure-cli-testsdk"
+for /f "tokens=*" %%e in (' %cmd% ') do (
+    pip install --no-deps %%e
+)
+
+set cmd="python .\scripts\get-python-version.py"
+for /f "tokens=*" %%v in (' %cmd% ') do set SHORT_PY_VER=%%v
+pip install -r .\src\azure-cli\requirements.%SHORT_PY_VER%.Windows.txt
+
+echo %OUTPUT%
+popd
