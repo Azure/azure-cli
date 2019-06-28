8aa0dee5586a9aa894c89863de04d8de54af88c7
Martin Strobel <marstr@microsoft.com>
2019-06-28 14:56:20 -0700

    Ignore failure to uninstall deps, likely means there were none to uninstall.

--- scripts/ci/dependency_check.sh
+++ scripts/ci/dependency_check.sh
@@ -6,7 +6,7 @@ REPO_ROOT="$(dirname ${BASH_SOURCE[0]})/../.."
 
 # Uninstall any cruft that can poison the rest of the checks in this script.
 pip freeze > baseline_deps.txt
-pip uninstall -y -r baseline_deps.txt
+pip uninstall -y -r baseline_deps.txt || true
 pip list
 pip check
 rm baseline_deps.txt
