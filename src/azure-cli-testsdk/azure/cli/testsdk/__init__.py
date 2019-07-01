13f921398ffbe78533c728428775e0a99fb32b85
adewaleo <oladewal@microsoft.com>
2019-07-01 11:22:59 -0700

    [Test] Traffic is not recorded by default in resource preparers (#9747)
    * Traffic is not recorded by default during RGPreparer setup. Add new GraphClient replacer. Update preparers. Workaround of bug in azure_devtools GeneralNameReplacer. Re-record tests.
    Handle live only test class in NoTrafficRecordingPreparer. Properly handle python3 bytes.
    
    * Fix issue when during test recording, preparer monikers have numerical part offset by the number of calls of ScenarioTest.create_random_name() before first call of the preparer's process_request method.
    
    * Rebase with dev and update function app recordings.
    
    * Re-record some failing tests.
    
    Re-record failed test.

--- src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/__init__.py
@@ -7,12 +7,12 @@ from azure_devtools.scenario_tests import live_only, record_only, get_sha1_hash
 
 from .base import ScenarioTest, LiveScenarioTest
 from .preparers import (StorageAccountPreparer, ResourceGroupPreparer, RoleBasedServicePrincipalPreparer,
-                        KeyVaultPreparer, AADGraphUserReplacer, ManagedApplicationPreparer)
+                        KeyVaultPreparer, ManagedApplicationPreparer)
 from .exceptions import CliTestError
 from .checkers import (JMESPathCheck, JMESPathCheckExists, JMESPathCheckGreaterThan, NoneCheck, StringCheck,
                        StringContainCheck)
 from .decorators import api_version_constraint
-from .utilities import create_random_name
+from .utilities import create_random_name, AADGraphUserReplacer
 from .patches import MOCKED_USER_NAME
 
 __all__ = ['ScenarioTest', 'LiveScenarioTest', 'ResourceGroupPreparer', 'StorageAccountPreparer',
