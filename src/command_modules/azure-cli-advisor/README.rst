6f8230c2706d4722cfba330764eb31ff65aa7cfa
Martin Strobel <marstr@microsoft.com>
2019-07-01 15:58:47 -0700

    Marking azure-cli-advisor as deprecated

--- src/command_modules/azure-cli-advisor/README.rst
+++ src/command_modules/azure-cli-advisor/README.rst
@@ -3,3 +3,8 @@ Microsoft Azure CLI 'advisor' Command Module
 
 This package is for the 'advisor' module.
 i.e. 'az advisor'
+
+This packages is now deprecated. The modules previously maintained here have been moved to the
+`azure-cli package`__.
+
+__ https://pypi.org/project/azure-cli/
--- src/command_modules/azure-cli-advisor/azure/cli/command_modules/advisor/__init__.py
+++ src/command_modules/azure-cli-advisor/azure/cli/command_modules/advisor/__init__.py
@@ -2,6 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+import warnings
+warnings.warn("deprecated", DeprecationWarning)
 
 from azure.cli.core import AzCommandsLoader
 
