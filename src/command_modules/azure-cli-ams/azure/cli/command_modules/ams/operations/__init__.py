f3ea6ccfc3f104410ae87999c3c8ef8502f25774
Martin Strobel <marstr@microsoft.com>
2019-07-03 16:07:30 -0700

    Removing missed deprecation warning

--- src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/__init__.py
+++ src/command_modules/azure-cli-ams/azure/cli/command_modules/ams/operations/__init__.py
@@ -2,6 +2,3 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-import warnings
-warnings.warn("deprecated", DeprecationWarning)
