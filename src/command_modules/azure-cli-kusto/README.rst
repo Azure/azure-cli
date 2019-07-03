8a61f61771ea4cd964bdacbad7817f74791c5b5e
Martin Strobel <marstr@microsoft.com>
2019-07-03 13:28:44 -0700

    Marking azure-cli-kusto as deprecated

--- src/command_modules/azure-cli-kusto/README.rst
+++ src/command_modules/azure-cli-kusto/README.rst
@@ -2,3 +2,8 @@ Microsoft Azure CLI 'KUSTO' Command Module
 ==========================================
 
 Provides management capabilities for Kusto (Azure Data Explorer) clusters.
+
+This packages is now deprecated. The modules previously maintained here have been moved to the
+`azure-cli package`__.
+
+__ https://pypi.org/project/azure-cli/
--- src/command_modules/azure-cli-kusto/setup.py
+++ src/command_modules/azure-cli-kusto/setup.py
@@ -12,10 +12,10 @@ except ImportError:
     logger.warn("Wheel is not available, disabling bdist_wheel hook")
     cmdclass = {}
 
-VERSION = "0.2.3"
+VERSION = "0.2.4"
 
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
+    'Development Status :: 7 - Inactive',
     'Intended Audience :: Developers',
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
