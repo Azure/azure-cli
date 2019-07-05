a4aa88c1e1f9933aaf438c1d9e31d3a272d57c84
Travis Prescott <trpresco@microsoft.com>
2019-07-05 14:47:11 -0700

    Update with published azdev.

--- scripts/ci/install_azdev.sh
+++ scripts/ci/install_azdev.sh
@@ -8,7 +8,5 @@ pip install virtualenv
 python -m virtualenv env
 . env/bin/activate
 pip install -U pip setuptools wheel -q
-# TODO: temp install from azdev working branch
-pip install git+https://github.com/Azure/azure-cli-dev-tools.git@ConvertCIWork -q
-# pip install azdev==0.1.3 -q
+pip install azdev==0.1.4 -q
 azdev setup -c
