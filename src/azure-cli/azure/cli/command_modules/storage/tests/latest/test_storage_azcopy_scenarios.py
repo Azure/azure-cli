90d1204ab433eedd7a6b090c777d521d61783b1f
Zunli Hu <zuh@microsoft.com>
2019-07-18 15:33:19 +0800

    add test scenario

--- src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_azcopy_scenarios.py
+++ src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_azcopy_scenarios.py
@@ -77,61 +77,14 @@ class StorageAzcopyTests(StorageScenarioMixin, LiveScenarioTest):
     @StorageAccountPreparer(parameter_name='account_1')
     @StorageAccountPreparer(parameter_name='account_2')
     @StorageTestFilesPreparer()
-    def test_storage_copy(self, account_1, account_2, test_dir):
+    def test_storage_blob_copy(self, account_1, account_2, test_dir):
         container_1 = self.create_container(account_1)
         container_2 = self.create_container(account_2)
-        # sync directory
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {}'.format(
-            test_dir, container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 41))
-
-        self.cmd('storage blob delete-batch -s {} --account-name {}'.format(
-            container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 0))
-
-        # resync container
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {}'.format(
-            test_dir, container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 41))
-
-        # update file
-        with open(os.path.join(test_dir, 'readme'), 'w') as f:
-            f.write('updated.')
-        # sync one blob
-        self.cmd('storage blob list -c {} --account-name {} --prefix readme'.format(
-            container, storage_account), checks=JMESPathCheck('[0].properties.contentLength', 87))
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {} -d readme'.format(
-            os.path.join(test_dir, 'readme'), container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {} --prefix readme'.format(
-            container, storage_account), checks=JMESPathCheck('[0].properties.contentLength', 8))
-
-        # delete one file and sync
-        os.remove(os.path.join(test_dir, 'readme'))
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {}'.format(
-            test_dir, container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 40))
-
-        # delete one folder and sync
-        shutil.rmtree(os.path.join(test_dir, 'apple'))
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {}'.format(
-            test_dir, container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 30))
-
-        # syn with another folder
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {}'.format(
-            os.path.join(test_dir, 'butter'), container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 20))
-
-        # empty the folder and sync
-        shutil.rmtree(os.path.join(test_dir, 'butter'))
-        shutil.rmtree(os.path.join(test_dir, 'duff'))
-        self.cmd('storage blob sync -s "{}" -c {} --account-name {}'.format(
-            test_dir, container, storage_account))
-        self.cmd('storage blob list -c {} --account-name {}'.format(
-            container, storage_account), checks=JMESPathCheck('length(@)', 0))
+        account_url_1 = 'https://{}.blob.core.windows.net'.format(account_1)
+        account_url_2 = 'https://{}.blob.core.windows.net'.format(account_2)
+        container_url_1 = '{}/{}'.format(account_url_1, container_1)
+        container_url_2 = '{}/{}'.format(account_url_2, container_2)
+        # Upload single file
+        self.cmd('storage copy -s "{}" -d "{}"'.format(
+            test_dir/readme, container_url_1))
+       
