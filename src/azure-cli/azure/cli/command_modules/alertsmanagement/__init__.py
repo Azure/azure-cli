ef23c6dd5020003893374e48487a75659b164e5b
Sarthak Rajwanshi <sarajwan@microsoft.com>
2019-07-23 16:14:06 +0530

    Added __init__.py(modified)

---
+++ src/azure-cli/azure/cli/command_modules/alertsmanagement/__init__.py
@@ -0,0 +1,25 @@
+from azure.cli.core import AzCommandsLoader
+
+import azure.cli.command_modules.alertsmanagement._help 
+
+
+class AlertsManagementCommandsLoader(AzCommandsLoader):
+
+    def __init__(self, cli_ctx=None):
+        from azure.cli.core.commands import CliCommandType
+        from azure.cli.core.profiles import ResourceType
+        alertsmanagement_custom = CliCommandType(operations_tmpl='azure.cli.command_modules.alertsmanagement.custom#{}')
+        super(AlertsManagementCommandsLoader, self).__init__(cli_ctx=cli_ctx, custom_command_type=alertsmanagement_custom,  
+                                                    resource_type=ResourceType.MGMT_BILLING)    #TODO: change resource tpye to alertsmanagement
+
+    def load_command_table(self, args):
+        from azure.cli.command_modules.alertsmanagement.commands import load_command_table
+        load_command_table(self, args)
+        return self.command_table
+
+    def load_arguments(self, command):
+        from azure.cli.command_modules.alertsmanagement._params import load_arguments
+        load_arguments(self, command)
+
+
+COMMAND_LOADER_CLS = AlertsManagementCommandsLoader
