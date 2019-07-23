640195079baa17f25f133abb99b034c519ac0f30
Sarthak Rajwanshi <sarajwan@microsoft.com>
2019-07-23 14:46:06 +0530

    Initial alertsmanagement cli commit

---
+++ src/azure-cli/azure/cli/command_modules/alertsmanagement/_client_factory.py
@@ -0,0 +1,15 @@
+
+def cf_alertsmanagement(cli_ctx, **_):
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.mgmt.alertsmanagement import AlertsManagementClient
+    return get_mgmt_service_client(cli_ctx, AlertsManagementClient)
+
+def alerts_mgmt_client_factory(cli_ctx, kwargs):
+    return cf_alertsmanagement(cli_ctx, **kwargs).alerts 
+
+
+def smart_groups_mgmt_client_factory(cli_ctx, kwargs):
+    return cf_alertsmanagement(cli_ctx, **kwargs).smart_groups
+
+import azure_sdk_for_python 
+
