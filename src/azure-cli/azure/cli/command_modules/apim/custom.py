5c81433aba6421d289ca90b6787309665861d9fc
Jon Lester <lesterjt@gmail.com>
2019-07-23 15:44:21 -0400

    Update custom.py

--- src/azure-cli/azure/cli/command_modules/apim/custom.py
+++ src/azure-cli/azure/cli/command_modules/apim/custom.py
@@ -56,8 +56,6 @@ def update_apim(cmd, instance, tags=None):
     return instance
 
 # API Operations
-def list_apim_api(client, service_name, resource_group_name=None):
+def list_apim_api(client, resource_group_name, service_name):
     """List all APis for the given service instance. """
-    if resource_group_name:
-        return client.api.list_by_service(resource_group_name, service_name)
-    return client.api.list_by_service()
+    return client.api.list_by_service(resource_group_name, service_name)
