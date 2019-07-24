5bac060dcc2381eb33b2b24711f05885a27ea20d
Kevin Hillinger <kevin.hillinger@microsoft.com>
2019-07-24 09:54:32 -0400

    apim cf updates

--- src/azure-cli/azure/cli/command_modules/apim/commands.py
+++ src/azure-cli/azure/cli/command_modules/apim/commands.py
@@ -6,23 +6,30 @@
 # pylint: disable=line-too-long
 from azure.cli.core.commands import CliCommandType
 from azure.cli.command_modules.apim._format import (service_output_format)
-from azure.cli.command_modules.apim._client_factory import (cf_apim, cf_service)
-
-apim_sdk = CliCommandType(
-    operations_tmpl='azure.mgmt.apimanagement.operations#ApiManagementServiceOperations.{}',
-    client_factory=cf_service
-)
+from azure.cli.command_modules.apim._client_factory import (api_client_factory, service_client_factory)
 
 def load_command_table(self, _):
-    with self.command_group('apim', apim_sdk,  is_preview=True) as g:
+
+    service_sdk = CliCommandType(
+        operations_tmpl='azure.mgmt.apimanagement.operations#ApiManagementServiceOperations.{}',
+        client_factory=service_client_factory
+    )
+
+    api_sdk = CliCommandType(
+        operations_tmpl='azure.mgmt.apimanagement.operations#ApiOperations.{}',
+        client_factory=api_client_factory
+    )
+
+    with self.command_group('apim', service_sdk,  is_preview=True) as g:
         g.custom_command('create', 'create_apim', supports_no_wait=True)
         g.custom_show_command('show', 'get_apim', table_transformer=service_output_format) 
         g.custom_command('list', 'list_apim', table_transformer=service_output_format)
         g.command('delete', 'delete', confirmation=True, supports_no_wait=True)
         g.generic_update_command('update', setter_name='update', custom_func_name='update_apim')
-        
         g.custom_command('check-name', 'check_name_availability')
         g.custom_command('backup', 'apim_backup', supports_no_wait=True)
 
-    with self.command_group('apim api', apim_sdk) as g:
+    with self.command_group('apim api', api_sdk) as g:
         g.custom_command('list', 'list_apim_api')
+        g.command('delete', 'delete', confirmation=True, supports_no_wait=True)
+
