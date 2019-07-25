60b0306b82ea248ef4897a277cf4353e98e9f60d
Jon Lester <lesterjt@gmail.com>
2019-07-25 11:12:01 -0400

    Update test_apim_scenario.py

--- src/azure-cli/azure/cli/command_modules/apim/tests/latest/test_apim_scenario.py
+++ src/azure-cli/azure/cli/command_modules/apim/tests/latest/test_apim_scenario.py
@@ -19,13 +19,19 @@ class ApimScenarioTest(ScenarioTest):
     def test_apim_core_service(self, resource_group, resource_group_location):
         service_name = self.create_random_name('cli-test-apim-', 50)
 
+        #try to use the injected location, but if the location is not known
+        #fall back to west us, otherwise we can't validate since the sdk returns displayName
+        if not resource_group_location in KNOWN_LOCS.keys():
+            resource_group_location = 'westus'
+
         self.kwargs.update({
             'service_name': service_name,
             'rg_loc': resource_group_location,
+            'rg_loc_displayName': KNOWN_LOCS.get(resource_group_location),
             'notification_sender_email': 'notifications@contsoso.com',
             'publisher_email': 'publisher@contsoso.com',
             'publisher_name': 'Contoso',
-            'skuname': 'Developer',
+            'sku_name': 'Developer',
             'skucapacity': 1,
             'enable_cert': True,
             'tags': ["foo=boo"]
@@ -34,11 +40,10 @@ class ApimScenarioTest(ScenarioTest):
         self.cmd('apim check-name -n {service_name}',
                  checks=[self.check('nameAvailable', True)])
 
-        self.cmd('apim create --name {service_name} -g {rg} -l {rg_loc} --sku {skuname} --publisher-email {publisher_email} --enable-client-certificate {enable_cert}' ,
+        self.cmd('apim create --name {service_name} -g {rg} -l {rg_loc} --sku-name {sku_name} --publisher-email {publisher_email} --publisher-name {publisher_name} --enable-client-certificate {enable_cert}' ,
                  checks=[self.check('name', '{service_name}'),
-                         #todo: fix location check.  if loc is passed as 'westus', the return object uses 'West US'
-                         #self.check('location', '{rg_loc}'),
-                         self.check('sku.name', '{skuname}'),
+                         self.check('location', '{rg_loc}'),
+                         self.check('sku.name', '{sku_name}'),
                          self.check('provisioningState', 'Succeeded'),
                          self.check('publisherEmail', '{publisher_email}')])
 
@@ -58,3 +63,38 @@ class ApimScenarioTest(ScenarioTest):
         self.cmd('apim delete -g {rg} -n {service_name}')
         final_count = len(self.cmd('apim list').get_output_in_json())
         self.assertTrue(final_count, count - 1)
+
+KNOWN_LOCS = {'eastasia': 'EastAsia',
+'southeastasia': 'SoutheastAsia',
+'centralus': 'CentralUS',
+'eastus': 'EastUS',
+'eastus2': 'EastUS2',
+'westus': 'WestUS',
+'northcentralus': 'NorthCentralUS',
+'southcentralus': 'SouthCentralUS',
+'northeurope': 'NorthEurope',
+'westeurope': 'WestEurope',
+'japanwest': 'JapanWest',
+'japaneast': 'JapanEast',
+'brazilsouth': 'BrazilSouth',
+'australiaeast': 'AustraliaEast',
+'australiasoutheast': 'AustraliaSoutheast',
+'southindia': 'SouthIndia',
+'centralindia': 'CentralIndia',
+'westindia': 'WestIndia',
+'canadacentral': 'CanadaCentral',
+'canadaeast': 'CanadaEast',
+'uksouth': 'UKSouth',
+'ukwest': 'UKWest',
+'westcentralus': 'WestCentralUS',
+'westus2': 'WestUS2',
+'koreacentral': 'KoreaCentral',
+'koreasouth': 'KoreaSouth',
+'francecentral': 'FranceCentral',
+'francesouth': 'FranceSouth',
+'australiacentral': 'AustraliaCentral',
+'australiacentral2': 'AustraliaCentral2',
+'uaecentral': 'UAECentral',
+'uaenorth': 'UAENorth',
+'southafricanorth': 'SouthAfricaNorth',
+'southafricawest': 'SouthAfricaWest'}
