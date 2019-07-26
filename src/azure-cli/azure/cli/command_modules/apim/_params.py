667c10d66c5ade158ecfe0216f0a2ebd8ad8f01c
Kevin Hillinger <kevin.hillinger@microsoft.com>
2019-07-26 11:22:07 -0400

    removing superfluous validator

--- src/azure-cli/azure/cli/command_modules/apim/_params.py
+++ src/azure-cli/azure/cli/command_modules/apim/_params.py
@@ -11,7 +11,6 @@ from azure.cli.core.commands.parameters import (get_enum_type,
                                                 get_three_state_flag)
 from azure.mgmt.apimanagement.models.api_management_client_enums import (SkuType, VirtualNetworkType)
 from azure.mgmt.apimanagement.models import (ApiManagementServiceIdentity)
-from ._validators import (validate_capacity)
 
 SKU_TYPES = SkuType
 VNET_TYPES =VirtualNetworkType
@@ -35,7 +34,7 @@ def load_arguments(self, _):
         c.argument('enable_client_certificate', arg_type=get_three_state_flag(), help='Enforces a client certificate to be presented on each request to the gateway and also enables the ability to authenticate the certificate in the policy on the gateway.')
         c.argument('virtual_network_type', get_enum_type(VNET_TYPES), options_list=['--virtual-network', '-vnet'], help='The virtual network type.')
         c.argument('sku_name', arg_type=get_enum_type(SKU_TYPES), help='The sku of the api management instance')
-        c.argument('sku_capacity', type=int, validator=validate_capacity, help='The number of deployed units of the SKU.')
+        c.argument('sku_capacity', type=int, help='The number of deployed units of the SKU.')
         c.argument('enable_managed_identity', arg_type=get_three_state_flag(), help='Create a managed identity for the API Management service to access other Azure resources.')
 
     with self.argument_context('apim update') as c:
@@ -44,5 +43,5 @@ def load_arguments(self, _):
         c.argument('enable_client_certificate', arg_type=get_three_state_flag(), help='Enforces a client certificate to be presented on each request to the gateway and also enables the ability to authenticate the certificate in the policy on the gateway.')
         c.argument('virtual_network_type', get_enum_type(VNET_TYPES), options_list=['--virtual-network', '-vnet'], help='The virtual network type.')
         c.argument('sku_name', arg_type=get_enum_type(SKU_TYPES), help='The sku of the api management instance')
-        c.argument('sku_capacity', type=int, validator=validate_capacity, help='The number of deployed units of the SKU.')
+        c.argument('sku_capacity', type=int, help='The number of deployed units of the SKU.')
         c.argument('enable_managed_identity', arg_type=get_three_state_flag(), help='Create a managed identity for the API Management service to access other Azure resources.')
