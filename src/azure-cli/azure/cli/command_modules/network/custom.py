122eb30011e9b8faa93d1d0320a736db83ecf8dd
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2019-07-26 22:26:39 +0800

    Network: Correct the error messages and help message for the `--vm` and `--nic` arguments for watcher test-ip-flow command (#10047)
    * Fix watcher test-ip-flow issues
    
    * fix comments
    
    * fix
    
    * sync the usage error, fix the --resource-name to --resource-group

--- src/azure-cli/azure/cli/command_modules/network/custom.py
+++ src/azure-cli/azure/cli/command_modules/network/custom.py
@@ -3023,14 +3023,24 @@ def check_nw_ip_flow(cmd, client, vm, watcher_rg, watcher_name, direction, proto
                      resource_group_name=None, nic=None, location=None):
     VerificationIPFlowParameters = cmd.get_models('VerificationIPFlowParameters')
 
-    local_ip_address, local_port = local.split(':')
-    remote_ip_address, remote_port = remote.split(':')
+    try:
+        local_ip_address, local_port = local.split(':')
+        remote_ip_address, remote_port = remote.split(':')
+    except:
+        raise CLIError("usage error: the format of the '--local' and '--remote' should be like x.x.x.x:port")
+
     if not is_valid_resource_id(vm):
+        if not resource_group_name:
+            raise CLIError("usage error: --vm NAME --resource-group NAME | --vm ID")
+
         vm = resource_id(
             subscription=get_subscription_id(cmd.cli_ctx), resource_group=resource_group_name,
             namespace='Microsoft.Compute', type='virtualMachines', name=vm)
 
     if nic and not is_valid_resource_id(nic):
+        if not resource_group_name:
+            raise CLIError("usage error: --nic NAME --resource-group NAME | --nic ID")
+
         nic = resource_id(
             subscription=get_subscription_id(cmd.cli_ctx), resource_group=resource_group_name,
             namespace='Microsoft.Network', type='networkInterfaces', name=nic)
