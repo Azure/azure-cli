7db97288058f86928cd9a5d84432100aa7525b09
FanQiu <fanqiu@microsoft.com>
2019-08-06 14:33:38 +0800

    correct the name of parameter

--- src/azure-cli/azure/cli/command_modules/network/_params.py
+++ src/azure-cli/azure/cli/command_modules/network/_params.py
@@ -618,7 +618,7 @@ def load_arguments(self, _):
         c.argument('private_connection_resource_id', help='The resource id of which private enpoint connect to')
         c.argument('group_ids', nargs='+', help='The ID(s) of the group(s) obtained from the remote resource that this private endpoint should connect to. You can use "az network private-resource show to obtain the list of group ids."')
         c.argument('request_message', help='A message passed to the owner of the remote resource with this connection request. Restricted to 140 chars.')
-        c.argument('munual_request', help='Use manual request to establish the connection', arg_type=get_three_state_flag())
+        c.argument('manual_request', help='Use manual request to establish the connection', arg_type=get_three_state_flag())
         c.ignore('expand')
     # endregion
 
