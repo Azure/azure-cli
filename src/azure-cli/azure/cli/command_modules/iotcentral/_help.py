b0aefc404374f33f15b18304c7afc388689fefe3
Eduardo Noriega de Armas <ednorieg@microsoft.com>
2019-08-16 14:15:25 -0700

    Sort commands in acr, acs, advisor, ams, appconfig, appservice, backup, batch, batchai, billing, botservice, cdn, cloud, cognitiveservices, configure, consumption, container, cosmosdb, deploymentmanager, dla, dms, eventgrid, eventhubs, extension, feedback, find, hdinsight, iot, iotcentral, kusto, lab, managedservices, maps, monitor, natgateway, netappfiles, policyinsights, privatedns, profile, rdbms, redis, relay, reservations, role, search, security, servicebus, servicefabric, signalr, sql, sqlvm, vm _help.py 	- Sort commands in help files for easier diffs in the future.

--- src/azure-cli/azure/cli/command_modules/iotcentral/_help.py
+++ src/azure-cli/azure/cli/command_modules/iotcentral/_help.py
@@ -4,7 +4,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from knack.help_files import helps
+from knack.help_files import helps  # pylint: disable=unused-import
 # pylint: disable=line-too-long, too-many-lines
 
 helps['iotcentral'] = """
