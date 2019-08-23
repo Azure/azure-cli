1693070c4eebcf73dab2c1907e6100144a1f5e47
Xin Shi <xinshi@microsoft.com>
2019-08-23 15:10:41 -0700

    Use the new endpoint

--- src/azure-cli/azure/cli/command_modules/find/custom.py
+++ src/azure-cli/azure/cli/command_modules/find/custom.py
@@ -23,42 +23,38 @@ WAIT_MESSAGE = ['Finding examples...']
 
 EXTENSION_NAME = 'find'
 
-
 def process_query(cli_term):
     print(random.choice(WAIT_MESSAGE), file=sys.stderr)
     response = call_aladdin_service(cli_term)
 
     if response.status_code != 200:
-        logger.error('[?] Unexpected Error: [HTTP %s]: Content: %s', response.status_code, response.content)
+        logger.error('[?] Unexpected Error: [HTTP %s]: Content: %s',
+                     response.status_code, response.content)
     else:
         if (platform.system() == 'Windows' and should_enable_styling()):
             colorama.init(convert=True)
+        hasPrunedAnswer = False
+        answer_list = json.loads(response.content)
+        if answer_list[0]['source'] == 'pruned':
+            hasPrunedAnswer = True
+            answer_list.pop(0)
+        print("\nHere are the most common ways to use [" + cli_term + "]: \n", file=sys.stderr)
 
-        answer_list = json.loads(response.content.decode(response.encoding))
-        if (not answer_list or answer_list[0]['source'] == 'bing'):
-            print("\nSorry I am not able to help with [" + cli_term + "]."
-                  "\nTry typing the beginning of a command e.g. " + style_message('az vm') + ".", file=sys.stderr)
-        else:
-            if answer_list[0]['source'] == 'pruned':
-                print("\nMore commands and examples are available in the latest version of the CLI,"
-                      "please update for the best experience.")
-                answer_list.pop(0)
-            print("\nHere are the most common ways to use [" + cli_term + "]: \n", file=sys.stderr)
-            num_results_to_show = min(3, len(answer_list))
-            for i in range(num_results_to_show):
-                current_title = answer_list[i]['title'].strip()
-                current_snippet = answer_list[i]['snippet'].strip()
-                if current_title.startswith("az "):
-                    current_title, current_snippet = current_snippet, current_title
-                    current_title = current_title.split('\r\n')[0]
-                elif '```azurecli\r\n' in current_snippet:
-                    start_index = current_snippet.index('```azurecli\r\n') + len('```azurecli\r\n')
-                    current_snippet = current_snippet[start_index:]
-                current_snippet = current_snippet.replace('```', '').replace(current_title, '').strip()
-                current_snippet = re.sub(r'\[.*\]', '', current_snippet).strip()
-                print(style_message(current_title))
-                print(current_snippet + '\n')
-
+        for answer in answer_list:
+            current_title = answer['title'].strip()
+            current_snippet = answer['snippet'].strip()
+            if current_title.startswith("az "):
+                current_title, current_snippet = current_snippet, current_title
+                current_title = current_title.split('\r\n')[0]
+            elif '```azurecli\r\n' in current_snippet:
+                start_index = current_snippet.index('```azurecli\r\n') + len('```azurecli\r\n')
+                current_snippet = current_snippet[start_index:]
+            current_snippet = current_snippet.replace('```', '').replace(current_title, '').strip()
+            current_snippet = re.sub(r'\[.*\]', '', current_snippet).strip()
+            print(style_message(current_title))
+            print(current_snippet + '\n') 
+        if hasPrunedAnswer:
+            print(style_message("More commands and examples are available in the latest version of the CLI. Please update for the best experience.\n"))
 
 def style_message(msg):
     if should_enable_styling():
@@ -67,7 +63,6 @@ def style_message(msg):
         except KeyError:
             pass
     return msg
-
 
 def should_enable_styling():
     try:
@@ -78,30 +73,27 @@ def should_enable_styling():
         pass
     return False
 
-
 def call_aladdin_service(query):
     client_request_id = ''
     if telemetry_core._session.application:  # pylint: disable=protected-access
         client_request_id = telemetry_core._session.application.data['headers']['x-ms-client-request-id']  # pylint: disable=protected-access
 
-    context = {
-        'session_id': telemetry_core._session._get_base_properties()['Reserved.SessionId'],  # pylint: disable=protected-access
-        'subscription_id': telemetry_core._get_azure_subscription_id(),  # pylint: disable=protected-access
-        'client_request_id': client_request_id,  # pylint: disable=protected-access
-        'installation_id': telemetry_core._get_installation_id(),  # pylint: disable=protected-access
-        'version_number': str(parse_version(core_version))
-    }
+    session_id = telemetry_core._session._get_base_properties()['Reserved.SessionId']  # pylint: disable=protected-access
+    subscription_id = telemetry_core._get_azure_subscription_id()  # pylint: disable=protected-access
+    client_request_id = client_request_id  # pylint: disable=protected-access
+    installation_id = telemetry_core._get_installation_id()  # pylint: disable=protected-access
+    version = str(parse_version(core_version))
 
-    service_input = {
-        'paragraphText': "<div id='dummyHeader'></div>",
-        'currentPageUrl': "",
-        'query': "ALADDIN-CLI:" + query,
-        'context': context
-    }
-
-    api_url = 'https://aladdinservice-prod.azurewebsites.net/api/aladdin/generateCards'
+    api_url = 'https://aladdin.microsoft.com/api/v1.0/examples'
     headers = {'Content-Type': 'application/json'}
 
-    response = requests.post(api_url, headers=headers, json=service_input)
+    response = requests.get(
+        api_url,
+        params={
+            'query': query,
+            'clientType': 'AzureCli',
+            'context': f'{{"sessionId": "{session_id}", "subscriptionId": "{subscription_id}", "clientRequestId": "{client_request_id}", "installationId": "{installation_id}", "versionNumber": "{version}"}}'
+        },
+        headers=headers)
 
     return response
