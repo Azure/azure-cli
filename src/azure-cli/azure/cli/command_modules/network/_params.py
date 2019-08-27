8cf0e96791e5648c06b78823beaf8e86a3c2e9f3
FanQiu <fanqiu@microsoft.com>
2019-08-27 13:51:55 +0800

    fix-8132

--- src/azure-cli/azure/cli/command_modules/network/_params.py
+++ src/azure-cli/azure/cli/command_modules/network/_params.py
@@ -946,7 +946,7 @@ def load_arguments(self, _):
         c.argument('log_version', help='Version (revision) of the flow log.', type=int)
 
     with self.argument_context('network watcher flow-log', arg_group='Traffic Analytics', min_api='2018-10-01') as c:
-        c.argument('traffic_analytics_interval', arg_type=ignore_type, options_list='--interval', help='Interval in minutes at which to conduct flow analytics.', min_api='2018-12-01')
+        c.argument('traffic_analytics_interval', type=int, options_list='--interval', help='Interval in minutes at which to conduct flow analytics. Temporarily only supports 10 and 60.', min_api='2018-12-01')
         c.argument('traffic_analytics_workspace', options_list='--workspace', help='Name or ID of a Log Analytics workspace.')
         c.argument('traffic_analytics_enabled', options_list='--traffic-analytics', arg_type=get_three_state_flag(), help='Enable traffic analytics. Defaults to true if `--workspace` is provided.')
 
