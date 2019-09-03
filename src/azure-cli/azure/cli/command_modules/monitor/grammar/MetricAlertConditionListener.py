e1f2ef7879504e62d6b6276081ed10c9639ebf26
FanQiu <fanqiu@microsoft.com>
2019-09-03 10:42:56 +0800

    fix

--- src/azure-cli/azure/cli/command_modules/monitor/grammar/MetricAlertConditionListener.py
+++ src/azure-cli/azure/cli/command_modules/monitor/grammar/MetricAlertConditionListener.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
- # pylint: disable=all
+# pylint: disable=all
 
 # Generated from MetricAlertCondition.g4 by ANTLR 4.7.2
 from antlr4 import *
--- src/azure-cli/azure/cli/command_modules/monitor/grammar/build_python.bat
+++ src/azure-cli/azure/cli/command_modules/monitor/grammar/build_python.bat
@@ -1,4 +1,3 @@
 echo off
 echo Building MetricAlertCondition
 call antlr -Dlanguage=Python2 MetricAlertCondition.g4
-pause
