5e828ce7b06b6049e134c9d61ede56ccca31c076
Nicholas King <nicking@microsoft.com>
2019-09-09 12:49:15 -0700

    Encapsulate docker image name parsing. Disable too-many-branches pylint check for create_webapp

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -75,18 +75,14 @@ logger = get_logger(__name__)
 # Please maintain compatibility in both interfaces and functionalities"
 
 
-def create_webapp(cmd, resource_group_name, name, plan, runtime=None, startup_file=None,  # pylint: disable=too-many-statements
+def create_webapp(cmd, resource_group_name, name, plan, runtime=None, startup_file=None,  # pylint: disable=too-many-statements,too-many-branches
                   deployment_container_image_name=None, deployment_source_url=None, deployment_source_branch='master',
                   deployment_local_git=None, docker_registry_server_password=None, docker_registry_server_user=None,
                   multicontainer_config_type=None, multicontainer_config_file=None, tags=None):
     if deployment_source_url and deployment_local_git:
         raise CLIError('usage error: --deployment-source-url <url> | --deployment-local-git')
 
-    if deployment_container_image_name:
-        slash_ix = deployment_container_image_name.rfind('/')
-        docker_registry_server_url = deployment_container_image_name[0:slash_ix]
-        if slash_ix == -1 or ("." not in docker_registry_server_url and ":" not in docker_registry_server_url):
-            docker_registry_server_url = None
+    docker_registry_server_url = parse_docker_image_name(deployment_container_image_name)
 
     client = web_client_factory(cmd.cli_ctx)
     if is_valid_resource_id(plan):
@@ -175,6 +171,16 @@ def validate_container_app_create_options(runtime=None, deployment_container_ima
         return False
     opts = [runtime, deployment_container_image_name, multicontainer_config_type]
     return len([x for x in opts if x]) == 1  # you can only specify one out the combinations
+
+
+def parse_docker_image_name(deployment_container_image_name):
+    if not deployment_container_image_name:
+        return None
+    slash_ix = deployment_container_image_name.rfind('/')
+    docker_registry_server_url = deployment_container_image_name[0:slash_ix]
+    if slash_ix == -1 or ("." not in docker_registry_server_url and ":" not in docker_registry_server_url):
+        return None
+    return docker_registry_server_url
 
 
 def update_app_settings(cmd, resource_group_name, name, settings=None, slot=None, slot_settings=None):
