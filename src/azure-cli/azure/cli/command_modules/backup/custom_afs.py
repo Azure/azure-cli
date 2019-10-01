7218b1bd245ab64615661499a5bb583b0df1a7a0
Sambit Rath <sarath@microsoft.com>
2019-10-01 11:25:23 +0530

    updated copyright header

--- src/azure-cli/azure/cli/command_modules/backup/custom_afs.py
+++ src/azure-cli/azure/cli/command_modules/backup/custom_afs.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 import azure.cli.command_modules.backup.custom_help as Helper
 import json
 # pylint: disable=import-error
