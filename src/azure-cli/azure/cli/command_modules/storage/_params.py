a01890d6a31708f2fcf2585426f05e89cc9280b8
Zunli Hu <zuh@microsoft.com>
2019-10-10 19:43:30 +0800

    remove preview flag

--- src/azure-cli/azure/cli/command_modules/storage/_params.py
+++ src/azure-cli/azure/cli/command_modules/storage/_params.py
@@ -67,7 +67,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
         validator=validate_storage_data_plane_list)
 
     large_file_share_type = CLIArgumentType(
-        action='store_true', min_api='2019-04-01', is_preview=True,
+        action='store_true', min_api='2019-04-01', 
         help='Enable the capability to support large file shares with more than 5 TiB capacity for storage account.'
              'Once the property is enabled, the feature cannot be disabled. Currently only supported for LRS and ZRS '
              'replication types, hence account conversions to geo-redundant accounts would not be possible. For more '
@@ -121,7 +121,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
         c.argument('sku', help='The storage account SKU.', arg_type=get_enum_type(t_sku_name, default='standard_ragrs'))
         c.argument('enable_files_aadds', arg_type=get_three_state_flag(), min_api='2018-11-01',
                    help='Enable the identity based authentication settings for Azure Files.')
-        c.argument('enable_large_file_share', large_file_share_type)
+        c.argument('enable_large_file_share', arg_type=large_file_share_type)
 
     with self.argument_context('storage account update', resource_type=ResourceType.MGMT_STORAGE) as c:
         c.register_common_storage_account_options()
@@ -134,7 +134,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
         c.argument('tags', tags_type, default=None)
         c.argument('enable_files_aadds', arg_type=get_three_state_flag(), min_api='2018-11-01',
                    help='Enable the identity based authentication settings for Azure Files.')
-        c.argument('enable_large_file_share', large_file_share_type)
+        c.argument('enable_large_file_share', arg_type=large_file_share_type)
 
     with self.argument_context('storage account update', arg_group='Customer managed key', min_api='2017-06-01') as c:
         c.extra('encryption_key_name', help='The name of the KeyVault key', )
