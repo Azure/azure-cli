b58945583e74eaa9f4b02825e6415e15ba7e66fa
Harold Jianhui <haroldrandom@gmail.com>
2019-10-15 14:53:40 +0800

    Fix a bug that would fail Azure Automation Test, Python 2 in CI

--- src/azure-cli/azure/cli/command_modules/network/tests/latest/test_network_commands.py
+++ src/azure-cli/azure/cli/command_modules/network/tests/latest/test_network_commands.py
@@ -304,7 +304,7 @@ class NetworkPublicIpPrefix(ScenarioTest):
         ])
 
         # Check with unsupported IP address version: IPv5
-        with self.assertRaisesRegex(SystemExit, '2'):
+        with self.assertRaisesRegexp(SystemExit, '2'):
             self.cmd('network public-ip prefix create -g {rg} -n {prefix_name_ipv6} --length 127 --version IPv5')
 
 
