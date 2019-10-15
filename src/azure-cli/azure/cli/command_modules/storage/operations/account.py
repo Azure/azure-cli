adfeec2549985859c654bcea4c47dd77f6df7f91
Zunli Hu <zuh@microsoft.com>
2019-10-15 10:48:33 +0800

    add blank line

--- src/azure-cli/azure/cli/command_modules/storage/operations/account.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/account.py
@@ -24,6 +24,7 @@ def create_storage_account(cmd, resource_group_name, account_name, sku=None, loc
     scf = storage_client_factory(cmd.cli_ctx)
     logger.warning("The default kind for created storage account will change to 'StorageV2' from 'Storage' in future")
     params = StorageAccountCreateParameters(sku=Sku(name=sku), kind=Kind(kind), location=location, tags=tags)
+    
     if custom_domain:
         params.custom_domain = CustomDomain(name=custom_domain, use_sub_domain=None)
     if encryption_services:
