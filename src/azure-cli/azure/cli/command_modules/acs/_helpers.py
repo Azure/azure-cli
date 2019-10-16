61bec06c06b73c3b84c77afa583f7d112e8f4ac9
GaneshaThirumurthi <ganesha.ashoka@gmail.com>
2019-10-16 12:13:23 -0700

    add lisence

--- src/azure-cli/azure/cli/command_modules/acs/_helpers.py
+++ src/azure-cli/azure/cli/command_modules/acs/_helpers.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 # pylint: disable=no-name-in-module,import-error
 from azure.mgmt.containerservice.v2019_08_01.models import ManagedClusterAPIServerAccessProfile
 
