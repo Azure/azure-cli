9ec3abe1a5d4bf923b87c67135e027011429945c
Marwan Ahmed <mrwnad@gmail.com>
2019-10-27 19:31:04 -0700

    [ACS] Add cluster autoscaler support for CLI (#10961)
    * Add cluster autoscaler flags to aks create
    
    * Add aks update flags for cluster autoscaler
    
    * Update history file
    
    * Incorporate az cli extension fixes
    
    * Change os_type comparison to case insensitive
    
    * Make use of upper case consistent in error messages

--- src/azure-cli/azure/cli/command_modules/acs/_params.py
+++ src/azure-cli/azure/cli/command_modules/acs/_params.py
@@ -18,8 +18,8 @@ from ._completers import (
     get_vm_size_completion_list, get_k8s_versions_completion_list, get_k8s_upgrades_completion_list)
 from ._validators import (
     validate_create_parameters, validate_k8s_client_version, validate_k8s_version, validate_linux_host_name,
-    validate_list_of_integers, validate_ssh_key, validate_connector_name, validate_max_pods, validate_nodepool_name,
-    validate_vm_set_type, validate_load_balancer_sku, validate_load_balancer_outbound_ips,
+    validate_list_of_integers, validate_ssh_key, validate_connector_name, validate_max_pods, validate_nodes_count,
+    validate_nodepool_name, validate_vm_set_type, validate_load_balancer_sku, validate_load_balancer_outbound_ips,
     validate_load_balancer_outbound_ip_prefixes, validate_taints, validate_ip_ranges, validate_acr)
 
 aci_connector_os_type = ['Windows', 'Linux', 'Both']
@@ -175,6 +175,9 @@ def load_arguments(self, _):
         c.argument('load_balancer_managed_outbound_ip_count', type=int)
         c.argument('load_balancer_outbound_ips', type=str, validator=validate_load_balancer_outbound_ips)
         c.argument('load_balancer_outbound_ip_prefixes', type=str, validator=validate_load_balancer_outbound_ip_prefixes)
+        c.argument('enable_cluster_autoscaler', action='store_true')
+        c.argument('min_count', type=int, validator=validate_nodes_count)
+        c.argument('max_count', type=int, validator=validate_nodes_count)
         c.argument('vm_set_type', type=str, validator=validate_vm_set_type)
         c.argument('zones', zones_type, options_list=['--zones', '-z'], help='Space-separated list of availability zones where agent nodes will be placed.')
         c.argument('enable_addons', options_list=['--enable-addons', '-a'])
@@ -198,6 +201,11 @@ def load_arguments(self, _):
         c.argument('detach_acr', acr_arg_type, validator=validate_acr)
 
     with self.argument_context('aks update') as c:
+        c.argument('enable_cluster_autoscaler', options_list=["--enable-cluster-autoscaler", "-e"], action='store_true')
+        c.argument('disable_cluster_autoscaler', options_list=["--disable-cluster-autoscaler", "-d"], action='store_true')
+        c.argument('update_cluster_autoscaler', options_list=["--update-cluster-autoscaler", "-u"], action='store_true')
+        c.argument('min_count', type=int, validator=validate_nodes_count)
+        c.argument('max_count', type=int, validator=validate_nodes_count)
         c.argument('load_balancer_managed_outbound_ip_count', type=int)
         c.argument('load_balancer_outbound_ips', type=str, validator=validate_load_balancer_outbound_ips)
         c.argument('load_balancer_outbound_ip_prefixes', type=str, validator=validate_load_balancer_outbound_ip_prefixes)
