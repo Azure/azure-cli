47296af50ad65ee4c42f3db5f273206b3fed0718
Matthew Booe <mabooe@microsoft.com>
2019-10-28 11:37:49 -0700

    Another style update

--- src/azure-cli/azure/cli/utils.py
+++ src/azure-cli/azure/cli/utils.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
 def get_default_cli():
     from azure.cli.core.azlogging import AzCliLogging
     from azure.cli.core.commands import AzCliCommandInvoker
