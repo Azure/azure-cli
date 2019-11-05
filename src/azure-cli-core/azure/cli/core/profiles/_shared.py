a5efebe5c292fedf6830b6268e7f9b9cfcdca853
Feiyue Yu <fey@microsoft.com>
2019-11-05 10:52:35 +0800

    Update all components to 20190701

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -126,9 +126,9 @@ AZURE_API_PROFILES = {
         ResourceType.MGMT_STORAGE: '2019-04-01',
         ResourceType.MGMT_NETWORK: '2019-09-01',
         ResourceType.MGMT_COMPUTE: SDKProfile('2019-07-01', {
-            'resource_skus': '2019-04-01',
-            'disks': '2019-03-01',
-            'snapshots': '2019-03-01',
+            'resource_skus': '2019-07-01',
+            'disks': '2019-07-01',
+            'snapshots': '2019-07-01',
             'galleries': '2019-07-01'
         }),
         ResourceType.MGMT_RESOURCE_FEATURES: '2015-12-01',
