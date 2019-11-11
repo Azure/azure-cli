868d773d6745241590ef08b49e42f0f993d0bbd3
MyronFanQiu <fanqiu@microsoft.com>
2019-11-11 16:02:03 +0800

    fix style

--- src/azure-cli-core/azure/cli/core/commands/template_create.py
+++ src/azure-cli-core/azure/cli/core/commands/template_create.py
@@ -43,8 +43,9 @@ def get_folded_parameter_help_string(
     # add parent name option string (if applicable)
     if other_required_option:
         help_text = '{} If name specified, also specify {}.'.format(help_text, other_required_option)
-        help_text = '{} If you want to use an existing {} in other resource group or subscription, ' \
-                    'please provide the ID instead of the name of the {}'.format(help_text, display_name, display_name)
+        help_text = '{} If you want to use an existing {display_name} in other resource group or subscription, ' \
+                    'please provide the ID instead of the name of the {display_name}'.format(help_text,
+                                                                                             display_name=display_name)
     return help_text
 
 
