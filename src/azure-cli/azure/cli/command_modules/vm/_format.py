ca17b9f54a8e9b4a5458a79a031e77fff2b529ca
MyronFanQiu <fanqiu@microsoft.com>
2019-11-15 14:23:43 +0800

    in progress 1/2

--- src/azure-cli/azure/cli/command_modules/vm/_format.py
+++ src/azure-cli/azure/cli/command_modules/vm/_format.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+from collections import OrderedDict
 
 
 def transform_ip_addresses(result):
@@ -123,3 +124,30 @@ def transform_vm_encryption_show_table_output(result):
         return OrderedDict([("status", status_dict.get("displayStatus", "N/A")),
                             ("message", status_dict.get("message", "N/A"))])
     return result
+
+
+def transform_log_analytics_query_output(result):
+    tables_output = []
+
+    def _transform_query_output(table):
+        columns = table.columns
+        name = table.name
+        rows = table.rows
+
+        column_names = []
+        table_output = []
+        for column in columns:
+            column_names.append(column.name)
+        for row in rows:
+            item = OrderedDict()
+            item['TableName'] = name
+            for index, value in enumerate(row):
+                item[column_names[index]] = str(value)
+            table_output.append(item)
+        return table_output
+
+    for table in result.tables:
+        table_output = _transform_query_output(table)
+        tables_output.extend(table_output)
+
+    return tables_output
