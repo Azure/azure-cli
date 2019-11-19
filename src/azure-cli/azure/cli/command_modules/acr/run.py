67647bb6382396dff0bca6cbda2fdb0fe71c9d6e
Eliana Liberman <elliberm@microsoft.com>
2019-11-19 18:57:29 -0300

    Clean code

--- src/azure-cli/azure/cli/command_modules/acr/run.py
+++ src/azure-cli/azure/cli/command_modules/acr/run.py
@@ -114,4 +114,3 @@ def acr_run(cmd,  # pylint: disable=too-many-locals
         return get_run_with_polling(cmd, client, run_id, registry_name, resource_group_name)
 
     return stream_logs(client, run_id, registry_name, resource_group_name, no_format, True)
-    
