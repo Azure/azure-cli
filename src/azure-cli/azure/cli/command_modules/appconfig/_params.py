8428cb3c3e5f0e26f37b5bed11684ac7a1500183
Shuai Wang <shuawan@microsoft.com>
2019-11-22 01:36:40 +0000

    Apply new management plane sdk version of App Configuration (#11036)
    * fix import separator bug
    
    * fix null label bug
    
    * run test again
    
    * fix help messages and examples
    
    * revise example
    
    * use back slash instead of slash
    
    * add error handling for key
    
    * add sku
    
    * update mgm pacakge to 0.3.0
    
    * add test for sku
    
    * resolve conflicts
    
    * update tests record
    
    * update pip package requirements
    
    * fix stylying
    
    * add history
    
    * fix history file order
    
    * add default value for sku

--- src/azure-cli/azure/cli/command_modules/appconfig/_params.py
+++ src/azure-cli/azure/cli/command_modules/appconfig/_params.py
@@ -65,6 +65,7 @@ def load_arguments(self, _):
 
     with self.argument_context('appconfig create') as c:
         c.argument('location', options_list=['--location', '-l'], arg_type=get_location_type(self.cli_ctx), validator=get_default_location_from_resource_group)
+        c.ignore('sku')
 
     with self.argument_context('appconfig update') as c:
         c.argument('tags', arg_type=tags_type)
@@ -82,7 +83,8 @@ def load_arguments(self, _):
         c.argument('path', help='Local configuration file path. Required for file arguments.')
         c.argument('format_', options_list=['--format'], choices=['json', 'yaml', 'properties'], help='Imported file format. Required for file arguments.')
         c.argument('depth', validator=validate_import_depth, help="Depth for flatterning the json or yaml file to key-value paris. Flattern to the deepest level by default.")
-        c.argument('separator', arg_type=get_enum_type(['.', ',', ';', '-', '_', '__', '/']), help="Delimiter for flatterning the json or yaml file to key-value pairs. Required for importing hierarchical structure. Not applicable for property files.")
+        # bypass cli allowed values limition
+        c.argument('separator', validator=validate_separator, help="Delimiter for flatterning the json or yaml file to key-value pairs. Required for importing hierarchical structure. Not applicable for property files. Supported values: '.', ',', ';', '-', '_', '__', '/', ':' ")
 
     with self.argument_context('appconfig kv import', arg_group='AppConfig') as c:
         c.argument('src_name', help='The name of the source App Configuration.')
@@ -104,7 +106,8 @@ def load_arguments(self, _):
         c.argument('path', help='Local configuration file path. Required for file arguments.')
         c.argument('format_', options_list=['--format'], choices=['json', 'yaml', 'properties'], help='File format exporting to. Required for file arguments.')
         c.argument('depth', validator=validate_import_depth, help="Depth for flatterning the json or yaml file to key-value paris. Flattern to the deepest level by default.")
-        c.argument('separator', validator=validate_separator, help="Delimiter for flatterning the json or yaml file to key-value pairs. Required for importing hierarchical structure. Not applicable for property files. Supported values: '.', ',', ';', '-', '_', '__', '/', ':', '' ")
+        # bypass cli allowed values limition
+        c.argument('separator', validator=validate_separator, help="Delimiter for flatterning the json or yaml file to key-value pairs. Required for importing hierarchical structure. Not applicable for property files. Supported values: '.', ',', ';', '-', '_', '__', '/', ':' ")
 
     with self.argument_context('appconfig kv export', arg_group='AppConfig') as c:
         c.argument('dest_name', help='The name of the destination App Configuration.')
@@ -127,11 +130,11 @@ def load_arguments(self, _):
 
     with self.argument_context('appconfig kv show') as c:
         c.argument('key', help='Key to be showed.')
-        c.argument('label', help="If no label specified, show entry with null label. Does NOT support filters like other commands")
+        c.argument('label', help="If no label specified, show entry with null label. Filtering is not supported.")
 
     with self.argument_context('appconfig kv list') as c:
         c.argument('key', help='If no key specified, return all keys by default. Support star sign as filters, for instance abc* means keys with abc as prefix. Similarly, *abc and *abc* are also supported.')
-        c.argument('label', help="If no label specified, list all labels. Support star sign as filters, for instance abc* means labels with abc as prefix. Similarly, *abc and *abc* are also supported.")
+        c.argument('label', help="If no label specified, list all labels. Support star sign as filters, for instance abc* means labels with abc as prefix. Similarly, *abc and *abc* are also supported. Use '\\0' for null label.")
 
     with self.argument_context('appconfig kv restore') as c:
         c.argument('key', help='If no key specified, restore all keys by default. Support star sign as filters, for instance abc* means keys with abc as prefix. Similarly, *abc and *abc* are also supported.')
@@ -139,19 +142,19 @@ def load_arguments(self, _):
 
     with self.argument_context('appconfig kv lock') as c:
         c.argument('key', help='Key to be locked.')
-        c.argument('label', help="If no label specified, lock entry with null label. Does NOT support filters like other commands")
+        c.argument('label', help="If no label specified, lock entry with null label. Filtering is not supported.")
 
     with self.argument_context('appconfig kv unlock') as c:
         c.argument('key', help='Key to be unlocked.')
-        c.argument('label', help="If no label specified, unlock entry with null label. Does NOT support filters like other commands")
+        c.argument('label', help="If no label specified, unlock entry with null label. Filtering is not supported.")
 
     with self.argument_context('appconfig revision list') as c:
         c.argument('key', help='If no key specified, return all keys by default. Support star sign as filters, for instance abc* means keys with abc as prefix. Similarly, *abc and *abc* are also supported.')
-        c.argument('label', help="If no label specified, list all labels. Support star sign as filters, for instance abc* means labels with abc as prefix. Similarly, *abc and *abc* are also supported.")
+        c.argument('label', help="If no label specified, list all labels. Support star sign as filters, for instance abc* means labels with abc as prefix. Similarly, *abc and *abc* are also supported. Use '\\0' for null label.")
 
     with self.argument_context('appconfig feature show') as c:
         c.argument('feature', help='Name of the feature flag to be retrieved')
-        c.argument('label', help="If no label specified, show entry with null label. Does NOT support filters like other commands.")
+        c.argument('label', help="If no label specified, show entry with null label. Filtering is not supported.")
         c.argument('fields', arg_type=feature_fields_arg_type)
 
     with self.argument_context('appconfig feature set') as c:
@@ -165,7 +168,7 @@ def load_arguments(self, _):
 
     with self.argument_context('appconfig feature list') as c:
         c.argument('feature', help='Key of the feature to be listed. Support star sign as filters, for instance * means all key and abc* means keys with abc as prefix. Similarly, *abc and *abc* are also supported. Comma separated keys are not supported. Please provide escaped string if your feature name contains comma.')
-        c.argument('label', help="If no label specified, list all labels. Support star sign as filters, for instance * means all labels and abc* means labels with abc as prefix. Similarly, *abc and *abc* are also supported.")
+        c.argument('label', help="If no label specified, list all labels. Support star sign as filters, for instance * means all labels and abc* means labels with abc as prefix. Similarly, *abc and *abc* are also supported. Use '\\0' for null label.")
         c.argument('fields', arg_type=feature_fields_arg_type)
         c.argument('all_', help="List all feature flags.")
 
--- src/azure-cli/azure/cli/command_modules/appconfig/keyvalue.py
+++ src/azure-cli/azure/cli/command_modules/appconfig/keyvalue.py
@@ -10,10 +10,10 @@ import json
 import sys
 import time
 
+from itertools import chain
 import chardet
 import javaproperties
 import yaml
-from itertools import chain
 from jsondiff import JsonDiffer
 from knack.log import get_logger
 from knack.util import CLIError
@@ -148,9 +148,13 @@ def set_key(cmd,
 
     retry_times = 3
     retry_interval = 1
+
+    label = label if label and label != ModifyKeyValueOptions.empty_label else None
     for i in range(0, retry_times):
-        retrieved_kv = azconfig_client.get_keyvalue(
-            key, QueryKeyValueOptions(label))
+        try:
+            retrieved_kv = azconfig_client.get_keyvalue(key, QueryKeyValueOptions(label))
+        except HTTPException as exception:
+            raise CLIError(str(exception))
 
         if retrieved_kv is None:
             set_kv = KeyValue(key, value, label, tags, content_type)
@@ -161,6 +165,7 @@ def set_key(cmd,
                               content_type=retrieved_kv.content_type if content_type is None else content_type,
                               tags=retrieved_kv.tags if tags is None else tags)
             set_kv.etag = retrieved_kv.etag
+
         verification_kv = {
             "key": set_kv.key,
             "label": set_kv.label,
@@ -243,21 +248,14 @@ def lock_key(cmd, key, label=None, name=None, connection_string=None, yes=False)
     retry_times = 3
     retry_interval = 1
     for i in range(0, retry_times):
-        retrieved_kv = azconfig_client.get_keyvalue(
-            key, QueryKeyValueOptions(label))
+        try:
+            retrieved_kv = azconfig_client.get_keyvalue(key, QueryKeyValueOptions(label))
+        except HTTPException as exception:
+            raise CLIError(exception)
         if retrieved_kv is None:
             raise CLIError("The key you are trying to lock does not exist.")
 
-        confirmation_entry = {
-            "key": retrieved_kv.key,
-            "label": retrieved_kv.label,
-            "content_type": retrieved_kv.content_type,
-            "value": retrieved_kv.value,
-            "tags": retrieved_kv.tags
-        }
-
-        entry = json.dumps(confirmation_entry, indent=2, sort_keys=True)
-        confirmation_message = "Are you sure you want to lock the key: \n" + entry + "\n"
+        confirmation_message = "Are you sure you want to lock the key '{}'".format(key)
         user_confirmation(confirmation_message, yes)
 
         try:
@@ -283,20 +281,14 @@ def unlock_key(cmd, key, label=None, name=None, connection_string=None, yes=Fals
     retry_times = 3
     retry_interval = 1
     for i in range(0, retry_times):
-        retrieved_kv = azconfig_client.get_keyvalue(
-            key, QueryKeyValueOptions(label))
+        try:
+            retrieved_kv = azconfig_client.get_keyvalue(key, QueryKeyValueOptions(label))
+        except HTTPException as exception:
+            raise CLIError(exception)
         if retrieved_kv is None:
             raise CLIError("The key you are trying to unlock does not exist.")
 
-        confirmation_entry = {
-            "key": retrieved_kv.key,
-            "label": retrieved_kv.label,
-            "content_type": retrieved_kv.content_type,
-            "value": retrieved_kv.value,
-            "tags": retrieved_kv.tags
-        }
-        entry = json.dumps(confirmation_entry, indent=2, sort_keys=True)
-        confirmation_message = "Are you sure you want to unlock the key: \n" + entry + "\n"
+        confirmation_message = "Are you sure you want to unlock the key '{}'".format(key)
         user_confirmation(confirmation_message, yes)
 
         try:
