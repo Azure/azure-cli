84da0509db15f3f6effc2060189dd2b10978f3fb
Feng Zhou <fenzhou@microsoft.com>
2019-11-29 11:40:15 +0000

    more updates for removing python 3.4

--- tools/setup.py
+++ tools/setup.py
@@ -15,9 +15,10 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'License :: OSI Approved :: MIT License',
 ]
 
