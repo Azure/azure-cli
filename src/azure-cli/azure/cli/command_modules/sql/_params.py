c3314eccd978be6404ddcc0b879cd97ef2da3c76
Brad Richards <bradrich@microsoft.com>
2019-12-02 10:46:57 -0800

    Changed read-scale param to three state flag

--- src/azure-cli/azure/cli/command_modules/sql/_params.py
+++ src/azure-cli/azure/cli/command_modules/sql/_params.py
@@ -230,7 +230,7 @@ read_scale_param_type = CLIArgumentType(
     help='If enabled, connections that have application intent set to readonly '
     'in their connection string may be routed to a readonly secondary replica. '
     'This property is only settable for Premium and Business Critical databases.',
-    arg_type=get_enum_type(DatabaseReadScale))
+    arg_type=get_three_state_flag('Enabled','Disabled', False, True))
 
 read_replicas_param_type = CLIArgumentType(
     options_list=['--read-replicas'],
