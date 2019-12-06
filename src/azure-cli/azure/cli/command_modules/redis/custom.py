f37be0481ecf7880636da91983e2028bbea94a87
Meghraj Chandra kanth <mechandr@microsoft.com>
2019-12-06 15:54:52 -0800

    Add preview param --replicas-per-master to "az redis create" command. Update azure-mgmt-redis from 6.0.0 to 7.0.0rc1.

--- src/azure-cli/azure/cli/command_modules/redis/custom.py
+++ src/azure-cli/azure/cli/command_modules/redis/custom.py
@@ -61,7 +61,8 @@ def cli_redis_update(cmd, instance, sku=None, vm_size=None):
 def cli_redis_create(cmd, client,
                      resource_group_name, name, location, sku, vm_size, tags=None,
                      redis_configuration=None, enable_non_ssl_port=None, tenant_settings=None,
-                     shard_count=None, minimum_tls_version=None, subnet_id=None, static_ip=None, zones=None):
+                     shard_count=None, minimum_tls_version=None, subnet_id=None, static_ip=None,
+                     zones=None, replicas_per_master=None):
     # pylint:disable=line-too-long
     if ((sku.lower() in ['standard', 'basic'] and vm_size.lower() not in allowed_c_family_sizes) or (sku.lower() in ['premium'] and vm_size.lower() not in allowed_p_family_sizes)):
         raise wrong_vmsize_error
@@ -76,6 +77,7 @@ def cli_redis_create(cmd, client,
         location=location,
         redis_configuration=redis_configuration,
         enable_non_ssl_port=enable_non_ssl_port,
+        replicas_per_master=replicas_per_master,
         tenant_settings=tenant_settings_in_json,
         shard_count=shard_count,
         minimum_tls_version=minimum_tls_version,
