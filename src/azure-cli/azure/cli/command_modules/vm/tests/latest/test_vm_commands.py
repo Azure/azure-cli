e97248484fed8eab15b09d0e19299c8913845551
MyronFanQiu <fanqiu@microsoft.com>
2019-12-20 10:02:17 +0800

    fix test

--- src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_commands.py
@@ -1234,8 +1234,8 @@ class VMMonitorTest(ScenarioTest):
         time.sleep(60)
 
         self.cmd('vm monitor metrics tail -n {vm} -g {rg} --metrics "Percentage CPU"', checks=[
-            self.check('type', 'Microsoft.Insights/metrics'),
-            self.check('name.value', 'Percentage CPU')
+            self.check('value[0].type', 'Microsoft.Insights/metrics'),
+            self.check('value[0].name.value', 'Percentage CPU')
         ])
         self.cmd('vm monitor metrics list-definitions -n {vm} -g {rg}', checks=[
             self.check("length(@[*].id) != '0'", True)
