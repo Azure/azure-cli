bd4c62ce297d34d86875f662057269da2409bb8b
Isaac Lee <Lee.Isaac@microsoft.com>
2020-01-09 11:13:52 -0800

    Use logger.warning and specify .exe for Windows

--- src/azure-cli/azure/cli/command_modules/acr/check_health.py
+++ src/azure-cli/azure/cli/command_modules/acr/check_health.py
@@ -160,7 +160,7 @@ def _get_helm_version(ignore_errors):
     if match_obj:
         output = match_obj.group(2)
 
-    print("Helm version: {}".format(output), file=sys.stderr)
+    logger.warning("Helm version: {}".format(output))
 
     # Display an error message if the current helm version < min required version
     if match_obj and LooseVersion(output) < LooseVersion(MIN_HELM_VERSION):
@@ -197,7 +197,7 @@ def _get_notary_version(ignore_errors):
     if match_obj:
         output = match_obj.group(1)
 
-    print("Notary version: {}".format(output), file=sys.stderr)
+    logger.warning("Notary version: {}".format(output))
 
     # Display error if the current version does not match the recommended version
     if match_obj and LooseVersion(output) != LooseVersion(RECOMMENDED_NOTARY_VERSION):
--- src/azure-cli/azure/cli/command_modules/acr/notary.py
+++ src/azure-cli/azure/cli/command_modules/acr/notary.py
@@ -11,24 +11,22 @@ logger = get_logger(__name__)
 
 def get_notary_command(is_diagnostics_context=False):
     from ._errors import NOTARY_COMMAND_ERROR
-    notary_command = "notary"
-
     from subprocess import PIPE, Popen
+    from platform import system
+
+    if system() == "Windows":
+        notary_command = "notary.exe"
+    else:
+        notary_command = "notary"
+
     try:
         p = Popen([notary_command, "--help"], stdout=PIPE, stderr=PIPE)
         _, stderr = p.communicate()
     except OSError as e:
         logger.debug("Could not run '%s' command. Exception: %s", notary_command, str(e))
-        # The executable may not be discoverable in WSL so retry *.exe once
-        try:
-            notary_command = 'notary.exe'
-            p = Popen([notary_command, "--help"], stdout=PIPE, stderr=PIPE)
-            _, stderr = p.communicate()
-        except OSError as inner:
-            logger.debug("Could not run '%s' command. Exception: %s", notary_command, str(inner))
-            if is_diagnostics_context:
-                return None, NOTARY_COMMAND_ERROR
-            raise CLIError(NOTARY_COMMAND_ERROR.get_error_message())
+        if is_diagnostics_context:
+            return None, NOTARY_COMMAND_ERROR
+        raise CLIError(NOTARY_COMMAND_ERROR.get_error_message())
 
     if stderr:
         if is_diagnostics_context:
