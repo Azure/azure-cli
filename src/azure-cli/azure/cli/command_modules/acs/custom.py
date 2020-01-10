97959ac96dac97b170185946632ec0607c802b30
Andy Liu <andyliuliming@outlook.com>
2020-01-10 21:12:20 +0800

    fix

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -1544,7 +1544,7 @@ def aks_browse(cmd, client, resource_group_name, name, disable_browser=False,
     else:
         protocol = 'http'
 
-    proxy_url = 'http://{1}:{2}/'.format(listen_address, listen_port)
+    proxy_url = 'http://{0}:{1}/'.format(listen_address, listen_port)
     dashboardURL = '{0}/api/v1/namespaces/kube-system/services/{1}:kubernetes-dashboard:/proxy'.format(proxy_url,
                                                                                                        protocol)
     # launch kubectl port-forward locally to access the remote dashboard
