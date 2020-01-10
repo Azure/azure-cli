1556d042d6b7f55c25567e6dfcc99743db2b0840
Zunli Hu <zuh@microsoft.com>
2020-01-10 11:40:49 +0800

    refine help

--- src/azure-cli/azure/cli/command_modules/storage/_params.py
+++ src/azure-cli/azure/cli/command_modules/storage/_params.py
@@ -160,14 +160,14 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                    "when storage account kind is StorageV2.",
                    min_api='2018-02-01')
         c.argument('encryption_key_type_for_table', arg_type=get_enum_type(['Account', 'Service']),
-                   help='Set the encryption key type for Table encryption service. "Account": Table will be encrypted '
+                   help='Set the encryption key type for Table service. "Account": Table will be encrypted '
                         'with account-scoped encryption key. "Service": Table will always be encrypted with '
-                        'Service-Managed keys. Currently the default encryption key type is "Service".',
+                        'service-scoped keys. Currently the default encryption key type is "Service".',
                    min_api='2019-06-01', options_list=['--encryption-key-type-for-table', '-t'])
         c.argument('encryption_key_type_for_queue', arg_type=get_enum_type(['Account', 'Service']),
-                   help='Set the encryption key type for Queue encryption service. "Account": Queue will be encrypted '
+                   help='Set the encryption key type for Queue service. "Account": Queue will be encrypted '
                         'with account-scoped encryption key. "Service": Queue will always be encrypted with '
-                        'Service-Managed keys. Currently the default encryption key type is "Service".',
+                        'service-scoped keys. Currently the default encryption key type is "Service".',
                    min_api='2019-06-01', options_list=['--encryption-key-type-for-queue', '-q'])
 
     with self.argument_context('storage account update', resource_type=ResourceType.MGMT_STORAGE) as c:
