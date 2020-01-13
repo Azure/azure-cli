0f4999fd490c09f0d023474a83e27710aaace1f4
Zunli Hu <zuh@microsoft.com>
2020-01-13 19:24:21 +0800

    pass more test

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -132,7 +132,7 @@ def create_webapp(cmd, resource_group_name, name, plan, runtime=None, startup_fi
         match = helper.resolve(runtime)
         if not match:
             raise CLIError("Runtime '{}' is not supported. Please invoke 'list-runtimes' to cross check".format(runtime))  # pylint: disable=line-too-long
-        match['setter'](match, site_config)
+        match['setter'](cmd, match, site_config)
         # Be consistent with portal: any windows webapp should have this even it doesn't have node in the stack
         if not match['displayName'].startswith('node'):
             site_config.app_settings.append(NameValuePair(name="WEBSITE_NODE_DEFAULT_VERSION",
--- src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_webapp_commands.py
+++ src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_webapp_commands.py
@@ -1067,7 +1067,7 @@ class WebappSSLImportCertTest(ScenarioTest):
         self.cmd('appservice plan create -g {} -n {} --sku B1'.format(resource_group, plan_name))
         self.cmd('webapp create -g {} -n {} --plan {}'.format(resource_group, webapp_name, plan_name))
         self.cmd('keyvault create -g {} -n {}'.format(resource_group, kv_name))
-        self.cmd('keyvault set-policy --name {} --spn {} --secret-permissions get'.format(kv_name, 'Microsoft.Azure.WebSites'))
+        self.cmd('keyvault set-policy -g {} --name {} --spn {} --secret-permissions get'.format(resource_group, kv_name, 'Microsoft.Azure.WebSites'))
         self.cmd('keyvault certificate import --name {} --vault-name {} --file "{}" --password {}'.format(cert_name, kv_name, pfx_file, cert_password))
 
         self.cmd('webapp config ssl import --resource-group {} --name {}  --key-vault {} --key-vault-certificate-name {}'.format(resource_group, webapp_name, kv_name, cert_name), checks=[
