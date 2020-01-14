a2e2f6844957f2e89c08a746f4b6f64f8e3f6ae1
Zunli Hu <zuh@microsoft.com>
2020-01-14 19:07:40 +0800

    fix typo

--- src/azure-cli/azure/cli/command_modules/resource/tests/latest/test_resource.py
+++ src/azure-cli/azure/cli/command_modules/resource/tests/latest/test_resource.py
@@ -28,7 +28,7 @@ class ResourceGroupScenarioTest(ScenarioTest):
             self.check('name', '{rg}'),
             self.check('tags', {'a': 'b', 'c': ''})
         ])
-        self.cmd('group exists -n {rgF}',
+        self.cmd('group exists -n {rg}',
                  checks=self.check('@', True))
         self.cmd('group show -n {rg}', checks=[
             self.check('name', '{rg}'),
