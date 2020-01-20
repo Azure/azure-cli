a873520b0992a9bd81cd579b0a2af77077a56ec8
Zunli Hu <zuh@microsoft.com>
2020-01-20 11:08:39 +0800

    fix typo

--- src/azure-cli/azure/cli/command_modules/storage/_help.py
+++ src/azure-cli/azure/cli/command_modules/storage/_help.py
@@ -1,8 +1,8 @@
 # coding=utf-8
-# --------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
-# --------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
 
 from knack.help_files import helps  # pylint: disable=unused-import
 # pylint: disable=line-too-long, too-many-lines
