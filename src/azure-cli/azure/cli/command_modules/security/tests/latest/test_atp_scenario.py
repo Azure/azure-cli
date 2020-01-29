e8166ed63c9d224198bee0e13c584801e386a2ab
Lital Bronshtein <libronsh@microsoft.com>
2020-01-29 11:28:20 +0200

    Add test file

---
+++ src/azure-cli/azure/cli/command_modules/security/tests/latest/test_atp_scenario.py
@@ -0,0 +1,45 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.testsdk import (ScenarioTest, ResourceGroupPreparer, StorageAccountPreparer, JMESPathCheck)
+
+
+class SecurityAtpSettingsTests(ScenarioTest):
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_security_atp_settings(self, resource_group, resource_group_location, storage_account):
+        # run show cli
+        self.cmd('security atp show --resource-group {} --storage-account-name {}'
+                 .format(resource_group, storage_account),
+                 checks=[
+                     JMESPathCheck('resourceGroup', resource_group)])
+
+        # enable atp
+        self.cmd('security atp update --resource-group {} --storage-account-name {} --is-enabled true'
+                 .format(resource_group, storage_account),
+                 checks=[
+                     JMESPathCheck('resourceGroup', resource_group),
+                     JMESPathCheck('isEnabled', True)])
+
+        # validate atp setting
+        self.cmd('security atp show --resource-group {} --storage-account-name {}'
+                 .format(resource_group, storage_account),
+                 checks=[
+                     JMESPathCheck('resourceGroup', resource_group),
+                     JMESPathCheck('isEnabled', True)])
+
+        # disable atp
+        self.cmd('security atp update --resource-group {} --storage-account-name {} --is-enabled false'
+                 .format(resource_group, storage_account),
+                 checks=[
+                     JMESPathCheck('resourceGroup', resource_group),
+                     JMESPathCheck('isEnabled', False)])
+
+        # validate atp setting
+        self.cmd('security atp show --resource-group {} --storage-account-name {}'
+                 .format(resource_group, storage_account),
+                 checks=[
+                     JMESPathCheck('resourceGroup', resource_group),
+                     JMESPathCheck('isEnabled', False)])
