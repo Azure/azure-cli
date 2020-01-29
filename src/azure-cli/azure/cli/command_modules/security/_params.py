8b88330b8a11706395790511db572320c7470aa4
libronsh <58980373+libronsh@users.noreply.github.com>
2020-01-29 11:28:19 +0200

    Update src/azure-cli/azure/cli/command_modules/security/_params.py
    Co-Authored-By: Guy Waldman <guywald1@gmail.com>

--- src/azure-cli/azure/cli/command_modules/security/_params.py
+++ src/azure-cli/azure/cli/command_modules/security/_params.py
@@ -80,8 +80,8 @@ def load_arguments(self, _):
 
     for scope in ['atp update']:
         with self.argument_context('security {}'.format(scope)) as c:
-            c.argument('storage_account_name', help='Name of an existing storage account.')
-            c.argument('is_enabled', help='Enable or disable atp for desired storage account. Possible values: {true, false}')
+            c.argument('storage_account_name', help='Name of an existing storage account to link to.')
+            c.argument('is_enabled', help='Enable or disable Advanced Threat Protection for a received storage account. Possible values: {true, false}')
 
     for scope in ['contact create']:
         with self.argument_context('security {}'.format(scope)) as c:
