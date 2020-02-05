bcc85ef15b350e7ca5914bb75ce449f71e2ae1bc
CHENYI ZHANG <czhang0727@gmail.com>
2020-02-05 02:55:33 +0000

    [ACS] aks create/update: add validation for --vnet-subnet-id argument (#12038)

--- src/azure-cli/azure/cli/command_modules/acs/_validators.py
+++ src/azure-cli/azure/cli/command_modules/acs/_validators.py
@@ -240,3 +240,12 @@ def validate_taints(namespace):
 def validate_acr(namespace):
     if namespace.attach_acr and namespace.detach_acr:
         raise CLIError('Cannot specify "--attach-acr" and "--detach-acr" at the same time.')
+
+
+def validate_vnet_subnet_id(namespace):
+    if namespace.vnet_subnet_id is not None:
+        if namespace.vnet_subnet_id == '':
+            return
+        from msrestazure.tools import is_valid_resource_id
+        if not is_valid_resource_id(namespace.vnet_subnet_id):
+            raise CLIError("--vnet-subnet-id is not a valid Azure resource ID.")
