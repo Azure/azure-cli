53f3a253299f55b9878ee277d2b5ed177fdb1d7a
Feng Zhou <fenzhou@microsoft.com>
2020-02-07 19:53:59 +0800

    add script to generate history notes

---
+++ scripts/release/generate_history_notes.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import datetime
+import json
+import re
+import requests
+
+
+base_url = 'https://api.github.com/repos/azure/azure-cli'
+release_head_url = '{}/git/ref/heads/release'.format(base_url)
+dev_commits_url = '{}/commits?sha=dev'.format(base_url)
+commit_pr_url = '{}/commits/commit_id/pulls'.format(base_url)
+
+days_before = 45
+history_notes = {}
+
+
+def generate_history_notes() -> str:
+    release_head_sha = get_release_head()
+    dev_commits = get_commits()
+    for commit in dev_commits:
+        if commit['sha'] == release_head_sha:
+            break
+        prs = get_prs_for_commit(commit['sha'])
+        # parse PR if one commit is mapped to one PR
+        if len(prs) == 1:
+            process_pr(prs[0])
+        else:
+            process_commit(commit)
+
+    history = ''
+    for component in sorted(history_notes, key=str.casefold):
+        history += '**{}**\n\n'.format(component)
+        for note in history_notes[component]:
+            history += '* {}\n'.format(note)
+        history += '\n'
+    return history
+
+def get_release_head() -> str:
+    response = requests.get(release_head_url)
+    if response.status_code != 200:
+        raise Exception("Request to {} failed with {}".format(release_head_url, response.status_code))
+
+    release_head_obj = json.loads(response.content.decode('utf-8'))
+    release_head_sha = release_head_obj['object']['sha']
+    return release_head_sha
+
+
+def get_commits():
+    today = datetime.date.today()
+    start_date = today - datetime.timedelta(days=days_before)
+    start_date_str = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+    dev_since_url = '{}&since={}'.format(dev_commits_url, start_date_str)
+    response = requests.get(dev_since_url)
+    if response.status_code != 200:
+        raise Exception("Request to {} failed with {}".format(dev_since_url, response.status_code))
+    dev_commits = json.loads(response.content.decode('utf-8'))
+    return dev_commits
+
+
+def get_prs_for_commit(commit: str):
+    headers = {'Accept': 'application/vnd.github.groot-preview+json'}
+    url = commit_pr_url.replace('commit_id', commit)
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception("Request to {} failed with {}".format(url, response.status_code))
+    prs = json.loads(response.content.decode('utf-8'))
+    return prs
+
+
+def process_pr(pr):
+    lines = [pr['title']]
+    lines.extend(pr['body'].splitlines())
+    process_lines(lines)
+
+
+def process_commit(commit):
+    lines = commit['commit']['message'].splitlines()
+    process_lines(lines)
+
+
+def process_lines(lines: [str]):
+    # do not put note of hotfix here since it's for last release
+    if re.search('hotfix', lines[0], re.IGNORECASE):
+        return
+    note_in_desc = False
+    for desc in lines[1:]:
+        component, note = parse_message(desc)
+        if component is not None:
+            note_in_desc = True
+            history_notes.setdefault(component, []).append(note)
+    # if description has no history notes, parse PR title/commit message
+    # otherwise should skip PR title/commit message
+    if not note_in_desc:
+        component, note = parse_message(lines[0])
+        if component is not None:
+            history_notes.setdefault(component, []).append(note)
+
+
+def parse_message(message: str) ->(str, str):
+    # do not include template
+    if message.startswith('[Component Name'):
+        return None, None
+    m = re.search(r'^\[(.*)\](.*)$', message)
+    if m is not None:
+        component = m.group(1)
+        note = m.group(2).strip()
+        #remove appended PR number in commit message
+        note = re.sub(r' \(#[0-9]+\)$', '', note)
+        note = re.sub('BREAKING CHANGE', '[BREAKING CHANGE]', note, flags=re.IGNORECASE)
+        return component, note
+    return None, None
+
+
+if __name__ == "__main__":
+    generate_history_notes()
