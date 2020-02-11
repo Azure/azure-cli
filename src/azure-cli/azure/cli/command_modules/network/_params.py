7ffae11da72f80c4383d9c8f1beb37b4d46ce34a
Jianhui Harold <haroldrandom@gmail.com>
2020-02-11 22:33:30 +0800

    Group V2 parameter set

--- src/azure-cli/azure/cli/command_modules/network/_params.py
+++ src/azure-cli/azure/cli/command_modules/network/_params.py
@@ -53,7 +53,9 @@ def load_arguments(self, _):
      IPVersion, LoadBalancerSkuName, LoadDistribution, ProbeProtocol, ProcessorArchitecture, Protocol, PublicIPAddressSkuName,
      RouteNextHopType, SecurityRuleAccess, SecurityRuleProtocol, SecurityRuleDirection, TransportProtocol,
      VirtualNetworkGatewaySkuName, VirtualNetworkGatewayType, VpnClientProtocol, VpnType, ZoneType,
-     ExpressRouteLinkMacSecCipher, ExpressRouteLinkAdminState) = self.get_models(
+     ExpressRouteLinkMacSecCipher, ExpressRouteLinkAdminState,
+     ConnectionMonitorEndpointFilterType, ConnectionMonitorTestConfigurationProtocol,
+     PreferredIPVersion, HTTPConfigurationMethod, OutputType) = self.get_models(
          'Access', 'ApplicationGatewayFirewallMode', 'ApplicationGatewayProtocol', 'ApplicationGatewayRedirectType',
          'ApplicationGatewayRequestRoutingRuleType', 'ApplicationGatewaySkuName', 'ApplicationGatewaySslProtocol', 'AuthenticationMethod',
          'Direction',
@@ -62,7 +64,9 @@ def load_arguments(self, _):
          'IPVersion', 'LoadBalancerSkuName', 'LoadDistribution', 'ProbeProtocol', 'ProcessorArchitecture', 'Protocol', 'PublicIPAddressSkuName',
          'RouteNextHopType', 'SecurityRuleAccess', 'SecurityRuleProtocol', 'SecurityRuleDirection', 'TransportProtocol',
          'VirtualNetworkGatewaySkuName', 'VirtualNetworkGatewayType', 'VpnClientProtocol', 'VpnType', 'ZoneType',
-         'ExpressRouteLinkMacSecCipher', 'ExpressRouteLinkAdminState')
+         'ExpressRouteLinkMacSecCipher', 'ExpressRouteLinkAdminState',
+         'ConnectionMonitorEndpointFilterType', 'ConnectionMonitorTestConfigurationProtocol',
+         'PreferredIPVersion', 'HTTPConfigurationMethod', 'OutputType')
 
     if self.supported_api_version(min_api='2018-02-01'):
         ExpressRoutePeeringType = self.get_models('ExpressRoutePeeringType')
@@ -1002,6 +1006,7 @@ def load_arguments(self, _):
         c.argument('network_watcher_name', arg_type=ignore_type, options_list=['--__NETWORK_WATCHER_NAME'])
         c.argument('connection_monitor_name', name_arg_type, help='Connection monitor name.')
 
+    # connection monitor V1 parameter set
     with self.argument_context('network watcher connection-monitor', arg_group='V1 Endpoint') as c:
         c.argument('source_resource', help='Name or ID of the resource from which to originate traffic. '
                                            'Currently only Virtual Machines are supported.')
@@ -1019,6 +1024,120 @@ def load_arguments(self, _):
         with self.argument_context('network watcher connection-monitor {}'.format(scope)) as c:
             c.extra('location', get_location_type(self.cli_ctx), required=True)
             c.argument('resource_group_name', arg_type=ignore_type, validator=nw_validator)
+
+    # connection monitor V2 parameter set
+    with self.argument_context('network watcher connection-monitor', min_api='2019-11-01', arg_group='V2') as c:
+        c.argument('notes', help='Optional notes to be associated with the connection monitor')
+
+    # Argument Group for endpoint to create a V2 connection monitor
+    with self.argument_context('network watcher connection-monitor',
+                               arg_group='V2 Endpoint',
+                               min_api='2019-11-01') as c:
+        c.argument('endpoint_dest_name',
+                   help='The name of the source of connection monitor endpoint. '
+                        'If you are creating a V2 Connection Monitor, it\'s required')
+        c.argument('endpoint_dest_resource_id',
+                   help='Resource ID of the source of connection monitor endpoint')
+        c.argument('endpoint_dest_address',
+                   help='Address of the source of connection monitor endpoint (IP or domain name)')
+        c.argument('endpoint_source_name',
+                   help='The name of the destination of connection monitor endpoint. '
+                        'If you are creating a V2 Connection Monitor, it\'s required')
+        c.argument('endpoint_source_resource_id',
+                   help='Resource ID of the destination of connection monitor endpoint. '
+                        'If endpoint is intended to used as source, this option is required.')
+        c.argument('endpoint_source_address',
+                   help='Address of the destination of connection monitor endpoint (IP or domain name)')
+
+    # Argument Group for test configuration to create a V2 connection monitor
+    with self.argument_context('network watcher connection-monitor',
+                               arg_group='V2 Test Configuration',
+                               min_api='2019-11-01') as c:
+        c.argument('test_config_name',
+                   help='The name of the connection monitor test configuration. '
+                        'If you are creating a V2 Connection Monitor, it\'s required')
+        c.argument('test_config_frequency',
+                   options_list='--frequency',
+                   help='The frequency of test evaluation, in seconds',
+                   type=int,
+                   default=60)
+        c.argument('test_config_protocol',
+                   options_list='--protocol',
+                   help='The protocol to use in test evaluation',
+                   arg_type=get_enum_type(ConnectionMonitorTestConfigurationProtocol))
+        c.argument('test_config_preferred_ip_version',
+                   options_list='--preferred-ip-version',
+                   help='The preferred IP version to use in test evaluation. '
+                        'The connection monitor may choose to use a different version depending on other parameters',
+                   arg_type=get_enum_type(PreferredIPVersion))
+        c.argument('test_config_threshold_failed_percent',
+                   options_list='--threshold-failed-percent',
+                   help='The maximum percentage of failed checks permitted for a test to evaluate as successful',
+                   type=int)
+        c.argument('test_config_threshold_round_trip_time',
+                   options_list='--threshold-round-trip-time',
+                   help='The maximum round-trip time in milliseconds permitted for a test to evaluate as successful',
+                   type=int)
+        # TCP protocol configuration
+        c.argument('test_config_tcp_port',
+                   options_list='--tcp-port',
+                   help='The port to connect to',
+                   type=int)
+        c.argument('test_config_tcp_disable_trace_route',
+                   options_list='--tcp-disable-trace-route',
+                   help='Value indicating whether path evaluation with trace route should be disabled. '
+                        'false is default.',
+                   arg_type=get_three_state_flag())
+        # ICMP protocol configuration
+        c.argument('test_config_icmp_disable_trace_route',
+                   options_list='--icmp-disable-trace-route',
+                   help='Value indicating whether path evaluation with trace route should be disabled. '
+                        'false is default.',
+                   arg_type=get_three_state_flag())
+        # HTTP protocol configuration
+        c.argument('test_config_http_port',
+                   options_list='--http-port',
+                   help='The port to connect to',
+                   type=int)
+        c.argument('test_config_http_method',
+                   options_list='--http-method',
+                   help='The HTTP method to use',
+                   arg_type=get_enum_type(HTTPConfigurationMethod))
+        c.argument('test_config_http_path',
+                   options_list='--http-path',
+                   help='The path component of the URI. For instance, "/dir1/dir2"')
+        c.argument('test_config_http_valid_status_codes',
+                   options_list='--http-valid-status-codes',
+                   help='Space-separated list of HTTP status codes to consider successful. '
+                        'For instance, "2xx 301-304 418"',
+                   nargs='+')
+        c.argument('test_config_http_prefer_https',
+                   options_list='--https-prefer',
+                   help='Value indicating whether HTTPS is preferred '
+                        'over HTTP in cases where the choice is not explicit',
+                   arg_type=get_three_state_flag())
+
+    # Argument Group for test group to create a V2 connection monitor
+    with self.argument_context('network watcher connection-monitor',
+                               arg_group='V2 Test Group',
+                               min_api='2019-11-01') as c:
+        c.argument('test_group_name',
+                   help='The name of the connection monitor test group',
+                   default='DefaultTestGroup')
+        c.argument('test_group_disable',
+                   help='Value indicating whether test group is disabled. false is default.',
+                   arg_type=get_three_state_flag())
+
+    # Argument Group for output to create a V2 connection monitor
+    with self.argument_context('network watcher connection-monitor',
+                               arg_group='V2 Output',
+                               min_api='2019-11-01') as c:
+        c.argument('output_type',
+                   help='Connection monitor output destination type. Currently, only "Workspace" is supported',
+                   arg_type=get_enum_type(OutputType))
+        c.argument('workspace_ids',
+                   help='Space-separated list of ids of log analytics workspace',
+                   nargs='+')
 
     with self.argument_context('network watcher configure') as c:
         c.argument('locations', get_location_type(self.cli_ctx), options_list=['--locations', '-l'], nargs='+')
