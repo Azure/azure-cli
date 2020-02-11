64373da32079e1883143ee4eb3d179071b0e5c36
Bin Ma <bim@microsoft.com>
2020-02-12 00:08:17 +0800

    [KeyVault] Support private endpoint connections (#12073)

--- src/azure-cli/azure/cli/command_modules/keyvault/tests/latest/test_keyvault_commands.py
+++ src/azure-cli/azure/cli/command_modules/keyvault/tests/latest/test_keyvault_commands.py
@@ -49,6 +49,104 @@ class DateTimeParseTest(unittest.TestCase):
         self.assertEqual(_asn1_to_iso8601("20170424163720Z"), expected)
 
 
+class KeyVaultPrivateLinkResourceScenarioTest(ScenarioTest):
+    @ResourceGroupPreparer(name_prefix='cli_test_keyvault_plr')
+    def test_keyvault_private_link_resource(self, resource_group):
+        self.kwargs.update({
+            'kv': self.create_random_name('cli-test-kv-plr-', 24),
+            'loc': 'centraluseuap'
+        })
+
+        _create_keyvault(self, self.kwargs)
+        self.cmd('keyvault private-link-resource show --vault-name {kv}',
+                 checks=self.check('value[0].groupId', 'vault'))
+
+
+class KeyVaultPrivateEndpointScenarioTest(ScenarioTest):
+    @ResourceGroupPreparer(name_prefix='cli_test_keyvault_pe')
+    def test_keyvault_private_endpoint(self, resource_group):
+        self.kwargs.update({
+            'kv': self.create_random_name('cli-test-kv-pe-', 24),
+            'loc': 'centraluseuap',
+            'vnet': self.create_random_name('cli-vnet-', 24),
+            'subnet': self.create_random_name('cli-subnet-', 24),
+            'pe': self.create_random_name('cli-pe-', 24),
+            'pe_connection': self.create_random_name('cli-pec-', 24)
+        })
+
+        # Prepare vault and network
+        keyvault = _create_keyvault(self, self.kwargs).get_output_in_json()
+        self.kwargs['kv_id'] = keyvault['id']
+        self.cmd('network vnet create -n {vnet} -g {rg} -l {loc} --subnet-name {subnet}',
+                 checks=self.check('length(newVNet.subnets)', 1))
+        self.cmd('network vnet subnet update -n {subnet} --vnet-name {vnet} -g {rg} '
+                 '--disable-private-endpoint-network-policies true',
+                 checks=self.check('privateEndpointNetworkPolicies', 'Disabled'))
+
+        # Create a private endpoint connection
+        pe = self.cmd('network private-endpoint create -g {rg} -n {pe} --vnet-name {vnet} --subnet {subnet} -l {loc} '
+                      '--connection-name {pe_connection} --private-connection-resource-id {kv_id} '
+                      '--group-ids vault').get_output_in_json()
+        self.kwargs['pe_id'] = pe['id']
+
+        # Show the connection at vault side
+        keyvault = self.cmd('keyvault show -n {kv}',
+                            checks=self.check('length(properties.privateEndpointConnections)', 1)).get_output_in_json()
+        self.kwargs['kv_pe_id'] = keyvault['properties']['privateEndpointConnections'][0]['id']
+        self.cmd('keyvault private-endpoint show --connection-id {kv_pe_id}',
+                 checks=self.check('id', '{kv_pe_id}'))
+        self.kwargs['kv_pe_name'] = self.kwargs['kv_pe_id'].split('/')[-1]
+        self.cmd('keyvault private-endpoint show --vault-name {kv} --connection-name {kv_pe_name}',
+                 checks=self.check('name', '{kv_pe_name}'))
+        self.cmd('keyvault private-endpoint show --vault-name {kv} -n {kv_pe_name}',
+                 checks=self.check('name', '{kv_pe_name}'))
+
+        # Try running `set-policy` on the linked vault
+        self.kwargs['policy_id'] = keyvault['properties']['accessPolicies'][0]['objectId']
+        self.cmd('keyvault set-policy -g {rg} -n {kv} --object-id {policy_id} --certificate-permissions get list',
+                 checks=self.check('length(properties.accessPolicies[0].permissions.certificates)', 2))
+
+        # Test approval/rejection
+        self.kwargs.update({
+            'approval_desc': 'You are approved!',
+            'rejection_desc': 'You are rejected!'
+        })
+        self.cmd('keyvault private-endpoint reject --connection-id {kv_pe_id} '
+                 '--rejection-description "{rejection_desc}"', checks=[
+                     self.check('privateLinkServiceConnectionState.status', 'Rejected'),
+                     self.check('privateLinkServiceConnectionState.description', '{rejection_desc}'),
+                     self.check('provisioningState', 'Updating')
+                 ])
+
+        max_retries = 20
+        retries = 0
+        while self.cmd('keyvault private-endpoint show --connection-id {kv_pe_id}').\
+                get_output_in_json()['provisioningState'] != 'Succeeded' or retries > max_retries:
+            if self.is_live:
+                time.sleep(5)
+            retries += 1
+
+        self.cmd('keyvault private-endpoint show --connection-id {kv_pe_id}',
+                 checks=self.check('provisioningState', 'Succeeded'))
+
+        self.cmd('keyvault private-endpoint approve --vault-name {kv} --connection-name {kv_pe_name} '
+                 '--approval-description "{approval_desc}"', checks=[
+                     self.check('privateLinkServiceConnectionState.status', 'Approved'),
+                     self.check('privateLinkServiceConnectionState.description', '{approval_desc}'),
+                     self.check('provisioningState', 'Updating')
+                 ])
+
+        retries = 0
+        while self.cmd('keyvault private-endpoint show --connection-id {kv_pe_id}'). \
+                get_output_in_json()['provisioningState'] != 'Succeeded' or retries > max_retries:
+            if self.is_live:
+                time.sleep(5)
+            retries += 1
+
+        self.cmd('keyvault private-endpoint show --connection-id {kv_pe_id}',
+                 checks=self.check('provisioningState', 'Succeeded'))
+
+
 class KeyVaultMgmtScenarioTest(ScenarioTest):
     @ResourceGroupPreparer(name_prefix='cli_test_keyvault_mgmt')
     def test_keyvault_mgmt(self, resource_group):
@@ -559,10 +657,8 @@ class KeyVaultCertificateIssuerScenarioTest(ScenarioTest):
 
 
 class KeyVaultPendingCertificateScenarioTest(ScenarioTest):
-
     @ResourceGroupPreparer(name_prefix='cli_test_kv_cert_pending')
     def test_keyvault_pending_certificate(self, resource_group):
-
         self.kwargs.update({
             'kv': self.create_random_name('cli-test-kv-cr-pe-', 24),
             'loc': 'westus',
@@ -599,7 +695,6 @@ class KeyVaultPendingCertificateScenarioTest(ScenarioTest):
 
 # TODO: Convert to ScenarioTest and re-record when issue #5146 is fixed.
 class KeyVaultCertificateDownloadScenarioTest(ScenarioTest):
-
     @ResourceGroupPreparer(name_prefix='cli_test_kv_cert_download')
     def test_keyvault_certificate_download(self, resource_group):
         import OpenSSL.crypto
