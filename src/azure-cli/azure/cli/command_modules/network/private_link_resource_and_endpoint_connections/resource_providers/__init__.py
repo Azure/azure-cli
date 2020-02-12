ca8b88e39504e9fb9d8f3a8dfecbc80eb66abb91
Myron <fanqiu@microsoft.com>
2020-02-12 18:05:45 +0800

    draft

---
+++ src/azure-cli/azure/cli/command_modules/network/private_link_resource_and_endpoint_connections/resource_providers/__init__.py
@@ -0,0 +1,26 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+import abc
+
+class PrivateEndpointClient(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def show_private_link_resource(self, cmd, resource_group_name, name):
+        return
+
+    @abc.abstractmethod
+    def approve_private_endpoint_connection(cmd, resource_group_name, service_name,
+                                            name, approval_description=None):
+        return
+
+    @abc.abstractmethod
+    def reject_private_endpoint_connection(cmd, resource_group_name, service_name,
+                                           name, rejection_description=None):
+        return
+
+    @abc.abstractmethod
+    def remove_private_endpoint_connection(cmd, resource_group_name, service_name, name):
+        return
