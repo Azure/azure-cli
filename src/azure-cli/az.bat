0a28deea521eaf60f8d796efa7817b1baf758e5c
Feng Zhou <fenzhou@microsoft.com>
2020-02-22 16:26:03 +0000

    use local python for az.bat

--- src/azure-cli/az.bat
+++ src/azure-cli/az.bat
@@ -1,5 +1,9 @@
 @echo off
 setlocal
 
-SET PYTHONPATH=%~dp0/src;%PYTHONPATH%
-python -m azure.cli %*
+IF EXIST "%~dp0\python.exe" (
+  "%~dp0\python.exe" -m azure.cli %*
+) ELSE (
+  SET PYTHONPATH=%~dp0/src;%PYTHONPATH%
+  python -m azure.cli %*
+)
