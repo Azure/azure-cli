b789ace8c1fe079558261adc6b4901d4fcb385a9
Zunli Hu <zuh@microsoft.com>
2020-02-24 15:49:33 +0800

    add examples'

--- src/azure-cli/azure/cli/command_modules/storage/_help.py
+++ src/azure-cli/azure/cli/command_modules/storage/_help.py
@@ -181,6 +181,33 @@ examples:
     crafted: true
 """
 
+helps['storage account file-service-properties'] = """
+type: group
+short-summary: Manage the properties of file service in storage account.
+"""
+
+helps['storage account file-service-properties show'] = """
+type: command
+short-summary: Show the properties of file service in storage account.
+long-summary: >
+    Show the properties of file service in storage account.
+examples:
+  - name: Show the properties of file service in storage account.
+    text: az storage account file-service-properties show -n mystorageaccount -g MyResourceGroup
+"""
+
+helps['storage account file-service-properties update'] = """
+type: command
+short-summary: Update the properties of file service in storage account.
+long-summary: >
+    Update the properties of file service in storage account.
+examples:
+  - name: Enable soft delete policy and set delete retention days to 100 for file service in storage account.
+    text: az storage account file-service-properties update --enable-delete-retention true --delete-retention-days 100 -n mystorageaccount -g MyResourceGroup
+  - name: Disable soft delete policy for file service. 
+    text: az storage account file-service-properties update --enable-delete-retention false -n mystorageaccount -g MyResourceGroup
+"""
+
 helps['storage account keys'] = """
 type: group
 short-summary: Manage storage account keys.
