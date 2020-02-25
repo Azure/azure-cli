f5810ef99e62cea3c792d2f01d774ffd36dca74f
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2020-02-25 16:31:40 +0800

    Update az.bat

--- src/azure-cli/az.bat
+++ src/azure-cli/az.bat
@@ -1,9 +1,10 @@
 @echo off
 setlocal
 
+SET PYTHONPATH=%~dp0\src;%PYTHONPATH%
+
 IF EXIST "%~dp0\python.exe" (
   "%~dp0\python.exe" -m azure.cli %*
 ) ELSE (
-  SET PYTHONPATH=%~dp0/src;%PYTHONPATH%
   python -m azure.cli %*
 )
