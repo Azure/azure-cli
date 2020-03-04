2eb3afd9aa6d85e8db42b61e3aad1e28552da910
RAJA OHRI <raohri@microsoft.com>
2020-03-04 20:25:40 +0530

    Add test for backup

--- src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_webapp_commands.py
+++ src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_webapp_commands.py
@@ -11,6 +11,7 @@ import os
 import time
 import tempfile
 import requests
+import datetime
 
 from azure_devtools.scenario_tests import AllowLargeResponse, record_only
 from azure.cli.testsdk import (ScenarioTest, LiveScenarioTest, ResourceGroupPreparer,
@@ -19,7 +20,6 @@ from azure.cli.testsdk import (ScenarioTest, LiveScenarioTest, ResourceGroupPrep
 TEST_DIR = os.path.abspath(os.path.join(os.path.abspath(__file__), '..'))
 
 # pylint: disable=line-too-long
-
 # In the future, for any reasons the repository get removed, the source code is under "sample-repo-for-deployment-test"
 # you can use to rebuild the repository
 TEST_REPO_URL = 'https://github.com/yugangw-msft/azure-site-test.git'
@@ -203,6 +203,26 @@ class WebappQuickCreateTest(ScenarioTest):
         self.cmd('webapp create -g {} -n webInOtherRG --plan {}'.format(resource_group2, plan_id), checks=[
             JMESPathCheck('name', 'webInOtherRG')
         ])
+
+class BackupWithName(ScenarioTest):
+    @ResourceGroupPreparer(parameter_name='resource_group')
+    def test_backup_with_name(self, resource_group):
+        plan = self.create_random_name(prefix='plan-backup', length=24)
+        self.cmd('appservice plan create -g {} -n {} --sku S1'.format(resource_group, plan))
+        webapp = self.create_random_name(prefix='backup-webapp',length=24)
+        self.cmd('webapp create -g {} -n {} --plan {}'.format(resource_group, webapp, plan))
+        storage_Account = self.create_random_name(prefix='backup',length=24)
+        self.cmd('storage account create -n {} -g {} --location westus'.format(storage_Account, resource_group))
+        container = self.create_random_name(prefix='backupcontainer',length=24)
+        self.cmd('storage container create --account-name {} --name {}'.format(storage_Account, container))
+        expirydate=(datetime.datetime.now() + datetime.timedelta(days=1, hours=3)).strftime("\"%Y-%m-%dT%H:%MZ\"")
+        sastoken = self.cmd('storage container generate-sas --account-name {} --name {} --expiry {} --permissions rwdl'.format(storage_Account, container, expirydate))
+        sasurl='\"https://{}.blob.core.windows.net/{}?{}\"'.format(storage_Account, container, sastoken)
+        backup_name = self.create_random_name(prefix='backup-name', length=24)
+        self.cmd('webapp config backup create -g {} --webapp-name {} --backup-name {} --container-url {}'.format(resource_group, webapp, backup_name, sasurl), checks=[
+            JMESPathCheck('backupItemName', backup_name)
+            ])
+
 
 
 # Test Framework is not able to handle binary file format, hence, only run live
