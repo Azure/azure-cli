0a470f91c1612d7d7d8b1d7928bdc6c3a947777d
Lixia (Sylvia) Lei <lixia_lei@outlook.com>
2020-03-05 16:50:08 +0800

    [ACR] Add license header for unit test file

--- src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_split_registry_and_image.py
+++ src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_split_registry_and_image.py
@@ -1,8 +1,12 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 import unittest
 import importlib
 
 from knack.util import CLIError
-
 
 acr_import = importlib.import_module('azure.cli.command_modules.acr.import')
 
