2d4daec934c9cd01423107d1c51b3b4a5de21ce3
Lixia (Sylvia) Lei <lixia_lei@outlook.com>
2020-03-07 13:54:57 +0800

    [ACR] Optimize unit test

--- src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_split_registry_and_image.py
+++ src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_split_registry_and_image.py
@@ -8,51 +8,43 @@ import importlib
 
 from knack.util import CLIError
 
+# The path contains a reserved keyword 'import', so we need a workaround here
 acr_import = importlib.import_module('azure.cli.command_modules.acr.import')
 
 
 class TestSplitRegistryAndImage(unittest.TestCase):
 
-    VALID_ACR_LOGIN_SERVER = 'myregistry.azurecr.io'
-    VALID_DOCKER_LOGIN_SERVER = 'docker.io'
-    VALID_IMAGE = 'myrepo/myimage:latest'
-
-    VALID_SOURCE_PATTERN = '{}/{}'
-    VALID_ACR_SOURCE = VALID_SOURCE_PATTERN.format(VALID_ACR_LOGIN_SERVER, VALID_IMAGE)
-    VALID_DOCKER_SOURCE = VALID_SOURCE_PATTERN.format(VALID_DOCKER_LOGIN_SERVER, VALID_IMAGE)
-
-    SINGLE_SLASH = '/'
-    SINGLE_DOT = '.'
-    EMPTY_SOURCE = ''
-
     def test_split_valid_acr_source(self):
-        login_server, image = acr_import._split_registry_and_image(TestSplitRegistryAndImage.VALID_ACR_SOURCE)
-        self.assertEqual((login_server, image), (TestSplitRegistryAndImage.VALID_ACR_LOGIN_SERVER, TestSplitRegistryAndImage.VALID_IMAGE))
+        valid_acr_source = 'myregistry.azurecr.io/myrepo/myimage:latest'
+        login_server, image = acr_import._split_registry_and_image(valid_acr_source)
+        self.assertEqual((login_server, image), ('myregistry.azurecr.io', 'myrepo/myimage:latest'))
 
     def test_split_valid_docker_source(self):
-        login_server, image = acr_import._split_registry_and_image(TestSplitRegistryAndImage.VALID_DOCKER_SOURCE)
-        self.assertEqual((login_server, image), (TestSplitRegistryAndImage.VALID_DOCKER_LOGIN_SERVER, TestSplitRegistryAndImage.VALID_IMAGE))
+        valid_docker_source = 'docker.io/myrepo/myimage:latest'
+        login_server, image = acr_import._split_registry_and_image(valid_docker_source)
+        self.assertEqual((login_server, image), ('docker.io', 'myrepo/myimage:latest'))
 
     def test_split_single_login_server(self):
+        single_login_server = 'myregistry.azurecr.io'
         with self.assertRaises(CLIError):
-            acr_import._split_registry_and_image(TestSplitRegistryAndImage.VALID_ACR_LOGIN_SERVER)
+            acr_import._split_registry_and_image(single_login_server)
 
     def test_split_single_image(self):
+        single_image = 'myrepo/myimage:latest'
         with self.assertRaises(CLIError):
-            acr_import._split_registry_and_image(TestSplitRegistryAndImage.VALID_IMAGE)
+            acr_import._split_registry_and_image(single_image)
 
     def test_split_single_slash(self):
+        single_slash = '/'
         with self.assertRaises(CLIError):
-            acr_import._split_registry_and_image(TestSplitRegistryAndImage.SINGLE_SLASH)
+            acr_import._split_registry_and_image(single_slash)
 
     def test_split_single_dot(self):
+        single_dot = '.'
         with self.assertRaises(CLIError):
-            acr_import._split_registry_and_image(TestSplitRegistryAndImage.SINGLE_DOT)
+            acr_import._split_registry_and_image(single_dot)
 
     def test_split_empy_source(self):
+        empty_source = ''
         with self.assertRaises(CLIError):
-            acr_import._split_registry_and_image(TestSplitRegistryAndImage.EMPTY_SOURCE)
-
-
-if __name__ == '__main__':
-    unittest.main()
+            acr_import._split_registry_and_image(empty_source)
