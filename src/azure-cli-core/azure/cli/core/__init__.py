7e91e089c663d3131088862b60fd204517eca89b
Jiashuo Li <jiasli@microsoft.com>
2020-03-12 13:32:43 +0800

    Fix tests

--- src/azure-cli-core/azure/cli/core/__init__.py
+++ src/azure-cli-core/azure/cli/core/__init__.py
@@ -220,9 +220,10 @@ class MainCommandsLoader(CLICommandsLoader):
                         self.command_group_table.update(extension_group_table)
                         elapsed_time = timeit.default_timer() - start_time
                         logger.debug("Loaded extension '%s' in %.3f seconds.", ext_name, elapsed_time)
-                    except Exception:  # pylint: disable=broad-except
+                    except Exception as ex:  # pylint: disable=broad-except
                         self.cli_ctx.raise_event(EVENT_FAILED_EXTENSION_LOAD, extension_name=ext_name)
-                        logger.warning("Unable to load extension '%s'. Use --debug for more information.", ext_name)
+                        logger.warning("Unable to load extension '%s: %s'. Use --debug for more information.",
+                                       ext_name, ex)
                         logger.debug(traceback.format_exc())
 
         def _wrap_suppress_extension_func(func, ext):
