e45395c1889a229f680d8ca0fe55d98cd4ee1b49
Yu Chen <ychenu@microsoft.com>
2020-03-26 11:30:39 +0800

    Fix style and linter error

--- src/azure-cli/azure/cli/command_modules/monitor/grammar/MetricAlertConditionLexer.py
+++ src/azure-cli/azure/cli/command_modules/monitor/grammar/MetricAlertConditionLexer.py
@@ -2,8 +2,10 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+# encoding: utf-8
 
 # pylint: disable=all
+# Generated from MetricAlertCondition.g4 by ANTLR 4.7.2
 from __future__ import print_function
 from antlr4 import *
 from io import StringIO
