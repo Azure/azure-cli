d1a058be811f327158f33707d069015ce8d8cf3c
Yu Chen <ychenu@microsoft.com>
2020-03-27 19:14:58 +0800

    fix style errors

--- src/azure-cli-core/azure/cli/core/util.py
+++ src/azure-cli-core/azure/cli/core/util.py
@@ -16,7 +16,6 @@ import six
 import re
 import logging
 
-from azure.core.exceptions import AzureError, HttpResponseError
 from six.moves.urllib.request import urlopen  # pylint: disable=import-error
 
 from azure.common import AzureException
