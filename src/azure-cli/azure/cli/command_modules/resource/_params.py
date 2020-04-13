abc4748022a0662597e228841e13ad255c054257
Shenglong Li <shenglong.li@outlook.com>
2020-04-13 11:05:22 -0700

    Create an alias -c for --confirm-with-what-if.

--- src/azure-cli/azure/cli/command_modules/resource/_params.py
+++ src/azure-cli/azure/cli/command_modules/resource/_params.py
@@ -58,7 +58,7 @@ def load_arguments(self, _):
     deployment_what_if_result_format_type = get_enum_type(WhatIfResultFormat, WhatIfResultFormat.full_resource_payloads)
     deployment_what_if_no_pretty_print_type = CLIArgumentType(options_list=['--no-pretty-print'], action='store_true',
                                                               help='Disable pretty-print for What-If results. When set, the output format type will be used.')
-    deployment_what_if_confirmation_type = CLIArgumentType(options_list=['--confirm-with-what-if'], action='store_true',
+    deployment_what_if_confirmation_type = CLIArgumentType(options_list=['--confirm-with-what-if', '-c'], action='store_true',
                                                            help='Instruct the command to run deployment What-If before excuting the deployment. It then prompts you to acknowledge resource changes before it continues.')
 
     _PROVIDER_HELP_TEXT = 'the resource namespace, aka \'provider\''
