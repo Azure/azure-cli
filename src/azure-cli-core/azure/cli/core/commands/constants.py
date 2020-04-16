0441f16fe995fa3af97a3a6edb27cb3e424c2ca9
Feng Zhou <fenzhou@microsoft.com>
2020-04-16 14:56:14 +0800

    change wording

--- src/azure-cli-core/azure/cli/core/commands/constants.py
+++ src/azure-cli-core/azure/cli/core/commands/constants.py
@@ -35,6 +35,7 @@ BLACKLISTED_MODS = ['context', 'shell', 'documentdb', 'component']
 SURVEY_PROMPT = 'Please let us know how we are doing: https://aka.ms/clihats'
 SURVEY_PROMPT_COLOR = Fore.YELLOW + Style.BRIGHT + 'Please let us know how we are doing: ' + Fore.BLUE + \
     'https://aka.ms/clihats' + Style.RESET_ALL
-UX_SURVEY_PROMPT = 'Mobile developer using Azure CLI? Come try our newest feature: https://aka.ms/CLIUXstudy'
-UX_SURVEY_PROMPT_COLOR = Fore.YELLOW + Style.BRIGHT + 'Mobile developer using Azure CLI? Come try our newest feature: '\
+UX_SURVEY_PROMPT = 'And let us know if you\'re interested in trying out our newest features: https://aka.ms/CLIUXstudy'
+UX_SURVEY_PROMPT_COLOR = Fore.YELLOW + Style.BRIGHT + \
+    'And let us know if you\'re interested in trying out our newest features: ' \
     + Fore.BLUE + 'https://aka.ms/CLIUXstudy' + Style.RESET_ALL
