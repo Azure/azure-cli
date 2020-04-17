0b3b6bb2ca49ec106024b9e3c564fc7c8faab66c
Rogelio Delgadillo <35089121+r-delgadillo@users.noreply.github.com>
2020-04-16 21:31:47 -0700

    [Iot Central] az iotcentral deprecated (#12681)
    * Deprecate 'iotcentral' and use 'iot central'
    
    * lint
    
    * Update help sku value
    
    * Use show_custom_command for 'show' command
    
    * Update method comments
    
    * linting, run tests
    
    * record tests
    
    * re-run tests
    
    * run tests
    
    * update test_iot_hub.yaml
    
    * revert test_iot_hub.yaml to previous PR commit
    
    * Bump version
    
    * fix history
    
    * remove expiration date
    
    * run test
    
    * Remove History comments
    
    * Add confirmation message for deleteing an application
    
    * Revert confirmation
    
    * Add confirmation
    
    * remove text from help file
    
    * Update src/azure-cli/azure/cli/command_modules/iot/_help.py
    
    * Update src/azure-cli/azure/cli/command_modules/iot/commands.py
    
    * Update src/azure-cli/azure/cli/command_modules/iot/commands.py
    
    * fix style
    
    Co-authored-by: rodelga <rodelga@microsoft.com>
    Co-authored-by: Feng Zhou <fenzhou@microsoft.com>
    Co-authored-by: Zunli Hu <zuh@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/iot/tests/latest/test_iot_central_commands.py
+++ src/azure-cli/azure/cli/command_modules/iot/tests/latest/test_iot_central_commands.py
@@ -6,10 +6,10 @@
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 
 
-class IoTCentralTestDeprecated(ScenarioTest):
+class IoTCentralTest(ScenarioTest):
 
     @ResourceGroupPreparer()  # name_prefix not required, but can be useful
-    def test_iotcentral_app(self, resource_group, resource_group_location):
+    def test_iot_central_app(self, resource_group, resource_group_location):
         app_name = self.create_random_name(prefix='iotc-cli-test', length=24)
         template_app_name = app_name + '-template'
         template_app_display_name = "My Custom App Display Name"
@@ -18,16 +18,16 @@ class IoTCentralTestDeprecated(ScenarioTest):
         template = 'iotc-pnp-preview@1.0.0'
         updatedName = app_name + 'update'
 
-        # Test 'az iotcentral app create'
-        self.cmd('iotcentral app create -n {0} -g {1} --subdomain {2} --sku {3}'.format(app_name, rg, app_name, 'ST2'), checks=[
+        # Test 'az iot central app create'
+        self.cmd('iot central app create -n {0} -g {1} --subdomain {2} --sku {3}'.format(app_name, rg, app_name, 'ST2'), checks=[
             self.check('resourceGroup', rg),
             self.check('location', location),
             self.check('subdomain', app_name),
             self.check('displayName', app_name),
             self.check('sku.name', 'ST2')])
 
-        # Test 'az iotcentral app create with template and display name'
-        self.cmd('iotcentral app create -n {0} -g {1} --subdomain {2} --template {3} --display-name \"{4}\" --sku {5}'
+        # Test 'az iot central app create with template and display name'
+        self.cmd('iot central app create -n {0} -g {1} --subdomain {2} --template {3} --display-name \"{4}\" --sku {5}'
                  .format(template_app_name, rg, template_app_name, template, template_app_display_name, 'ST1'), checks=[
                      self.check('resourceGroup', rg),
                      self.check('location', location),
@@ -36,8 +36,8 @@ class IoTCentralTestDeprecated(ScenarioTest):
                      self.check('sku.name', 'ST1'),
                      self.check('template', template)])
 
-        # Test 'az iotcentral app update'
-        self.cmd('iotcentral app update -n {0} -g {1} --set displayName={2} subdomain={3} sku.name={4}'
+        # Test 'az iot central app update'
+        self.cmd('iot central app update -n {0} -g {1} --set displayName={2} subdomain={3} sku.name={4}'
                  .format(template_app_name, rg, updatedName, updatedName, 'ST2'), checks=[
                      self.check('resourceGroup', rg),
                      self.check('location', location),
@@ -45,16 +45,16 @@ class IoTCentralTestDeprecated(ScenarioTest):
                      self.check('displayName', updatedName),
                      self.check('sku.name', 'ST2')])
 
-        # Test 'az iotcentral app show'
-        self.cmd('iotcentral app show -n {0} -g {1}'.format(app_name, rg), checks=[
+        # Test 'az iot central app show'
+        self.cmd('iot central app show -n {0} -g {1}'.format(app_name, rg), checks=[
             self.check('resourceGroup', rg),
             self.check('location', location),
             self.check('subdomain', app_name),
             self.check('displayName', app_name),
             self.check('sku.name', 'ST2')])
 
-        # Test 'az iotcentral app show with template and display name'
-        self.cmd('iotcentral app show -n {0} -g {1}'.format(template_app_name, rg), checks=[
+        # Test 'az iot central app show with template and display name'
+        self.cmd('iot central app show -n {0} -g {1}'.format(template_app_name, rg), checks=[
             self.check('resourceGroup', rg),
             self.check('location', location),
             self.check('subdomain', updatedName),
@@ -62,12 +62,12 @@ class IoTCentralTestDeprecated(ScenarioTest):
             self.check('sku.name', 'ST2'),
             self.check('template', template)])
 
-        # Test 'az iotcentral app delete with template and display name'
-        self.cmd('iotcentral app delete -n {0} -g {1}'.format(template_app_name, rg), checks=[
+        # Test 'az iot central app delete with template and display name'
+        self.cmd('iot central app delete -n {0} -g {1} --yes'.format(template_app_name, rg), checks=[
             self.is_empty()])
 
-        # Test 'az iotcentral app list'
-        self.cmd('iotcentral app list -g {0}'.format(rg), checks=[
+        # Test 'az iot central app list'
+        self.cmd('iot central app list -g {0}'.format(rg), checks=[
             self.check('length([*])', 1),
             self.check('[0].resourceGroup', rg),
             self.check('[0].location', location),
@@ -75,6 +75,6 @@ class IoTCentralTestDeprecated(ScenarioTest):
             self.check('[0].displayName', app_name),
             self.check('[0].sku.name', 'ST2')])
 
-        # Test 'az iotcentral app delete'
-        self.cmd('iotcentral app delete -n {0} -g {1}'.format(app_name, rg), checks=[
+        # Test 'az iot central app delete'
+        self.cmd('iot central app delete -n {0} -g {1} --yes'.format(app_name, rg), checks=[
             self.is_empty()])
