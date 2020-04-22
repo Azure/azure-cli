c5e8609d7286a239131c445b849fec0448834a8b
Soyoung Eom <soeom@microsoft.com>
2020-04-21 17:13:04 -0700

    fix ci

--- src/azure-cli/azure/cli/command_modules/cosmosdb/custom.py
+++ src/azure-cli/azure/cli/command_modules/cosmosdb/custom.py
@@ -1007,7 +1007,8 @@ def _update_private_endpoint_connection_status(client, resource_group_name, acco
     return client.create_or_update(resource_group_name=resource_group_name,
                                    account_name=account_name,
                                    private_endpoint_connection_name=private_endpoint_connection_name,
-                                   private_link_service_connection_state=private_endpoint_connection.private_link_service_connection_state)
+                                   private_link_service_connection_state=private_endpoint_connection.private_link_service_connection_state,
+                                   parameters=private_endpoint_connection)
 
 
 def approve_private_endpoint_connection(client, resource_group_name, account_name, private_endpoint_connection_name,
