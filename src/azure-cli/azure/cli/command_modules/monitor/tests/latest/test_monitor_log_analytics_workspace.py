d1632092f000e97be6ffd9867f659603c59a4af3
MyronFanQiu <49134743+MyronFanQiu@users.noreply.github.com>
2020-04-27 13:00:33 +0800

    [Monitor] az monitor log-analytics workspace linked-storage: supports BYOS features (#13187)
    * finish code
    
    recording
    
    finish tests
    
    * address comments and rerun tests
    
    * refine logic
    
    * address comments
    
    * fix style

--- src/azure-cli/azure/cli/command_modules/monitor/tests/latest/test_monitor_log_analytics_workspace.py
+++ src/azure-cli/azure/cli/command_modules/monitor/tests/latest/test_monitor_log_analytics_workspace.py
@@ -3,7 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, record_only
+from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, record_only, StorageAccountPreparer
 from azure_devtools.scenario_tests import AllowLargeResponse
 
 
@@ -107,3 +107,136 @@ class TestLogProfileScenarios(ScenarioTest):
 
         self.cmd("monitor log-analytics workspace linked-service list -g {rg} --workspace-name {workspace_name}",
                  checks=[self.check('length(@)', 0)])
+
+    @ResourceGroupPreparer(name_prefix='cli_test_monitor_workspace_linked_storage', location='eastus')
+    @AllowLargeResponse()
+    @StorageAccountPreparer(name_prefix='saws1', kind='StorageV2', sku='Standard_LRS', parameter_name='account_1', location='eastus')
+    @StorageAccountPreparer(name_prefix='saws2', kind='StorageV2', sku='Standard_LRS', parameter_name='account_2', location='eastus')
+    @StorageAccountPreparer(name_prefix='saws3', kind='StorageV2', sku='Standard_LRS', parameter_name='account_3', location='eastus')
+    @StorageAccountPreparer(name_prefix='saws4', kind='StorageV2', sku='Standard_LRS', parameter_name='account_4', location='eastus')
+    def test_monitor_log_analytics_workspace_linked_storage(self, resource_group, account_1,
+                                                            account_2, account_3, account_4):
+        from msrestazure.tools import resource_id
+        self.kwargs.update({
+            'name': self.create_random_name('clitest', 20),
+            'name_2': self.create_random_name('clitest', 20),
+            'rg': resource_group,
+            'sa_1': account_1,
+            'sa_2': account_2,
+            'sa_3': account_3,
+            'sa_4': account_4,
+            'sa_id_1': resource_id(
+                resource_group=resource_group,
+                subscription=self.get_subscription_id(),
+                name=account_1,
+                namespace='Microsoft.Storage',
+                type='storageAccounts'),
+            'sa_id_2': resource_id(
+                resource_group=resource_group,
+                subscription=self.get_subscription_id(),
+                name=account_2,
+                namespace='Microsoft.Storage',
+                type='storageAccounts'),
+            'sa_id_3': resource_id(
+                resource_group=resource_group,
+                subscription=self.get_subscription_id(),
+                name=account_3,
+                namespace='Microsoft.Storage',
+                type='storageAccounts'),
+            'sa_id_4': resource_id(
+                resource_group=resource_group,
+                subscription=self.get_subscription_id(),
+                name=account_4,
+                namespace='Microsoft.Storage',
+                type='storageAccounts'),
+        })
+
+        self.cmd("monitor log-analytics workspace create -g {rg} -n {name} --tags clitest=myron", checks=[
+            self.check('provisioningState', 'Succeeded'),
+            self.check('retentionInDays', 30),
+            self.check('sku.name', 'pergb2018')
+        ])
+
+        self.cmd('monitor log-analytics workspace linked-storage create '
+                 '--type AzureWatson -g {rg} -n {name} --storage-accounts {sa_1}',
+                 checks=[
+                     self.check('storageAccountIds[0]', '{sa_id_1}'),
+                     self.check('name', 'azurewatson')
+                 ])
+
+        self.cmd('monitor log-analytics workspace linked-storage add '
+                 '--type AzureWatson -g {rg} -n {name} --storage-accounts {sa_2} {sa_id_3}',
+                 checks=[
+                     self.check('storageAccountIds[0]', '{sa_id_1}'),
+                     self.check('storageAccountIds[1]', '{sa_id_2}'),
+                     self.check('storageAccountIds[2]', '{sa_id_3}')
+                 ])
+
+        self.cmd('monitor log-analytics workspace linked-storage remove '
+                 '--type AzureWatson -g {rg} -n {name} --storage-accounts {sa_1}',
+                 checks=[
+                     self.check('storageAccountIds[0]', '{sa_id_2}'),
+                     self.check('storageAccountIds[1]', '{sa_id_3}')
+                 ])
+
+        self.cmd('monitor log-analytics workspace linked-storage show '
+                 '--type AzureWatson -g {rg} -n {name}',
+                 checks=[
+                     self.check('storageAccountIds[0]', '{sa_id_2}'),
+                     self.check('storageAccountIds[1]', '{sa_id_3}')
+                 ])
+
+        self.cmd("monitor log-analytics workspace create -g {rg} -n {name_2} --tags clitest=myron", checks=[
+            self.check('provisioningState', 'Succeeded'),
+            self.check('retentionInDays', 30),
+            self.check('sku.name', 'pergb2018')
+        ])
+
+        self.cmd('monitor log-analytics workspace linked-storage create '
+                 '--type CustomLogs -g {rg} -n {name} --storage-accounts {sa_1} {sa_id_4}',
+                 checks=[
+                     self.check('storageAccountIds[0]', '{sa_id_1}'),
+                     self.check('storageAccountIds[1]', '{sa_id_4}')
+                 ])
+
+        self.cmd('monitor log-analytics workspace linked-storage list '
+                 '-g {rg} -n {name}',
+                 checks=[
+                     self.check('length(@)', 2)
+                 ])
+
+        self.cmd('monitor log-analytics workspace linked-storage delete '
+                 '--type CustomLogs -g {rg} -n {name} -y')
+
+        self.cmd('monitor log-analytics workspace linked-storage list '
+                 '-g {rg} -n {name}',
+                 checks=[
+                     self.check('length(@)', 1)
+                 ])
+
+    @ResourceGroupPreparer(name_prefix='cli_test_monitor_workspace_public_access', location='eastus')
+    def test_monitor_log_analytics_workspace_public_access(self, resource_group):
+        from msrestazure.tools import resource_id
+        self.kwargs.update({
+            'name': self.create_random_name('clitest', 20),
+            'name_2': self.create_random_name('clitest', 20)
+        })
+
+        self.cmd("monitor log-analytics workspace create -g {rg} -n {name} --tags clitest=myron "
+                 "--query-access Disabled --ingestion-access Disabled",
+                 checks=[
+                     self.check('publicNetworkAccessForIngestion', 'Disabled'),
+                     self.check('publicNetworkAccessForQuery', 'Disabled')
+                 ])
+
+        self.cmd("monitor log-analytics workspace update -g {rg} -n {name} "
+                 "--query-access Disabled --ingestion-access Enabled",
+                 checks=[
+                     self.check('publicNetworkAccessForIngestion', 'Enabled'),
+                     self.check('publicNetworkAccessForQuery', 'Disabled')
+                 ])
+
+        self.cmd("monitor log-analytics workspace show -g {rg} -n {name}", checks=[
+            self.check('publicNetworkAccessForIngestion', 'Enabled'),
+            self.check('publicNetworkAccessForQuery', 'Disabled')
+        ])
