cf9a7bd2908ff2746e88a6fabe4fc01731e725ce
harold random <haroldrandom@gmail.com>
2020-04-28 13:27:41 +0800

    fix loading wrong wheel type extension metadata

--- src/azure-cli-core/azure/cli/core/extension/__init__.py
+++ src/azure-cli-core/azure/cli/core/extension/__init__.py
@@ -138,7 +138,8 @@ class WheelExtension(Extension):
             return None
         metadata = {}
         ext_dir = self.path or get_extension_path(self.name)
-        info_dirs = glob(os.path.join(ext_dir, '*.*-info'))
+        info_dirs = glob(os.path.join(ext_dir, self.name.replace('-', '_') + '*.*-info'))
+
         azext_metadata = WheelExtension.get_azext_metadata(ext_dir)
         if azext_metadata:
             metadata.update(azext_metadata)
