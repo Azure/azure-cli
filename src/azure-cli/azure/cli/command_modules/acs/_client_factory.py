0d02ed2ad308817485122bfa6fce3adbec993c4b
huahuan <hhz1992@gmail.com>
2020-04-30 20:39:53 -0700

    clean up code

--- src/azure-cli/azure/cli/command_modules/acs/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/acs/_client_factory.py
@@ -5,7 +5,7 @@
 
 from azure.cli.core.commands.client_factory import get_mgmt_service_client
 from azure.cli.core.commands.parameters import get_resources_in_subscription
-from azure.cli.core.profiles import ResourceType, get_api_version
+from azure.cli.core.profiles import ResourceType
 from knack.util import CLIError
 
 
