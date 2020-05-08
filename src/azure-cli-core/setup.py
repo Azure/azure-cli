b41f3d5877bf2afe0f25874eecf100cc1d363bc2
Xiaojian Xu <xiaojxu@microsoft.com>
2020-05-08 17:47:18 +0800

    update knack version to 0.7.0

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -56,7 +56,7 @@ DEPENDENCIES = [
     'colorama>=0.3.9',
     'humanfriendly>=4.7,<9.0',
     'jmespath',
-    'knack==0.7.0rc4',
+    'knack==0.7.0',
     'msal~=1.0.0',
     'msal-extensions~=0.1.3',
     'msrest>=0.4.4',
