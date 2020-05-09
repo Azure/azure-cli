09d61fb5f643b4da0186c8f959bdecaf06a2c322
Xiaojian Xu <xiaojxu@microsoft.com>
2020-05-09 16:53:08 +0800

    fix sytle error

--- src/azure-cli/azure/cli/command_modules/configure/_validators.py
+++ src/azure-cli/azure/cli/command_modules/configure/_validators.py
@@ -12,4 +12,3 @@ def validate_local_context(cmd, namespace):  # pylint: disable=unused-argument
     if not cmd.cli_ctx.local_context.current_dir:
         raise CLIError('The working directory has been deleted or recreated. You can change to another working '
                        'directory or reenter current one if it is recreated.')
-
