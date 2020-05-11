aad834b27efc5ad5142817ec8cac58276f42a1cc
Feng Zhou <fenzhou@microsoft.com>
2020-05-11 13:44:59 +0800

    test new azdev

--- scripts/ci/install_azdev.sh
+++ scripts/ci/install_azdev.sh
@@ -8,5 +8,5 @@ pip install virtualenv
 python -m virtualenv env
 . env/bin/activate
 pip install -U pip setuptools wheel -q
-pip install azdev==0.1.4 -q
-azdev setup -c
+pip install git+https://github.com/fengzhou-msft/azure-cli-dev-tools.git@dependency_choice -q
+azdev setup -c -d requirements
