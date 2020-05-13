e364268ba24f13a11d5f8a1afc84b417d977d031
Monet <hhz1992@gmail.com>
2020-05-12 21:33:27 -0700

    add acr for azurestack (#13442)

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -225,6 +225,7 @@ AZURE_API_PROFILES = {
         ResourceType.MGMT_APPSERVICE: '2018-02-01',
         ResourceType.MGMT_EVENTHUB: '2017-04-01',
         ResourceType.MGMT_IOTHUB: '2019-03-22',
+        ResourceType.MGMT_CONTAINERREGISTRY: '2019-05-01',
         ResourceType.MGMT_CONTAINERSERVICE: SDKProfile('2020-03-01', {
             'container_services': '2017-07-01',
             'open_shift_managed_clusters': '2019-10-27-preview'
