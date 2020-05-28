f9d5eab13b3f32bfdd5a16a20737cdcae7470aa9
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2020-05-28 17:53:14 +0800

    {Telemetry} Log exception (#13538)

--- src/azure-cli/azure/cli/__main__.py
+++ src/azure-cli/azure/cli/__main__.py
@@ -41,6 +41,8 @@ try:
     exit_code = cli_main(az_cli, sys.argv[1:])
 
     if exit_code and exit_code != 0:
+        if az_cli.result.error is not None and not telemetry.has_exceptions():
+            telemetry.set_exception(az_cli.result.error, fault_type='')
         telemetry.set_failure()
     else:
         telemetry.set_success()
