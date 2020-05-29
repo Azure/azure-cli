497020fed28dd5cbfe4bc4efb876453a8a3cf3a3
Jorge Cotillo <jcotillo@microsoft.com>
2020-05-28 17:48:43 -0700

    removed str reference from param

--- src/azure-cli/azure/cli/command_modules/resource/custom.py
+++ src/azure-cli/azure/cli/command_modules/resource/custom.py
@@ -1035,7 +1035,7 @@ def export_group_as_template(
         cmd, resource_group_name, include_comments=False, include_parameter_default_value=False, resource_ids=None, skip_resource_name_params=False, skip_all_params=False):
     """Captures a resource group as a template.
     :param str resource_group_name: the name of the resource group.
-    :param str resource_ids: space-separated resource ids to filter the export by. To export all resources, do not specify this argument or supply "*".
+    :param resource_ids: space-separated resource ids to filter the export by. To export all resources, do not specify this argument or supply "*".
     :param bool include_comments: export template with comments.
     :param bool include_parameter_default_value: export template parameter with default value.
     :param bool skip_resource_name_params: export template and skip resource name parameterization.
