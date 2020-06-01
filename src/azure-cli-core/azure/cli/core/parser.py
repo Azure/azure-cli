763c17524c366866bd395e00f01ef6c63b974de1
houk-ms <honc@microsoft.com>
2020-06-01 12:53:37 +0800

    format code style

--- src/azure-cli-core/azure/cli/core/parser.py
+++ src/azure-cli-core/azure/cli/core/parser.py
@@ -153,8 +153,7 @@ class AzCliCommandParser(CLICommandParser):
             logger.error('%(prog)s: error: %(message)s', args)
         self.print_usage(sys.stderr)
         # Manual recommendations
-        manual_recommendations = self._get_manual_recommendations(**args)
-        self._suggestion_msg.extend(manual_recommendations)
+        self._set_manual_recommendations(**args)
         # AI recommendations
         failure_recovery_recommendations = self._get_failure_recovery_recommendations()
         self._suggestion_msg.extend(failure_recovery_recommendations)
@@ -183,12 +182,13 @@ class AzCliCommandParser(CLICommandParser):
         argcomplete.autocomplete(self, validator=lambda c, p: c.lower().startswith(p.lower()),
                                  default_completer=lambda _: ())
 
-    def _get_manual_recommendations(self, **kwargs):
+    def _set_manual_recommendations(self, **kwargs):
         recommendations = []
         # recommendation for --query value error
         if 'message' in kwargs and '--query' in kwargs['message']:
-            recommendations.append('To learn more about [--query JMESPATH] usage in AzureCLI, visit https://aka.ms/CLIQuery')
-        return recommendations
+            recommendations.append('To learn more about [--query JMESPATH] usage in AzureCLI, '
+                                   'visit https://aka.ms/CLIQuery')
+        self._suggestion_msg.extend(recommendations)
 
     def _get_failure_recovery_arguments(self, action=None):
         # Strip the leading "az " and any extraneous whitespace.
