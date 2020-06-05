458e85f2f0dba3165dcf7d0e76b70a5456e4bc25
Jiashuo Li <jiasli@microsoft.com>
2020-06-05 15:13:46 +0800

    Fix timer

--- src/azure-cli/azure/cli/__main__.py
+++ src/azure-cli/azure/cli/__main__.py
@@ -3,15 +3,15 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import timeit
+# Log the start time
+start_time = timeit.default_timer()
+
 import sys
 import uuid
-import timeit
-
 from knack.completion import ARGCOMPLETE_ENV_NAME
 from knack.log import get_logger
-
 from azure.cli.core import get_default_cli
-
 import azure.cli.core.telemetry as telemetry
 
 
@@ -34,9 +34,11 @@ az_cli = get_default_cli()
 
 telemetry.set_application(az_cli, ARGCOMPLETE_ENV_NAME)
 
+# Log the init finish time
+init_finish_time = timeit.default_timer()
+
 try:
     telemetry.start()
-    start_time = timeit.default_timer()
 
     exit_code = cli_main(az_cli, sys.argv[1:])
 
@@ -47,8 +49,6 @@ try:
     else:
         telemetry.set_success()
 
-    elapsed_time = timeit.default_timer() - start_time
-
     sys.exit(exit_code)
 
 except KeyboardInterrupt:
@@ -56,18 +56,17 @@ except KeyboardInterrupt:
     sys.exit(1)
 except SystemExit as ex:  # some code directly call sys.exit, this is to make sure command metadata is logged
     exit_code = ex.code if ex.code is not None else 1
-
-    try:
-        elapsed_time = timeit.default_timer() - start_time
-    except NameError:
-        pass
-
     raise ex
 
 finally:
     telemetry.conclude()
 
     try:
-        logger.info("command ran in %.3f seconds.", elapsed_time)
+        # Log the invoke finish time
+        invoke_finish_time = timeit.default_timer()
+        logger.info("Command ran in %.3f seconds (init: %.3f, invoke: %.3f)",
+                    invoke_finish_time - start_time,
+                    init_finish_time - start_time,
+                    invoke_finish_time - init_finish_time)
     except NameError:
         pass
