79a8d71ae84459d28f2a45a6e4e7ef79ff6440e7
Xiaojian Xu <xiaojxu@microsoft.com>
2020-06-09 15:58:53 +0800

    {Package} init pep420 update

--- scripts/release/get_version.sh
+++ scripts/release/get_version.sh
@@ -2,6 +2,6 @@
 
 : "${BUILD_STAGINGDIRECTORY:?BUILD_STAGINGDIRECTORY environment variable not set}"
 
-ver=`cat src/azure-cli/azure/cli/__init__.py | grep __version__ | sed s/' '//g | sed s/'__version__='// |  sed s/\"//g`
+ver=`cat src/azure-cli/azure/cli/command_modules/__init__.py | grep __version__ | sed s/' '//g | sed s/'__version__='// |  sed s/\"//g`
 echo $ver > $BUILD_STAGINGDIRECTORY/version
 echo $ver > $BUILD_STAGINGDIRECTORY/azure-cli-${ver}.txt
---
+++ src/azure-cli/azure/cli/command_modules/__init__.py
@@ -0,0 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+__author__ = "Microsoft Corporation <python@microsoft.com>"
+__version__ = "2.7.0"
