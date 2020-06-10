74cf3049b4cbcc7c082a162862c21a38090b9dee
Xiaojian Xu <xiaojxu@microsoft.com>
2020-06-10 13:51:47 +0800

    remove azure-cli-nspkg and azure-cli-command_modules dependency

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -52,9 +52,7 @@ CLASSIFIERS = [
 DEPENDENCIES = [
     'antlr4-python3-runtime~=4.7.2',
     'azure-batch~=9.0',
-    'azure-cli-command_modules-nspkg~=2.0',
     'azure-cli-core=={}.*'.format(VERSION),
-    'azure-cli-nspkg~=3.0,>=3.0.3',
     'azure-cosmos~=3.0,>=3.0.2',
     'azure-datalake-store~=0.0.48',
     'azure-functions-devops-build~=0.0.22',
