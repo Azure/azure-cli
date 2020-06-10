d87516abec384fa3d1459befbe49bf458a293fbb
Feiyue Yu <fey@microsoft.com>
2020-06-10 16:28:49 +0800

    [Compute] vm/vmss create: Encryption of cache & data-in-transit for OS/Data disks and temp disks for VM & VMSS

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -645,7 +645,8 @@ def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_
               boot_diagnostics_storage=None, ultra_ssd_enabled=None, ephemeral_os_disk=None,
               proximity_placement_group=None, dedicated_host=None, dedicated_host_group=None, aux_subscriptions=None,
               priority=None, max_price=None, eviction_policy=None, enable_agent=None, workspace=None, vmss=None,
-              os_disk_encryption_set=None, data_disk_encryption_sets=None, specialized=None):
+              os_disk_encryption_set=None, data_disk_encryption_sets=None, specialized=None,
+              encryption_at_host=None):
     from azure.cli.core.commands.client_factory import get_subscription_id
     from azure.cli.core.util import random_string, hash_string
     from azure.cli.core.commands.arm import ArmTemplateBuilder
@@ -815,7 +816,8 @@ def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_
         proximity_placement_group=proximity_placement_group, computer_name=computer_name,
         dedicated_host=dedicated_host, priority=priority, max_price=max_price, eviction_policy=eviction_policy,
         enable_agent=enable_agent, vmss=vmss, os_disk_encryption_set=os_disk_encryption_set,
-        data_disk_encryption_sets=data_disk_encryption_sets, specialized=specialized)
+        data_disk_encryption_sets=data_disk_encryption_sets, specialized=specialized,
+        encryption_at_host=encryption_at_host)
 
     vm_resource['dependsOn'] = vm_dependencies
 
@@ -2200,7 +2202,7 @@ def create_vmss(cmd, vmss_name, resource_group_name, image=None,
                 proximity_placement_group=None, aux_subscriptions=None, terminate_notification_time=None,
                 max_price=None, computer_name_prefix=None, orchestration_mode='ScaleSetVM', scale_in_policy=None,
                 os_disk_encryption_set=None, data_disk_encryption_sets=None, data_disk_iops=None, data_disk_mbps=None,
-                automatic_repairs_grace_period=None, specialized=None, os_disk_size_gb=None):
+                automatic_repairs_grace_period=None, specialized=None, os_disk_size_gb=None, encryption_at_host=None):
     from azure.cli.core.commands.client_factory import get_subscription_id
     from azure.cli.core.util import random_string, hash_string
     from azure.cli.core.commands.arm import ArmTemplateBuilder
@@ -2430,7 +2432,7 @@ def create_vmss(cmd, vmss_name, resource_group_name, image=None,
             scale_in_policy=scale_in_policy, os_disk_encryption_set=os_disk_encryption_set,
             data_disk_encryption_sets=data_disk_encryption_sets, data_disk_iops=data_disk_iops,
             data_disk_mbps=data_disk_mbps, automatic_repairs_grace_period=automatic_repairs_grace_period,
-            specialized=specialized, os_disk_size_gb=os_disk_size_gb)
+            specialized=specialized, os_disk_size_gb=os_disk_size_gb, encryption_at_host=encryption_at_host)
 
         vmss_resource['dependsOn'] = vmss_dependencies
 
