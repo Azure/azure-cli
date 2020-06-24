6120c650be0a5610080dfb6f7b16d9818c2d02ab
Xiaojian Xu <xiaojxu@microsoft.com>
2020-06-24 16:22:25 +0800

    upgrade azure-multiapi-storage version

--- src/azure-cli/azure/cli/__init__.py
+++ src/azure-cli/azure/cli/__init__.py
@@ -7,8 +7,6 @@
 This tools provides a command-line interface to Azure's management and storage
 APIs.
 '''
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
 
 __author__ = "Microsoft Corporation <python@microsoft.com>"
 __version__ = "2.8.0"
