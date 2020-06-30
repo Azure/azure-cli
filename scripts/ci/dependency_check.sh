46b14161b9f3b8ac2ff16f56e4c5b4252da2dcb2
qianwens <qianwens@microsoft.com>
2020-06-30 18:45:14 +0800

    [MSAL]fix CI

--- scripts/ci/dependency_check.sh
+++ scripts/ci/dependency_check.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
+apt install libgirepository1.0-dev libcairo2-dev gir1.2-secret-1
+
 REPO_ROOT="$(dirname ${BASH_SOURCE[0]})/../.."
 
 # Uninstall any cruft that can poison the rest of the checks in this script.
