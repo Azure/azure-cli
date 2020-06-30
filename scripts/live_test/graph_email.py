39d22279e2f902d1ff977711347cd7480a1adcf9
Feiyue Yu <fey@microsoft.com>
2020-06-30 20:13:17 +0800

    gragh_email.py

--- scripts/live_test/graph_email.py
+++ scripts/live_test/graph_email.py
@@ -6,7 +6,56 @@ import json
 USER = False
 
 
+def delegated():
+    client_id = 'b1023c2f-3625-4726-a173-09449d190793'
+    # client_id = 'df1f9b2a-1f75-4032-8396-2b03d4631e6b'
+    # authority = 'https://login.microsoftonline.com/54826b22-38d6-4fb2-bad9-b7b93a3e9c5a'
+    # authority = 'https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47'
+    authority = 'https://login.microsoftonline.com/organizations'
+    username = 'azurecli@microsoft.com'
+    password = ''
+    scopes = []
+    endpoint = 'https://graph.microsoft.com/v1.0/me/sendMail'
+
+    app = msal.PublicClientApplication(
+        client_id,
+        authority=authority
+    )
+
+    result = None
+
+    accounts = app.get_accounts(username)
+    print(accounts)
+    if accounts:
+        logging.info("Account(s) exists in cache, probably with token too. Let's try.")
+        result = app.acquire_token_silent(scopes, account=accounts[0])
+
+    if not result:
+        logging.info("No suitable token exists in cache. Let's get a new one from AAD.")
+        result = app.acquire_token_by_username_password(username, password, scopes=scopes)
+        # result = app.acquire_token_by_authorization_code()
+
+    # print(result)
+
+    if "access_token" in result:
+        token = result['access_token']
+    else:
+        print(result.get("error"))
+        print(result.get("error_description"))
+        raise Exception()
+
+    response = requests.get(
+        endpoint,
+        headers={'Authorization': 'Bearer ' + token}
+    )
+
+    print(response.json())
+
+
 def main():
+    delegated()
+    return
+
     app = msal.ConfidentialClientApplication(
         'b1023c2f-3625-4726-a173-09449d190793',
         authority='https://login.microsoftonline.com/54826b22-38d6-4fb2-bad9-b7b93a3e9c5a',
