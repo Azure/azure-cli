20275b5432e2d669b199b19278136525d21dcfcf
qianwens <qianwens@microsoft.com>
2020-07-01 15:04:24 +0800

    [MSAL]fix CI

--- scripts/ci/dependency_check.sh
+++ scripts/ci/dependency_check.sh
@@ -2,7 +2,9 @@
 
 set -ev
 
-sudo apt-get -y install libgirepository1.0-dev libcairo2-dev gir1.2-secret-1
+if [ "$(uname)" != "Darwin" ]; then
+    sudo apt-get -y install libgirepository1.0-dev libcairo2-dev gir1.2-secret-1
+fi
 
 REPO_ROOT="$(dirname ${BASH_SOURCE[0]})/../.."
 
