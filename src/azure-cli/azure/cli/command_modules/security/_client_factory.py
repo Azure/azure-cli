c8d192e0bf386fa913fe1b77566fea2abd5bb38f
Arik Riklin <arik.riklin@microsoft.com>
2020-07-02 19:54:33 +0300

    add newline at end of file

--- src/azure-cli/azure/cli/command_modules/security/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/security/_client_factory.py
@@ -94,3 +94,4 @@ def cf_security_regulatory_compliance_assessment(cli_ctx, _):
 def cf_security_allowed_connections(cli_ctx, _):
     # do not return cli_ctx.allowed_connections for home region compatibility
     return _cf_security(cli_ctx)
+    
