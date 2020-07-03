35fedce9210805964fafc9aa7b17df5017697e50
Feiyue Yu <fey@microsoft.com>
2020-07-03 09:52:34 +0800

    disks 2020-05-01

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -134,7 +134,7 @@ AZURE_API_PROFILES = {
         ResourceType.MGMT_NETWORK: '2020-05-01',
         ResourceType.MGMT_COMPUTE: SDKProfile('2019-07-01', {
             'resource_skus': '2019-04-01',
-            'disks': '2019-11-01',
+            'disks': '2020-05-01',
             'snapshots': '2019-07-01',
             'galleries': '2019-12-01',
             'gallery_images': '2019-12-01',
