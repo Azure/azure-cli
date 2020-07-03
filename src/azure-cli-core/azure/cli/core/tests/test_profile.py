b93e2444f9524c9f7a0932f5a680e92b1c78cd87
Zunli Hu <zuh@microsoft.com>
2020-07-03 11:29:58 +0800

    pass test_get_login_credentials_msi_system_assigned

--- src/azure-cli-core/azure/cli/core/tests/test_profile.py
+++ src/azure-cli-core/azure/cli/core/tests/test_profile.py
@@ -645,7 +645,7 @@ class TestProfile(unittest.TestCase):
                                                                      aux_tenants=[test_tenant_id2])
 
     @mock.patch('azure.cli.core._profile._load_tokens_from_file', autospec=True)
-    @mock.patch('msrestazure.azure_active_directory.MSIAuthentication', autospec=True)
+    @mock.patch('azure.cli.core._profile.MSIAuthenticationWrapper', autospec=True)
     def test_get_login_credentials_msi_system_assigned(self, mock_msi_auth, mock_read_cred_file):
         mock_read_cred_file.return_value = []
 
