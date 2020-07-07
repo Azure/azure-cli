26bfc65de08be8b35f38743db6739fe3790ea333
MyronFanQiu <fanqiu@microsoft.com>
2020-07-07 16:24:00 +0800

    add help and license

--- src/azure-cli/azure/cli/command_modules/monitor/grammar/autoscale/AutoscaleConditionParser.py
+++ src/azure-cli/azure/cli/command_modules/monitor/grammar/autoscale/AutoscaleConditionParser.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 # Generated from AutoscaleCondition.g4 by ANTLR 4.7.2
 # encoding: utf-8
 from __future__ import print_function
