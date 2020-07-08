a26684cf20957e490c131d50fd420718ce710dec
Xiaojian Xu <xiaojxu@microsoft.com>
2020-07-08 16:43:59 +0800

    fix style error

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -3131,21 +3131,21 @@ def fix_gallery_image_date_info(date_info):
 
 def update_image_version(cmd, resource_group_name, gallery_name, gallery_image_name, gallery_image_version_name,
                          target_regions=None, replica_count=None, no_wait=False, **kwargs):
-    gallery_image_version = kwargs['gallery_image_version']
+    image_version = kwargs['gallery_image_version']
 
     if target_regions:
-        gallery_image_version.publishing_profile.target_regions = target_regions
+        image_version.publishing_profile.target_regions = target_regions
     if replica_count:
-        gallery_image_version.publishing_profile.replica_count = replica_count
-    if gallery_image_version.storage_profile.source is not None:
-        gallery_image_version.storage_profile.os_disk_image = gallery_image_version.storage_profile.data_disk_images = None
+        image_version.publishing_profile.replica_count = replica_count
+    if image_version.storage_profile.source is not None:
+        image_version.storage_profile.os_disk_image = image_version.storage_profile.data_disk_images = None
 
     aux_subscriptions = None
-    if gallery_image_version.storage_profile and gallery_image_version.storage_profile.source and \
-            gallery_image_version.storage_profile.source.id:
+    if image_version.storage_profile and image_version.storage_profile.source and \
+            image_version.storage_profile.source.id:
         from msrestazure.tools import is_valid_resource_id, parse_resource_id
-        if is_valid_resource_id(gallery_image_version.storage_profile.source.id):
-            res = parse_resource_id(gallery_image_version.storage_profile.source.id)
+        if is_valid_resource_id(image_version.storage_profile.source.id):
+            res = parse_resource_id(image_version.storage_profile.source.id)
             aux_subscriptions = [res['subscription']]
     client = _compute_client_factory(cmd.cli_ctx, aux_subscriptions=aux_subscriptions)
     return sdk_no_wait(no_wait, client.gallery_image_versions.create_or_update, resource_group_name, gallery_name,
