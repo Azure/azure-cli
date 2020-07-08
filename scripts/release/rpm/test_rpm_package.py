3b621afdd743e690aaba6bffb319b5d162309647
Feng Zhou <fenzhou@microsoft.com>
2020-07-08 13:17:28 +0800

    fix botservice

--- scripts/release/rpm/test_rpm_package.py
+++ scripts/release/rpm/test_rpm_package.py
@@ -8,4 +8,8 @@ mod_list = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.j
 
 for mod in mod_list:
     mod_name = os.path.basename(os.path.normpath(mod))
+    if mod_name == 'botservice':
+        subprocess.call(['export PYTHONPATH=/usr/lib64/az/lib/python3.6/site-packages && python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN --junit-xml /result/{}.xml {}'.format(mod_name, mod)], shell=True)
     subprocess.call(['export PYTHONPATH=/usr/lib64/az/lib/python3.6/site-packages && python3 -m pytest -x -v --boxed -p no:warnings --log-level=WARN --junit-xml /result/{}.xml -n auto {}'.format(mod_name, mod)], shell=True)
+
+
