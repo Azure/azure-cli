29dd577d36a9d80882de1316d753aeeb4c3c0c25
Xiaojian Xu <xiaojxu@microsoft.com>
2020-07-09 14:07:22 +0800

    add __init__.py in azure-cli

--- src/azure-cli/azure/cli/__init__.py
+++ src/azure-cli/azure/cli/__init__.py
@@ -2,4 +2,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+__author__ = "Microsoft Corporation <python@microsoft.com>"
+__version__ = "2.8.0"
