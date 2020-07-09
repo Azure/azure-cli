6558419386422cc5e48a6665a8d6de5192200e0b
Bin Ma <bim@microsoft.com>
2020-07-09 15:47:20 +0800

    Add license

--- src/azure-cli/azure/cli/command_modules/keyvault/vendored_sdks/challenge_auth_policy.py
+++ src/azure-cli/azure/cli/command_modules/keyvault/vendored_sdks/challenge_auth_policy.py
@@ -1,7 +1,7 @@
-# ------------------------------------
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-# ------------------------------------
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 """Policy implementing Key Vault's challenge authentication protocol.
 
 Normally the protocol is only used for the client's first service request, upon which:
