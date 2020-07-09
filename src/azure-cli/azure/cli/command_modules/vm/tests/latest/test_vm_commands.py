166478e449d9e30390a322785f9db2a33e4c5a4c
Feiyue Yu <fey@microsoft.com>
2020-07-09 16:15:15 +0800

    upgrade VMSS API version to 2020-06-01

--- src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_commands.py
@@ -3308,6 +3308,22 @@ class VMDiskEncryptionTest(ScenarioTest):
         self.cmd('vm encryption show -g {rg} -n {vm}', checks=[self.check('disks[0].statuses[0].code', 'EncryptionState/encrypted')])
         self.cmd('vm encryption disable -g {rg} -n {vm}')
 
+    # @ResourceGroupPreparer(name_prefix='cli_test_vm_encryption_at_host_', location='westus')
+    # def test_vm_encryption_at_host(self, resource_group):
+    #     self.kwargs.update({
+    #         'vm': 'vm1',
+    #         'vmss': 'vmss1'
+    #     })
+    #
+    #     self.cmd('vm create -g {rg} -n {vm} --image centos --generate-ssh-keys --nsg-rule NONE --encryption-at-host')
+    #     self.cmd('vm show -g {rg} -n {vm}', checks=[
+    #         self.check('securityProfile.encryptionAtHost', True)
+    #     ])
+    #     self.cmd('vmss create -g {rg} -n {vmss} --image centos --generate-ssh-keys --encryption-at-host')
+    #     self.cmd('vmss show -g {rg} -n {vmss}', checks=[
+    #
+    #     ])
+
 
 @api_version_constraint(ResourceType.MGMT_COMPUTE, min_api='2017-03-30')
 class VMSSRollingUpgrade(ScenarioTest):
