933a60f2721873838eb2f7b3795e03e9c564735d
Arik Riklin <arik.riklin@microsoft.com>
2020-07-09 18:26:46 +0300

    fix final4

--- src/azure-cli/azure/cli/command_modules/security/tests/latest/test_adaptive_application_controls.py
+++ src/azure-cli/azure/cli/command_modules/security/tests/latest/test_adaptive_application_controls.py
@@ -12,10 +12,10 @@ class SecurityCenterAdaptiveadaptiveApplicationControlsTests(ScenarioTest):
     @AllowLargeResponse()
     def test_adaptive_application_controls(self):
 
-        adaptive_application_controls = self.cmd('az security adaptive_application_controls list').get_output_in_json()
+        adaptive_application_controls = self.cmd('az security adaptive-application-controls list').get_output_in_json()
 
         assert len(adaptive_application_controls) >= 0
 
-        adaptive_application_controls = self.cmd('az security adaptive_application_controls show --group-name GROUP1').get_output_in_json()
+        adaptive_application_controls = self.cmd('az security adaptive-application-controls show --group-name GROUP1').get_output_in_json()
 
         assert len(adaptive_application_controls) >= 0
