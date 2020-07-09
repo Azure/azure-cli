fe8e1e0cfae4d032273fd9eb07229df41c2d93db
Bin Ma <bim@microsoft.com>
2020-07-09 15:35:19 +0800

    Add license

--- src/azure-cli/azure/cli/command_modules/keyvault/vendored_sdks/http_challenge.py
+++ src/azure-cli/azure/cli/command_modules/keyvault/vendored_sdks/http_challenge.py
@@ -1,7 +1,7 @@
-# ------------------------------------
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-# ------------------------------------
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 try:
     import urllib.parse as parse
 except ImportError:
