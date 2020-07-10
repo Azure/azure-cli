d42631bcaaf51eeabe9929cc42db433d4994bea2
Feiyue Yu <fey@microsoft.com>
2020-07-10 15:45:51 +0800

    email

--- scripts/live_test/sendemail.py
+++ scripts/live_test/sendemail.py
@@ -5,7 +5,6 @@
 
 import sys
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
 
 SENDGRID_KEY = sys.argv[1]
 BUILD_ID = sys.argv[2]
@@ -14,6 +13,7 @@ USER_BRANCH = sys.argv[4]
 USER_TARGET = sys.argv[5]
 USER_LIVE = sys.argv[6]
 ARTIFACT_DIR = sys.argv[7]
+REQUESTED_FOR_EMAIL = sys.argv[8]
 
 
 def main():
@@ -25,15 +25,40 @@ def main():
     print(USER_TARGET)
     print(USER_LIVE)
     print(ARTIFACT_DIR)
-    message = Mail(
-        from_email='azclibot@microsoft.com',
-        to_emails='AzPyCLI@microsoft.com',
-        subject='Test results of Azure CLI',
-        html_content=get_content())
+    print(REQUESTED_FOR_EMAIL)
+
+    # message = Mail(
+    #     from_email='azclibot@microsoft.com',
+    #     to_emails='AzPyCLI@microsoft.com',
+    #     subject='Test results of Azure CLI',
+    #     html_content=get_content())
+    data = {
+        "personalizations": [
+            {
+                "to": [
+                    {
+                        "email": REQUESTED_FOR_EMAIL
+                    }
+                ],
+                "subject": "Test results of Azure CLI"
+            }
+        ],
+        "from": {
+            "email": "azclibot@microsoft.com"
+        },
+        "content": [
+            {
+                "type": "text/html",
+                "value": get_content()
+            }
+        ]
+    }
+    if USER_TARGET == '' and USER_REPO == 'https://github.com/Azure/azure-cli.git' and USER_LIVE == '--live':
+        data['personalizations'][0]['to'].append({'email': 'AzPyCLI@microsoft.com'})
     try:
         sendgrid_key = sys.argv[1]
         sg = SendGridAPIClient(sendgrid_key)
-        response = sg.send(message)
+        response = sg.send(data)
         print(response.status_code)
         print(response.body)
         print(response.headers)
