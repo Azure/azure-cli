5ddc3fb74c294a17cc9c0aabbbe94925542e95ea
Houk <62928370+houk-ms@users.noreply.github.com>
2020-07-13 13:30:53 +0800

    Update src/azure-cli/azure/cli/command_modules/vm/_params.py
    Co-authored-by: Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>

--- src/azure-cli/azure/cli/command_modules/vm/_params.py
+++ src/azure-cli/azure/cli/command_modules/vm/_params.py
@@ -514,7 +514,7 @@ def load_arguments(self, _):
         c.argument('name', name_arg_type)
         c.argument('nat_backend_port', default=None, help='Backend port to open with NAT rules. Defaults to 22 on Linux and 3389 on Windows.')
         c.argument('single_placement_group', arg_type=get_three_state_flag(), help="Limit the scale set to a single placement group."
-                   " See https://docs.microsoft.com/azure/virtual-machine-scale-sets/virtual-machine-scale-sets-placement-groups for details, default to false", default=False)
+                   " See https://docs.microsoft.com/azure/virtual-machine-scale-sets/virtual-machine-scale-sets-placement-groups for details. Default to false", default=False)
         c.argument('platform_fault_domain_count', type=int, help='Fault Domain count for each placement group in the availability zone', min_api='2017-12-01')
         c.argument('vmss_name', name_arg_type, id_part=None, help='Name of the virtual machine scale set.')
         c.argument('instance_count', help='Number of VMs in the scale set.', type=int)
