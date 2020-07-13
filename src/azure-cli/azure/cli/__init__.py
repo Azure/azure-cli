13fe8e236d04a04b235f9696f5b83159f3023014
Xiaojian Xu <xiaojxu@microsoft.com>
2020-07-13 14:48:59 +0800

    fix another linter error

--- src/azure-cli/azure/cli/__init__.py
+++ src/azure-cli/azure/cli/__init__.py
@@ -2,7 +2,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
-
 __author__ = "Microsoft Corporation <python@microsoft.com>"
 __version__ = "2.9.1"
