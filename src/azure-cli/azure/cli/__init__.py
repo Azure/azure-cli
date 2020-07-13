09b7101ea4c6308cf3f7568a754e671e8276c387
Xiaojian Xu <xiaojxu@microsoft.com>
2020-07-13 13:44:38 +0800

    init pep420 change

--- src/azure-cli/azure/cli/__init__.py
+++ src/azure-cli/azure/cli/__init__.py
@@ -2,13 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-'''The Azure Command-line tool.
-
-This tools provides a command-line interface to Azure's management and storage
-APIs.
-'''
-import pkg_resources
-pkg_resources.declare_namespace(__name__)
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
 __author__ = "Microsoft Corporation <python@microsoft.com>"
 __version__ = "2.9.1"
