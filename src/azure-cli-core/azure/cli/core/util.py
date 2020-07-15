2bcf7b14225e951458e5d2c6c8e819755b333a09
Feiyue Yu <fey@microsoft.com>
2020-07-15 10:30:45 +0800

    fix a bug; help

--- src/azure-cli-core/azure/cli/core/util.py
+++ src/azure-cli-core/azure/cli/core/util.py
@@ -599,13 +599,14 @@ def get_default_admin_username():
         username = getpass.getuser()
     except KeyError:
         username = None
+    # A list of reserved names that cannot be used as admin username of VM
     disallowed_user_names = [
         "administrator", "admin", "user", "user1", "test", "user2",
         "test1", "user3", "admin1", "1", "123", "a", "actuser", "adm",
         "admin2", "aspnet", "backup", "console", "guest",
         "owner", "root", "server", "sql", "support", "support_388945a0",
         "sys", "test2", "test3", "user4", "user5"]
-    if username.lower() in disallowed_user_names:
+    if username is None or username.lower() in disallowed_user_names:
         username = 'AzureUser'
     return username
 
