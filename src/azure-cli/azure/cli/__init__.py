3687e19a8f13cf9c8b9190a76c5733c3f11aa08f
Xiaojian Xu <xiaojxu@microsoft.com>
2020-07-16 17:34:24 +0800

    fix merge conflict

--- src/azure-cli/azure/cli/__init__.py
+++ src/azure-cli/azure/cli/__init__.py
@@ -2,9 +2,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-<<<<<<< HEAD
-__author__ = "Microsoft Corporation <python@microsoft.com>"
-__version__ = "2.9.1"
-=======
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
->>>>>>> elegant way to handle __main__.py
