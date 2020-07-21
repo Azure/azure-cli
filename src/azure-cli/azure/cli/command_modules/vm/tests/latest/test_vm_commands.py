cd8a9b071d93848e91bdba5bcde6dbaaf1206a2d
Feiyue Yu <fey@microsoft.com>
2020-07-21 10:23:17 +0800

    test

--- src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_commands.py
@@ -75,6 +75,7 @@ class VMImageListThruServiceScenarioTest(ScenarioTest):
 
 class VMOpenPortTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_open_port')
     def test_vm_open_port(self, resource_group):
 
@@ -216,6 +217,7 @@ class VMImageShowScenarioTest(ScenarioTest):
 
 class VMGeneralizeScenarioTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_generalize_vm')
     def test_vm_generalize(self, resource_group):
 
@@ -238,6 +240,7 @@ class VMGeneralizeScenarioTest(ScenarioTest):
             self.check('storageProfile.zoneResilient', None)
         ])
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_generalize_vm')
     def test_vm_capture_zone_resilient_image(self, resource_group):
 
@@ -263,6 +266,7 @@ class VMGeneralizeScenarioTest(ScenarioTest):
 
 class VMWindowsLicenseTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_windows_license_type')
     def test_vm_windows_license_type(self, resource_group):
         self.kwargs.update({
@@ -452,6 +456,7 @@ class VMImageWithPlanTest(ScenarioTest):
 
 class VMCreateFromUnmanagedDiskTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_vm_from_unmanaged_disk')
     def test_vm_create_from_unmanaged_disk(self, resource_group):
         # create a vm with unmanaged os disk
@@ -481,6 +486,7 @@ class VMCreateFromUnmanagedDiskTest(ScenarioTest):
 
 class VMCreateWithSpecializedUnmanagedDiskTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_vm_with_specialized_unmanaged_disk')
     def test_vm_create_with_specialized_unmanaged_disk(self, resource_group):
 
@@ -533,6 +539,7 @@ class VMCreateWithSpecializedUnmanagedDiskTest(ScenarioTest):
 
 class VMAttachDisksOnCreate(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer()
     def test_vm_create_by_attach_os_and_data_disks(self, resource_group):
         # the testing below follow a real custom's workflow requiring the support of attaching data disks on create
@@ -566,6 +573,7 @@ class VMAttachDisksOnCreate(ScenarioTest):
             self.check('storageProfile.osDisk.diskSizeGb', 100)
         ])
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer()
     def test_vm_create_by_attach_unmanaged_os_and_data_disks(self, resource_group):
         # creating a vm
@@ -587,7 +595,7 @@ class VMAttachDisksOnCreate(ScenarioTest):
 class VMOSDiskSize(ScenarioTest):
 
     @ResourceGroupPreparer(name_prefix='cli_test_os_disk_size')
-    @AllowLargeResponse()
+    @AllowLargeResponse(99999)
     def test_vm_set_os_disk_size(self, resource_group):
         # test unmanaged disk
         self.kwargs.update({'sa': self.create_random_name(prefix='cli', length=12)})
@@ -903,6 +911,7 @@ class VMSimulateEvictionScenarioTest(ScenarioTest):
 
 class VMNoWaitScenarioTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_vm_no_wait')
     def test_vm_create_no_wait(self, resource_group):
 
@@ -1021,6 +1030,7 @@ class ComputeListSkusScenarioTest(ScenarioTest):
 
 class VMExtensionScenarioTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_vm_extension')
     def test_vm_extension(self, resource_group):
 
@@ -1081,7 +1091,7 @@ class VMExtensionScenarioTest(ScenarioTest):
         self.cmd('vm extension delete --resource-group {rg} --vm-name {vm} --name {ext_name}')
 
     @ResourceGroupPreparer(name_prefix='cli_test_vm_extension_with_id_')
-    @AllowLargeResponse()
+    @AllowLargeResponse(99999)
     def test_vm_extension_with_id(self, resource_group):
         self.kwargs.update({
             'vm': 'vm1'
@@ -1141,6 +1151,7 @@ class VMExtensionImageSearchScenarioTest(LiveScenarioTest):
 
 class VMCreateUbuntuScenarioTest(ScenarioTest):
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_vm_create_ubuntu')
     def test_vm_create_ubuntu(self, resource_group, resource_group_location):
 
@@ -1290,6 +1301,7 @@ class VMMonitorTestDefault(ScenarioTest):
             replay_processors=[TimeSpanProcessor(TIMESPANTEMPLATE)]
         )
 
+    @AllowLargeResponse(99999)
     @ResourceGroupPreparer(name_prefix='cli_test_vm_create_with_monitor', location='eastus')
     def test_vm_create_with_monitor(self, resource_group):
 
