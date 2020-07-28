a1a80bae0cc2408ecfe8bacfb78e048320f2e03a
shiyue <shiyuezju@gmail.com>
2020-07-28 15:25:10 +0800

    query recommend results through web API

--- src/azure-cli/azure/cli/command_modules/next/custom.py
+++ src/azure-cli/azure/cli/command_modules/next/custom.py
@@ -3,20 +3,37 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+import json
 from knack.util import CLIError
-import subprocess
+
+
+def _get_api_url():
+    return "http://azure-cli.azurewebsites.net/orchestrators/recommendation"
 
 
 def _get_last_cmd():
     '''Get last executed command from local log files'''
-    pass
+    return "vm create", "-s,-g"
 
 
-def _get_recommend_from_api(last_cmd, last_param):
+def _get_recommend_from_api(last_cmd, last_param, request_type, top_num=5, extra_data=None):
     '''query next command from web api'''
-    nx_cmd = "role assignment create"
-    nx_param = "--role,--assignee,--scope"
-    return nx_cmd, nx_param
+    import requests
+    url = _get_api_url()
+    payload = {
+        "command": last_cmd,
+        "param": last_param,
+        "extra_data": extra_data,
+        "type": request_type,
+        "top_num": top_num
+    }
+    response = requests.post(url, json.dumps(payload))
+    if response.status_code != 200:
+        raise CLIError("Failed to connect to '{}' with status code '{}' and reason '{}'".format(
+            url, response.status_code, response.reason))
+    recommends = response.content.decode("utf-8")["data"]
+
+    return recommends
 
 
 def _send_feedback():
@@ -26,7 +43,12 @@ def _send_feedback():
 def handle_next(cmd):
     option = int(input(
         "What kind of recommendation do you want? (1.operation 2.command 3.resource 4.service 5.mix): "))
-    nx_cmd, nx_param = _get_recommend_from_api("", "")
+    last_cmd, last_param = _get_last_cmd()
+    recommends = _get_recommend_from_api(last_cmd, last_param, option)
+    if not recommends:
+        raise CLIError("Failed to get recommend for '{}'.".format(last_cmd))
+    nx_cmd = recommends[0]["command"]
+    nx_param = recommends[0]["param"]
     print("az {}".format(nx_cmd))
     feedback = input("Does it help for you? (y/n): ")
     doit = input("Do you want to do it now? (y/n): ")
@@ -34,7 +56,6 @@ def handle_next(cmd):
     if doit not in ["y", "yes", "Y", "Yes", "YES"]:
         ret = {"result": "Thank you for your feedback"}
         return ret
-
 
     nx_params = [item[2:] for item in nx_param.split(',')]
     for param in nx_params:
@@ -45,6 +66,5 @@ def handle_next(cmd):
                                             commands_loader_cls=cmd.cli_ctx.commands_loader_cls,
                                             help_cls=cmd.cli_ctx.help_cls)
     args = ["version"]
-    ret =  invocation.execute(args)
+    ret = invocation.execute(args)
     return ret
-
