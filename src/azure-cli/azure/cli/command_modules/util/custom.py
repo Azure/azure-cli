230457ab392b53a42ae0415e7f6ba757d79cee73
Feng Zhou <fenzhou@microsoft.com>
2020-07-30 15:03:01 +0800

    add az upgrade

--- src/azure-cli/azure/cli/command_modules/util/custom.py
+++ src/azure-cli/azure/cli/command_modules/util/custom.py
@@ -27,3 +27,29 @@ def show_version(cmd):  # pylint: disable=unused-argument
     from azure.cli.core.util import get_az_version_json
     versions = get_az_version_json()
     return versions
+
+
+def upgrade_version(cmd):
+    from azure.cli.core._environment import _ENV_AZ_INSTALLER
+    import subprocess
+    # TODO check whether it's already the latest version
+    try:
+        installer = os.getenv(_ENV_AZ_INSTALLER)
+        if installer == 'DEB':
+            subprocess.call('sudo apt-get update && sudo apt-get install --only-upgrade -y azure-cli', shell=True)
+        elif installer == 'RPM':
+            subprocess.call('sudo yum update -y azure-cli', shell=True)
+        elif installer == 'HOMEBREW':
+            subprocess.call('brew update && brew upgrade -y azure-cli', shell=True)
+        elif installer == 'PIP':
+            subprocess.call('pip install --upgrade azure-cli', shell=True)
+        elif installer == 'DOCKER':
+            logger.warning('Exit the container to pull latest image with docker pull mcr.microsoft.com/azure-cli or pip install --upgrade azure-cli in this container')
+        elif installer == 'MSI':
+            # TODO put the script in a storage account, download it and store in a tmp dir
+            subprocess.call('powershell.exe "C:\\upgrade.ps1"', shell=True)
+            # logger.warning('Update with the latest MSI https://aka.ms/installazurecliwindows')
+        else:
+            logger.warning('Not able to upgrade automatically. Instructions can be found at https://docs.microsoft.com/en-us/cli/azure/install-azure-cli')
+    except Exception:  # pylint: disable=broad-except
+        pass
---
+++ src/azure-cli/azure/cli/command_modules/util/upgrade.ps1
@@ -0,0 +1,11 @@
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process powershell -Verb runAs -ArgumentList $arguments
+    Break
+}
+
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi;
+Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet';
+Remove-Item .\AzureCLI.msi
+Write-Output "Upgrade Complete!"
