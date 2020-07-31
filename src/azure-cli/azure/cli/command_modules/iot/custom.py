47b203847bf74e08c3536c8a2b3e10ad69b78d70
Teng Lu <telu@microsoft.com>
2020-07-31 12:05:03 -0700

    [IoTCentral] CLI: App delete operation should be long running operation

--- src/azure-cli/azure/cli/command_modules/iot/custom.py
+++ src/azure-cli/azure/cli/command_modules/iot/custom.py
@@ -1072,7 +1072,7 @@ def iot_central_app_get(client, app_name, resource_group_name=None):
     return client.apps.get(resource_group_name, app_name)
 
 
-def iot_central_app_delete(client, app_name, resource_group_name, no_wait=False):
+def iot_central_app_delete(cmd, client, app_name, resource_group_name, no_wait=False):
     if no_wait:
         return client.apps.delete(resource_group_name, app_name)
     result = LongRunningOperation(cmd.cli_ctx)(client.apps.delete(resource_group_name, app_name))
