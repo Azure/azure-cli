aba32fc1414b1f05e45ea8077477b5610eca4f1d
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2020-07-31 13:45:38 +0800

    [Extension] Support automatically installing an extension if the extension of a command is not installed (#14478)
    * dynamic extension install poc
    
    * fix cli_ctx
    
    * continue run with subprocess
    
    * add config
    
    * donwload remote index
    
    * check close matches first
    
    * change config name
    
    * add option to turn off dynamic install
    
    * add option to turn off dynamic install
    
    * fix when cli_ctx is None
    
    * add no prompt msg
    
    * modify message
    
    * default to no
    
    * refactor error msg
    
    * fix style
    
    * resolve UX comments
    
    * make changes backward compatible
    
    * add test
    
    * fix shell
    
    * fix style

--- src/azure-cli/azure/cli/command_modules/extension/__init__.py
+++ src/azure-cli/azure/cli/command_modules/extension/__init__.py
@@ -39,12 +39,12 @@ class ExtensionCommandsLoader(AzCommandsLoader):
         extension_custom = CliCommandType(operations_tmpl='azure.cli.command_modules.extension.custom#{}')
 
         with self.command_group('extension', extension_custom) as g:
-            g.command('add', 'add_extension', confirmation=ext_add_has_confirmed, validator=validate_extension_add)
+            g.command('add', 'add_extension_cmd', confirmation=ext_add_has_confirmed, validator=validate_extension_add)
             g.command('remove', 'remove_extension')
             g.command('list', 'list_extensions')
             g.show_command('show', 'show_extension')
             g.command('list-available', 'list_available_extensions', table_transformer=transform_extension_list_available)
-            g.command('update', 'update_extension')
+            g.command('update', 'update_extension_cmd')
 
         return self.command_table
 
