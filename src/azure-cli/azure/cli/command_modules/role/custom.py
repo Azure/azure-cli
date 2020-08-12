c80d21400ffdb98547204dd9ac0b43f14cd143d5
Xiaojian Xu <xiaojxu@microsoft.com>
2020-08-12 16:19:08 +0800

    [RBAC] az ad group create: support specify description when creating a group (#14668)
    * [RBAC] az ad group create: support specify description when creating a group
    
    * fix sytle error

--- src/azure-cli/azure/cli/command_modules/role/custom.py
+++ src/azure-cli/azure/cli/command_modules/role/custom.py
@@ -664,7 +664,7 @@ def get_user_member_groups(cmd, upn_or_object_id, security_enabled_only=False):
     return [{'objectId': x, 'displayName': stubs.get(x)} for x in results]
 
 
-def create_group(cmd, display_name, mail_nickname, force=None):
+def create_group(cmd, display_name, mail_nickname, force=None, description=None):
     graph_client = _graph_client_factory(cmd.cli_ctx)
 
     # workaround to ensure idempotent even AAD graph service doesn't support it
@@ -678,8 +678,10 @@ def create_group(cmd, display_name, mail_nickname, force=None):
                 raise CLIError(err.format(', '.join([x.object_id for x in matches])))
             logger.warning('A group with the same display name and mail nickname already exists, returning.')
             return matches[0]
-    group = graph_client.groups.create(GroupCreateParameters(display_name=display_name,
-                                                             mail_nickname=mail_nickname))
+        group_create_parameters = GroupCreateParameters(display_name=display_name, mail_nickname=mail_nickname)
+        if description is not None:
+            group_create_parameters.additional_properties = {'description': description}
+    group = graph_client.groups.create(group_create_parameters)
 
     return group
 
