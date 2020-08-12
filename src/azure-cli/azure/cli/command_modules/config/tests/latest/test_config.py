ac8c1b5b32fecee428746b32b101dbee5423bb5c
bquantump <53361486+bquantump@users.noreply.github.com>
2020-08-12 13:15:56 -0700

    WIP: Fix config test

--- src/azure-cli/azure/cli/command_modules/config/tests/latest/test_config.py
+++ src/azure-cli/azure/cli/command_modules/config/tests/latest/test_config.py
@@ -27,8 +27,8 @@ class ConfigTest(ScenarioTest):
         original_path = os.getcwd()
         os.chdir(tempdir)
         print("Using temp dir: {}".format(tempdir))
-
-        global_test_args = {"source": os.path.expanduser(os.path.join('~', '.azure', 'config')), "flag": ""}
+        os.getenv('AZURE_CONFIG_DIR', None) or os.path.expanduser(os.path.join('~', '.azure'))
+        global_test_args = {"source": config, "flag": ""}
         local_test_args = {"source": os.path.join(tempdir, '.azure', 'config'), "flag": " --local"}
 
         for args in (global_test_args, local_test_args):
