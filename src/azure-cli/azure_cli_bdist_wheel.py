acbdbc6ac76656f98a50852e5cae57fc6d419477
Xiaojian Xu <xiaojxu@microsoft.com>
2020-08-13 14:02:22 +0800

    fix style error

--- src/azure-cli/azure_cli_bdist_wheel.py
+++ src/azure-cli/azure_cli_bdist_wheel.py
@@ -1,8 +1,8 @@
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
-#--------------------------------------------------------------------------
+# --------------------------------------------------------------------------
 
 from distutils.command.build_py import build_py
 
