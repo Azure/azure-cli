c5ab10e99b079f6a4504f2c7f8fba7f005c3678c
Edmund-Shi <shiyuezju@gmail.com>
2020-08-14 18:07:58 +0800

    add license info

--- src/azure-cli-core/azure/cli/core/commands/recommend.py
+++ src/azure-cli-core/azure/cli/core/commands/recommend.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from knack.log import get_logger
 from knack.util import todict
 from knack import events
