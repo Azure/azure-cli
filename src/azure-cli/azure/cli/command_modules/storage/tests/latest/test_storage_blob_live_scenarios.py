a72cf1ba1c3f35ae09657a3573a71fe5ddbdfa62
Feiyue Yu <fey@microsoft.com>
2020-08-14 16:13:18 +0800

    update

--- src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_blob_live_scenarios.py
+++ src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_blob_live_scenarios.py
@@ -5,7 +5,6 @@
 
 import os
 from datetime import datetime, timedelta
-
 from azure.cli.testsdk import (LiveScenarioTest, ResourceGroupPreparer, StorageAccountPreparer,
                                JMESPathCheck, JMESPathCheckExists, NoneCheck, api_version_constraint)
 from azure.cli.core.profiles import ResourceType
