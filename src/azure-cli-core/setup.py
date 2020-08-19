7df19e852acd3f516d6da29c0c5cbbc2f0f247a3
Dania Etienne <etienne.dania@gmail.com>
2020-08-19 04:23:49 -0400

    {ARM} Bump azure-mgmt-resource SDK from 10.1.0 to 10.2.0 (#14678)
    * {Docs} Remove stale reference in README to closed issue about extensions (#12771)
    
    * Preliminary changes made to add support for template-specs
    
    * Reconfiguration
    
    * validate function error rectified
    
    * validate command works for template-uri
    
    * Template-specs deployment works at resource-group level
    
    * Group deployment updated to function with 2020 SDK
    
    * validate with template specs added
    
    * Management scope and tenant scope errors fixed
    
    * Template-Specs Deployment Added
    
    * Template-Specs Deployment Added
    
    * Updated test cases and removed unnecessary comments
    
    * Removed test cases from launch.json
    
    * Uncommented test case
    
    * Cleaned up repo
    
    * Cleaned up repo
    
    * Style checks complete, template_spec parameter placed at the end, time stamp added to deployment_name, excess file removed in commit, launch,json reverted
    
    * Fixed failing CLI Check Test
    
    * Fix getting resource API version in ARM cmdlt
    
    Latest swagger updates added the 'defaultApiVersion' property to the GET resource providers response.
    The old code was trying to use the default API version and only than fall back to the API versions specified in the manifest.
    This was wrong because:
    1. default API version was not part of the response (until the latest SDK update), so this code was not doing anything
    2. default API versions can be null
    3. default API version is mostly used for internal purposes and must be included in the list of available API versions.
    
    This change address this issue.
    
    * reset
    
    * @eladperets Fix getting resource API version in ARM cmdlt
    
    * requirements updated to 10.2.0
    
    * reverted changes to launch.json
    
    * Remove useless object inheritance
    
    * @eladperets Fix getting resource API version in ARM cmdlt
    
    Co-authored-by: Daniel Miller <daniel.keegan.miller@gmail.com>
    Co-authored-by: Azure CLI Team <AzPyCLI@microsoft.com>
    Co-authored-by: Dania Etienne <daetienn@microsoft.com>
    Co-authored-by: Elad Perets <elpere@microsoft.com>

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -67,7 +67,7 @@ DEPENDENCIES = [
     'requests~=2.22',
     'six~=1.12',
     'pkginfo>=1.5.0.1',
-    'azure-mgmt-resource==10.1.0',
+    'azure-mgmt-resource==10.2.0',
     'azure-mgmt-core==1.2.0'
 ]
 
