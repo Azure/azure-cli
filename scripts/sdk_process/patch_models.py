eeb68f98439665cf8afcd8f87c6526b5f1b91060
Bin Ma <bim@microsoft.com>
2020-08-19 11:02:54 +0800

    updated

--- scripts/sdk_process/patch_models.py
+++ scripts/sdk_process/patch_models.py
@@ -68,12 +68,9 @@ def solve_mro(models, track2=False):
     for models_module in models:
         models_path = models_module.__path__[0]
         _LOGGER.info("Working on %s", models_path)
-        if Path(models_path, "models_py3.py").exists():
-            if track2 and Path(models_path, '_models_py3.py').exists():
-                _LOGGER.info('Overwrite models for track2 module')
-            else:
-                _LOGGER.info("Skipping since already patched")
-                return
+        if track2 or Path(models_path, "models_py3.py").exists():
+            _LOGGER.info("Skipping since already patched")
+            return
 
         # Build the new files in a temp folder
         with tempfile.TemporaryDirectory() as temp_folder:
