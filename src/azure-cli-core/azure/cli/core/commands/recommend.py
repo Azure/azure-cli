5db2195f8436182a3055398d157bc4cde3a82703
shiyue <shiyuezju@gmail.com>
2020-08-21 13:54:19 +0800

    change argument name to query-example

--- src/azure-cli-core/azure/cli/core/commands/recommend.py
+++ src/azure-cli-core/azure/cli/core/commands/recommend.py
@@ -11,20 +11,20 @@ from azure.cli.core.commands.events import EVENT_INVOKER_PRE_LOAD_ARGUMENTS
 logger = get_logger(__name__)
 
 
-def register_global_query_recommend_argument(cli_ctx):
-    '''Register --query-recommend argument, and register handler
+def register_global_query_example_argument(cli_ctx):
+    '''Register --query-example argument, and register handler
     '''
 
-    def handle_recommend_parameter(cli, **kwargs):  # pylint: disable=unused-argument
+    def handle_example_parameter(cli, **kwargs):  # pylint: disable=unused-argument
         args = kwargs['args']
-        if args._query_recommend is not None:  # pylint: disable=protected-access
+        if args._query_example is not None:  # pylint: disable=protected-access
             cli_ctx.invocation.data['output'] = 'table'
 
             def analyze_output(cli_ctx, **kwargs):
                 tree_builder = TreeBuilder()
                 tree_builder.build(kwargs['event_data']['result'])
-                kwargs['event_data']['result'] = tree_builder.generate_recommend(
-                    args._query_recommend)  # pylint: disable=protected-access
+                kwargs['event_data']['result'] = tree_builder.generate_example(
+                    args._query_example)  # pylint: disable=protected-access
                 cli_ctx.unregister_event(
                     events.EVENT_INVOKER_FILTER_RESULT, analyze_output)
 
@@ -32,12 +32,12 @@ def register_global_query_recommend_argument(cli_ctx):
                 events.EVENT_INVOKER_FILTER_RESULT, analyze_output)
             cli_ctx.invocation.data['query_active'] = True
 
-    def register_query_recommend(cli, **kwargs):
+    def register_query_example(cli, **kwargs):
         from knack.experimental import ExperimentalItem
         commands_loader = kwargs.get('commands_loader')
         cmd_tbl = commands_loader.command_table
         experimental_info = ExperimentalItem(cli.local_context.cli_ctx,
-                                             object_type='parameter', target='_query_recommend')
+                                             object_type='parameter', target='_query_example')
         default_kwargs = {
             'help': 'Recommend JMESPath string for you. Currently this is a dummy parameter',
             'arg_group': 'Global',
@@ -46,19 +46,18 @@ def register_global_query_recommend_argument(cli_ctx):
             'experimental_info': experimental_info
         }
         for _, cmd in cmd_tbl.items():
-            cmd.add_argument('_query_recommend', *
-                             ['--query-recommend'], **default_kwargs)
+            cmd.add_argument('_query_example', *
+                             ['--query-example'], **default_kwargs)
 
     cli_ctx.register_event(
-        EVENT_INVOKER_PRE_LOAD_ARGUMENTS, register_query_recommend
+        EVENT_INVOKER_PRE_LOAD_ARGUMENTS, register_query_example
     )
     cli_ctx.register_event(
-        events.EVENT_INVOKER_POST_PARSE_ARGS, handle_recommend_parameter)
+        events.EVENT_INVOKER_POST_PARSE_ARGS, handle_example_parameter)
 
 
 class TreeBuilder:
-    '''Parse entry. Generate parse tree from json. And
-    then give recommendation
+    '''Parse entry. Generate parse tree from json. And then give examples.
     '''
 
     def __init__(self):
@@ -70,6 +69,6 @@ class TreeBuilder:
         '''
         self._data = data
 
-    def generate_recommend(self, keywords_list):  # pylint: disable=unused-argument, no-self-use
-        recommendations = []
-        return todict(recommendations)
+    def generate_example(self, keywords_list):  # pylint: disable=unused-argument, no-self-use
+        examples = []
+        return todict(examples)
