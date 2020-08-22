2394d3e585bf03343923c16ba56fca6ce9cea324
Meha Kaushik <mekaushi@microsoft.com>
2020-08-22 16:18:20 -0700

    nit

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -74,7 +74,7 @@ DEPENDENCIES = [
     'azure-mgmt-containerinstance~=1.4',
     'azure-mgmt-containerregistry==3.0.0rc14',
     'azure-mgmt-containerservice~=9.0.1',
-    'azure-mgmt-cosmosdb~=1.0.0,
+    'azure-mgmt-cosmosdb~=1.0.0',
     'azure-mgmt-datalake-analytics~=0.2.1',
     'azure-mgmt-datalake-store~=0.5.0',
     'azure-mgmt-datamigration~=0.1.0',
