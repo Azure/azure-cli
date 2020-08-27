fcfde950c090b2fdaae7d45c9d2f6fb2275b24b8
Xiaojian Xu <xiaojxu@microsoft.com>
2020-08-27 09:52:35 +0800

    remove local test code

--- scripts/pyinstaller/hooks/hook-azure.cli.py
+++ scripts/pyinstaller/hooks/hook-azure.cli.py
@@ -1,6 +1,9 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 import os
-import re
-import json
 import pkgutil
 from importlib import import_module
 
@@ -11,46 +14,12 @@ SRC_DIR = os.path.join(BASE_DIR, 'src')
 PATTERN = r'operations_tmpl=(?P<quote>[\'"])(?P<module>.+)#.+(?P=quote)'
 EXCLUDE_SUFFIX = ['.{}', '.operations']
 
-_hiddenimports = [] # collect_submodules('win32ctypes')
+_hiddenimports = []
 modules = []
 
 mods_ns_pkg = import_module('azure.cli.command_modules')
 command_modules = [modname for _, modname, _ in pkgutil.iter_modules(mods_ns_pkg.__path__)]
 for command_module in command_modules:
-    print(command_module)
     _hiddenimports.extend(collect_submodules('azure.cli.command_modules.{}'.format(command_module)))
 
-'''
-for root, dirs, files in os.walk(SRC_DIR):
-    for name in files:
-        if name.endswith('.py') and not name.startswith('test_'):
-            filepath = os.path.join(root, name)
-            try:
-                with open(filepath, 'r') as fp:
-                    content = fp.read()
-                    for match in re.finditer(PATTERN, content):
-                        module = match.group('module')
-                        for suffix in EXCLUDE_SUFFIX:
-                            if module.endswith(suffix):
-                                module = module[:-1*len(suffix)]
-                        if module.startswith('azure.multiapi'):
-                            module = 'azure.multiapi'
-                        if module.startswith('azure.cli.command_modules.storage'):
-                            continue
-                        if module not in modules:
-                            modules.append(module)
-            except UnicodeDecodeError as e:
-                print('ERROR: {}'.format(filepath))
-print(json.dumps(modules, indent=4))
-
-
-for module in modules:
-    if module == 'custom':
-        continue
-    print('handling {}'.format(module))
-    if module.endswith('.custom'):
-        _hiddenimports.append(module)
-    _hiddenimports.extend(collect_submodules(module))
-print('handle done')
-'''
 hiddenimports = _hiddenimports
--- scripts/pyinstaller/release/debian/pipeline.sh
+++ scripts/pyinstaller/release/debian/pipeline.sh
@@ -4,13 +4,8 @@
 # This script assumes the Azure CLI is installed and logged in.
 
 set -exv
-DISTRO='xenial'
-DISTRO_BASE_IMAGE=ubuntu:xenial
 : ${DISTRO:?"DISTRO is not set"}
 : ${DISTRO_BASE_IMAGE:?"DISTRO_BASE_IMAGE is not set"}
-
-BUILD_SOURCESDIRECTORY=/home/xiaojxu/code/pyinstaller/azure-cli
-BUILD_STAGINGDIRECTORY=/home/xiaojxu/code/pyinstaller/staging
 
 CLI_VERSION=`cat src/azure-cli/azure/cli/__main__.py | grep __version__ | sed s/' '//g | sed s/'__version__='// |  sed s/\"//g`
 
@@ -20,8 +15,5 @@ docker run \
            -e CLI_VERSION=$CLI_VERSION \
            -e CLI_VERSION_REVISION=1~$DISTRO \
            -e DEBIAN_FRONTEND=noninteractive \
-	   -it \
            $DISTRO_BASE_IMAGE \
-	   /bin/bash
-
-#	   /mnt/repo/scripts/pyinstaller/release/debian/build.sh
+	   /mnt/repo/scripts/pyinstaller/release/debian/build.sh
