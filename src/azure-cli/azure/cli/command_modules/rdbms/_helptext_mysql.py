f44cd0d35c78ec1bae52d316963c5517c4b4bf75
Raj Sellappan <rajsell@microsoft.com>
2020-08-30 21:13:38 -0700

    updating help text

--- src/azure-cli/azure/cli/command_modules/rdbms/_helptext_mysql.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_helptext_mysql.py
@@ -7,8 +7,52 @@
 from knack.help_files import helps  # pylint: disable=unused-import
 # pylint: disable=line-too-long, too-many-lines
 
-
 helps['mysql flexible-server'] = """
 type: group
 short-summary: Manage Azure Database for MySQL Flexible Servers.
+"""
+
+helps['mysql flexible-server create'] = """
+type: command
+short-summary: Creates a new Azure Database for MySQL Flexible Server.
+"""
+
+helps['mysql flexible-server list'] = """
+type: command
+short-summary: Provides a list of Azure Database for MySQL Flexible Server.
+"""
+
+helps['mysql flexible-server restore'] = """
+type: command
+short-summary: Restore a Azure Database for MySQL Flexible Server from the backup.
+"""
+
+helps['mysql flexible-server update'] = """
+type: command
+short-summary: Update a Azure Database for MySQL Flexible Server.
+"""
+
+helps['mysql flexible-server db'] = """
+type: group
+short-summary: Manage database for the Azure Database for MySQL Flexible Servers.
+"""
+
+helps['mysql flexible-server firewall-rule'] = """
+type: group
+short-summary: Manage firewall rules for the Azure Database for MySQL Flexible Servers.
+"""
+
+helps['mysql flexible-server parameter'] = """
+type: group
+short-summary: Manage server parameters for the Azure Database for MySQL Flexible Servers.
+"""
+
+helps['mysql flexible-server vnet-rule'] = """
+type: group
+short-summary: Manage Virtual Network rules for the Azure Database for MySQL Flexible Servers.
+"""
+
+helps['mysql flexible-server replica'] = """
+type: group
+short-summary: Manage replica servers for the Azure Database for MySQL Flexible Servers.
 """
--- src/azure-cli/azure/cli/command_modules/rdbms/_helptext_pg.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_helptext_pg.py
@@ -12,3 +12,38 @@ helps['postgres flexible-server'] = """
 type: group
 short-summary: Manage Azure Database for PostgreSQL Flexible Servers.
 """
+
+helps['postgres flexible-server create'] = """
+type: command
+short-summary: Creates a new Azure Database for PostgreSQL Flexible Server.
+"""
+
+helps['postgres flexible-server list'] = """
+type: command
+short-summary: Provides a list of Azure Database for PostgreSQL Flexible Server.
+"""
+
+helps['postgres flexible-server restore'] = """
+type: command
+short-summary: Restore a Azure Database for PostgreSQL Flexible Server from the backup.
+"""
+
+helps['postgres flexible-server update'] = """
+type: command
+short-summary: Update a Azure Database for PostgreSQL Flexible Server.
+"""
+
+helps['postgres flexible-server firewall-rule'] = """
+type: group
+short-summary: Manage firewall rules for the Azure Database for PostgreSQL Flexible Servers.
+"""
+
+helps['postgres flexible-server parameter'] = """
+type: group
+short-summary: Manage server parameters for the Azure Database for PostgreSQL Flexible Servers.
+"""
+
+helps['postgres flexible-server vnet-rule'] = """
+type: group
+short-summary: Manage Virtual Network rules for the Azure Database for PostgreSQL Flexible Servers.
+"""
