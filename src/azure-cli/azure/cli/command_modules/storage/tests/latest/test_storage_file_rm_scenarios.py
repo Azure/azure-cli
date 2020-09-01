839b2605258fa4f1ce3010a994af62b67c20e49b
Zunli Hu <zuh@microsoft.com>
2020-09-01 11:25:53 +0800

    fix style

--- src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_file_rm_scenarios.py
+++ src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_file_rm_scenarios.py
@@ -217,3 +217,19 @@ class StorageFileShareRmScenarios(StorageScenarioMixin, ScenarioTest):
             JMESPathCheck('[0].name', self.kwargs['share']),
             JMESPathCheck('accessTier', 'Hot')
         })
+
+    @ResourceGroupPreparer(name_prefix="cli_stats")
+    @StorageAccountPreparer(name_prefix="stats")
+    def test_storage_share_rm_with_stats(self, resource_group, storage_account):
+        account_info = self.get_account_info(group=resource_group, name=storage_account)
+        share_name = self.create_random_name(prefix='share', length=12)
+        self.cmd('storage share-rm create -n {} --storage-account {}'.format(share_name, storage_account))
+        result = self.cmd('storage share-rm stats -n {} --storage-account {}'.format(
+            share_name, storage_account)).output.strip('\n')
+        self.assertEqual(result, str(0))
+
+        local_file = self.create_temp_file(512, full_random=False)
+        self.storage_cmd('storage file upload --share-name {} --source "{}"', account_info, share_name, local_file)
+        result = self.cmd('storage share-rm stats -n {} --storage-account {}'.format(
+            share_name, storage_account)).output.strip('\n')
+        self.assertEqual(result, str(512 * 1024))
