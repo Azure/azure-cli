d0a943cbe1ba2af4137927f025a58d47b758b455
Zunli Hu <zuh@microsoft.com>
2020-09-03 09:53:23 +0800

    fix style

--- src/azure-cli/azure/cli/command_modules/storage/operations/azcopy.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/azcopy.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 from ..azcopy.util import AzCopy, client_auth_for_azcopy, login_auth_for_azcopy
 
 
-# pylint: disable=too-many-statements, too-many-locals, disable=unused-argument
+# pylint: disable=too-many-statements, too-many-locals, unused-argument
 def storage_copy(source, destination, put_md5=None, recursive=None, blob_type=None,
                  preserve_s2s_access_tier=None, content_type=None, follow_symlinks=None,
                  exclude_pattern=None, include_pattern=None, exclude_path=None, include_path=None, **kwargs):
