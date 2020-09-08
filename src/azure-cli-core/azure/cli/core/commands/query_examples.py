d1dcc42035b05149615e7c063208f59d76d12230
shiyue <shiyuezju@gmail.com>
2020-09-08 23:39:05 +0800

    Add more comments

--- src/azure-cli-core/azure/cli/core/commands/query_examples.py
+++ src/azure-cli-core/azure/cli/core/commands/query_examples.py
@@ -12,18 +12,20 @@ logger = get_logger(__name__)
 
 
 def register_global_query_examples_argument(cli_ctx):
-    '''Register --query-examples argument, and register handler
-    '''
+    """Register --query-examples argument, and register handler."""
 
     def handle_example_parameter(cli, **kwargs):  # pylint: disable=unused-argument
         args = kwargs['args']
+        # `query_examples` has been registered and `--query-examples` appers in the command args
         if hasattr(args, '_query_examples') and args._query_examples is not None:  # pylint: disable=protected-access
+            # change the default output format to table
             if cli_ctx.invocation.data['output'] == 'json':
                 cli_ctx.invocation.data['output'] = 'table'
 
             def analyze_output(cli_ctx, **kwargs):
-                tree_builder = TreeBuilder(cli.config)
+                tree_builder = QueryTreeBuilder(cli.config)
                 tree_builder.build(kwargs['event_data']['result'])
+                # replace the result with query examples
                 kwargs['event_data']['result'] = tree_builder.generate_examples(
                     args._query_examples, cli_ctx.invocation.data['output'])  # pylint: disable=protected-access
                 cli_ctx.unregister_event(
@@ -50,6 +52,7 @@ def register_global_query_examples_argument(cli_ctx):
 
         allow_list = cli.config.get('query', 'allow_list', "list,show").split(',')
         allow_list = [s.strip() for s in allow_list if s.strip()]  # remove empty string
+        # when allow_list="", do nothing
         if not allow_list:
             return
 
@@ -67,7 +70,7 @@ def register_global_query_examples_argument(cli_ctx):
         events.EVENT_INVOKER_POST_PARSE_ARGS, handle_example_parameter)
 
 
-class Example:
+class QueryExample:
     def __init__(self, query_str, help_str="", max_length=None):
         self._query_str = query_str
         self._help_str = help_str
@@ -91,7 +94,7 @@ class Example:
         return "{}\t{}".format(self._query_str, self._help_str)
 
 
-class TreeNode:
+class QueryTreeNode:
     def __init__(self, name, parent, under_array):
         self.is_dummy = False
         self._name = name
@@ -108,6 +111,7 @@ class TreeNode:
         self._data = data
 
     def _get_one_data(self):
+        """Try to get not None data."""
         if not self._data:
             return None
         for value in self._data:
@@ -116,14 +120,20 @@ class TreeNode:
         return None
 
     def get_help_str(self, help_type):
+        """Return help string based on help_type."""
         help_table = {
             'contains': "Display {} field that contains given string.".format(self._name),
-            'filter': "Display resources that satisify the condition.",
+            'filter': "Display resources that satisfy the condition.",
             'select': "Display value of {} field".format(self._name),
         }
         return help_table.get(help_type, '')
 
     def get_trace_to_array(self, inner_trace):
+        """
+        Get trace to the nearest array. Return None if no array found. This function return two values,
+        inner trace and outer trace. The inner trace is the trace inside the brackets and the outer
+        trace is the trace outside the brackets.
+        """
         if self._under_array:
             if self._parent:
                 outer_trace = '{}.{}'.format(self._parent.get_trace_to_root(), self._name)
@@ -143,6 +153,7 @@ class TreeNode:
         return None, None
 
     def get_trace_to_root(self):
+        """Return the trace string(aka, select string in JMESPath) to the root node."""
         if self._parent:
             if self._parent.is_dummy and not self._parent._under_array:  # pylint: disable=protected-access
                 trace_str = self._name
@@ -155,6 +166,7 @@ class TreeNode:
         return trace_str
 
     def get_filter_str(self):
+        """Return the kind of JMESPath that filters the results with some conditions."""
         outer_trace, inner_trace = self.get_trace_to_array('')
         if outer_trace is None or inner_trace is None:
             return None
@@ -165,6 +177,7 @@ class TreeNode:
         return filter_str
 
     def get_contains_str(self):
+        """Return the JMESPath that filter the results with a contains function."""
         outer_trace, inner_trace = self.get_trace_to_array('')
         if outer_trace is None or inner_trace is None:
             return None
@@ -175,20 +188,21 @@ class TreeNode:
         return contains_str
 
     def get_examples(self):
+        """The entry function for recommend examples for current node."""
         ans = []
         select_string = self.get_trace_to_root()
-        ans.append(Example(select_string, self.get_help_str('select')))
+        ans.append(QueryExample(select_string, self.get_help_str('select')))
         filter_str = self.get_filter_str()
         if filter_str:
-            ans.append(Example(filter_str, self.get_help_str('filter')))
+            ans.append(QueryExample(filter_str, self.get_help_str('filter')))
         contains_str = self.get_contains_str()
         if contains_str:
-            ans.append(Example(contains_str, self.get_help_str('contains')))
+            ans.append(QueryExample(contains_str, self.get_help_str('contains')))
         return ans
 
 
-class TreeBuilder:
-    '''Parse entry. Generate parse tree from json. And then give examples.'''
+class QueryTreeBuilder:
+    """Parse entry. Generate parse tree from json. And then give examples."""
 
     def __init__(self, config):
         self._root = None  # dummy root node
@@ -197,14 +211,13 @@ class TreeBuilder:
         self.update_config(config)
 
     def build(self, data):
-        '''Build a query tree with a given json file
-        :param str data: json format data
-        '''
-        self._root = self._do_parse('', [data], None)
+        """Build a query tree with a given json file."""
+        self._root = self._parse('', [data], None)
         if self._root:
             self._root.is_dummy = True
 
     def generate_examples(self, keywords_list, output_format):
+        """Generate JMESPath query string examples based on keyword_list."""
         examples = []
         match_list = self._get_matched_nodes(keywords_list)
         for node_name in match_list:
@@ -222,79 +235,87 @@ class TreeBuilder:
         self._config['max_examples'] = int(config.get('query', 'max_examples', '10'))
 
     def _get_matched_nodes(self, keywords_list):
+        """
+        Return a list of keys that match the user provided keywords.
+        Return all available keys if no keyword is provided.
+        """
         def name_match(pattern, line):
             return pattern.lower() in line.lower()
 
         match_list = []
+        # return all available key if no keyword is provided
         if not keywords_list:
             return self._all_nodes.keys()
+
         for pattern in keywords_list:
             for node_name in self._all_nodes:
                 if node_name not in match_list and name_match(pattern, node_name):
                     match_list.append(node_name)
-
         return match_list
 
-    def _do_parse(self, name, data, parent, under_array=False):
-        '''do parse for a single node
+    def _parse(self, name, data, parent, under_array=False):
+        """do parse for a single node
 
         :param str name:
-            Name of the node
+            Name of the node.
         :param list data:
-            All data in the json with the same depth and the same name
+            All data in the json with the same depth and the same name.
+            This field must be a list.
         :param TreeNode parent:
-            The parent node of current node. None if this is the root node
-        '''
+            The parent node of current node. None if this is the root node.
+        :param bool under_array:
+            True if the value of this JSON node is an array.
+        """
         if not data:
             return None
         if all(isinstance(d, list) for d in data):
-            node = self._do_parse_list(name, data, parent)
+            node = self._parse_list(name, data, parent)
         elif all(isinstance(d, dict) for d in data):
-            node = self._do_parse_dict(name, data, parent, under_array)
+            node = self._parse_dict(name, data, parent, under_array)
         elif any(isinstance(d, (dict, list)) for d in data):
             node = None  # inhomogeneous type
         else:
-            node = self._do_parse_leaf(name, data, parent, under_array)
+            node = self._parse_leaf(name, data, parent, under_array)
         return node
 
-    def _do_parse_list(self, name, data, parent):
+    def _parse_list(self, name, data, parent):
         flatten_data = []
         # flatten list of list to list of data
         for d in data:
             flatten_data.extend(d)
         if not flatten_data:
             return None
-        node = self._do_parse(name, flatten_data, parent, under_array=True)
+        node = self._parse(name, flatten_data, parent, under_array=True)
         return node
 
-    def _do_parse_leaf(self, name, data, parent, under_array):
-        node = TreeNode(name, parent, under_array)
+    def _parse_leaf(self, name, data, parent, under_array):
+        node = QueryTreeNode(name, parent, under_array)
         node.update_node_data(data)
         self._record_node(name, node)
         return node
 
-    def _do_parse_dict(self, name, data, parent, under_array):
-        node = TreeNode(name, parent, under_array)
+    def _parse_dict(self, name, data, parent, under_array):
+        node = QueryTreeNode(name, parent, under_array)
         all_keys = self._get_all_keys(data)
         for key in all_keys:
             values = self._get_not_none_values(data, key)
             if not values:  # all values are None
                 continue
-            child_node = self._do_parse(key, values, node, under_array=False)
+            child_node = self._parse(key, values, node, under_array=False)
             node.add_child(child_node)
         self._record_node(name, node)
         return node
 
     def _get_all_keys(self, data):  # pylint: disable=no-self-use
-        '''Get all keys in a list of dict'''
+        """Get all keys in a list of dict"""
         return set().union(*(d.keys() for d in data))
 
     def _get_not_none_values(self, data, key):  # pylint: disable=no-self-use
-        '''Get all not None values in a list of dict'''
+        """Get all not None values in a list of dict"""
         return [d.get(key) for d in data if d.get(key, None) is not None]
 
     def _record_node(self, name, node):
-        '''Recode name and node to a dict'''
+        """Add name and node to `self._all_nodes`"""
         if not node:
             return
         if name in self._all_nodes:
