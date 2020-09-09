3d1dd58e106c810e20f77a23f462df4662e1eaff
Alfredo Santamaria Gomez <alsantam@microsoft.com>
2020-09-09 00:27:51 -0700

    fix recordings after rebase with dv

--- src/azure-cli/azure/cli/command_modules/servicefabric/_sf_utils.py
+++ src/azure-cli/azure/cli/command_modules/servicefabric/_sf_utils.py
@@ -18,8 +18,9 @@ def _get_resource_group_by_name(cli_ctx, resource_group_name):
         return resource_client.get(resource_group_name)
     except Exception as ex:  # pylint: disable=broad-except
         azureError = getattr(ex, 'Azure Error', ex)
-        if azureError.error.error == 'ResourceGroupNotFound':
-            return None
+        if hasattr(azureError, 'error') and hasattr(azureError.error, 'error'):
+            if azureError.error.error == 'ResourceGroupNotFound':
+                return None
         raise
 
 
