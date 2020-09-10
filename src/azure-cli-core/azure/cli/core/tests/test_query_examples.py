9702eb5416bdbd8656276fe1e31b2368d1af2998
shiyue <shiyuezju@gmail.com>
2020-09-10 16:04:48 +0800

    add more unit tests

--- src/azure-cli-core/azure/cli/core/tests/test_query_examples.py
+++ src/azure-cli-core/azure/cli/core/tests/test_query_examples.py
@@ -6,6 +6,7 @@ import unittest
 import json
 
 from azure.cli.core.commands.query_examples import QueryTreeBuilder
+from azure.cli.core.commands.query_examples import QueryExample
 
 
 class MockConfig:
@@ -20,6 +21,10 @@ class MockConfig:
         if module != 'query':
             return default_value
         return self._config.get(field, default_value)
+
+    def set(self, field, value):
+        if field in self._config:
+            self._config[field] = value
 
 
 class TestQueryExamplesQueryTreeBuilder(unittest.TestCase):
@@ -115,6 +120,27 @@ class TestQueryExamplesQueryTreeBuilder(unittest.TestCase):
         self.assertIn('name.firstName', examples)
         self.assertIn('age', examples)
 
+        input_json = '''
+        {
+            "name": {
+                "firstName": "Ann",
+                "lastName": "King"
+            },
+            "age": 20,
+            "location":"us",
+            "state": "ABC",
+            "major": "CS"
+        }
+        '''
+        json_obj = json.loads(input_json)
+        max_examples = 3
+        mock_config.set('max_examples', max_examples)
+        builder = QueryTreeBuilder(mock_config)
+        builder.build(json_obj)
+        examples_list = builder.generate_examples(keyword_list, output_format)
+        self.assertEqual(len(examples_list), max_examples)
+        mock_config.set('max_examples', -1)
+
     def test_array_recommend(self):
         mock_config = MockConfig()
         keyword_list = []
@@ -145,6 +171,51 @@ class TestQueryExamplesQueryTreeBuilder(unittest.TestCase):
         self.assertIn("[?age=='20']", examples)
         self.assertIn('[].friends[].Name', examples)
         self.assertIn("[].friends[?Name=='Bob']", examples)
+
+    def test_dict_recommend_with_keywords(self):
+        mock_config = MockConfig()
+        keyword_list = ['name']
+        output_format = 'json'
+
+        input_json = '''
+        {
+            "name": {
+                "firstName": "Ann"
+            },
+            "age": 20
+        }
+        '''
+        json_obj = json.loads(input_json)
+        builder = QueryTreeBuilder(mock_config)
+        builder.build(json_obj)
+        examples_list = builder.generate_examples(keyword_list, output_format)
+        examples = self.examples_to_dict(examples_list)
+        self.assertIn('name', examples)
+        self.assertIn('name.firstName', examples)
+        self.assertNotIn('age', examples)
+        keyword_list = ['age']
+        examples_list = builder.generate_examples(keyword_list, output_format)
+        examples = self.examples_to_dict(examples_list)
+        self.assertNotIn('name', examples)
+        self.assertNotIn('name.firstName', examples)
+        self.assertIn('age', examples)
+
+    def test_query_examples(self):
+        query_str = "[?contains(name,'Ann')==`true`]"
+        help_str = 'This is the help message.'
+        example = QueryExample(query_str, help_str)
+        result_query_str = example._asdict().get("query string")
+        self.assertEqual(result_query_str, "[?contains(name,'Ann')==\\`true\\`]")
+
+        query_str = "This.is.a.very.very.very.long.query.string"
+        help_str = "This is a very very very long help message."
+        max_length = 10
+        example = QueryExample(query_str, help_str)
+        example.set_max_length(max_length, max_length)
+        result_query_str = example._asdict().get("query string")
+        result_help_str = example._asdict().get("help")
+        self.assertLessEqual(len(result_query_str), max_length + 5)  # extra length for dots
+        self.assertLessEqual(len(result_help_str), max_length + 5)  # extra length for dots
 
 
 if __name__ == "__main__":
