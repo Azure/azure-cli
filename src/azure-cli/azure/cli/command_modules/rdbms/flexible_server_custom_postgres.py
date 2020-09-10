a2d2efcd1a349dfcf7c9ccb20f89b7446a1d7673
Aritra De <arde@microsoft.com>
2020-09-10 02:43:01 -0700

    refactored create

--- src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_postgres.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_postgres.py
@@ -13,7 +13,7 @@ from azure.cli.core.commands.client_factory import get_subscription_id
 from azure.cli.core.local_context import ALL
 from azure.cli.core.util import CLIError, sdk_no_wait
 from ._client_factory import cf_postgres_flexible_firewall_rules, get_postgresql_flexible_management_client
-from .flexible_server_custom_common import user_confirmation
+from .flexible_server_custom_common import user_confirmation, _server_list_custom_func
 from ._flexible_server_util import generate_missing_parameters, resolve_poller, create_vnet, create_firewall_rule, \
     parse_public_access_input, update_kwargs, generate_password, parse_maintenance_window
 
@@ -31,24 +31,77 @@ def _flexible_server_create(cmd, client,
                             assign_identity=False, subnet_arm_resource_id=None,
                             high_availability=None, zone=None):
     from azure.mgmt.rdbms import postgresql
-    db_context = DbContext(
-        azure_sdk=postgresql, cf_firewall=cf_postgres_flexible_firewall_rules, logging_name='PostgreSQL', command_group='postgres', server_client=client)
-
-    firewall_id = subnet_id = None
-    if subnet_arm_resource_id is not None:
-        subnet_id = subnet_arm_resource_id # set the subnet id to be the one passed in
-        delegated_subnet_arguments=postgresql.flexibleservers.models.ServerPropertiesDelegatedSubnetArguments(
-            subnet_arm_resource_id=subnet_id
-        )
-    else:
-        delegated_subnet_arguments=None
-
-    location, resource_group_name, server_name = generate_missing_parameters(cmd, location, resource_group_name, server_name)
     try:
-        # The server name already exists in the resource group
-        server_result = client.get(resource_group_name, server_name)
-        logger.warning('Found existing PostgreSQL Server \'%s\' in group \'%s\'',
-                       server_name, resource_group_name)
+        db_context = DbContext(
+            azure_sdk=postgresql, cf_firewall=cf_postgres_flexible_firewall_rules, logging_name='PostgreSQL', command_group='postgres', server_client=client)
+
+        # Raise error when user passes values for both parameters
+        if subnet_arm_resource_id is not None and public_access is not None:
+            raise CLIError("usage error : A combination of the parameters --subnet "
+                           "and --public_access is invalid. Use either one of them.")
+
+        server_result = firewall_id = subnet_id = None
+
+        # Populate desired parameters
+        location, resource_group_name, server_name = generate_missing_parameters(cmd, location, resource_group_name, server_name)
+
+        # Get list of servers in the current sub
+        server_list = _server_list_custom_func(client)
+
+        # Ensure that the server name is not in the rg and in the subscription
+        for key in server_list:
+            if server_name == key.name and key.id.find(resource_group_name) != -1:
+                logger.warning('Found existing PostgreSQL Server \'%s\' in group \'%s\'',
+                               server_name, resource_group_name)
+                server_result = client.get(resource_group_name, server_name)
+            elif server_name == key.name:
+                raise CLIError("The server name '{}' exists in this subscription.Please re-run command with a "
+                                   "valid server name.".format(server_name))
+
+        administrator_login_password = generate_password(administrator_login_password)
+        if server_result is None:
+            # If subnet is provided, use that subnet to create the server, else create subnet if public access is not enabled.
+            if subnet_arm_resource_id is not None:
+                subnet_id = subnet_arm_resource_id # set the subnet id to be the one passed in
+            elif public_access is None and subnet_arm_resource_id is None:
+                subnet_id = create_vnet(cmd, server_name, location, resource_group_name,
+                                        "Microsoft.DBforPostgreSQL/flexibleServers")
+            # Create postgresql
+            # Note : passing public_access has no effect as the accepted values are 'Enabled' and 'Disabled'. So the value ends up being ignored.
+            server_result = _create_server(db_context, cmd, resource_group_name, server_name, location, backup_retention,
+                                           sku_name, tier, storage_mb, administrator_login, administrator_login_password,
+                                           version, tags, public_access, assign_identity, subnet_id,
+                                           high_availability, zone)
+
+            # Adding firewall rule
+            if public_access is not None and public_access != 'none':
+                if public_access == 'all':
+                    start_ip, end_ip = '0.0.0.0', '255.255.255.255'
+                else:
+                    start_ip, end_ip = parse_public_access_input(public_access)
+                firewall_id = create_firewall_rule(db_context, cmd, resource_group_name, server_name, start_ip, end_ip)
+
+        user = server_result.administrator_login
+        id = server_result.id
+        loc = server_result.location
+        version = server_result.version
+        sku = server_result.sku.name
+
+        # This is a workaround for getting the hostname right .
+        # Without Vnet, the hostname returned by the API os <servername>.postgres.database.azure.com
+        # with Vnet enabled, the hostname returned by the API is <servername>.<servername>.postgres.database.azure.com
+        if len(server_result.fully_qualified_domain_name.split('.')) != 6:
+            host = server_result.fully_qualified_domain_name
+        else:
+            host = server_result.fully_qualified_domain_name[
+                (server_result.fully_qualified_domain_name.index('.')) + 1:]
+
+        '''
+        try:
+            # The server name already exists in the resource group
+            server_result = client.get(resource_group_name, server_name)
+            logger.warning('Found existing PostgreSQL Server \'%s\' in group \'%s\'',
+                           server_name, resource_group_name)
 
     except CloudError:
         administrator_login_password = generate_password(administrator_login_password)
@@ -84,15 +137,17 @@ def _flexible_server_create(cmd, client,
     else:
         host = server_result.fully_qualified_domain_name[(server_result.fully_qualified_domain_name.index('.'))+1:]
 
-    logger.warning('Make a note of your password. If you forget, you would have to' \
-                   ' reset your password with \'az postgres flexible-server update -n %s -g %s -p <new-password>\'.',
-                   server_name, resource_group_name)
+    '''
+        logger.warning('Make a note of your password. If you forget, you would have to' \
+                       ' reset your password with \'az postgres flexible-server update -n %s -g %s -p <new-password>\'.',
+                       server_name, resource_group_name)
 
-    _update_local_contexts(cmd, server_name, resource_group_name, location, user)
+        _update_local_contexts(cmd, server_name, resource_group_name, location, user)
 
-    return _form_response(user, sku, loc, id, host, version,
-        administrator_login_password if administrator_login_password is not None else '*****',
-        _create_postgresql_connection_string(host, administrator_login_password), firewall_id, subnet_id)
+        return _form_response(user, sku, loc, id, host, version, administrator_login_password if administrator_login_password is not None else '*****',
+                              _create_postgresql_connection_string(host, administrator_login_password), firewall_id, subnet_id)
+    except Exception as ex:  # pylint: disable=broad-except
+        logger.error(ex)
 
 
 def _flexible_server_restore(cmd, client,
@@ -228,7 +283,7 @@ def _flexible_list_skus(client, location):
 
 def _create_server(db_context, cmd, resource_group_name, server_name, location, backup_retention, sku_name, tier,
                    storage_mb, administrator_login, administrator_login_password, version, tags, public_network_access,
-                   assign_identity, delegated_subnet_arguments, ha_enabled, availability_zone):
+                   assign_identity, subnet_id, ha_enabled, availability_zone):
     logging_name, azure_sdk, server_client = db_context.logging_name, db_context.azure_sdk, db_context.server_client
     logger.warning('Creating %s Server \'%s\' in group \'%s\'...', logging_name, server_name, resource_group_name)
 
@@ -248,7 +303,8 @@ def _create_server(db_context, cmd, resource_group_name, server_name, location, 
         storage_profile=postgresql.flexibleservers.models.StorageProfile(
             backup_retention_days=backup_retention,
             storage_mb=storage_mb),
-        delegated_subnet_arguments = delegated_subnet_arguments,
+        delegated_subnet_arguments=postgresql.flexibleservers.models.ServerPropertiesDelegatedSubnetArguments(
+                    subnet_arm_resource_id=subnet_id),
         ha_enabled=ha_enabled,
         availability_zone=availability_zone,
         location=location,
