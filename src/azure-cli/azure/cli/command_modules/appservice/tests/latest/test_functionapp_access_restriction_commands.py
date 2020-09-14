ffcc3652c47e9449ba797e2bc00af233415ea7fa
ThejaChoudary <57453616+ThejaChoudary@users.noreply.github.com>
2020-09-14 07:16:27 +0530

    [App Service] Fix #12653: az webapp log config --application-logging false doesn't turn it off (#14929)
    * #12653
    
    * #12653 commit 2
    
    * #commit 3
    
    * Commit3#12653
    
    * Commit4#12653

--- src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_functionapp_access_restriction_commands.py
+++ src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_functionapp_access_restriction_commands.py
@@ -81,7 +81,8 @@ class FunctionAppAccessRestrictionScenarioTest(ScenarioTest):
             JMESPathCheck('scmIpSecurityRestrictionsUseMain', False)
         ])
 
-    @ResourceGroupPreparer(parameter_name_for_location='location', location=WINDOWS_ASP_LOCATION_WEBAPP)
+    @ResourceGroupPreparer(random_name_length=17, parameter_name_for_location='location', location=WINDOWS_ASP_LOCATION_WEBAPP)
+    # random_name_length is temporary until the bug fix in the API is deployed successfully & then should be removed.
     @StorageAccountPreparer()
     def test_functionapp_access_restriction_add(self, resource_group, location):
         self.kwargs.update({
