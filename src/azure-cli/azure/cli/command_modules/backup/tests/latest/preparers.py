3c7abf19dc310f0b100650989bb0cc44b2e70daa
Feng Zhou <fenzhou@microsoft.com>
2020-09-22 17:50:05 +0800

    fix large response in backup

--- src/azure-cli/azure/cli/command_modules/backup/tests/latest/preparers.py
+++ src/azure-cli/azure/cli/command_modules/backup/tests/latest/preparers.py
@@ -17,7 +17,7 @@ class VaultPreparer(AbstractPreparer, SingleValueReplacer):
     def __init__(self, name_prefix='clitest-vault', parameter_name='vault_name',
                  resource_group_location_parameter_name='resource_group_location',
                  resource_group_parameter_name='resource_group',
-                 dev_setting_name='AZURE_CLI_TEST_DEV_BACKUP_ACCT_NAME'):
+                 dev_setting_name='AZURE_CLI_TEST_DEV_BACKUP_ACCT_NAME', soft_delete=True):
         super(VaultPreparer, self).__init__(name_prefix, 24)
         from azure.cli.core.mock import DummyCli
         self.cli_ctx = DummyCli()
@@ -27,14 +27,17 @@ class VaultPreparer(AbstractPreparer, SingleValueReplacer):
         self.location = None
         self.resource_group_location_parameter_name = resource_group_location_parameter_name
         self.dev_setting_value = os.environ.get(dev_setting_name, None)
+        self.soft_delete = soft_delete
 
     def create_resource(self, name, **kwargs):
         if not self.dev_setting_value:
             self.resource_group = self._get_resource_group(**kwargs)
             self.location = self._get_resource_group_location(**kwargs)
             cmd = 'az backup vault create -n {} -g {} --location {}'.format(name, self.resource_group, self.location)
-
             execute(self.cli_ctx, cmd)
+            if not self.soft_delete:
+                cmd = 'az backup vault backup-properties set -n {} -g {} --soft-delete-feature-state Disable'.format(name, self.resource_group)
+                execute(self.cli_ctx, cmd)
             return {self.parameter_name: name}
         return {self.parameter_name: self.dev_setting_value}
 
