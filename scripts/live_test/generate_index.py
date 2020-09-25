1e5bc96876521578eae51ffd3f6f6db1c52b0f39
Feiyue Yu <fey@microsoft.com>
2020-09-25 15:16:38 +0800

    index.html

--- scripts/live_test/generate_index.py
+++ scripts/live_test/generate_index.py
@@ -12,7 +12,7 @@ import requests
 import xml.etree.ElementTree as ET
 
 
-def generate(container, container_url):
+def generate(container, container_url, testdata, USER_REPO, USER_BRANCH, COMMIT_ID, USER_LIVE):
     """
     Generate index.html. Upload it to storage account
     :param container:
@@ -33,12 +33,12 @@ def generate(container, container_url):
                 if e.tag == 'Url':
                     url = e.text
             if name == '' or url == '':
-                print('Warning, name: {}, ')
+                print('Warning, name: {}, url: {}'.format(name, url))
             if name.endswith('.html'):
                 data.append({'name': name, 'url': url})
         break
     print(data)
-    html = render(data, container_url)
+    html = render(data, container_url, testdata, USER_REPO, USER_BRANCH, COMMIT_ID, USER_LIVE)
     with open('index.html', 'w') as f:
         f.write(html)
 
@@ -48,7 +48,7 @@ def generate(container, container_url):
     os.system(cmd)
 
 
-def render(data, container_url):
+def render(data, container_url, testdata, USER_REPO, USER_BRANCH, COMMIT_ID, USER_LIVE):
     content = """
     <!DOCTYPE html>
     <html>
@@ -60,17 +60,67 @@ def render(data, container_url):
     }
     </head>
     </style>
-    """
-    content += """
     <body>
-    <h2>Testing results in {}</h2>
-    """.format(container_url)
+    <h2>Testing results of Azure CLI</h2>
+    """
+
+    content += """
+    <p>
+    Repository: {}<br>
+    Branch: {}<br>
+    Commit: {}<br>
+    Live: {}<br>
+    </p>
+    """.format(USER_REPO, USER_BRANCH, COMMIT_ID, USER_LIVE)
+
+    content += """
+    <p>
+    <b>User Manual of Live Test Pipeline</b><br>
+    <a href=https://microsoft-my.sharepoint.com/:w:/p/fey/EZGC9LwrN3RAscVS5ylG4HMBX9h7W0ZSA7CDrhXN5Lvx6g?e=V8HUmd>Word</a> 
+    <a href=https://microsoft.sharepoint.com/teams/IoTToolingTeam/_layouts/OneNote.aspx?id=%2Fteams%2FIoTToolingTeam%2FShared%20Documents%2FAzure%20Management%20Experience%2FAzure%20Management%20Experience&wd=target%28AZ%20CLI%2FKnowledge%20base.one%7C18BC64EE-9328-497D-804E-6436006CA9A5%2FUser%20Manual%20of%20Live%20Test%20Pipeline%7C243EFA3E-FC7F-4612-9DA5-8E6BB2A11BD3%2F%29>OneNote</a>
+    </p>
+    """
+
+    table = """
+    <p><b>Test results summary</b></p>
+    <table>
+      <tr>
+        <th>Module</th>
+        <th>Passed</th>
+        <th>Failed</th>
+        <th>Pass rate</th>
+      </tr>
+    """
+
+    for module, passed, failed, rate in testdata.modules:
+        table += """
+          <tr>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+          </tr>
+        """.format(module, passed, failed, rate)
+
+    table += """
+      <tr>
+        <td>Total</td>
+        <td>{}</td>
+        <td>{}</td>
+        <td>{}</td>
+      </tr>
+    </table>
+    """.format(testdata.total[1], testdata.total[2], testdata.total[3])
+
+    content += table
+
     for item in data:
         name = item['name']
         url = item['url']
         content += """
         <p><a href={}>{}</a></p>
         """.format(url, name)
+
     content += """
     </body>
     </html>
