d9a06e58e44eb246c6480d782c6cbbbbb4465c8d
Daeun Yim <daeunyim@microsoft.com>
2020-10-05 13:43:18 -0700

    linter pass

--- src/azure-cli/azure/cli/command_modules/rdbms/_helptext_mysql.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_helptext_mysql.py
@@ -72,8 +72,6 @@ short-summary: Delete a flexible server.
 examples:
   - name: Delete a flexible server.
     text: az mysql flexible-server delete --resource-group testGroup --name testServer
-  - name: Delete a flexible server without prompt or confirmation.
-    text: az mysql flexible-server delete --resource-group testGroup --name testServer --yes
 """
 
 helps['mysql flexible-server firewall-rule'] = """
