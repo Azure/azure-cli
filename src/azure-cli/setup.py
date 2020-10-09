2fb11b9ebe89f146ec178c58e7bda9919380a5f4
Avani Gupta <avanigupta@users.noreply.github.com>
2020-10-09 00:02:15 -0700

    [AppConfig] Support AAD auth for data operations (#15160)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -51,7 +51,7 @@ CLASSIFIERS = [
 
 DEPENDENCIES = [
     'antlr4-python3-runtime~=4.7.2',
-    'azure-appconfiguration~=1.1.0',
+    'azure-appconfiguration~=1.1.1',
     'azure-batch~=9.0',
     'azure-cli-core=={}.*'.format(VERSION),
     'azure-cosmos~=3.0,>=3.0.2',
