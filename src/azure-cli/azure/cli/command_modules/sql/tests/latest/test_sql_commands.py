9d7c1092dc3b5c3ecb563fc731a949492894c970
Jared Welsh <jareddwelsh@gmail.com>
2020-10-20 10:53:31 -0700

    adding style changes

--- src/azure-cli/azure/cli/command_modules/sql/tests/latest/test_sql_commands.py
+++ src/azure-cli/azure/cli/command_modules/sql/tests/latest/test_sql_commands.py
@@ -984,7 +984,7 @@ class SqlServerADOnlyAuthScenarioTest(ScenarioTest):
 
         self.cmd('sql server ad-admin create -s {} -g {} -u {} -i {}'.format(server, resource_group, user, oid), checks=[])
         
-        self.cmd('sql server ad-only-auth enable -n {} -g {}'.format(server, resource_group),checks=[])
+        self.cmd('sql server ad-only-auth enable -n {} -g {}'.format(server, resource_group), checks=[])
         self.cmd('sql server ad-only-auth disable -n {} -g {}'.format(server, resource_group), checks=[])
         self.cmd('sql server ad-only-auth get -n {} -g {}'.format(server, resource_group), checks=[])
 
@@ -1065,9 +1065,9 @@ class SqlManagedInstanceAzureActiveDirectoryAdministratorScenarioTest(ScenarioTe
                      self.check('login', '{user}'),
                      self.check('sid', '{oid}')])
 
-        self.cmd ('sql mi ad-only-auth enable -n {managed_instance_name} -g {rg}', checks=[])
-        self.cmd ('sql mi ad-only-auth disable -n {managed_instance_name} -g {rg}', checks=[])
-        self.cmd ('sql mi ad-only-auth get -n {managed_instance_name} -g {rg}', checks=[])
+        self.cmd('sql mi ad-only-auth enable -n {managed_instance_name} -g {rg}', checks=[])
+        self.cmd('sql mi ad-only-auth disable -n {managed_instance_name} -g {rg}', checks=[])
+        self.cmd('sql mi ad-only-auth get -n {managed_instance_name} -g {rg}', checks=[])
 
 
 class SqlServerDbCopyScenarioTest(ScenarioTest):
