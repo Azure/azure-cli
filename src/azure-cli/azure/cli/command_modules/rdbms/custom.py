765668a0f8e634305be960c6f491df7807a534dc
Aritra De <arde@microsoft.com>
2020-10-19 23:36:11 -0700

    Remove password as a keyword arg

--- src/azure-cli/azure/cli/command_modules/rdbms/custom.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/custom.py
@@ -161,16 +161,16 @@ def _server_create(cmd, client, resource_group_name=None, server_name=None, sku_
     update_local_contexts(cmd, server_name, resource_group_name, location, user)
 
     if engine_name == 'postgres':
-        return form_response(username=user, sku=sku, location=loc, server_id=server_id, host=host, version=version,
-                             password=administrator_login_password if administrator_login_password is not None else '*****',
+        return form_response(administrator_login_password if administrator_login_password is not None else '*****',
+                             username=user, sku=sku, location=loc, server_id=server_id, host=host, version=version,
                              connection_string=create_postgresql_connection_string(server_name, host, user, administrator_login_password),
                              infrastructure_encryption=infrastructure_encryption, database_name=None, firewall_id=firewall_id)
     # Serves both - MySQL and MariaDB
     # Create mysql database if it does not exist
     database_name = DEFAULT_DB_NAME
     create_database(cmd, resource_group_name, server_name, database_name, engine_name)
-    return form_response(username=user, sku=sku, location=loc, server_id=server_id, host=host, version=version,
-                         password=administrator_login_password if administrator_login_password is not None else '*****',
+    return form_response(administrator_login_password if administrator_login_password is not None else '*****',
+                         username=user, sku=sku, location=loc, server_id=server_id, host=host, version=version,
                          connection_string=create_mysql_connection_string(server_name, host, database_name, user, administrator_login_password),
                          infrastructure_encryption=infrastructure_encryption, database_name=database_name, firewall_id=firewall_id)
 
@@ -708,7 +708,7 @@ def create_database(cmd, resource_group_name, server_name, database_name, engine
         database_client.create_or_update(resource_group_name, server_name, database_name, 'utf8').result()
 
 
-def form_response(username, sku, location, server_id, host, version, password, connection_string, infrastructure_encryption, database_name=None, firewall_id=None):
+def form_response(password, username, sku, location, server_id, host, version, connection_string, infrastructure_encryption, database_name=None, firewall_id=None):
     output = {
         'host': host,
         'username': username,
