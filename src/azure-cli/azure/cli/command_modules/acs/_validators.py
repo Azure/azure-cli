2d3f850a401cad2f0fcf73c3a8971a0ae9ff9451
Ganesha <ganesha.ashoka@gmail.com>
2020-10-21 01:47:01 -0700

    update slb timeout to 100 (#15562)

--- src/azure-cli/azure/cli/command_modules/acs/_validators.py
+++ src/azure-cli/azure/cli/command_modules/acs/_validators.py
@@ -229,8 +229,8 @@ def validate_load_balancer_outbound_ports(namespace):
 def validate_load_balancer_idle_timeout(namespace):
     """validate load balancer profile idle timeout"""
     if namespace.load_balancer_idle_timeout is not None:
-        if namespace.load_balancer_idle_timeout < 4 or namespace.load_balancer_idle_timeout > 120:
-            raise CLIError("--load-balancer-idle-timeout must be in the range [4,120]")
+        if namespace.load_balancer_idle_timeout < 4 or namespace.load_balancer_idle_timeout > 100:
+            raise CLIError("--load-balancer-idle-timeout must be in the range [4,100]")
 
 
 def validate_nodes_count(namespace):
