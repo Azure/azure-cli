1210e99973bdc33b82cb2a9202e95a8856a7b12a
Zunli Hu <zuh@microsoft.com>
2020-10-23 14:01:18 +0800

    [Storage] `az storage fs access`: Support managing ACLs recursively (#15221)
    * add acl recursive support
    
    * try to ad callback
    
    * recursive pass
    
    * add test
    
    * refine test but still failed
    
    * add failed entries
    
    * test pass for set recursive
    
    * add update-recursive but test failed
    
    * test pass
    
    * refine help
    
    * update doc
    
    * fix style
    
    * fix style
    
    * test
    
    * test pass
    
    * test pass
    
    * refine help

--- src/azure-cli/azure/cli/command_modules/storage/_validators.py
+++ src/azure-cli/azure/cli/command_modules/storage/_validators.py
@@ -1475,7 +1475,7 @@ def get_url_with_sas(cmd, namespace, url=None, container=None, blob=None, share=
 def _is_valid_uri(uri):
     if not uri:
         return False
-    if os.path.isdir(os.path.dirname(uri)):
+    if os.path.isdir(os.path.dirname(uri)) or os.path.isdir(uri):
         return uri
     if "?" in uri:  # sas token exists
         logger.debug("Find ? in %s. ", uri)
@@ -1538,3 +1538,18 @@ def validate_text_configuration(cmd, ns):
         ns.in_escape_char, ns.in_has_header
     del ns.output_format, ns.out_line_separator, ns.out_column_separator, ns.out_quote_char, ns.out_record_separator, \
         ns.out_escape_char, ns.out_has_header
+
+
+def add_acl_progress_hook(namespace):
+    if namespace.progress_hook:
+        return
+
+    failed_entries = []
+
+    # the progress callback is invoked each time a batch is completed
+    def progress_callback(acl_changes):
+        # keep track of failed entries if there are any
+        print(acl_changes.batch_failures)
+        failed_entries.append(acl_changes.batch_failures)
+
+    namespace.progress_hook = progress_callback
