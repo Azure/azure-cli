00cf5e3b3ecadfa43d4b1bee8c5c2d352626d227
Jianhui Harold <haroldrandom@gmail.com>
2020-10-26 10:43:00 +0800

    [Billing] Introduce new commands and upgrade to 2020-05-01 (#15568)

--- src/azure-cli/azure/cli/command_modules/billing/action.py
+++ src/azure-cli/azure/cli/command_modules/billing/action.py
@@ -9,24 +9,11 @@
 #
 # Generation mode: Incremental
 # --------------------------------------------------------------------------
-
-# pylint: disable=unused-wildcard-import
 # pylint: disable=wildcard-import
+# pylint: disable=unused-wildcard-import
 
-from .generated.custom import *  # noqa: F403
+from .generated.action import *  # noqa: F403
 try:
-    from .manual.custom import *  # noqa: F403
+    from .manual.action import *  # noqa: F403
 except ImportError:
     pass
-
-
-def cli_billing_list_invoices(client, generate_url=False):
-    """List all available invoices of the subscription"""
-    return client.list(expand='downloadUrl' if generate_url else None)
-
-
-def cli_billing_get_invoice(client, name=None):
-    """Retrieve invoice of specific name of the subscription"""
-    if name:
-        return client.get(name)
-    return client.get_latest()
