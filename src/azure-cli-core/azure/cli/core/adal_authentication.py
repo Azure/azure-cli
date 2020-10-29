bb22ee14659b8b435f1ac0ae19dd174d2d0b500f
Bin Ma <bim@microsoft.com>
2020-10-29 14:08:10 +0800

    Revert adal change

--- src/azure-cli-core/azure/cli/core/adal_authentication.py
+++ src/azure-cli-core/azure/cli/core/adal_authentication.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import datetime
 import time
 import requests
 import adal
