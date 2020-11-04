def8d330a1d1649b8b15486c9a055ef76b0fc625
Jiashuo Li <jiasli@microsoft.com>
2020-11-04 15:47:52 +0800

    [Profile] az login: Add back the warning that a browser is opened (#14505)

--- src/azure-cli-core/azure/cli/core/_profile.py
+++ src/azure-cli-core/azure/cli/core/_profile.py
@@ -1281,6 +1281,12 @@ def _get_authorization_code_worker(authority_url, resource, results):
         results['no_browser'] = True
         return
 
+    # Emit a warning to inform that a browser is opened.
+    # Only show the path part of the URL and hide the query string.
+    logger.warning("The default web browser has been opened at %s. Please continue the login in the web browser.\n"
+                   "If no web browser is available or if the web browser fails to open, use device code flow "
+                   "with `az login --use-device-code`.", url.split('?')[0])
+
     # wait for callback from browser.
     while True:
         web_server.handle_request()
