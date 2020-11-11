593d4bb02f2aa7308301c1764ee3f5aba563ac49
Jianhui Harold <haroldrandom@gmail.com>
2020-11-11 16:24:42 +0800

    [Network] az network dns zone import: Raise FileOperationError instead of FileNotFoundError if zone file doesn't exist (#15870)

--- src/azure-cli/azure/cli/command_modules/network/tests/latest/test_dns_commands.py
+++ src/azure-cli/azure/cli/command_modules/network/tests/latest/test_dns_commands.py
@@ -6,7 +6,7 @@
 import os
 import unittest
 
-from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
+from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, live_only
 
 from azure.cli.command_modules.network.zone_file import parse_zone_file
 
@@ -58,6 +58,14 @@ class DnsZoneImportTest(ScenarioTest):
 
         # verify that each record in the original import is unchanged after export/re-import
         self._check_records(records1, records2)
+
+    @live_only()
+    @ResourceGroupPreparer(name_prefix='test_dns_import_file_not_found')
+    def test_dns_import_file_not_found(self, resource_group):
+        from azure.cli.core.azclierror import FileOperationError
+        with self.assertRaises(FileOperationError) as e:
+            self._test_zone('404zone.com', 'non_existing_zone_description_file.txt')
+            self.assertEqual(e.errno, 1)
 
     @ResourceGroupPreparer(name_prefix='cli_dns_zone1_import')
     def test_dns_zone1_import(self, resource_group):
