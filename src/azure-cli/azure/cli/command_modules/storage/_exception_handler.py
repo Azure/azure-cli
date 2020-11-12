322ae20fc8e44b97d3ce3a093100ebe79e85c4c8
evelyn-ys <yishiwang@microsoft.com>
2020-11-12 13:32:53 +0800

    add recommendations

--- src/azure-cli/azure/cli/command_modules/storage/_exception_handler.py
+++ src/azure-cli/azure/cli/command_modules/storage/_exception_handler.py
@@ -5,8 +5,13 @@
 
 def file_related_exception_handler(ex):
     from azure.cli.core.azclierror import FileOperationError
-    if isinstance(ex, (FileNotFoundError, PermissionError, IsADirectoryError)):
-        raise FileOperationError(ex)
+    if isinstance(ex, FileNotFoundError):
+        raise FileOperationError(ex, recommendation='Please check the file path.')
+    if isinstance(ex, PermissionError):
+        raise FileOperationError(ex,
+                                 recommendation='Please make sure you have enough permissions on the file/directory.')
+    if isinstance(ex, IsADirectoryError):
+        raise FileOperationError(ex, recommendation='File is excepted, not a directory.')
     import sys
     from six import reraise
     reraise(*sys.exc_info())
