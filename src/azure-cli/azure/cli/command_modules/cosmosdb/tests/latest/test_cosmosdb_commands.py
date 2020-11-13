e8495748ce1f8ac3eb26c5e2c056f0e08c8ca37c
Kristyn Hamasaki <kristynh@microsoft.com>
2020-11-13 12:23:06 -0800

    Change Role test and rerun

--- src/azure-cli/azure/cli/command_modules/cosmosdb/tests/latest/test_cosmosdb_commands.py
+++ src/azure-cli/azure/cli/command_modules/cosmosdb/tests/latest/test_cosmosdb_commands.py
@@ -7,7 +7,6 @@
 
 from azure.cli.testsdk import JMESPathCheck, ScenarioTest, ResourceGroupPreparer
 from knack.util import CLIError
-import uuid
 
 
 class CosmosDBTests(ScenarioTest):
@@ -663,12 +662,14 @@ class CosmosDBTests(ScenarioTest):
         db_name = self.create_random_name(prefix='cli', length=15)
 
         subscription = self.get_subscription_id()
-        role_def_id = uuid.uuid4()
-        role_assignment_id = uuid.uuid4()
+        role_def_id = 'be79875a-2cc4-40d5-8958-566017875b39'
+        role_def_id2 = '6328f5f7-dbf7-4244-bba8-fbb9d8066506'
+        role_assignment_id = 'cb8ed2d7-2371-4e3c-bd31-6cc1560e84f8'
         role_definition_create_body = ' {{ \\"Id\\": \\"{0}\\", \\"RoleName\\": \\"roleName\\", \\"Type\\": \\"CustomRole\\", \\"AssignableScopes\\": [ \\"/\\" ], \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/create\\", \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/read\\" ] }} '.format(role_def_id)
         role_definition_update_body = ' {{ \\"Id\\": \\"{0}\\", \\"RoleName\\": \\"roleName2\\", \\"Type\\": \\"CustomRole\\", \\"AssignableScopes\\": [ \\"/\\" ], \\"Permissions\\": [ {{ \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/create\\" ] }}, {{ \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/read\\" ] }} ] }}'.format(role_def_id)
-        role_definition_create_body2 = ' { \\"RoleName\\": \\"roleName3\\", \\"AssignableScopes\\": [ \\"/\\" ], \\"Permissions\\": [ { \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/create\\" ] }, { \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/read\\" ] }, { \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/replace\\" ] } ] } '
+        role_definition_create_body2 = ' {{ \\"Id\\": \\"{0}\\", \\"RoleName\\": \\"roleName3\\", \\"Type\\": \\"CustomRole\\", \\"AssignableScopes\\": [ \\"/\\" ], \\"Permissions\\": [ {{ \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/create\\" ] }}, {{ \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/read\\" ] }}, {{ \\"DataActions\\": [ \\"Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/replace\\" ] }} ] }}'.format(role_def_id2)
         fully_qualified_role_def_id = '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.DocumentDB/databaseAccounts/{2}/sqlRoleDefinitions/{3}'.format(subscription, resource_group, acc_name, role_def_id)
+        fully_qualified_role_def_id2 = '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.DocumentDB/databaseAccounts/{2}/sqlRoleDefinitions/{3}'.format(subscription, resource_group, acc_name, role_def_id2)
         fully_qualified_role_assignment_id = '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.DocumentDB/databaseAccounts/{2}/sqlRoleAssignments/{3}'.format(subscription, resource_group, acc_name, role_assignment_id)
         assignable_scope = '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.DocumentDB/databaseAccounts/{2}'.format(subscription, resource_group, acc_name)
         scope = '/subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.DocumentDB/databaseAccounts/{2}/dbs/{3}'.format(subscription, resource_group, acc_name, db_name)
@@ -682,6 +683,8 @@ class CosmosDBTests(ScenarioTest):
             'create_body2': role_definition_create_body2,
             'role_def_id': role_def_id,
             'fully_qualified_role_def_id': fully_qualified_role_def_id,
+            'role_def_id2': role_def_id2,
+            'fully_qualified_role_def_id2': fully_qualified_role_def_id2,
             'role_assignment_id': role_assignment_id,
             'fully_qualified_role_assignment_id': fully_qualified_role_assignment_id,
             'scope': scope,
@@ -712,12 +715,13 @@ class CosmosDBTests(ScenarioTest):
             self.check('length(permissions)', 2)
         ])
 
-        role_definition_create2 = self.cmd('az cosmosdb sql role definition create -g {rg} -a {acc} -b "{create_body2}"', checks=[
+        self.cmd('az cosmosdb sql role definition create -g {rg} -a {acc} -b "{create_body2}"', checks=[
+            self.check('id', fully_qualified_role_def_id2),
             self.check('roleName', 'roleName3'),
             self.check('sqlRoleDefinitionGetResultsType', 'CustomRole'),
             self.check('assignableScopes[0]', assignable_scope),
             self.check('length(permissions)', 3)
-        ]).get_output_in_json()
+        ])
 
         role_definition_list = self.cmd('az cosmosdb sql role definition list -g {rg} -a {acc}').get_output_in_json()
         assert len(role_definition_list) == 2
@@ -735,13 +739,9 @@ class CosmosDBTests(ScenarioTest):
             self.check('id', fully_qualified_role_assignment_id)
         ])
 
-        self.kwargs.update({
-            'fully_qualified_role_def_id2': role_definition_create2['name']
-        })
-
-        self.cmd('az cosmosdb sql role assignment update -g {rg} -a {acc} -d {fully_qualified_role_def_id2} -i {fully_qualified_role_assignment_id}', checks=[
+        self.cmd('az cosmosdb sql role assignment update -g {rg} -a {acc} -d {role_def_id2} -i {fully_qualified_role_assignment_id}', checks=[
             self.check('id', fully_qualified_role_assignment_id),
-            self.check('roleDefinitionId', role_definition_create2['id']),
+            self.check('roleDefinitionId', fully_qualified_role_def_id2),
             self.check('scope', scope),
             self.check('principalId', principal_id)
         ])
