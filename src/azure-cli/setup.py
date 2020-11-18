e1813890802d2cef15aa29ab7399ee87fae2b872
Feiyue Yu <fey@microsoft.com>
2020-11-18 10:39:25 +0800

    Compute 18.0.0

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -71,7 +71,7 @@ DEPENDENCIES = [
     'azure-mgmt-botservice~=0.2.0',
     'azure-mgmt-cdn==5.0.0',
     'azure-mgmt-cognitiveservices~=6.3.0',
-    'azure-mgmt-compute~=17.0',
+    'azure-mgmt-compute~=18.0',
     'azure-mgmt-consumption~=2.0',
     'azure-mgmt-containerinstance~=1.4',
     'azure-mgmt-containerregistry==3.0.0rc15',
