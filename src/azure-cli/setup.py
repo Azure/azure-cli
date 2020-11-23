d9603961d7efcc1299ea1087e83e1d4e9858da68
Jorge Cotillo <jcotillo@microsoft.com>
2020-11-23 10:43:58 -0800

    removed preview flag and bump api version

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -112,7 +112,7 @@ DEPENDENCIES = [
     'azure-mgmt-relay~=0.1.0',
     # 'azure-mgmt-reservations~=0.6.0',
     'azure-mgmt-reservations==0.6.0',  # TODO: Use requirements.txt instead of '==' #9781
-    'azure-mgmt-resource==10.2.0',
+    'azure-mgmt-resource==10.3.0',
     'azure-mgmt-search~=2.0',
     'azure-mgmt-security~=0.4.1',
     'azure-mgmt-servicebus~=0.6.0',
