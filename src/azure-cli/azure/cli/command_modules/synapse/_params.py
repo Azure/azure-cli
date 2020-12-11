44f04817d99a012e2bc85fb0e3dcb6dfd6626e89
Zhenyu Zhou <zhezhou@microsoft.com>
2020-12-11 16:11:18 +0800

    Fix style error: too-many-branches

--- src/azure-cli/azure/cli/command_modules/synapse/_params.py
+++ src/azure-cli/azure/cli/command_modules/synapse/_params.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# pylint: disable=too-many-statements, line-too-long
+# pylint: disable=too-many-statements, line-too-long, too-many-branches
 from knack.arguments import CLIArgumentType
 from argcomplete import FilesCompleter
 from azure.mgmt.synapse.models import TransparentDataEncryptionStatus, SecurityAlertPolicyState, BlobAuditingPolicyState
