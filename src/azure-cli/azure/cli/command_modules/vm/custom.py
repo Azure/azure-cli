50ae3b7a937f481ca6a87626ec6fead6ee619b74
Feiyue Yu <fey@microsoft.com>
2020-12-14 15:25:24 +0800

    [Compute] Fix issue of none storage_profile

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -174,7 +174,9 @@ def _grant_access(cmd, resource_group_name, name, duration_in_seconds, is_disk, 
 
 
 def _is_linux_os(vm):
-    os_type = vm.storage_profile.os_disk.os_type if vm.storage_profile.os_disk.os_type else None
+    os_type = None
+    if vm and vm.storage_profile and vm.storage_profile.os_disk and vm.storage_profile.os_disk.os_type:
+        os_type = vm.storage_profile.os_disk.os_type
     if os_type:
         return os_type.lower() == 'linux'
     # the os_type could be None for VM scaleset, let us check out os configurations
