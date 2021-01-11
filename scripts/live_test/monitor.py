bbcaa32def8bb82bb874482c0be6374dc5bf0ba6
Feiyue Yu <fey@microsoft.com>
2021-01-11 13:00:17 +0800

    monitor

--- scripts/live_test/monitor.py
+++ scripts/live_test/monitor.py
@@ -1,12 +1,14 @@
-
+#
 
 
 def main():
+    import mysql.connector
     # Connect
     cnx = mysql.connector.connect(user='fey@clisqldbserver',
                                   password=DB_PWD,
                                   host='clisqldbserver.mysql.database.azure.com',
                                   database='clidb')
+    sql = ''
 
 
 if '__name__' == '__main__':
