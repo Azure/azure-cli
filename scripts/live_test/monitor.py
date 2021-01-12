f86b35d96ab06d84a663aa83bef3e6882800cb64
Feiyue Yu <fey@microsoft.com>
2021-01-12 20:34:18 +0800

    monitor

--- scripts/live_test/monitor.py
+++ scripts/live_test/monitor.py
@@ -1,4 +1,8 @@
-#
+# Monitor test results. Create issues when dropping.
+
+import sys
+
+DB_PWD = sys.argv[1]
 
 
 def main():
@@ -8,8 +12,20 @@ def main():
                                   password=DB_PWD,
                                   host='clisqldbserver.mysql.database.azure.com',
                                   database='clidb')
-    sql = ''
+    cursor = cnx.cursor()
+    sql = 'select module from t2 group by module'
+    cursor.execute(sql)
+    modules = []
+    for value in cursor:
+        if not value[0].startswith('ext-'):
+            modules.append(value[0])
+
+    print(modules)
+
+    for module in modules:
+        sql = 'select t2.module from t1 join t2 on t1.id = t2.id'
+        cursor.execute(sql)
 
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     main()
