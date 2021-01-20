3dbbfaab3e809824860534b62086da93b5142be0
Jiashuo Li <jiasli@microsoft.com>
2021-01-20 15:51:51 +0800

    Bump to 3.3.1

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -131,7 +131,7 @@ DEPENDENCIES = [
     'azure-synapse-accesscontrol~=0.2.0',
     'azure-synapse-artifacts~=0.3.0',
     'azure-synapse-spark~=0.2.0',
-    'cryptography>=3.2,<4.0.0',
+    'cryptography>=3.0,<4.0',
     'fabric~=2.4',
     'jsmin~=2.2.2',
     'pytz==2019.1',
