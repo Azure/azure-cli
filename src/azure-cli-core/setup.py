a8b362679e76576789162941402c3bdea0e929f6
Jiashuo Li <jiasli@microsoft.com>
2021-01-20 15:02:07 +0800

    Pin PyJWT to 1.7.1

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -54,7 +54,7 @@ DEPENDENCIES = [
     'msal-extensions~=0.1.3',
     'msrestazure>=0.6.3',
     'paramiko>=2.0.8,<3.0.0',
-    'PyJWT',
+    'PyJWT==1.7.1',
     'pyopenssl>=17.1.0',  # https://github.com/pyca/pyopenssl/pull/612
     'requests~=2.22',
     'six~=1.12',
