4bf35f607a267baa1f58e822b758a95919c0598c
zhoxing <Zhou.Xing@microsoft.com>
2021-01-26 15:36:42 +0800

    Fix mock function

--- src/azure-cli-core/azure/cli/core/tests/test_parser.py
+++ src/azure-cli-core/azure/cli/core/tests/test_parser.py
@@ -143,7 +143,7 @@ class TestParser(unittest.TestCase):
     def _mock_extension_modname(ext_name, ext_dir):
         return ext_name
 
-    def _mock_get_extensions():
+    def _mock_get_extensions(**kwargs):
         MockExtension = namedtuple('Extension', ['name', 'preview', 'experimental', 'path', 'get_metadata'])
         return [MockExtension(name=__name__ + '.ExtCommandsLoader', preview=False, experimental=False, path=None, get_metadata=lambda: {}),
                 MockExtension(name=__name__ + '.Ext2CommandsLoader', preview=False, experimental=False, path=None, get_metadata=lambda: {})]
