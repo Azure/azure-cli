6809f49098f95ba73cb671d68b96f5deadb95492
Nick Keller <nikelle@microsoft.com>
2021-02-01 18:45:34 +0000

    Fixed spacing

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -1504,7 +1504,7 @@ def aks_check_acr(cmd, client, resource_group_name, name, acr):
         kubectl_minor_version = int(kubectl_version["clientVersion"]["minor"])
         kubectl_server_minor_version = int(kubectl_version["serverVersion"]["minor"])
         kubectl_server_patch = int(kubectl_version["serverVersion"]["gitVersion"].split(".")[-1])
-        if  kubectl_server_minor_version < 17 or (kubectl_server_minor_version == 17 and kubectl_server_patch < 14):
+        if kubectl_server_minor_version < 17 or (kubectl_server_minor_version == 17 and kubectl_server_patch < 14):
             logger.warning('There is a known issue for Kuberentes versions < 1.17.14 when connecting to '
                            'ACR using MSI. See https://github.com/kubernetes/kubernetes/pull/96355 for'
                            'more information.')
