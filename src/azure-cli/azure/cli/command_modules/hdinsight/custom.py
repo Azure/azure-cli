2519444748a5441973ef940cdf5cb1a9cba6b096
aim-for-better <zhenyu.zhou@microsoft.com>
2021-02-02 16:07:47 +0800

    [HDInsight]Support compute isolation and return private ip address (#16752)
    * {Docs} Remove stale reference in README to closed issue about extensions (#12771)
    
    * Support compute isolation and return private ip address
    
    * Update test case: adding the paramter --host-sku, --host-sku is not mandatory for creating cluster
    
    Co-authored-by: Daniel Miller <daniel.keegan.miller@gmail.com>
    Co-authored-by: Azure CLI Team <AzPyCLI@microsoft.com>
    Co-authored-by: Zhenyu Zhou <zhezhou@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/hdinsight/custom.py
+++ src/azure-cli/azure/cli/command_modules/hdinsight/custom.py
@@ -36,8 +36,8 @@ def create_cluster(cmd, client, cluster_name, resource_group_name, cluster_type,
                    autoscale_type=None, autoscale_min_workernode_count=None, autoscale_max_workernode_count=None,
                    timezone=None, days=None, time=None, autoscale_workernode_count=None,
                    encryption_at_host=None, esp=False, idbroker=False,
-                   resource_provider_connection=None, enable_private_link=None,
-                   no_validation_timeout=False):
+                   resource_provider_connection=None, enable_private_link=None, enable_compute_isolation=None,
+                   host_sku=None, no_validation_timeout=False):
     from .util import build_identities_info, build_virtual_network_profile, parse_domain_name, \
         get_storage_account_endpoint, validate_esp_cluster_create_params
     from azure.mgmt.hdinsight.models import ClusterCreateParametersExtended, ClusterCreateProperties, OSType, \
@@ -46,7 +46,7 @@ def create_cluster(cmd, client, cluster_name, resource_group_name, cluster_type,
         DirectoryType, DiskEncryptionProperties, Tier, SshProfile, SshPublicKey, \
         KafkaRestProperties, ClientGroupInfo, EncryptionInTransitProperties, \
         Autoscale, AutoscaleCapacity, AutoscaleRecurrence, AutoscaleSchedule, AutoscaleTimeAndCapacity, \
-        NetworkProperties, PrivateLink
+        NetworkProperties, PrivateLink, ComputeIsolationProperties
 
     validate_esp_cluster_create_params(esp, cluster_name, resource_group_name, cluster_type,
                                        subnet, domain, cluster_admin_account, assign_identity,
@@ -350,6 +350,12 @@ def create_cluster(cmd, client, cluster_name, resource_group_name, cluster_type,
         private_link=PrivateLink.enabled if enable_private_link is True else PrivateLink.disabled
     )
 
+    # compute isolation
+    compute_isolation_properties = enable_compute_isolation and ComputeIsolationProperties(
+        enable_compute_isolation=enable_compute_isolation,
+        host_sku=host_sku
+    )
+
     create_params = ClusterCreateParametersExtended(
         location=location,
         tags=tags,
@@ -373,7 +379,8 @@ def create_cluster(cmd, client, cluster_name, resource_group_name, cluster_type,
             kafka_rest_properties=kafka_rest_properties,
             min_supported_tls_version=minimal_tls_version,
             encryption_in_transit_properties=encryption_in_transit_properties,
-            network_properties=network_properties
+            network_properties=network_properties,
+            compute_isolation_properties=compute_isolation_properties
         ),
         identity=cluster_identity
     )
