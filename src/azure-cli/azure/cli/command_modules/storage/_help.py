761734a59c526d5d399e23458ed460159a7afc58
Zunli Hu <zuh@microsoft.com>
2021-02-25 11:44:35 +0800

    fix linter

--- src/azure-cli/azure/cli/command_modules/storage/_help.py
+++ src/azure-cli/azure/cli/command_modules/storage/_help.py
@@ -873,19 +873,9 @@ helps['storage blob rewrite'] = """
 type: command
 short-summary:  Create a new Block Blob where the content of the blob is read from a given URL.
 long-summary: The content of an existing blob is overwritten with the new blob.
-parameters:
-  - name: --source-url -u
-    type: string
-    short-summary: >
-        A URL of up to 2 KB in length that specifies a file or blob.
-        The value should be URL-encoded as it would appear in a request URI.
-        If the source is in another account, the source must either be public
-        or must be authenticated via a shared access signature. If the source
-        is public, no authentication is required.
-        Examples:
-        `https://myaccount.blob.core.windows.net/mycontainer/myblob`
-        `https://myaccount.blob.core.windows.net/mycontainer/myblob?snapshot=<DateTime>`
-        `https://otheraccount.blob.core.windows.net/mycontainer/myblob?sastoken`
+examples:
+  - name: Update encryption scope for existing blob.
+    text: az storage blob rewrite --source-uri https://srcaccount.blob.core.windows.net/mycontainer/myblob?<sastoken> --encryption-scope newscope -c mycontainer -n myblob --account-name mystorageaccount --aaccount-key 0000-0000
 """
 
 helps['storage blob service-properties'] = """
