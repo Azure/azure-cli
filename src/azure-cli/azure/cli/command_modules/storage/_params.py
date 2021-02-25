f4a0e0c8eefa783c2fb1830a2f0c975b6a270622
Zunli Hu <zuh@microsoft.com>
2021-02-25 02:53:57 +0000

    test pass

--- src/azure-cli/azure/cli/command_modules/storage/_params.py
+++ src/azure-cli/azure/cli/command_modules/storage/_params.py
@@ -357,10 +357,6 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
         c.argument('resource_group_name', validator=process_resource_group, required=False)
         c.argument('encryption_scope_name', options_list=['--name', '-n'],
                    help='The name of the encryption scope within the specified storage account.')
-        c.argument('require_infrastructure_encryption', options_list=['--require-infrastructure-encryption', '-i'],
-                   arg_type=get_three_state_flag(), min_api='2021-01-01', 
-                   help='A boolean indicating whether or not the service applies a secondary layer of encryption with '
-                   'platform managed keys for data at rest.')
 
     for scope in ['storage account encryption-scope create', 'storage account encryption-scope update']:
         with self.argument_context(scope, resource_type=ResourceType.MGMT_STORAGE) as c:
@@ -373,6 +369,10 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                        help='The object identifier for a key vault key object. When applied, the encryption scope will '
                        'use the key referenced by the identifier to enable customer-managed key support on this '
                        'encryption scope.')
+            c.argument('require_infrastructure_encryption', options_list=['--require-infrastructure-encryption', '-i'],
+                   arg_type=get_three_state_flag(), min_api='2021-01-01',
+                   help='A boolean indicating whether or not the service applies a secondary layer of encryption with '
+                   'platform managed keys for data at rest.')
 
     with self.argument_context('storage account encryption-scope update') as c:
         t_state = self.get_models("EncryptionScopeState", resource_type=ResourceType.MGMT_STORAGE)
