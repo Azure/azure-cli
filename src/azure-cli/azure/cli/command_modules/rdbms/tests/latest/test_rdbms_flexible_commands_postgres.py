baf21e9d60baa04795f18891e1ea366283328d40
Daeun Yim <daeunyim@microsoft.com>
2021-02-25 02:50:46 -0800

    adding back

--- src/azure-cli/azure/cli/command_modules/rdbms/tests/latest/test_rdbms_flexible_commands_postgres.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/tests/latest/test_rdbms_flexible_commands_postgres.py
@@ -347,43 +347,43 @@ class PostgresFlexibleServerValidatorScenarioTest(FlexibleServerValidatorScenari
         self._test_mgmt_validator('postgres', resource_group)
 
 
-# class PostgresFlexibleServerVnetMgmtScenarioTest(FlexibleServerVnetMgmtScenarioTest):
+class PostgresFlexibleServerVnetMgmtScenarioTest(FlexibleServerVnetMgmtScenarioTest):
 
-#     postgres_location = postgres_location
+    postgres_location = postgres_location
 
-#     @AllowLargeResponse()
-#     @ResourceGroupPreparer(location=postgres_location)
-#     @VirtualNetworkPreparer(location=postgres_location)
-#     def test_postgres_flexible_server_vnet_mgmt_supplied_subnetid(self, resource_group):
-#         # Provision a server with supplied Subnet ID that exists, where the subnet is not delegated
-#         self._test_flexible_server_vnet_mgmt_existing_supplied_subnetid('postgres', resource_group)
-#         # Provision a server with supplied Subnet ID whose vnet exists, but subnet does not exist and the vnet does not contain any other subnet
-#         self._test_flexible_server_vnet_mgmt_non_existing_supplied_subnetid('postgres', resource_group)
+    @AllowLargeResponse()
+    @ResourceGroupPreparer(location=postgres_location)
+    @VirtualNetworkPreparer(location=postgres_location)
+    def test_postgres_flexible_server_vnet_mgmt_supplied_subnetid(self, resource_group):
+        # Provision a server with supplied Subnet ID that exists, where the subnet is not delegated
+        self._test_flexible_server_vnet_mgmt_existing_supplied_subnetid('postgres', resource_group)
+        # Provision a server with supplied Subnet ID whose vnet exists, but subnet does not exist and the vnet does not contain any other subnet
+        self._test_flexible_server_vnet_mgmt_non_existing_supplied_subnetid('postgres', resource_group)
 
-#     @AllowLargeResponse()
-#     @ResourceGroupPreparer(location=postgres_location)
-#     def test_postgres_flexible_server_vnet_mgmt_supplied_vnet(self, resource_group):
-#         self._test_flexible_server_vnet_mgmt_supplied_vnet('postgres', resource_group)
+    @AllowLargeResponse()
+    @ResourceGroupPreparer(location=postgres_location)
+    def test_postgres_flexible_server_vnet_mgmt_supplied_vnet(self, resource_group):
+        self._test_flexible_server_vnet_mgmt_supplied_vnet('postgres', resource_group)
 
-#     @AllowLargeResponse()
-#     @ResourceGroupPreparer(location=postgres_location)
-#     @VirtualNetworkPreparer(parameter_name='virtual_network', location=postgres_location)
-#     def test_postgres_flexible_server_vnet_mgmt_supplied_vname_and_subnetname(self, resource_group, virtual_network):
-#         self._test_flexible_server_vnet_mgmt_supplied_vname_and_subnetname('postgres', resource_group, virtual_network)
+    @AllowLargeResponse()
+    @ResourceGroupPreparer(location=postgres_location)
+    @VirtualNetworkPreparer(parameter_name='virtual_network', location=postgres_location)
+    def test_postgres_flexible_server_vnet_mgmt_supplied_vname_and_subnetname(self, resource_group, virtual_network):
+        self._test_flexible_server_vnet_mgmt_supplied_vname_and_subnetname('postgres', resource_group, virtual_network)
     
-#     @AllowLargeResponse()
-#     @ResourceGroupPreparer(location=postgres_location, parameter_name='resource_group_1')
-#     @ResourceGroupPreparer(location=postgres_location, parameter_name='resource_group_2')
-#     def test_postgres_flexible_server_vnet_mgmt_supplied_subnet_id_in_different_rg(self, resource_group_1, resource_group_2):
-#         self._test_flexible_server_vnet_mgmt_supplied_subnet_id_in_different_rg('postgres', resource_group_1, resource_group_2)
+    @AllowLargeResponse()
+    @ResourceGroupPreparer(location=postgres_location, parameter_name='resource_group_1')
+    @ResourceGroupPreparer(location=postgres_location, parameter_name='resource_group_2')
+    def test_postgres_flexible_server_vnet_mgmt_supplied_subnet_id_in_different_rg(self, resource_group_1, resource_group_2):
+        self._test_flexible_server_vnet_mgmt_supplied_subnet_id_in_different_rg('postgres', resource_group_1, resource_group_2)
 
 
-# class PostgresFlexibleServerPublicAccessMgmtScenarioTest(FlexibleServerPublicAccessMgmtScenarioTest):
+class PostgresFlexibleServerPublicAccessMgmtScenarioTest(FlexibleServerPublicAccessMgmtScenarioTest):
 
-#     postgres_location = postgres_location
+    postgres_location = postgres_location
 
-#     @AllowLargeResponse()
-#     @ResourceGroupPreparer(location=postgres_location)
-#     @live_only()
-#     def test_postgres_flexible_server_public_access_mgmt(self, resource_group):
-#         self._test_flexible_server_public_access_mgmt('postgres', resource_group)
+    @AllowLargeResponse()
+    @ResourceGroupPreparer(location=postgres_location)
+    @live_only()
+    def test_postgres_flexible_server_public_access_mgmt(self, resource_group):
+        self._test_flexible_server_public_access_mgmt('postgres', resource_group)
