bdea0f62c97becc7381c42a7b316c3e0439ae4cc
t-bzhan <61817681+t-bzhan@users.noreply.github.com>
2021-02-25 12:42:30 +0800

    [CDN] Add cli support for AFD SKU (#16951)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -69,7 +69,7 @@ DEPENDENCIES = [
     'azure-mgmt-batchai~=2.0',
     'azure-mgmt-billing==1.0.0',
     'azure-mgmt-botservice~=0.3.0',
-    'azure-mgmt-cdn==5.2.0',
+    'azure-mgmt-cdn==7.0.0',
     'azure-mgmt-cognitiveservices~=6.3.0',
     'azure-mgmt-compute~=18.1',
     'azure-mgmt-consumption~=2.0',
