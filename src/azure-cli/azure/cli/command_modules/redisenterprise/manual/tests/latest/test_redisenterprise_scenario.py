b1cafaac26934e9d2e7b1d4c987e2c32191098ca
Andrew Green <andrewgreen@microsoft.com>
2021-03-01 15:24:56 -0800

    Refactor and rerun tests

--- src/azure-cli/azure/cli/command_modules/redisenterprise/manual/tests/latest/test_redisenterprise_scenario.py
+++ src/azure-cli/azure/cli/command_modules/redisenterprise/manual/tests/latest/test_redisenterprise_scenario.py
@@ -16,12 +16,16 @@ from .. import (
 # Test class for scenario1
 class Redisenterprisescenario1Test(ScenarioTest):
 
-    @ResourceGroupPreparer(name_prefix='clitest-redisenterprise-rg1-', key='rg', parameter_name='rg',
-                           location='eastus', random_name_length=40)
-    def test_redisenterprise_scenario1(self, rg):
+    def __init__(self, *args, **kwargs):
+        super(Redisenterprisescenario1Test, self).__init__(*args, **kwargs)
+
         self.kwargs.update({
-            'cluster': self.create_random_name(prefix='clitest-cache1-', length=30)
+            'cluster': self.create_random_name(prefix='clitest-cache1-', length=21)
         })
+
+    @ResourceGroupPreparer(name_prefix='clitest-redisenterprise-rg1-', key='rg', parameter_name='rg',
+                           location='eastus', random_name_length=34)
+    def test_redisenterprise_scenario1(self, rg):
         from ....tests.latest import test_redisenterprise_scenario as g
         g.call_scenario1(self, rg)
         calc_coverage(__file__)
@@ -31,13 +35,17 @@ class Redisenterprisescenario1Test(ScenarioTest):
 # Test class for scenario2
 class Redisenterprisescenario2Test(ScenarioTest):
 
-    @ResourceGroupPreparer(name_prefix='clitest-redisenterprise-rg2-', key='rg', parameter_name='rg',
-                           location='eastus', random_name_length=40)
-    def test_redisenterprise_scenario2(self, rg):
+    def __init__(self, *args, **kwargs):
+        super(Redisenterprisescenario2Test, self).__init__(*args, **kwargs)
+
         self.kwargs.update({
-            'cluster': self.create_random_name(prefix='clitest-cache2-', length=30),
+            'cluster': self.create_random_name(prefix='clitest-cache2-', length=21),
             'no_database': True
         })
+
+    @ResourceGroupPreparer(name_prefix='clitest-redisenterprise-rg2-', key='rg', parameter_name='rg',
+                           location='eastus', random_name_length=34)
+    def test_redisenterprise_scenario2(self, rg):
         from ....tests.latest import test_redisenterprise_scenario as g
         g.call_scenario2(self, rg)
         calc_coverage(__file__)
