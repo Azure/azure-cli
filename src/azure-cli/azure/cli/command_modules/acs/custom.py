1ec35f328cd620b3dc46571f36e3d14ab22d9ce6
Tongyao Si <tosi@microsoft.com>
2021-03-11 16:46:00 +0800

    Fix lint

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -772,7 +772,7 @@ def _generate_properties(api_version, orchestrator_type, orchestrator_version, m
 
 
 def _get_user_assigned_identity_client_id(cli_ctx, resource_id):
-    pattern = '/subscriptions/(.*?)/resourcegroups/(.*?)/providers/microsoft.managedidentity/userassignedidentities/(.*)'
+    pattern = '/subscriptions/(.*?)/resourcegroups/(.*?)/providers/microsoft.managedidentity/userassignedidentities/(.*)' # pylint: disable=line-too-long
     resource_id = resource_id.lower()
     match = re.search(pattern, resource_id)
     if match:
@@ -4073,4 +4073,5 @@ def openshift_monitor_disable(cmd, client, resource_group_name, name, no_wait=Fa
 
 def _is_msi_cluster(managed_cluster):
     return (managed_cluster and managed_cluster.identity and
-            (managed_cluster.identity.type.casefold() == "systemassigned" or managed_cluster.identity.type.casefold() == "userassigned"))
+            (managed_cluster.identity.type.casefold() == "systemassigned" or
+            managed_cluster.identity.type.casefold() == "userassigned"))
