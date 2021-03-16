1f518df95634b682a97f008c5a7bf9a85388a3c8
Jun Sun <33297523+JunSun17@users.noreply.github.com>
2021-03-16 03:34:37 -0700

    {AKS} az aks to use 2021-02-01 API. (#17229)

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -63,21 +63,21 @@ from azure.graphrbac.models import (ApplicationCreateParameters,
 
 from azure.mgmt.containerservice.models import ContainerServiceOrchestratorTypes
 
-from azure.mgmt.containerservice.v2020_09_01.models import ContainerServiceNetworkProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ContainerServiceLinuxProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterServicePrincipalProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ContainerServiceSshConfiguration
-from azure.mgmt.containerservice.v2020_09_01.models import ContainerServiceSshPublicKey
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedCluster
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterAADProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterAddonProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterAgentPoolProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterIdentity
-from azure.mgmt.containerservice.v2020_09_01.models import AgentPool
-from azure.mgmt.containerservice.v2020_09_01.models import AgentPoolUpgradeSettings
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterSKU
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterWindowsProfile
-from azure.mgmt.containerservice.v2020_09_01.models import ManagedClusterIdentityUserAssignedIdentitiesValue
+from azure.mgmt.containerservice.v2021_02_01.models import ContainerServiceNetworkProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ContainerServiceLinuxProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterServicePrincipalProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ContainerServiceSshConfiguration
+from azure.mgmt.containerservice.v2021_02_01.models import ContainerServiceSshPublicKey
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedCluster
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterAADProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterAddonProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterAgentPoolProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterIdentity
+from azure.mgmt.containerservice.v2021_02_01.models import AgentPool
+from azure.mgmt.containerservice.v2021_02_01.models import AgentPoolUpgradeSettings
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterSKU
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterWindowsProfile
+from azure.mgmt.containerservice.v2021_02_01.models import ManagedClusterIdentityUserAssignedIdentitiesValue
 
 from azure.mgmt.containerservice.v2019_09_30_preview.models import OpenShiftManagedClusterAgentPoolProfile
 from azure.mgmt.containerservice.v2019_09_30_preview.models import OpenShiftAgentPoolProfileRole
@@ -96,7 +96,7 @@ from ._client_factory import get_graph_rbac_management_client
 from ._client_factory import cf_resources
 from ._client_factory import get_resource_by_name
 from ._client_factory import cf_container_registry_service
-from ._client_factory import cf_managed_clusters
+from ._client_factory import cf_agent_pools
 from ._client_factory import get_msi_client
 
 from ._helpers import (_populate_api_server_access_profile, _set_vm_set_type, _set_outbound_type,
@@ -2663,7 +2663,9 @@ def aks_upgrade(cmd,
             if vmas_cluster:
                 raise CLIError('This cluster is not using VirtualMachineScaleSets. Node image upgrade only operation '
                                'can only be applied on VirtualMachineScaleSets cluster.')
-            _upgrade_single_nodepool_image_version(True, client, resource_group_name, name, agent_pool_profile.name)
+            agent_pool_client = cf_agent_pools(cmd.cli_ctx)
+            _upgrade_single_nodepool_image_version(True, agent_pool_client,
+                                                   resource_group_name, name, agent_pool_profile.name)
         mc = client.get(resource_group_name, name)
         return _remove_nulls([mc])[0]
 
@@ -3462,9 +3464,8 @@ def aks_agentpool_upgrade(cmd, client, resource_group_name, cluster_name,
                        'If you only want to upgrade the node version please use the "--node-image-only" option only.')
 
     if node_image_only:
-        managed_cluster_client = cf_managed_clusters(cmd.cli_ctx)
         return _upgrade_single_nodepool_image_version(no_wait,
-                                                      managed_cluster_client,
+                                                      client,
                                                       resource_group_name,
                                                       cluster_name,
                                                       nodepool_name)
