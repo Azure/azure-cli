ce91fb09beb963fc4313885f9fc3c8da26093bf2
Yunge Zhu <yungez@microsoft.com>
2021-03-18 20:26:29 +0800

    bump sdk version

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -99,7 +99,7 @@ DEPENDENCIES = [
     'azure-mgmt-managedservices~=1.0',
     'azure-mgmt-managementgroups~=0.1',
     'azure-mgmt-maps~=0.1.0',
-    'azure-mgmt-marketplaceordering~=0.1',
+    'azure-mgmt-marketplaceordering==1.1.0',
     'azure-mgmt-media~=3.0',
     'azure-mgmt-monitor~=2.0.0',
     'azure-mgmt-msi~=0.2',
