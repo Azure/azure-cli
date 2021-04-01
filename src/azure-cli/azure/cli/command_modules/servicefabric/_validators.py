030218217a1b7ca675cbbfb2c38e9a68cdbeea60
Luke Slevinsky <luslevin@microsoft.com>
2021-04-01 10:53:16 -0700

    update exceptions in validations

--- src/azure-cli/azure/cli/command_modules/servicefabric/_validators.py
+++ src/azure-cli/azure/cli/command_modules/servicefabric/_validators.py
@@ -4,8 +4,9 @@
 # --------------------------------------------------------------------------------------------
 
 from msrestazure.azure_exceptions import CloudError
-from knack.util import CLIError
 from knack.log import get_logger
+from knack.util import CLIError
+from azure.cli.core.azclierror import ValidationError
 from azure.cli.command_modules.servicefabric._sf_utils import _get_resource_group_by_name
 from azure.mgmt.servicefabricmanagedclusters.models import (PartitionScheme, ServiceKind)
 from ._client_factory import servicefabric_client_factory, servicefabric_managed_client_factory
@@ -115,39 +116,40 @@ def validate_create_managed_service(namespace):
 
     if namespace.state.lower() == ServiceKind.STATELESS.lower():
         if namespace.target_replica_set_size or namespace.min_replica_set_size:
-            raise CLIError("--target-replica-set-size and --min-replica-set-size should only be use with "
-                           "--state stateful")
+            raise ValidationError("--target-replica-set-size and --min-replica-set-size should only be use with "
+                                  "--state stateful")
         if not namespace.instance_count:
-            raise CLIError("--instance-count is required")
+            raise ValidationError("--instance-count is required")
         namespace.instance_count = int(namespace.instance_count)
     elif namespace.state.lower() == ServiceKind.STATEFUL.lower():
         if namespace.instance_count:
-            raise CLIError("Unexpected parameter --instance-count should only be use with --state stateless")
+            raise ValidationError("Unexpected parameter --instance-count should only be use with --state stateless")
         if not namespace.target_replica_set_size or not namespace.min_replica_set_size:
-            raise CLIError("--target-replica-set-size and --min-replica-set-size are required")
+            raise ValidationError("--target-replica-set-size and --min-replica-set-size are required")
         namespace.target_replica_set_size = int(namespace.target_replica_set_size)
         namespace.min_replica_set_size = int(namespace.min_replica_set_size)
     else:
-        raise CLIError("Invalid --state '%s': service state is not valid." % namespace.state)
+        raise ValidationError("Invalid --state '%s': service state is not valid." % namespace.state)
 
     if namespace.partition_scheme is None:
-        raise CLIError("--partition-scheme is required")
+        raise ValidationError("--partition-scheme is required")
 
     if namespace.partition_scheme.lower() == PartitionScheme.NAMED.lower():
         if namespace.partition_names is None:
-            raise CLIError("--partition-names is required for partition scheme '%s'" % namespace.partition_scheme)
+            raise ValidationError("--partition-names is required for partition scheme '%s'"
+                                  % namespace.partition_scheme)
     elif namespace.partition_scheme.lower() == PartitionScheme.SINGLETON.lower():
         pass  # No parameters needed for singleton
     elif namespace.partition_scheme.lower() == PartitionScheme.UNIFORM_INT64_RANGE.lower():
         if namespace.partition_count is None or namespace.low_key is None or namespace.high_key is None:
-            raise CLIError(
+            raise ValidationError(
                 "--partition-count, --low-key and --high-key are required for partition scheme '%s'"
                 % namespace.partition_scheme)
         namespace.partition_count = int(namespace.partition_count)
         namespace.low_key = int(namespace.low_key)
         namespace.high_key = int(namespace.high_key)
     else:
-        raise CLIError(
+        raise ValidationError(
             "Invalid --partition_scheme '%s': service partition_scheme is not valid." % namespace.partition_scheme)
 
 
@@ -158,19 +160,19 @@ def validate_update_managed_service(cmd, namespace):
                                   namespace.application_name, namespace.service_name))
     if service.properties.service_kind.lower() == ServiceKind.STATELESS.lower():
         if namespace.target_replica_set_size or namespace.min_replica_set_size:
-            raise CLIError("--target-replica-set-size and --min-replica-set-size should only be use with "
-                           "--state stateful")
+            raise ValidationError("--target-replica-set-size and --min-replica-set-size should only be use with "
+                                  "--state stateful")
         if namespace.instance_count is not None:
             namespace.instance_count = int(namespace.instance_count)
     elif service.properties.service_kind.lower() == ServiceKind.STATEFUL.lower():
         if namespace.instance_count:
-            raise CLIError("Unexpected parameter --instance-count should only be use with --state stateless")
+            raise ValidationError("Unexpected parameter --instance-count should only be use with --state stateless")
         if namespace.target_replica_set_size is not None:
             namespace.target_replica_set_size = int(namespace.target_replica_set_size)
         if namespace.min_replica_set_size is not None:
             namespace.min_replica_set_size = int(namespace.min_replica_set_size)
     else:
-        raise CLIError("Invalid --state '%s': service state is not valid." % service.properties.service_kind)
+        raise ValidationError("Invalid --state '%s': service state is not valid." % service.properties.service_kind)
 
 
 def validate_create_managed_service_load_metric(cmd, namespace):
@@ -180,27 +182,28 @@ def validate_create_managed_service_load_metric(cmd, namespace):
                                   namespace.application_name, namespace.service_name))
 
     if service is None:
-        raise CLIError("Service '{}' Not Found.".format(namespace.service_name))
+        raise ValidationError("Service '{}' Not Found.".format(namespace.service_name))
     if service.properties.service_kind.lower() == ServiceKind.STATELESS.lower():
         if namespace.metric_name is None or namespace.weight is None or namespace.default_load is None:
-            raise CLIError("--metric-name, --weight and --default-load are required")
+            raise ValidationError("--metric-name, --weight and --default-load are required")
         if namespace.primary_default_load is not None or namespace.secondary_default_load is not None:
-            raise CLIError(
+            raise ValidationError(
                 "--primary-default-load and --secondary-default-load can only be used for stateful services."
             )
         namespace.default_load = int(namespace.default_load)
     elif service.properties.service_kind.lower() == ServiceKind.STATEFUL.lower():
         if namespace.metric_name is None or namespace.weight is None or \
            namespace.primary_default_load is None or namespace.secondary_default_load is None:
-            raise CLIError("--metric-name, --weight, --primary-default-load and --secondary-default-load are required")
+            raise ValidationError("--metric-name, --weight, --primary-default-load and "
+                                  "--secondary-default-load are required")
         if namespace.default_load is not None:
-            raise CLIError("--default-load can only be used for stateless services.")
+            raise ValidationError("--default-load can only be used for stateless services.")
         namespace.primary_default_load = int(namespace.primary_default_load)
         namespace.secondary_default_load = int(namespace.secondary_default_load)
     else:
-        raise CLIError("Invalid --state '%s': service state is not valid." % service.properties.service_kind)
+        raise ValidationError("Invalid --state '%s': service state is not valid." % service.properties.service_kind)
     if any(namespace.metric_name == metric.name for metric in service.properties.service_load_metrics):
-        raise CLIError("Duplicate metric names are not allowed: %s." % namespace.metric_name)
+        raise ValidationError("Duplicate metric names are not allowed: %s." % namespace.metric_name)
 
 
 def validate_update_managed_service_load_metric(cmd, namespace):
@@ -213,20 +216,20 @@ def validate_update_managed_service_load_metric(cmd, namespace):
         raise CLIError("Service '{}' Not Found.".format(namespace.service_name))
     if service.properties.service_kind.lower() == ServiceKind.STATELESS.lower():
         if namespace.primary_default_load is not None or namespace.secondary_default_load is not None:
-            raise CLIError(
+            raise ValidationError(
                 "--primary-default-load and --secondary-default-load can only be used for stateful services."
             )
         if namespace.default_load is not None:
             namespace.default_load = int(namespace.default_load)
     elif service.properties.service_kind.lower() == ServiceKind.STATEFUL.lower():
         if namespace.default_load is not None:
-            raise CLIError("--default-load can only be used for stateless services.")
+            raise ValidationError("--default-load can only be used for stateless services.")
         if namespace.primary_default_load is not None:
             namespace.primary_default_load = int(namespace.primary_default_load)
         if namespace.secondary_default_load is not None:
             namespace.secondary_default_load = int(namespace.secondary_default_load)
     else:
-        raise CLIError("Invalid --state '%s': service state is not valid." % service.properties.service_kind)
+        raise ValidationError("Invalid --state '%s': service state is not valid." % service.properties.service_kind)
 
 
 def validate_create_managed_service_correlation(cmd, namespace):
@@ -236,10 +239,10 @@ def validate_create_managed_service_correlation(cmd, namespace):
                                   namespace.application_name, namespace.service_name))
 
     if service is None:
-        raise CLIError("Service '{}' Not Found.".format(namespace.service_name))
+        raise ValidationError("Service '{}' Not Found.".format(namespace.service_name))
 
     if service.properties.correlation_scheme:
-        raise CLIError("There can only be one service correlation per service.")
+        raise ValidationError("There can only be one service correlation per service.")
 
 
 def validate_update_managed_service_correlation(cmd, namespace):
@@ -249,7 +252,7 @@ def validate_update_managed_service_correlation(cmd, namespace):
                                   namespace.application_name, namespace.service_name))
 
     if service is None:
-        raise CLIError("Service '{}' Not Found.".format(namespace.service_name))
+        raise ValidationError("Service '{}' Not Found.".format(namespace.service_name))
 
 
 def validate_update_managed_application(cmd, namespace):
@@ -260,8 +263,8 @@ def validate_update_managed_application(cmd, namespace):
         raise CLIError("Application '{}' Not Found.".format(namespace.application_name))
     if namespace.application_type_version is not None:
         if app.version.endswith(namespace.application_type_version):
-            raise CLIError("The application '{}' is alrady running with type version '{}'."
-                           .format(app.name, app.version))
+            raise ValidationError("The application '{}' is alrady running with type version '{}'."
+                                  .format(app.name, app.version))
         app_type_name = app.version.split("/")[-3]
         type_version = _safe_get_resource(client.application_type_versions.get,
                                           (namespace.resource_group_name,
@@ -269,9 +272,9 @@ def validate_update_managed_application(cmd, namespace):
                                            app_type_name,
                                            namespace.application_type_version))
         if type_version is None:
-            raise CLIError("Application type version {}:{} not found. "
-                           "Create the type version before running this command."
-                           .format(app.type_name, namespace.application_type_version))
+            raise ValidationError("Application type version {}:{} not found. "
+                                  "Create the type version before running this command."
+                                  .format(app.type_name, namespace.application_type_version))
 
     if namespace.upgrade_replica_set_check_timeout:
         namespace.upgrade_replica_set_check_timeout = int(namespace.upgrade_replica_set_check_timeout)
@@ -296,9 +299,10 @@ def validate_create_managed_application(cmd, namespace):
                                            namespace.application_type_name,
                                            namespace.application_type_version))
         if type_version is None:
-            raise CLIError("Application type version {}:{} not found. "
-                           "Create the type version before running this command or use --package-url to create it."
-                           .format(namespace.application_type_name, namespace.application_type_version))
+            raise ValidationError("Application type version {}:{} not found. "
+                                  "Create the type version before running this "
+                                  "command or use --package-url to create it."
+                                  .format(namespace.application_type_name, namespace.application_type_version))
 
 
 # Helpers
