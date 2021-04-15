980e4741face381fe7c03f82b05ca66a8ce40b78
xfz11 <81600993+xfz11@users.noreply.github.com>
2021-04-15 13:59:42 +0800

    Update src/azure-cli/azure/cli/command_modules/vm/custom.py

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -672,7 +672,7 @@ def assign_vm_identity(cmd, resource_group_name, vm_name, assign_identity=None, 
         if external_identities:
             vm.identity.user_assigned_identities = {}
             if not cmd.supported_api_version(min_api='2018-06-01', resource_type=ResourceType.MGMT_COMPUTE):
-                raise CLIInternalError("usage error: user assigned identity is not available under current profile.",
+                raise CLIInternalError("Usage error: user assigned identity is not available under current profile.",
                                        "Please set the cloud's profile to latest with 'az cloud set --profile latest"
                                        " --name <cloud name>'")
             for identity in external_identities:
