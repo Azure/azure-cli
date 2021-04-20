cd1a907cfaed4e19613c92fde05336ee83f8f0ab
Jiashuo Li <4003950+jiasli@users.noreply.github.com>
2021-04-20 13:57:00 +0800

    [Style] Define theme for Cloud Shell (#17283)

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -48,11 +48,10 @@ DEPENDENCIES = [
     'azure-common~=1.1',
     'azure-core==1.12.0',
     'azure-mgmt-core>=1.2.0,<2.0.0',
-    'colorama~=0.4.1',
     'cryptography>=3.2,<3.4',
     'humanfriendly>=4.7,<10.0',
     'jmespath',
-    'knack~=0.8.0',
+    'knack~=0.8.1',
     'msal>=1.10.0,<2.0.0',
     # Dependencies of the vendored subscription SDK
     # https://github.com/Azure/azure-sdk-for-python/blob/ab12b048ddf676fe0ccec16b2167117f0609700d/sdk/resources/azure-mgmt-resource/setup.py#L82-L86
