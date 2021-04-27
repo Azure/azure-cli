294c2c104a591bbade2be8f724dba0859206e17a
jiasli <4003950+jiasli@users.noreply.github.com>
2021-04-27 14:02:45 +0800

    tests

--- src/azure-cli-core/azure/cli/core/auth/tests/test_util.py
+++ src/azure-cli-core/azure/cli/core/auth/tests/test_util.py
@@ -6,7 +6,7 @@
 # pylint: disable=protected-access
 
 import unittest
-from ..util import _extract_claims, scopes_to_resource, resource_to_scopes
+from ..util import _extract_claims, scopes_to_resource, resource_to_scopes, _generate_login_command
 
 
 class TestUtil(unittest.TestCase):
@@ -63,6 +63,26 @@ class TestUtil(unittest.TestCase):
         # resource without trailing slash
         self.assertEqual(resource_to_scopes('https://managedhsm.azure.com'),
                          ['https://managedhsm.azure.com/.default'])
+
+    def test_generate_login_command(self):
+        # No parameter is given
+        assert _generate_login_command() == 'az login'
+
+        base64_claims = "eyJhY2Nlc3NfdG9rZW4iOnsibmJmIjp7ImVzc2VudGlhbCI6dHJ1ZSwgInZhbHVlIjoiMTYxNzE3MjE1NiJ9fX0="
+        json_claims = '{"access_token":{"nbf":{"essential":true, "value":"1617172156"}}}'
+        expect = 'az logout\naz login --claims eyJhY2Nlc3NfdG9rZW4iOnsibmJmIjp7ImVzc2VudGlhbCI6dHJ1ZSwgInZhbHVlIjoiMTYxNzE3MjE1NiJ9fX0='
+
+        # Base64 string is preserved
+        actual = _generate_login_command(claims=base64_claims)
+        assert actual == expect
+
+        # JSON string is converted to base64
+        actual = _generate_login_command(claims=json_claims)
+        assert actual == expect
+
+        # scopes
+        actual = _generate_login_command(scopes=["https://management.core.windows.net//.default"])
+        assert actual == 'az login --scope https://management.core.windows.net//.default'
 
 
 if __name__ == '__main__':
