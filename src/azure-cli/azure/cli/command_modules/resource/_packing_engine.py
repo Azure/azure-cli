2527128266c9c2287bd5582b2c8c8365ee1035e1
Dania Etienne <daetienn@microsoft.com>
2021-04-27 13:49:01 -0400

    Removed _packing_engine changes

--- src/azure-cli/azure/cli/command_modules/resource/_packing_engine.py
+++ src/azure-cli/azure/cli/command_modules/resource/_packing_engine.py
@@ -69,7 +69,7 @@ def _pack_artifacts(cmd, template_abs_file_path, context):
     :type template_abs_file_path : str
     :param context : The packing context of the current packing operation
     :type content : PackingContext
-    :param artifactableTemplateObj : The packageable template object
+    :param artifactableTemplateObj : The packagable template object
     :type artifactableTemplateObj : JSON
     """
     original_directory = getattr(context, 'CurrentDirectory')
@@ -112,11 +112,11 @@ def _pack_artifacts(cmd, template_abs_file_path, context):
                 if os.path.samefile(prev_added_artifact, abs_local_path):
                     continue
             _pack_artifacts(cmd, abs_local_path, context)
-            LinkedTemplateArtifact = get_sdk(cmd.cli_ctx, ResourceType.MGMT_RESOURCE_TEMPLATESPECS,
-                                                   'LinkedTemplateArtifact', mod='models')
+            TemplateSpecTemplateArtifact = get_sdk(cmd.cli_ctx, ResourceType.MGMT_RESOURCE_TEMPLATESPECS,
+                                                   'TemplateSpecTemplateArtifact', mod='models')
             template_content = read_file_content(abs_local_path)
             template_json = json.loads(json.dumps(process_template(template_content)))
-            artifact = LinkedTemplateArtifact(path=as_relative_path, template=template_json)
+            artifact = TemplateSpecTemplateArtifact(path=as_relative_path, template=template_json)
             context.Artifact.append(artifact)
     finally:
         context.CurrentDirectory = original_directory
@@ -135,9 +135,9 @@ def _get_deployment_resource_objects(cmd, template_obj, includeNested=False):
         results.append(deployment_resource_obj)
         if(includeNested and 'properties' in deployment_resource_obj):
             deployment_resource_props_obj = deployment_resource_obj['properties']
-            if 'main_template' in deployment_resource_props_obj:
+            if 'template' in deployment_resource_props_obj:
                 results.extend(_get_deployment_resource_objects(cmd,
-                                                                deployment_resource_props_obj['main_template'],
+                                                                deployment_resource_props_obj['template'],
                                                                 includeNested=True))
     return results
 
@@ -197,7 +197,7 @@ def unpack(cmd, exported_template, target_dir, template_file_name):
                                   _normalize_directory_seperators_for_local_file_system(getattr(artifact, 'path')))
         abs_local_path = os.path.abspath(local_path)
         if os.path.commonpath([target_dir]) != os.path.commonpath([target_dir, abs_local_path]):
-            raise CLIError('Unable to unpack linked template ' + getattr(artifact, 'path') + 'because it would create a file' +
+            raise CLIError('Unable to unpack artifact ' + getattr(artifact, 'path') + 'because it would create a file' +
                            'outside of the target directory hierarchy of' + target_dir)
 
     # Now that the artifact paths checkout...let's begin by writing our main template
@@ -208,15 +208,15 @@ def unpack(cmd, exported_template, target_dir, template_file_name):
     with open(root_template_file_path, 'w') as root_file:
         json.dump(getattr(packaged_template, 'RootTemplate'), root_file, indent=2)
 
-    LinkedTemplateArtifact = get_sdk(cmd.cli_ctx, ResourceType.MGMT_RESOURCE_TEMPLATESPECS,
-                                           'LinkedTemplateArtifact', mod='models')
+    TemplateSpecTemplateArtifact = get_sdk(cmd.cli_ctx, ResourceType.MGMT_RESOURCE_TEMPLATESPECS,
+                                           'TemplateSpecTemplateArtifact', mod='models')
     for artifact in getattr(packaged_template, 'Artifacts'):
-        if not isinstance(artifact, LinkedTemplateArtifact):
-            raise CLIError('Unknown linked template type encountered...')
+        if not isinstance(artifact, TemplateSpecTemplateArtifact):
+            raise CLIError('Unknown artifact type encountered...')
         artifact_path = _normalize_directory_seperators_for_local_file_system(getattr(artifact, 'path'))
         abs_local_path = os.path.abspath(os.path.join(target_dir, artifact_path))
         if not os.path.exists(os.path.dirname(abs_local_path)):
             os.makedirs(os.path.dirname(abs_local_path))
         with open(abs_local_path, 'w') as artifact_file:
-            json.dump(getattr(artifact, 'main_template'), artifact_file, indent=2)
+            json.dump(getattr(artifact, 'template'), artifact_file, indent=2)
     return target_dir
