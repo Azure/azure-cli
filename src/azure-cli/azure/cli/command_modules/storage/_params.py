15566dbd8bf0f871a7740243bb08840ae7764fd0
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2021-05-01 11:20:35 +0800

    split-words-in-help-storage-container-generate_sas

--- src/azure-cli/azure/cli/command_modules/storage/_params.py
+++ src/azure-cli/azure/cli/command_modules/storage/_params.py
@@ -1145,15 +1145,15 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
         c.argument('permission', options_list='--permissions',
                    help=sas_help.format(get_permission_help_string(t_container_permissions)),
                    validator=get_permission_validator(t_container_permissions))
-        c.argument('cache_control', help='Response header value for Cache-Control when resource is accessed'
+        c.argument('cache_control', help='Response header value for Cache-Control when resource is accessed '
                                          'using this shared access signature.')
-        c.argument('content_disposition', help='Response header value for Content-Disposition when resource is accessed'
+        c.argument('content_disposition', help='Response header value for Content-Disposition when resource is accessed '
                                                'using this shared access signature.')
-        c.argument('content_encoding', help='Response header value for Content-Encoding when resource is accessed'
+        c.argument('content_encoding', help='Response header value for Content-Encoding when resource is accessed '
                                             'using this shared access signature.')
-        c.argument('content_language', help='Response header value for Content-Language when resource is accessed'
+        c.argument('content_language', help='Response header value for Content-Language when resource is accessed '
                                             'using this shared access signature.')
-        c.argument('content_type', help='Response header value for Content-Type when resource is accessed'
+        c.argument('content_type', help='Response header value for Content-Type when resource is accessed '
                                         'using this shared access signature.')
         c.argument('as_user', min_api='2018-11-09', action='store_true',
                    validator=as_user_validator,
