a5b9be0d0856f76102096d8d2473cf7f807b5bfe
xiaofanzhou <xiaofanzhou@microsoft.com>
2021-05-10 19:44:13 +0800

    change the command format

--- src/azure-cli/azure/cli/command_modules/vm/manual/_params.py
+++ src/azure-cli/azure/cli/command_modules/vm/manual/_params.py
@@ -23,18 +23,29 @@ def load_arguments(self, _):
         c.argument('ssh_public_key_name', options_list=['--name', '-n', '--ssh-public-key-name'], type=str, help='The '
                    'name of the SSH public key.', id_part='name')
 
-    with self.argument_context('sig share update') as c:
-        c.argument('operation_type', arg_type=get_enum_type(['Add', 'Remove', 'Reset']),
-                   help='This property allows you to specify the operation type of gallery sharing update.')
-
-    with self.argument_context('sig share wait') as c:
-        c.argument('gallery_unique_name', type=str, help='The unique name of the Shared Gallery.',
-                   id_part='child_name_1')
-
     with self.argument_context('sig group-list') as c:
         c.argument('shared_to', options_list=['--scope'], arg_type=get_enum_type(['tenant', 'subscription']),
                    help='The query parameter to decide what shared galleries to fetch when doing listing operations.',
                    default='subscription')
+
+    for scope in ['sig share add', 'sig share remove']:
+        with self.argument_context(scope) as c:
+            c.argument('resource_group_name', resource_group_name_type)
+            c.argument('gallery_name', type=str, help='The name of the Shared Image Gallery.', id_part='name')
+            c.argument('subscription_ids', nargs='+', help='A list of subscription ids to share the gallery.')
+            c.argument('tenant_ids', nargs='+', help='A list of tenant ids to share the gallery.')
+
+    with self.argument_context('sig share add') as c:
+        c.argument('op_type', default='Add', deprecate_info=c.deprecate(hide=True),
+                   help='distinguish add operation and remove operation')
+
+    with self.argument_context('sig share remove') as c:
+        c.argument('op_type', default='Remove', deprecate_info=c.deprecate(hide=True),
+                   help='distinguish add operation and remove operation')
+
+    with self.argument_context('sig share reset') as c:
+        c.argument('resource_group_name', resource_group_name_type)
+        c.argument('gallery_name', type=str, help='The name of the Shared Image Gallery.', id_part='name')
 
     with self.argument_context('sig share image-definition list') as c:
         c.argument('shared_to', options_list=['--scope'], arg_type=get_enum_type(['tenant', 'subscription']),
