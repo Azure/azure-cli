f79208d656ff4e1731e1e60cad27f2b44d81f045
Fuming Zhang <fumingzhang@microsoft.com>
2021-05-14 09:34:47 +0800

    fix pylint error

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/custom_preparers.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/custom_preparers.py
@@ -8,8 +8,8 @@ from datetime import datetime
 
 from azure.cli.testsdk import CliTestError
 from azure.cli.testsdk.preparers import NoTrafficRecordingPreparer
-from azure_devtools.scenario_tests import SingleValueReplacer
 from azure.cli.testsdk.reverse_dependency import get_dummy_cli
+from azure_devtools.scenario_tests import SingleValueReplacer
 
 
 class AKSCustomResourceGroupPreparer(NoTrafficRecordingPreparer, SingleValueReplacer):
