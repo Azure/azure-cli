10273d3f23886b2a9e5d74c302efa8691686c8aa
Zunli Hu <zuh@microsoft.com>
2021-05-19 10:47:47 +0800

    draft command design

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -146,7 +146,7 @@ AZURE_API_PROFILES = {
             'galleries': '2019-12-01',
             'gallery_images': '2020-09-30',
             'gallery_image_versions': '2020-09-30',
-            'virtual_machine_scale_sets': '2020-12-01'
+            'virtual_machine_scale_sets': '2021-03-01'
         }),
         ResourceType.MGMT_RESOURCE_FEATURES: '2015-12-01',
         ResourceType.MGMT_RESOURCE_LINKS: '2016-09-01',
