4faec75232119e5e3540cc19cc81fe2c8b715147
Fuming Zhang <fumingzhang@microsoft.com>
2021-05-20 10:13:31 +0800

    update code format

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/custom_preparers.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/custom_preparers.py
@@ -9,14 +9,21 @@ from azure.cli.testsdk.preparers import ResourceGroupPreparer
 
 
 class AKSCustomResourceGroupPreparer(ResourceGroupPreparer):
-    def __init__(self, name_prefix='clitest.rg',
-                 parameter_name='resource_group',
-                 parameter_name_for_location='resource_group_location', location='westus',
-                 dev_setting_name='AZURE_CLI_TEST_DEV_RESOURCE_GROUP_NAME',
-                 dev_setting_location='AZURE_CLI_TEST_DEV_RESOURCE_GROUP_LOCATION',
-                 random_name_length=75, key='rg'):
-        super(AKSCustomResourceGroupPreparer, self).__init__(
-            name_prefix, parameter_name, parameter_name_for_location, location, dev_setting_name, dev_setting_location, random_name_length, key)
+    def __init__(
+            self,
+            name_prefix='clitest.rg',
+            parameter_name='resource_group',
+            parameter_name_for_location='resource_group_location',
+            location='westus',
+            dev_setting_name='AZURE_CLI_TEST_DEV_RESOURCE_GROUP_NAME',
+            dev_setting_location='AZURE_CLI_TEST_DEV_RESOURCE_GROUP_LOCATION',
+            random_name_length=75,
+            key='rg'):
+        super(AKSCustomResourceGroupPreparer,
+              self).__init__(name_prefix, parameter_name,
+                             parameter_name_for_location, location,
+                             dev_setting_name, dev_setting_location,
+                             random_name_length, key)
 
         # use environment variable to modify the default value of location
         self.dev_setting_location = os.environ.get(dev_setting_location, None)
