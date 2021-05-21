f1ff2a3415a9f7d5f486be2f785ab92df3f31a39
Calvin <calvinch4n@gmail.com>
2021-05-20 23:06:23 -0400

    [AppService] Add interactive way to get token for staticwebapp (#18083)
    * Add interactive way to get token for staticwebapp
    
    * Update staticwebapp tests
    
    * Add warning message if both token and --login-with-github flag are provided

--- src/azure-cli/azure/cli/command_modules/appservice/_params.py
+++ src/azure-cli/azure/cli/command_modules/appservice/_params.py
@@ -984,8 +984,9 @@ def load_arguments(self, _):
         c.argument('token', options_list=['--token', '-t'],
                    help="A user's github repository token. This is used to setup the Github Actions workflow file and "
                         "API secrets. If you need to create a Github Personal Access Token, "
-                        "please follow the steps found at the following link:\n"
+                        "please run with the '--login-with-github' flag or follow the steps found at the following link:\n"
                         "https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line")
+        c.argument('login_with_github', help="Interactively log in with Github to retrieve the Personal Access Token")
         c.argument('branch', options_list=['--branch', '-b'], help="The target branch in the repository.")
     with self.argument_context('staticwebapp environment') as c:
         c.argument('environment_name',
