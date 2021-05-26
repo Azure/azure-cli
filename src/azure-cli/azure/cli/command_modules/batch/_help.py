c1f2b78f5f5efd9e18d5aae509c3031f59bc1534
BigCat20196 <1095260342@qq.com>
2021-05-26 15:28:55 +0800

    Fix _help

--- src/azure-cli/azure/cli/command_modules/batch/_help.py
+++ src/azure-cli/azure/cli/command_modules/batch/_help.py
@@ -51,6 +51,14 @@ examples:
     crafted: true
 """
 
+helps['batch account renew'] = """
+type: command
+short-summary: Renew keys for a Batch account.
+examples:
+  - name: Renew keys for a Batch account.
+    text: az batch account renew --name MyBatchAccount --resource-group MyResourceGroup --key-name primary
+"""
+
 helps['batch account show'] = """
 type: command
 short-summary: Get a specified Batch account or the currently set account.
