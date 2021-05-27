739438a60643d2e92d2c5491f4f8d59f71a9a66a
Zeshi Luo <zesluo@microsoft.com>
2021-05-27 20:57:45 +0800

    'fixerror19'

--- src/azure-cli/azure/cli/command_modules/synapse/_params.py
+++ src/azure-cli/azure/cli/command_modules/synapse/_params.py
@@ -298,6 +298,8 @@ def load_arguments(self, _):
                        help='The number of days to retain audit logs.')
 
     with self.argument_context('synapse sql audit-policy update') as c:
+        c.argument('blob_auditing_policy_name', options_list=['--blob-audit-ply', '-b'],
+                   help='Name of the blob auditing policy name.')
         c.argument('queue_delay_milliseconds', type=int,
                    options_list=['--queue-delay-time', '--queue-delay-milliseconds'],
                    help='The amount of time in milliseconds that can elapse before audit actions are forced to be processed')
