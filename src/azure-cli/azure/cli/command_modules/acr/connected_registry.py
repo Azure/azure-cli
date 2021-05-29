74f8c4fa4f1fc0951da90bbd7c9a74a49c5ba852
Rodrigo Mendoza <43052640+rosanch@users.noreply.github.com>
2021-05-28 17:17:28 -0700

    bug fix

--- src/azure-cli/azure/cli/command_modules/acr/connected_registry.py
+++ src/azure-cli/azure/cli/command_modules/acr/connected_registry.py
@@ -63,7 +63,9 @@ def acr_connected_registry_create(cmd,  # pylint: disable=too-many-locals, too-m
                                   sync_audit_logs_enabled=False):
 
     if bool(sync_token_name) == bool(repositories):
-        raise CLIError("usage error: you need to provide either --sync-token-name or --repository, but not both.")
+        raise CLIError("usage error: you must provide either --sync-token-name or --repository, but not both.")
+    if (len(connected_registry_name) < 5): # Check needed since the sync token gateway actions must be at least 5 characters long.
+        raise CLIError("argument error: Connected registry name must be at least 5 characters long")
     registry, resource_group_name = get_registry_by_name(cmd.cli_ctx, registry_name, resource_group_name)
     subscription_id = get_subscription_id(cmd.cli_ctx)
 
