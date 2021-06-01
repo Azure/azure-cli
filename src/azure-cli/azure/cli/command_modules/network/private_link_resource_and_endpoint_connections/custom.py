3935962fcb7c73145ade68899f4a3844cb050df5
Yishi Wang <yishiwang@microsoft.com>
2021-06-01 15:02:28 +0800

    {Keyvault} Support managed HSM private link operations (#18273)
    * private link resource
    
    * private endpoint connection
    
    * tests
    
    * network module tests
    
    * linter
    
    * merge network changes

--- src/azure-cli/azure/cli/command_modules/network/private_link_resource_and_endpoint_connections/custom.py
+++ src/azure-cli/azure/cli/command_modules/network/private_link_resource_and_endpoint_connections/custom.py
@@ -34,6 +34,7 @@ def register_providers():
     _register_one_provider("Microsoft.EventHub/namespaces", "2018-01-01-preview", True)
     _register_one_provider("Microsoft.HealthcareApis/services", "2020-03-30", True)
     _register_one_provider('microsoft.insights/privateLinkScopes', '2019-10-17-preview', True)
+    _register_one_provider('Microsoft.KeyVault/managedHSMs', '2021-04-01-preview', True)
     _register_one_provider('Microsoft.Keyvault/vaults', '2019-09-01', False)
     # "Microsoft.Media/mediaservices", "2020-05-01", True
     # "Microsoft.Migrate/assessmentProjects", "2020-05-01-preview", False
