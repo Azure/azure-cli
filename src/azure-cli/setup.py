35a72f8108ca79d0c7698ec763895d9e1ab63fee
Daeun Yim <daeunyim@microsoft.com>
2021-06-02 17:09:42 -0700

    fix setup py

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -197,8 +197,8 @@ setup(
         ],
         'azure.cli.command_modules.rdbms': [
             'randomname/adjectives.txt',
-            'randomname/nouns.txt'
-            'templates/mysql_githubaction_template.yaml'
+            'randomname/nouns.txt',
+            'templates/mysql_githubaction_template.yaml',
             'templates/postgresql_githubaction_template.yaml'
         ]
     },
