53d16a17a5f818028246cf31eb113fdbf7732156
songlu <442586197@QQ.COM>
2021-06-03 17:05:22 +0800

    Update azcopy.py

--- src/azure-cli/azure/cli/command_modules/storage/operations/azcopy.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/azcopy.py
@@ -9,7 +9,8 @@ from ..azcopy.util import AzCopy, client_auth_for_azcopy, login_auth_for_azcopy,
 # pylint: disable=too-many-statements, too-many-locals, unused-argument
 def storage_copy(source, destination, put_md5=None, recursive=None, blob_type=None,
                  preserve_s2s_access_tier=None, content_type=None, follow_symlinks=None,
-                 exclude_pattern=None, include_pattern=None, exclude_path=None, include_path=None, cap_mbps=None, **kwargs):
+                 exclude_pattern=None, include_pattern=None, exclude_path=None, include_path=None,
+                 cap_mbps=None, **kwargs):
 
     azcopy = AzCopy()
     flags = []
