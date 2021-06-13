f80d3d2a551ca16c05d2234a731e78d04d23f8a4
Mads Damgård <madsd@microsoft.com>
2021-06-13 12:08:59 +0200

    quotes in help text

--- src/azure-cli/azure/cli/command_modules/appservice/_help.py
+++ src/azure-cli/azure/cli/command_modules/appservice/_help.py
@@ -1246,7 +1246,7 @@ examples:
   - name: turn on "alwaysOn"
     text: >
         az webapp config set -g MyResourceGroup -n MyUniqueApp --always-on true
-  - name: turn on "alwaysOn" through a json with content "{"alwaysOn", true}"
+  - name: turn on "alwaysOn" through a json with content '{"alwaysOn", true}'
     text: >
         az webapp config set -g MyResourceGroup -n MyUniqueApp --generic-configurations '{"alwaysOn": true}'
 
