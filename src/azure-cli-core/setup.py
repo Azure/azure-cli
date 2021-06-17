5cbb798ac28c07ef3453be4f960fce8d17c81f4e
Jiashuo Li <4003950+jiasli@users.noreply.github.com>
2021-06-17 12:52:00 +0800

    {Packaging} Remove full Release History from project description (#18485)

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -72,14 +72,12 @@ TESTS_REQUIRE = [
 
 with open('README.rst', 'r', encoding='utf-8') as f:
     README = f.read()
-with open('HISTORY.rst', 'r', encoding='utf-8') as f:
-    HISTORY = f.read()
 
 setup(
     name='azure-cli-core',
     version=VERSION,
     description='Microsoft Azure Command-Line Tools Core Module',
-    long_description=README + '\n\n' + HISTORY,
+    long_description=README,
     license='MIT',
     author='Microsoft Corporation',
     author_email='azpycli@microsoft.com',
