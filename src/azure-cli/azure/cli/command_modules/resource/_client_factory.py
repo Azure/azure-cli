fa7339dd5daefdb1553f3f6810bdbc81126d5dce
Kerwin(Kaihui) Sun <sunkaihuisos@gmail.com>
2021-06-23 13:58:58 +0800

    [Resource] Third party S2S Consent/Permission Enumeration (#18433)
    * feature permission list
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * Apply suggestions from code review
    
    * fix test and help info
    
    * fix
    
    Co-authored-by: Yu Chen <ychenu@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/resource/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/resource/_client_factory.py
@@ -4,10 +4,10 @@
 # --------------------------------------------------------------------------------------------
 
 
-def _resource_client_factory(cli_ctx, **_):
+def _resource_client_factory(cli_ctx, api_version=None, **_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.cli.core.profiles import ResourceType
-    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
+    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES, api_version=api_version)
 
 
 def _resource_feature_client_factory(cli_ctx, **_):
@@ -72,8 +72,8 @@ def cf_resources(cli_ctx, _):
     return _resource_client_factory(cli_ctx).resources
 
 
-def cf_providers(cli_ctx, _):
-    return _resource_client_factory(cli_ctx).providers
+def cf_providers(cli_ctx, _, api_version=None):
+    return _resource_client_factory(cli_ctx, api_version=api_version).providers
 
 
 def cf_tags(cli_ctx, _):
