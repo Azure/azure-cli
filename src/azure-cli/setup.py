d2236e6477dbb92f84e74e939000f369f899e3d4
songlu <37168047+PARADISSEEKR@users.noreply.github.com>
2021-06-28 13:13:57 +0800

    [Billing] Migrate billing to track2 SDK (#18608)
    * billing 1.0.0->6.0.0
    
    * Update custom.py
    
    * parameter

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -69,7 +69,7 @@ DEPENDENCIES = [
     'azure-mgmt-authorization~=0.61.0',
     'azure-mgmt-batchai~=2.0',
     'azure-mgmt-batch~=15.0.0',
-    'azure-mgmt-billing==1.0.0',
+    'azure-mgmt-billing==6.0.0',
     'azure-mgmt-botservice~=0.3.0',
     'azure-mgmt-cdn==11.0.0',
     'azure-mgmt-cognitiveservices~=12.0.0',
