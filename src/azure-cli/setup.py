8b75ae5319e071ddf428d5c74aabcc536059933d
abalogun-dev <80120000+abalogun-dev@users.noreply.github.com>
2021-07-09 04:45:24 -0400

    {IOT} Iot central track 2 migration (#18717)
    * track 2 changes
    
    * Rerecord yaml files that service team doesn't have permission
    
    * record of test_certificate and test_iotcental
    
    * Add yaml file for test_dps_lifecycle
    
    Co-authored-by: zhoxing-ms <Zhou.Xing@microsoft.com>

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -91,7 +91,7 @@ DEPENDENCIES = [
     'azure-mgmt-extendedlocation~=1.0.0b2',
     'azure-mgmt-hdinsight~=8.0.0',
     'azure-mgmt-imagebuilder~=0.4.0',
-    'azure-mgmt-iotcentral~=4.1.0',
+    'azure-mgmt-iotcentral~=9.0.0b1',
     'azure-mgmt-iothub==2.0.0',
     'azure-mgmt-iothubprovisioningservices~=0.2.0',
     'azure-mgmt-keyvault==9.0.0',
