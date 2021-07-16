6bd29e5ddaf07b61a219fafb1588c2c288b8624c
zesluo <zesluo@microsoft.com>
2021-07-16 15:54:07 +0800

    [Synapse]Workspace key update failure (#18719)
    * 'workspacekeyupdatefailure'
    
    * 'reformat'
    
    * 'edithelp'
    
    * 'format'

--- src/azure-cli/azure/cli/command_modules/synapse/_help.py
+++ src/azure-cli/azure/cli/command_modules/synapse/_help.py
@@ -577,7 +577,8 @@ examples:
 
 helps['synapse workspace key update'] = """
 type: command
-short-summary: Update a workspace's key or update the state of key to change the workspace state from pending to success state when the workspace is first being provisioned.
+short-summary: Update a workspace's key.
+long-summary: Activate a workspace and change it's state from pending to success state when the workspace is first being provisioned through updating a workspace's key. Switching to another CMK after activating the workspace needs to execute 'az synapse workspace update' instead.
 examples:
   - name: Update a workspace's key.
     text: |-
