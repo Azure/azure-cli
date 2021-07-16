4a356929f04c1a75f944c968ca26533558dc0535
Vidhi-2510 <52592382+Vidhi-2510@users.noreply.github.com>
2021-07-16 13:06:05 +0530

    [BACKUP] Added support for Customer Managed Keys(CMK) for Recovery Services Vault (#18733)
    * Merged with commit added support for CMK feature
    
    * Merged with commit Changes made
    
    * Merged with commit More changes
    
    * Changes with new version
    
    * Linter file changed
    
    * Changed test file
    
    * encryption test changed
    
    * Help Text Changed
    
    * Changes in command
    
    * help_text changed again
    
    * modified param names based on review comments
    
    * Encryption examples added
    
    Co-authored-by: Vidhi Katyal <t-vkatyal@microsoft.com>
    Co-authored-by: Himanshu Agarwal <hiaga@microsoft.com>

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -110,7 +110,7 @@ DEPENDENCIES = [
     'azure-mgmt-privatedns~=1.0.0',
     'azure-mgmt-rdbms~=8.1.0b4',
     'azure-mgmt-recoveryservicesbackup~=0.12.0',
-    'azure-mgmt-recoveryservices~=1.0.0',
+    'azure-mgmt-recoveryservices~=2.0.0',
     'azure-mgmt-redhatopenshift==0.1.0',
     'azure-mgmt-redis~=7.0.0rc1',
     'azure-mgmt-relay~=0.1.0',
