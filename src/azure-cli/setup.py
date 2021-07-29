ac3b190d4d6860718e2b19959946a5452e8e356d
Daeun Yim <69321306+DaeunYim@users.noreply.github.com>
2021-07-28 23:59:53 -0700

    [RDBMS] PostgreSQL, MySQL migration to GA API (#18921)
    * migrated to PG GA API
    
    * mysql migration initial commit
    
    * mysql migration and vnet subnet
    
    * fix params and mysql ga migration
    
    * add tests for GA changes
    
    * linter issue resolve
    
    * linter issue fix2
    
    * linter fix 3
    
    * style linter issue fix
    
    * test recordings
    
    * test fix to network and resource
    
    * test fiex 2
    
    * add validator update test
    
    * trigger CI
    
    * Disable geo restore

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -108,7 +108,7 @@ DEPENDENCIES = [
     'azure-mgmt-network~=19.0.0',
     'azure-mgmt-policyinsights~=1.0.0',
     'azure-mgmt-privatedns~=1.0.0',
-    'azure-mgmt-rdbms~=8.1.0b4',
+    'azure-mgmt-rdbms~=9.1.0b1',
     'azure-mgmt-recoveryservicesbackup~=0.14.0',
     'azure-mgmt-recoveryservices~=2.0.0',
     'azure-mgmt-redhatopenshift==0.1.0',
