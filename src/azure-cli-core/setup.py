957cfd88d5a011520366410a7e52e01401294b79
jiasli <4003950+jiasli@users.noreply.github.com>
2021-08-17 15:34:38 +0800

    azure-core==1.17.0

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -47,7 +47,7 @@ DEPENDENCIES = [
     'argcomplete~=1.8',
     'azure-cli-telemetry==1.0.6.*',
     'azure-common~=1.1',
-    'azure-core==1.16.0',
+    'azure-core==1.17.0',
     'azure-identity==1.6.0b3',
     'azure-mgmt-core==1.3.0b3',
     'cryptography>=3.2,<3.4',
