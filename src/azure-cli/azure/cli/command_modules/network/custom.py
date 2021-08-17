795fc4bbf8877664078b7bf41792ef75955ea073
aavalang@microsoft.com <aavalang@microsoft.com>
2021-08-17 11:20:24 -0700

    fixing pylint errors

--- src/azure-cli/azure/cli/command_modules/network/custom.py
+++ src/azure-cli/azure/cli/command_modules/network/custom.py
@@ -26,11 +26,6 @@ from azure.cli.command_modules.network.zone_file.make_zone_file import make_zone
 
 from .tunnel import TunnelServer
 
-import threading
-import time
-import platform
-import subprocess
-
 logger = get_logger(__name__)
 
 
