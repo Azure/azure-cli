a6f3ef7cf1cbea2860cec3e20f79dae5946fdd68
00Kai0 <sunkaihuisos@gmail.com>
2021-08-17 10:09:41 +0800

    fix style

--- src/azure-cli/azure/cli/command_modules/network/custom.py
+++ src/azure-cli/azure/cli/command_modules/network/custom.py
@@ -2502,8 +2502,8 @@ def _check_aaaa_record_exist(record, exist_list):
 def _check_caa_record_exist(record, exist_list):
     for r in exist_list:
         if (r.flags == record.flags and
-            r.tag == record.tag and
-            r.value == record.value):
+                r.tag == record.tag and
+                r.value == record.value):
             return True
     return False
 
@@ -2518,7 +2518,7 @@ def _check_cname_record_exist(record, exist_list):
 def _check_mx_record_exist(record, exist_list):
     for r in exist_list:
         if (r.preference == record.preference and
-            r.exchange == record.exchange):
+                r.exchange == record.exchange):
             return True
     return False
 
@@ -2540,9 +2540,9 @@ def _check_ptr_record_exist(record, exist_list):
 def _check_srv_record_exist(record, exist_list):
     for r in exist_list:
         if (r.priority == record.priority and
-            r.weight == record.weight and
-            r.port == record.port and
-            r.target == record.target):
+                r.weight == record.weight and
+                r.port == record.port and
+                r.target == record.target):
             return True
     return False
 
