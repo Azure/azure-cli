f47c0431579f6f69a163fec3e7e539dfd9b53682
Joseph Porter <josephporter@microsoft.com>
2021-08-19 09:47:04 -0700

    reverting extra line

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -110,7 +110,6 @@ class ResourceType(Enum):  # pylint: disable=too-few-public-methods
     DATA_STORAGE_FILESHARE = ('azure.multiapi.storagev2.fileshare', None)
     DATA_STORAGE_QUEUE = ('azure.multiapi.storagev2.queue', None)
 
-
     def __init__(self, import_prefix, client_name):
         """Constructor.
 
