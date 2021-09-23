4cddb6f076090dc0f93953be0ed8252875a9c195
Yishi Wang <yishiwang@microsoft.com>
2021-09-23 10:44:38 +0800

    [Storage] `az storage container-rm create/update` : Add `--root-squash` to support enable nfsv3 root squash or all squash (#19549)
    * storgae module tests
    
    * other modules tests
    
    * container nfsv3 squash
    
    * add test

--- src/azure-cli/azure/cli/command_modules/storage/_params.py
+++ src/azure-cli/azure/cli/command_modules/storage/_params.py
@@ -1308,6 +1308,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
     for item in ['create', 'update']:
         with self.argument_context('storage container-rm {}'.format(item),
                                    resource_type=ResourceType.MGMT_STORAGE) as c:
+            from ._validators import validate_container_nfsv3_squash
+            t_root_squash = self.get_models('RootSquashType', resource_type=ResourceType.MGMT_STORAGE)
             c.argument('default_encryption_scope', options_list=['--default-encryption-scope', '-d'],
                        arg_group='Encryption Policy', min_api='2019-06-01',
                        help='Default the container to use specified encryption scope for all writes.')
@@ -1315,6 +1317,10 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
                        options_list=['--deny-encryption-scope-override', '--deny-override'],
                        arg_type=get_three_state_flag(), arg_group='Encryption Policy', min_api='2019-06-01',
                        help='Block override of encryption scope from the container default.')
+            c.extra('root_squash', arg_type=get_enum_type(t_root_squash), min_api='2021-06-01',
+                    help='Enable NFSv3 squash on blob container.', validator=validate_container_nfsv3_squash)
+            c.ignore('enable_nfs_v3_root_squash')
+            c.ignore('enable_nfs_v3_all_squash')
 
     with self.argument_context('storage container-rm list', resource_type=ResourceType.MGMT_STORAGE) as c:
         c.argument('account_name', storage_account_type, id_part=None)
