837083e5df3ec0f4270eda248853c4551ff0a5b8
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2021-09-24 10:09:11 +0800

    lint

--- src/azure-cli/azure/cli/command_modules/storage/_validators.py
+++ src/azure-cli/azure/cli/command_modules/storage/_validators.py
@@ -1849,10 +1849,11 @@ def validate_policy(namespace):
         logger.warning("\nPlease do not specify --expiry and --permissions if they are already specified in your "
                        "policy.")
 
+
 def validate_immutability_arguments(namespace):
     from azure.cli.core.azclierror import InvalidArgumentValueError
     if any([namespace.immutability_period_since_creation_in_days,
-                namespace.immutability_policy_state, namespace.allow_protected_append_writes is not None]):
+            namespace.immutability_policy_state, namespace.allow_protected_append_writes is not None]):
         if not all([namespace.immutability_period_since_creation_in_days,
                     namespace.immutability_policy_state, namespace.allow_protected_append_writes is not None]):
             raise InvalidArgumentValueError("Incorrect usage: To enable account level immutability, "
@@ -1862,11 +1863,10 @@ def validate_immutability_arguments(namespace):
     if namespace.enable_vlw is not None:
         if not namespace.enable_vlw:
             if any([namespace.immutability_period_since_creation_in_days,
-                       namespace.immutability_policy_state, namespace.allow_protected_append_writes is not None]):
+                    namespace.immutability_policy_state, namespace.allow_protected_append_writes is not None]):
                 raise InvalidArgumentValueError("Incorrect usage: To enable account level immutability, "
                                                 "need to specify --enable_vlw true. "
                                                 "Cannot set --enable_vlw to false and specify "
                                                 "--immutability-period --immutability-state "
                                                 "--allow-append")
-
 
