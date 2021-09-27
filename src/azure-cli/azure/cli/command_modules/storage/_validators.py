ff45af197ed69d93281039e69852112b146a2367
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2021-09-27 09:38:38 +0800

    [Storage] Fix #17858: `az storage blob upload`: make --name optional (#19553)
    * Allow uploading blob without specifying the file name, instead infer from local file name
    
    * fix test: recorded test would fail because of random path name
    
    * lint
    
    * use os.path.basename instead

--- src/azure-cli/azure/cli/command_modules/storage/_validators.py
+++ src/azure-cli/azure/cli/command_modules/storage/_validators.py
@@ -1869,3 +1869,8 @@ def validate_policy(namespace):
     if namespace.id is not None:
         logger.warning("\nPlease do not specify --expiry and --permissions if they are already specified in your "
                        "policy.")
+
+
+def validate_blob_name_for_upload(namespace):
+    if not namespace.blob_name:
+        namespace.blob_name = os.path.basename(namespace.file_path)
