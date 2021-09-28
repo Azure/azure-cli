d7b432871657a79f0dc6b0ac97d86757c81db3e4
jiasli <4003950+jiasli@users.noreply.github.com>
2021-09-28 17:40:23 +0800

    Fix tests for expiresOn

--- src/azure-cli/azure/cli/command_modules/profile/tests/latest/test_profile_custom.py
+++ src/azure-cli/azure/cli/command_modules/profile/tests/latest/test_profile_custom.py
@@ -35,7 +35,7 @@ class ProfileCommandTest(unittest.TestCase):
         cmd = mock.MagicMock()
         cmd.cli_ctx = DummyCli()
 
-        get_raw_token_mock.return_value = (['bearer', 'token123', {'expiresOn': 1593497681}], 'sub123', 'tenant123')
+        get_raw_token_mock.return_value = (['bearer', 'token123', {'expiresOn': '2100-01-01'}], 'sub123', 'tenant123')
 
         result = get_access_token(cmd)
 
@@ -44,7 +44,7 @@ class ProfileCommandTest(unittest.TestCase):
         expected_result = {
             'tokenType': 'bearer',
             'accessToken': 'token123',
-            'expiresOn': 1593497681,
+            'expiresOn': '2100-01-01',
             'subscription': 'sub123',
             'tenant': 'tenant123'
         }
@@ -53,7 +53,7 @@ class ProfileCommandTest(unittest.TestCase):
         # assert it takes customized resource, subscription
         resource = 'https://graph.microsoft.com/'
         subscription_id = '00000001-0000-0000-0000-000000000000'
-        get_raw_token_mock.return_value = (['bearer', 'token123', {'expiresOn': 1593497681}], subscription_id,
+        get_raw_token_mock.return_value = (['bearer', 'token123', {'expiresOn': '2100-01-01'}], subscription_id,
                                            'tenant123')
         result = get_access_token(cmd, subscription=subscription_id, resource=resource)
         get_raw_token_mock.assert_called_with(mock.ANY, resource, None, subscription_id, None)
@@ -65,12 +65,12 @@ class ProfileCommandTest(unittest.TestCase):
 
         # test get token with tenant
         tenant_id = '00000000-0000-0000-0000-000000000000'
-        get_raw_token_mock.return_value = (['bearer', 'token123', {'expiresOn': 1593497681}], None, tenant_id)
+        get_raw_token_mock.return_value = (['bearer', 'token123', {'expiresOn': '2100-01-01'}], None, tenant_id)
         result = get_access_token(cmd, tenant=tenant_id)
         expected_result = {
             'tokenType': 'bearer',
             'accessToken': 'token123',
-            'expiresOn': 1593497681,
+            'expiresOn': '2100-01-01',
             'tenant': tenant_id
         }
         self.assertEqual(result, expected_result)
