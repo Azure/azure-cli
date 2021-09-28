c0113064d182b9fb3a937071b11f679d74f79869
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2021-09-28 10:35:05 +0800

    fix test

--- src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_container_legal_hold.py
+++ src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_container_legal_hold.py
@@ -12,6 +12,9 @@ class StorageLegalHold(ScenarioTest):
     @AllowLargeResponse()
     @ResourceGroupPreparer()
     def test_legal_hold(self, resource_group):
+        storage_account = self.create_random_name('clistorage', 20)
+        self.cmd('storage account create -g {} -n {} --kind StorageV2'.format(
+            resource_group, storage_account))
         container_name = 'container1'
         self.cmd('storage container create --account-name {} -n {} --metadata k1=v1 k2=v2'.format(storage_account, container_name))
 
