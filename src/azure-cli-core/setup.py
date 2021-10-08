dca385b68da5ead70dee4d8e1d5f5ed83a3955b5
jiasli <4003950+jiasli@users.noreply.github.com>
2021-10-08 14:18:01 +0800

    Bump msal to 1.15.0

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -51,7 +51,7 @@ DEPENDENCIES = [
     'humanfriendly>=4.7,<10.0',
     'jmespath',
     'knack~=0.8.2',
-    'msal>=1.14.0,<2.0.0',
+    'msal>=1.15.0,<2.0.0',
     'paramiko>=2.0.8,<3.0.0',
     'pkginfo>=1.5.0.1',
     'PyJWT>=2.1.0',
