75a3eb4ad038a7f26946f7ab88dd50afde542f57
王泽霖 <zelinwang@microsoft.com>
2021-10-13 13:05:48 +0800

    modify help examples

--- src/azure-cli/azure/cli/command_modules/vm/_help.py
+++ src/azure-cli/azure/cli/command_modules/vm/_help.py
@@ -2529,14 +2529,15 @@ examples:
     text: az vm update -n name -g group --add networkProfile.networkInterfaces primary=false id={NIC_ID}
   - name: Remove the fourth NIC from a VM.
     text: az vm update -n name -g group --remove networkProfile.networkInterfaces 3
-  - name: Add an existing VM to a dedicated host or a dedicated host group
+  - name: Add an existing VM to a dedicated host
     text: |-
         az vm deallocate -n name -g group
-
         az vm update -n name -g group --host my-host
-        or
+        az vm start -n name -g group
+  - name: Add an existing VM to a dedicated host group
+    text: |-
+        az vm deallocate -n name -g group
         az vm update -n name -g group --host-group my-host-group
-
         az vm start -n name -g group
 """
 
