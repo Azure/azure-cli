91022d0ff6fc2f443ec71e37203d6ee12918948d
Jiashuo Li <4003950+jiasli@users.noreply.github.com>
2021-10-20 19:00:13 +0800

    Update msal_authentication.py

--- src/azure-cli-core/azure/cli/core/auth/msal_authentication.py
+++ src/azure-cli-core/azure/cli/core/auth/msal_authentication.py
@@ -41,9 +41,7 @@ class UserCredential(PublicClientApplication):
         accounts = self.get_accounts(username)
 
         if not accounts:
-            raise CLIError("User {} doesn't exist in the credential cache. The user could have been logged out by "
-                           "another application that uses Single Sign-On. "
-                           "Please run `az login` to re-login.".format(username))
+            raise CLIError("User {} does not exist in the MSAL token cache. Please run `az login`.".format(username))
 
         if len(accounts) > 1:
             raise CLIError("Found multiple accounts with the same username. Please report to us via Github: "
