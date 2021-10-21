711b50ea6c635e771cb194c3fbbd16460271dacc
Yu Chen <ychenu@microsoft.com>
2021-10-21 14:54:27 +0800

    [Compute] : Support in-guest patching

--- src/azure-cli/azure/cli/command_modules/vm/_params.py
+++ src/azure-cli/azure/cli/command_modules/vm/_params.py
@@ -635,6 +635,12 @@ def load_arguments(self, _):
                         "value is 2020-11-01.")
         c.argument('enable_spot_restore', arg_type=get_three_state_flag(), min_api='2021-04-01', help='Enable the Spot-Try-Restore feature where evicted VMSS SPOT instances will be tried to be restored opportunistically based on capacity availability and pricing constraints')
         c.argument('spot_restore_timeout', min_api='2021-04-01', help='Timeout value expressed as an ISO 8601 time duration after which the platform will not try to restore the VMSS SPOT instances')
+        c.argument('enable_agent', arg_type=get_three_state_flag(), min_api='2018-06-01',
+                   help='Indicates whether virtual machine agent should be provisioned on the virtual machine. When this property is not specified, default behavior is to set it to true. This will ensure that VM Agent is installed on the VM so that extensions can be added to the VM later')
+        c.argument('enable_auto_update', arg_type=get_three_state_flag(), min_api='2020-06-01',
+                   help='Indicate whether Automatic Updates is enabled for the Windows virtual machine')
+        c.argument('patch_mode', arg_type=get_enum_type(['AutomaticByOS', 'AutomaticByPlatform', 'Manual', 'ImageDefault']), min_api='2020-12-01',
+                   help='Mode of in-guest patching to IaaS virtual machine. Allowed values for Windows VM: AutomaticByOS, AutomaticByPlatform, Manual. Allowed values for Linux VM: AutomaticByPlatform, ImageDefault. Manual - You control the application of patches to a virtual machine. You do this by applying patches manually inside the VM. In this mode, automatic updates are disabled; the paramater --enable-auto-update must be false. AutomaticByOS - The virtual machine will automatically be updated by the OS. The parameter --enable-auto-update must be true. AutomaticByPlatform - the virtual machine will automatically updated by the OS. ImageDefault - The virtual machine\'s default patching configuration is used. The parameter --enable-agent and --enable-auto-update must be true')
 
     with self.argument_context('vmss create', arg_group='Network Balancer') as c:
         LoadBalancerSkuName = self.get_models('LoadBalancerSkuName', resource_type=ResourceType.MGMT_NETWORK)
--- src/azure-cli/azure/cli/command_modules/vm/_template_builder.py
+++ src/azure-cli/azure/cli/command_modules/vm/_template_builder.py
@@ -832,7 +832,8 @@ def build_vmss_resource(cmd, name, computer_name_prefix, location, tags, overpro
                         max_unhealthy_upgraded_instance_percent=None, pause_time_between_batches=None,
                         enable_cross_zone_upgrade=None, prioritize_unhealthy_instances=None, edge_zone=None,
                         orchestration_mode=None, user_data=None, network_api_version=None,
-                        enable_spot_restore=None, spot_restore_timeout=None, capacity_reservation_group=None):
+                        enable_spot_restore=None, spot_restore_timeout=None, capacity_reservation_group=None,
+                        enable_auto_update=None, patch_mode=None, enable_agent=None):
 
     # Build IP configuration
     ip_configuration = {}
@@ -994,6 +995,38 @@ def build_vmss_resource(cmd, name, computer_name_prefix, location, tags, overpro
 
     if secrets:
         os_profile['secrets'] = secrets
+
+    if enable_agent is not None:
+        if os_type.lower() == 'linux':
+            if 'linuxConfiguration' not in os_profile:
+                os_profile['linuxConfiguration'] = {}
+            os_profile['linuxConfiguration']['provisionVMAgent'] = enable_agent
+        elif os_type.lower() == 'windows':
+            if 'windowsConfiguration' not in os_profile:
+                os_profile['windowsConfiguration'] = {}
+            os_profile['windowsConfiguration']['provisionVMAgent'] = enable_agent
+
+    if enable_auto_update is not None and os_type.lower() == 'windows':
+        os_profile['windowsConfiguration']['enableAutomaticUpdates'] = enable_auto_update
+
+    # Windows patch settings
+    if patch_mode is not None and os_type.lower() == 'windows':
+        if patch_mode.lower() not in ['automaticbyos', 'automaticbyplatform', 'manual']:
+            raise ValidationError(
+                'Invalid value of --patch-mode for Windows VMSS. Valid values are AutomaticByOS, '
+                'AutomaticByPlatform, Manual.')
+        os_profile['windowsConfiguration']['patchSettings'] = {
+            'patchMode': patch_mode
+        }
+
+    # Linux patch settings
+    if patch_mode is not None and os_type.lower() == 'linux':
+        if patch_mode.lower() not in ['automaticbyplatform', 'imagedefault']:
+            raise ValidationError(
+                'Invalid value of --patch-mode for Linux VMSS. Valid values are AutomaticByPlatform, ImageDefault.')
+        os_profile['linuxConfiguration']['patchSettings'] = {
+            'patchMode': patch_mode
+        }
 
     # Build VMSS
     nic_config = {}
