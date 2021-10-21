1595b8c7572d0a34495ea27afce4cf09430b69cf
Major Hayden <major@redhat.com>
2021-10-21 08:32:50 -0500

    {Packaging} Update pytz to 2021.3
    The pytz version in azure-cli is pinned to an old version from 2019. The
    2021.3 version contains lots of time zone updates and still works with
    Python 2.x versions.
    
    Signed-off-by: Major Hayden <major@redhat.com>

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -143,7 +143,7 @@ DEPENDENCIES = [
     'packaging~=20.9',
     'PyGithub~=1.38',
     'PyNaCl~=1.4.0',
-    'pytz==2019.1',
+    'pytz==2021.3',
     'scp~=0.13.2',
     'semver==2.13.0',
     'six>=1.10.0',  # six is still used by countless extensions
