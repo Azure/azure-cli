fc101c43044c5bfc8491f03d8d7d19c91a0ce5e8
vivek-microsoft <32766101+vivek-microsoft@users.noreply.github.com>
2021-10-27 06:05:44 -0700

    [CosmosDB] `az managed-cassandra cluster/data-center`: GA support for managed cassandra cluster and data center (#19962)
    * managed-cassandra initial commit
    
    * remove preview features
    
    * bugfix
    
    * bugfix on cassandra_stop_start
    
    * update invoke-command arguments and remove backup commands
    
    * bugfix
    
    * addressing review comment and adding test files
    
    * adding more tests and updating recording file
    
    * renaming file
    
    * make linter happy
    
    * minor fix
    
    * rebase diff
    
    * make style checker happy
    
    * style: remove whitespace
    
    * update test_managed_cassandra_cluster_without_datacenters.yaml
    
    * update tests/latest/recordings/test_managed_cassandra_verify_lists.yaml
    
    * update service name
    
    Co-authored-by: Vivek Sundararajan <s.vivek.rajan@gmail.com>

--- src/azure-cli/azure/cli/command_modules/cosmosdb/_params.py
+++ src/azure-cli/azure/cli/command_modules/cosmosdb/_params.py
@@ -19,10 +19,14 @@ from azure.cli.command_modules.cosmosdb._validators import (
     validate_role_definition_id,
     validate_fully_qualified_role_definition_id,
     validate_role_assignment_id,
-    validate_scope)
+    validate_scope,
+    validate_gossip_certificates,
+    validate_client_certificates,
+    validate_seednodes,
+    validate_node_count)
 
 from azure.cli.command_modules.cosmosdb.actions import (
-    CreateLocation, CreateDatabaseRestoreResource, UtcDatetimeAction)
+    CreateLocation, CreateDatabaseRestoreResource, UtcDatetimeAction, InvokeCommandArgumentsAddAction)
 from azure.cli.command_modules.cosmosdb.custom import (
     CosmosKeyTypes)
 
@@ -422,3 +426,85 @@ def load_arguments(self, _):
     # Account locations
     with self.argument_context('cosmosdb locations show') as c:
         c.argument('location', options_list=['--location', '-l'], help="Name of the location", required=True)
+
+    # Managed Cassandra Cluster
+    for scope in [
+            'managed-cassandra cluster create',
+            'managed-cassandra cluster update',
+            'managed-cassandra cluster show',
+            'managed-cassandra cluster delete',
+            'managed-cassandra cluster deallocate',
+            'managed-cassandra cluster start',
+            'managed-cassandra cluster status',
+            'managed-cassandra cluster invoke-command']:
+        with self.argument_context(scope) as c:
+            c.argument('cluster_name', options_list=['--cluster-name', '-c'], help="Cluster Name", required=True)
+
+    # Managed Cassandra Cluster
+    for scope in [
+            'managed-cassandra cluster create',
+            'managed-cassandra cluster update']:
+        with self.argument_context(scope) as c:
+            c.argument('tags', arg_type=tags_type)
+            c.argument('external_gossip_certificates', nargs='+', validator=validate_gossip_certificates, options_list=['--external-gossip-certificates', '-e'], help="A list of certificates that the managed cassandra data center's should accept.")
+            c.argument('cassandra_version', help="The version of Cassandra chosen.")
+            c.argument('authentication_method', arg_type=get_enum_type(['None', 'Cassandra']), help="Authentication mode can be None or Cassandra. If None, no authentication will be required to connect to the Cassandra API. If Cassandra, then passwords will be used.")
+            c.argument('hours_between_backups', help="The number of hours between backup attempts.")
+            c.argument('repair_enabled', help="Enables automatic repair.")
+            c.argument('client_certificates', nargs='+', validator=validate_client_certificates, help="If specified, enables client certificate authentication to the Cassandra API.")
+            c.argument('gossip_certificates', help="A list of certificates that should be accepted by on-premise data centers.")
+            c.argument('external_seed_nodes', nargs='+', validator=validate_seednodes, help="A list of ip addresses of the seed nodes of on-premise data centers.")
+            c.argument('identity_type', options_list=['--identity-type'], arg_type=get_enum_type(['None', 'SystemAssigned']), help="Type of identity used for Customer Managed Disk Key.")
+
+    # Managed Cassandra Cluster
+    with self.argument_context('managed-cassandra cluster create') as c:
+        c.argument('location', options_list=['--location', '-l'], help="Azure Location of the Cluster", required=True)
+        c.argument('delegated_management_subnet_id', options_list=['--delegated-management-subnet-id', '-s'], help="The resource id of a subnet where the ip address of the cassandra management server will be allocated. This subnet must have connectivity to the delegated_subnet_id subnet of each data center.", required=True)
+        c.argument('initial_cassandra_admin_password', options_list=['--initial-cassandra-admin-password', '-i'], help="The intial password to be configured when a cluster is created for authentication_method Cassandra.")
+        c.argument('restore_from_backup_id', help="The resource id of a backup. If provided on create, the backup will be used to prepopulate the cluster. The cluster data center count and node counts must match the backup.")
+        c.argument('cluster_name_override', help="If a cluster must have a name that is not a valid azure resource name, this field can be specified to choose the Cassandra cluster name. Otherwise, the resource name will be used as the cluster name.")
+
+    # Managed Cassandra Cluster
+    for scope in ['managed-cassandra cluster invoke-command']:
+        with self.argument_context(scope) as c:
+            c.argument('command_name', options_list=['--command-name'], help="The command which should be run", required=True)
+            c.argument('host', options_list=['--host'], help="IP address of the cassandra host to run the command on", required=True)
+            c.argument('arguments', options_list=['--arguments'], action=InvokeCommandArgumentsAddAction, nargs='+', help="The key=\"value\" of arguments for the command.")
+            c.argument('cassandra_stop_start', options_list=['--cassandra-stop-start'], arg_type=get_three_state_flag(), help="If true, stops cassandra before executing the command and then start it again.")
+            c.argument('readwrite', options_list=['--readwrite'], arg_type=get_three_state_flag(), help="If true, allows the command to *write* to the cassandra directory, otherwise read-only.")
+
+    # Managed Cassandra Datacenter
+    for scope in [
+            'managed-cassandra datacenter create',
+            'managed-cassandra datacenter update',
+            'managed-cassandra datacenter show',
+            'managed-cassandra datacenter delete']:
+        with self.argument_context(scope) as c:
+            c.argument('cluster_name', options_list=['--cluster-name', '-c'], help="Cluster Name", required=True)
+            c.argument('data_center_name', options_list=['--data-center-name', '-d'], help="Datacenter Name", required=True)
+
+    # Managed Cassandra Datacenter
+    for scope in [
+            'managed-cassandra datacenter create',
+            'managed-cassandra datacenter update']:
+        with self.argument_context(scope) as c:
+            c.argument('node_count', options_list=['--node-count', '-n'], validator=validate_node_count, help="The number of Cassandra virtual machines in this data center. The minimum value is 3.")
+            c.argument('base64_encoded_cassandra_yaml_fragment', options_list=['--base64-encoded-cassandra-yaml-fragment', '-b'], help="This is a Base64 encoded yaml file that is a subset of cassandra.yaml.  Supported fields will be honored and others will be ignored.")
+            c.argument('data_center_location', options_list=['--data-center-location', '-l'], help="The region where the virtual machine for this data center will be located.")
+            c.argument('delegated_subnet_id', options_list=['--delegated-subnet-id', '-s'], help="The resource id of a subnet where ip addresses of the Cassandra virtual machines will be allocated. This must be in the same region as data_center_location.")
+            c.argument('managed_disk_customer_key_uri', options_list=['--managed-disk-customer-key-uri', '-k'], help="Key uri to use for encryption of managed disks. Ensure the system assigned identity of the cluster has been assigned appropriate permissions(key get/wrap/unwrap permissions) on the key.")
+            c.argument('backup_storage_customer_key_uri', options_list=['--backup-storage-customer-key-uri', '-p'], help="Indicates the Key Uri of the customer key to use for encryption of the backup storage account.")
+
+    # Managed Cassandra Datacenter
+    with self.argument_context('managed-cassandra datacenter create') as c:
+        c.argument('data_center_location', options_list=['--data-center-location', '-l'], help="Azure Location of the Datacenter", required=True)
+        c.argument('delegated_subnet_id', options_list=['--delegated-subnet-id', '-s'], help="The resource id of a subnet where ip addresses of the Cassandra virtual machines will be allocated. This must be in the same region as data_center_location.", required=True)
+        c.argument('node_count', options_list=['--node-count', '-n'], validator=validate_node_count, help="The number of Cassandra virtual machines in this data center. The minimum value is 3.", required=True)
+        c.argument('sku', options_list=['--sku'], help="Virtual Machine SKU used for data centers. Default value is Standard_DS14_v2")
+        c.argument('disk_sku', options_list=['--disk-sku'], help="Disk SKU used for data centers. Default value is P30.")
+        c.argument('disk_capacity', options_list=['--disk-capacity'], help="Number of disk used for data centers. Default value is 4.")
+        c.argument('availability_zone', options_list=['--availability-zone', '-z'], arg_type=get_three_state_flag(), help="If the data center haves Availability Zone feature, apply it to the Virtual Machine ScaleSet that host the data center virtual machines.")
+
+    # Managed Cassandra Datacenter
+    with self.argument_context('managed-cassandra datacenter list') as c:
+        c.argument('cluster_name', options_list=['--cluster-name', '-c'], help="Cluster Name", required=True)
