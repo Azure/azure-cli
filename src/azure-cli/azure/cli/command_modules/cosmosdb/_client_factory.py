d93115b1ae7c46799cd6a0cfb1470125e03b08dc
Amit Singh <31076927+amisi01@users.noreply.github.com>
2021-10-26 19:01:35 -0700

    [CosmosDB] Update Backup API for fetching latest restorable timestamp.Also, Update database account backup redundancy. (#20007)
    * new changes
    
    * Update Backup API for fetching latest restorable timestamp.Also, Update database account backup redundancy.
    
    * Test Results
    
    * New Updates
    
    * Update help message
    
    * New Update
    
    * Fixing linter/style error and updating test results
    
    * Updating help message
    
    * Version update and also updated some test record files.
    
    * Updating test recordings
    
    * fix tests

--- src/azure-cli/azure/cli/command_modules/cosmosdb/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/cosmosdb/_client_factory.py
@@ -136,3 +136,7 @@ def cf_restorable_mongodb_collections(cli_ctx, _):
 
 def cf_restorable_mongodb_resources(cli_ctx, _):
     return cf_cosmosdb(cli_ctx).restorable_mongodb_resources
+
+
+def cf_db_locations(cli_ctx, _):
+    return cf_cosmosdb(cli_ctx).locations
