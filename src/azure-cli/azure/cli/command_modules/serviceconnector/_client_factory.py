dd6fd033358d6cb35d0a02711c8bb7a1bbc22de1
Houk <62928370+houk-ms@users.noreply.github.com>
2021-10-28 17:45:32 +0800

    {ServiceConnector} Add user token when KeyVault as a target resource (#19941)
    * draft for ServiceLinker
    
    * add sdk dependency
    
    * improve style and linter
    
    * clean codes
    
    * improvements
    
    * hide credentials in test
    
    * mark addon tests as live_only
    
    * hide token in header
    
    * improve interactive input
    
    * Ignore case when matching resource-id
    
    * add more examples in command help
    
    * apply suggestions in comments
    
    * improvements
    
    * leave --name to source resource
    
    * update code owners
    
    * add preview tag
    
    * Hack spring-cloud to app level
    
    * remove deployment paramter temporaryily
    
    * add user token header when KeyVault as target resource
    
    * remove sql as target resource
    
    * skip sql test
    
    * skip spring-cloud tests
    
    * support more target resources
    
    * skip spring-cloud test cases
    
    * add support for target resource: confluent-kafka
    
    * skip spring-cloud test
    
    * adjust target resources
    
    * get linker before validation
    
    * update target list
    
    * update tests

--- src/azure-cli/azure/cli/command_modules/serviceconnector/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/serviceconnector/_client_factory.py
@@ -3,21 +3,12 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from azure.cli.core._profile import Profile
-
 
 def cf_connection_cl(cli_ctx, *_):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.mgmt.servicelinker import MicrosoftServiceLinker
 
-    # pylint: disable=protected-access
-    # Hack: set custom header to work around OBO
-    profile = Profile(cli_ctx=cli_ctx)
-    creds, _, _ = profile.get_raw_token()
-
-    client = get_mgmt_service_client(cli_ctx, MicrosoftServiceLinker, subscription_bound=False)
-    client._client._config.headers_policy._headers['x-ms-cupertino-test-token'] = creds[1]
-    return client
+    return get_mgmt_service_client(cli_ctx, MicrosoftServiceLinker, subscription_bound=False)
 
 
 def cf_linker(cli_ctx, *_):
