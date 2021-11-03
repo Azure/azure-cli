270fe59c8ac8e61f4603c92f6dcb13fa4bc43797
kevinzz6 <632604666@qq.com>
2021-11-03 18:05:54 +0800

    add params for create spark pool

--- src/azure-cli/azure/cli/command_modules/synapse/manual/operations/sparkpool.py
+++ src/azure-cli/azure/cli/command_modules/synapse/manual/operations/sparkpool.py
@@ -2,11 +2,12 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-# pylint: disable=unused-argument, line-too-long
+# pylint: disable=unused-argument, line-too-long, too-many-locals
 from azure.cli.core.util import sdk_no_wait, read_file_content
-from azure.mgmt.synapse.models import BigDataPoolResourceInfo, AutoScaleProperties, AutoPauseProperties, LibraryRequirements, NodeSizeFamily, LibraryInfo
+from azure.mgmt.synapse.models import BigDataPoolResourceInfo, AutoScaleProperties, AutoPauseProperties, LibraryRequirements, NodeSizeFamily, LibraryInfo, SparkConfigProperties
 from .._client_factory import cf_synapse_client_workspace_factory
 from .artifacts import get_workspace_package
+from pathlib import Path
 
 
 # Synapse sparkpool
@@ -17,10 +18,9 @@ def get_spark_pool(cmd, client, resource_group_name, workspace_name, spark_pool_
 def create_spark_pool(cmd, client, resource_group_name, workspace_name, spark_pool_name,
                       spark_version, node_size, node_count,
                       node_size_family=NodeSizeFamily.memory_optimized.value, enable_auto_scale=None,
-                      min_node_count=None, max_node_count=None,
+                      min_node_count=None, max_node_count=None, Spark_Config_File_Path=None,
                       enable_auto_pause=None, delay=None, spark_events_folder="/events",
                       spark_log_folder="/logs", tags=None, no_wait=False):
-
     workspace_client = cf_synapse_client_workspace_factory(cmd.cli_ctx)
     workspace_object = workspace_client.get(resource_group_name, workspace_name)
     location = workspace_object.location
@@ -36,6 +36,17 @@ def create_spark_pool(cmd, client, resource_group_name, workspace_name, spark_po
     big_data_pool_info.auto_pause = AutoPauseProperties(enabled=enable_auto_pause,
                                                         delay_in_minutes=delay)
 
+    filename = Path(Spark_Config_File_Path).stem
+    try:
+        with open(Spark_Config_File_Path, 'r') as stream:
+            content = stream.read()
+    except:
+        from azure.cli.core.azclierror import InvalidArgumentValueError
+        err_msg = 'Spark config file path is invalid'
+        raise InvalidArgumentValueError(err_msg)
+    big_data_pool_info.spark_config_properties = SparkConfigProperties(content=content,
+                                                                       filename=filename)
+
     return sdk_no_wait(no_wait, client.begin_create_or_update, resource_group_name, workspace_name, spark_pool_name,
                        big_data_pool_info)
 
@@ -44,7 +55,7 @@ def update_spark_pool(cmd, client, resource_group_name, workspace_name, spark_po
                       node_size=None, node_count=None, enable_auto_scale=None,
                       min_node_count=None, max_node_count=None,
                       enable_auto_pause=None, delay=None,
-                      library_requirements=None,
+                      library_requirements=None, Spark_Config_File_Path=None,
                       package_action=None, package=None,
                       tags=None, force=False, no_wait=False):
     existing_spark_pool = client.get(resource_group_name, workspace_name, spark_pool_name)
@@ -92,6 +103,17 @@ def update_spark_pool(cmd, client, resource_group_name, workspace_name, spark_po
         if package_action == "Remove":
             existing_spark_pool.custom_libraries = [library for library in existing_spark_pool.custom_libraries if library.name not in package]
 
+    if Spark_Config_File_Path:
+        filename = Path(Spark_Config_File_Path).stem
+        try:
+            with open(Spark_Config_File_Path, 'r') as stream:
+                content = stream.read()
+        except:
+            from azure.cli.core.azclierror import InvalidArgumentValueError
+            err_msg = 'Spark config file path is invalid'
+            raise InvalidArgumentValueError(err_msg)
+        existing_spark_pool.spark_config_properties = SparkConfigProperties(content=content,
+                                                                            filename=filename)
     return sdk_no_wait(no_wait, client.begin_create_or_update, resource_group_name, workspace_name, spark_pool_name,
                        existing_spark_pool, force=force)
 
