b98ac41159ecebfe4d7e7beae0e5c54d6b96d300
jiasli <4003950+jiasli@users.noreply.github.com>
2021-11-09 10:48:16 +0800

    update message

--- src/azure-cli-core/azure/cli/core/auth/msal_authentication.py
+++ src/azure-cli-core/azure/cli/core/auth/msal_authentication.py
@@ -44,8 +44,9 @@ class UserCredential(PublicClientApplication):
         accounts = self.get_accounts(username)
 
         # MSAL should never return multiple account for the same username
-        assert len(accounts) <= 1, (f"Found multiple accounts with the same username '{username}': {accounts}\n"
-                                    "Please report to us via Github: https://github.com/Azure/azure-cli/issues/new")
+        if len(accounts) > 1:
+            raise CLIError(f"Found multiple accounts with the same username '{username}': {accounts}\n"
+                           "Please report to us via Github: https://github.com/Azure/azure-cli/issues/20168")
 
         if not accounts:
             raise CLIError("User '{}' does not exist in MSAL token cache. Run `az login`.".format(username))
