62084215d244fb4b63f96408ffa957ffbee7469d
Jiashuo Li <4003950+jiasli@users.noreply.github.com>
2021-11-23 17:13:32 +0800

    {Profile} `az account list`: Fix: `--refresh` returns `user` as `null` (#20430)

--- src/azure-cli-core/azure/cli/core/_profile.py
+++ src/azure-cli-core/azure/cli/core/_profile.py
@@ -633,7 +633,7 @@ class Profile:
                 if not subscriptions:
                     continue
 
-            consolidated = self._normalize_properties(subscription_finder.user_id,
+            consolidated = self._normalize_properties(user_name,
                                                       subscriptions,
                                                       is_service_principal)
             result += consolidated
@@ -731,8 +731,6 @@ class SubscriptionFinder:
     # An ARM client. It finds subscriptions for a user or service principal. It shouldn't do any
     # authentication work, but only find subscriptions
     def __init__(self, cli_ctx):
-
-        self.user_id = None  # will figure out after log user in
         self.cli_ctx = cli_ctx
         self.secret = None
         self._arm_resource_id = cli_ctx.cloud.endpoints.active_directory_resource_id
