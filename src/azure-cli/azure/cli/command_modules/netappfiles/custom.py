77c1abe77ddc4ddf5ea629e32bc9a2f3f8a12074
Audunn Baldvinsson <audunn.baldvinsson@netapp.com>
2021-11-29 10:59:44 +0000

    Fix style issues

--- src/azure-cli/azure/cli/command_modules/netappfiles/custom.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/custom.py
@@ -152,18 +152,18 @@ def create_volume(cmd, client, account_name, pool_name, volume_name, resource_gr
 
     # if NFSv4 is specified then the export policy must reflect this
     # the RP ordinarily only creates a default setting NFSv3.
-    if (protocol_types is not None):
+    if protocol_types is not None:
         rules = []
         isNfs41 = False
         isNfs3 = False
 
-        if("NFSv4.1" in protocol_types):
+        if "NFSv4.1" in protocol_types:
             isNfs41 = True
             if allowed_clients is None:
                 raise CLIError("Parameter allowed-clients needs to be set when protocol-type is NFSv4.1")
             if rule_index is None:
                 raise CLIError("Parameter rule-index needs to be set when protocol-type is NFSv4.1")
-        if("NFSv3" in protocol_types):
+        if "NFSv3" in protocol_types:
             isNfs3 = True
 
         export_policy = ExportPolicyRule(rule_index=rule_index, unix_read_only=unix_read_only,
