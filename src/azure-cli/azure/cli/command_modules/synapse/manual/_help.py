f294200088cb174183e5b271009f731d007b4a55
kevinzz6 <632604666@qq.com>
2021-12-21 13:38:01 +0800

    add definition file explanation

--- src/azure-cli/azure/cli/command_modules/synapse/manual/_help.py
+++ src/azure-cli/azure/cli/command_modules/synapse/manual/_help.py
@@ -1922,7 +1922,7 @@ helps['synapse managed-private-endpoints create'] = """
 type: command
 short-summary: Create a synapse managed private endpoints.
 examples:
-  - name: Create a synapse managed private endpoints.
+  - name: Create a synapse managed private endpoints. Definition file should contains privateLinkResourceId and groupId.
     text: |-
         az synapse managed-private-endpoints create --workspace-name testsynapseworkspace \\
           --pe-name testendpointname \\
