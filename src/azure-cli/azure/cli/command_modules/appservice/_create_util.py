d0d380ff199c0b4d42cff2822c8ab0f3ffc8504d
Silas Strawn <strawnsc@gmail.com>
2022-01-21 13:46:33 -0800

    remove erroneous import

--- src/azure-cli/azure/cli/command_modules/appservice/_create_util.py
+++ src/azure-cli/azure/cli/command_modules/appservice/_create_util.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from ensurepip import version
 import os
 import zipfile
 from random import randint
