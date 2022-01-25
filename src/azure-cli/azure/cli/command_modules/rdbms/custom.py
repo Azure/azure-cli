Azure/azure-cli

commit 50706170e298ddee5c99c9320729ba972371349b

[RDBMS] Management Plane updates for PostgreSQL and MySQL (#15177)

src/azure-cli/azure/cli/command_modules/rdbms/custom.py
@@ -368,6 +368,20 @@ def _server_update_set(client, resource_group_name, server_name, parameters):
     return client.update(resource_group_name, server_name, parameters)
 
 
+def _server_delete(cmd, client, resource_group_name, server_name):
+    database_engine = 'postgres'
+    if isinstance(client, MySqlServersOperations):
+        database_engine = 'mysql'
+
+    result = client.delete(resource_group_name, server_name)
+
+    if cmd.cli_ctx.local_context.is_on:
+        local_context_file = cmd.cli_ctx.local_context._get_local_context_file()  # pylint: disable=protected-access
+        local_context_file.remove_option('{} server'.format(database_engine), 'server_name')
+
+    return result
+
+
 def _get_sku_name(tier, family, capacity):
     return '{}_{}_{}'.format(SKU_TIER_MAP[tier], family, str(capacity))
 

src/azure-cli/azure/cli/command_modules/rdbms/__init__.py
@@ -6,6 +6,8 @@
 from azure.cli.core import AzCommandsLoader
 
 import azure.cli.command_modules.rdbms._help  # pylint: disable=unused-import
+import azure.cli.command_modules.rdbms._helptext_pg  # pylint: disable=unused-import
+import azure.cli.command_modules.rdbms._helptext_mysql  # pylint: disable=unused-import
 
 
 class RdbmsCommandsLoader(AzCommandsLoader):
@@ -31,7 +33,9 @@ class RdbmsCommandsLoader(AzCommandsLoader):
 
     def load_command_table(self, args):
         from azure.cli.command_modules.rdbms.commands import load_command_table
+        from azure.cli.command_modules.rdbms.flexible_server_commands import load_flexibleserver_command_table
         load_command_table(self, args)
+        load_flexibleserver_command_table(self, args)
         return self.command_table
 
     def load_arguments(self, command):

src/azure-cli/azure/cli/command_modules/rdbms/_client_factory.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.core.commands.client_factory import get_mgmt_service_client
+from azure.cli.core.profiles import ResourceType
 
 # CLIENT FACTORIES
 
@@ -31,7 +32,7 @@ def get_mariadb_management_client(cli_ctx, **_):
                 secret=getenv(CLIENT_SECRET),
                 tenant=getenv(TENANT_ID))
         else:
-            from msrest.authentication import Authentication    # pylint: disable=import-error
+            from msrest.authentication import Authentication  # pylint: disable=import-error
             credentials = Authentication()
 
         return MariaDBManagementClient(
@@ -59,7 +60,35 @@ def get_mysql_management_client(cli_ctx, **_):
                 secret=getenv(CLIENT_SECRET),
                 tenant=getenv(TENANT_ID))
         else:
-            from msrest.authentication import Authentication    # pylint: disable=import-error
+            from msrest.authentication import Authentication  # pylint: disable=import-error
+            credentials = Authentication()
+
+        return MySQLManagementClient(
+            subscription_id=getenv(SUB_ID_OVERRIDE),
+            base_url=rm_uri_override,
+            credentials=credentials)
+    # Normal production scenario.
+    return get_mgmt_service_client(cli_ctx, MySQLManagementClient)
+
+
+def get_mysql_flexible_management_client(cli_ctx, **_):
+    from os import getenv
+    from azure.mgmt.rdbms.mysql_flexibleservers import MySQLManagementClient
+
+    # Allow overriding resource manager URI using environment variable
+    # for testing purposes. Subscription id is also determined by environment
+    # variable.
+    rm_uri_override = getenv(RM_URI_OVERRIDE)
+    if rm_uri_override:
+        client_id = getenv(CLIENT_ID)
+        if client_id:
+            from azure.common.credentials import ServicePrincipalCredentials
+            credentials = ServicePrincipalCredentials(
+                client_id=client_id,
+                secret=getenv(CLIENT_SECRET),
+                tenant=getenv(TENANT_ID))
+        else:
+            from msrest.authentication import Authentication  # pylint: disable=import-error
             credentials = Authentication()
 
         return MySQLManagementClient(
@@ -87,7 +116,34 @@ def get_postgresql_management_client(cli_ctx, **_):
                 secret=getenv(CLIENT_SECRET),
                 tenant=getenv(TENANT_ID))
         else:
-            from msrest.authentication import Authentication    # pylint: disable=import-error
+            from msrest.authentication import Authentication  # pylint: disable=import-error
+            credentials = Authentication()
+
+        return PostgreSQLManagementClient(
+            subscription_id=getenv(SUB_ID_OVERRIDE),
+            base_url=rm_uri_override,
+            credentials=credentials)
+    # Normal production scenario.
+    return get_mgmt_service_client(cli_ctx, PostgreSQLManagementClient)
+
+
+def get_postgresql_flexible_management_client(cli_ctx, **_):
+    from os import getenv
+    from azure.mgmt.rdbms.postgresql_flexibleservers import PostgreSQLManagementClient
+    # Allow overriding resource manager URI using environment variable
+    # for testing purposes. Subscription id is also determined by environment
+    # variable.
+    rm_uri_override = getenv(RM_URI_OVERRIDE)
+    if rm_uri_override:
+        client_id = getenv(CLIENT_ID)
+        if client_id:
+            from azure.common.credentials import ServicePrincipalCredentials
+            credentials = ServicePrincipalCredentials(
+                client_id=client_id,
+                secret=getenv(CLIENT_SECRET),
+                tenant=getenv(TENANT_ID))
+        else:
+            from msrest.authentication import Authentication  # pylint: disable=import-error
             credentials = Authentication()
 
         return PostgreSQLManagementClient(
@@ -220,3 +276,53 @@ def cf_mysql_server_ad_administrators_operations(cli_ctx, _):
 
 def cf_postgres_server_ad_administrators_operations(cli_ctx, _):
     return get_postgresql_management_client(cli_ctx).server_administrators
+
+
+# Meru operations for flexible servers
+def cf_mysql_flexible_servers(cli_ctx, _):
+    return get_mysql_flexible_management_client(cli_ctx).servers
+
+
+def cf_mysql_flexible_firewall_rules(cli_ctx, _):
+    return get_mysql_flexible_management_client(cli_ctx).firewall_rules
+
+
+def cf_mysql_flexible_config(cli_ctx, _):
+    return get_mysql_flexible_management_client(cli_ctx).configurations
+
+
+def cf_mysql_flexible_db(cli_ctx, _):
+    return get_mysql_flexible_management_client(cli_ctx).databases
+
+
+def cf_mysql_flexible_replica(cli_ctx, _):
+    return get_mysql_flexible_management_client(cli_ctx).replicas
+
+
+def cf_mysql_flexible_location_capabilities(cli_ctx, _):
+    return get_mysql_flexible_management_client(cli_ctx).location_based_capabilities
+
+
+def cf_postgres_flexible_servers(cli_ctx, _):
+    return get_postgresql_flexible_management_client(cli_ctx).servers
+
+
+def cf_postgres_flexible_firewall_rules(cli_ctx, _):
+    return get_postgresql_flexible_management_client(cli_ctx).firewall_rules
+
+
+def cf_postgres_flexible_config(cli_ctx, _):
+    return get_postgresql_flexible_management_client(cli_ctx).configurations
+
+
+def cf_postgres_flexible_location_capabilities(cli_ctx, _):
+    return get_postgresql_flexible_management_client(cli_ctx).location_based_capabilities
+
+
+def resource_client_factory(cli_ctx, **_):
+    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_RESOURCE_RESOURCES)
+
+
+def network_client_factory(cli_ctx):
+    from azure.mgmt.network import NetworkManagementClient
+    return get_mgmt_service_client(cli_ctx, NetworkManagementClient, api_version="2018-08-01")

src/azure-cli/azure/cli/command_modules/rdbms/_flexible_server_util.py
@@ -1,0 +1,186 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=unused-argument, line-too-long
+
+import random
+from knack.log import get_logger
+from azure.cli.core.commands import LongRunningOperation, _is_poller
+from azure.cli.core.util import CLIError
+from azure.mgmt.resource.resources.models import ResourceGroup
+from ._client_factory import resource_client_factory
+
+logger = get_logger(__name__)
+
+DEFAULT_LOCATION = 'eastus'  # For testing: 'eastus2euap'
+
+
+def resolve_poller(result, cli_ctx, name):
+    if _is_poller(result):
+        return LongRunningOperation(cli_ctx, 'Starting {}'.format(name))(result)
+    return result
+
+
+def create_random_resource_name(prefix='azure', length=15):
+    append_length = length - len(prefix)
+    digits = [str(random.randrange(10)) for i in range(append_length)]
+    return prefix + ''.join(digits)
+
+
+def generate_missing_parameters(cmd, location, resource_group_name, server_name):
+    # if location is not passed as a parameter or is missing from local context
+    if location is None:
+        location = DEFAULT_LOCATION
+
+    # If resource group is there in local context, check for its existence.
+    resource_group_exists = True
+    if resource_group_name is not None:
+        logger.warning('Checking the existence of the resource group \'%s\'...', resource_group_name)
+        resource_group_exists = _check_resource_group_existence(cmd, resource_group_name)
+        logger.warning('Resource group \'%s\' exists ? : %s ', resource_group_name, resource_group_exists)
+
+    # If resource group is not passed as a param or is not in local context or the rg in the local context has been deleted
+    if not resource_group_exists or resource_group_name is None:
+        resource_group_name = _create_resource_group(cmd, location, resource_group_name)
+
+    # If servername is not passed, always create a new server - even if it is stored in the local context
+    if server_name is None:
+        server_name = create_random_resource_name('server')
+
+    # This is for the case when user does not pass a location but the resource group exists in the local context.
+    #  In that case, the location needs to be set to the location of the rg, not the default one.
+
+    # TODO: Fix this because it changes the default location even when I pass in a location param
+    # location = _update_location(cmd, resource_group_name)
+
+    return location, resource_group_name, server_name
+
+
+def generate_password(administrator_login_password):
+    import secrets
+    if administrator_login_password is None:
+        passwordLength = 16
+        special_character = random.choice('!@#,?;:$&*')
+        administrator_login_password = secrets.token_urlsafe(passwordLength)
+        random_position = random.randint(1, len(administrator_login_password) - 1)
+        administrator_login_password = administrator_login_password[
+            :random_position] + special_character + administrator_login_password[
+                random_position + 1:]
+    return administrator_login_password
+
+
+def create_firewall_rule(db_context, cmd, resource_group_name, server_name, start_ip, end_ip):
+    from datetime import datetime
+    # allow access to azure ip addresses
+    cf_firewall, logging_name = db_context.cf_firewall, db_context.logging_name  # NOQA pylint: disable=unused-variable
+    now = datetime.now()
+    firewall_name = 'FirewallIPAddress_{}-{}-{}_{}-{}-{}'.format(now.year, now.month, now.day, now.hour, now.minute,
+                                                                 now.second)
+    if start_ip == '0.0.0.0' and end_ip == '0.0.0.0':
+        logger.warning('Configuring server firewall rule, \'azure-access\', to accept connections from all '
+                       'Azure resources...')
+        firewall_name = 'AllowAllAzureServicesAndResourcesWithinAzureIps_{}-{}-{}_{}-{}-{}'.format(now.year, now.month,
+                                                                                                   now.day, now.hour,
+                                                                                                   now.minute,
+                                                                                                   now.second)
+    elif start_ip == end_ip:
+        logger.warning('Configuring server firewall rule to accept connections from \'%s\'...', start_ip)
+    else:
+        if start_ip == '0.0.0.0' and end_ip == '255.255.255.255':
+            firewall_name = 'AllowAll_{}-{}-{}_{}-{}-{}'.format(now.year, now.month, now.day, now.hour, now.minute,
+                                                                now.second)
+        logger.warning('Configuring server firewall rule to accept connections from \'%s\' to \'%s\'...', start_ip,
+                       end_ip)
+    firewall_client = cf_firewall(cmd.cli_ctx, None)
+
+    # Commenting out until firewall_id is properly returned from RP
+    # return resolve_poller(
+    #    firewall_client.create_or_update(resource_group_name, server_name, firewall_name , start_ip, end_ip),
+    #    cmd.cli_ctx, '{} Firewall Rule Create/Update'.format(logging_name))
+
+    firewall = firewall_client.create_or_update(resource_group_name, server_name, firewall_name, start_ip,
+                                                end_ip).result()
+    return firewall.name
+
+
+# pylint: disable=inconsistent-return-statements
+def parse_public_access_input(public_access):
+    # pylint: disable=no-else-return
+    if public_access is not None:
+        parsed_input = public_access.split('-')
+        if len(parsed_input) == 1:
+            return parsed_input[0], parsed_input[0]
+        elif len(parsed_input) == 2:
+            return parsed_input[0], parsed_input[1]
+        else:
+            raise CLIError('incorrect usage: --public-access. Acceptable values are \'all\', \'none\',\'<startIP>\' and \'<startIP>-<destinationIP>\' '
+                           'where startIP and destinationIP ranges from 0.0.0.0 to 255.255.255.255')
+
+
+def server_list_custom_func(client, resource_group_name=None):
+    if resource_group_name:
+        return client.list_by_resource_group(resource_group_name)
+    return client.list()
+
+
+def flexible_firewall_rule_update_custom_func(instance, start_ip_address=None, end_ip_address=None):
+    if start_ip_address is not None:
+        instance.start_ip_address = start_ip_address
+    if end_ip_address is not None:
+        instance.end_ip_address = end_ip_address
+    return instance
+
+
+def update_kwargs(kwargs, key, value):
+    if value is not None:
+        kwargs[key] = value
+
+
+def parse_maintenance_window(maintenance_window_string):
+    parsed_input = maintenance_window_string.split(':')
+    # pylint: disable=no-else-return
+    if len(parsed_input) == 1:
+        return _map_maintenance_window(parsed_input[0]), None, None
+    elif len(parsed_input) == 2:
+        return _map_maintenance_window(parsed_input[0]), parsed_input[1], None
+    elif len(parsed_input) == 3:
+        return _map_maintenance_window(parsed_input[0]), parsed_input[1], parsed_input[2]
+    return None, None, None
+
+
+def _update_location(cmd, resource_group_name):
+    resource_client = resource_client_factory(cmd.cli_ctx)
+    rg = resource_client.resource_groups.get(resource_group_name)
+    location = rg.location
+    return location
+
+
+def _create_resource_group(cmd, location, resource_group_name):
+    if resource_group_name is None:
+        resource_group_name = create_random_resource_name('group')
+    params = ResourceGroup(location=location)
+    resource_client = resource_client_factory(cmd.cli_ctx)
+    logger.warning('Creating Resource Group \'%s\'...', resource_group_name)
+    resource_client.resource_groups.create_or_update(resource_group_name, params)
+    return resource_group_name
+
+
+def _check_resource_group_existence(cmd, resource_group_name):
+    resource_client = resource_client_factory(cmd.cli_ctx)
+    return resource_client.resource_groups.check_existence(resource_group_name)
+
+
+# Map day_of_week string to integer to day of week
+# Possible values can be 0 - 6
+def _map_maintenance_window(day_of_week):
+    options = {"Mon": 1,
+               "Tue": 2,
+               "Wed": 3,
+               "Thu": 4,
+               "Fri": 5,
+               "Sat": 6,
+               "Sun": 0,
+               }
+    return options[day_of_week]

commit 7c7c421609721ece09d06521577bda0a014da005

[mysql] Fix #20563: `az mysql server show-connection-string -s foo-mysql-server`: Fix PHP wrong connection strings (#20567)
Before this commit, the following was outputted for PHP:
host=foo-mysql-server.mysql.database.azure.com port=3306 dbname={database} user={username}@foo-mysql-server password={password}
Now, a connection string which permits connection is printed:
$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);] mysqli_real_connect($con, 'fran92-mysql-server.mysql.database.azure.com, '{username}@fran92-mysql-server', '{password}', '{database}', 3306);

Fixes: 50706170e298 ("[RDBMS] Management Plane updates for PostgreSQL and MySQL (#15177)")
Fixes: cc4298a755a0 ("[RDBMS] Add single server show-connection-string and tests for local-context commands, server creation  (#15844)")
Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

src/azure-cli/azure/cli/command_modules/rdbms/custom.py
@@ -820,7 +820,7 @@ def get_connection_string(cmd, client, server_name='{server}', database_name='{d
             'jdbc': "jdbc:mysql://{host}:3306/{database}?user={user}@{server}&password={password}",
             'node.js': "var conn = mysql.createConnection({{host: '{host}', user: '{user}@{server}',"
                        "password: {password}, database: {database}, port: 3306}});",
-            'php': "host={host} port=3306 dbname={database} user={user}@{server} password={password}",
+            'php': "$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {{ca-cert filename}}, NULL, NULL);] mysqli_real_connect($con, '{host}', '{user}@{server}', '{password}', '{database}', 3306);",
             'python': "cnx = mysql.connector.connect(user='{user}@{server}', password='{password}', host='{host}', "
                       "port=3306, database='{database}')",
             'ruby': "client = Mysql2::Client.new(username: '{user}@{server}', password: '{password}', "
@@ -873,7 +873,7 @@ def get_connection_string(cmd, client, server_name='{server}', database_name='{d
             'jdbc': "jdbc:mariadb://{host}:3306/{database}?user={user}@{server}&password={password}",
             'node.js': "var conn = mysql.createConnection({{host: '{host}', user: '{user}@{server}',"
                        "password: {password}, database: {database}, port: 3306}});",
-            'php': "host={host} port=3306 dbname={database} user={user}@{server} password={password}",
+            'php': "$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {{ca-cert filename}}, NULL, NULL);] mysqli_real_connect($con, '{host}', '{user}@{server}', '{password}', '{database}', 3306);",
             'python': "cnx = mysql.connector.connect(user='{user}@{server}', password='{password}', host='{host}', "
                       "port=3306, database='{database}')",
             'ruby': "client = Mysql2::Client.new(username: '{user}@{server}', password: '{password}', "

src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_mysql.py
@@ -611,7 +611,7 @@ def _create_mysql_connection_strings(host, user, password, database):
                        "spring.datasource.password={password}",
         'node.js': "var conn = mysql.createConnection({{host: '{host}', user: '{user}', "
                    "password: {password}, database: {database}, port: 3306}});",
-        'php': "host={host} port=3306 dbname={database} user={user} password={password}",
+        'php': "$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {{ca-cert filename}}, NULL, NULL);] mysqli_real_connect($con, '{host}', '{user}', '{password}', '{database}', 3306);",
         'python': "cnx = mysql.connector.connect(user='{user}', password='{password}', host='{host}', "
                   "port=3306, database='{database}')",
         'ruby': "client = Mysql2::Client.new(username: '{user}', password: '{password}', "