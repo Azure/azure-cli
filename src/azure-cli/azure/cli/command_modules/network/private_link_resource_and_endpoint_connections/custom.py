aeeb22c33d1c5a0e38ddf37664ed85121579bdea
dorothyhu <72949285+dorothyhu@users.noreply.github.com>
2022-01-25 21:21:17 -0800

    [Network] Add Microsoft.HybridCompute/privateLinkScopes to supported Private Endpoints (#21096)
    * added Microsoft.HybridCompute/privateLinkScopes type and test cases
    
    * fixed the ,

--- src/azure-cli/azure/cli/command_modules/network/private_link_resource_and_endpoint_connections/custom.py
+++ src/azure-cli/azure/cli/command_modules/network/private_link_resource_and_endpoint_connections/custom.py
@@ -36,6 +36,7 @@ def register_providers():
     _register_one_provider('Microsoft.EventGrid/domains', '2020-04-01-preview', True)
     _register_one_provider("Microsoft.EventHub/namespaces", "2021-06-01-preview", True)
     _register_one_provider("Microsoft.HDInsight/clusters", '2018-06-01-preview', True)
+    _register_one_provider("Microsoft.HybridCompute/privateLinkScopes", '2021-05-20', True)
     _register_one_provider("Microsoft.HealthcareApis/services", "2020-03-30", True)
     _register_one_provider('microsoft.insights/privateLinkScopes', '2019-10-17-preview', True)
     _register_one_provider('Microsoft.KeyVault/managedHSMs', '2021-04-01-preview', True)
