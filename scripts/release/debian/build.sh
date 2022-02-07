29d9b58680ba36817489779c51418cf14ad84b11
jiasli <4003950+jiasli@users.noreply.github.com>
2022-02-07 11:50:23 +0800

    3.8.12

--- scripts/release/debian/build.sh
+++ scripts/release/debian/build.sh
@@ -15,7 +15,7 @@ set -exv
 ls -Rl /mnt/artifacts
 
 WORKDIR=`cd $(dirname $0); cd ../../../; pwd`
-PYTHON_VERSION="3.8.11"
+PYTHON_VERSION="3.8.12"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update APT packages
