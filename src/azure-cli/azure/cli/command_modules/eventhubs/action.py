406f148b3115a2c55200e27fd89717f0258ffac7
davadhani <davadhani@microsoft.com>
2022-02-12 19:38:03 +0530

    Fixing PR Comments

--- src/azure-cli/azure/cli/command_modules/eventhubs/action.py
+++ src/azure-cli/azure/cli/command_modules/eventhubs/action.py
@@ -1,3 +1,12 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=line-too-long
+# pylint: disable=too-many-lines
+# pylint: disable=inconsistent-return-statements
+
 import argparse
 
 class AlertAddEncryption(argparse._AppendAction):
