4341d981a970bc8a32b7b12e0fdd1724ad2ee089
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2022-02-25 09:37:58 +0800

    [Storage] `az storage blob upload/upload-batch`: Add `--tier`, migrate from storage-blob-preview with track2 sdk (#21359)
    * migrate storage blob upload from extension with track2 sdk, tests not added yet
    
    * fix tests to match track2 sdk
    
    * use PremiumPageBlobAccessTier model from generated folder, fix more tests, lint
    
    * broke previous tests
    
    * upload-batch
    
    * backward compatible
    
    * fix tests
    
    * test and lint
    
    * skip page blob tier test
    
    * remove preview flag
    
    * use transformer for upload output
    
    * lint
    
    * fix upload-batch validator
    
    * revert back change
    
    * lint
    
    * PR review fix

--- src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_blob_live_scenarios.py
+++ src/azure-cli/azure/cli/command_modules/storage/tests/latest/test_storage_blob_live_scenarios.py
@@ -96,6 +96,39 @@ class StorageBlobUploadLiveTests(LiveScenarioTest):
                              'The download file size is not right.')
 
 
+    @ResourceGroupPreparer()
+    @StorageAccountPreparer()
+    def test_storage_blob_upload(self, resource_group, storage_account):
+        from azure.cli.core.azclierror import AzureResponseError
+        file_size_kb = 128
+        local_file = self.create_temp_file(file_size_kb, full_random=True)
+
+        container = self.create_random_name(prefix='cont', length=10)
+        blob_name = self.create_random_name(prefix='blob', length=24)
+        account_key = self.cmd('storage account keys list -n {} -g {} --query "[0].value" -otsv'
+                               .format(storage_account, resource_group)).output
+
+        self.set_env('AZURE_STORAGE_ACCOUNT', storage_account)
+        self.set_env('AZURE_STORAGE_KEY', account_key)
+
+        self.cmd('storage container create -n {}'.format(container))
+        # test upload through file path
+        self.cmd('storage blob upload -c {} -f "{}" -n {}'.format(container, local_file, blob_name))
+
+        self.cmd('storage blob exists -n {} -c {}'.format(blob_name, container), checks=JMESPathCheck('exists', True))
+
+        self.cmd('storage blob show -n {} -c {}'.format(blob_name, container), checks=[
+            JMESPathCheck('properties.contentLength', file_size_kb * 1024),
+            JMESPathCheck('name', blob_name)])
+
+        # test upload from data
+        self.cmd('storage blob upload -c {} --data {} --length 4 -n {} --overwrite'.format(
+            container, "test", blob_name))
+        self.cmd('storage blob show -n {} -c {}'.format(blob_name, container), checks=[
+            JMESPathCheck('properties.contentLength', 4),
+            JMESPathCheck('name', blob_name)])
+
+
 @api_version_constraint(ResourceType.DATA_STORAGE_BLOB, min_api='2019-12-12')
 class StorageBlobQueryTests(StorageScenarioMixin, LiveScenarioTest):
     @ResourceGroupPreparer()
