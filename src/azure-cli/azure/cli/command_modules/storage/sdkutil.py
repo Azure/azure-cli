9268b9ee57fcfc481ed7efbfa1132b98e1e97d8c
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2022-02-25 09:37:58 +0800

    [Storage] `az storage blob upload/upload-batch`: Add `--tier`, migrate from storage-blob-preview with track2 sdk (#21359)
    * migrate storage blob upload from extension with track2 sdk, tests not added yet
    
    * fix tests to match track2 sdk
    
    * use PremiumPageBlobAccessTier model from generated folder, fix more tests, lint
    
    * broke previous tests
    
    * upload-batch
    
    * backward compatible
    
    * fix tests
    
    * test and lint
    
    * skip page blob tier test
    
    * remove preview flag
    
    * use transformer for upload output
    
    * lint
    
    * fix upload-batch validator
    
    * revert back change
    
    * lint
    
    * PR review fix

--- src/azure-cli/azure/cli/command_modules/storage/sdkutil.py
+++ src/azure-cli/azure/cli/command_modules/storage/sdkutil.py
@@ -45,6 +45,13 @@ def get_blob_tier_names(cli_ctx, model):
     return [v for v in dir(t_blob_tier_model) if not v.startswith('_')]
 
 
+def get_blob_tier_names_track2(cli_ctx, model_path, track2=False):
+    t_blob_tier_model = get_sdk(cli_ctx, ResourceType.DATA_STORAGE, model_path)
+    if track2:
+        t_blob_tier_model = get_sdk(cli_ctx, ResourceType.DATA_STORAGE_BLOB, model_path)
+    return [v for v in dir(t_blob_tier_model) if not v.startswith('_')]
+
+
 def get_delete_blob_snapshot_type_names():
     return 'include', 'only'
 
