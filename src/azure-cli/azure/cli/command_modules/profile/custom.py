12465f1e960cb33b60ff8c831347888a13dd2e0b
Silas Strawn <strawnsc@gmail.com>
2022-03-03 15:04:44 -0800

    remove erroneous VSCode import

--- src/azure-cli/azure/cli/command_modules/profile/custom.py
+++ src/azure-cli/azure/cli/command_modules/profile/custom.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from email.policy import default
 import os
 from typing import OrderedDict
 
