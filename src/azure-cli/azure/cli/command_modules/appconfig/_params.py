5e92481c9b4bf5a41f1f2deac6099819e38b96cc
Pratik Sanglikar <prsangli@microsoft.com>
2022-03-09 10:08:45 -0800

    update params

--- src/azure-cli/azure/cli/command_modules/appconfig/_params.py
+++ src/azure-cli/azure/cli/command_modules/appconfig/_params.py
@@ -102,18 +102,17 @@ def load_arguments(self, _):
         c.argument('enable_public_network', options_list=['--enable-public-network', '-e'], arg_type=get_three_state_flag(), is_preview=True,
                    help='When true, requests coming from public networks have permission to access this store while private endpoint is enabled. When false, only requests made through Private Links can reach this store.')
         c.argument('disable_local_auth', arg_type=get_three_state_flag(), is_preview=True, help='Disable all authentication methods other than AAD authentication.')
-        c.argument('enable_purge_protection', options_list=['--enable-purge-protection', '-p'], arg_type=get_three_state_flag(), is_preview=True,
-                   help='Property specifying whether protection against purge is enabled for this App Configuration. Setting this property to true activates protection against purge for this App Configuration and its contents. Enabling this functionality is irreversible.')
+        c.argument('enable_purge_protection', options_list=['--enable-purge-protection', '-p'], arg_type=get_three_state_flag(), is_preview=True, help='Property specifying whether protection against purge is enabled for this App Configuration. Setting this property to true activates protection against purge for this App Configuration and its contents. Enabling this functionality is irreversible.')
 
     with self.argument_context('appconfig recover') as c:
-        c.argument('location', options_list=['--location', '-l'], arg_type=get_location_type(self.cli_ctx), help='Location of the deleted App Configuration. Can be viewed using command `az appconfig show-deleted`.')
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), help='Location of the deleted App Configuration. Can be viewed using command `az appconfig show-deleted`.')
         c.argument('resource_group_name', arg_type=resource_group_name_type, help='Resource group of the deleted App Configuration.')
 
     with self.argument_context('appconfig show-deleted') as c:
-        c.argument('location', options_list=['--location', '-l'], arg_type=get_location_type(self.cli_ctx), help='Location of the deleted App Configuration.')
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), help='Location of the deleted App Configuration.')
 
     with self.argument_context('appconfig purge') as c:
-        c.argument('location', options_list=['--location', '-l'], arg_type=get_location_type(self.cli_ctx), help='Location of the deleted App Configuration. Can be viewed using command `az appconfig show-deleted`.')
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), help='Location of the deleted App Configuration. Can be viewed using command `az appconfig show-deleted`.')
 
     with self.argument_context('appconfig update', arg_group='Customer Managed Key') as c:
         c.argument('encryption_key_name', help='The name of the KeyVault key.')
