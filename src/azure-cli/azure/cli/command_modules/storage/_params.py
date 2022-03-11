db539d485890e65da4ca5fbccd651526845535f5
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2022-03-11 10:24:34 +0800

    [Storage] Fix #21494: `az storage blob upload/upload-batch`: Fix `--content-md5` for upload, ignore `--content-md5` for upload-batch (#21523)
    * fix upload --content-md5, ignore --content-md5 for upload-batch
    
    * add tests
    
    * delete unused commands
    
    * remove duplicate code
    
    * lint

--- src/azure-cli/azure/cli/command_modules/storage/_params.py
+++ src/azure-cli/azure/cli/command_modules/storage/_params.py
@@ -926,7 +926,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-locals, too-many-statem
 
         c.register_blob_arguments_track2()
         c.register_precondition_options()
-        c.register_content_settings_argument(t_blob_content_settings, update=False, arg_group="Content Control")
+        c.register_content_settings_argument(t_blob_content_settings, update=False, arg_group="Content Control",
+                                             process_md5=True)
         c.extra('blob_name', validator=validate_blob_name_for_upload)
 
         c.argument('file_path', options_list=('--file', '-f'), type=file_type, completer=FilesCompleter(),
