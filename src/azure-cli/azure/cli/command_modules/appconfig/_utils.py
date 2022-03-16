2d84719fe003c7fae9a58393abe7be77dbd6c8d5
Pratik Sanglikar <pratiksanglikar@gmail.com>
2022-03-15 20:17:13 -0700

    [App Config] `az appconfig`: Add new commands - list-deleted, show-deleted, recover, purge (#21362)
    * Add list-deleted, show-deleted, purge, and recover commands to az appconfig
    
    * Upgrade the Azure CLI Python SDK package
    
    * Add optional location parameter, error out when multiple stores with same name are found
    
    * Commit code review suggestion
    
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>
    
    * Update SDK version
    
    * update test recordings
    
    * Resolve feedback comments
    
    * feedback comments
    
    * Update test recordings
    
    * Fix linting issue
    
    * Resolve feedback comments
    
    * Update test recordings
    
    * Fix issue when creating a store
    
    * Commit code review suggestion
    
    Co-authored-by: RuiJun Hu (MSFT) <v-ruih@microsoft.com>
    
    * Commit code review suggestion
    
    Co-authored-by: RuiJun Hu (MSFT) <v-ruih@microsoft.com>
    
    * Update test and test recordings
    
    * Fix styling errors related to imports
    
    * update params
    
    * remove special error handling for purge protection enabled
    
    * Fix linting errors and resolve comments
    
    Co-authored-by: Pratik Sanglikar <prsangli@microsoft.com>
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>
    Co-authored-by: RuiJun Hu (MSFT) <v-ruih@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/appconfig/_utils.py
+++ src/azure-cli/azure/cli/command_modules/appconfig/_utils.py
@@ -8,6 +8,7 @@ from knack.log import get_logger
 from knack.util import CLIError
 from azure.appconfiguration import AzureAppConfigurationClient
 from azure.core.exceptions import HttpResponseError
+from azure.cli.core.azclierror import ValidationError, AzureResponseError, ResourceNotFoundError
 
 from ._client_factory import cf_configstore
 from ._constants import HttpHeaders
@@ -34,17 +35,52 @@ def construct_connection_string(cmd, config_store_name):
 
 
 def resolve_store_metadata(cmd, config_store_name):
+    resource_group = None
+    endpoint = None
     try:
         config_store_client = cf_configstore(cmd.cli_ctx)
         all_stores = config_store_client.list()
         for store in all_stores:
             if store.name.lower() == config_store_name.lower():
-                # Id has a fixed structure /subscriptions/subscriptionName/resourceGroups/groupName/providers/providerName/configurationStores/storeName"
-                return store.id.split('/')[4], store.endpoint
+                if resource_group is None:
+                    # Id has a fixed structure /subscriptions/subscriptionName/resourceGroups/groupName/providers/providerName/configurationStores/storeName"
+                    resource_group = store.id.split('/')[4]
+                    endpoint = store.endpoint
+                else:
+                    raise ValidationError('Multiple configuration stores found with name {}.'.format(config_store_name))
     except HttpResponseError as ex:
-        raise CLIError("Failed to get the list of App Configuration stores for the current user. Make sure that the account that logged in has sufficient permissions to access the App Configuration store.\n{}".format(str(ex)))
+        raise AzureResponseError("Failed to get the list of App Configuration stores for the current user. Make sure that the account that logged in has sufficient permissions to access the App Configuration store.\n{}".format(str(ex)))
 
-    raise CLIError("Failed to find the App Configuration store '{}'.".format(config_store_name))
+    if resource_group is not None and endpoint is not None:
+        return resource_group, endpoint
+
+    raise ResourceNotFoundError("Failed to find the App Configuration store '{}'.".format(config_store_name))
+
+
+def resolve_deleted_store_metadata(cmd, config_store_name, resource_group_name=None, location=None):
+    resource_group = None
+    metadata_location = None
+    try:
+        client = cf_configstore(cmd.cli_ctx)
+        deleted_stores = client.list_deleted()
+        for deleted_store in deleted_stores:
+            # configuration_store_id has a fixed structure /subscriptions/subscription_id/resourceGroups/resource_group_name/providers/Microsoft.AppConfiguration/configurationStores/configuration_store_name
+            metadata_resource_group = deleted_store.configuration_store_id.split('/')[4]
+            # match the name and additionally match resource group and location if available.
+            if deleted_store.name.lower() == config_store_name.lower() and (resource_group_name is None or resource_group_name.lower() == metadata_resource_group.lower()) and (location is None or location == deleted_store.location):
+                if metadata_location is None:
+                    resource_group = metadata_resource_group
+                    metadata_location = deleted_store.location
+                else:
+                    # It should reach here only when the user has provided only name. If they provide either location or resource group, we should be able to uniquely find the store.
+                    raise ValidationError('Multiple configuration stores found with name {}.'.format(config_store_name))
+    except HttpResponseError as ex:
+        raise AzureResponseError("Failed to get the list of deleted App Configuration stores for the current user. Make sure that the account that logged in has sufficient permissions to access the App Configuration store.\n{}".format(str(ex)))
+
+    if resource_group is not None and metadata_location is not None:
+        return resource_group, metadata_location
+
+    raise ResourceNotFoundError("Failed to find the deleted App Configuration store '{}'. If you think that the store name is correct, please validate all your input parameters again.".format(config_store_name))
 
 
 def resolve_connection_string(cmd, config_store_name=None, connection_string=None):
