9d62c67c405cd5071a6f08f52046e883dc962f93
hodd <hodd@netapp.com>
2022-03-21 09:03:40 +0000

    added ldap search scope parameters to help

--- src/azure-cli/azure/cli/command_modules/netappfiles/_help.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/_help.py
@@ -68,6 +68,12 @@ parameters:
     short-summary: This specifies the group DN, which overrides the base DN for group lookups
   - name: --group-filter
     short-summary: This specifies the custom LDAP search filter to be used when looking up group membership from LDAP server
+  - name: --user-dn
+    short-summary: This specifies the user DN, which overrides the base DN for user lookups.
+  - name: group-dn
+    short-summary: This specifies the group DN, which overrides the base DN for group lookups.
+  - name: group-filter
+    short-summary: This specifies the custom LDAP search filter to be used when looking up group membership from LDAP server.
 examples:
   - name: Add an active directory to the account
     text: >
