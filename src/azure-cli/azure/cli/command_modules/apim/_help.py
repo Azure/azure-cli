14d26e436d0cbe71d481415a23358329d4225e2f
Ananda Reddy Srinivasa Reddy <reddyanand604@gmail.com>
2022-03-21 20:54:35 -0500

    Update src/azure-cli/azure/cli/command_modules/apim/_help.py
    Co-authored-by: ZelinWang <zelinwang@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/apim/_help.py
+++ src/azure-cli/azure/cli/command_modules/apim/_help.py
@@ -551,7 +551,7 @@ parameters:
     type: string
     short-summary: Schema type  (e.g. application/json, application/vnd.ms-azure-apim.graphql.schema).
     long-summary: |
-        Must be a valid a media type used in a Content-Type header as defined in the RFC 2616. Media type of the schema document
+        Must be a valid media type used in a Content-Type header as defined in the RFC 2616. Media type of the schema document
 examples:
   - name: Create a API Schema.
     text: |-
