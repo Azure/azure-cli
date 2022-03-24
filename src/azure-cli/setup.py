9fdbeefe5a3d9b40416ff06f2c7e24c65785607c
Bo Zhang <bzhan@microsoft.com>
2022-03-24 17:47:12 +0800

    [CDN] upgrade azure-mgmt-cdn to 12.0.0 for Azure Front Door Standard/Premium GA

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -72,7 +72,7 @@ DEPENDENCIES = [
     'azure-mgmt-batch~=16.0.0',
     'azure-mgmt-billing==6.0.0',
     'azure-mgmt-botservice~=0.3.0',
-    'azure-mgmt-cdn==11.0.0',
+    'azure-mgmt-cdn==12.0.0',
     'azure-mgmt-cognitiveservices~=13.0.0',
     'azure-mgmt-compute~=23.1.0',
     'azure-mgmt-consumption~=2.0',
