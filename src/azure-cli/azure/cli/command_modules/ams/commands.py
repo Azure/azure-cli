74218dc82b039747af91621cddc85f0ae8232156
hivyas <hivyas@microsoft.com>
2022-03-28 22:25:11 -0700

    fixing style issue

--- src/azure-cli/azure/cli/command_modules/ams/commands.py
+++ src/azure-cli/azure/cli/command_modules/ams/commands.py
@@ -80,7 +80,7 @@ def load_command_table(self, _):  # pylint: disable=too-many-locals, too-many-st
         g.custom_command('remove', 'remove_identity',
                          custom_command_type=get_custom_sdk('identity', get_mediaservices_client))
         g.custom_show_command('show', 'show_identity',
-                         custom_command_type=get_custom_sdk('identity', get_mediaservices_client))
+                              custom_command_type=get_custom_sdk('identity', get_mediaservices_client))
 
     with self.command_group('ams transform', get_sdk('Transforms', get_transforms_client)) as g:
         g.custom_show_command('show', 'get_transform',
