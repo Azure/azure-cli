b45f64235cafd38f06f5c38f5c63692d7a5cda16
hodd <hodd@netapp.com>
2022-04-01 09:39:21 +0000

    Add licence headers

--- src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_calculate_throughput_for_vg.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_calculate_throughput_for_vg.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 from azure.cli.command_modules.netappfiles.custom import VolumeType, calculate_throughput
 
 
--- src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_volumegroup_commands.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_volumegroup_commands.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 from azure.cli.testsdk import ScenarioTest
 from azure.cli.testsdk.decorators import serial_test
 
