13cc7e5f0f368961faa274b4120cf54b368aa4a8
surashed <55134940+surashed@users.noreply.github.com>
2022-04-05 18:49:42 +0300

    Update Automation tests

--- src/azure-cli/azure/cli/command_modules/security/tests/latest/test_security_automations_scenario.py
+++ src/azure-cli/azure/cli/command_modules/security/tests/latest/test_security_automations_scenario.py
@@ -24,14 +24,14 @@ class SecurityCenterSecurityAutomationsTests(ScenarioTest):
         security_automation = self.cmd('az security automation show -g Sample-RG -n ExportToWorkspace').get_output_in_json()
         assert security_automation["name"] == "ExportToWorkspace"
 
-        # Validates Automations
-        self.cmd('az security automation create_or_update')
-
         # Create/Update Automations
-        self.cmd('az security automation create_or_update')
+        self.cmd("az security automation create_or_update -g Sample-RG -n ExportToWorkspaceTemp -l eastus --scopes '[{\"description\": \"Scope for 487bb485-b5b0-471e-9c0d-10717612f869\", \"scopePath\": \"/subscriptions/487bb485-b5b0-471e-9c0d-10717612f869\"}]' --sources '[{\"eventSource\":\"SubAssessments\",\"ruleSets\":null}]' --actions '[{\"actionType\":\"EventHub\",\"eventHubResourceId\":\"subscriptions/212f9889-769e-45ae-ab43-6da33674bd26/resourceGroups/ContosoSiemPipeRg/providers/Microsoft.EventHub/namespaces/contososiempipe-ns/eventhubs/surashed-test\",\"connectionString\":\"Endpoint=sb://contososiempipe-ns.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=dummy=;EntityPath=dummy\",\"SasPolicyName\":\"dummy\"}]'")
+
+        # Validates Automations
+        self.cmd("az security automation validate -g Sample-RG -n ExportToWorkspaceTemp -l eastus --scopes '[{\"description\": \"Scope for 487bb485-b5b0-471e-9c0d-10717612f869\", \"scopePath\": \"/subscriptions/487bb485-b5b0-471e-9c0d-10717612f869\"}]' --sources '[{\"eventSource\":\"SubAssessments\",\"ruleSets\":null}]' --actions '[{\"actionType\":\"EventHub\",\"eventHubResourceId\":\"subscriptions/212f9889-769e-45ae-ab43-6da33674bd26/resourceGroups/ContosoSiemPipeRg/providers/Microsoft.EventHub/namespaces/contososiempipe-ns/eventhubs/surashed-test\",\"connectionString\":\"Endpoint=sb://contososiempipe-ns.servicebus.windows.net/;SharedAccessKeyName=Send;SharedAccessKey=dummy=;EntityPath=dummy\",\"SasPolicyName\":\"dummy\"}]'")
 
         # Delete Automation
-        security_automation = self.cmd('az security automation delete -g Sample-RG -n ExportToWorkspace').get_output_in_json()
+        security_automation = self.cmd('az security automation delete -g Sample-RG -n ExportToWorkspaceTemp').get_output_in_json()
         assert security_automation["name"] == "ExportToWorkspace"
         security_automations = self.cmd('az security automation list').get_output_in_json()
         assert len(security_automations) == subscription_previous_automations_count
