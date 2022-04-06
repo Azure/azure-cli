25adc8f66349afb0e9c2f062597300517af6fc90
hodd <hodd@netapp.com>
2022-04-06 12:26:11 +0000

    update restore file test and check for error responce instead of happy thath since not able to mount volume

--- src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_snapshot_commands.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_snapshot_commands.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
 from azure.cli.testsdk.decorators import serial_test
+from knack.util import CLIError
 
 POOL_DEFAULT = "--service-level 'Premium' --size 4"
 VOLUME_DEFAULT = "--service-level 'Premium' --usage-threshold 100"
@@ -178,7 +179,7 @@ class AzureNetAppFilesSnapshotServiceScenarioTest(ScenarioTest):
 
     @serial_test()
     @ResourceGroupPreparer(name_prefix='cli_netappfiles_test_snapshot_', additional_tags={'owner': 'cli_test'})
-    def test_restore_file(self):
+    def test_restore_file_returns_not_found(self):
         # create volume
         account_name = self.create_random_name(prefix='cli-acc-', length=24)
         pool_name = self.create_random_name(prefix='cli-pool-', length=24)
@@ -193,10 +194,6 @@ class AzureNetAppFilesSnapshotServiceScenarioTest(ScenarioTest):
 
         snapshot_file_path = "'/snap_file_path_1.txt' '/snap_file_path_2.txt'"
 
-        self.cmd("az netappfiles snapshot restore-files -g {rg} -a %s -p %s -v %s -s %s --file-paths %s" %
-                 (account_name, pool_name, volume_name, snapshot_name, snapshot_file_path))
-
-        snapshot = self.cmd("az netappfiles snapshot show -g {rg} -a %s -p %s -v %s -s %s" %
-                            (account_name, pool_name, volume_name, snapshot_name)).get_output_in_json()
-
-        assert snapshot['provisioningState'] == 'Succeeded'
+        with self.assertRaisesRegex(CLIError, "The specified filePath /snap_file_path_1.txt does not exist"):
+            self.cmd("az netappfiles snapshot restore-files -g {rg} -a %s -p %s -v %s -s %s --file-paths %s" %
+                     (account_name, pool_name, volume_name, snapshot_name, snapshot_file_path))
