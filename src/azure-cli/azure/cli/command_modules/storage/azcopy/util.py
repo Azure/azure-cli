d75e97a8c34db9c25fc36fa3a2922bcb5df2db8a
Jonie Liu (Shanghai Wicresoft Co <v-jonieliu@microsoft.com>
2022-04-10 01:26:07 +0800

    fix suggestions

--- src/azure-cli/azure/cli/command_modules/storage/azcopy/util.py
+++ src/azure-cli/azure/cli/command_modules/storage/azcopy/util.py
@@ -76,8 +76,8 @@ class AzCopy:
         args = [self.executable] + args
         args_hides = args.copy()
         for i in range(len(args_hides)):
-            if args_hides[i].find('?') > 0:
-                args_hides[i] = args_hides[i][0:args_hides[i].index('?')]
+            if args_hides[i].find('sig') > 0:
+                args_hides[i] = args_hides[i][0:args_hides[i].index('sig')+4]
         logger.warning("Azcopy command: %s", args_hides)
         env_kwargs = {}
         if self.creds and self.creds.token_info:
