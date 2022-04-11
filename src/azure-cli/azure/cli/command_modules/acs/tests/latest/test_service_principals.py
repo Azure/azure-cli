117f57d0c59f4550c8c34e7ebaf22fa3de09f026
Fuming Zhang <fumingzhang@microsoft.com>
2022-04-11 16:14:16 +0800

    sort imports for tests

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_service_principals.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_service_principals.py
@@ -8,7 +8,6 @@ import tempfile
 import unittest
 from unittest import mock
 
-from knack.util import CLIError
 from azure.cli.command_modules.acs.custom import (
     load_acs_service_principal,
     store_acs_service_principal,
