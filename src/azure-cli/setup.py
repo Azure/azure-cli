3b647211484e8516a1fae296926cf35abc56ecd8
necusjz <necusjz@gmail.com>
2022-04-11 16:55:29 +0800

    {Monitor} Bump azure-mgmt-loganalytics from 13.0.0b2 to 13.0.0b4 (#22013)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -98,7 +98,7 @@ DEPENDENCIES = [
     'azure-mgmt-iothubprovisioningservices==1.1.0',
     'azure-mgmt-keyvault==9.3.0',
     'azure-mgmt-kusto~=0.3.0',
-    'azure-mgmt-loganalytics==13.0.0b2',
+    'azure-mgmt-loganalytics==13.0.0b4',
     'azure-mgmt-managedservices~=1.0',
     'azure-mgmt-managementgroups~=1.0.0',
     'azure-mgmt-maps~=2.0.0',
