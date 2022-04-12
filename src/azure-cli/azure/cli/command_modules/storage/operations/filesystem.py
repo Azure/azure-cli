d9b18c0f788dbe65ada1b20e7366abe05f2cd01f
yishiwang@microsoft.com <yishiwang@microsoft.com>
2022-04-12 20:30:12 +0800

    GA storage file datalake soft delete

--- src/azure-cli/azure/cli/command_modules/storage/operations/filesystem.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/filesystem.py
@@ -50,3 +50,64 @@ def generate_sas_fs_uri(client, cmd, file_system, permission=None,
         return file_system_client.url
 
     return sas_token
+
+
+def list_deleted_path(client, marker=None, num_results=None, path_prefix=None, timeout=None, **kwargs):
+    from ..track2_util import list_generator
+
+    generator = client.list_deleted_paths(path_prefix=path_prefix, timeout=timeout, results_per_page=num_results, **kwargs)
+
+    pages = generator.by_page(continuation_token=marker)  # BlobPropertiesPaged
+    result = list_generator(pages=pages, num_results=num_results)
+
+    if pages.continuation_token:
+        next_marker = {"nextMarker": pages.continuation_token}
+        result.append(next_marker)
+
+    return result
+
+
+def set_service_properties(cmd, client, delete_retention=None, delete_retention_period=None,
+                           enable_static_website=False, index_document=None, error_document_404_path=None):
+    parameters = client.get_service_properties()
+    # update
+    kwargs = {}
+    delete_retention_policy = cmd.get_models('_models#RetentionPolicy',
+                                             resource_type=ResourceType.DATA_STORAGE_FILEDATALAKE)()
+    if parameters.get('delete_retention_policy', None):
+        delete_retention_policy = parameters['delete_retention_policy']
+    if delete_retention is not None:
+        delete_retention_policy.enabled = delete_retention
+    if delete_retention_period is not None:
+        delete_retention_policy.days = delete_retention_period
+    delete_retention_policy.allow_permanent_delete = False
+
+    static_website = cmd.get_models('_models#StaticWebsite',
+                                    resource_type=ResourceType.DATA_STORAGE_FILEDATALAKE)()
+    if parameters.get('static_website', None):
+        static_website = parameters['static_website']
+
+    if static_website is not None:
+        static_website.enabled = enable_static_website
+    if index_document is not None:
+        static_website.index_document = index_document
+    if error_document_404_path is not None:
+        static_website.error_document_404_path = error_document_404_path
+
+    if parameters.get('hour_metrics', None):
+        kwargs['hour_metrics'] = parameters['hour_metrics']
+    if parameters.get('logging', None):
+        kwargs['logging'] = parameters['logging']
+    if parameters.get('minute_metrics', None):
+        kwargs['minute_metrics'] = parameters['minute_metrics']
+    if parameters.get('cors', None):
+        kwargs['cors'] = parameters['cors']
+
+    # checks
+    if delete_retention_policy and delete_retention_policy.enabled and not delete_retention_policy.days:
+        from azure.cli.core.azclierror import InvalidArgumentValueError
+        raise InvalidArgumentValueError("must specify days-retained")
+
+    client.set_service_properties(delete_retention_policy=delete_retention_policy, static_website=static_website,
+                                  **kwargs)
+    return client.get_service_properties()
