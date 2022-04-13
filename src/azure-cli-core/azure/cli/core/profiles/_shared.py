d159ac1036deada09b492267e62d99d667eb8fe5
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2022-04-13 10:53:48 +0800

    {Storage} Bump azure-mgmt-storage to 20.0.0 (#22026)
    * bump mgmt-storage
    
    * missed files
    
    * rerun tests, add back and delete yaml

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -145,7 +145,7 @@ class SDKProfile:  # pylint: disable=too-few-public-methods
 
 AZURE_API_PROFILES = {
     'latest': {
-        ResourceType.MGMT_STORAGE: '2021-08-01',
+        ResourceType.MGMT_STORAGE: '2021-09-01',
         ResourceType.MGMT_NETWORK: '2021-05-01',
         ResourceType.MGMT_COMPUTE: SDKProfile('2021-11-01', {
             'resource_skus': '2019-04-01',
