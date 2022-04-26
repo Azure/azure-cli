04fe7a508c1a58e970a8d4bee5591470141cc3e7
Fuming Zhang <fumingzhang@microsoft.com>
2022-04-26 17:58:51 +0800

    delete dup typing

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_agentpool_decorator.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_agentpool_decorator.py
@@ -26,7 +26,6 @@ from azure.cli.command_modules.acs._consts import (
     DecoratorMode,
 )
 from azure.cli.command_modules.acs.agentpool_decorator import (
-    AgentPool,
     AKSAgentPoolAddDecorator,
     AKSAgentPoolContext,
     AKSAgentPoolModels,
@@ -68,14 +67,7 @@ class AKSAgentPoolModelsTestCase(unittest.TestCase):
 
 
 class AKSAgentPoolContextCommonTestCase(unittest.TestCase):
-    def _remove_defaults_in_agentpool(self, agentpool: AgentPool) -> AgentPool:
-        """Internal function to remove values from properties with default values of the `agentpool` object.
-
-        Removing default values is to prevent getters from mistakenly overwriting user provided values with default
-        values in the object.
-
-        :return: the AgentPool object
-        """
+    def _remove_defaults_in_agentpool(self, agentpool):
         self.defaults_in_agentpool = {}
         for attr_name, attr_value in vars(agentpool).items():
             if not attr_name.startswith("_") and attr_name != "name" and attr_value is not None:
@@ -83,14 +75,7 @@ class AKSAgentPoolContextCommonTestCase(unittest.TestCase):
                 setattr(agentpool, attr_name, None)
         return agentpool
 
-    def _restore_defaults_in_agentpool(self, agentpool: AgentPool) -> AgentPool:
-        """Internal function to restore values of properties with default values of the `agentpool` object.
-
-        Restoring default values is to keep the content of the request sent by cli consistent with that before the
-        refactoring.
-
-        :return: the AgentPool object
-        """
+    def _restore_defaults_in_agentpool(self, agentpool):
         for key, value in self.defaults_in_agentpool.items():
             if getattr(agentpool, key, None) is None:
                 setattr(agentpool, key, value)
@@ -98,7 +83,7 @@ class AKSAgentPoolContextCommonTestCase(unittest.TestCase):
 
     def create_initialized_agentpool_instance(
         self, nodepool_name="nodepool1", remove_defaults=True, restore_defaults=True, **kwargs
-    ) -> AgentPool:
+    ):
         """Helper function to create a properly initialized agentpool instance.
 
         :return: the AgentPool object
@@ -1561,14 +1546,7 @@ class AKSAgentPoolContextManagedClusterModeTestCase(AKSAgentPoolContextCommonTes
 
 
 class AKSAgentPoolAddDecoratorCommonTestCase(unittest.TestCase):
-    def _remove_defaults_in_agentpool(self, agentpool: AgentPool) -> AgentPool:
-        """Internal function to remove values from properties with default values of the `agentpool` object.
-
-        Removing default values is to prevent getters from mistakenly overwriting user provided values with default
-        values in the object.
-
-        :return: the AgentPool object
-        """
+    def _remove_defaults_in_agentpool(self, agentpool):
         self.defaults_in_agentpool = {}
         for attr_name, attr_value in vars(agentpool).items():
             if not attr_name.startswith("_") and attr_name != "name" and attr_value is not None:
@@ -1576,14 +1554,7 @@ class AKSAgentPoolAddDecoratorCommonTestCase(unittest.TestCase):
                 setattr(agentpool, attr_name, None)
         return agentpool
 
-    def _restore_defaults_in_agentpool(self, agentpool: AgentPool) -> AgentPool:
-        """Internal function to restore values of properties with default values of the `agentpool` object.
-
-        Restoring default values is to keep the content of the request sent by cli consistent with that before the
-        refactoring.
-
-        :return: the AgentPool object
-        """
+    def _restore_defaults_in_agentpool(self, agentpool):
         for key, value in self.defaults_in_agentpool.items():
             if getattr(agentpool, key, None) is None:
                 setattr(agentpool, key, value)
@@ -1591,7 +1562,7 @@ class AKSAgentPoolAddDecoratorCommonTestCase(unittest.TestCase):
 
     def create_initialized_agentpool_instance(
         self, nodepool_name="nodepool1", remove_defaults=True, restore_defaults=True, **kwargs
-    ) -> AgentPool:
+    ):
         """Helper function to create a properly initialized agentpool instance.
 
         :return: the AgentPool object
@@ -2224,14 +2195,7 @@ class AKSAgentPoolAddDecoratorManagedClusterModeTestCase(AKSAgentPoolAddDecorato
 
 
 class AKSAgentPoolUpdateDecoratorCommonTestCase(unittest.TestCase):
-    def _remove_defaults_in_agentpool(self, agentpool: AgentPool) -> AgentPool:
-        """Internal function to remove values from properties with default values of the `agentpool` object.
-
-        Removing default values is to prevent getters from mistakenly overwriting user provided values with default
-        values in the object.
-
-        :return: the AgentPool object
-        """
+    def _remove_defaults_in_agentpool(self, agentpool):
         self.defaults_in_agentpool = {}
         for attr_name, attr_value in vars(agentpool).items():
             if not attr_name.startswith("_") and attr_name != "name" and attr_value is not None:
@@ -2239,14 +2203,7 @@ class AKSAgentPoolUpdateDecoratorCommonTestCase(unittest.TestCase):
                 setattr(agentpool, attr_name, None)
         return agentpool
 
-    def _restore_defaults_in_agentpool(self, agentpool: AgentPool) -> AgentPool:
-        """Internal function to restore values of properties with default values of the `agentpool` object.
-
-        Restoring default values is to keep the content of the request sent by cli consistent with that before the
-        refactoring.
-
-        :return: the AgentPool object
-        """
+    def _restore_defaults_in_agentpool(self, agentpool):
         for key, value in self.defaults_in_agentpool.items():
             if getattr(agentpool, key, None) is None:
                 setattr(agentpool, key, value)
@@ -2254,7 +2211,7 @@ class AKSAgentPoolUpdateDecoratorCommonTestCase(unittest.TestCase):
 
     def create_initialized_agentpool_instance(
         self, nodepool_name="nodepool1", remove_defaults=True, restore_defaults=True, **kwargs
-    ) -> AgentPool:
+    ):
         """Helper function to create a properly initialized agentpool instance.
 
         :return: the AgentPool object
