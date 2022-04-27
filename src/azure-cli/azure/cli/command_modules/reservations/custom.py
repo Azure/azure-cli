cc401a2f840fc970e77ce2c2574d6f0589b7a847
gaoyp830 <51546332+gaoyp830@users.noreply.github.com>
2022-04-27 11:36:54 -0700

    Remove unused

--- src/azure-cli/azure/cli/command_modules/reservations/custom.py
+++ src/azure-cli/azure/cli/command_modules/reservations/custom.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from statistics import quantiles
 from azure.mgmt.reservations.models import (Patch,
                                             PurchaseRequest,
                                             SplitRequest,
