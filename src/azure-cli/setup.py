ade361c107ee1774e1f55c43421e64cc1830d4a4
Josef <github@online.ms>
2022-04-28 05:20:03 +0200

    {Packaging} Bump PyNaCl from 1.4.0 to 1.5.0 to support arm64 wheel installation (#22167)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -145,7 +145,7 @@ DEPENDENCIES = [
     'jsondiff~=1.3.0',
     'packaging>=20.9,<22.0',
     'PyGithub~=1.38',
-    'PyNaCl~=1.4.0',
+    'PyNaCl~=1.5.0',
     'scp~=0.13.2',
     'semver==2.13.0',
     'six>=1.10.0',  # six is still used by countless extensions
