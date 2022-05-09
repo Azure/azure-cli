e4e32835ce2a3d84692367682916cc1184289942
Kai Ru <kairu@microsoft.com>
2022-05-09 15:11:49 +0800

    fix style issue

--- src/azure-cli-core/azure/cli/core/aaz/_utils.py
+++ src/azure-cli-core/azure/cli/core/aaz/_utils.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import importlib
 from collections import OrderedDict
 
 from azure.cli.core.aaz.exceptions import AAZInvalidShorthandSyntaxError
