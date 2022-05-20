c80554262f46e695f821c82ca77e386600789bb2
hmmorales <100385146+hmmorales@users.noreply.github.com>
2022-05-19 18:20:02 -0700

    [IoT] Add private link and private end point paramaters to IoT Central CLI (#22273)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -92,7 +92,7 @@ DEPENDENCIES = [
     'azure-mgmt-extendedlocation==1.0.0b2',
     'azure-mgmt-hdinsight~=9.0.0',
     'azure-mgmt-imagebuilder~=1.0.0',
-    'azure-mgmt-iotcentral~=9.0.0',
+    'azure-mgmt-iotcentral~=10.0.0b1',
     'azure-mgmt-iothub==2.2.0',
     'azure-mgmt-iothubprovisioningservices==1.1.0',
     'azure-mgmt-keyvault==9.3.0',
