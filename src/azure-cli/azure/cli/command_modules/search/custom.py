8c7d38990e32d8a1c9efe79291f41de6647db50f
Matt <matthew.gotteiner@microsoft.com>
2022-05-24 14:32:53 -0700

    fix style violations

--- src/azure-cli/azure/cli/command_modules/search/custom.py
+++ src/azure-cli/azure/cli/command_modules/search/custom.py
@@ -30,10 +30,11 @@ def create_search_service(cmd, resource_group_name, search_service_name, sku, lo
     :param replica_count: Number of replicas in the search service.
     :param public_network_access: Public accessibility to the search service;
                                   allowed values are "enabled" or "disabled".
-    :param ip_rules: Public IP(v4) addresses or CIDR ranges to the search service, seperated by comma or semicolon;
+    :param ip_rules: Public IP(v4) addresses or CIDR ranges to the search service, separated by comma or semicolon;
                      these IP rules are applicable only when --public-network-access is "enabled".
     :param identity_type: The identity type; possible values include: "None", "SystemAssigned".
-    :param hosting_mode: The hosting mode; possible values include: "default", "highDensity". Note that "highDensity" is only applicable to the standard3 SKU.
+    :param hosting_mode: The hosting mode; possible values include: "default", "highDensity";
+                     Note that "highDensity" is only applicable to the standard3 SKU.
     """
     from azure.mgmt.search.models import SearchService, Sku, NetworkRuleSet, IpRule, Identity
     from azure.cli.command_modules.search._client_factory import cf_search_services
