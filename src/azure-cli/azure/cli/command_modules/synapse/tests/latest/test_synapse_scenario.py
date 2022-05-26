e01b777b2273d36d2e467e8b5ed34c13b6ad8336
kevinzz6 <632604666@qq.com>
2022-05-26 15:51:41 +0800

    fix update workspace package test

--- src/azure-cli/azure/cli/command_modules/synapse/tests/latest/test_synapse_scenario.py
+++ src/azure-cli/azure/cli/command_modules/synapse/tests/latest/test_synapse_scenario.py
@@ -2636,8 +2636,8 @@ class SynapseScenarioTests(ScenarioTest):
     @StorageAccountPreparer(name_prefix='adlsgen2', length=16, location=location, key='storage-account')
     def test_workspace_package(self):
         self.kwargs.update({
-            'name': 'wordcount.jar',
-            'file': os.path.join(os.path.join(os.path.dirname(__file__), 'assets'), 'wordcount.jar')
+            'name': 'urllib3-1.26.9-py2.py3-none-any.whl',
+            'file': os.path.join(os.path.join(os.path.dirname(__file__), 'assets'), 'urllib3-1.26.9-py2.py3-none-any.whl')
         })
 
         # create a workspace
