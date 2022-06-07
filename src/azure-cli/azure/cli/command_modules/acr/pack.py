0cd346d151c0ec374755be296196deeb492248c5
Magnus Longva (IT MSS LID) <magnus@longva.email>
2022-06-07 10:37:04 +0200

    default to None

--- src/azure-cli/azure/cli/command_modules/acr/pack.py
+++ src/azure-cli/azure/cli/command_modules/acr/pack.py
@@ -49,7 +49,7 @@ def acr_pack_build(cmd,  # pylint: disable=too-many-locals
 
     client_registries = cf_acr_registries_tasks(cmd.cli_ctx)
     source_location = prepare_source_location(
-        cmd, source_location, client_registries, registry_name, resource_group_name, None)
+        cmd, source_location, client_registries, registry_name, resource_group_name)
     if not source_location:
         raise CLIError('Building with Buildpacks requires a valid source location.')
 
