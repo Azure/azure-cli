fe2273195695c05c947924f52295d477f8b72b9a
Zeng Taoxu <96050289+ZengTaoxu@users.noreply.github.com>
2022-06-09 12:10:50 +0800

    {Packaging} Bump azure-core from 1.21.1 to 1.24.0 (#22676)

--- src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_account_commands.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/tests/latest/test_account_commands.py
@@ -4,7 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer
-LOCATION = "eastus2"
+LOCATION = "southcentralusstage"
 
 # No tidy up of tests required. The resource group is automatically removed
 
