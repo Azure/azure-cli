355fd6a3efa24212c882f480a6f29d7d2baf73cc
Jonie Liu (Shanghai Wicresoft Co <v-jonieliu@microsoft.com>
2022-06-15 20:03:02 +0800

    fix conflicts

--- src/azure-cli/azure/cli/command_modules/storage/operations/file.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/file.py
@@ -137,23 +137,51 @@ def list_share_files(cmd, client, directory_name=None, timeout=None, exclude_dir
     return results
 
 
+def storage_file_upload(client, local_file_path, content_settings=None,
+                        metadata=None, validate_content=False, progress_callback=None, max_connections=2, timeout=None):
+    upload_args = {
+        'content_settings': content_settings,
+        'metadata': metadata,
+        'validate_content': validate_content,
+        'max_concurrency': max_connections,
+        'timeout': timeout
+    }
+    if progress_callback:
+        upload_args['raw_response_hook'] = progress_callback
+    # Because the contents of the uploaded file may be too large, it should be passed into the a stream object,
+    # upload_file() read file data in batches to avoid OOM problems
+    count = os.path.getsize(local_file_path)
+    with open(local_file_path, 'rb') as stream:
+        response = client.upload_file(data=stream, length=count, **upload_args)
+
+    if isinstance(response['content_md5'], bytearray):
+        response['content_md5'] = ''.join(hex(x) for x in response['content_md5'])
+
+    return response
+
+
 def storage_file_upload_batch(cmd, client, destination, source, destination_path=None, pattern=None, dryrun=False,
                               validate_content=False, content_settings=None, max_connections=1, metadata=None,
-                              progress_callback=None):
+                              progress_callback=None, dsts=None):
     """ Upload local files to Azure Storage File Share in batch """
 
     from azure.cli.command_modules.storage.util import glob_files_locally, normalize_blob_file_path
 
     source_files = list(glob_files_locally(source, pattern))
-    settings_class = cmd.get_models('file.models#ContentSettings')
+    settings_class = cmd.get_models('_models#ContentSettings')
 
     if dryrun:
         logger.info('upload files to file share')
         logger.info('    account %s', client.account_name)
         logger.info('      share %s', destination)
         logger.info('      total %d', len(source_files))
-        return [{'File': client.make_file_url(destination, os.path.dirname(dst) or None, os.path.basename(dst)),
-                 'Type': guess_content_type(src, content_settings, settings_class).content_type} for src, dst in
+        kwargs = {
+            'dir_name': os.path.dirname(dsts),
+            'file_name': os.path.basename(dsts)
+        }
+
+        return [{'File': create_file_url(client, **kwargs),
+                 'Type': guess_content_type(src, content_settings, settings_class).content_type} for src, dsts in
                 source_files]
 
     # TODO: Performance improvement
@@ -163,19 +191,17 @@ def storage_file_upload_batch(cmd, client, destination, source, destination_path
         dir_name = os.path.dirname(dst)
         file_name = os.path.basename(dst)
 
-        _make_directory_in_files_share(client, destination, dir_name)
-        create_file_args = {'share_name': destination, 'directory_name': dir_name, 'file_name': file_name,
-                            'local_file_path': src, 'progress_callback': progress_callback,
-                            'content_settings': guess_content_type(src, content_settings, settings_class),
-                            'metadata': metadata, 'max_connections': max_connections}
-
-        if cmd.supported_api_version(min_api='2016-05-31'):
-            create_file_args['validate_content'] = validate_content
+        _make_directory_in_files_share(client, destination, dir_name, V2=True)
 
         logger.warning('uploading %s', src)
-        client.create_file_from_path(**create_file_args)
+        storage_file_upload(client.get_file_client(dst), src, content_settings, metadata, validate_content,
+                            progress_callback, max_connections)
 
-        return client.make_file_url(destination, dir_name, file_name)
+        args = {
+            'dir_name': dir_name,
+            'file_name': file_name
+        }
+        return create_file_url(client, **args)
 
     return list(_upload_action(src, dst) for src, dst in source_files)
 
