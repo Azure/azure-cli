792d8faf3fdd4b1a49c72a7c8a21b33c0c16a3c7
Fuming Zhang <fumingzhang@microsoft.com>
2022-06-21 14:06:10 +0800

    reset __init__.py

--- src/azure-cli/azure/cli/command_modules/acs/__init__.py
+++ src/azure-cli/azure/cli/command_modules/acs/__init__.py
@@ -3,7 +3,10 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+
 from azure.cli.core import AzCommandsLoader
+
+import azure.cli.command_modules.acs._help  # pylint: disable=unused-import
 
 
 class ContainerServiceCommandsLoader(AzCommandsLoader):
