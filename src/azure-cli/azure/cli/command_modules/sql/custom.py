9657e862331f5969b31205308efb35814bf94156
Bogdan Ostojic <49651629+ostojicb@users.noreply.github.com>
2022-06-22 02:34:08 +0200

    Update src/azure-cli/azure/cli/command_modules/sql/custom.py

--- src/azure-cli/azure/cli/command_modules/sql/custom.py
+++ src/azure-cli/azure/cli/command_modules/sql/custom.py
@@ -5193,7 +5193,7 @@ def managed_db_log_replay_stop(
         resource_group_name=resource_group_name,
         restore_details_name=RestoreDetailsName.DEFAULT)
     # If type is present, it must be lrsrestore in order to proceed with stop-log-replay
-    if (hasattr(logReplayData, 'type_properties_type') and logReplayData.type_properties_type.lower() != 'lrsrestore'):        
+    if (hasattr(logReplayData, 'type_properties_type') and logReplayData.type_properties_type.lower() != 'lrsrestore'):
         raise CLIError(f'Cannot stop the log replay as database {database_name} on the instance {managed_instance_name}'
                       'in the resource group {resource_group_name} was not created with log replay service.')
 
