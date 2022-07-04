eeaa611b3c2d0ee335379ffdc431e5147ba062ba
jiasli <4003950+jiasli@users.noreply.github.com>
2022-07-04 16:28:23 +0800

    py36

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -35,7 +35,6 @@ CLASSIFIERS = [
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
