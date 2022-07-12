a9b6e2b0b4463c387a8fe18212e451f7e03d63bb
Yan Zhu <105691024+yanzhudd@users.noreply.github.com>
2022-07-12 17:48:09 +0800

    [Compute] `az ppg create/update`: Add parameter `--intentvmsizes` to support one or more VM sizes and `--zone` to support specifying availability zone where the ppg should be created (#23167)
    * draft: function and test are finished, need check
    
    * --intentvmsizes change to --intent-vm-sizes, and some details
    
    * param
    
    * style check

--- src/azure-cli/azure/cli/command_modules/vm/_validators.py
+++ src/azure-cli/azure/cli/command_modules/vm/_validators.py
@@ -2129,6 +2129,17 @@ def _disk_encryption_set_format(cmd, namespace, name):
 # endregion
 
 
+def process_ppg_create_namespace(namespace):
+    """
+    The availability zone can be provided only when an intent is provided
+    """
+    if namespace.zone and not namespace.intent_vm_sizes:
+        raise RequiredArgumentMissingError('The --zone can be provided only when an intent is provided. '
+                                           'Please use parameter --intent-vm-sizes to specify possible sizes of '
+                                           'virtual machines that can be created in the proximity placement group.')
+# endregion
+
+
 def process_image_version_create_namespace(cmd, namespace):
     process_gallery_image_version_namespace(cmd, namespace)
     process_image_resource_id_namespace(namespace)
