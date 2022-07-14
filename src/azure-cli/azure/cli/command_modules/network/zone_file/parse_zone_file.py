f2784c3ebb98e90177e904339f50efda6b29e43f
Fayaz Shaik <shaikfayaz@microsoft.com>
2022-07-14 23:33:53 +0530

    resolved comments

--- src/azure-cli/azure/cli/command_modules/network/zone_file/parse_zone_file.py
+++ src/azure-cli/azure/cli/command_modules/network/zone_file/parse_zone_file.py
@@ -436,6 +436,7 @@ def parse_zone_file(text, zone_name, ignore_invalid=False):
 
         if not parse_match and not ignore_invalid:
             raise InvalidArgumentValueError('Unable to parse: {}'.format(record_line))
+
         record_type = record['delim'].lower()
         if record_type == '$origin':
             origin_value = record['val']
