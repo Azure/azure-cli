12bb90235879bc4e947b0bd579a188220ae93470
necusjz <necusjz@gmail.com>
2022-07-15 15:43:49 +0800

    remove useless code blocks

--- src/azure-cli/azure/cli/command_modules/network/_params.py
+++ src/azure-cli/azure/cli/command_modules/network/_params.py
@@ -2269,29 +2269,3 @@ def load_arguments(self, _):
             c.argument('resource_group_name', required=False)
             c.argument('resource_name', required=False, help='Name of the resource')
     # endregion
-
-    # region Network Virtual Appliance
-    with self.argument_context('network virtual-appliance', arg_group='Sku') as c:
-        c.argument('vendor', help='Virtual Appliance Vendor.')
-        c.argument('bundled_scale_unit', options_list=['--scale-unit'], help='Virtual Appliance Scale Unit.')
-        c.argument('market_place_version', options_list=['--version', '-v'], help='Virtual Appliance Version.')
-    with self.argument_context('network virtual-appliance') as c:
-        c.argument('network_virtual_appliance_name', help='The name of Network Virtual Appliance', options_list=['--name', '-n'])
-        c.argument('boot_strap_configuration_blobs', options_list=['--boot-strap-config-blobs', '--boot-blobs'], nargs='+', help='Space-separated list of BootStrapConfigurationBlobs storage URLs.')
-        c.argument('cloud_init_configuration_blobs', options_list=['--cloud-init-config-blobs', '--cloud-blobs'], nargs='+', help='Space-separated list of CloudInitConfigurationBlob storage URLs.')
-        c.argument('virtual_hub', options_list=['--vhub'], help='Name or ID of the virtual hub to which the Security Partner Provider belongs.', validator=validate_virtual_hub)
-        c.argument('cloud_init_configuration', options_list=['--cloud-init-config', '--init-config'], help='CloudInitConfiguration scripts that will be run during cloud initialization')
-        c.argument('asn', type=int, help='VirtualAppliance ASN. The valid value ranges from 1 to 4294967295. ')
-
-    with self.argument_context('network virtual-appliance sku') as c:
-        c.argument('sku_name', help='The name of Network Virtual Appliance SKU', options_list=['--name', '-n'])
-
-    with self.argument_context('network virtual-appliance site') as c:
-        c.argument('network_virtual_appliance_name', options_list=['--appliance-name'])
-        c.argument('site_name', help='The name of Network Virtual Appliance Site', options_list=['--name', '-n'])
-        c.argument('address_prefix', help='Address Prefix of Network Virtual Appliance Site')
-    with self.argument_context('network virtual-appliance site', arg_group='Breakout of O365') as c:
-        c.argument('allow', arg_type=get_three_state_flag(), help='Flag to control breakout of o365 allow category.')
-        c.argument('optimize', arg_type=get_three_state_flag(), help='Flag to control breakout of o365 optimize category.')
-        c.argument('default', arg_type=get_three_state_flag(), help='Flag to control breakout of o365 default category.')
-    # endregion
