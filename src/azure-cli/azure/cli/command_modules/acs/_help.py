6b67cd16e0209909b7229cd4c71ba189581629ae
RakeshMohan-MSFT <49954584+RakeshMohanMSFT@users.noreply.github.com>
2022-07-18 15:45:31 +0530

    [AKS] Fix #2457: Clarify subnet id description to resource id (#23234)
    Fixes https://github.com/Azure/azure-cli-extensions/issues/2457

--- src/azure-cli/azure/cli/command_modules/acs/_help.py
+++ src/azure-cli/azure/cli/command_modules/acs/_help.py
@@ -987,10 +987,10 @@ parameters:
     short-summary: Public IP prefix ID used to assign public IPs to VMSS nodes.
   - name: --vnet-subnet-id
     type: string
-    short-summary: The ID of a subnet in an existing VNet into which to deploy the cluster.
+    short-summary: The Resource Id of a subnet in an existing VNet into which to deploy the cluster.
   - name: --pod-subnet-id
     type: string
-    short-summary: The ID of a subnet in an existing VNet into which to assign pods in the cluster (requires azure network-plugin).
+    short-summary: The Resource Id of a subnet in an existing VNet into which to assign pods in the cluster (requires azure network-plugin).
   - name: --ppg
     type: string
     short-summary: The ID of a PPG.
