37fb5bb58c6a1d6d8b645428c4ee3f7b9e204bb6
Victoria Litvinova <73560279+vilit1@users.noreply.github.com>
2022-07-25 03:05:46 -0700

    [IoT] Change certificate loading to encode to b64 strings by default (#23140)

--- src/azure-cli/azure/cli/command_modules/iot/tests/latest/test_iot_certificate_commands.py
+++ src/azure-cli/azure/cli/command_modules/iot/tests/latest/test_iot_certificate_commands.py
@@ -6,14 +6,15 @@
 
 from azure.cli.testsdk import ResourceGroupPreparer, ScenarioTest
 
-from OpenSSL import crypto, SSL
-from os.path import exists, join
-from azure.cli.command_modules.iot.tests.latest._test_utils import _create_test_cert, _delete_test_cert, _create_verification_cert
+from azure.cli.command_modules.iot.tests.latest._test_utils import (
+    _create_test_cert, _delete_test_cert, _create_verification_cert, _create_fake_chain_cert
+)
 import random
 
 VERIFICATION_FILE = "verify.cer"
 CERT_FILE = "testcert.cer"
 KEY_FILE = "testkey.pvk"
+CHAIN_FILE = "testcert-chain.pem"
 MAX_INT = 9223372036854775807
 
 
@@ -22,15 +23,17 @@ class IotHubCertificateTest(ScenarioTest):
         super(IotHubCertificateTest, self).__init__('test_certificate_lifecycle')
         self.hub_name = 'iot-hub-for-cert-test'
         _create_test_cert(CERT_FILE, KEY_FILE, self.create_random_name(prefix='TESTCERT', length=24), 3, random.randint(0, MAX_INT))
+        _create_fake_chain_cert(CERT_FILE, CHAIN_FILE)
 
     def __del__(self):
-        _delete_test_cert(CERT_FILE, KEY_FILE, VERIFICATION_FILE)
+        _delete_test_cert([CERT_FILE, KEY_FILE, VERIFICATION_FILE, CHAIN_FILE])
 
     @ResourceGroupPreparer()
     def test_certificate_lifecycle(self, resource_group):
         hub = self._create_test_hub(resource_group)
         cert_name = self.create_random_name(prefix='certificate-', length=48)
         cert_name_verified = self.create_random_name(prefix='verified-certificate-', length=48)
+        chain_name = self.create_random_name(prefix='certificate-', length=48)
 
         # Create certificate
         self.cmd('iot hub certificate create --hub-name {0} -g {1} -n {2} -p {3}'.format(hub, resource_group, cert_name, CERT_FILE),
@@ -72,9 +75,15 @@ class IotHubCertificateTest(ScenarioTest):
                         checks=[self.check('name', cert_name),
                                 self.check('properties.isVerified', True)]).get_output_in_json()['etag']
 
+        # Create certificate from a chain - test how certificate is encoded in the service call
+        self.cmd('iot hub certificate create --hub-name {0} -g {1} -n {2} -p {3}'.format(hub, resource_group, chain_name, CHAIN_FILE),
+                 checks=[self.check('name', chain_name),
+                         self.check('properties.isVerified', False)])
+
         # Delete certificates
         self.cmd('iot hub certificate delete --hub-name {0} -g {1} -n {2} --etag {3}'.format(hub, resource_group, cert_name, etag))
         self.cmd('iot hub certificate delete --hub-name {0} -g {1} -n {2} --etag {3}'.format(hub, resource_group, cert_name_verified, etag_verified))
+        self.cmd('iot hub certificate delete --hub-name {0} -g {1} -n {2} --etag *'.format(hub, resource_group, chain_name))
 
     def _create_test_hub(self, resource_group):
         hub = self.create_random_name(prefix='iot-hub-for-cert-test', length=48)
