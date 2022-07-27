1348b20b55a332345b9c5f7cafb5954141500205
Haroon Feisal <haroonfeisal@microsoft.com>
2022-07-27 10:58:12 -0700

    Fixed powershell linux edge case.

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -5760,8 +5760,12 @@ def _get_functionapp_runtime_version(cmd, runtime_string, runtime_version, funct
     if not matched_runtime:
         return None
     if matched_runtime.github_actions_properties:
-        if matched_runtime.github_actions_properties.supported_version:
-            return matched_runtime.github_actions_properties.supported_version
+        gh_props = matched_runtime.github_actions_properties
+        if gh_props.is_supported:
+            if not is_linux and runtime_string.lower() == "powershell":
+                return runtime_version
+            # when stacks api is fixed, return supported_version if not null else runtime_verson
+            return gh_props.supported_version
     return None
 
 
