d960b97d950adde5eb4f85c098e25bad3411af68
ariklin <ariklin@microsoft.com>
2022-08-08 09:39:07 +0300

    update help

--- src/azure-cli/azure/cli/command_modules/security/_help.py
+++ src/azure-cli/azure/cli/command_modules/security/_help.py
@@ -1083,12 +1083,12 @@ examples:
         az security secure-score-control-definitions list
 """
 
-helps['security security_solutions_reference_data'] = """
+helps['security security-solutions-reference-data'] = """
 type: group
 short-summary: Display all security solutions reference data at the subscription level.
 """
 
-helps['security security_solutions_reference_data list'] = """
+helps['security security-solutions-reference-data list'] = """
 type: command
 short-summary: Display all security solutions reference data at the subscription level.
 examples:
