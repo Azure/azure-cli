be7a5eecec3ae1a1b9b9781c55189a978c5fabd8
Zeng Taoxu <96050289+ZengTaoxu@users.noreply.github.com>
2022-08-17 14:19:44 +0800

    {Network} `az network route-filter`: Migrate to atomic commands (#23553)
    * Migration
    
    * Delete test_network_route_filter.yaml

--- src/azure-cli/azure/cli/command_modules/network/tests/latest/test_network_commands.py
+++ src/azure-cli/azure/cli/command_modules/network/tests/latest/test_network_commands.py
@@ -2686,10 +2686,10 @@ class NetworkRouteFilterScenarioTest(ScenarioTest):
         self.cmd('network route-filter show -g {rg} -n {filter}')
         self.cmd('network route-filter list -g {rg}')
 
-        # temporalily disable this test
-        # self.cmd('network route-filter rule list-service-communities')
+
+        self.cmd('network route-filter rule list-service-communities')
         self.cmd('network route-filter rule create -g {rg} --filter-name {filter} -n rule1 --communities 12076:5040 12076:5030 --access allow')
-        self.cmd('network route-filter rule update -g {rg} --filter-name {filter} -n rule1 --set access=Deny')
+        self.cmd('network route-filter rule update -g {rg} --filter-name {filter} -n rule1')
         self.cmd('network route-filter rule show -g {rg} --filter-name {filter} -n rule1')
         self.cmd('network route-filter rule list -g {rg} --filter-name {filter}')
         self.cmd('network route-filter rule delete -g {rg} --filter-name {filter} -n rule1')
