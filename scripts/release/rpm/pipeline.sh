f16eaa1a929c8b7a7c8167b01f5ba38d4ccebc3e
Hang Lei <hanglei@microsoft.com>
2022-08-22 12:00:16 +0800

    Rename docker_file to dockerfile

--- scripts/release/rpm/pipeline.sh
+++ scripts/release/rpm/pipeline.sh
@@ -6,8 +6,8 @@ set -exv
 
 : "${BUILD_STAGINGDIRECTORY:?BUILD_STAGINGDIRECTORY environment variable not set.}"
 
-# DOCKER_FILE should be one of 'ubi' or 'fedora'
-: "${DOCKER_FILE:?DOCKER_FILE environment variable not set.}"
+# DOCKERFILE should be one of 'ubi' or 'fedora'
+: "${DOCKERFILE:?DOCKERFILE environment variable not set.}"
 # IMAGE should be RHEL or Fedora image url
 : "${IMAGE:?IMAGE environment variable not set.}"
 # PYTHON_PACKAGE should be python package name
@@ -21,8 +21,8 @@ docker build \
     --build-arg cli_version=${CLI_VERSION} \
     --build-arg image=${IMAGE} \
     --build-arg python_package=${PYTHON_PACKAGE} \
-    -f ./scripts/release/rpm/${DOCKER_FILE}.dockerfile \
-    -t azure/azure-cli:${DOCKER_FILE}-builder \
+    -f ./scripts/release/rpm/${DOCKERFILE}.dockerfile \
+    -t azure/azure-cli:${DOCKERFILE}-builder \
     .
 
 # Continue the previous build, and create a container that has the current azure-cli build but not the source code.
@@ -30,13 +30,13 @@ docker build \
     --build-arg cli_version=${CLI_VERSION} \
     --build-arg image=${IMAGE} \
     --build-arg python_package=${PYTHON_PACKAGE} \
-    -f ./scripts/release/rpm/${DOCKER_FILE}.dockerfile \
-    -t azure/azure-cli:${DOCKER_FILE} \
+    -f ./scripts/release/rpm/${DOCKERFILE}.dockerfile \
+    -t azure/azure-cli:${DOCKERFILE} \
     .
 
 # Extract the built RPM so that it can be distributed independently.
 # The RPM file looks like azure-cli-2.32.0-1.el7.x86_64.rpm
-id=$(docker create azure/azure-cli:${DOCKER_FILE}-builder)
+id=$(docker create azure/azure-cli:${DOCKERFILE}-builder)
 # https://docs.docker.com/engine/reference/commandline/cp/
 # Append /. so that the x86_64 folder's content is copied, instead of x86_64 folder itself.
 docker cp $id:/root/rpmbuild/RPMS/x86_64/. ${BUILD_STAGINGDIRECTORY}
