7f171fbe6cd3bac3ac4232973187d740dcce4f1b
ZengTaoxu <v-taoxuzeng@microsoft.com>
2022-08-23 15:25:03 +0800

    {Network} `az network local-gateway`: Migrate to atomic commands

--- src/azure-cli/azure/cli/command_modules/network/_validators.py
+++ src/azure-cli/azure/cli/command_modules/network/_validators.py
@@ -1044,17 +1044,6 @@ def process_cross_region_lb_frontend_ip_namespace(cmd, namespace):
     get_public_ip_validator()(cmd, namespace)
 
 
-def process_local_gateway_create_namespace(cmd, namespace):
-    ns = namespace
-    get_default_location_from_resource_group(cmd, ns)
-    validate_tags(ns)
-
-    use_bgp_settings = any([ns.asn or ns.bgp_peering_address or ns.peer_weight])
-    if use_bgp_settings and (not ns.asn or not ns.bgp_peering_address):
-        raise ValueError(
-            'incorrect usage: --bgp-peering-address IP --asn ASN [--peer-weight WEIGHT]')
-
-
 def process_nic_create_namespace(cmd, namespace):
     get_default_location_from_resource_group(cmd, namespace)
     validate_tags(namespace)
