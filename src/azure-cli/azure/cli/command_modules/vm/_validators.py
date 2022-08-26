eaafe4bca1a603620e6c5a143c4a23108840951f
songlu <442586197@qq.com>
2022-08-26 16:01:06 +0800

    Update _validators.py

--- src/azure-cli/azure/cli/command_modules/vm/_validators.py
+++ src/azure-cli/azure/cli/command_modules/vm/_validators.py
@@ -1315,7 +1315,7 @@ def _validate_trusted_launch(namespace):
     if not namespace.security_type or namespace.security_type.lower() != 'trustedlaunch':
         return
 
-    if not namespace.enable_vtpm or not namespace.enable_secure_boot:
+    if not namespace.enable_secure_boot:
         logger.warning('It is recommended to specify "--enable-secure-boot True" to receive the full suite of security'
                        ' features that comes with Trusted Launch. Please note that the "--enable-secure-boot" will'
                        ' be enabled by default in Microsoft Ignite Event (around November)')
