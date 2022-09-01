bcf7d8d5dffc20c8e1c5ab17b93a382d2e0f34df
yishiwang@microsoft.com <yishiwang@microsoft.com>
2022-09-01 11:23:29 +0800

    unused import

--- src/azure-cli-core/azure/cli/core/intercept_survey.py
+++ src/azure-cli-core/azure/cli/core/intercept_survey.py
@@ -7,7 +7,6 @@ import os
 import sys
 import json
 from datetime import datetime, timedelta
-from azure.cli.core import __version__ as core_version
 from azure.cli.core._config import GLOBAL_CONFIG_DIR
 from azure.cli.core._profile import Profile
 from azure.cli.core.style import print_styled_text
@@ -80,8 +79,6 @@ def prompt_survey_message(cli):
         return
 
     # prompt message
-    installation_id = Profile(cli_ctx=cli).get_installation_id()
-    survey_link = _SURVEY_URL.format(installation_id=installation_id, version=core_version, day=0)
     print_styled_text((SURVEY_STYLE, NEW_LINE))
     print_styled_text([
         (SURVEY_STYLE, f"[Survey] Help us improve Azure CLI by sharing your experience. "
