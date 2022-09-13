876c55c22a93cc222c3275a9777d68d73b9d59d7
jiasli <4003950+jiasli@users.noreply.github.com>
2022-09-13 10:32:09 +0800

    update test

--- src/azure-cli/azure/cli/command_modules/role/_msgrpah/tests/test_grpah_client.py
+++ src/azure-cli/azure/cli/command_modules/role/_msgrpah/tests/test_grpah_client.py
@@ -9,11 +9,11 @@ from azure.cli.command_modules.role._msgrpah._graph_client import _get_user_url
 def test_get_user_url():
     # userPrincipalName beginning with a $ character
     url = _get_user_url('$AdeleVance@contoso.com')
-    assert url == "/users('$AdeleVance@contoso.com')"
+    assert url == "/users('%24AdeleVance%40contoso.com')"
 
     # userPrincipalName with # character (B2B user)
     url = _get_user_url('AdeleVance_adatum.com#EXT#@contoso.com')
-    assert url == "/users/AdeleVance_adatum.com%23EXT%23@contoso.com"
+    assert url == '/users/AdeleVance_adatum.com%23EXT%23%40contoso.com'
 
     # object ID
     url = _get_user_url('bc04869a-5338-4dc1-95a8-5bc10da06c91')
