321c7effdf7ef1a0cd1c98d97789877242f423f6
necusjz <necusjz@gmail.com>
2022-09-19 16:23:52 +0800

    upload test recordings

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/vnet/peering/_delete.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/vnet/peering/_delete.py
@@ -13,7 +13,6 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "network vnet peering delete",
-    confirmation="Are you sure you want to perform this operation?",
 )
 class Delete(AAZCommand):
     """Delete a peering.
