d9d9fe84c66f9ec597285c4e28995b0adffeefd6
Chander Prabh Jain <chanderprabhjain95@gmail.com>
2022-09-28 12:23:05 +0530

    {RDBMS}: Switch Back to Online migration by default for PG single to flex migrations using DMS (#24022)
    * reverting to online mode for dms migrations
    
    * retrigger checks
    
    * retrigger checks
    
    * Ran style checks
    
    Co-authored-by: Chanderprabh Jain <chajain@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_common.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_common.py
@@ -63,9 +63,7 @@ def migration_create_func(cmd, client, resource_group_name, server_name, propert
         raise FileOperationError("Properties file does not exist in the given location")
     with open(properties_filepath, "r") as f:
         try:
-            request_payload = json.load(f)
-            request_payload.get("properties")['TriggerCutover'] = 'true'
-            json_data = json.dumps(request_payload)
+            json_data = json.dumps(json.load(f))
         except ValueError as err:
             logger.error(err)
             raise BadRequestError("Invalid json file. Make sure that the json file content is properly formatted.")
@@ -95,7 +93,7 @@ def migration_list_func(cmd, client, resource_group_name, server_name, migration
     return r.json()
 
 
-def migration_update_func(cmd, client, resource_group_name, server_name, migration_name, setup_logical_replication=None, db_names=None, overwrite_dbs=None, start_data_migration=None):
+def migration_update_func(cmd, client, resource_group_name, server_name, migration_name, setup_logical_replication=None, db_names=None, overwrite_dbs=None, cutover=None, start_data_migration=None):
 
     subscription_id = get_subscription_id(cmd.cli_ctx)
 
@@ -118,6 +116,12 @@ def migration_update_func(cmd, client, resource_group_name, server_name, migrati
             raise MutuallyExclusiveArgumentError("Incorrect Usage: Can only specify one update operation.")
         operationSpecified = True
         properties = "{\"properties\": {\"overwriteDBsInTarget\": \"true\"} }"
+
+    if cutover is True:
+        if operationSpecified is True:
+            raise MutuallyExclusiveArgumentError("Incorrect Usage: Can only specify one update operation.")
+        operationSpecified = True
+        properties = "{\"properties\": {\"triggerCutover\": \"true\"} }"
 
     if start_data_migration is True:
         if operationSpecified is True:
