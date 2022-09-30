2f309cf7e55dc102c0f3b34baeca08eb897ea0e5
Jiashuo Li <4003950+jiasli@users.noreply.github.com>
2022-09-30 13:41:35 +0800

    [Core] Revert #23514: Rename entry script `az.ps1` to `azps.ps1` (#24015)

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -173,7 +173,7 @@ setup(
         'az',
         'az.completion.sh',
         'az.bat',
-        'az.ps1'
+        'azps.ps1'
     ],
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "azure", "azure.cli"]),
     install_requires=DEPENDENCIES,
