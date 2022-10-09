2041698bf24503f4854bfb2e44de852d2c8692a3
Yan Zhu <zhuyan@microsoft.com>
2022-10-09 10:13:34 +0800

    resolve conflict

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -429,7 +429,9 @@ def create_managed_disk(cmd, resource_group_name, disk_name, location=None,  # p
                 # will set default value of hyper_v_generation
                 if hasattr(gallery_image_info, 'features') and gallery_image_info.features \
                         and 'SecurityType' in gallery_image_info.features \
-                        and gallery_image_info.features['SecurityType'] == 'TrustedLaunchSupported':
+                        and gallery_image_info.features['SecurityType'] in \
+                        ['TrustedLaunchSupported', 'TrustedLaunch', 'TrustedLaunchAndConfidentialVmSupported'] \
+                        and security_type != 'TrustedLaunch':
                     # will set default value of security_type
                     logger.warning('Starting Build 2023 event, az disk create command will deploy Trusted Launch VM '
                                    'by default. Please use "--security-type TrustedLaunch" in your scripts in advance '
