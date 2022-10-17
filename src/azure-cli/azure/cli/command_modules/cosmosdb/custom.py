e586238a5b146ae9c219f714b86b31dee336908a
navba-MSFT <57353862+navba-MSFT@users.noreply.github.com>
2022-10-17 19:33:20 +0530

    Update custom.py

--- src/azure-cli/azure/cli/command_modules/cosmosdb/custom.py
+++ src/azure-cli/azure/cli/command_modules/cosmosdb/custom.py
@@ -389,7 +389,8 @@ def cli_cosmosdb_update(client,
         from azure.cli.core.azclierror import InvalidArgumentValueError
         for resource_id in network_acl_bypass_resource_ids:
             if not is_valid_resource_id(resource_id):
-                raise InvalidArgumentValueError(f'{resource_id} is not a valid resource ID for --network-acl-bypass-resource-ids')
+                raise InvalidArgumentValueError(
+                    f'{resource_id} is not a valid resource ID for --network-acl-bypass-resource-ids')
 
     if max_staleness_prefix is None:
         max_staleness_prefix = existing.consistency_policy.max_staleness_prefix
