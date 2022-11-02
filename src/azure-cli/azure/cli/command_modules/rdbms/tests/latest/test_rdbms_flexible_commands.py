a332548485d263be956bb52346b5e3795f51d154
Alan Enrique Ontiveros Salazar <alanenriqueo@microsoft.com>
2022-11-02 00:47:28 -0700

    Update azure-mgmt-rdbms to 10.0.2b4

--- src/azure-cli/azure/cli/command_modules/rdbms/tests/latest/test_rdbms_flexible_commands.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/tests/latest/test_rdbms_flexible_commands.py
@@ -3,7 +3,6 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 import os
-import unittest
 import time
 
 from datetime import datetime, timedelta, tzinfo
