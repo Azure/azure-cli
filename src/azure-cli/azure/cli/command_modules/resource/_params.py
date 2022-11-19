230f406817bb47e240e653e85cedef0d984e71f2
Janusz Nowak <janono@gmail.com>
2022-11-19 01:17:48 +0100

    Update _params.py

--- src/azure-cli/azure/cli/command_modules/resource/_params.py
+++ src/azure-cli/azure/cli/command_modules/resource/_params.py
@@ -103,6 +103,10 @@ def load_arguments(self, _):
     bicep_outdir_type = CLIArgumentType(options_list=['--outdir'], completer=DirectoriesCompleter(), help="When set, saves the output at the specified directory.")
     bicep_outfile_type = CLIArgumentType(options_list=['--outfile'], completer=FilesCompleter(), help="When set, saves the output as the specified file path.")
     bicep_stdout_type = CLIArgumentType(options_list=['--stdout'], action='store_true', help="When set, prints all output to stdout instead of corresponding files.")
+    bicep_indentkind_type = CLIArgumentType(options_list=['--indentKind'], action='store_true', help="Set indentation kind. Valid values are ( Space | Tab ).")
+    bicep_indentsize_type = CLIArgumentType(options_list=['--indentSize'], action='store_true', help="Number of spaces to indent with (Only valid with --indentKind set to Space).")
+    bicep_insertfinalnewline_type = CLIArgumentType(options_list=['--insertFinalNewline'], action='store_true', help="Insert a final newline.")
+    bicep_newline_type = CLIArgumentType(options_list=['--newline'], action='store_true', help="Set newline char. Valid values are ( Auto | LF | CRLF | CR ).")
     bicep_target_platform_type = CLIArgumentType(options_list=['--target-platform', '-t'],
                                                  arg_type=get_enum_type(
                                                      ["win-x64", "linux-musl-x64", "linux-x64", "osx-x64"]),
@@ -659,10 +663,10 @@ def load_arguments(self, _):
         c.argument('outdir', arg_type=bicep_outdir_type)
         c.argument('outfile', arg_type=bicep_outfile_type)
         c.argument('stdout', arg_type=bicep_stdout_type)
-        c.argument('indentKind', arg_type=bicep_no_restore_type, help="Set indentation kind. Valid values are ( Space | Tab ).)
-        c.argument('indentSize', arg_type=bicep_no_restore_type, help="Number of spaces to indent with (Only valid with --indentKind set to Space).)
-        c.argument('insertFinalNewline', arg_type=bicep_no_restore_type, help="Insert a final newline.")
-        c.argument('newline', arg_type=bicep_no_restore_type, help="Set newline char. Valid values are ( Auto | LF | CRLF | CR ).")
+        c.argument('indentKind', arg_type=bicep_indentkind_type, help="Set indentation kind. Valid values are ( Space | Tab ).)
+        c.argument('indentSize', arg_type=bicep_indentsize_type, help="Number of spaces to indent with (Only valid with --indentKind set to Space).)
+        c.argument('insertFinalNewline', arg_type=bicep_insertfinalnewline_type, help="Insert a final newline.")
+        c.argument('newline', arg_type=bicep_newline_type, help="Set newline char. Valid values are ( Auto | LF | CRLF | CR ).")
 
     with self.argument_context('bicep decompile') as c:
         c.argument('file', arg_type=bicep_file_type, help="The path to the ARM template to decompile in the file system.")
