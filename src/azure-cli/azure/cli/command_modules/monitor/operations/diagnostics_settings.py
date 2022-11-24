cef745c55edb3a5aa2ff75bf965c62143d0c13c4
AllyWang <liwang1025@gmail.com>
2022-11-24 10:55:17 +0800

    add parameter for show and delete

--- src/azure-cli/azure/cli/command_modules/monitor/operations/diagnostics_settings.py
+++ src/azure-cli/azure/cli/command_modules/monitor/operations/diagnostics_settings.py
@@ -8,68 +8,93 @@ from azure.cli.command_modules.monitor.aaz.latest.monitor.diagnostic_settings im
 from azure.cli.command_modules.monitor.aaz.latest.monitor.diagnostic_settings import Delete as _DiagnosticSettingsDelete
 from knack.util import CLIError
 
+def create_resource_parameters(arg_schema):
+    from azure.cli.core.aaz import AAZResourceGroupNameArg, AAZStrArg, AAZResourceIdArgFormat
+    arg_schema.resource_group_name = AAZResourceGroupNameArg(arg_group="Target Resource")
+    arg_schema.namespace = AAZStrArg(
+        options=['--resource-namespace'],
+        help="Target resource provider namespace.",
+        arg_group="Target Resource",
+    )
+    arg_schema.parent = AAZStrArg(
+        options=['--resource-parent'],
+        help="Target resource parent path, if applicable.",
+        arg_group="Target Resource",
+    )
+    arg_schema.resource_type = AAZStrArg(
+        options=['--resource-type'],
+        help="Target resource type. Can also accept namespace/type format (Ex: 'Microsoft.Compute/virtualMachines')",
+        arg_group="Target Resource",
+    )
+
+def update_resource_parameters(ctx, alias="resource"):
+    args = ctx.args
+    from msrestazure.tools import is_valid_resource_id
+    from azure.mgmt.core.tools import parse_resource_id
+    from azure.cli.core.aaz import has_value
+    name_or_id = args.resource.to_serialized_data()
+    usage_error = CLIError('usage error: --{0} ID | --{0} NAME --resource-group NAME '
+                           '--{0}-type TYPE [--{0}-parent PARENT] '
+                           '[--{0}-namespace NAMESPACE]'.format(alias))
+    if is_valid_resource_id(name_or_id):
+        if has_value(args.namespace) or has_value(args.parent) or has_value(args.resource_type):
+            raise usage_error
+        parsed_id = parse_resource_id(name_or_id)
+        subscription_id = parsed_id.get('subscription', None)
+        if subscription_id:
+            # update subscription_id to support cross tenants
+            ctx.update_aux_subscriptions(subscription_id)
+    else:
+        res_ns = args.namespace.to_serialized_data()
+        rg = args.resource_group_name.to_serialized_data()
+        res_type = args.resource_type.to_serialized_data()
+        parent = args.parent.to_serialized_data()
+        if has_value(res_type):
+            if '/' in res_type:
+                res_ns = res_ns or res_type.rsplit('/', 1)[0]
+                res_type = res_type.rsplit('/', 1)[1]
+
+        if not res_ns or not rg or not res_type or not name_or_id:
+            raise usage_error
+
+        args.resource = f"/subscriptions/{ctx.subscription_id}/resourceGroups/{rg}/providers/{res_ns}/{parent + '/' if parent else ''}{res_type}/{name_or_id}"
+
 class DiagnosticSettingsShow(_DiagnosticSettingsShow):
-    pass
+
+    @classmethod
+    def _build_arguments_schema(cls, *args, **kwargs):
+        arg_schema = super()._build_arguments_schema(*args, **kwargs)
+        create_resource_parameters(arg_schema)
+        return arg_schema
+
+    def pre_operations(self):
+        ctx = self.ctx
+        update_resource_parameters(ctx)
+
 
 class DiagnosticSettingsList(_DiagnosticSettingsList):
 
     @classmethod
     def _build_arguments_schema(cls, *args, **kwargs):
-        from azure.cli.core.aaz import AAZResourceGroupNameArg, AAZStrArg, AAZResourceIdArgFormat
         arg_schema = super()._build_arguments_schema(*args, **kwargs)
-
-        arg_schema.resource_group_name = AAZResourceGroupNameArg()
-        arg_schema.namespace = AAZStrArg(
-            options=['--resource-namespace'],
-            help="Target resource provider namespace.",
-        )
-        arg_schema.parent = AAZStrArg(
-            options=['--resource-parent'],
-            help="Target resource parent path, if applicable.",
-        )
-        arg_schema.resource_type = AAZStrArg(
-            options=['--resource-type'],
-            help="Target resource type. Can also accept namespace/type format (Ex: 'Microsoft.Compute/virtualMachines')",
-        )
-
+        create_resource_parameters(arg_schema)
         return arg_schema
 
     def pre_operations(self):
-        from msrestazure.tools import is_valid_resource_id
-        from azure.mgmt.core.tools import parse_resource_id
-        from azure.cli.core.aaz import has_value
-        args = self.ctx.args
-        name_or_id = args.resource.to_serialized_data()
-        alias = 'resource'
-        usage_error = CLIError('usage error: --{0} ID | --{0} NAME --resource-group NAME '
-                               '--{0}-type TYPE [--{0}-parent PARENT] '
-                               '[--{0}-namespace NAMESPACE]'.format(alias))
-        if is_valid_resource_id(name_or_id):
-            if has_value(args.namespace) or has_value(args.parent) or has_value(args.resource_type):
-                raise usage_error
-            parsed_id = parse_resource_id(name_or_id)
-            subscription_id = parsed_id.get('subscription', None)
-            if subscription_id:
-                # update subscription_id to support cross tenants
-                self.ctx.update_aux_subscriptions(subscription_id)
-        else:
-            res_ns = args.namespace.to_serialized_data()
-            rg = args.resource_group_name.to_serialized_data()
-            res_type = args.resource_type.to_serialized_data()
-            parent = args.parent.to_serialized_data()
-            if has_value(res_type):
-                if '/' in res_type:
-                    res_ns = res_ns or res_type.rsplit('/', 1)[0]
-                    res_type = res_type.rsplit('/', 1)[1]
-
-            if not res_ns or not rg or not res_type or not name_or_id:
-                raise usage_error
-
-            args.resource = f"/subscriptions/{self.ctx.subscription_id}/resourceGroups/{rg}/providers/{res_ns}/{parent + '/' if parent else ''}{res_type}/{name_or_id}"
-
+        ctx = self.ctx
+        update_resource_parameters(ctx)
 
 class DiagnosticSettingsDelete(_DiagnosticSettingsDelete):
-    pass
+
+    @classmethod
+    def _build_arguments_schema(cls, *args, **kwargs):
+        arg_schema = super()._build_arguments_schema(*args, **kwargs)
+        create_resource_parameters(arg_schema)
+        return arg_schema
+
+    def pre_operations(self):
+        ctx = self.ctx
+        update_resource_parameters(ctx)
 
 
 # pylint: disable=unused-argument, line-too-long
