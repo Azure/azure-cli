a885557ef5556cd7fd353d4e9a894d6ce046dc9a
Yan Zhu <zhuyan@microsoft.com>
2022-12-07 16:23:14 +0800

    fix style

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -4550,7 +4550,7 @@ def create_image_version(cmd, resource_group_name, gallery_name, gallery_image_n
     profile = ImageVersionPublishingProfile(exclude_from_latest=exclude_from_latest,
                                             end_of_life_date=end_of_life_date,
                                             target_regions=target_regions or [TargetRegion(name=location)],
-                                            replica_count=replica_count,storage_account_type=storage_account_type,
+                                            replica_count=replica_count, storage_account_type=storage_account_type,
                                             target_extended_locations=target_edge_zones)
     if replication_mode is not None:
         profile.replication_mode = replication_mode
