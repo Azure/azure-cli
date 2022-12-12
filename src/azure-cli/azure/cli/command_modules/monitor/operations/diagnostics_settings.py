e3a47c8c77b54a7a7fceb4935040801b8d00ccf4
AllyWang <liwang1025@gmail.com>
2022-12-12 14:03:05 +0800

    fix resource-group parse bug and add export workspace validation

--- src/azure-cli/azure/cli/command_modules/monitor/operations/diagnostics_settings.py
+++ src/azure-cli/azure/cli/command_modules/monitor/operations/diagnostics_settings.py
@@ -95,7 +95,7 @@ class DiagnosticSettingsCreate(_DiagnosticSettingsCreate):
         rg = args.resource_group_name.to_serialized_data()
 
         if not has_value(rg):
-            rg = parse_resource_id(ctx.resource)['resource_group']
+            rg = parse_resource_id(args.resource.to_serialized_data())['resource_group']
             args.resource_group_name = rg
 
         storage_account = args.storage_account.to_serialized_data()
@@ -149,6 +149,8 @@ class DiagnosticSettingsCreate(_DiagnosticSettingsCreate):
         export_to_resource_specific = args.export_to_resource_specific.to_serialized_data()
         if has_value(export_to_resource_specific) and export_to_resource_specific:
             args.log_analytics_destination_type = 'Dedicated'
+            if not has_value(workspace):
+                raise CLIError('usage error: --workspace and --export-to-specific-resource')
         else:
             args.log_analytics_destination_type = None
 
