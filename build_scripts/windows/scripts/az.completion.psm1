248c9cbef76aaa0e0fb61fdaf120c9a9250a5e7a
Hang Lei <hanglei@microsoft.com>
2022-12-14 11:56:06 +0800

    Minor fix

--- build_scripts/windows/scripts/az.completion.psm1
+++ build_scripts/windows/scripts/az.completion.psm1
@@ -11,7 +11,7 @@ Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
     az 2>&1 | Out-Null
     
     Get-Content $completion_file | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
     }
     rm $completion_file
     Remove-Item Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS
