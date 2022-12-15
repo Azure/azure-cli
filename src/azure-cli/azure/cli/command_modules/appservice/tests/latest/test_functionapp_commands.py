e75af8b99682ddce27d60a51b80e81d6e87fdebd
Kirstyn Joy Amperiadis <kamperiadis@microsoft.com>
2022-12-15 11:54:02 -0600

    Add consumption plan validation for az functionapp vnet-integration-add command

--- src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_functionapp_commands.py
+++ src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_functionapp_commands.py
@@ -1559,6 +1559,26 @@ class FunctionappNetworkConnectionTests(ScenarioTest):
     @AllowLargeResponse()
     @ResourceGroupPreparer(location=WINDOWS_ASP_LOCATION_FUNCTIONAPP)
     @StorageAccountPreparer()
+    def test_functionapp_vnet_integration_consumption_plan(self, resource_group, storage_account):
+        functionapp_name = self.create_random_name('swiftfunctionapp', 24)
+        subnet_name = self.create_random_name('swiftsubnet', 24)
+        vnet_name = self.create_random_name('swiftname', 24)
+
+        self.cmd('network vnet create -g {} -n {} --address-prefix 10.0.0.0/16 --subnet-name {} --subnet-prefix 10.0.0.0/24'.format(
+            resource_group, vnet_name, subnet_name))
+        self.cmd(
+            'functionapp create -g {} -n {} -s {} --consumption-plan-location {} --functions-version 4'.format(resource_group, functionapp_name, storage_account, WINDOWS_ASP_LOCATION_FUNCTIONAPP))
+        # Wait for the functionapp and service plan to be created
+        time.sleep(30)
+        self.cmd('functionapp vnet-integration add -g {} -n {} --vnet {} --subnet {}'.format(
+            resource_group, functionapp_name, vnet_name, subnet_name), expect_failure=True)
+        self.cmd('functionapp vnet-integration list -g {} -n {}'.format(resource_group, functionapp_name), checks=[
+            JMESPathCheck('length(@)', 0),
+        ])
+
+    @AllowLargeResponse()
+    @ResourceGroupPreparer(location=WINDOWS_ASP_LOCATION_FUNCTIONAPP)
+    @StorageAccountPreparer()
     def test_functionapp_vnet_basic_sku_E2E(self, resource_group, storage_account):
         functionapp_name = self.create_random_name('swiftfunctionapp', 24)
         plan = self.create_random_name('swiftplan', 24)
