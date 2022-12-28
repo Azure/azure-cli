d48f1dbd97d83e7c7580da29dfd8f2f78669e52a
Xing Zhou <Zhou.Xing@microsoft.com>
2022-12-28 15:32:39 +0800

    Update src/azure-cli/azure/cli/command_modules/vm/aaz/latest/sig/gallery_application/version/_delete.py

--- src/azure-cli/azure/cli/command_modules/vm/aaz/latest/sig/gallery_application/version/_delete.py
+++ src/azure-cli/azure/cli/command_modules/vm/aaz/latest/sig/gallery_application/version/_delete.py
@@ -16,7 +16,7 @@ from azure.cli.core.aaz import *
     confirmation="Are you sure you want to perform this operation?",
 )
 class Delete(AAZCommand):
-    """Delete a gallery Application Version.
+    """Delete a gallery application version.
     """
 
     _aaz_info = {
