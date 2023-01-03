1c41f7bf6c87cc0ba895d0a2dcd8c453061dd641
unknown <schaudhari@microsoft.com>
2023-01-04 01:09:37 +0530

    Updates and Optimization

--- src/azure-cli/azure/cli/command_modules/eventhubs/Operation/Namespace_custom_file.py
+++ src/azure-cli/azure/cli/command_modules/eventhubs/Operation/Namespace_custom_file.py
@@ -2,18 +2,32 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+from azure.cli.command_modules.eventhubs.constants import *
+def create_keyvault_object(col):
+    vault_object={}
+    if 'userAssignedIdentity' in col:
+        vault_object['user_assigned_identity'] = col['userAssignedIdentity']
+
+    vault_object.update({
+        "key_name" : col['keyName'],
+        "key_vault_uri" : col['keyVaultUri'],
+        "Key_version" : col['keyVersion']
+    })
+
+    return  vault_object
+
 def create_eventhub_namespace(cmd, resource_group_name, namespace_name, location=None, tags=None, sku='Standard',
                          capacity=None,  tier='Standard', mi_user_assigned=None, mi_system_assigned=False,
-                          encryption_config=None, minimum_tls_version=None,disable_local_auth = None,
-                         maximum_throughput_units=None,private_endpoint_connection_name='enabled',
-                         require_infrastructure_encryption=None,is_kafka_enabled=None,
-                              is_auto_inflate_enabled=None,alternate_name=None, zone_redundant=None):
+                         encryption_config=None, minimum_tls_version=None,disable_local_auth = None,
+                         maximum_throughput_units=None,require_infrastructure_encryption=None,is_kafka_enabled=None,
+                         is_auto_inflate_enabled=None,alternate_name=None, zone_redundant=None):
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Create
-    user_assign = {}
-    dict1 = {}
-    a = "None"
-    print(encryption_config)
-    dict1 = {
+
+    user_assigned_identity = {}
+    command_args_dict = {}
+    type = "None"
+
+    command_args_dict = {
         "resource_group": resource_group_name,
         "namespace_name": namespace_name,
         "tags": tags,
@@ -28,79 +42,68 @@ def create_eventhub_namespace(cmd, resource_group_name, namespace_name, location
         "maximum_throughput_units":maximum_throughput_units,
         "is_kafka_enabled":is_kafka_enabled,
         "zone_redundant": zone_redundant,
-        "private_endpoint_connection_name": private_endpoint_connection_name,
         "disable_local_auth": disable_local_auth,
         "alternate_name": alternate_name
     }
+
     if mi_system_assigned:
-        a = "SystemAssigned"
+        type = SYSTEM
+
     if mi_user_assigned:
         if mi_system_assigned:
-            a = "SystemAssigned, UserAssigned"
+            type = SYSTEMUSER
         else:
-            a = "UserAssigned"
-
-        for col in mi_user_assigned:
-            user_assign[col] = {}
-        dict2 = {
-            "identity": {
-                "type": a,
-                "user_assigned_identities": user_assign
-            },
-        }
-        dict1.update(dict2)
+            type = USER
+        for val in mi_user_assigned:
+            user_assigned_identity[val] = {}
+        command_args_dict.update({"identity": {
+                "type": type,
+                "user_assigned_identities": user_assigned_identity
+            }})
     else:
-        dict2 = {
+        command_args_dict.update({
             "identity": {
-                "type": a,
+                "type": type,
                 "user_assigned_identities": None
-            },
-        }
-        dict1.update(dict2)
+            }
+        })
+
     if encryption_config:
-        dict3 = {
-            "encryption": {
+        command_args_dict.update({"encryption": {
                 "key_vault_properties": encryption_config,
                 "key_source": "Microsoft.KeyVault",
                 "require_infrastructure_encryption":require_infrastructure_encryption
-            }
-        }
-        dict1.update(dict3)
-    print(dict1)
-    return Create(cli_ctx=cmd.cli_ctx)(command_args=dict1)
+            }})
+
+    return Create(cli_ctx=cmd.cli_ctx)(command_args=command_args_dict)
 
 def cli_add_encryption(cmd, resource_group_name, namespace_name, encryption_config,require_infrastructure_encryption=None):
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Update
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Create
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Show
+
     eventhubsnm = Show(cli_ctx=cmd.cli_ctx)(command_args={
         "resource_group": resource_group_name,
         "namespace_name": namespace_name
     })
-    keys=[]
+    key_vault_object=[]
     for col in encryption_config:
-        keys.append(col)
+        key_vault_object.append(col)
+
     if 'encryption' in eventhubsnm:
-        i=0
         for col in eventhubsnm['encryption']['keyVaultProperties']:
-            user = {}
-            if 'userAssignedIdentity' in eventhubsnm['encryption']['keyVaultProperties'][i]:
-                user['user_assigned_identity'] = eventhubsnm['encryption']['keyVaultProperties'][i][
-                    'userAssignedIdentity']
-            user["key_name"] = eventhubsnm['encryption']['keyVaultProperties'][i]['keyName']
-            user["key_vault_uri"] = eventhubsnm['encryption']['keyVaultProperties'][i]['keyVaultUri']
-            user["Key_version"] = eventhubsnm['encryption']['keyVaultProperties'][i]['keyVersion']
-            i=i+1
-            if user not in keys:
-                keys.append(user)
+            object= create_keyvault_object(col)
+            if object not in key_vault_object:
+                key_vault_object.append(object)
         if require_infrastructure_encryption is None:
             require_infrastructure_encryption= eventhubsnm['encryption']['requireInfrastructureEncryption']
+
     return Update(cli_ctx=cmd.cli_ctx)(command_args={
         "resource_group": resource_group_name,
         "namespace_name": namespace_name,
         "encryption": {
             "key_source": "Microsoft.KeyVault",
-            "key_vault_properties": keys,
+            "key_vault_properties": key_vault_object,
             "require_infrastructure_encryption": require_infrastructure_encryption
         }
     })
@@ -109,30 +112,26 @@ def cli_remove_encryption(cmd,resource_group_name, namespace_name, encryption_co
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Update
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Show
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Create
+
     eventhubsnm = Show(cli_ctx=cmd.cli_ctx)(command_args={
         "resource_group": resource_group_name,
         "namespace_name": namespace_name
     })
-    keys = []
-    k = 0
+
+    key_vault_object = []
+
     for col in eventhubsnm['encryption']['keyVaultProperties']:
-        user = {}
-        if 'userAssignedIdentity' in eventhubsnm['encryption']['keyVaultProperties'][k]:
-            user['user_assigned_identity'] = eventhubsnm['encryption']['keyVaultProperties'][k]['userAssignedIdentity']
-        user["key_name"] = eventhubsnm['encryption']['keyVaultProperties'][k]['keyName']
-        user["key_vault_uri"] = eventhubsnm['encryption']['keyVaultProperties'][k]['keyVaultUri']
-        user["key_version"] = eventhubsnm['encryption']['keyVaultProperties'][k]['keyVersion']
-        keys.append(user)
-        k=k+1
+        object = create_keyvault_object(col)
+        key_vault_object.append(object)
     for col in encryption_config:
-        if col in keys:
-            keys.remove(col)
+        if col in key_vault_object:
+            key_vault_object.remove(col)
     return Update(cli_ctx=cmd.cli_ctx)(command_args={
         "resource_group": resource_group_name,
         "namespace_name": namespace_name,
         "encryption":{
             "require_infrastructure_encryption":eventhubsnm['encryption']['requireInfrastructureEncryption'],
-            "key_vault_properties": keys,
+            "key_vault_properties": key_vault_object,
             "key_source":"Microsoft.KeyVault"
         }
     })
@@ -140,47 +139,51 @@ def cli_add_identity(cmd, resource_group_name, namespace_name,system_assigned=No
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Update
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Show
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Create
+
     eventhubsnm = Show(cli_ctx=cmd.cli_ctx)(command_args={
         "resource_group": resource_group_name,
         "namespace_name": namespace_name
     })
+
     from azure.cli.core import CLIError
     if 'identity' not in eventhubsnm:
         eventhubsnm['identity'] = {
             "type":"None",
             "userAssignedIdentities":None
         }
+
+    type = eventhubsnm['identity']['type']
     if system_assigned:
-        if eventhubsnm['identity']['type']=="UserAssigned":
-            eventhubsnm['identity']['type']="UserAssigned, SystemAssigned"
-        elif eventhubsnm['identity']['type']=="None":
-            eventhubsnm['identity']['type'] ="SystemAssigned"
+        if type == USER:
+            type = SYSTEMUSER
+        elif type == "None":
+            type = SYSTEM
+
     if user_assigned:
-        if eventhubsnm['identity']['type']=="SystemAssigned":
-            a="SystemAssigned, UserAssigned"
+        if type == SYSTEM:
+            type = SYSTEMUSER
         else:
-            a="UserAssigned"
-        eventhubsnm['identity']['type']=a
-        user_assign={}
+            type = USER
+        user_assigned_identity = {}
         for col in user_assigned:
-            user_assign[col]={}
+            user_assigned_identity[col] = {}
+
         if 'userAssignedIdentities' in eventhubsnm['identity']:
             if eventhubsnm['identity']['userAssignedIdentities'] is None:
-                eventhubsnm['identity']['userAssignedIdentities']=user_assign
+                eventhubsnm['identity']['userAssignedIdentities'] = user_assigned_identity
             else:
-                eventhubsnm['identity']['userAssignedIdentities'].update(user_assign)
-
+                eventhubsnm['identity']['userAssignedIdentities'].update(user_assigned_identity)
         else:
-            eventhubsnm['identity']={
-                'userAssignedIdentities':user_assign,
-                'type':a
+            eventhubsnm['identity'] = {
+                'userAssignedIdentities' : user_assigned_identity,
+                'type' : type
             }
     return Update(cli_ctx=cmd.cli_ctx)(command_args={
-        "resource_group": resource_group_name,
-        "namespace_name": namespace_name,
-        "identity":{
-            "type":eventhubsnm['identity']['type'],
-            "user_assigned_identities":eventhubsnm['identity']['userAssignedIdentities']
+        "resource_group" : resource_group_name,
+        "namespace_name" : namespace_name,
+        "identity" : {
+            "type" : type,
+            "user_assigned_identities" : eventhubsnm['identity']['userAssignedIdentities']
         }
     })
 
@@ -188,40 +191,45 @@ def cli_remove_identity(cmd, resource_group_name, namespace_name, system_assigne
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Update
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Show
     from azure.cli.command_modules.eventhubs.aaz.latest.eventhubs.namespace import Create
+
     eventhubsnm = Show(cli_ctx=cmd.cli_ctx)(command_args={
         "resource_group": resource_group_name,
         "namespace_name": namespace_name
     })
+
     if eventhubsnm['identity'] is None:
         raise CLIError('The namespace does not have identity enabled')
-    dict = {}
+
+    type = eventhubsnm['identity']['type']
     if system_assigned:
-        if eventhubsnm['identity']['type'] == "SystemAssigned":
-            eventhubsnm['identity']['type'] = "None"
-        if eventhubsnm['identity']['type'] == "SystemAssigned, UserAssigned":
-            eventhubsnm['identity']['type'] = "UserAssigned"
+        if type == SYSTEM:
+            type = "None"
+        if type == SYSTEMUSER:
+            type = USER
+
     if user_assigned:
         if eventhubsnm['identity']['userAssignedIdentities']:
             for x in user_assigned:
                 eventhubsnm['identity']['userAssignedIdentities'].pop(x)
-            if eventhubsnm['identity']['type'] == "UserAssigned":
+            if type == USER:
                 if len(eventhubsnm['identity']['userAssignedIdentities']) == 0:
-                    eventhubsnm['identity']['type'] = "None"
+                    type = "None"
                     eventhubsnm['identity']['userAssignedIdentities'] = None
-            if eventhubsnm['identity']['type'] == "SystemAssigned, UserAssigned":
+            if type == SYSTEMUSER:
                 if len(eventhubsnm['identity']['userAssignedIdentities']) == 0:
-                    eventhubsnm['identity']['type'] = "SystemAssigned"
+                    type = "SystemAssigned"
                     eventhubsnm['identity']['userAssignedIdentities'] = None
-    dict={}
-    dict["resource_group"]=resource_group_name
-    dict["namespace_name"]=namespace_name
-    if 'userAssignedIdentities' in eventhubsnm['identity']:
-        dict["identity"]= {
-                "type": eventhubsnm['identity']['type'],
-                "user_assigned_identities": eventhubsnm['identity']['userAssignedIdentities']
+
+    command_args = {
+        "resource_group" : resource_group_name,
+        "namespace_name" : namespace_name,
+        "identity" : {
+            "type" : type
         }
-    else:
-        dict["identity"]= {
-                "type": eventhubsnm['identity']['type']
-            }
-    return Update(cli_ctx=cmd.cli_ctx)(command_args=dict)
+    }
+
+    if 'userAssignedIdentities' in eventhubsnm['identity']:
+        command_args["identity"].update({
+                "user_assigned_identities": eventhubsnm['identity']['userAssignedIdentities']
+        })
+    return Update(cli_ctx=cmd.cli_ctx)(command_args = command_args)
