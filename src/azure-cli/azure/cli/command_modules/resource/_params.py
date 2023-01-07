dc9db08183f36b7f46cf68b3691a20dd1bec5c7a
Janusz Nowak <janono@gmail.com>
2023-01-07 11:34:47 +0100

    Update _params.py

--- src/azure-cli/azure/cli/command_modules/resource/_params.py
+++ src/azure-cli/azure/cli/command_modules/resource/_params.py
@@ -671,7 +671,7 @@ def load_arguments(self, _):
         c.argument('outfile', arg_type=bicep_outfile_type)
         c.argument('stdout', arg_type=bicep_stdout_type)
         c.argument('indent_kind', arg_type=bicep_indentkind_type, help="Set indentation kind. Valid values are ( Space | Tab ).")
-        c.argument('indent_size', arg_type=bicep_indentsize_type, help="Number of spaces to indent with (Only valid with --indentKind set to Space).")
+        c.argument('indent_size', arg_type=bicep_indentsize_type, help="Number of spaces to indent with (Only valid with --indent-kind set to Space).")
         c.argument('insert_final_newline', arg_type=bicep_insertfinalnewline_type, help="Insert a final newline.")
         c.argument('newline', arg_type=bicep_newline_type, help="Set newline char. Valid values are ( Auto | LF | CRLF | CR ).")
 
