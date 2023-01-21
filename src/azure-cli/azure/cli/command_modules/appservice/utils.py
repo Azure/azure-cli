f98327cf0de2e34326a3ada327b55f2ccd362058
Ved Kirloskar <vekirlos@microsoft.com>
2023-01-20 16:18:41 -0800

    Final changes

--- src/azure-cli/azure/cli/command_modules/appservice/utils.py
+++ src/azure-cli/azure/cli/command_modules/appservice/utils.py
@@ -78,7 +78,9 @@ def get_sku_tier(name):  # pylint: disable=too-many-return-statements
         return 'PREMIUM'
     if name in ['P1V2', 'P2V2', 'P3V2']:
         return 'PREMIUMV2'
-    if name in ['P0V3', 'P1V3', 'P2V3', 'P3V3']:
+    if name in ['P0V3']:
+        return 'Premium0V3'
+    if name in ['P1V3', 'P2V3', 'P3V3']:
         return 'PREMIUMV3'
     if name in ['P1MV3', 'P2MV3', 'P3MV3', 'P4MV3', 'P5MV3']:
         return 'PREMIUMMV3'
