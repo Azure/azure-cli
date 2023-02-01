ee84bd5f4bde282b82aef79990de885d3eca90f0
Zubair Abid <zubairabid@microsoft.com>
2023-02-01 18:03:14 +0530

    Revert "PR Nit: action=store_true instead of three_state_flag argparse"
    This reverts commit db2eea03a9a252c0fed4b97d7f571277bab0281a.

--- src/azure-cli/azure/cli/command_modules/backup/_params.py
+++ src/azure-cli/azure/cli/command_modules/backup/_params.py
@@ -294,7 +294,7 @@ def load_arguments(self, _):
 
     with self.argument_context('backup protection disable') as c:
         c.argument('delete_backup_data', arg_type=get_three_state_flag(), help='Option to delete existing backed up data in the Recovery services vault.')
-        c.argument('retain_recovery_points_as_per_policy', action='store_true', options_list=['--retain-recovery-points-as-per-policy', '--retain-as-per-policy'], help='Switch parameter that specifies that existing recovery points should be retained for the duration specified by the backup policy')
+        c.argument('retain_recovery_points_as_per_policy', arg_type=get_three_state_flag(), options_list=['--retain-recovery-points-as-per-policy', '--retain-as-per-policy'], help='Switch parameter that specifies that existing recovery points should be retained for the duration specified by the backup policy')
         c.argument('backup_management_type', backup_management_type)
         c.argument('workload_type', workload_type)
         c.argument('tenant_id', help='ID of the tenant if the Resource Guard protecting the vault exists in a different tenant.')
