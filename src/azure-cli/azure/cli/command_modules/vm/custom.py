406ca885c3205dbe7c892985ef58858dfc9ff01a
Xing Zhou <Zhou.Xing@microsoft.com>
2023-02-02 10:47:35 +0800

    [ARM] Fix #25022: az resource tag: Fix updating tag for Microsoft.insights/workbooks issue (#25142)

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -5116,7 +5116,7 @@ def list_generator(pages, num_results=50):
 
         # handle num results
         if num_results is not None:
-            if num_results == len(result):
+            if num_results >= len(result):
                 break
 
         page = list(next(pages))
