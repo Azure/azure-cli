8d0e83c49961e4f2af1fac32f3216c6f3c2ca199
Hang <hanglei@microsoft.com>
2023-02-14 14:33:58 +0800

    {Packaging} Bump Python version to 3.10.10 (#25438)

--- scripts/release/debian/build.sh
+++ scripts/release/debian/build.sh
@@ -15,7 +15,7 @@ set -exv
 ls -Rl /mnt/artifacts
 
 WORKDIR=`cd $(dirname $0); cd ../../../; pwd`
-PYTHON_VERSION="3.10.8"
+PYTHON_VERSION="3.10.10"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update APT packages
