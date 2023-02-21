c9d8fea118615c2543785189fb7ba3b71e997012
necusjz <necusjz@gmail.com>
2023-02-21 14:41:45 +0800

    {Network} `az network watcher`: Migrate commands to codegen v2 (#25520)
    * migrate watcher configure
    
    * migrate non-validators
    
    * fix ci
    
    * remove useless parameter

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/watcher/__init__.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/watcher/__init__.py
@@ -9,4 +9,10 @@
 # flake8: noqa
 
 from .__cmd_group import *
+from ._create import *
+from ._delete import *
 from ._list import *
+from ._show_next_hop import *
+from ._show_security_group_view import *
+from ._test_ip_flow import *
+from ._update import *
