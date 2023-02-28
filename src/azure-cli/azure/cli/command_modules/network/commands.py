eaadb340a299328ce557103ab29bc82ee8c7ed91
kai ru <69238381+kairu-ms@users.noreply.github.com>
2023-02-28 13:57:55 +0800

    {Network} `az network vnet-gateway`: Migrate azure stack profiles to AAZ (#25588)

--- src/azure-cli/azure/cli/command_modules/network/commands.py
+++ src/azure-cli/azure/cli/command_modules/network/commands.py
@@ -702,16 +702,10 @@ def load_command_table(self, _):
         self.command_table['network vnet-gateway list-learned-routes'] = ListLearnedRoutes(loader=self, table_transformer=transform_vnet_gateway_routes_table)
 
     with self.command_group('network vnet-gateway packet-capture'):
-        from .custom import VnetGatewayPackageCaptureStart, VnetGatewayPackageCaptureStop
         from .aaz.latest.network.vnet_gateway import Wait
-        self.command_table['network vnet-gateway packet-capture start'] = VnetGatewayPackageCaptureStart(loader=self)
-        self.command_table['network vnet-gateway packet-capture stop'] = VnetGatewayPackageCaptureStop(loader=self)
         self.command_table['network vnet-gateway packet-capture wait'] = Wait(loader=self)
 
     with self.command_group('network vnet-gateway vpn-client') as g:
-        from .custom import VpnProfilePackageUrlShow, VpnClientConnectionHealthShow
-        self.command_table['network vnet-gateway vpn-client show-url'] = VpnProfilePackageUrlShow(loader=self)
-        self.command_table['network vnet-gateway vpn-client show-health'] = VpnClientConnectionHealthShow(loader=self)
         g.custom_command('generate', 'generate_vpn_client')
 
     with self.command_group('network vnet-gateway vpn-client ipsec-policy'):
@@ -761,9 +755,8 @@ def load_command_table(self, _):
         g.custom_command('clear', 'clear_vpn_conn_ipsec_policies', supports_no_wait=True)
 
     with self.command_group('network vpn-connection packet-capture'):
-        from .custom import VpnConnPackageCaptureStart, VpnConnPackageCaptureStop
+        from .custom import VpnConnPackageCaptureStop
         from .aaz.latest.network.vpn_connection import Wait
-        self.command_table['network vpn-connection packet-capture start'] = VpnConnPackageCaptureStart(loader=self)
         self.command_table['network vpn-connection packet-capture stop'] = VpnConnPackageCaptureStop(loader=self)
         self.command_table['network vpn-connection packet-capture wait'] = Wait(loader=self)
     # endregion
