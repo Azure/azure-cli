fa41cb583724d582fef5583459efba4bd9a2529e
Zeng Taoxu <96050289+ZengTaoxu@users.noreply.github.com>
2023-03-08 10:01:04 +0800

    {Network} azure-stack clear (#25702)
     azure-stack clear

--- src/azure-cli/azure/cli/command_modules/network/azure_stack/_format.py
+++ src/azure-cli/azure/cli/command_modules/network/azure_stack/_format.py
@@ -81,29 +81,6 @@ def transform_local_gateway_table_output(result):
     return final_result
 
 
-def transform_vpn_connection_list(result):
-    return [transform_vpn_connection(v) for v in result]
-
-
-def transform_vpn_connection(result):
-    if result:
-        properties_to_strip = \
-            ['virtual_network_gateway1', 'virtual_network_gateway2', 'local_network_gateway2', 'peer']
-        for prop in properties_to_strip:
-            prop_val = getattr(result, prop, None)
-            if not prop_val:
-                delattr(result, prop)
-            else:
-                null_props = [key for key in prop_val.__dict__ if not prop_val.__dict__[key]]
-                for null_prop in null_props:
-                    delattr(prop_val, null_prop)
-    return result
-
-
-def transform_vnet_create_output(result):
-    return {'newVNet': result.result()}
-
-
 def transform_vnet_table_output(result):
 
     def _transform(result):
@@ -124,16 +101,6 @@ def transform_vnet_table_output(result):
 
 def transform_public_ip_create_output(result):
     return {'publicIp': result.result()}
-
-
-def transform_traffic_manager_create_output(result):
-    return {'TrafficManagerProfile': result}
-
-
-def transform_nic_create_output(result):
-    if result:
-        return {'NewNIC': result.result()}
-    return None
 
 
 def transform_nsg_rule_table_output(result):
@@ -158,71 +125,6 @@ def transform_nsg_rule_table_output(result):
     if 'DestinationASG' in result:
         item['DestinationASG'] = result.get('destinationApplicationSecurityGroups', 'None')
     return item
-
-
-def transform_vnet_gateway_create_output(result):
-    result = {'vnetGateway': result.result()} if result else result
-    return result
-
-
-def transform_geographic_hierachy_table_output(result):
-    transformed = []
-
-    def _extract_values(obj):
-        obj = obj if isinstance(obj, list) else [obj]
-        for item in obj:
-            item_obj = OrderedDict()
-            item_obj['code'] = item['code']
-            item_obj['name'] = item['name']
-            transformed.append(item_obj)
-            _extract_values(item['regions'])
-
-    _extract_values(result['geographicHierarchy'])
-    return transformed
-
-
-def transform_service_community_table_output(result):
-    transformed = []
-    for item in result:
-        service_name = item['serviceName']
-        for community in item['bgpCommunities']:
-            item_obj = OrderedDict()
-            item_obj['serviceName'] = service_name
-            item_obj['communityValue'] = community['communityValue']
-            item_obj['supportedRegion'] = community['serviceSupportedRegion']
-            transformed.append(item_obj)
-    return transformed
-
-
-def transform_waf_rule_sets_table_output(result):
-    transformed = []
-    for item in result:
-        rule_set_name = item['name']
-        for group in item['ruleGroups']:
-            rule_group_name = group['ruleGroupName']
-            if group['rules']:
-                for rule in group['rules']:
-                    item_obj = OrderedDict()
-                    item_obj['ruleSet'] = rule_set_name
-                    item_obj['ruleGroup'] = rule_group_name
-                    item_obj['ruleId'] = rule['ruleId']
-                    item_obj['description'] = rule['description']
-                    transformed.append(item_obj)
-            else:
-                item_obj = OrderedDict()
-                item_obj['ruleSet'] = rule_set_name
-                item_obj['ruleGroup'] = rule_group_name
-                transformed.append(item_obj)
-    return transformed
-
-
-def transform_network_usage_list(result):
-    result = list(result)
-    for item in result:
-        item.current_value = str(item.current_value)
-        item.limit = str(item.limit)
-        item.local_name = item.name.localized_value
-    return result
 
 
 def transform_network_usage_table(result):
