27f30589704db9ad0fb1da4b0cbfb62eedc2f867
Kirstyn Joy Amperiadis <kamperiadis@microsoft.com>
2023-03-09 11:45:29 -0600

    Update forward link name

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -694,7 +694,7 @@ def validate_zip_deploy_app_setting_exists(cmd, resource_group_name, name, slot=
     if storage_connection is None:
         raise ValidationError(('Unable to automatically deploy using the Azure CLI. Please '
                                'configure the app to deploy from a remote package using the steps here: '
-                               'https://aka.ms/zipdeploy'))
+                               'https://aka.ms/deployfromurl'))
 
 
 def upload_zip_to_storage(cmd, resource_group_name, name, src, slot=None):
