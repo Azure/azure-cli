c45608847a27fb4e5166bad2d49162c4f610f67e
Hang Lei <hanglei@microsoft.com>
2023-03-16 12:56:03 +0800

    Print folder size

--- scripts/use_pyc.py
+++ scripts/use_pyc.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 import logging
+import os
 import re
 import sys
 import glob
@@ -12,8 +13,18 @@ import shutil
 _LOGGER = logging.getLogger(__name__)
 
 
+def get_folder_size(folder):
+    total_size = 0
+    for path, dirs, files in os.walk(folder):
+        for f in files:
+            fp = os.path.join(path, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
 def main(folder, version=None):
     _LOGGER.info(f'Replace .py with .pyc, base folder: {folder}')
+    _LOGGER.info(f'Base folder size: {get_folder_size(folder)}')
     if version is None:
         version = re.search(r'python(\d\.\d+)', folder).group(1)
     else:
@@ -35,6 +46,9 @@ def main(folder, version=None):
 
     for folder in glob.glob(f'{folder}/**/__pycache__', recursive=True):
         shutil.rmtree(folder)
+
+    _LOGGER.info('Finish processing')
+    _LOGGER.info(f'Base folder size: {get_folder_size(folder)}')
 
 
 if __name__ == '__main__':
