931ce6ea07ffae93ec4244e78b6ca4efd833a8d5
ambrahma <61298407+ambrahma@users.noreply.github.com>
2023-03-16 03:45:37 -0700

    Update help text for replica create describing behavior in case zone is not passed

--- src/azure-cli/azure/cli/command_modules/rdbms/_helptext_pg.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_helptext_pg.py
@@ -513,6 +513,11 @@ examples:
         --vnet newVnet --subnet newSubnet \\
         --address-prefixes 172.0.0.0/16 --subnet-prefixes 172.0.0.0/24 \\
         --private-dns-zone testDNS.postgres.database.azure.com
+  - name: Create a read replica 'testReplicaServer' for 'testserver' with public or private access \\
+        in the specified location if available. SInce zone is not passed, it will automatically pick up zone in the \\
+        replica location which is different from source server, if available, else will pick up zone same as source server \\
+        in the replica location if available, else will set the zone as None, i.e. No preference
+    text: az postgres flexible-server replica create --replica-name testReplicaServer -g testGroup --source-server testserver --location testLocation
 """
 
 helps['postgres flexible-server replica list'] = """
