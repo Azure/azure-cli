e696123b6261cfde9b3518b30298344b38bd7e08
Yan Zhu <zhuyan@microsoft.com>
2023-03-21 16:56:25 +0800

    migrate 'vm host' group

--- src/azure-cli/azure/cli/command_modules/vm/commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/commands.py
@@ -460,13 +460,9 @@ def load_command_table(self, _):
 
     with self.command_group('vm host', compute_dedicated_host_sdk, client_factory=cf_dedicated_hosts,
                             min_api='2019-03-01') as g:
-        g.show_command('show', 'get')
         g.custom_command('get-instance-view', 'get_dedicated_host_instance_view')
         g.custom_command('create', 'create_dedicated_host')
-        g.command('list', 'list_by_host_group')
         g.generic_update_command('update', setter_name='begin_create_or_update')
-        g.command('restart', 'begin_restart', min_api='2021-07-01')
-        g.command('delete', 'begin_delete', confirmation=True)
 
     with self.command_group('vm host group', compute_dedicated_host_groups_sdk, client_factory=cf_dedicated_host_groups,
                             min_api='2019-03-01') as g:
