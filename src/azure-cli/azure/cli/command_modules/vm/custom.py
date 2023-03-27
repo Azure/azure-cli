299e1ca6e61cb0e6f8f91b36b7815d85864d73b9
wu5bocheng <wu5bocheng@gmail.com>
2023-03-27 17:57:47 +0800

    Update src/azure-cli/azure/cli/command_modules/vm/custom.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -922,9 +922,9 @@ def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_
 
     # Breaking Change Warning, change image alias
     if image:
-        if image == "Win2008R2SP1":
+        if image == "UbuntuLTS":
             logger.warning(
-                '[Breaking Change] On April 04, 2023, the image deployed using "Win2008R2SP1" will reach its end of life.')
+                'Since on April 04, 2023, the image deployed using "UbuntuLTS" will reach its end of life, we will remove the image alias "UbuntuLTS" in the next Build Event release. So please use the new image alias "Ubuntu2204" instead.')
         if image in ["RHEL", "Debian", "CentOS", "Flatcar"]:
             logger.warning('Consider using the image alias including the version of the distribution you want to use. For example: please use Debian11 instead of Debian')
 
