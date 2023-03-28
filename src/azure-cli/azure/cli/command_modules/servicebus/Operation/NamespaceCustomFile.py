47db62a3700884f4d809a978849420d515e871d2
schaudhari6254888 <111409694+schaudhari6254888@users.noreply.github.com>
2023-03-28 11:18:56 +0530

    {Servicebus} `az servicebus topic subscription rule`: Migrate servicebus rule cmdlets to AAZ (#25888)
    * updates
    
    * Updates2
    
    * rule create
    
    * updates
    
    * Rebasing & Conflict resolved
    
    * Updates
    
    * Updates4

--- src/azure-cli/azure/cli/command_modules/servicebus/Operation/NamespaceCustomFile.py
+++ src/azure-cli/azure/cli/command_modules/servicebus/Operation/NamespaceCustomFile.py
@@ -232,3 +232,46 @@ def cli_remove_identity(cmd, resource_group_name, namespace_name, system_assigne
             "user_assigned_identities": servicebusnm['identity']['userAssignedIdentities']
         })
     return Update(cli_ctx=cmd.cli_ctx)(command_args=command_args)
+
+
+def sb_rule_create(cmd, resource_group_name, namespace_name, topic_name, subscription_name, rule_name,
+                   action_sql_expression=None, action_compatibility_level=None, correlation_id=None,
+                   action_requires_preprocessing=None, reply_to=None, label=None, session_id=None,
+                   filter_sql_expression=None, filter_requires_preprocessing=None, reply_to_session_id=None,
+                   message_id=None, to=None, content_type=None, requires_preprocessing=None,
+                   filter_type=None, tags=None):
+    from azure.cli.command_modules.servicebus.aaz.latest.servicebus.topic.subscription.rule import Create
+    command_arg_dict = {}
+    command_arg_dict.update({
+        'resource_group': resource_group_name,
+        'namespace_name': namespace_name,
+        'topic_name': topic_name,
+        'subscription_name': subscription_name,
+        'rule_name': rule_name,
+        'action_sql_expression': action_sql_expression,
+        'action_compatibility_level': action_compatibility_level,
+        'action_requires_preprocessing': action_requires_preprocessing,
+        'filter_type': filter_type
+    })
+
+    if filter_type == 'SqlFilter' or filter_type is None:
+        command_arg_dict.update({
+            'filter_sql_expression': filter_sql_expression,
+            'filter_requires_preprocessing': filter_requires_preprocessing
+        })
+
+    if filter_type == 'CorrelationFilter':
+        command_arg_dict.update({
+            'properties': tags,
+            'correlation_id': correlation_id,
+            'to': to,
+            'message_id': message_id,
+            'reply_to': reply_to,
+            'correlation_filter_property': tags,
+            'session_id': session_id,
+            'reply_to_session_id': reply_to_session_id,
+            'content_type': content_type,
+            'requires_preprocessing': requires_preprocessing,
+            'label': label
+        })
+    return Create(cli_ctx=cmd.cli_ctx)(command_args=command_arg_dict)
