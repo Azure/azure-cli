ed5b2cefd7cc2ca5e1f877fa64acf9bbbeffce6a
wu5bocheng <wu5bocheng@gmail.com>
2023-03-30 11:06:57 +0800

    Update warning message according to Damien Suggest

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -923,9 +923,9 @@ def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_
     # Breaking Change Warning, change image alias
     if image:
         if image == "UbuntuLTS":
-            logger.warning('Since on April 04, 2023, the image deployed using "UbuntuLTS" will reach its end of life,'
-                           'we will remove the image alias "UbuntuLTS" in the next Build Event release. '
-                           'So please use the new image alias "Ubuntu2204" instead.')
+            logger.warning('Consider using the "Ubuntu2204" alias. On April 30, 2023,'
+                           'the image deployed by the "UbuntuLTS" alias reaches its end of life. '
+                           'The "UbuntuLTS" will be removed with the breaking change release of Fall 2023.')
         if image in ["RHEL", "Debian", "CentOS", "Flatcar"]:
             logger.warning('Consider using the image alias including the version of the distribution you want to use. '
                            'For example: please use Debian11 instead of Debian')
@@ -3110,9 +3110,9 @@ def create_vmss(cmd, vmss_name, resource_group_name, image=None,
     # Breaking Change Warning, change image alias
     if image:
         if image == "UbuntuLTS":
-            logger.warning('Since on April 04, 2023, the image deployed using "UbuntuLTS" will reach its end of life,'
-                           'we will remove the image alias "UbuntuLTS" in the next Build Event release. '
-                           'So please use the new image alias "Ubuntu2204" instead.')
+            logger.warning('Consider using the "Ubuntu2204" alias. On April 30, 2023,'
+                           'the image deployed by the "UbuntuLTS" alias reaches its end of life. '
+                           'The "UbuntuLTS" will be removed with the breaking change release of Fall 2023.')
         if image in ["RHEL", "Debian", "CentOS", "Flatcar"]:
             logger.warning('Consider using the image alias including the version of the distribution you want to use. '
                            'For example: please use Debian11 instead of Debian')
