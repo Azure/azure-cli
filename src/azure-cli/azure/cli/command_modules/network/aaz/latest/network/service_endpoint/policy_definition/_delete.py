0d7037bc9eb0000a40847134638953bda8187ff8
kai ru <69238381+kairu-ms@users.noreply.github.com>
2023-03-30 18:10:35 +0800

    Update src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/service_endpoint/policy_definition/_delete.py

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/service_endpoint/policy_definition/_delete.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/service_endpoint/policy_definition/_delete.py
@@ -13,7 +13,6 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "network service-endpoint policy-definition delete",
-    confirmation="Are you sure you want to perform this operation?",
 )
 class Delete(AAZCommand):
     """Delete a service endpoint policy definition.
