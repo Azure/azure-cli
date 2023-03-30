1528c7f8e2acc60988140522ab30309d1ccf2777
kai ru <69238381+kairu-ms@users.noreply.github.com>
2023-03-30 18:09:15 +0800

    Update src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/route_table/route/_delete.py

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/route_table/route/_delete.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/route_table/route/_delete.py
@@ -13,7 +13,6 @@ from azure.cli.core.aaz import *
 
 @register_command(
     "network route-table route delete",
-    confirmation="Are you sure you want to perform this operation?",
 )
 class Delete(AAZCommand):
     """Delete a route from a route table.
