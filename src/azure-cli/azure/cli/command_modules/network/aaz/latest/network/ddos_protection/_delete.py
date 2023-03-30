26f8aa1ece587cdb92d58b3349e2bdf2c47cc34e
kai ru <69238381+kairu-ms@users.noreply.github.com>
2023-03-30 18:01:40 +0800

    Update src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/ddos_protection/_delete.py

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/ddos_protection/_delete.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/ddos_protection/_delete.py
@@ -12,8 +12,7 @@ from azure.cli.core.aaz import *
 
 
 @register_command(
-    "network ddos-protection delete",
-    confirmation="Are you sure you want to perform this operation?",
+    "network ddos-protection delete"
 )
 class Delete(AAZCommand):
     """Delete a DDoS protection plan.
