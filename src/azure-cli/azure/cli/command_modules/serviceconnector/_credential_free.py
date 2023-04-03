27e12519080f45b49b59113d0f973a66e088fed2
xiaofanzhou <xiaofanzhou@microsoft.com>
2023-04-03 16:04:55 +0800

    lint

--- src/azure-cli/azure/cli/command_modules/serviceconnector/_credential_free.py
+++ src/azure-cli/azure/cli/command_modules/serviceconnector/_credential_free.py
@@ -3,18 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-import struct
-import sys
 from knack.log import get_logger
-from msrestazure.tools import parse_resource_id
-from azure.cli.core.azclierror import (
-    AzureConnectionError,
-    ValidationError,
-    CLIInternalError
-)
-from azure.cli.core.extension.operations import _install_deps_for_psycopg2, _run_pip
-from azure.cli.core._profile import Profile
-from ._utils import run_cli_cmd, generate_random_string, is_packaged_installed, get_object_id_of_current_user
 from ._resource_config import (
     RESOURCE,
     AUTH_TYPE
@@ -38,7 +27,10 @@ AUTHTYPES = {
 # For db(mysqlFlex/psql/psqlFlex/sql) linker with AAD auth type, it's passwordless connection and requires extension installation
 def is_passwordless_command(cmd, auth_info):
     # return if connection is not for db mi
-    if auth_info['auth_type'] not in {AUTHTYPES[AUTH_TYPE.SystemIdentity], AUTHTYPES[AUTH_TYPE.UserAccount], AUTHTYPES[AUTH_TYPE.ServicePrincipalSecret], AUTHTYPES[AUTH_TYPE.UserIdentity]}:
+    if auth_info['auth_type'] not in [AUTHTYPES[AUTH_TYPE.SystemIdentity],
+                                      AUTHTYPES[AUTH_TYPE.UserAccount],
+                                      AUTHTYPES[AUTH_TYPE.ServicePrincipalSecret],
+                                      AUTHTYPES[AUTH_TYPE.UserIdentity]]:
         return False
     source_type = get_source_resource_name(cmd)
     target_type = get_target_resource_name(cmd)
