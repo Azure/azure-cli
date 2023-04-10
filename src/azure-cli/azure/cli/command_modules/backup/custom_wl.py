f871a4037eb23e7e0cfd5deb84753bb9533aa5c1
Zubair Abid <zubairabid@microsoft.com>
2023-04-10 17:10:18 +0530

    removing debug print statements

--- src/azure-cli/azure/cli/command_modules/backup/custom_wl.py
+++ src/azure-cli/azure/cli/command_modules/backup/custom_wl.py
@@ -344,7 +344,6 @@ def list_protectable_items(cmd, client, resource_group_name, vault_name, workloa
     items = client.list(vault_name, resource_group_name, filter_string)
     paged_items = cust_help.get_list_from_paged_response(items)
 
-    print("og: ", paged_items, len(paged_items))
     if protectable_item_type is not None:
         # Protectable Item Type filter
         paged_items = [item for item in paged_items if item.properties.protectable_item_type is not None and
@@ -360,7 +359,6 @@ def list_protectable_items(cmd, client, resource_group_name, vault_name, workloa
     if instance_name:
         # Instance name filter
         paged_items = [item for item in paged_items if item.name.lower() == instance_name.lower()]
-    print("after filtering: ", paged_items, len(paged_items))
 
     _fetch_nodes_list_and_auto_protection_policy(cmd, paged_items, resource_group_name, vault_name)
 
