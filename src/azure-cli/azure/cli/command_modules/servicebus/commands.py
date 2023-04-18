cd439f327de0ce66fc7491bc6e125f9dbe354c2e
schaudhari6254888 <111409694+schaudhari6254888@users.noreply.github.com>
2023-04-18 08:56:57 +0530

    Update commands.py

--- src/azure-cli/azure/cli/command_modules/servicebus/commands.py
+++ src/azure-cli/azure/cli/command_modules/servicebus/commands.py
@@ -81,13 +81,13 @@ def load_command_table(self, _):
         g.custom_command('abort', 'revert')
 
 # NetwrokRuleSet Region
-    with self.command_group('servicebus namespace network-rule', sb_namespace_util, deprecate_info=self.deprecate(redirect='eventhubs namespace network-rule-set'), client_factory=namespaces_mgmt_client_factory, resource_type=ResourceType.MGMT_SERVICEBUS) as g:
-        g.custom_command('add', 'cli_networkrule_createupdate', deprecate_info=self.deprecate(redirect='eventhubs namespace network-rule-set ip-rule/virtual-network-rule add'),
+    with self.command_group('servicebus namespace network-rule', sb_namespace_util, deprecate_info=self.deprecate(redirect='servicebus namespace network-rule-set'), client_factory=namespaces_mgmt_client_factory, resource_type=ResourceType.MGMT_SERVICEBUS) as g:
+        g.custom_command('add', 'cli_networkrule_createupdate', deprecate_info=self.deprecate(redirect='servicebus namespace network-rule-set ip-rule/virtual-network-rule add'),
                          validator=validate_subnet)
-        g.show_command('list', 'get_network_rule_set', deprecate_info=self.deprecate(redirect='eventhubs namespace network-rule-set list'))
-        g.custom_command('remove', 'cli_networkrule_delete', deprecate_info=self.deprecate(redirect='eventhubs namespace network-rule-set ip-rule/virtual-network-rule remove'),
+        g.show_command('list', 'get_network_rule_set', deprecate_info=self.deprecate(redirect='servicebus namespace network-rule-set list'))
+        g.custom_command('remove', 'cli_networkrule_delete', deprecate_info=self.deprecate(redirect='servicebus namespace network-rule-set ip-rule/virtual-network-rule remove'),
                          validator=validate_subnet)
-        g.custom_command('update', 'cli_networkrule_update', deprecate_info=self.deprecate(redirect='eventhubs namespace network-rule-set update'))
+        g.custom_command('update', 'cli_networkrule_update', deprecate_info=self.deprecate(redirect='servicebus namespace network-rule-set update'))
 
 # Identity Region
     with self.command_group('servicebus namespace identity', custom_command_type=sb_namespace_custom, is_preview=True) as g:
