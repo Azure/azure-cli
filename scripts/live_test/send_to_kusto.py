e111b2600a8be6157ed92783a1391bbe5c8dfbd8
ZelinWang <zelinwang@microsoft.com>
2023-04-18 14:40:10 +0800

    Update send_to_kusto.py

--- scripts/live_test/send_to_kusto.py
+++ scripts/live_test/send_to_kusto.py
@@ -13,11 +13,10 @@ from bs4 import BeautifulSoup
 import csv
 import datetime
 import logging
+import os
 import sys
 
-
 logger = logging.getLogger(__name__)
-
 
 # # authenticate with AAD application.
 KUSTO_CLUSTER = sys.argv[1]
@@ -32,44 +31,57 @@ BUILDID = sys.argv[8]
 
 
 def generate_csv_file():
+    logger.warning('Start generate csv file for {TARGET}.'.format(TARGET=TARGET))
     data = []
-    with open(f'/mnt/vss/_work/1/{TARGET}.report.parallel.html') as file:
-        bs = BeautifulSoup(file, "html.parser")
-        environment = bs.find(id="environment")
-        PythonVersion = environment.find(string="Python").findNext('td').text
-        results = bs.find(id="results-table")
-        Source = 'LiveTest'
-        BuildId = BUILDID
-        OSVersion = 'Ubuntu20.04'
-        Module = TARGET
-        Description = ''
-        ExtendedProperties = ''
-        for result in results.find_all('tbody'):
-            Name = result.find('td', {'class': 'col-name'}).text
-            Duration = result.find('td', {'class': 'col-duration'}).text
-            IsSuccess = result.find('td', {'class': 'col-result'}).text
-            if IsSuccess == 'Failed':
-                contents = result.find('td', {'class': 'extra'}).find('div', {'class': 'log'}).contents
-                Errors = ''
-                for content in contents:
-                    if content.name == 'br':
-                        Errors += '\n'
-                    if not content.name:
-                        Errors += content
+    parallel_file = f'/mnt/vss/_work/1/{TARGET}.report.parallel.html'
+    sequential_file = f'/mnt/vss/_work/1/{TARGET}.report.sequential.html'
+
+    def _get_data(html_file):
+        data = []
+        if os.path.exists(html_file):
+            with open(html_file) as file:
+                bs = BeautifulSoup(file, "html.parser")
+                environment = bs.find(id="environment")
+                PythonVersion = environment.find(string="Python").findNext('td').text
+                results = bs.find(id="results-table")
+                Source = 'LiveTest'
+                BuildId = BUILDID
+                OSVersion = 'Ubuntu20.04'
+                Module = TARGET
+                Description = ''
+                ExtendedProperties = ''
+                for result in results.find_all('tbody'):
+                    Name = result.find('td', {'class': 'col-name'}).text.split('::')[2]
+                    Duration = result.find('td', {'class': 'col-duration'}).text
+                    IsSuccess = result.find('td', {'class': 'col-result'}).text
+                    if IsSuccess == 'Failed':
+                        contents = result.find('td', {'class': 'extra'}).find('div', {'class': 'log'}).contents
+                        Errors = ''
+                        for content in contents:
+                            if content.name == 'br':
+                                Errors += '\n'
+                            if not content.name:
+                                Errors += content
+                            else:
+                                logger.warning(content.name)
                     else:
-                        logger.info(content.name)
-            else:
-                Errors = ''
-            EndDateTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            StartDateTime = (datetime.datetime.now() - datetime.timedelta(seconds=int(float(Duration)))).strftime("%Y-%m-%d %H:%M:%S")
-            data.append([Source, BuildId, OSVersion, PythonVersion, Module, Name, Description, StartDateTime, EndDateTime, IsSuccess, Errors, ExtendedProperties])
+                        Errors = ''
+                    EndDateTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    StartDateTime = (datetime.datetime.now() - datetime.timedelta(seconds=int(float(Duration)))).strftime("%Y-%m-%d %H:%M:%S")
+                    data.append([Source, BuildId, OSVersion, PythonVersion, Module, Name, Description, StartDateTime, EndDateTime, IsSuccess, Errors, ExtendedProperties])
+        return data
+
+    data.extend(_get_data(parallel_file))
+    data.extend(_get_data(sequential_file))
         
     with open(f'/mnt/vss/_work/1/{TARGET}.csv', mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(data)
+    logger.warning('Finish generate csv file for {TARGET}.'.format(TARGET=TARGET))
 
 
 def send_to_kusto():
+    logger.warning('Start send csv data to kusto db for {TARGET}'.format(TARGET=TARGET))
     kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(KUSTO_CLUSTER, KUSTO_CLIENT_ID, KUSTO_CLIENT_SECRET, KUSTO_TENANT_ID)
     # The authentication method will be taken from the chosen KustoConnectionStringBuilder.
     client = QueuedIngestClient(kcsb)
@@ -85,7 +97,8 @@ def send_to_kusto():
     # ingest from file
     result = client.ingest_from_file(f"/mnt/vss/_work/1/{TARGET}.csv", ingestion_properties=ingestion_props)
     # Inspect the result for useful information, such as source_id and blob_url
-    logger.info(repr(result))
+    print(f'Finsh send csv data to kusto db for {TARGET}.')
+    print(repr(result))
 
 
 if __name__ == '__main__':
