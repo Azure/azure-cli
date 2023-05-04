5c3d6320f137f459911d70f3ba5a4d71ffa8a19d
Tanmay Satam <tsatam@redhat.com>
2023-05-04 16:44:07 -0400

    Update az aro --pull-secret help example output
    This is to simplify the help output for az aro create --help. An @ symbol is no longer required before the pull secret file.

--- src/azure-cli/azure/cli/command_modules/aro/_help.py
+++ src/azure-cli/azure/cli/command_modules/aro/_help.py
@@ -20,7 +20,7 @@ helps['aro create'] = """
   - name: Create a cluster with a supported OpenShift version.
     text: az aro create --resource-group MyResourceGroup --name MyCluster --vnet MyVnet --master-subnet MyMasterSubnet --worker-subnet MyWorkerSubnet --version X.Y.Z
   - name: Create a cluster with 5 compute nodes and Red Hat pull secret.
-    text: az aro create --resource-group MyResourceGroup --name MyCluster --vnet MyVnet --master-subnet MyMasterSubnet --worker-subnet MyWorkerSubnet --worker-count 5 --pull-secret @pullsecret.txt
+    text: az aro create --resource-group MyResourceGroup --name MyCluster --vnet MyVnet --master-subnet MyMasterSubnet --worker-subnet MyWorkerSubnet --worker-count 5 --pull-secret pullsecret.txt
   - name: Create a private cluster.
     text: az aro create --resource-group MyResourceGroup --name MyCluster --vnet MyVnet --master-subnet MyMasterSubnet --worker-subnet MyWorkerSubnet --apiserver-visibility Private --ingress-visibility Private
 """
