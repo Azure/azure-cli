2123dc204c50b6958dc766e332fd517419862fa5
Harsh Patel <harshpatel@microsoft.com>
2023-05-10 17:01:05 -0400

    Addressed location in params.py comments

--- src/azure-cli/azure/cli/command_modules/resource/_params.py
+++ src/azure-cli/azure/cli/command_modules/resource/_params.py
@@ -106,7 +106,7 @@ def load_arguments(self, _):
     stacks_delete_resources_type = CLIArgumentType(options_list=['--delete-resources'], help='Flag to indicate delete rather than detach for the resources.')
     stacks_delete_resource_groups_type = CLIArgumentType(options_list=['--delete-resource-groups'], help='Flag to indicate delete rather than detach for the resource groups.')
     stacks_delete_all_type = CLIArgumentType(options_list=['--delete-all'], help='Flag to indicate delete rather than detach for the resources and resource groups.')
-    stacks_deny_settings_mode = CLIArgumentType(help='Defines which operations are denied on resources managed by the stack: denyWrite or denyWriteAndDelete.')
+    stacks_deny_settings_mode = CLIArgumentType(help='Define which operations are denied on resources managed by the stack: denyWrite or denyWriteAndDelete.')
     stacks_excluded_principals = CLIArgumentType(help='List of AAD principal IDs excluded from the lock. Up to 5 principals are permitted.')
     stacks_excluded_actions = CLIArgumentType(help="List of role-based management operations that are excluded from the denySettings. Up to 200 actions are permitted.")
     stacks_apply_to_child_scopes = CLIArgumentType(help='DenySettings will be applied to child scopes.')
@@ -683,7 +683,7 @@ def load_arguments(self, _):
     
     with self.argument_context('stack mg create') as c:
         c.argument('name', options_list=['--name', '-n'], arg_type=stacks_name_type)
-        c.argument('location', options_list=['--location', '-l'], help='The location to store deployment stack.')
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), help='The location to store deployment stack.')
         c.argument('template_file', arg_type=deployment_template_file_type)
         c.argument('template_spec', arg_type=deployment_template_spec_type)
         c.argument('template_uri', arg_type=deployment_template_uri_type)
@@ -719,7 +719,7 @@ def load_arguments(self, _):
     with self.argument_context('stack sub create') as c:
         c.argument('name', options_list=['--name', '-n'], arg_type=stacks_name_type)
         c.argument('deployment_resource_group', arg_type=stacks_stack_deployment_resource_group)
-        c.argument('location', options_list=['--location', '-l'], help='The location to store deployment stack.')
+        c.argument('location', arg_type=get_location_type(self.cli_ctx), help='The location to store deployment stack.')
         c.argument('template_file', arg_type=deployment_template_file_type)
         c.argument('template_spec', arg_type=deployment_template_spec_type)
         c.argument('template_uri', arg_type=deployment_template_uri_type)
