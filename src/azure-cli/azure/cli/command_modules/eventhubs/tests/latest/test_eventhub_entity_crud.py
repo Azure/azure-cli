c7d7bcbed0f6812d7924c36d387b380c3db600b5
schaudhari6254888 <111409694+schaudhari6254888@users.noreply.github.com>
2023-05-17 09:02:33 +0530

    {EventHub} `az eventhubs eventhub create/delete/update/show/list`: Migrating Eventhub Entity cmdlets to AAZ (#26168)
    * UPdates
    
    * UPdates
    
    * External service recordings
    
    * external test failures
    
    * Update custom.py
    
    * downgrading api version for python sdk
    
    * recordings
    
    * recordings
    
    * iot hub recording
    
    * pipeline fix
    
    * fix
    
    * fix
    
    * Updates
    
    * fixes
    
    * Updates
    
    * UPdates
    
    * Revert "UPdates"
    
    This reverts commit db1bc1fa8026bffb981d7ca046a5ce7748a1f46f.
    
    * UPdates
    
    * CI's Againnnnnnnnnn
    
    * fixing cli style error
    
    ---------
    
    Co-authored-by: unknown <davadhani@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/eventhubs/tests/latest/test_eventhub_entity_crud.py
+++ src/azure-cli/azure/cli/command_modules/eventhubs/tests/latest/test_eventhub_entity_crud.py
@@ -24,11 +24,13 @@ class EHNamespaceEntityCURDScenarioTest(ScenarioTest):
             'loc': 'westus2',
             'rg': resource_group,
             'namespacename': self.create_random_name(prefix='eventhubs-nscli', length=20),
+            'namespacename1': self.create_random_name(prefix='eventhubs-nscli', length=20),
             'tags': {'tag1: value1', 'tag2: value2'},
             'sku': 'Standard',
             'tier': 'Standard',
             'eventhubname1': self.create_random_name(prefix='eventhubs-eventhubcli', length=25),
             'eventhubname2': self.create_random_name(prefix='eventhubs-eventhubcli', length=25),
+            'eventhubname3': self.create_random_name(prefix='eventhubs-eventhubcli', length=25),
             'isautoinflateenabled': 'True',
             'maximumthroughputunits': 4,
             'messageretentionindays': 4,
@@ -48,11 +50,11 @@ class EHNamespaceEntityCURDScenarioTest(ScenarioTest):
 
         self.cmd('eventhubs namespace create --resource-group {rg} --name {namespacename} --location {loc} --tags {tags} --sku {sku} --enable-auto-inflate {isautoinflateenabled} --maximum-throughput-units {maximumthroughputunits}')
 
-        eh1 = self.cmd('eventhubs eventhub create -g {rg} -n {eventhubname1} --namespace-name {namespacename} --message-retention 3 --partition-count 15 --enable-capture --capture-interval 100 --capture-size-limit 314572799 '
-                       '--destination-name {destinationname} --storage-account {storageid} --blob-container {containername} --archive-name-format {archinvenameformat}').get_output_in_json()
+        eh1 = self.cmd('eventhubs eventhub create -g {rg} -n {eventhubname1} --namespace-name {namespacename} --retention-time-in-hours 24 --partition-count 15 --enable-capture true --capture-interval 100 --capture-size-limit 314572799 '
+                       '--destination-name {destinationname} --storage-account {storageid} --blob-container {containername} --archive-name-format {archinvenameformat} --cleanup-policy Delete').get_output_in_json()
 
         self.assertEqual(eh1['name'], self.kwargs['eventhubname1'])
-        self.assertEqual(eh1['messageRetentionInDays'], 3)
+        self.assertEqual(eh1['retentionDescription']['retentionTimeInHours'], 24)
         self.assertEqual(eh1['partitionCount'], 15)
         self.assertEqual(eh1['captureDescription']['enabled'], True)
         self.assertEqual(eh1['captureDescription']['intervalInSeconds'], 100)
@@ -66,7 +68,7 @@ class EHNamespaceEntityCURDScenarioTest(ScenarioTest):
             'eventhubs eventhub update -g {rg} -n {eventhubname1} --namespace-name {namespacename} --enable-capture false').get_output_in_json()
 
         self.assertEqual(eh1['name'], self.kwargs['eventhubname1'])
-        self.assertEqual(eh1['messageRetentionInDays'], 3)
+        self.assertEqual(eh1['messageRetentionInDays'], 1)
         self.assertEqual(eh1['partitionCount'], 15)
         self.assertEqual(eh1['captureDescription']['enabled'], False)
         self.assertEqual(eh1['captureDescription']['intervalInSeconds'], 100)
@@ -78,18 +80,16 @@ class EHNamespaceEntityCURDScenarioTest(ScenarioTest):
         self.assertEqual(eh1['captureDescription']['destination']['storageAccountResourceId'], self.kwargs['storageid'])
 
         eh2 = self.cmd(
-            'eventhubs eventhub create -g {rg} -n {eventhubname2} --namespace-name {namespacename} --message-retention 3 --partition-count 15').get_output_in_json()
+            'eventhubs eventhub create -g {rg} -n {eventhubname2} --namespace-name {namespacename} --partition-count 15').get_output_in_json()
 
         self.assertEqual(eh2['name'], self.kwargs['eventhubname2'])
-        self.assertEqual(eh2['messageRetentionInDays'], 3)
         self.assertEqual(eh2['partitionCount'], 15)
 
         eh2 = self.cmd(
-            'eventhubs eventhub update -g {rg} -n {eventhubname2} --namespace-name {namespacename} --message-retention 3 --enable-capture --capture-interval 100 --capture-size-limit 314572799 '
+            'eventhubs eventhub update -g {rg} -n {eventhubname2} --namespace-name {namespacename} --enable-capture --capture-interval 100 --capture-size-limit 314572799 '
             '--destination-name {destinationname} --storage-account {storageid} --blob-container {containername} --archive-name-format {archinvenameformat}').get_output_in_json()
 
         self.assertEqual(eh2['name'], self.kwargs['eventhubname2'])
-        self.assertEqual(eh2['messageRetentionInDays'], 3)
         self.assertEqual(eh2['partitionCount'], 15)
         self.assertEqual(eh2['captureDescription']['enabled'], True)
         self.assertEqual(eh2['captureDescription']['intervalInSeconds'], 100)
@@ -99,3 +99,15 @@ class EHNamespaceEntityCURDScenarioTest(ScenarioTest):
         self.assertEqual(eh2['captureDescription']['destination']['blobContainer'], self.kwargs['containername'])
         self.assertEqual(eh2['captureDescription']['destination']['name'], self.kwargs['destinationname'])
         self.assertEqual(eh2['captureDescription']['destination']['storageAccountResourceId'], self.kwargs['storageid'])
+
+        self.cmd(
+            'eventhubs namespace create --resource-group {rg} --name {namespacename1} --location {loc} --tags {tags} --sku Premium ')
+        eh3 = self.cmd(
+            'eventhubs eventhub create -g {rg} -n {eventhubname3} --namespace-name {namespacename1} --cleanup-policy Delete --retention-time 7 ').get_output_in_json()
+
+        self.assertEqual(eh3['name'], self.kwargs['eventhubname3'])
+        self.assertEqual(eh3['retentionDescription']['cleanupPolicy'], "Delete")
+        self.assertEqual(eh3['retentionDescription']['retentionTimeInHours'], 7)
+
+        self.cmd('eventhubs namespace delete --resource-group {rg} --name {namespacename}')
+        self.cmd('eventhubs namespace delete --resource-group {rg} --name {namespacename1}')
