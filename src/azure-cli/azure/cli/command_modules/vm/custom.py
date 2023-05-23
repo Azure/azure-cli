36c773e408af1b5d038cd5a8c6d8d1060c7dd56c
wu5bocheng <wu5bocheng@gmail.com>
2023-05-23 18:26:34 +0800

    Update src/azure-cli/azure/cli/command_modules/vm/custom.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -928,7 +928,7 @@ def create_vm(cmd, vm_name, resource_group_name, image=None, size='Standard_DS1_
         if image in ["RHEL", "Debian", "CentOS", "Flatcar"]:
             logger.warning('Consider using the image alias including the version of the distribution you want to use. '
                            'For example: please use Debian11 instead of Debian. In Ignite (November) 2023, '
-                           'the aliases `UbuntuLTS`, `CentOS`, `Debian`, `Flatcar`, and `RHEL` will be removed.')
+                           'the aliases without version suffix (such as: `UbuntuLTS`, `CentOS`, `Debian`, `Flatcar`, and `RHEL`) will be removed.')
 
     subscription_id = get_subscription_id(cmd.cli_ctx)
     if os_disk_encryption_set is not None and not is_valid_resource_id(os_disk_encryption_set):
