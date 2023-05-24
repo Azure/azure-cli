fa7b08f708c60ce17977f854af8db042dbb0f32f
Victoria Litvinova <vilit@microsoft.com>
2023-05-24 15:57:46 -0700

    deprecation message

--- src/azure-cli/azure/cli/command_modules/iot/commands.py
+++ src/azure-cli/azure/cli/command_modules/iot/commands.py
@@ -10,7 +10,8 @@ from azure.cli.command_modules.iot._client_factory import (iot_hub_service_facto
 from azure.cli.command_modules.iot._utils import _dps_certificate_response_transform
 
 CS_DEPRECATION_INFO = 'IoT Extension (azure-iot) connection-string command (az iot hub connection-string show)'
-
+ROUTE_DEPRECATION_INFO = 'IoT Extension (azure-iot) message-route command group (az iot hub message-route)'
+ENDPOINT_DEPRECATION_INFO = 'IoT Extension (azure-iot) message-endpoint command group (az iot hub message-endpoint)'
 
 class PolicyUpdateResultTransform(LongRunningOperation):  # pylint: disable=too-few-public-methods
     def __call__(self, poller):
@@ -162,7 +163,8 @@ def load_command_table(self, _):  # pylint: disable=too-many-statements
         g.custom_command('renew-key', 'iot_hub_policy_key_renew', supports_no_wait=True)
 
     # iot hub routing endpoint commands
-    with self.command_group('iot hub routing-endpoint', client_factory=iot_hub_service_factory) as g:
+    with self.command_group('iot hub routing-endpoint', client_factory=iot_hub_service_factory,
+                            deprecate_info=self.deprecate(redirect=ENDPOINT_DEPRECATION_INFO)) as g:
         g.custom_command('create', 'iot_hub_routing_endpoint_create',
                          transform=EndpointUpdateResultTransform(self.cli_ctx))
         g.custom_show_command('show', 'iot_hub_routing_endpoint_show')
@@ -179,7 +181,8 @@ def load_command_table(self, _):  # pylint: disable=too-many-statements
         g.custom_command('update', 'iot_message_enrichment_update')
 
     # iot hub route commands
-    with self.command_group('iot hub route', client_factory=iot_hub_service_factory) as g:
+    with self.command_group('iot hub route', client_factory=iot_hub_service_factory,
+                            deprecate_info=self.deprecate(redirect=ROUTE_DEPRECATION_INFO)) as g:
         g.custom_command('create', 'iot_hub_route_create', transform=RouteUpdateResultTransform(self.cli_ctx))
         g.custom_show_command('show', 'iot_hub_route_show')
         g.custom_command('list', 'iot_hub_route_list')
