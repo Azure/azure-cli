0cef5562f3d8dea3f7979628088aa3aded278295
yishiwang@microsoft.com <yishiwang@microsoft.com>
2023-05-30 17:41:28 +0800

    remove cross sub support

--- src/azure-cli/azure/cli/command_modules/keyvault/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/keyvault/_client_factory.py
@@ -289,7 +289,7 @@ def data_plane_azure_keyvault_secret_client(cli_ctx, command_args):
 def _prepare_data_plane_azure_keyvault_client(cli_ctx, command_args, resource_type):
     version = str(get_api_version(cli_ctx, resource_type))
     profile = Profile(cli_ctx=cli_ctx)
-    credential, _, _ = profile.get_login_credentials(subscription_id=cli_ctx.data.get('subscription_id'))
+    credential, _, _ = profile.get_login_credentials()
     vault_url = \
         command_args.get('hsm_name', None) or \
         command_args.get('vault_base_url', None) or \
