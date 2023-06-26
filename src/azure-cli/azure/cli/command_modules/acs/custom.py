e0f057ca9e0d479bdfce0201939563a64bc3f610
anushkasingh16 <anushkasingh@microsoft.com>
2023-06-26 15:47:54 -0700

    added pylint: disable=line-too-long

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=line-too-long
+
 import base64
 import errno
 import io
--- src/azure-cli/azure/cli/command_modules/acs/maintenanceconfiguration.py
+++ src/azure-cli/azure/cli/command_modules/acs/maintenanceconfiguration.py
@@ -1,8 +1,11 @@
-# pylint: disable=too-many-lines
+
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+
+# pylint: disable=too-many-lines
+# pylint: disable=line-too-long
 
 from knack.log import get_logger
 from azure.cli.core.util import get_file_json
