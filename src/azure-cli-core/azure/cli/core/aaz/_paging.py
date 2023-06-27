9d5239e4cb7cff78d6703c272633734e2d2980e3
necusjz <necusjz@gmail.com>
2023-06-27 17:54:40 +0800

    refine comments

--- src/azure-cli-core/azure/cli/core/aaz/_paging.py
+++ src/azure-cli-core/azure/cli/core/aaz/_paging.py
@@ -6,8 +6,6 @@ import base64
 import itertools
 import json
 
-from azure.cli.core.aaz import AAZUndefined, has_value
-
 
 class AAZPageIterator:  # pylint: disable=too-many-instance-attributes
     def __init__(self, executor, extract_result, cli_ctx, next_link, offset, page_size):
@@ -16,10 +14,10 @@ class AAZPageIterator:  # pylint: disable=too-many-instance-attributes
         self._cli_ctx = cli_ctx
         self._next_link = next_link
         self._did_once_already = bool(next_link)  # desired start
-        self._total = page_size + offset if has_value(page_size) else None
+        self._total = page_size + offset if isinstance(page_size, int) else None
         self._start = offset
         self._curr_link = None
-        self._page_size = None
+        self._curr_size = None
 
     def __iter__(self):
         return self
@@ -28,12 +26,13 @@ class AAZPageIterator:  # pylint: disable=too-many-instance-attributes
         def next_token(_, result):  # pylint: disable=unused-argument
             from knack.log import get_logger
             logger = get_logger(__name__)
+
+            token = {"next_link": self._curr_link, "offset": self._curr_size + self._total}
+            token = json.dumps(token).encode("utf-8")
             logger.warning("Token of next page: %s", base64.b64encode(token).decode("utf-8"))
 
         if self._total is not None and self._total < 0:
             from knack.events import EVENT_CLI_SUCCESSFUL_EXECUTE
-            token = {"next_link": self._curr_link, "offset": self._page_size + self._total}
-            token = json.dumps(token).encode("utf-8")
             self._cli_ctx.register_event(EVENT_CLI_SUCCESSFUL_EXECUTE, next_token)
 
             raise StopIteration
@@ -51,8 +50,8 @@ class AAZPageIterator:  # pylint: disable=too-many-instance-attributes
 
         start = self._start  # record 1st time only
         self._start = 0
-        self._page_size = len(curr_page)
-        self._total -= self._page_size
+        self._curr_size = len(curr_page)
+        self._total -= self._curr_size
 
         if self._total < 0:
             return iter(curr_page[start:self._total])  # smaller than current page
@@ -61,8 +60,8 @@ class AAZPageIterator:  # pylint: disable=too-many-instance-attributes
 
 
 class AAZPaged:
-    def __init__(self, executor, extract_result, cli_ctx, next_token=AAZUndefined, page_size=AAZUndefined):
-        if has_value(page_size) and has_value(next_token):
+    def __init__(self, executor, extract_result, cli_ctx, next_token=None, page_size=None):
+        if isinstance(page_size, int) and isinstance(next_token, str):
             next_token = json.loads(next_token)
         else:
             next_token = {"next_link": None, "offset": 0}  # default value
--- src/azure-cli-core/azure/cli/core/tests/test_aaz_paging.py
+++ src/azure-cli-core/azure/cli/core/tests/test_aaz_paging.py
@@ -5,7 +5,7 @@
 
 import unittest
 from azure.cli.core.aaz import AAZUndefined
-from azure.cli.core.aaz._paging import AAZPaged
+from azure.cli.core.aaz._paging import AAZPaged, AAZPageIterator
 from azure.cli.core.mock import DummyCli
 
 
@@ -31,7 +31,7 @@ class TestAAZPaging(unittest.TestCase):
         paged = AAZPaged(executor=executor, extract_result=extract_result, cli_ctx=DummyCli())
         self.assertTrue(list(paged) == ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
 
-    def test_aaz_paging_with_token_and_limit(self):
+    def test_aaz_paging_with_limit_and_token(self):
         data_by_pages = [
             (["a", "b", "c"], 1),
             (["d", "e"], 2),
@@ -54,13 +54,57 @@ class TestAAZPaging(unittest.TestCase):
             return result["value"], result["next_link"]
 
         next_token = '{"next_link": 1, "offset": 1}'
-        paged = AAZPaged(
-            executor=executor, extract_result=extract_result, cli_ctx=DummyCli(), next_token=next_token, page_size=3
-        )
-        self.assertTrue(list(paged) == ["e", "f", "g"])
 
-        next_token = '{"next_link": 2, "offset": 1}'
         paged = AAZPaged(
-            executor=executor, extract_result=extract_result, cli_ctx=DummyCli(), next_token=next_token, page_size=4
+            executor=executor, extract_result=extract_result, cli_ctx=DummyCli(),
+            next_token=next_token, page_size=4
         )
-        self.assertTrue(list(paged) == ["g", "h"])
+        self.assertTrue(list(paged) == ["e", "f", "g", "h"])
+
+    def test_aaz_paging_iterator(self):
+        data_by_pages = [
+            (["a", "b", "c"], 1),
+            (["d", "e"], 2),
+            (["f"], 3),
+            (["g", "h"], AAZUndefined)
+        ]
+        result = {
+            "value": AAZUndefined,
+            "next_link": AAZUndefined
+        }
+
+        def executor(next_link):
+            if next_link is None:
+                next_link = 0
+            value, next_link = data_by_pages[next_link]
+            result["value"] = value
+            result["next_link"] = next_link
+
+        def extract_result():
+            return result["value"], result["next_link"]
+
+        page_iterator = AAZPageIterator(
+            executor=executor, extract_result=extract_result, cli_ctx=DummyCli(),
+            next_link=1, offset=1, page_size=4
+        )
+
+        # | a b c | d e | f | g h |
+        #           *
+        self.assertTrue(page_iterator._next_link == 1)
+        self.assertTrue(page_iterator._start == 1)  # offset
+        self.assertTrue(page_iterator._total == 5)
+        # | a b c | d e | f | g h |
+        #                 *
+        next(page_iterator)
+        self.assertTrue(page_iterator._next_link == 2)
+        self.assertTrue(page_iterator._total == 3)
+        # | a b c | d e | f | g h |
+        #                     *
+        next(page_iterator)
+        self.assertTrue(page_iterator._next_link == 3)
+        self.assertTrue(page_iterator._total == 2)
+        # | a b c | d e | f | g h |
+        #                          *
+        next(page_iterator)
+        self.assertTrue(page_iterator._next_link == AAZUndefined)
+        self.assertTrue(page_iterator._total == 0)
