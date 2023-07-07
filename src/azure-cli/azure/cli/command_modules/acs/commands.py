8235008b371d378685878ae6ec175ca18d6bf2b5
Chris Lopez <91150577+pineapplethevoyager@users.noreply.github.com>
2023-07-06 19:38:08 -0700

    [AKS] `az aks nodepool snapshot update`: Add aks nodepool snapshot update command (#26790)

--- src/azure-cli/azure/cli/command_modules/acs/commands.py
+++ src/azure-cli/azure/cli/command_modules/acs/commands.py
@@ -131,6 +131,7 @@ def load_command_table(self, _):
                               table_transformer=aks_show_nodepool_snapshot_table_format)
         g.custom_command('create', 'aks_nodepool_snapshot_create', supports_no_wait=True)
         g.custom_command('delete', 'aks_nodepool_snapshot_delete', supports_no_wait=True)
+        g.custom_command('update', 'aks_nodepool_snapshot_update')
         g.wait_command('wait')
 
     with self.command_group('aks oidc-issuer', managed_clusters_sdk, client_factory=cf_managed_clusters) as g:
