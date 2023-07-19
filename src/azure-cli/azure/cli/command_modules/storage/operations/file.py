35d357e1641cc38b1587494abea750d38d1c6b23
Jay Patel <jay.patel@uipath.com>
2023-07-19 14:17:49 -0400

    Improve performance for file upload batch operation

--- src/azure-cli/azure/cli/command_modules/storage/operations/file.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/file.py
@@ -188,8 +188,6 @@ def storage_file_upload_batch(cmd, client, destination, source, destination_path
             res.append({'File': file, 'Type': guessed_type})
         return res
 
-    # TODO: Performance improvement
-    # 1. Upload files in parallel
     def _upload_action(src, dst2):
         dst2 = normalize_blob_file_path(destination_path, dst2)
         dir_name = os.path.dirname(dst2)
@@ -207,7 +205,17 @@ def storage_file_upload_batch(cmd, client, destination, source, destination_path
         }
         return create_file_url(client, **args)
 
-    return list(_upload_action(src, dst) for src, dst in source_files)
+    # 1. Upload files in parallel
+    # 2. Return the list of uploaded files
+    from concurrent.futures import ThreadPoolExecutor, as_completed
+
+    with ThreadPoolExecutor(max_workers=max_connections) as executor:
+        futures = []
+        for src, dst in source_files:
+            futures.append(executor.submit(_upload_action, src, dst))
+
+        return list(f.result() for f in as_completed(futures))
+
 
 
 def download_file(client, destination_path=None, timeout=None, max_connections=2, open_mode='wb', **kwargs):
