ca80145938650eb960465cbe5b04fc3dba764fe5
schaudhari6254888 <111409694+schaudhari6254888@users.noreply.github.com>
2023-07-24 12:11:52 +0530

    {Eventhub} API-Version for all `az eventhubs` cmdlets updated to 2023-01-01-preview (#26930)
    * [Eventhub] API-Version for all cmdlets updated to 2023-01-01-preview
    
    * Recording
    
    * Updates
    
    * updates
    
    * updates
    
    * Updates
    
    * Udasdsaf
    
    * UPdates recording
    
    * updates Revert API for 2019-03-01 profile
    
    * Update __cmd_group.py
    
    * Updates
    
    * UPdates
    
    * UPdates
    
    * UPdates
    
    * updates
    
    * updates
    
    * deleted the loa1.pdf

--- src/azure-cli/azure/cli/command_modules/eventhubs/aaz/latest/eventhubs/eventhub/authorization_rule/_show.py
+++ src/azure-cli/azure/cli/command_modules/eventhubs/aaz/latest/eventhubs/eventhub/authorization_rule/_show.py
@@ -22,9 +22,9 @@ class Show(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2022-01-01-preview",
+        "version": "2023-01-01-preview",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.eventhub/namespaces/{}/eventhubs/{}/authorizationrules/{}", "2022-01-01-preview"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.eventhub/namespaces/{}/eventhubs/{}/authorizationrules/{}", "2023-01-01-preview"],
         ]
     }
 
@@ -151,7 +151,7 @@ class Show(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2022-01-01-preview",
+                    "api-version", "2023-01-01-preview",
                     required=True,
                 ),
             }
