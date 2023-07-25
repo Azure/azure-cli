681f424e34e0bc6a7c2a1082c50e3c5b5be0c57f
necusjz <necusjz@gmail.com>
2023-07-25 14:30:16 +0800

    {Network} `az network private-dns link vnet`: Migrate commands to codegen (#26976)

--- src/azure-cli/azure/cli/command_modules/privatedns/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/privatedns/_client_factory.py
@@ -10,12 +10,6 @@ def cf_privatedns_mgmt_zones(cli_ctx, _):
     return get_mgmt_service_client(cli_ctx, PrivateDnsManagementClient).private_zones
 
 
-def cf_privatedns_mgmt_virtual_network_links(cli_ctx, _):
-    from azure.cli.core.commands.client_factory import get_mgmt_service_client
-    from azure.mgmt.privatedns import PrivateDnsManagementClient
-    return get_mgmt_service_client(cli_ctx, PrivateDnsManagementClient).virtual_network_links
-
-
 def cf_privatedns_mgmt_record_sets(cli_ctx, _):
     from azure.cli.core.commands.client_factory import get_mgmt_service_client
     from azure.mgmt.privatedns import PrivateDnsManagementClient
