6f2b4bbcb60e3edeec08355a6c8a2bd55230dda2
ZelinWang <zelinwang@microsoft.com>
2023-07-28 18:05:20 +0800

    Update codegen_report.py

--- scripts/ci/codegen_report.py
+++ scripts/ci/codegen_report.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from azure.kusto.data import KustoConnectionStringBuilder
 from azure.kusto.data.data_format import DataFormat
 from azure.kusto.ingest import (
