47dae703c37868ced7d379bfe89656ce8aa919b8
zhifanhui <zhifanhui@microsoft.com>
2023-08-02 20:42:30 -0700

    remove comment

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_aks_commands.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_aks_commands.py
@@ -8695,7 +8695,6 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             self.is_empty(),
         ])
 
-    # @live_only()
     @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2')
     def test_aks_create_with_vpa(self, resource_group, resource_group_location):
@@ -8722,7 +8721,6 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             self.is_empty(),
         ])
 
-    # @live_only()
     @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='westus2')
     def test_aks_update_with_vpa(self, resource_group, resource_group_location):
