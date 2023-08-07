c324648a50daba2f4598cf9b7da39afbe0baffcc
ZelinWang <zelinwang@microsoft.com>
2023-08-07 15:20:04 +0800

    Update automation_full_test.py

--- scripts/ci/automation_full_test.py
+++ scripts/ci/automation_full_test.py
@@ -577,7 +577,7 @@ class AutomaticScheduling(object):
             serial_tests.append('cloud')
         pipeline_result = build_pipeline_result() if enable_pipeline_result else None
         pytest_args = '-o junit_family=xunit1 --durations=10'
-        if enable_traceback:
+        if not enable_traceback:
             pytest_args += ' --tb=no'
         if parallel_tests:
             azdev_test_result_fp = os.path.join(azdev_test_result_dir, f"test_results_{python_version}_{profile}_{instance_idx}.parallel.xml")
