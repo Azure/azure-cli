32169d27afa709c6060072d66703a70125ffbcb3
qinkaiwu <qinkaiwu@microsoft.com>
2023-08-08 12:50:29 +0800

    fix style

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -3624,7 +3624,8 @@ def _build_identities_info_from_system_user_assigned(cmd, mi_system_assigned, mi
 
 def deallocate_vmss(cmd, resource_group_name, vm_scale_set_name, instance_ids=None, no_wait=False, hibernate=None):
     client = _compute_client_factory(cmd.cli_ctx)
-    # This is a walkaround because the REST service of `VirtualMachineScaleSetVMs#begin_deallocate` does not accept `hibernate` at present
+    # This is a walkaround because the REST service of `VirtualMachineScaleSetVMs#begin_deallocate`
+    # does not accept `hibernate` at present
     if instance_ids and len(instance_ids) == 1 and hibernate is None:
         return sdk_no_wait(no_wait, client.virtual_machine_scale_set_vms.begin_deallocate,
                            resource_group_name, vm_scale_set_name, instance_ids[0])
