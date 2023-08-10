368974ad8c39b5335d0562f774f203b3cde727b2
hoppe <hoppewang@microsoft.com>
2023-08-10 11:38:29 +0800

    remove self.in_recording according to comments

--- src/azure-cli/azure/cli/command_modules/batch/tests/latest/test_batch_mgmt_commands.py
+++ src/azure-cli/azure/cli/command_modules/batch/tests/latest/test_batch_mgmt_commands.py
@@ -54,7 +54,7 @@ class BatchMgmtScenarioTests(ScenarioTest):
             self.check('encryption.keySource', 'Microsoft.Batch'),
             self.check('resourceGroup', '{rg}')])
 
-        if self.is_live or self.in_recording:
+        if self.is_live:
             time.sleep(100)
 
         self.cmd('batch account set -g {rg} -n {acc} --storage-account {str_n}').assert_with_checks([
