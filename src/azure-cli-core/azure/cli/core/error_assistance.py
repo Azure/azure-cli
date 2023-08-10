e4d57ec33c542a79d20113b234f596de27904267
Amabel Gale <avgale@ucdavis.edu>
2023-08-10 15:05:54 -0700

    Getting response from OpenAI function. Need to extrance relevant info.

--- src/azure-cli-core/azure/cli/core/error_assistance.py
+++ src/azure-cli-core/azure/cli/core/error_assistance.py
@@ -3,24 +3,6 @@ import os
 import openai
 
 def error_assistance(prompt, error_message):      
-        """headers = {
-        'Content-Type': 'application/json',
-        'api-key': <api-key>
-        }
-
-        revised_prompt = "Azure CLI Command: " + prompt + ", Error received: " + error_message + ". What am I doing wrong?"
-
-        data = {
-            "messages": [
-                {"role": "system", "content": "You are a helpful assistant that provides concise explanations about the Azure CLI error provided and provides a remedied command."},
-                {"role": "user", "content": revised_prompt}
-            ]
-        }
-
-        url = 'https://internsopenai.openai.azure.com/openai/deployments/openai-interns-model/chat/completions?api-version=2023-05-15'
-
-        response = requests.post(url, json=data, headers=headers)"""
-
         openai.api_key = <api-key>
         openai.api_version = "2023-07-01-preview"
         openai.api_type = "azure"
@@ -55,18 +37,10 @@ def error_assistance(prompt, error_message):
         ]   
 
         response = openai.ChatCompletion.create(
-            #engine="gpt-35-turbo-0613",
             deployment_id="openai-interns-model",
             messages=messages,
             functions=functions,
             function_call={"name": "error_response"}, 
         )   
-
-        if response.status_code == 200:
-            #return response.json()['choices'][0]['message']['content']
-            return response.json()['choices'][0]['message']
-        else:
-            print(f"API call failed with status code {response.status_code}: {response.text}")
-            return None
-
+        
         return response
