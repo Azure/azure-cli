ac9cea08bfc01cc3f6dd96c12c79991c9e6baf02
Zeng Taoxu <v-taoxuzeng@microsoft.com>
2023-08-11 16:29:02 +0800

    update

--- src/azure-cli/azure/cli/command_modules/consumption/custom.py
+++ src/azure-cli/azure/cli/command_modules/consumption/custom.py
@@ -14,55 +14,6 @@ from azure.cli.core.aaz import has_value
 from datetime import datetime
 
 # pylint: disable=line-too-long
-
-
-def cli_consumption_list_usage(client, billing_period_name=None, top=None, include_additional_properties=False, include_meter_details=False, start_date=None, end_date=None):
-    if include_additional_properties and include_meter_details:
-        expand = 'properties/additionalProperties,properties/meterDetails'
-    elif include_additional_properties:
-        expand = 'properties/additionalProperties'
-    elif include_meter_details:
-        expand = 'properties/meterDetails'
-    else:
-        expand = None
-
-    filter_from = None
-    filter_to = None
-    filter_expression = None
-    if start_date and end_date:
-        filter_from = "properties/usageEnd ge \'{}\'".format(start_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
-        filter_to = "properties/usageEnd le \'{}\'".format(end_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
-        filter_expression = "{} and {}".format(filter_from, filter_to)
-
-    if billing_period_name and top:
-        return list(client.list_by_billing_period(billing_period_name, expand=expand, filter=filter_expression, top=top).advance_page())
-    if billing_period_name and not top:
-        return list(client.list_by_billing_period(billing_period_name, expand=expand, filter=filter_expression))
-    if not billing_period_name and top:
-        return list(client.list(expand=expand, filter=filter_expression, top=top).advance_page())
-    return client.list(expand=expand, filter=filter_expression)
-
-
-def cli_consumption_list_reservation_summary(client, grain, reservation_order_id, reservation_id=None, start_date=None, end_date=None):
-    filter_from = None
-    filter_to = None
-    filter_expression = None
-    if start_date and end_date:
-        print(type(start_date))
-        filter_from = "properties/UsageDate ge {}".format(start_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
-        filter_to = "properties/UsageDate le {}".format(end_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
-        filter_expression = "{} and {}".format(filter_from, filter_to)
-        print(filter_from)
-        print(filter_to)
-        if reservation_id:
-            return list(client.list_by_reservation_order_and_reservation(reservation_order_id, reservation_id, grain=grain, filter=filter_expression))
-        return list(client.list_by_reservation_order(reservation_order_id, grain=grain, filter=filter_expression))
-
-    if reservation_id:
-        return list(client.list_by_reservation_order_and_reservation(reservation_order_id, reservation_id, grain=grain))
-    return list(client.list_by_reservation_order(reservation_order_id, grain=grain))
-
-
 def validate_both_start_end_dates(args):
     """Validates the existence of both start and end dates in the parameter or neither"""
     if bool(has_value(args.start_date)) != bool(has_value(args.end_date)):
@@ -211,18 +162,6 @@ def reservation_summary_output(result):
     return result
 
 
-def cli_consumption_list_reservation_detail(client, reservation_order_id, start_date, end_date, reservation_id=None):
-    filter_from = None
-    filter_to = None
-    filter_expression = None
-    filter_from = "properties/UsageDate ge {}".format(start_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
-    filter_to = "properties/UsageDate le {}".format(end_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
-    filter_expression = "{} and {}".format(filter_from, filter_to)
-    if reservation_id:
-        return list(client.list_by_reservation_order_and_reservation(reservation_order_id, reservation_id, filter=filter_expression))
-    return list(client.list_by_reservation_order(reservation_order_id, filter=filter_expression))
-
-
 class ConsumptionReservationDetailList(_ConsumptionReservationDetailList):
     @classmethod
     def _build_arguments_schema(cls, *args, **kwargs):
@@ -266,17 +205,6 @@ def reservation_detail_output(result):
     result['usedHours'] = str(result['usedHours'])
     result['totalReservedQuantity'] = str(result['totalReservedQuantity'])
     return result
-
-
-def cli_consumption_list_pricesheet_show(client, billing_period_name=None, include_meter_details=False):
-    if include_meter_details:
-        expand_properties = 'properties/meterDetails'
-    else:
-        expand_properties = None
-
-    if billing_period_name:
-        return client.get_by_billing_period(billing_period_name, expand=expand_properties)
-    return client.get(expand=expand_properties)
 
 
 class ConsumptionPricesheetShow(_ConsumptionPricesheetShow):
@@ -388,12 +316,6 @@ class ConsumptionBudgetsList(_ConsumptionBudgetsList):
         result = [budget_output(item) for item in result]
         next_link = self.deserialize_output(self.ctx.vars.instance.next_link)
         return result, next_link
-
-
-def cli_consumption_list_budgets(client, resource_group_name=None):
-    if resource_group_name:
-        return client.list_by_resource_group_name(resource_group_name)
-    return client.list()
 
 
 def cli_consumption_show_budget(cmd, client, budget_name, resource_group_name=None):
