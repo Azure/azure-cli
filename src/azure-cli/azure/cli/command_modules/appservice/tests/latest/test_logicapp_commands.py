e2c7b48583a81620e3f65e2d68a03221dbae4f2a
Arjun Chiddarwar <56557520+archidda@users.noreply.github.com>
2023-08-17 18:48:36 -0700

    [App Service] Fix #26736: `az logicapp create`: Add `--runtime-version` and `--functions-version` optional parameters (#26957)

--- src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_logicapp_commands.py
+++ src/azure-cli/azure/cli/command_modules/appservice/tests/latest/test_logicapp_commands.py
@@ -107,6 +107,41 @@ class LogicappBasicE2ETest(ScenarioTest):
             JMESPathCheck('length([])', 0)
         ])
 
+
+    @ResourceGroupPreparer(location=DEFAULT_LOCATION)
+    @StorageAccountPreparer(location=DEFAULT_LOCATION)
+    def test_logicapp_e2etest_logicapp_versions_e2e(self, resource_group, storage_account):
+        logicapp_name = self.create_random_name(prefix='logic-e2e', length=24)
+        plan = self.create_random_name(prefix='logic-e2e-plan', length=24)
+        runtime_version = '~16'
+        functions_version = '4'
+        self.cmd('appservice plan create -g {} -n {} --sku WS1'.format(resource_group, plan)).get_output_in_json()['id']
+        self.cmd('appservice plan list -g {}'.format(resource_group))
+        self.cmd('logicapp create -g {} -n {} -p {} -s {} --runtime-version {} --functions-version {}'.format(
+            resource_group,
+            logicapp_name,
+            plan,
+            storage_account,
+            runtime_version,
+            functions_version
+            ),
+        checks=[
+                JMESPathCheck('state', 'Running'),
+                JMESPathCheck('name', logicapp_name),
+                JMESPathCheck('hostNames[0]', logicapp_name + '.azurewebsites.net')
+        ])
+
+        # show
+        result = self.cmd('logicapp config appsettings list -g {} -n {}'.format(
+            resource_group, logicapp_name)).get_output_in_json()
+        appsetting_runtime_version = next((x for x in result if x['name'] == 'WEBSITE_NODE_DEFAULT_VERSION'))
+        self.assertEqual(appsetting_runtime_version['name'], 'WEBSITE_NODE_DEFAULT_VERSION')
+        self.assertEqual(appsetting_runtime_version['value'], '~16')
+        appsetting_functions_version = next((x for x in result if x['name'] == 'FUNCTIONS_EXTENSION_VERSION'))
+        self.assertEqual(appsetting_functions_version['name'], 'FUNCTIONS_EXTENSION_VERSION')
+        self.assertEqual(appsetting_functions_version['value'], '~4')
+
+
     @ResourceGroupPreparer(location=DEFAULT_LOCATION)
     @StorageAccountPreparer()
     def test_logicapp_https_only(self, resource_group, storage_account):
@@ -237,7 +272,6 @@ class LogicAppDeployTest(LiveScenarioTest):
             JMESPathCheck('status', 4),
             JMESPathCheck('complete', True),
         ])
-
 
 
 class LogicAppPlanTest(ScenarioTest):
