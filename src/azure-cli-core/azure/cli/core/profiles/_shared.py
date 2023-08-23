f690c6eb55068924515bbf46abe197821d1b3c76
FumingZhang <81607949+FumingZhang@users.noreply.github.com>
2023-08-23 14:14:27 +0800

    {AKS} Vendor new SDK and bump API version to 2023-07-01 (#27222)

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -262,7 +262,7 @@ AZURE_API_PROFILES = {
         ResourceType.MGMT_ARO: '2023-04-01',
         ResourceType.MGMT_DATABOXEDGE: '2021-02-01-preview',
         ResourceType.MGMT_CUSTOMLOCATION: '2021-03-15-preview',
-        ResourceType.MGMT_CONTAINERSERVICE: SDKProfile('2023-06-01', {
+        ResourceType.MGMT_CONTAINERSERVICE: SDKProfile('2023-07-01', {
             'container_services': '2017-07-01',
             'open_shift_managed_clusters': '2019-09-30-preview'
         }),
