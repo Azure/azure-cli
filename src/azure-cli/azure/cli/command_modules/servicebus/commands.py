604da0e8b08f7bc6aa8b5f9974b8915996cc5bc3
Shubham Chaudhari <schaudhari@microsoft.com>
2023-08-26 08:11:05 +0530

    update

--- src/azure-cli/azure/cli/command_modules/servicebus/commands.py
+++ src/azure-cli/azure/cli/command_modules/servicebus/commands.py
@@ -39,6 +39,7 @@ def load_command_table(self, _):
     with self.command_group('servicebus georecovery-alias', custom_command_type=sb_namespace_custom,
                             is_preview=True) as g:
         g.custom_command('set', 'set_georecovery_alias', supports_no_wait=True)
+
     from azure.cli.command_modules.servicebus.aaz.latest.servicebus.georecovery_alias._update import Update
     self.command_table['servicebus georecovery-alias update'] = Update(loader=self, deprecate_info=self.deprecate())
 
@@ -63,5 +64,5 @@ def load_command_table(self, _):
         g.custom_command('add', 'cli_add_encryption')
         g.custom_command('remove', 'cli_remove_encryption')
 
-# Migration
+# Migration Region
     self.command_table['servicebus migration update'] = Update(loader=self, deprecate_info=self.deprecate())
