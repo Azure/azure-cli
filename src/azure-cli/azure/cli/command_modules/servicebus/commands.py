25db1491852084dff032a19c5d10193f4aa2c6f6
Shubham Chaudhari <schaudhari@microsoft.com>
2023-08-28 09:50:42 +0530

    UPdatresa

--- src/azure-cli/azure/cli/command_modules/servicebus/commands.py
+++ src/azure-cli/azure/cli/command_modules/servicebus/commands.py
@@ -6,7 +6,7 @@
 # pylint: disable=line-too-long
 # pylint: disable=too-many-statements
 # pylint: disable=too-many-locals
-# pylint: disable=unused-import
+# pylint: disable=all
 
 from azure.cli.core.commands import CliCommandType
 
