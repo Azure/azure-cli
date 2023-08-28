6794ad5b726bba5480408255f933121af9b99198
Vinit Dinesh Parekh <viparek@microsoft.com>
2023-08-27 19:53:43 -0700

    change in setup

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -123,7 +123,7 @@ DEPENDENCIES = [
     'azure-mgmt-servicefabric~=1.0.0',
     'azure-mgmt-signalr==1.1.0',
     'azure-mgmt-sqlvirtualmachine==1.0.0b5',
-    'azure-mgmt-sql==4.0.0b10',
+    'azure-mgmt-sql==4.0.0b11',
     'azure-mgmt-storage~=21.0.0',
     'azure-mgmt-synapse==2.1.0b5',
     'azure-mgmt-trafficmanager~=1.0.0',
