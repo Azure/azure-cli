a60b04a3c42c8ab721e87863b3e4f0b191124d2d
nitishsharma25 <nitishsharma@microsoft.com>
2023-08-28 11:49:32 +0530

    resolved conflict

--- src/azure-cli/azure/cli/command_modules/mysql/custom.py
+++ src/azure-cli/azure/cli/command_modules/mysql/custom.py
@@ -28,7 +28,7 @@ from ._util import resolve_poller, generate_missing_parameters, get_mysql_list_s
     run_subprocess_get_output, fill_action_template, get_git_root_dir, get_single_to_flex_sku_mapping, GITHUB_ACTION_PATH
 from ._network import prepare_mysql_exist_private_dns_zone, prepare_mysql_exist_private_network, prepare_private_network, prepare_private_dns_zone, prepare_public_network
 from ._validators import mysql_arguments_validator, mysql_auto_grow_validator, mysql_georedundant_backup_validator, mysql_restore_tier_validator, \
-    mysql_retention_validator, mysql_sku_name_validator, mysql_storage_validator, validate_mysql_replica, validate_server_name, validate_georestore_location, \
+    mysql_retention_validator, mysql_sku_name_validator, mysql_storage_validator, validate_mysql_replica, validate_server_name, \
     validate_mysql_tier_update, validate_and_format_restore_point_in_time, validate_public_access_server, mysql_import_single_server_ready_validator, \
     mysql_import_version_validator, mysql_import_storage_validator
 
