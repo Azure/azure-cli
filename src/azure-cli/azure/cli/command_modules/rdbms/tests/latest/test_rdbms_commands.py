de4357f6144a2a21ca9f9a57e8c92e85aeae7c2e
nitishsharma25 <nitishsharma@microsoft.com>
2023-10-02 15:58:13 +0530

    u

--- src/azure-cli/azure/cli/command_modules/rdbms/tests/latest/test_rdbms_commands.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/tests/latest/test_rdbms_commands.py
@@ -316,8 +316,10 @@ class ServerMgmtScenarioTest(ScenarioTest):
                      .format(database_engine, resource_group_1, servers[4]), checks=NoneCheck())
             result = self.cmd('{} server show -g {} -n {}'
                               .format(database_engine, resource_group_1, servers[4])).get_output_in_json()
-            server_version = result['version']
-            self.assertEqual(server_version, '5.7')
+            # server_version = result['version']
+            user_visible_state = result['userVisibleState']
+            # self.assertEqual(server_version, '5.7')
+            self.assertEqual(user_visible_state, 'Upgrading')
 
         # test delete server
         self.cmd('{} server delete -g {} --name {} --yes'
