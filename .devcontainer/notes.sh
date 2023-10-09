5c961b02db2ef6a9f912ed152ef4cfa2f67d9f01
Haitao Chen <haitch@microsoft.com>
2023-10-09 22:11:11 +0000

    tweaks

---
+++ .devcontainer/notes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Manual config steps:"
+echo "  - config vscode python interpreter to '/workspaces/azdev/bin/python'"
+echo "  - run 'source /workspaces/azdev/bin/activate', so you have azdev venv loaded."
+echo "==============="
+echo "for debug launch:"
+echo "  - modify .vscode/launch.json 'args' in 'Azure CLI Debug (Integrated Console)' with your own command in string array (not include az itself) "
+echo "  - seems won't close the debug session, you would have to watch a terminal tab 'Python Debug Console' for debug output."
