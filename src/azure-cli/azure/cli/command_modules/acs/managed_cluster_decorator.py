a35c19397c62fc12621d55087605f96c3cac07a3
Roma Bogatikov <robogatkov@microsoft.com>
2023-10-12 21:54:57 +0000

    remove comma

--- src/azure-cli/azure/cli/command_modules/acs/managed_cluster_decorator.py
+++ src/azure-cli/azure/cli/command_modules/acs/managed_cluster_decorator.py
@@ -6794,7 +6794,7 @@ class AKSManagedClusterUpdateDecorator(BaseAKSManagedClusterDecorator):
             _,
             _,
             _,
-            _,
+            _
         ) = self.context.get_pod_cidr_and_service_cidr_and_dns_service_ip_and_docker_bridge_address_and_network_policy()
 
         network_dataplane = self.context.get_network_dataplane()
