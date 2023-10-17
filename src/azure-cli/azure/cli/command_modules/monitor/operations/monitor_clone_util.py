3f0238c994b93c545976fe37b1c6a5d317ec200c
AllyWang <liwang1025@gmail.com>
2023-10-17 14:31:21 +0800

    remove dup codes

--- src/azure-cli/azure/cli/command_modules/monitor/operations/monitor_clone_util.py
+++ src/azure-cli/azure/cli/command_modules/monitor/operations/monitor_clone_util.py
@@ -14,17 +14,6 @@ logger = get_logger(__name__)
 CLONED_NAME = "cloned-{}-{}"
 
 
-def _gen_metrics_alert_rules_clone_list(monitor_client, source_resource, target_resource):
-    alert_rules = list(monitor_client.metric_alerts.list_by_subscription())
-    for alert_rule in alert_rules:
-        if source_resource in alert_rule.scopes:
-            if target_resource not in alert_rule.scopes:
-                yield alert_rule
-            else:
-                logger.warning('The target resource already has alert rule %s. '
-                               'Skip cloning this one.', alert_rule.name)
-
-
 def _get_metrics_alert_rules_clone_list(cmd, source_resource, target_resource):
     subscription_id = parse_resource_id(source_resource)['subscription']
     from ..aaz.latest.monitor.metrics.alert import List
@@ -39,15 +28,7 @@ def _get_metrics_alert_rules_clone_list(cmd, source_resource, target_resource):
                                'Skip cloning this one.', alert_rule['name'])
 
 
-def _add_into_existing_scopes(monitor_client, alert_rule, target_resource):
-    alert_rule.scopes.append(target_resource)
-    resource_group_name, name = _parse_id(alert_rule.id).values()  # pylint: disable=unbalanced-dict-unpacking
-    return monitor_client.metric_alerts.create_or_update(resource_group_name=resource_group_name,
-                                                         rule_name=name,
-                                                         parameters=alert_rule)
-
-
-def _add_into_existing_scopes_v2(cmd, source_resource, alert_rule, target_resource):
+def _add_into_existing_scopes(cmd, source_resource, alert_rule, target_resource):
     subscription_id = parse_resource_id(source_resource)['subscription']
     alert_rule['scopes'].append(target_resource)
     resource_group_name, name = _parse_id(alert_rule['id']).values()  # pylint: disable=unbalanced-dict-unpacking
@@ -58,28 +39,7 @@ def _add_into_existing_scopes_v2(cmd, source_resource, alert_rule, target_resour
     return Update(cli_ctx=cmd.cli_ctx)(command_args=alert_rule)
 
 
-def _clone_and_replace_action_group(source_monitor_client, target_monitor_client,
-                                    cmd, alert_rule, action_group_mapping, target_resource):
-    for index, action in enumerate(alert_rule.actions):
-        if action.action_group_id in action_group_mapping:
-            alert_rule.actions[index] = action_group_mapping[action.action_group_id][1]
-        else:
-            resource_group_name, name = _parse_id(action.action_group_id).values()  # pylint: disable=unbalanced-dict-unpacking
-            action_group = source_monitor_client.action_groups.get(resource_group_name, name)
-            name = CLONED_NAME.format(name, gen_guid())
-            resource_group_name, _ = _parse_id(target_resource).values()  # pylint: disable=unbalanced-dict-unpacking
-            new_action_group = target_monitor_client.action_groups.create_or_update(resource_group_name,
-                                                                                    name,
-                                                                                    action_group)
-            MetricAlertAction = cmd.get_models('MetricAlertAction', operation_group='metric_alerts')
-            new_action = MetricAlertAction(action_group_id=new_action_group.id,
-                                           web_hook_properties=action.web_hook_properties)
-            alert_rule.actions[index] = new_action
-            action_group_mapping[action.action_group_id] = [new_action_group.id, new_action]
-    return alert_rule
-
-
-def _clone_and_replace_action_group_v2(cmd, source_resource, alert_rule, action_group_mapping, target_resource):
+def _clone_and_replace_action_group(cmd, source_resource, alert_rule, action_group_mapping, target_resource):
     source_subscription_id = parse_resource_id(source_resource)['subscription']
     target_subscription_id = parse_resource_id(target_resource)['subscription']
     for index, action in enumerate(alert_rule['actions']):
@@ -131,16 +91,7 @@ def format_metrics_alert_req(alert_rule):
     del alert_rule['type']
 
 
-def _clone_alert_rule(monitor_client, alert_rule, target_resource):
-    alert_rule.scopes = [target_resource]
-    resource_group_name, name = _parse_id(target_resource).values()  # pylint: disable=unbalanced-dict-unpacking
-    name = CLONED_NAME.format(name, gen_guid())
-    return monitor_client.metric_alerts.create_or_update(resource_group_name=resource_group_name,
-                                                         rule_name=name,
-                                                         parameters=alert_rule)
-
-
-def _clone_alert_rule_v2(cmd, source_resource, alert_rule, target_resource):
+def _clone_alert_rule(cmd, source_resource, alert_rule, target_resource):
     alert_rule['scopes'] = [target_resource]
     resource_group_name, name = _parse_id(target_resource).values()  # pylint: disable=unbalanced-dict-unpacking
     subscription_id = parse_resource_id(source_resource)['subscription']
@@ -161,15 +112,15 @@ def _clone_monitor_metrics_alerts(cmd, source_resource, target_resource, always_
     from azure.core.exceptions import HttpResponseError
     for alert_rule in _get_metrics_alert_rules_clone_list(cmd, source_resource, target_resource):
         if always_clone or not same_sub:
-            alert_rule = _clone_and_replace_action_group_v2(cmd, source_resource, alert_rule, action_group_mapping,
+            alert_rule = _clone_and_replace_action_group(cmd, source_resource, alert_rule, action_group_mapping,
                                                             target_resource)
-            alert_rule = _clone_alert_rule_v2(cmd, source_resource, alert_rule, target_resource)
+            alert_rule = _clone_alert_rule(cmd, source_resource, alert_rule, target_resource)
         else:
             try:
-                alert_rule = _add_into_existing_scopes_v2(cmd, source_resource, alert_rule, target_resource)
+                alert_rule = _add_into_existing_scopes(cmd, source_resource, alert_rule, target_resource)
             except HttpResponseError as ex:  # Create new alert rule
                 if ex.status_code == 400:
-                    alert_rule = _clone_alert_rule_v2(cmd, source_resource, alert_rule, target_resource)
+                    alert_rule = _clone_alert_rule(cmd, source_resource, alert_rule, target_resource)
                 else:
                     raise ex
         updated_metrics_alert_rules.append(alert_rule)
