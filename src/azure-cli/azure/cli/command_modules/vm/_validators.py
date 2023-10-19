8e87ae58e758cd14f360c707d3034b80c782631f
Zeng Taoxu <v-taoxuzeng@microsoft.com>
2023-10-19 14:57:06 +0800

    update

--- src/azure-cli/azure/cli/command_modules/vm/_validators.py
+++ src/azure-cli/azure/cli/command_modules/vm/_validators.py
@@ -412,8 +412,14 @@ def _validate_vm_create_storage_profile(cmd, namespace, for_scale_set=False):
         else:
             raise CLIError('Unrecognized image type: {}'.format(image_type))
     else:
+        namespace.image = 'MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:latest'
+        _parse_image_argument(cmd, namespace)
+        namespace.storage_profile = StorageProfile.ManagedPirImage
+        namespace.enable_secure_boot = True
+        namespace.enable_vtpm = True
+        namespace.security_type = 'TrustedLaunch'
         # did not specify image XOR attach-os-disk
-        raise CLIError('incorrect usage: --image IMAGE | --attach-os-disk DISK')
+        # raise CLIError('incorrect usage: --image IMAGE | --attach-os-disk DISK')
 
     auth_params = ['admin_password', 'admin_username', 'authentication_type',
                    'generate_ssh_keys', 'ssh_dest_key_path', 'ssh_key_value']
