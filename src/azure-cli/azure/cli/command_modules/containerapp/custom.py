21243ae480062ba8e2d2fd5f3b08951e25271c13
Juliehzl <zuh@microsoft.com>
2023-10-25 22:19:24 +0800

    fix

--- src/azure-cli/azure/cli/command_modules/containerapp/custom.py
+++ src/azure-cli/azure/cli/command_modules/containerapp/custom.py
@@ -4898,7 +4898,7 @@ def add_workload_profile(cmd, resource_group_name, env_name, workload_profile_na
     return update_managed_environment(cmd, env_name, resource_group_name, workload_profile_type=workload_profile_type, workload_profile_name=workload_profile_name, min_nodes=min_nodes, max_nodes=max_nodes)
 
 
-def update_workload_profile(cmd, resource_group_name, env_name, workload_profile_name,  min_nodes=None, max_nodes=None):
+def update_workload_profile(cmd, resource_group_name, env_name, workload_profile_name, min_nodes=None, max_nodes=None):
     try:
         r = ManagedEnvironmentClient.show(cmd=cmd, resource_group_name=resource_group_name, name=env_name)
     except CLIError as e:
