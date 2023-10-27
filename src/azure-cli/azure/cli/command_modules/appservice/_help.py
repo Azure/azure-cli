f2f035e2534566d7f573c14b8e6198d96a8714dc
kamperiadis <kamperiadis@microsoft.com>
2023-10-27 17:24:06 -0500

    Add redaction note in documentation

--- src/azure-cli/azure/cli/command_modules/appservice/_help.py
+++ src/azure-cli/azure/cli/command_modules/appservice/_help.py
@@ -638,6 +638,7 @@ short-summary: Manage function keys.
 helps['functionapp function keys set'] = """
 type: command
 short-summary: Create or update a function key.
+long-summary: Note that key values are now redacted in the result. Please use the `az functionapp function keys list` command to view the key values.
 examples:
   - name: Create a function key.
     text: >
@@ -742,6 +743,7 @@ short-summary: Manage function app keys.
 helps['functionapp keys set'] = """
 type: command
 short-summary: Create or update a function app key.
+long-summary: Note that key values are now redacted in the result. Please use the `az functionapp keys list` command to view the key values.
 examples:
   - name: Create a function key for an Azure Function app.
     text: >
