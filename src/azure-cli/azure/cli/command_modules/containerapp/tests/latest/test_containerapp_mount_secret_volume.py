e1c20dddf3506f434c418c0ee807860857a0b187
Stacy Zeng <stacyzeng@microsoft.com>
2023-10-30 10:28:36 -0700

    modify secret values to not be caught by security check

--- src/azure-cli/azure/cli/command_modules/containerapp/tests/latest/test_containerapp_mount_secret_volume.py
+++ src/azure-cli/azure/cli/command_modules/containerapp/tests/latest/test_containerapp_mount_secret_volume.py
@@ -37,17 +37,26 @@ class ContainerAppMountSecretTest(ScenarioTest):
             JMESPathCheck('name', env)            
         ])
 
-        secretRef1 = "mysecret"
-        secretValue1 = "secretvalue1"
-        secretRef2 = "anothersecret"
-        secretValue2 = "secretvalue2"
+        secretRef1 = "fakemysecret"
+        fakesecretValue1 = "foo1"
+        secretRef2 = "fakeanothersecret"
+        fakesecretValue2 = "food2"
 
-        self.cmd(f'az containerapp create -g {resource_group} --environment {env} -n {app} --secrets {secretRef1}={secretValue1} {secretRef2}={secretValue2} --secret-volume-mount "mnt/secrets"')        
+        # here is where the error is occuring rn
+        self.cmd(f'az containerapp create -g {resource_group} --environment {env} -n {app} --secrets {secretRef1}={fakesecretValue1} {secretRef2}={fakesecretValue2} --secret-volume-mount "mnt/secrets"')        
         
         self.cmd('containerapp show -g {} -n {}'.format(resource_group, app), checks=[
             JMESPathCheck('properties.template.volumes[0].storageType', 'Secret'), 
             # --secret-volume-mount mounts all secrets, not specific secrets, therefore no secrets should be returned.
             JMESPathCheck('properties.template.volumes[0].secrets', None)
+        ])
+
+        self.cmd(f'az containerapp update -n {app} -g {resource_group} --secret-volume-mount "mnt/newpath"')
+
+        self.cmd('containerapp show -g {} -n {}'.format(resource_group, app), checks=[
+            JMESPathCheck('properties.template.volumes[0].storageType', 'Secret'),
+            JMESPathCheck('properties.template.volumes[0].secrets', None),
+            JMESPathCheck('properties.template.containers[0].volumeMounts[0].mountPath', 'mnt/newpath'),
         ])
 
     @AllowLargeResponse(8192)
@@ -61,17 +70,18 @@ class ContainerAppMountSecretTest(ScenarioTest):
         app = self.create_random_name(prefix='app1', length=24)
 
         create_containerapp_env(self, env, resource_group, logs_workspace=laworkspace_customer_id, logs_workspace_shared_key=laworkspace_shared_key)
-
-        secretRef1 = "mysecret"
-        secretValue1 = "secretvalue1"
-        secretRef2 = "anothersecret"
-        secretValue2 = "secretvalue2"
+        
+        secretRef1 = "fakemysecret"
+        fakesecretValue1 = "foo1"
+        secretRef2 = "fakeanothersecret"
+        fakesecretValue2 = "food2"
 
         self.cmd('containerapp env show -n {} -g {}'.format(env, resource_group), checks=[
             JMESPathCheck('name', env)
         ])
 
-        self.cmd(f'az containerapp create -g {resource_group} --environment {env} -n {app} --secrets {secretRef1}={secretValue1} {secretRef2}={secretValue2}')        
+        # same issue here
+        self.cmd(f'az containerapp create -g {resource_group} --environment {env} -n {app} --secrets {secretRef1}={fakesecretValue1} {secretRef2}={fakesecretValue2}')        
 
         self.cmd('containerapp show -g {} -n {}'.format(resource_group, app), checks=[
             JMESPathCheck('properties.template.volumes', None),
