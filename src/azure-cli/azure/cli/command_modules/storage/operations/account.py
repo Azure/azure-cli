7ac649e81a92030e49dc84442b523aaef9adea4a
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2023-11-24 17:11:12 +0800

    run check_name_availability before trying to create the storage account and throw a warning that update will be run if a storage account with the same name already exists. In the future, an error will be thrown instead.

--- src/azure-cli/azure/cli/command_modules/storage/operations/account.py
+++ src/azure-cli/azure/cli/command_modules/storage/operations/account.py
@@ -78,6 +78,16 @@ def create_storage_account(cmd, resource_group_name, account_name, sku=None, loc
         cmd.get_models('StorageAccountCreateParameters', 'Kind', 'Sku', 'CustomDomain', 'AccessTier', 'Identity',
                        'Encryption', 'NetworkRuleSet')
     scf = storage_client_factory(cmd.cli_ctx)
+
+    # check name availability and throw a warning if an account with the same name is found
+    # TODO throw error instead of just a warning during the next breaking change window
+    StorageAccountCheckNameAvailabilityParameters = cmd.get_models('StorageAccountCheckNameAvailabilityParameters')
+    account_name_param = StorageAccountCheckNameAvailabilityParameters(name=account_name)
+    name_is_available = scf.storage_accounts.check_name_availability(account_name_param)
+    if name_is_available and not name_is_available.name_available and name_is_available.reason == "AlreadyExists":
+        logger.warning("A storage account with the provided name {} is found. Will continue to run the update command "
+                       "instead.".format(account_name))
+
     if kind is None:
         logger.warning("The default kind for created storage account will change to 'StorageV2' from 'Storage' "
                        "in the future")
