cee9af0dcd373f038573f7dee2f5e9b1fd6686be
Kirstyn Amperiadis <102087132+kamperiadis@users.noreply.github.com>
2023-12-18 00:57:51 -0600

    [AppService] `az functionapp create`: Remove workarounds for Centauri (#27687)

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -1785,9 +1785,11 @@ def update_container_settings_functionapp(cmd, resource_group_name, name, regist
                                           dapr_http_max_request_size=None, dapr_http_read_buffer_size=None,
                                           dapr_log_level=None, dapr_enable_api_logging=None):
     if is_centauri_functionapp(cmd, resource_group_name, name):
-        update_dapr_config(cmd, resource_group_name, name, enable_dapr, dapr_app_id, dapr_app_port,
-                           dapr_http_max_request_size, dapr_http_read_buffer_size, dapr_log_level,
-                           dapr_enable_api_logging)
+        if any([enable_dapr, dapr_app_id, dapr_app_port, dapr_http_max_request_size, dapr_http_read_buffer_size,
+                dapr_log_level, dapr_enable_api_logging]):
+            update_dapr_config(cmd, resource_group_name, name, enable_dapr, dapr_app_id, dapr_app_port,
+                               dapr_http_max_request_size, dapr_http_read_buffer_size, dapr_log_level,
+                               dapr_enable_api_logging)
     return update_container_settings(cmd, resource_group_name, name, registry_server,
                                      image, registry_username, None,
                                      registry_password, multicontainer_config_type=None,
@@ -3945,7 +3947,6 @@ def create_functionapp(cmd, resource_group_name, name, storage_account, plan=Non
     con_string = _validate_and_get_connection_string(cmd.cli_ctx, resource_group_name, storage_account)
 
     if environment is not None:
-        site_config.app_settings.append(NameValuePair(name='AzureWebJobsStorage', value=con_string))
         if docker_registry_server_url is not None:
             site_config.app_settings.append(
                 NameValuePair(name='DOCKER_REGISTRY_SERVER_URL', value=docker_registry_server_url)
@@ -4042,11 +4043,7 @@ def create_functionapp(cmd, resource_group_name, name, storage_account, plan=Non
         location = managed_environment.location
         functionapp_def.location = location
 
-        functionapp_def.enable_additional_properties_sending()
-        existing_properties = functionapp_def.serialize()["properties"]
-        functionapp_def.additional_properties["properties"] = existing_properties
-        functionapp_def.additional_properties["properties"]["name"] = name
-        functionapp_def.additional_properties["properties"]["managedEnvironmentId"] = managed_environment.id
+        functionapp_def.managed_environment_id = managed_environment.id
 
     # temporary workaround for dotnet-isolated linux consumption apps
     if is_linux and consumption_plan_location is not None and runtime == 'dotnet-isolated':
