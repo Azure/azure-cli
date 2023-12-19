1a217d723bdf8d6674c327f470beae12441da763
Tyler Lloyd <tylloyd@microsoft.com>
2023-12-19 17:23:27 +0000

    chore: remove from params

--- src/azure-cli/azure/cli/command_modules/acs/_params.py
+++ src/azure-cli/azure/cli/command_modules/acs/_params.py
@@ -361,7 +361,6 @@ def load_arguments(self, _):
         c.argument('nat_gateway_idle_timeout', type=int, validator=validate_nat_gateway_idle_timeout)
         c.argument('network_dataplane', arg_type=get_enum_type(network_dataplanes))
         c.argument('network_plugin', arg_type=get_enum_type(network_plugins))
-        c.argument('network_plugin_mode', arg_type=get_enum_type(network_plugin_modes))
         c.argument('network_policy')
         c.argument('outbound_type', arg_type=get_enum_type(outbound_types))
         c.argument('auto_upgrade_channel', arg_type=get_enum_type(auto_upgrade_channels))
