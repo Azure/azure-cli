15121788c84022584fd4cc05a114221433fb56de
Yitong Feng <369397590@qq.com>
2023-12-22 17:46:54 +0800

    {AKS} feat: Support trustedaccess role and rolebinding operations (#27931)

--- src/azure-cli/azure/cli/command_modules/acs/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/acs/_client_factory.py
@@ -43,6 +43,14 @@ def get_snapshots_client(cli_ctx, subscription_id=None):
     return get_container_service_client(cli_ctx, subscription_id=subscription_id).snapshots
 
 
+def cf_trustedaccess_role(cli_ctx, *_):
+    return get_container_service_client(cli_ctx).trusted_access_roles
+
+
+def cf_trustedaccess_role_binding(cli_ctx, *_):
+    return get_container_service_client(cli_ctx).trusted_access_role_bindings
+
+
 # dependent clients
 def get_compute_client(cli_ctx, *_):
     return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_COMPUTE)
