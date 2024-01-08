5f636af3f279ca08408402333a56f0fde7d26046
Amit Brahma <ambrahma@microsoft.com>
2024-01-08 11:07:55 +0530

    Fix linter errors

--- src/azure-cli/azure/cli/command_modules/rdbms/_helptext_pg.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_helptext_pg.py
@@ -986,7 +986,7 @@ helps['postgres flexible-server private-endpoint-connection show'] = """
 type: command
 short-summary: Show details of a private endpoint connection associated with a PostgreSQL flexible server.
 examples:
-  - name:Show details of a private endpoint connection associated with a PostgreSQL flexible server.
+  - name: Show details of a private endpoint connection associated with a PostgreSQL flexible server.
     text: az postgres flexible-server private-endpoint-connection show -g testgroup -s testsvr -n pec-connection.40e3d3a8-7d8f-41eb-8462-1cd05bc3e33b
 """
 
@@ -994,7 +994,7 @@ helps['postgres flexible-server private-endpoint-connection approve'] = """
 type: command
 short-summary: Approve the specified private endpoint connection associated with a PostgreSQL flexible server.
 examples:
-  - name:Approve a private endpoint connection associated with a PostgreSQL flexible server.
+  - name: Approve a private endpoint connection associated with a PostgreSQL flexible server.
     text: >
       az postgres flexible-server private-endpoint-connection reject -g testgroup -s testsvr \
         -n pec-connection.40e3d3a8-7d8f-41eb-8462-1cd05bc3e33b \
@@ -1005,7 +1005,7 @@ helps['postgres flexible-server private-endpoint-connection reject'] = """
 type: command
 short-summary: Reject the specified private endpoint connection associated with a PostgreSQL flexible server.
 examples:
-  - name:Reject a private endpoint connection associated with a PostgreSQL flexible server.
+  - name: Reject a private endpoint connection associated with a PostgreSQL flexible server.
     text: >
       az postgres flexible-server private-endpoint-connection reject -g testgroup -s testsvr \
         -n pec-connection.40e3d3a8-7d8f-41eb-8462-1cd05bc3e33b \
@@ -1016,7 +1016,7 @@ helps['postgres flexible-server private-endpoint-connection delete'] = """
 type: command
 short-summary: Delete the specified private endpoint connection associated with a PostgreSQL flexible server.
 examples:
-  - name:Delete a private endpoint connection associated with a PostgreSQL flexible server.
+  - name: Delete a private endpoint connection associated with a PostgreSQL flexible server.
     text: >
       az postgres flexible-server private-endpoint-connection reject -g testgroup -s testsvr \
         --id /subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/testgroup/providers/Microsoft.DBforPostgreSQL/flexibleServers/testsvr/privateEndpointConnections/pec-connection.40e3d3a8-7d8f-41eb-8462-1cd05bc3e33b
