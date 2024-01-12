8b2d4fd892cd74e71e05bf1f49f4ec8d129e6aa7
Kanchan Joshi <kanchanjoshi@microsoft.com>
2024-01-12 12:12:53 +0530

    Re run pipeline

--- src/azure-cli/azure/cli/command_modules/redis/_params.py
+++ src/azure-cli/azure/cli/command_modules/redis/_params.py
@@ -69,7 +69,8 @@ def load_arguments(self, _):  # pylint: disable=too-many-statements
     with self.argument_context('redis access-policy-assignment') as c:
         c.argument('access_policy_assignment_name', help='The name of the access policy assignment')
         c.argument('object_id', help='Object Id to assign access policy to')
-        c.argument('object_id_alias', help='User friendly name for object id. Also represents username for token based authentication')
+        c.argument('object_id_alias', help='User friendly name for object id. Also represents username for token '
+                                           'based authentication')
         c.argument('access_policy_name', help='The name of the access policy that is being assigned')
 
     with self.argument_context('redis force-reboot') as c:
