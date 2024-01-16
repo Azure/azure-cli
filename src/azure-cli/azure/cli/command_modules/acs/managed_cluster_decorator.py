56ecb7f48023b5634ca89231c1e885c5e5cd88e4
Devesh Dama <ddama@microsoft.com>
2024-01-16 20:02:53 +0000

    fix style issues

--- src/azure-cli/azure/cli/command_modules/acs/managed_cluster_decorator.py
+++ src/azure-cli/azure/cli/command_modules/acs/managed_cluster_decorator.py
@@ -4261,8 +4261,8 @@ class AKSManagedClusterContext(BaseAKSContext):
 
         if updated:
             return new_profile
-        else:
-            return self.mc.service_mesh_profile
+
+        return self.mc.service_mesh_profile
 
     def _sort_revisions(self, revisions):
         def _convert_revision_to_semver(rev):
