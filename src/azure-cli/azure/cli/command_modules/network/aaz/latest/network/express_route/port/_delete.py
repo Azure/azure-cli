cba98c9f1160a0b458d7e4b549bf7ad18fa07a60
Jing-song <v-jingszhang@microsoft.com>
2024-01-17 10:29:34 +0800

    {Network} `az network express route`: Bump up API version to `2023-09-01` (#28169)

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/express_route/port/_delete.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/express_route/port/_delete.py
@@ -23,9 +23,9 @@ class Delete(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2022-01-01",
+        "version": "2023-09-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.network/expressrouteports/{}", "2022-01-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.network/expressrouteports/{}", "2023-09-01"],
         ]
     }
 
@@ -143,7 +143,7 @@ class Delete(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2022-01-01",
+                    "api-version", "2023-09-01",
                     required=True,
                 ),
             }
