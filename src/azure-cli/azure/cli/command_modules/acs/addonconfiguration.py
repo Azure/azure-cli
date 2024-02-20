5399551cfb197f36c96c9c0e8c36ec2179c62da5
Ganga Mahesh Siddem <gangams@microsoft.com>
2024-02-19 22:24:05 -0800

    fix bug

--- src/azure-cli/azure/cli/command_modules/acs/addonconfiguration.py
+++ src/azure-cli/azure/cli/command_modules/acs/addonconfiguration.py
@@ -689,7 +689,7 @@ def ensure_container_insights_for_monitoring(
                 }
             )
             link_dce_ampls_url = cmd.cli_ctx.cloud.endpoints.resource_manager + \
-                f"{azure_monitor_private_link_scope_resource_id}/{dataCollectionEndpointName}-connection?apiVersion:2021-07-01-preview"
+                f"{azure_monitor_private_link_scope_resource_id}/scopedresources/{dataCollectionEndpointName}-connection?apiVersion:2021-07-01-preview"
 
             for _ in range(3):
                 try:
@@ -717,7 +717,7 @@ def ensure_container_insights_for_monitoring(
                 }
             )
             link_ws_ampls_url = cmd.cli_ctx.cloud.endpoints.resource_manager + \
-                f"{azure_monitor_private_link_scope_resource_id}/{workspace_name}-connection?apiVersion:2021-07-01-preview"
+                f"{azure_monitor_private_link_scope_resource_id}/scopedresources/{workspace_name}-connection?apiVersion:2021-07-01-preview"
 
             for _ in range(3):
                 try:
