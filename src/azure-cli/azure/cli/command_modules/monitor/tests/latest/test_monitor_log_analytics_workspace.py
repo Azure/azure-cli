71d6b9aaa6d616923fb973e6db744b651feec064
AllyW <liwang1025@gmail.com>
2024-02-21 13:56:31 +0800

    fix test

--- src/azure-cli/azure/cli/command_modules/monitor/tests/latest/test_monitor_log_analytics_workspace.py
+++ src/azure-cli/azure/cli/command_modules/monitor/tests/latest/test_monitor_log_analytics_workspace.py
@@ -483,7 +483,7 @@ class TestLogProfileScenarios(ScenarioTest):
             'rule_id': '/subscriptions/0b1f6471-1bf0-4dda-aec3-cb9272f09590/resourceGroups/tbtest/providers/Microsoft.Insights/dataCollectionRules/rule11'
         })
         # extension command
-        # rule = self.cmd('monitor data-collection rule show -g tbtest -n rule11').get_output_in_json()
+        rule = self.cmd('monitor data-collection rule show -g tbtest -n rule11').get_output_in_json()
         # self.kwargs.update({
         #     'rule_id': rule['id']
         # })
@@ -512,7 +512,7 @@ class TestLogProfileScenarios(ScenarioTest):
             self.check('schema.columns[1].name', 'TimeGenerated'),
             self.check('schema.columns[1].type', 'datetime'),
         ])
-        self.cmd('monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 50 --total-retention-time 80 --columns col2=guid', checks=[
+        self.cmd('monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 50 --total-retention-time 80 --columns col2=guid TimeGenerated=datetime', checks=[
             self.check('name', '{table_name}'),
             self.check('retentionInDays', 50),
             self.check('totalRetentionInDays', 80),
@@ -555,7 +555,7 @@ class TestLogProfileScenarios(ScenarioTest):
                 self.check('schema.columns[1].type', 'datetime'),
             ])
         self.cmd(
-            'monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --total-retention-time 400 --columns col2=guid',
+            'monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --total-retention-time 400 --columns col2=guid TimeGenerated=datetime',
             checks=[
                 self.check('name', '{table_name}'),
                 self.check('totalRetentionInDays', 400),
@@ -671,7 +671,7 @@ class TestLogProfileScenarios(ScenarioTest):
         with self.assertRaisesRegex(InvalidArgumentValueError, "usage error: --retention-time should between 4 and 730"):
             self.cmd('monitor log-analytics workspace table create -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 3 --total-retention-time 3 --plan Analytics --columns col1=guid TimeGenerated=datetime')
 
-        with self.assertRaisesRegex(InvalidArgumentValueError, "usage error: --total-retention-time should between 4 and 2556"):
+        with self.assertRaisesRegex(InvalidArgumentValueError, "usage error: --total-retention-time should between 4 and 4383"):
             self.cmd('monitor log-analytics workspace table create -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time -1 --total-retention-time 3 --plan Analytics --columns col1=guid TimeGenerated=datetime')
 
         self.cmd(
@@ -685,14 +685,14 @@ class TestLogProfileScenarios(ScenarioTest):
             ])
         with self.assertRaisesRegex(InvalidArgumentValueError, "usage error: --retention-time should between 4 and 730"):
             self.cmd('monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 2 --total-retention-time 2')
-        with self.assertRaisesRegex(InvalidArgumentValueError, "usage error: --total-retention-time should between 4 and 2556"):
+        with self.assertRaisesRegex(InvalidArgumentValueError, "usage error: --total-retention-time should between 4 and 4383"):
             self.cmd('monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 4 --total-retention-time 2')
         self.cmd(
-            'monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 4 --total-retention-time 2556 --columns col2=guid',
+            'monitor log-analytics workspace table update -g {rg} -n {table_name} --workspace-name {ws_name} --retention-time 4 --total-retention-time 4383 --columns col2=guid TimeGenerated=datetime',
             checks=[
                 self.check('name', '{table_name}'),
                 self.check('retentionInDays', 4),
-                self.check('totalRetentionInDays', 2556),
+                self.check('totalRetentionInDays', 4383),
                 self.check('schema.columns[0].name', 'col2'),
                 self.check('schema.columns[0].type', 'guid'),
             ])
@@ -700,7 +700,7 @@ class TestLogProfileScenarios(ScenarioTest):
                  checks=[
                      self.check('name', '{table_name}'),
                      self.check('retentionInDays', 4),
-                     self.check('totalRetentionInDays', 2556),
+                     self.check('totalRetentionInDays', 4383),
                  ])
 
         self.cmd('monitor log-analytics workspace table delete -g {rg} -n {table_name} --workspace-name {ws_name} -y')
@@ -713,7 +713,7 @@ class TestLogProfileScenarios(ScenarioTest):
         })
 
         self.cmd('monitor log-analytics workspace create -g {rg} -n {ws_name}')
-        from azure.core.exceptions import ResourceNotFoundError
-        with self.assertRaises(ResourceNotFoundError):
+        from azure.core.exceptions import ClientAuthenticationError
+        with self.assertRaises(ClientAuthenticationError):
             self.cmd("monitor log-analytics workspace list-link-target")
         self.cmd("monitor log-analytics workspace list-available-service-tier -g {rg} --workspace-name {ws_name}")
