e90011be362a59723669701864e59fa53ded7cef
Jingnan Xu <jingnanxu@microsoft.com>
2024-02-22 15:52:46 +0800

    change para

--- src/azure-cli/azure/cli/command_modules/cdn/tests/latest/test_afd_origin_group_scenarios.py
+++ src/azure-cli/azure/cli/command_modules/cdn/tests/latest/test_afd_origin_group_scenarios.py
@@ -30,9 +30,8 @@ class CdnAfdOriginGroupScenarioTest(CdnAfdScenarioMixin, ScenarioTest):
                                          profile_name,
                                          origin_group_name,
                                          "--probe-request-type GET --probe-protocol Http --probe-interval-in-seconds 120 --probe-path /test1/azure.txt " +
-                                         "--sample-size 4 --successful-samples-required 3 --additional-latency-in-milliseconds 50 --enable-health-probe",
+                                         "--sample-size 4 --successful-samples-required 3 --additional-latency-in-milliseconds 50 --enable-health-probe true",
                                          checks=checks)
-
         list_checks = [JMESPathCheck('length(@)', 1),
                        JMESPathCheck('@[0].name', origin_group_name)]
         self.afd_origin_group_list_cmd(resource_group, profile_name, checks=list_checks)
@@ -46,7 +45,7 @@ class CdnAfdOriginGroupScenarioTest(CdnAfdScenarioMixin, ScenarioTest):
                          JMESPathCheck('healthProbeSettings.probeIntervalInSeconds', 120),
                          JMESPathCheck('healthProbeSettings.probeRequestType', "GET"),
                          JMESPathCheck('provisioningState', 'Succeeded')]
-        options = '--probe-request-type GET --probe-protocol Https --enable-health-probe'
+        options = '--probe-request-type GET --probe-protocol Https --enable-health-probe true'
         self.afd_origin_group_update_cmd(resource_group,
                                          profile_name,
                                          origin_group_name,
@@ -62,7 +61,7 @@ class CdnAfdOriginGroupScenarioTest(CdnAfdScenarioMixin, ScenarioTest):
                          JMESPathCheck('healthProbeSettings.probeIntervalInSeconds', 120),
                          JMESPathCheck('healthProbeSettings.probeRequestType', "GET"),
                          JMESPathCheck('provisioningState', 'Succeeded')]
-        options = '--sample-size 5 --additional-latency-in-milliseconds 30 --enable-health-probe'
+        options = '--sample-size 5 --additional-latency-in-milliseconds 30 --enable-health-probe true'
         self.afd_origin_group_update_cmd(resource_group,
                                          profile_name,
                                          origin_group_name,
@@ -78,7 +77,7 @@ class CdnAfdOriginGroupScenarioTest(CdnAfdScenarioMixin, ScenarioTest):
                          JMESPathCheck('healthProbeSettings.probeIntervalInSeconds', 120),
                          JMESPathCheck('healthProbeSettings.probeRequestType', "HEAD"),
                          JMESPathCheck('provisioningState', 'Succeeded')]
-        options = '--sample-size 4 --additional-latency-in-milliseconds 30 --probe-request-type HEAD --enable-health-probe'
+        options = '--sample-size 4 --additional-latency-in-milliseconds 30 --probe-request-type HEAD --enable-health-probe true'
         self.afd_origin_group_update_cmd(resource_group,
                                          profile_name,
                                          origin_group_name,
