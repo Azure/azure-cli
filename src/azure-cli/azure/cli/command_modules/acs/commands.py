20af41e90e10612cba6900f94764cfaac6973ade
Brandon Foley <brandonfoley13@gmail.com>
2024-02-29 10:53:31 -0500

    Update to single quotes for linter

--- src/azure-cli/azure/cli/command_modules/acs/commands.py
+++ src/azure-cli/azure/cli/command_modules/acs/commands.py
@@ -237,17 +237,17 @@ def load_command_table(self, _):
 
     # AKS approuting commands
     with self.command_group(
-        "aks approuting", managed_clusters_sdk, client_factory=cf_managed_clusters
+        'aks approuting', managed_clusters_sdk, client_factory=cf_managed_clusters
     ) as g:
-        g.custom_command("enable", "aks_approuting_enable")
-        g.custom_command("disable", "aks_approuting_disable", confirmation=True)
-        g.custom_command("update", "aks_approuting_update")
+        g.custom_command('enable', 'aks_approuting_enable')
+        g.custom_command('disable', 'aks_approuting_disable', confirmation=True)
+        g.custom_command('update', 'aks_approuting_update')
 
     # AKS approuting dns-zone commands
     with self.command_group(
-        "aks approuting zone", managed_clusters_sdk, client_factory=cf_managed_clusters
+        'aks approuting zone', managed_clusters_sdk, client_factory=cf_managed_clusters
     ) as g:
-        g.custom_command("add", "aks_approuting_zone_add")
-        g.custom_command("delete", "aks_approuting_zone_delete", confirmation=True)
-        g.custom_command("update", "aks_approuting_zone_update")
-        g.custom_command("list", "aks_approuting_zone_list")
+        g.custom_command('add', 'aks_approuting_zone_add')
+        g.custom_command('delete', 'aks_approuting_zone_delete', confirmation=True)
+        g.custom_command('update', 'aks_approuting_zone_update')
+        g.custom_command('list', 'aks_approuting_zone_list')
