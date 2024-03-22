163de3ace62305a64bae7d464ec65bcf32afc764
Kirstyn Amperiadis <102087132+kamperiadis@users.noreply.github.com>
2024-03-21 21:33:21 -0500

    {AppService} `az functionapp`: Add flex support (#28001)

--- src/azure-cli/azure/cli/command_modules/appservice/_constants.py
+++ src/azure-cli/azure/cli/command_modules/appservice/_constants.py
@@ -136,3 +136,44 @@ LANGUAGE_EOL_DEPRECATION_NOTICES = {
                "14-lts-is-ending-on-30-april-2023/",
     "node|16": "https://azure.microsoft.com/en-us/updates/node16support/"
 }
+
+FLEX_RUNTIMES = [
+    {
+        'runtime': 'dotnet-isolated',
+        'version': '8.0'
+    },
+    {
+        'runtime': 'java',
+        'version': '17'
+    },
+    {
+        'runtime': 'java',
+        'version': '11'
+    },
+    {
+        'runtime': 'node',
+        'version': '18'
+    },
+    {
+        'runtime': 'python',
+        'version': '3.10'
+    },
+    {
+        'runtime': 'python',
+        'version': '3.11'
+    },
+    {
+        'runtime': 'powershell',
+        'version': '7.2'
+    }
+]
+
+FLEX_SUBNET_DELEGATION = "Microsoft.App/environments"
+
+DEFAULT_INSTANCE_SIZE = 2048
+
+DEFAULT_MAXIMUM_INSTANCE_COUNT = 100
+
+DEPLOYMENT_STORAGE_AUTH_TYPES = ['StorageAccountConnectionString']
+
+STORAGE_BLOB_DATA_CONTRIBUTOR_ROLE_ID = 'ba92f5b4-2d11-453d-a403-e96b0029c9fe'
