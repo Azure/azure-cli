f1f8cb8ed3c05b24c707eb446d97846ce49a85c9
Jing-song <v-jingszhang@microsoft.com>
2024-03-25 15:30:03 +0800

    test

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -369,7 +369,7 @@ def create_managed_disk(cmd, resource_group_name, disk_name, location=None,  # p
     if security_data_uri:
         option = getattr(DiskCreateOption, 'import_secure')
     elif source_blob_uri:
-        option = getattr(DiskCreateOption, 'import')
+        option = getattr(DiskCreateOption, 'import_enum')
     elif source_disk or source_snapshot:
         option = getattr(DiskCreateOption, 'copy')
     elif source_restore_point:
@@ -684,7 +684,7 @@ def create_snapshot(cmd, resource_group_name, snapshot_name, location=None, size
 
     location = location or _get_resource_group_location(cmd.cli_ctx, resource_group_name)
     if source_blob_uri:
-        option = getattr(DiskCreateOption, 'import')
+        option = getattr(DiskCreateOption, 'import_enum')
     elif source_disk or source_snapshot:
         option = getattr(DiskCreateOption, 'copy')
         if cmd.supported_api_version(min_api='2021-04-01', operation_group='snapshots'):
