d3c5a5a63eb3bf2553df1c707a3d29e8393efd1f
Mwesigwa Guma <mwesigwaguma@microsoft.com>
2024-04-15 23:55:17 -0400

    fix if else statement

--- src/azure-cli/azure/cli/command_modules/servicefabric/operations/managed_clusters.py
+++ src/azure-cli/azure/cli/command_modules/servicefabric/operations/managed_clusters.py
@@ -385,8 +385,8 @@ def delete_network_security_rule(cmd,
             if initial_size > len(cluster.network_security_rules):
                 poller = client.managed_clusters.begin_create_or_update(resource_group_name, cluster_name, cluster)
                 return LongRunningOperation(cmd.cli_ctx)(poller)
-            else:
-                raise 'NSG with name {} not found'.format(name)
+            
+            raise 'NSG with name {} not found'.format(name)
 
         return cluster
     except HttpResponseError as ex:
