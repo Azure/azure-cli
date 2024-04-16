0b4b04d1338d45c562ed17cc0ac697d5c14db46b
xinyupang <xinyupang@microsoft.com>
2024-04-16 17:44:37 +0800

    fix

--- src/azure-cli/azure/cli/command_modules/containerapp/_ssh_utils.py
+++ src/azure-cli/azure/cli/command_modules/containerapp/_ssh_utils.py
@@ -151,7 +151,8 @@ def _send_stdin(connection: WebSocketConnection, getch_fn):
 
 
 def _resize_terminal(connection: WebSocketConnection):
-    size = os.get_terminal_size()
+    from shutil import get_terminal_size
+    size = get_terminal_size()
     if connection.is_connected:
         connection.send(b"".join([SSH_TERM_RESIZE_PREFIX,
                                   f'{{"Width": {size.columns}, '
