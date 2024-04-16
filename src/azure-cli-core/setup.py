6c253662715b2e781bd7d2698e5469fe7289ef2c
Jiashuo Li <4003950+jiasli@users.noreply.github.com>
2024-04-16 14:54:22 +0800

    {Core} Bump `msal-extensions` to 1.2.0b1 (#28747)

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -52,7 +52,7 @@ DEPENDENCIES = [
     'humanfriendly~=10.0',
     'jmespath',
     'knack~=0.11.0',
-    'msal-extensions~=1.0.0',
+    'msal-extensions==1.2.0b1',
     'msal[broker]==1.28.0',
     'msrestazure~=0.6.4',
     'packaging>=20.9',
