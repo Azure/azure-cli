58282ed506ef90d176b16b321808612ab5f1e38c
hoppe <wanghp000@gmail.com>
2024-04-18 18:18:33 -0700

    rm used

--- temp.sh
+++
@@ -1,6 +0,0 @@
-python \
-src/azure-cli/azure/cli/__main__.py batch pool create --help \
-# --id tp001 \
-# --vm-size Standard_A1 \
-# --image "MicrosoftWindowsServer:WindowsServer:2016-datacenter-smalldisk"
-# --node-agent-sku-id "batch.node.windows amd64"
