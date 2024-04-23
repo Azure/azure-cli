b64bfda1283a989ebc9b8f9a4ba79ad106fe11a4
necusjz <necusjz@gmail.com>
2024-04-23 17:23:00 +0800

    style: add license header

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/_clients.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/_clients.py
@@ -1,3 +1,7 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
 from azure.cli.core.aaz import AAZClientConfiguration, register_client
 from azure.cli.core.aaz._client import AAZMgmtClient
 
