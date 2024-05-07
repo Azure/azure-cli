8e2f178f224bc32e92348eaf1dbda3a9ab0ad970
Hang <hanglei@microsoft.com>
2024-05-07 16:15:38 +0800

    {Packaging} Bump argcomplete to 3.3.0 (#28830)

--- src/azure-cli-core/setup.py
+++ src/azure-cli-core/setup.py
@@ -43,7 +43,7 @@ CLASSIFIERS = [
 ]
 
 DEPENDENCIES = [
-    'argcomplete~=3.1.1',
+    'argcomplete~=3.3.0',
     'azure-cli-telemetry==1.1.0.*',
     'azure-mgmt-core>=1.2.0,<2',
     'cryptography',
