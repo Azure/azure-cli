623422f7c91c7004e69ec1fbf462cd7f30e3e65d
Greedygre <46143499+Greedygre@users.noreply.github.com46143499+Greedygre@users.noreply.github.com>
2024-05-09 17:48:09 +0800

    fix

--- src/azure-cli/azure/cli/command_modules/containerapp/containerapp_decorator.py
+++ src/azure-cli/azure/cli/command_modules/containerapp/containerapp_decorator.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 # pylint: disable=line-too-long, consider-using-f-string, no-else-return, duplicate-string-formatting-argument, expression-not-assigned, too-many-locals, logging-fstring-interpolation, broad-except, pointless-statement, bare-except, too-many-public-methods, logging-format-interpolation, too-many-boolean-expressions, too-many-branches, useless-parent-delegation
+from copy import deepcopy
 from typing import Dict, Any
 
 from azure.cli.core.commands import AzCliCommand
@@ -278,7 +279,7 @@ class ContainerAppCreateDecorator(BaseContainerAppDecorator):
         self, cmd: AzCliCommand, client: Any, raw_parameters: Dict, models: str
     ):
         super().__init__(cmd, client, raw_parameters, models)
-        self.containerapp_def = ContainerAppModel
+        self.containerapp_def = deepcopy(ContainerAppModel)
 
     def validate_arguments(self):
         validate_container_app_name(self.get_argument_name(), AppType.ContainerApp.name)
