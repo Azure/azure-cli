6f52b0f274ea5c12ce68cb53a49d2a60adc56883
mwesigwaguma <111312378+mwesigwaguma@users.noreply.github.com>
2024-05-20 12:38:33 -0400

    Update src/azure-cli/azure/cli/command_modules/servicefabric/_params.py
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/servicefabric/_params.py
+++ src/azure-cli/azure/cli/command_modules/servicefabric/_params.py
@@ -331,7 +331,7 @@ def load_arguments(self, _):  # pylint: disable=too-many-statements
         c.argument('is_stateless', arg_type=get_three_state_flag(), help='Indicates if the node type can only host Stateless workloads.', default=False)
         c.argument('multiple_placement_groups', options_list=['--multiple-placement-groups', '--multi-place-groups'], arg_type=get_three_state_flag(),
                    help='Indicates if scale set associated with the node type can be composed of multiple placement groups.', default=False)
-        c.argument('update_type', arg_type=get_enum_type(UpdateType), help='Specifies the way the operation will be performed'
+        c.argument('update_type', arg_type=get_enum_type(UpdateType), help='Specify the way the operation will be performed'
                    'accepted values are ByUpgradeDomain and Default')
 
     with self.argument_context('sf managed-node-type node') as c:
