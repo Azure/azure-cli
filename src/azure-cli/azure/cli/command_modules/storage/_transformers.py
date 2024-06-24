b53a75d2b6742f3d02112f02786039c1702cc0e7
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2024-06-24 15:59:35 +0800

    fix pr suggestion

--- src/azure-cli/azure/cli/command_modules/storage/_transformers.py
+++ src/azure-cli/azure/cli/command_modules/storage/_transformers.py
@@ -38,9 +38,9 @@ def transform_acl_edit(result):
 
 
 def transform_acl_datetime(result):
-    result = todict(result)
     if result is None:
         return result
+    result = todict(result)
     if result['start']:
         result['start'] = result["start"].split('.')[0] + '+00:00'
     if result['expiry']:
