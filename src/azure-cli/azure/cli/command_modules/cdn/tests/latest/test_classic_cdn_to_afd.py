e0cf6fd92468f81b7c26a9f2e33f6628b2bedc04
Jingnan Xu <jingnanxu@microsoft.com>
2024-06-25 11:11:36 +0800

    skip a test

--- src/azure-cli/azure/cli/command_modules/cdn/tests/latest/test_classic_cdn_to_afd.py
+++ src/azure-cli/azure/cli/command_modules/cdn/tests/latest/test_classic_cdn_to_afd.py
@@ -29,26 +29,27 @@ class ClassicCdnMigration(CdnScenarioMixin, ScenarioTest):
 
         self.profile_delete_cmd(resource_group, profile_name)
 
-    @ResourceGroupPreparer(additional_tags={'owner': 'jingnanxu'})
-    def test_classic_cdn_migration_abort(self, resource_group):
-        list_checks = [JMESPathCheck('length(@)', 0)]
-        self.profile_list_cmd(resource_group, checks=list_checks)
+    # skip "https://github.com/Azure/azure-cli/issues/28678"
+    # @ResourceGroupPreparer(additional_tags={'owner': 'jingnanxu'})
+    # def test_classic_cdn_migration_abort(self, resource_group):
+    #     list_checks = [JMESPathCheck('length(@)', 0)]
+    #     self.profile_list_cmd(resource_group, checks=list_checks)
 
-        profile_name = 'profile123'
-        checks = [JMESPathCheck('name', profile_name),
-                  JMESPathCheck('sku.name', 'Standard_Microsoft')]
-        self.profile_create_cmd(resource_group, profile_name, sku='Standard_Microsoft', checks=checks)
+    #     profile_name = 'profile123'
+    #     checks = [JMESPathCheck('name', profile_name),
+    #               JMESPathCheck('sku.name', 'Standard_Microsoft')]
+    #     self.profile_create_cmd(resource_group, profile_name, sku='Standard_Microsoft', checks=checks)
 
-        checks = [JMESPathCheck('canMigrate', True),
-                  JMESPathCheck('type', 'Microsoft.Cdn/canmigrate')]
-        self.cdn_can_migrate_to_afd(resource_group, profile_name, checks=checks)
+    #     checks = [JMESPathCheck('canMigrate', True),
+    #               JMESPathCheck('type', 'Microsoft.Cdn/canmigrate')]
+    #     self.cdn_can_migrate_to_afd(resource_group, profile_name, checks=checks)
 
-        checks = [JMESPathCheck('type', 'Microsoft.Cdn/migrate')]
-        self.cdn_migrate_to_afd(resource_group, profile_name, sku='Premium_AzureFrontDoor', checks=checks)
+    #     checks = [JMESPathCheck('type', 'Microsoft.Cdn/migrate')]
+    #     self.cdn_migrate_to_afd(resource_group, profile_name, sku='Premium_AzureFrontDoor', checks=checks)
 
-        self.cdn_migration_abort(resource_group, profile_name)
+    #     self.cdn_migration_abort(resource_group, profile_name)
 
-        self.profile_delete_cmd(resource_group, profile_name)
+    #     self.profile_delete_cmd(resource_group, profile_name)
 
     def test_classic_cdn_migration_with_endpoint(self):
         rg_name = "cli-test-rg"
