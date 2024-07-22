d68cbd75fcfc11b1de7459c59638055acc596713
Fuming Zhang <fumingzhang@microsoft.com>
2024-07-22 15:16:22 +0800

    update SDK & API version

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -77,7 +77,7 @@ DEPENDENCIES = [
     'azure-mgmt-compute~=31.0.0',
     'azure-mgmt-containerinstance==10.1.0',
     'azure-mgmt-containerregistry==10.3.0',
-    'azure-mgmt-containerservice~=30.0.0',
+    'azure-mgmt-containerservice~=31.0.0',
     'azure-mgmt-cosmosdb==9.5.1',
     'azure-mgmt-databoxedge~=1.0.0',
     'azure-mgmt-datamigration~=10.0.0',
