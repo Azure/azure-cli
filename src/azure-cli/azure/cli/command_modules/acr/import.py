b3dbe15585987617544601e643188c1b359e6a8b
Siby George <siby.george@gmail.com>
2024-07-23 00:19:51 +0000

    Fix for supporting all Cloud

--- src/azure-cli/azure/cli/command_modules/acr/import.py
+++ src/azure-cli/azure/cli/command_modules/acr/import.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from azure.cli.command_modules.acr._docker_utils import get_login_server_suffix
 from knack.util import CLIError
 from knack.log import get_logger
 from msrestazure.tools import is_valid_resource_id, parse_resource_id
@@ -77,7 +78,9 @@ def acr_import(cmd,  # pylint: disable=too-many-locals
                                   credentials=ImportSourceCredentials(password=source_registry_password,
                                                                       username=source_registry_username))
         else:
-            if registry_uri.endswith('azurecr.io') or registry_uri.endswith('.azurecr-test.io'):
+             # Try to get the pre-defined login server suffix.
+            login_server_suffix = get_login_server_suffix(cmd.cli_ctx)
+            if not login_server_suffix or registry_uri.endswith(login_server_suffix):
                 registry = get_registry_from_name_or_login_server(cmd.cli_ctx, registry_uri)
             if registry:
                 # For Azure container registry
