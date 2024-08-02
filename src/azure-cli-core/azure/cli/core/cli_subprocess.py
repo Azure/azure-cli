76cd678dfb231a8b8d4cd0693760897a2fe95a75
AllyW <liwang1025@gmail.com>
2024-08-02 13:48:32 +0800

    remove unused

--- src/azure-cli-core/azure/cli/core/cli_subprocess.py
+++ src/azure-cli-core/azure/cli/core/cli_subprocess.py
@@ -3,6 +3,8 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+# pylint: disable=no-else-return
+
 import subprocess
 import re
 from functools import wraps
