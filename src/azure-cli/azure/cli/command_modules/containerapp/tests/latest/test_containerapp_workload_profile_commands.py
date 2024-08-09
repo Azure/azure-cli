7cb4cc5eccf906be6b55e079055fc4741d6968bd
Greedygre <46143499+Greedygre@users.noreply.github.com46143499+Greedygre@users.noreply.github.com>
2024-08-09 10:50:35 +0800

    fix test typo

--- src/azure-cli/azure/cli/command_modules/containerapp/tests/latest/test_containerapp_workload_profile_commands.py
+++ src/azure-cli/azure/cli/command_modules/containerapp/tests/latest/test_containerapp_workload_profile_commands.py
@@ -161,14 +161,14 @@ class ContainerAppWorkloadProfilesTest(ScenarioTest):
             JMESPathCheck("properties.workloadProfiles", None),
         ])
 
-        with self.assertRaisesRegexp(ValidationError,
+        with self.assertRaisesRegex(ValidationError,
                                      "Cannot add workload profile because the environment doesn't enable workload profile.\n"
                                      "If you want to use Consumption and Dedicated environment, please create a new one."):
             self.cmd(
                 'containerapp env workload-profile add -w wp -g {} -n {} --workload-profile-type D4 --min-nodes 1 --max-nodes 2'.format(
                     resource_group, env))
 
-        with self.assertRaisesRegexp(ValidationError,
+        with self.assertRaisesRegex(ValidationError,
                                      "Cannot update workload profile because the environment doesn't enable workload profile.\n"
                                      "If you want to use Consumption and Dedicated environment, please create a new one."):
             self.cmd(
