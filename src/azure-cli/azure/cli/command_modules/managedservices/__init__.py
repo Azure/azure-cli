5bb0dc6a7b73a09ea1a8c24aeb61635b46c74e38
Jing-song <v-jingszhang@microsoft.com>
2024-08-20 16:09:12 +0800

    mangedservices

--- src/azure-cli/azure/cli/command_modules/managedservices/__init__.py
+++ src/azure-cli/azure/cli/command_modules/managedservices/__init__.py
@@ -14,11 +14,21 @@ class ManagedServicesCommandsLoader(AzCommandsLoader):
         from azure.cli.core.commands import CliCommandType
         managedservices_custom = CliCommandType(operations_tmpl='azure.cli.command_modules.managedservices.custom#{}')
         super(ManagedServicesCommandsLoader, self).__init__(cli_ctx=cli_ctx,
-                                                            resource_type=ResourceType.MGMT_MANAGEDSERVICES,
                                                             custom_command_type=managedservices_custom)
 
     def load_command_table(self, args):
         from .commands import load_command_table
+        from azure.cli.core.aaz import load_aaz_command_table
+        try:
+            from . import aaz
+        except ImportError:
+            aaz = None
+        if aaz:
+            load_aaz_command_table(
+                loader=self,
+                aaz_pkg_name=aaz.__name__,
+                args=args
+            )
         load_command_table(self, args)
         return self.command_table
 
