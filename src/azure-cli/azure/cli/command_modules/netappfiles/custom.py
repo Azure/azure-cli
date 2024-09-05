508a28ce24039928e0df584e436d7f80002db848
Audunn Baldvinsson <audunn.baldvinsson@netapp.com>
2024-09-05 09:24:53 +0000

    add --vnet again to avoid breaking2

--- src/azure-cli/azure/cli/command_modules/netappfiles/custom.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/custom.py
@@ -421,7 +421,7 @@ class VolumeCreate(_VolumeCreate):
 class VolumeUpdate(_VolumeUpdate):
     @classmethod
     def _build_arguments_schema(cls, *args, **kwargs):
-        from azure.cli.core.aaz import AAZIntArgFormat
+        from azure.cli.core.aaz import AAZIntArgFormat, AAZStrArg
         args_schema = super()._build_arguments_schema(*args, **kwargs)
         args_schema.vnet = AAZStrArg(
             options=["--vnet"],
