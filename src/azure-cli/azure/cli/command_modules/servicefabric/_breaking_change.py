6b1979bc4b8d8bb3cae335abe4a03cbada799925
Mwesigwa Guma (from Dev Box) <mwesigwaguma@microsoft.com>
2024-09-23 17:06:24 -0400

    mwes_announce_bc

--- src/azure-cli/azure/cli/command_modules/servicefabric/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/servicefabric/_breaking_change.py
@@ -5,22 +5,29 @@
 
 from azure.cli.core.breaking_change import register_command_group_deprecate, register_command_deprecate
 register_command_group_deprecate('sf cluster setting', redirect='sf cluster update')
-# Warning Message: This command group has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command group has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
 
 register_command_group_deprecate('sf cluster upgrade-type', redirect='sf cluster update')
-# Warning Message: This command group has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command group has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
 
 register_command_deprecate('sf cluster durability', redirect='sf cluster update')
-# Warning Message: This command has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
 
 register_command_deprecate('sf cluster reliability update', redirect='sf cluster update')
-# Warning Message: This command has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
 
 register_command_deprecate('sf cluster setting remove', redirect='sf cluster update')
-# Warning Message: This command has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
 
 register_command_deprecate('sf cluster setting set', redirect='sf cluster update')
-# Warning Message: This command has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
 
 register_command_deprecate('sf cluster upgrade-type set', redirect='sf cluster update')
-# Warning Message: This command has been deprecated and will be removed in next breaking change release. Use `sf cluster update` instead.
+# Warning Message: This command has been deprecated and will be removed in next breaking change release.
+# Use `sf cluster update` instead.
