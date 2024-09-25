2f467a85207b4dd785e6d5c186abf9e0dffbc6e5
Jing-song <v-jingszhang@microsoft.com>
2024-09-25 15:22:14 +0800

    update

--- src/azure-cli-testsdk/azure/cli/testsdk/patches.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/patches.py
@@ -85,10 +85,10 @@ def patch_long_run_operation_delay(unit_test):
         return
 
     mock_in_unit_test(unit_test,
-                      'msrestazure.azure_operation.AzureOperationPoller._delay',
+                      'azure.core.polling.LROPoller._delay',
                       _shortcut_long_run_operation)
     mock_in_unit_test(unit_test,
-                      'msrestazure.polling.arm_polling.ARMPolling._delay',
+                      'azure.core.polling.arm_polling.ARMPolling._delay',
                       _shortcut_long_run_operation)
     mock_in_unit_test(unit_test,
                       'azure.cli.core.commands.LongRunningOperation._delay',
--- src/azure-cli-testsdk/azure/cli/testsdk/scenario_tests/patches.py
+++ src/azure-cli-testsdk/azure/cli/testsdk/scenario_tests/patches.py
@@ -18,7 +18,7 @@ def patch_long_run_operation_delay(unit_test):
         return
 
     mock_in_unit_test(unit_test,
-                      'msrestazure.azure_operation.AzureOperationPoller._delay',
+                      'azure.core.polling.LROPoller._delay',
                       _shortcut_long_run_operation)
 
 
