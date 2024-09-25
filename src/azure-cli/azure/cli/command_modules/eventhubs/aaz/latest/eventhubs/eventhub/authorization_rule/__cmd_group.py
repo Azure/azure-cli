183cdb98a05e41e091db53dbaaa50c85b46c6a01
Shubham Chaudhari <schaudhari@microsoft.com>
2024-09-25 11:44:18 +0530

    test recording

--- src/azure-cli/azure/cli/command_modules/eventhubs/aaz/latest/eventhubs/eventhub/authorization_rule/__cmd_group.py
+++ src/azure-cli/azure/cli/command_modules/eventhubs/aaz/latest/eventhubs/eventhub/authorization_rule/__cmd_group.py
@@ -12,7 +12,7 @@ from azure.cli.core.aaz import *
 
 
 @register_command_group(
-    "Manage Azure Service Bus Authorizationrule for Eventhub.",
+    "eventhubs eventhub authorization-rule",
 )
 class __CMDGroup(AAZCommandGroup):
     """Eventhub authorization rule.
