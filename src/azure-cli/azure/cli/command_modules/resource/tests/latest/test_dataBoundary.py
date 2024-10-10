eefe1665950132ee9b51a27c8a0cf3eb6c5c1cd0
Subhashni Kadhiresan <skadhiresan@microsoft.com>
2024-10-10 10:36:01 -0700

    added passing tests

--- src/azure-cli/azure/cli/command_modules/resource/tests/latest/test_dataBoundary.py
+++ src/azure-cli/azure/cli/command_modules/resource/tests/latest/test_dataBoundary.py
@@ -4,13 +4,14 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.testsdk import ScenarioTest
+from azure.core.exceptions import HttpResponseError
 
 class AzureDataBoundaryScenarioTest(ScenarioTest):
 
     def test_get_data_boundary_tenant(self):
         self.cmd('az data-boundary show-tenant --default default', checks=[
-            self.check('dataBoundary', 'EU'),
-            self.check('properties.provisioningState', 'Succeeded')
+            self.check('properties.dataBoundary', 'EU'),
+            self.check('properties.provisioningState', 'Created')
         ])
 
     def test_get_data_boundary_scope(self):
@@ -19,12 +20,12 @@ class AzureDataBoundaryScenarioTest(ScenarioTest):
             **self.kwargs)
         
         self.cmd('az data-boundary show --scope {scope} --default default', checks=[
-            self.check('dataBoundary', 'EU'),
+            self.check('properties.dataBoundary', 'EU'),
             self.check('properties.provisioningState', 'Succeeded')
         ])
 
     def test_put_data_boundary(self):
-        self.cmd('az data-boundary create --data-boundary EU --default default', checks=[
-            self.check('dataBoundary', 'EU'),
-            self.check('properties.provisioningState', 'Succeeded')
-        ])
+        with self.assertRaises(HttpResponseError) as err:
+            self.cmd('az data-boundary create --data-boundary EU --default default')
+            self.assertTrue('does not have authorization to perform action' in str(err.exception))
+            self.assertTrue('or the scope is invalid. If access was recently granted, please refresh your credentials.' in str(err.exception))
