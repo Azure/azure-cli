8d82dcea1ad188546fee0ac15f1e62285362d119
skadhiresan <165337276+skadhiresan@users.noreply.github.com>
2024-10-13 19:53:24 -0700

    [ARM] `az data-boundary` Add DataBoundary Cmdlets (#29961)

--- src/azure-cli/azure/cli/command_modules/resource/aaz/latest/data_boundary/__cmd_group.py
+++ src/azure-cli/azure/cli/command_modules/resource/aaz/latest/data_boundary/__cmd_group.py
@@ -8,3 +8,16 @@
 # pylint: skip-file
 # flake8: noqa
 
+from azure.cli.core.aaz import *
+
+
+@register_command_group(
+    "data-boundary",
+)
+class __CMDGroup(AAZCommandGroup):
+    """data boundary operations
+    """
+    pass
+
+
+__all__ = ["__CMDGroup"]
