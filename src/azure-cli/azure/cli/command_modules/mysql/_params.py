71df241aa082ff30ad63830495517ed32ba2f128
Haoran Hong <haoranhong@microsoft.com>
2024-10-23 17:07:12 +0800

    resolve linter error

--- src/azure-cli/azure/cli/command_modules/mysql/_params.py
+++ src/azure-cli/azure/cli/command_modules/mysql/_params.py
@@ -124,7 +124,7 @@ def load_arguments(self, _):    # pylint: disable=too-many-statements, too-many-
 
     maintenance_policy_patch_strategy_arg_type = CLIArgumentType(
         arg_type=get_enum_type(['Regular', 'VirtualCanary']),
-        options_list=['--maintenance-policy-patch-strategy'],
+        options_list=['--maintenance-policy-patch-strategy', '--patch-strategy'],
         help='The patch strategy of maintenance policy. Accepted values: Regular, VirtualCanary. Default value is Regular.'
     )
 
