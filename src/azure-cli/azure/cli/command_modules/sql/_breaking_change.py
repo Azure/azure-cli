e198e9242d8c15fc2cf89e29eb7b06e5893d88ff
jovancevic123 <jojovancevic@microsoft.com>
2024-10-24 11:47:48 +0200

    Linter errors fix.

--- src/azure-cli/azure/cli/command_modules/sql/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/sql/_breaking_change.py
@@ -5,10 +5,11 @@
 
 from azure.cli.core.breaking_change import register_output_breaking_change, register_argument_deprecate
 
-
 register_argument_deprecate('sql mi link create', '--target-database', '--databases')
-register_argument_deprecate('sql mi link create', '--primary-availability-group-name', '--partner-availability-group-name')
-register_argument_deprecate('sql mi link create', '--secondary-availability-group-name', '--instance-availability-group-name')
+register_argument_deprecate('sql mi link create', '--primary-availability-group-name',
+                            '--partner-availability-group-name')
+register_argument_deprecate('sql mi link create', '--secondary-availability-group-name',
+                            '--instance-availability-group-name')
 register_argument_deprecate('sql mi link create', '--source-endpoint', '--partner-endpoint')
 register_output_breaking_change('sql mi link create',
                                 description='Deprecated output properties: targetDatabase, '
@@ -31,7 +32,7 @@ register_output_breaking_change('sql mi link show',
                                             ' distributedAvailabilityGroupName, instanceLinkRole,'
                                             ' partnerLinkRole, failoverMode, seedingMode.',
                                 doc_link='aka.ms/mi-link-rest-api-get')
-                              
+
 register_output_breaking_change('sql mi link list',
                                 description='Deprecated output properties: targetDatabase, '
                                             'primaryAvailabilityGroupName, secondaryAvailabilityGroupName'
@@ -42,7 +43,7 @@ register_output_breaking_change('sql mi link list',
                                             ' distributedAvailabilityGroupName, instanceLinkRole,'
                                             ' partnerLinkRole, failoverMode, seedingMode.',
                                 doc_link='aka.ms/mi-link-rest-api-list-by-instance')
-          
+
 register_output_breaking_change('sql mi link update',
                                 description='Deprecated output properties: targetDatabase, '
                                             'primaryAvailabilityGroupName, secondaryAvailabilityGroupName'
