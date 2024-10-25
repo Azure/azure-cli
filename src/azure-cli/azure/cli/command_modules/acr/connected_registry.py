b434935f3ab725a2ce28e448feb5ab6471247341
Mai Nguyen (from Dev Box) <nguyenma@microsoft.com>
2024-10-25 10:23:19 -0700

    add wait for result

--- src/azure-cli/azure/cli/command_modules/acr/connected_registry.py
+++ src/azure-cli/azure/cli/command_modules/acr/connected_registry.py
@@ -253,7 +253,7 @@ def acr_connected_registry_delete(cmd,
     try:
         connected_registry = acr_connected_registry_show(
             cmd, client, connected_registry_name, registry_name, resource_group_name)
-        result = client.begin_delete(resource_group_name, registry_name, connected_registry_name)
+        result = client.begin_delete(resource_group_name, registry_name, connected_registry_name).result()
         sync_token = get_token_from_id(cmd, connected_registry.parent.sync_properties.token_id)
         sync_token_name = sync_token.name
         sync_scope_map_name = sync_token.scope_map_id.split('/scopeMaps/')[1]
