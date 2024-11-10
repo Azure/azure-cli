311343c61804bad9912aed66c33ebedefcb26c14
Naia Scott <nasc@microsoft.com>
2024-11-10 17:54:36 +0530

    byok and geo

--- src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_postgres.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/flexible_server_custom_postgres.py
@@ -635,12 +635,15 @@ def flexible_server_georestore(cmd, client, resource_group_name, server_name, so
 
     pg_byok_validator(byok_identity, byok_key, backup_byok_identity, backup_byok_key, geo_redundant_backup)
 
+    storage = postgresql_flexibleservers.models.Storage(type=None)
+
     parameters = postgresql_flexibleservers.models.Server(
         point_in_time_utc=get_current_time(),
         location=location,
         source_server_resource_id=source_server_id,
         create_mode="GeoRestore",
-        availability_zone=zone
+        availability_zone=zone,
+        storage=storage
     )
 
     if source_server_object.network.public_network_access == 'Disabled':
