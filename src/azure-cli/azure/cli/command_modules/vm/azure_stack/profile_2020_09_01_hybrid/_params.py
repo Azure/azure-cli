33caf115381b62c9f0398fa2cbb56468491244a8
Yan Zhu <zhuyan@microsoft.com>
2024-11-11 17:05:40 +0800

    detach azure stack from profiles

---
+++ src/azure-cli/azure/cli/command_modules/vm/azure_stack/profile_2020_09_01_hybrid/_params.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+# pylint: disable=unused-argument, too-many-locals, too-many-branches, too-many-statements
+def load_arguments(self, _):
+    pass
