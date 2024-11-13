09b59cb1706d61fe5f12c95d68c729821bdb9524
Mads Jensen <atombrella@users.noreply.github.com>
2024-11-13 10:14:20 +0100

    Remove pylint markers

--- src/azure-cli/azure/cli/command_modules/mysql/custom.py
+++ src/azure-cli/azure/cli/command_modules/mysql/custom.py
@@ -1991,7 +1991,7 @@ def migrate_firewall_rules_from_single_to_flex(db_context, cmd, source_server_id
                              firewall_rule_name=rule.name)
 
 
-# pylint: disable=too-many-instance-attributes, too-few-public-methods, useless-object-inheritance
+# pylint: disable=too-many-instance-attributes, too-few-public-methods
 class DbContext:
     def __init__(self, cmd=None, azure_sdk=None, logging_name=None, cf_firewall=None, cf_db=None,
                  cf_availability=None, cf_availability_without_location=None, cf_private_dns_zone_suffix=None,
