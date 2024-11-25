e288f1cfbd219154b18c4f24980554643ea55402
Yan Zhu <zhuyan@microsoft.com>
2024-11-25 11:10:53 +0800

    fix

--- src/azure-cli/azure/cli/command_modules/vm/azure_stack/profile_2017_03_09_profile/commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/azure_stack/profile_2017_03_09_profile/commands.py
@@ -3,7 +3,13 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 # pylint: disable=unused-argument, too-many-locals, too-many-branches, too-many-statements
+from azure.cli.command_modules.vm.azure_stack._format import (
+    transform_vmss_list_without_zones_table_output)
+
+from .operations._util import import_aaz_by_profile
 
 
 def load_command_table(self, _):
-    pass
+    VMSS = import_aaz_by_profile("vmss")
+    self.command_table['vmss list'] = VMSS.List(loader=self,
+                                                table_transformer=transform_vmss_list_without_zones_table_output)
