b25b3412f56235cc66b8b9bc49a5c55bf3e0655c
Hang Lei <hanglei@microsoft.com>
2024-12-03 14:07:48 +0800

    Minor fix

--- src/azure-cli/azure/cli/command_modules/vm/azure_stack/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/azure_stack/custom.py
@@ -5863,7 +5863,7 @@ def set_vm_applications(cmd, vm_name, resource_group_name, application_version_i
         index = 0
         for treat_as_failure in treat_deployment_as_failure:
             vm.application_profile.gallery_applications[index].treat_failure_as_deployment_failure = \
-                (treat_as_failure.lower() == 'true')
+                treat_as_failure.lower() == 'true'
             index += 1
     return sdk_no_wait(no_wait, client.virtual_machines.begin_create_or_update, resource_group_name, vm_name, vm)
 
@@ -5907,7 +5907,7 @@ def set_vmss_applications(cmd, vmss_name, resource_group_name, application_versi
         index = 0
         for treat_as_failure in treat_deployment_as_failure:
             vmss.virtual_machine_profile.application_profile.gallery_applications[
-                index].treat_failure_as_deployment_failure = (treat_as_failure.lower() == 'true')
+                index].treat_failure_as_deployment_failure = treat_as_failure.lower() == 'true'
             index += 1
     return sdk_no_wait(no_wait, client.virtual_machine_scale_sets.begin_create_or_update, resource_group_name,
                        vmss_name, vmss)
