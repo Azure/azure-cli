2302cef91b7f505aa42cc397bf9092400e87ea5a
Mads Jensen <mje@inducks.org>
2024-12-09 11:22:01 +0100

    {Misc} Remove Python 3.8 typing fallbacks (#30473)

--- src/azure-cli/azure/cli/command_modules/keyvault/vendored_sdks/azure_keyvault_securitydomain/_internal/async_challenge_auth_policy.py
+++ src/azure-cli/azure/cli/command_modules/keyvault/vendored_sdks/azure_keyvault_securitydomain/_internal/async_challenge_auth_policy.py
@@ -14,6 +14,7 @@ requirements can change. For example, a vault may move to a new tenant. In such 
 protocol again.
 """
 
+from collections.abc import Awaitable
 from copy import deepcopy
 import sys
 import time
@@ -31,11 +32,6 @@ from azure.core.rest import AsyncHttpResponse, HttpRequest
 from .http_challenge import HttpChallenge
 from . import http_challenge_cache as ChallengeCache
 from .challenge_auth_policy import _enforce_tls, _has_claims, _update_challenge
-
-if sys.version_info < (3, 9):
-    from typing import Awaitable
-else:
-    from collections.abc import Awaitable
 
 
 P = ParamSpec("P")
