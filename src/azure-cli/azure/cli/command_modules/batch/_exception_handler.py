1ca0e35c7bb7cffa6a40e7a42c9d7b7693a1db7e
Rena Chen <rechen@microsoft.com>
2024-12-10 17:43:56 -0800

    wip transformers and exception handler

--- src/azure-cli/azure/cli/command_modules/batch/_exception_handler.py
+++ src/azure-cli/azure/cli/command_modules/batch/_exception_handler.py
@@ -5,24 +5,25 @@
 
 from knack.util import CLIError
 
-
+# for data plane this should work for custom code
 def batch_exception_handler(ex):
     from msrest.exceptions import ValidationError, ClientRequestError
     from azure.core.exceptions import HttpResponseError
-    #from azure.batch.models import BatchErrorException
-    ''' if isinstance(ex, BatchErrorException):
+    if isinstance(ex, HttpResponseError): # make sure the default httpresponseerror works for mgmt plane
         try:
-            message = ex.error.message.value
-            if ex.error.values:
-                for detail in ex.error.values:
+            message = ex.model.message.value
+            if ex.model.values_property:
+                for detail in ex.model.values_property:
                     message += f"\n{detail.key}: {detail.value}"
             raise CLIError(message)
-        except AttributeError:
+        except AttributeError: 
             raise CLIError(ex)
-    elif isinstance(ex, (ValidationError, ClientRequestError)):'''
-    if isinstance(ex, (ValidationError, ClientRequestError)):
-        raise CLIError(ex)
-    elif isinstance(ex, HttpResponseError):
+    elif isinstance(ex, (ValidationError, ClientRequestError)):
         raise CLIError(ex)
     else:
         raise ex
+
+# custom.py
+# list jobs should work for custom command check
+
+# also do a mgmt plane code check to make sure it raises errors correctly
