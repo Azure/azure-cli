51209b9286ea70f2a8cfd002c8f0f87418abb37f
Rena Chen <rechen@microsoft.com>
2024-12-19 14:53:30 -0800

    Update exception handling

--- src/azure-cli/azure/cli/command_modules/batch/_exception_handler.py
+++ src/azure-cli/azure/cli/command_modules/batch/_exception_handler.py
@@ -5,25 +5,19 @@
 
 from knack.util import CLIError
 
-# for data plane this should work for custom code
 def batch_exception_handler(ex):
     from msrest.exceptions import ValidationError, ClientRequestError
     from azure.core.exceptions import HttpResponseError
-    if isinstance(ex, HttpResponseError): # make sure the default httpresponseerror works for mgmt plane
-        try:
+    if isinstance(ex, HttpResponseError):
+        if hasattr(ex, "model"):
             message = ex.model.message.value
             if ex.model.values_property:
                 for detail in ex.model.values_property:
                     message += f"\n{detail.key}: {detail.value}"
             raise CLIError(message)
-        except AttributeError: 
-            raise CLIError(ex)
+        raise CLIError(ex)
     elif isinstance(ex, (ValidationError, ClientRequestError)):
         raise CLIError(ex)
     else:
-        raise ex
-
-# custom.py
-# list jobs should work for custom command check
-
-# also do a mgmt plane code check to make sure it raises errors correctly
+        raise CLIError(ex)
+    
