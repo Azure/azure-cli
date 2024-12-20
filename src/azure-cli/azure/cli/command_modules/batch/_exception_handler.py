3bbe07a4c6f76788dee759288703fddcb5e43f04
David Watrous <dave.watrous@microsoft.com>
2024-12-20 16:06:49 -0500

    Fix exception handler and add tests

--- src/azure-cli/azure/cli/command_modules/batch/_exception_handler.py
+++ src/azure-cli/azure/cli/command_modules/batch/_exception_handler.py
@@ -4,20 +4,26 @@
 # --------------------------------------------------------------------------------------------
 
 from knack.util import CLIError
+from azure.batch.models import BatchError
 
 def batch_exception_handler(ex):
     from msrest.exceptions import ValidationError, ClientRequestError
     from azure.core.exceptions import HttpResponseError
+
     if isinstance(ex, HttpResponseError):
-        if hasattr(ex, "model"):
-            message = ex.model.message.value
-            if ex.model.values_property:
-                for detail in ex.model.values_property:
-                    message += f"\n{detail.key}: {detail.value}"
-            raise CLIError(message)
-        raise CLIError(ex)
+        if ex.model and isinstance(ex, BatchError) and ex.model.code:
+            _handle_batch_exception(ex.model)
     elif isinstance(ex, (ValidationError, ClientRequestError)):
         raise CLIError(ex)
-    else:
-        raise CLIError(ex)
     
+    raise CLIError(ex)
+
+def _handle_batch_exception(err):
+    """Handle a BatchError from the data plane and raise a CLI exception"""
+    message = f"({err.code})"
+    if err.message and err.message.value:
+        message += f" {err.message.value}"
+    if err.values_property:
+        for detail in err.values_property:
+            message += f"\n{detail.key}: {detail.value}"
+    return CLIError(message)
---
+++ src/azure-cli/azure/cli/command_modules/batch/tests/latest/test_batch_exception_handler.py
@@ -0,0 +1,63 @@
+import unittest
+
+from azure.cli.command_modules.batch._exception_handler import batch_exception_handler
+from azure.core.exceptions import HttpResponseError
+from azure.batch.models import BatchError, BatchErrorMessage, BatchErrorDetail
+
+from knack.util import CLIError
+
+
+class TestBatchExceptionHandler(unittest.TestCase):
+
+    def test_response_err(self):
+        err = HttpResponseError("Kaboom")
+        with self.assertRaises(CLIError, msg="Kaboom"):
+            batch_exception_handler(err)
+    
+    def test_response_err_empty_model(self):
+        err = HttpResponseError("Kaboom")
+        err.model = BatchError()
+        with self.assertRaises(CLIError, msg="Kaboom"):
+            batch_exception_handler(err)
+
+    def test_response_err_code_only(self):
+        err = HttpResponseError("Kaboom")
+        err.model = BatchError(code="explosion")
+        with self.assertRaises(CLIError, msg="(explosion)"):
+            batch_exception_handler(err)
+
+    def test_response_err_msg_and_code(self):
+        err = HttpResponseError("Kaboom")
+        err.model = BatchError(code="explosion", message=BatchErrorMessage(lang="en-us", value="Blew up"))
+        with self.assertRaises(CLIError, msg="(explosion) Blew up"):
+            batch_exception_handler(err)
+
+    def test_response_err_details(self):
+        err = HttpResponseError("Kaboom")
+
+        err_details = []
+
+        detail1 = BatchErrorDetail()
+        detail1.key = "key1"
+        detail1.value = "value1"
+        err_details.append(detail1)
+
+        detail2 = BatchErrorDetail()
+        detail2.value = "value2"
+        err_details.append(detail2)
+
+        detail3 = BatchErrorDetail()
+        detail3.key = "key3"
+        err_details.append(detail3)
+
+        detail4 = BatchErrorDetail()
+        err_details.append(detail4)
+
+        err.model = BatchError(
+            code="explosion",
+            message=BatchErrorMessage(lang="en-us", value="Blew up"),
+            values_property=err_details)
+        # Key/value should never be null in practice, but since they are marked Optional,
+        # the error handler shouldn't fail on nulls.
+        with self.assertRaises(CLIError, msg="(explosion) Blew up \nkey1: value1\nNone: value2\nkey3: None\nNone: None"):
+            batch_exception_handler(err)
