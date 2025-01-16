198d4c43429f3eb51217428d39941d9a90dca4a7
Hans Strijker <45356184+HStry@users.noreply.github.com>
2025-01-16 18:04:52 +0100

    honor XDG vars
    This commit modifies the `get_config_dir` function to honor the XDG Base Directory Specification in accordance with https://specifications.freedesktop.org/basedir-spec/latest/, and falls back to a dot-dir in the user's home directory if the relevant environment variables and home directories do not exist.

--- src/azure-cli-core/azure/cli/core/_environment.py
+++ src/azure-cli-core/azure/cli/core/_environment.py
@@ -10,4 +10,20 @@ _ENV_AZ_BICEP_GLOBALIZATION_INVARIANT = 'AZ_BICEP_GLOBALIZATION_INVARIANT'
 
 def get_config_dir():
     import os
-    return os.getenv('AZURE_CONFIG_DIR', None) or os.path.expanduser(os.path.join('~', '.azure'))
+    def sanitize(*d):
+        return os.path.expanduser(os.path.expandvars(os.path.join(*d)))
+    
+    # Directory set in env has precedence
+    if env := os.getenv('AZURE_CONFIG_DIR'):
+        return sanitize(env)
+    
+    # Honor XDG variables
+    if env := os.getenv('XDG_CONFIG_HOME'):
+        return sanitize(env, 'azure')
+    
+    # Or the default fallover directory conform the XDG Base Directory Specification
+    if os.path.isdir(sanitize('~', '.config')):
+        return sanitize('~', '.config', 'azure')
+    
+    # If all others fail, return a dot-dir in the user's home directory.
+    return sanitize('~', '.azure')
