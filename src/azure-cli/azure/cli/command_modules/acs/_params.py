cf4ed159c2f7a08deea1fe158335126517aca283
skuchipudi295 <128435195+skuchipudi295@users.noreply.github.com>
2025-02-04 12:32:38 -0800

    Update src/azure-cli/azure/cli/command_modules/acs/_params.py
    Co-authored-by: FumingZhang <81607949+FumingZhang@users.noreply.github.com>

--- src/azure-cli/azure/cli/command_modules/acs/_params.py
+++ src/azure-cli/azure/cli/command_modules/acs/_params.py
@@ -683,8 +683,8 @@ def load_arguments(self, _):
         c.argument('node_name')
 
     with self.argument_context('aks machine') as c:
-        c.argument('cluster_name', help='The cluster name.')
-        c.argument('nodepool_name', validator=validate_nodepool_name, help='The node pool name.')
+        c.argument('cluster_name')
+        c.argument('nodepool_name', validator=validate_nodepool_name)
 
     with self.argument_context('aks machine show') as c:
         c.argument('machine_name')
