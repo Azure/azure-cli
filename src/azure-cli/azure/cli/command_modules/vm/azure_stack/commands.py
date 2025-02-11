f6aa010ea91f6b3ed70f59bbf89ee9e3630ab27d
Yan Zhu <zhuyan@microsoft.com>
2025-02-11 14:34:11 +0800

    migrate to codegen

--- src/azure-cli/azure/cli/command_modules/vm/azure_stack/commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/azure_stack/commands.py
@@ -10,7 +10,6 @@ from azure.cli.command_modules.vm.azure_stack._client_factory import (cf_vm, cf_
                                                                       cf_disk_accesses, cf_images, cf_run_commands,
                                                                       cf_gallery_images,
                                                                       cf_gallery_image_versions,
-                                                                      cf_proximity_placement_groups,
                                                                       cf_dedicated_hosts, cf_dedicated_host_groups,
                                                                       cf_log_analytics_data_plane,
                                                                       cf_disk_encryption_set,
@@ -35,7 +34,7 @@ from azure.cli.command_modules.vm.azure_stack._validators import (
     process_remove_identity_namespace, process_vm_secret_format, process_vm_vmss_stop, validate_vmss_update_namespace,
     process_vm_update_namespace, process_set_applications_namespace, process_vm_disk_attach_namespace,
     process_image_version_create_namespace, process_image_version_update_namespace,
-    process_image_version_undelete_namespace, process_ppg_create_namespace, process_vm_disk_detach_namespace)
+    process_image_version_undelete_namespace, process_vm_disk_detach_namespace)
 
 from azure.cli.command_modules.vm.azure_stack._image_builder import (
     process_image_template_create_namespace, process_img_tmpl_output_add_namespace,
@@ -152,10 +151,6 @@ def load_command_table(self, _):
     compute_gallery_application_version_sdk = CliCommandType(
         operations_tmpl='azure.mgmt.compute.operations#GalleryApplicationVersionsOperations.{}',
         client_factory=cf_gallery_application_version,
-    )
-
-    compute_proximity_placement_groups_sdk = CliCommandType(
-        operations_tmpl='azure.mgmt.compute.operations#ProximityPlacementGroupsOperations.{}',
     )
 
     compute_dedicated_host_sdk = CliCommandType(
@@ -495,11 +490,6 @@ def load_command_table(self, _):
     with self.command_group('sig gallery-application version', compute_gallery_application_version_sdk, client_factory=cf_gallery_application_version, min_api='2021-07-01', operation_group='gallery_application_versions') as g:
         g.custom_command('create', 'gallery_application_version_create', supports_no_wait=True)
         g.custom_command('update', 'gallery_application_version_update', supports_no_wait=True)
-
-    with self.command_group('ppg', compute_proximity_placement_groups_sdk, min_api='2018-04-01', client_factory=cf_proximity_placement_groups) as g:
-        g.custom_command('create', 'create_proximity_placement_group', validator=process_ppg_create_namespace)
-        g.custom_command('list', 'list_proximity_placement_groups')
-        g.generic_update_command('update', setter_name='create_or_update', custom_func_name='update_ppg')
 
     with self.command_group('vm monitor log', client_factory=cf_log_analytics_data_plane) as g:
         g.custom_command('show', 'execute_query_for_vm', transform=transform_log_analytics_query_output)  # pylint: disable=show-command
