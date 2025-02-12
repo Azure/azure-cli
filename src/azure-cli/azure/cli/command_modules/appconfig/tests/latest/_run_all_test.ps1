698766e7575f56ecce6e916c41e6031190ca57c9
Christine WANJAU <cwanjau@microsoft.com>
2025-02-12 13:30:40 +0300

    add powershell file to run all tests locally

---
+++ src/azure-cli/azure/cli/command_modules/appconfig/tests/latest/_run_all_test.ps1
@@ -0,0 +1,55 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# Run all tests for the appconfig module locally and clean up resources if needed
+
+[CmdletBinding()]
+Param(
+    [Parameter()]
+    [ValidateSet('clean_up')]
+    [string]$clean_up,
+
+    [Parameter()]
+    [string]$resource_group_name = "Cli-local-test"
+)
+
+# Set rg for local testing
+$env:AZURE_CLI_TEST_DEV_RESOURCE_GROUP_NAME=$resource_group_name
+
+# Run tests
+azdev test appconfig --live
+
+function clean_up_resources {
+    param (
+        [string]$rgName
+    )
+
+    # List all resources in the Resource Group
+    $resources = az resource list --resource-group $rgName --query "[].id" -o tsv
+
+    if ($resources -eq "") {
+        Write-Host "No resources found in resource group $rgName."
+        return
+    }
+
+    # Delete resources
+    foreach ($resourceId in $resources) {
+        Write-Host "Deleting resource: $resourceId"
+        
+        try {
+            # Delete the resource using its ID
+            az resource delete --ids $resourceId --no-wait
+            Write-Host "Successfully deleted resource: $resourceId"
+        } catch {
+            Write-Host "Failed to delete resource: $resourceId. Error: $_"
+        }
+    }
+}
+
+# Clean up 
+if ($clean_up -eq 'clean_up') {
+    Write-Host "Cleaning up resources"
+    clean_up_resources -rgName $resource_group_name
+}
