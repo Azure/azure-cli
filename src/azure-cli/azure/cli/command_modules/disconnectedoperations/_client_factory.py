fdb30b00a11e5abf59015d74d8670984825d1de0
Sankalp Kotewar <skotewar@microsoft.com>
2025-02-12 08:59:12 +0530

    initial commit

---
+++ src/azure-cli/azure/cli/command_modules/disconnectedoperations/_client_factory.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+def get_disconnectedoperations_management_client(cli_ctx, *_):
+    from azure.cli.core.commands.client_factory import get_mgmt_service_client
+    from azure.mgmt.disconnectedoperations import DisconnectedOperationsClient
+    return get_mgmt_service_client(cli_ctx, DisconnectedOperationsClient)
+
+
+def cf_image(cli_ctx, *_):
+    return get_disconnectedoperations_management_client(cli_ctx).image
+
+def cf_logos(cli_ctx, *_):
+    return get_disconnectedoperations_management_client(cli_ctx).logos
+
+def cf_metadata(cli_ctx, *_):
+    return get_disconnectedoperations_management_client(cli_ctx).metadata
+
