03c3642802f5f6fe8205dfb63958d174c6fd0262
Shenglong Li <shenglol@microsoft.com>
2025-03-14 18:35:51 -0700

    Add license header

--- src/azure-cli/azure/cli/command_modules/resource/_bicep_config.py
+++ src/azure-cli/azure/cli/command_modules/resource/_bicep_config.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 _config_section = "bicep"
 
 _use_binary_from_path_config_key = "use_binary_from_path"
