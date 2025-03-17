c41906d1079c5a96a2a7965e5dd00a3422a3239e
Mitchell Bifeld <mbifeld@gmail.com>
2025-03-17 17:55:43 -0500

    Create _breaking_change.py for node-vm-size announcement

---
+++ src/azure-cli/azure/cli/command_modules/acs/_breaking_change.py
@@ -0,0 +1,9 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------
+
+from azure.cli.core.breaking_change import register_default_value_breaking_change
+
+register_default_value_breaking_change('aks create', '--node-vm-size', 'Standard_DS2_V2 (Linux), Standard_DS2_V3 (Windows)', 'Dynamically Selected By Azure')
+# The default value of `--node-vm-size` will be changed to `namically Selected By Azure` from `Standard_DS2_V2 (Linux), Standard_DS2_V3 (Windows)` in next breaking change release(2.xx.0).
