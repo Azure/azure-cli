74b47c992ccfaeffa79f5380393bbca0d0a30f7e
anujmaheshwari1 <amaheshwari@microsoft.com>
2025-03-21 16:31:40 -0700

    test: change location to eastus2euap

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_aks_commands.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_aks_commands.py
@@ -3296,7 +3296,7 @@ class AzureKubernetesServiceScenarioTest(ScenarioTest):
             'aks delete -g {resource_group} -n {name} --yes --no-wait', checks=[self.is_empty()])
 
     @AllowLargeResponse()
-    @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='eastus')
+    @AKSCustomResourceGroupPreparer(random_name_length=17, name_prefix='clitest', location='eastus2euap')
     def test_aks_nodepool_add_with_ossku_ubuntu2204(self, resource_group, resource_group_location):
         aks_name = self.create_random_name('cliakstest', 16)
         node_pool_name = self.create_random_name('c', 6)
