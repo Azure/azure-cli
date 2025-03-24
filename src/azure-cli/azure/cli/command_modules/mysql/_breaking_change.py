cf8410baf3c71fef4dc237ca705bd0db356bc792
linyudoa <50021265+linyudoa@users.noreply.github.com>
2025-03-24 14:21:07 +0800

    fix pylint

--- src/azure-cli/azure/cli/command_modules/mysql/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/mysql/_breaking_change.py
@@ -4,5 +4,11 @@
 # --------------------------------------------------------------------------------------------
 from azure.cli.core.breaking_change import register_logic_breaking_change
 
-register_logic_breaking_change('mysql flexible-server create', 'Update the validator', detail='the argument `--high-availability` will no longer accept value `SameZone` for new servers created in Business-Critical service tier if the region supports multiple zones.')
-register_logic_breaking_change('mysql flexible-server update', 'Update the validator', detail='the argument `--high-availability` will no longer accept value `SameZone` for new servers created in Business-Critical service tier if the region supports multiple zones.')
+register_logic_breaking_change('mysql flexible-server create', 'Update the validator',
+                               detail='the argument `--high-availability` will no longer accept value `SameZone` for '
+                               'new servers created in Business-Critical service tier '
+                               'if the region supports multiple zones.')
+register_logic_breaking_change('mysql flexible-server update', 'Update the validator',
+                               detail='the argument `--high-availability` will no longer accept value `SameZone` for '
+                               'new servers created in Business-Critical service tier '
+                               'if the region supports multiple zones.')
