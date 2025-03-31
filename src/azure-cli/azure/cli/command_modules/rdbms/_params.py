226ddc35e10afe9f25484bfcf9b57d7d1b5a13c7
Matthew Boentoro <mattboentoro@yahoo.com>
2025-03-30 20:03:44 -0700

    [RDBMS] `az postgres flexible-server upgrade`: Add server capability API check to the `--version` parameter and allow user to upgrade to PG17 when it is available (#31146)
    * {RDBMS} az postgres flexible-server upgrade: Add server capability API check to the --version parameter
    
    * fix help text
    
    ---------
    
    Co-authored-by: Matthew Boentoro <mboentoro@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/rdbms/_params.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_params.py
@@ -459,7 +459,7 @@ def load_arguments(self, _):    # pylint: disable=too-many-statements, too-many-
         )
 
         pg_version_upgrade_arg_type = CLIArgumentType(
-            arg_type=get_enum_type(['12', '13', '14', '15', '16']),
+            arg_type=get_enum_type(['12', '13', '14', '15', '16', '17']),
             options_list=['--version', '-v'],
             help='Server major version.'
         )
