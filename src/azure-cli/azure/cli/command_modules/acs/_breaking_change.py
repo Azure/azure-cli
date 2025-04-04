3f4d075580ef7e94896add2bbdc13bc486488e4b
Fuming Zhang <fumingzhang@microsoft.com>
2025-04-04 16:41:39 +1100

    update

--- src/azure-cli/azure/cli/command_modules/acs/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/acs/_breaking_change.py
@@ -4,6 +4,7 @@
 # --------
 
 from azure.cli.core.breaking_change import (
+    AzCLIOtherChange,
     register_default_value_breaking_change,
     register_conditional_breaking_change,
 )
@@ -17,16 +18,20 @@ register_default_value_breaking_change(
 
 register_conditional_breaking_change(
     tag="aks_create_cluster_autoscaler_profile",
-    breaking_change="The option `--cluster-autoscaler-profile` in command `az aks create` will be changed to "
-    "allow multiple values for the same key, separated by commas. Different key-value pairs will be separated by "
-    "spaces.",
-    command_name="aks create",
+    breaking_change=AzCLIOtherChange(
+        cmd="aks create",
+        message="The option `--cluster-autoscaler-profile` in command `az aks create` will be changed to "
+        "allow multiple values for the same key, separated by commas. Different key-value pairs will be separated by "
+        "spaces.",
+    ),
 )
 
 register_conditional_breaking_change(
     tag="aks_update_cluster_autoscaler_profile",
-    breaking_change="The option `--cluster-autoscaler-profile` in command `az aks update` will be changed to "
-    "allow multiple values for the same key, separated by commas. Different key-value pairs will be separated by "
-    "spaces.",
-    command_name="aks update",
+    breaking_change=AzCLIOtherChange(
+        cmd="aks update",
+        message="The option `--cluster-autoscaler-profile` in command `az aks update` will be changed to "
+        "allow multiple values for the same key, separated by commas. Different key-value pairs will be separated by "
+        "spaces.",
+    ),
 )
