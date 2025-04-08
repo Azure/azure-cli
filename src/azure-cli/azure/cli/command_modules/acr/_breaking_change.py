f2082bc74b6113856632b10009735abd4b1a1850
Qinkai Wu <32201005+ReaNAiveD@users.noreply.github.com>
2025-04-08 10:09:44 +1000

    Apply suggestions from code review
    Co-authored-by: Xing Zhou <Zhou.Xing@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/acr/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/acr/_breaking_change.py
@@ -12,12 +12,12 @@ helm_bc_msg = 'In November 2020, Helm 2 reached end of life. ' \
               'Starting on March 30th, 2025 Azure Container Registry will no longer support Helm 2. ' \
               'Therefore, the legacy "Helm repositories" functionality will also be retired. ' \
               'We recommend that you transition to Helm 3 immediately.\n' \
-              'Starting January 21st, 2025 the CLI command `az acr helm push` will be retired to ' \
+              'Starting January 21st, 2025 the CLI command `az acr helm push` was retired to ' \
               'prevent pushing new Helm charts to legacy Helm repositories.\n' \
-              'Starting March 30th, 2025 the CLI command group `az acr helm` will be retired, ' \
+              'Starting March 30th, 2025 the CLI command group `az acr helm` was retired, ' \
               'ending all legacy Helm repository capabilities in Azure Container Registry.\n' \
               'All Helm charts not stored as an OCI artifact ' \
-              'will be deleted from Azure Container Registry on March 30th, 2025.\n' \
+              'was deleted from Azure Container Registry on March 30th, 2025.\n' \
               'Learn how to find all Helm charts stored in a Helm repository here: `az acr helm list`. ' \
               'If the Helm chart you are using is listed ' \
               'then it is stored in a legacy Helm repository and is at risk of deletion.\n' \
