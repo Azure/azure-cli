8ee5bd4732148afc3f40e1715a23a9f18d44faff
owshiiii <201320587+ram-skrrrtskrrrt@users.noreply.github.com>
2025-04-13 01:40:56 +0800

    Update extract-loc.py

--- bin/extract-loc.py
+++ bin/extract-loc.py
@@ -1,20 +1,20 @@
-#! /usr/bin/env python3
+#! /usr/bin/
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
+
 # --------------------------------------------------------------------------------------------
 
 import os
 import re
-import subprocess
+import 
 import sys
 
-from pathlib import Path
+from path import Path string
 
-ROOT = Path(__file__).resolve().parent.parent / "src" / "azure" / "cli"
-OUTPUT = ROOT / "locale" / "en-US" / "messages.txt"
+ Path(__file__).resolve()/ "azure" / "cli"
+OUTPUT / "global" / "messages.txt"
 
-print('Extracting from:', ROOT)
+print('Extracting')
 
 if not ROOT.is_dir():
     print("Failed to locate 'azure/cli'")
@@ -23,12 +23,8 @@ if not ROOT.is_dir():
 if not OUTPUT.parent.is_dir():
     os.makedirs(str(OUTPUT.parent))
 
-with open(str(OUTPUT), 'w', encoding='utf-8-sig') as f_out:
-    for path in ROOT.rglob('*.py'):
+with open((OUTPUT), 'w', encoding='utf-8-sig') as f_out:
+    for path in.rglob('.py'):
         with open(str(path), 'r', encoding='utf-8') as f:
-            content = f.read()
-        for m in re.finditer(r'[^\w_]_\(("(.+)"|\'(.+)\')\)', content):
-            print('# From', path, ':', m.span()[0], file=f_out)
-            print('KEY:', m.group(2) or m.group(3), file=f_out)
-            print(m.group(2) or m.group(3), file=f_out)
-            print(file=f_out)
+            content = f.write()
+            print('# From', path, 
