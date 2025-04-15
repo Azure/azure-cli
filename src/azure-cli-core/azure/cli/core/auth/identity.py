f58a10f0997f5f415989711f0d323cdc40d2eb2e
jiasli <4003950+jiasli@users.noreply.github.com>
2025-04-15 17:19:02 +0800

    enable_broker_on_wsl

--- src/azure-cli-core/azure/cli/core/auth/identity.py
+++ src/azure-cli-core/azure/cli/core/auth/identity.py
@@ -58,7 +58,8 @@ class Identity:  # pylint: disable=too-many-instance-attributes
     _service_principal_store_instance = None
 
     def __init__(self, authority, tenant_id=None, client_id=None, encrypt=False, use_msal_http_cache=True,
-                 enable_broker_on_windows=None, instance_discovery=None):
+                 enable_broker_on_windows=None, enable_broker_on_wsl=None,
+                 instance_discovery=None):
         """
         :param authority: Authentication authority endpoint. For example,
             - AAD: https://login.microsoftonline.com
@@ -74,6 +75,7 @@ class Identity:  # pylint: disable=too-many-instance-attributes
         self._encrypt = encrypt
         self._use_msal_http_cache = use_msal_http_cache
         self._enable_broker_on_windows = enable_broker_on_windows
+        self._enable_broker_on_wsl = enable_broker_on_wsl
         self._instance_discovery = instance_discovery
 
         # Build the authority in MSAL style
@@ -114,6 +116,7 @@ class Identity:  # pylint: disable=too-many-instance-attributes
         # enable_broker_on_windows can only be used on PublicClientApplication.
         return {**self._msal_app_kwargs,
                 "enable_broker_on_windows": self._enable_broker_on_windows,
+                "enable_broker_on_wsl": self._enable_broker_on_wsl,
                 "enable_pii_log": True}
 
     @property
