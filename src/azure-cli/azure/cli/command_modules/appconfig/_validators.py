a1895c811859d9376c0899c3b135c66d8dfa90e7
Christine WANJAU <cwanjau@microsoft.com>
2025-04-16 09:57:36 +0300

    update warning

--- src/azure-cli/azure/cli/command_modules/appconfig/_validators.py
+++ src/azure-cli/azure/cli/command_modules/appconfig/_validators.py
@@ -389,7 +389,7 @@ def validate_snapshot_import(namespace):
 def validate_sku(namespace):
     if namespace.sku.lower() == 'free':
         if (namespace.enable_purge_protection or namespace.retention_days or namespace.replica_name or namespace.replica_location or namespace.no_replica or namespace.enable_arm_private_network_access):  # pylint: disable=too-many-boolean-expressions
-            logger.warning("Options '--enable-purge-protection', '--replica-name', '--replica-location' , '--no-replica' and '--retention-days' will be ignored when creating a free store.")
+            logger.warning("Options '--enable-purge-protection', '--replica-name', '--replica-location' , '--no-replica' , 'enable-arm-private-network-access' and '--retention-days' will be ignored when creating a free store.")
             namespace.retention_days = None
             namespace.enable_purge_protection = None
             namespace.replica_name = None
