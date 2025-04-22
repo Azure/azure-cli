340a90cf9f2c5d395525cb836fabfde992247f64
Kai Ru <kairu@microsoft.com>
2025-04-22 15:03:41 +0800

    fix tests

--- src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_acr_commands_mock.py
+++ src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_acr_commands_mock.py
@@ -7,9 +7,6 @@ from urllib.parse import urlencode
 import json
 import unittest
 from unittest import mock
-import sys
-
-from azure.mgmt.containerregistry.v2019_05_01.models import Registry, Sku
 
 from azure.cli.command_modules.acr.repository import (
     acr_repository_list,
@@ -1192,6 +1189,7 @@ class AcrMockCommandsTests(unittest.TestCase):
                                    tenant_suffix=test_tenant_suffix)
 
     def _core_token_scenarios(self, mock_get_raw_token, mock_requests_get, mock_requests_post, mock_get_registry_by_name, registry_exists, registry_name, login_server, tenant_suffix):
+        from azure.mgmt.containerregistry.v2024_11_01_preview.models import Registry, Sku
         cmd = self._setup_cmd()
 
         if registry_exists:
