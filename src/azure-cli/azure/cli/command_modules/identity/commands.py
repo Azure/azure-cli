93e4cfebd002a01c48bc4ad3ee90fc204cf136de
Srujan Bandarkar <sbandarkar@microsoft.com>
2025-04-30 12:59:34 -0400

    properly register the federated-credential subcommands

--- src/azure-cli/azure/cli/command_modules/identity/commands.py
+++ src/azure-cli/azure/cli/command_modules/identity/commands.py
@@ -27,21 +27,22 @@ def load_command_table(self, _):
         client_factory=_msi_federated_identity_credentials_operations
     )
 
-    with self.command_group('identity', identity_sdk, client_factory=_msi_user_identities_operations) as g:
-        g.custom_command('create', 'create_identity', validator=process_msi_namespace)
-        g.show_command('show', 'get')
-        g.command('delete', 'delete')
-        g.custom_command('list', 'list_user_assigned_identities')
-        g.custom_command('list-resources', 'list_identity_resources', min_api='2021-09-30-preview')
+    with self.command_group('identity', is_preview=True) as g:
+        g.command_group('', identity_sdk, client_factory=_msi_user_identities_operations) as g2:
+            g2.custom_command('create', 'create_identity', validator=process_msi_namespace)
+            g2.show_command('show', 'get')
+            g2.command('delete', 'delete')
+            g2.custom_command('list', 'list_user_assigned_identities')
+            g2.custom_command('list-resources', 'list_identity_resources', min_api='2021-09-30-preview')
 
-    with self.command_group('identity', msi_operations_sdk, client_factory=_msi_operations_operations) as g:
-        g.command('list-operations', 'list')
+        g.command_group('', msi_operations_sdk, client_factory=_msi_operations_operations) as g2:
+            g2.command('list-operations', 'list')
 
-    with self.command_group('identity federated-credential', federated_identity_credentials_sdk,
-                            client_factory=_msi_federated_identity_credentials_operations,
-                            min_api='2025-01-31-PREVIEW') as g:
-        g.custom_command('create', 'create_or_update_federated_credential')
-        g.custom_command('update', 'create_or_update_federated_credential')
-        g.custom_show_command('show', 'show_federated_credential')
-        g.custom_command('delete', 'delete_federated_credential', confirmation=True)
-        g.custom_command('list', 'list_federated_credential')
+        g.command_group('federated-credential', federated_identity_credentials_sdk,
+                      client_factory=_msi_federated_identity_credentials_operations,
+                      min_api='2025-01-31-PREVIEW') as g2:
+            g2.custom_command('create', 'create_or_update_federated_credential')
+            g2.custom_command('update', 'create_or_update_federated_credential')
+            g2.custom_show_command('show', 'show_federated_credential')
+            g2.custom_command('delete', 'delete_federated_credential', confirmation=True)
+            g2.custom_command('list', 'list_federated_credential')
