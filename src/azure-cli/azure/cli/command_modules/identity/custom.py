8c897c0044347e5eab72e45dec44fe677b85025f
Srujan Bandarkar <sbandarkar@microsoft.com>
2025-04-30 14:31:49 -0400

    [Identity] Add az identity federated-credential create/update: Add support for claims matching expressions with 2025-01-31-PREVIEW API version

--- src/azure-cli/azure/cli/command_modules/identity/custom.py
+++ src/azure-cli/azure/cli/command_modules/identity/custom.py
@@ -5,7 +5,8 @@
 
 from azure.cli.core.profiles import ResourceType
 from azure.cli.core.azclierror import (
-    RequiredArgumentMissingError
+    RequiredArgumentMissingError,
+    MutuallyExclusiveArgumentError
 )
 
 
@@ -35,15 +36,36 @@ def list_identity_resources(cmd, resource_group_name, resource_name):
 
 
 def create_or_update_federated_credential(cmd, client, resource_group_name, identity_name, federated_credential_name,
-                                          issuer=None, subject=None, audiences=None):
+                                          issuer=None, subject=None, audiences=None, claims_matching_expression_value=None, claims_matching_expression_version=None):
     _default_audiences = ['api://AzureADTokenExchange']
     audiences = _default_audiences if not audiences else audiences
-    if not issuer or not subject:
-        raise RequiredArgumentMissingError('usage error: please provide both --issuer and --subject parameters')
+    if not issuer:
+        recommendation = "Please provide the issuer URL using --issuer parameter. For GitHub Actions, use 'https://token.actions.githubusercontent.com'"
+        raise RequiredArgumentMissingError('Issuer URL is required for federated credential creation', recommendation)
+    
+    # When subject is present, CME should be null
+    if subject:
+        claims_matching_expression_value = None
+        claims_matching_expression_version = None
+    elif not claims_matching_expression_value:
+        recommendation = [
+            "Option 1: Provide --subject to create a federated credential with direct subject matching",
+            "Option 2: Provide --claims-matching-expression-value and --claims-matching-expression-version to create a federated credential with claims matching expression"
+        ]
+        raise RequiredArgumentMissingError('Missing required authentication criteria for federated credential', recommendation)
+    
+    if subject and claims_matching_expression_value:
+        recommendation = [
+            "Option 1: Remove --subject to use claims matching expression",
+            "Option 2: Remove --claims-matching-expression-value and --claims-matching-expression-version to use subject"
+        ]
+        raise MutuallyExclusiveArgumentError('Subject and claims matching expression cannot be used together', recommendation)
 
     FederatedIdentityCredential = cmd.get_models('FederatedIdentityCredential', resource_type=ResourceType.MGMT_MSI,
                                                  operation_group='federated_identity_credentials')
-    parameters = FederatedIdentityCredential(issuer=issuer, subject=subject, audiences=audiences)
+    parameters = FederatedIdentityCredential(issuer=issuer, subject=subject, audiences=audiences, 
+                                           claims_matching_expression_value=claims_matching_expression_value,
+                                           claims_matching_expression_version=claims_matching_expression_version)
 
     return client.create_or_update(resource_group_name=resource_group_name, resource_name=identity_name,
                                    federated_identity_credential_resource_name=federated_credential_name,
