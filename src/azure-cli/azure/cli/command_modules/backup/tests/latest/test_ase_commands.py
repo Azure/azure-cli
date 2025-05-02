9d71a274dd87fd8f155a86f5847072c3a4a7aab4
Utkarsh Kumar <kumarutkarsh@microsoft.com>
2025-05-02 17:41:16 +0530

    bkp res policy command tests added

--- src/azure-cli/azure/cli/command_modules/backup/tests/latest/test_ase_commands.py
+++ src/azure-cli/azure/cli/command_modules/backup/tests/latest/test_ase_commands.py
@@ -10,18 +10,20 @@ from azure.cli.testsdk import ScenarioTest, record_only
 import json
 import os
 
-vault_ase_olr = 'ase-rsv-ccy'
-vault_ase_crr = 'ase-rsv-grs'
-container_name_ase = 'VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm2'
-vm2_friendly_name = 'ase-ccy-vm2'
+vault_ase_reg = 'ase-rsv-ccy'
+vault_ase_restore = 'ase-rsv-grs'
+reg_vm_name_ase = 'VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm2'
+res_vm_name_ase = 'VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm5'
+reg_vm_friendly_name = 'ase-ccy-vm2'
+res_vm_friendly_name = 'ase-ccy-vm5'
 vm5_friendly_name = 'ase-ccy-vm5'
 rg_ase = 'ase-rg-ccy'
 item_friendly_name = 'asetestdb1'
 backup_item_name_db1 = 'SAPAseDatabase;ab4;asetestdb1'
 backup_item_name_db2 = 'SAPAseDatabase;ab4;asetestdb2'
-id_ase = '/subscriptions/38304e13-357e-405e-9e9a-220351dcce8c/resourceGroups/ASE-RG-CCY/providers/Microsoft.Compute/virtualMachines/ase-ccy-vm5'
+reg_vm_id = '/subscriptions/38304e13-357e-405e-9e9a-220351dcce8c/resourceGroups/ase-rg-ccy/providers/Microsoft.Compute/virtualMachines/ase-ccy-vm2'
 
-# id_ase = '/subscriptions/38304e13-357e-405e-9e9a-220351dcce8c/resourceGroups/ASE-RG-CCY/providers/Microsoft.Compute/virtualMachines/ase-ccy-vm2'
+# reg_vm_id = '/subscriptions/38304e13-357e-405e-9e9a-220351dcce8c/resourceGroups/ASE-RG-CCY/providers/Microsoft.Compute/virtualMachines/ase-ccy-vm2'
 # sub_ase = '38304e13-357e-405e-9e9a-220351dcce8c'
 # server_friendly_name = 'ase-ccy-vm2'
 # item_name = 'SAPAseDatabase;ab4;asetestdb2'
@@ -31,48 +33,64 @@ class ASEBackupTests(ScenarioTest, unittest.TestCase):
 
     def test_backup_wl_ase_container(self):
         self.kwargs.update({
-            'vault': vault_ase_olr,
-            'vm': container_name_ase,
-            'vm2_friendly_name': vm2_friendly_name,
-            'vm5_friendly_name': vm5_friendly_name,
+            'vault': vault_ase_restore,
+            'vm_full_name': res_vm_name_ase,
+            'vm_friendly_name': res_vm_friendly_name,
             'rg': rg_ase,
             'backup_item_friendly_name': item_friendly_name,
-            'backup_item_protection_state': 'IRPending',
-            'default': 'DefaultPolicy',
-            'enhanced': 'EnhancedPolicy',
-            'id_ase': id_ase,
+            'backup_item_protection_state': 'Protected',
+            'backup_policy': 'DailyPolicy-ma2c99lv',
+            'policy_new': self.create_random_name('clitest-policy', 24),
+            'reg_vm_id': reg_vm_id,
         })
 
-        self.kwargs['container'] = self.cmd('backup container show -n {vm} -v {vault} -g {rg} --backup-management-type AzureIaasVM', checks = [
-            self.check('name', '{vm}'),
-            self.check('properties.friendlyName', '{vm2_friendly_name}')
+        # az backup container show -n VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm5 -v ase-rsv-grs -g ase-rg-ccy --backup-management-type AzureWorkload
+        self.kwargs['container'] = self.cmd('backup container show -n {vm_full_name} -v {vault} -g {rg} --backup-management-type AzureWorkload', checks = [
+            self.check('name', '{vm_full_name}'),
+            self.check('properties.friendlyName', '{vm_friendly_name}')
         ]).get_output_in_json()
 
-        self.cmd('backup container list --backup-management-type AzureIaasVM -v {vault} -g {rg}', checks=[
-            self.check("length(@)", 0),
-            # self.check("length([?properties.friendlyName == '{vm1}'])", 1),
-            # self.check("length([?properties.friendlyName == '{vm2}'])", 1)
+        # az backup container list --backup-management-type AzureWorkload -v ase-rsv-grs -g ase-rg-ccy
+        self.cmd('backup container list --backup-management-type AzureWorkload -v {vault} -g {rg}', checks=[
+            self.check("length(@)", 2)
         ])
-
-        self.cmd('backup item show --backup-management-type AzureWorkload -g {rg} -v {vault} -c {vm} -n {backup_item_friendly_name}', checks=[
+        
+        # az backup item show --backup-management-type AzureWorkload -g ase-rg-ccy -v ase-rsv-grs -c VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm5 -n SAPAseDatabase;ab4;asetestdb1
+        self.cmd('backup item show --backup-management-type AzureWorkload -g {rg} -v {vault} -c {vm_full_name} -n {backup_item_friendly_name}', checks=[
             self.check("properties.friendlyName", '{backup_item_friendly_name}'),
             self.check("properties.protectionState", '{backup_item_protection_state}'),
             self.check("resourceGroup", '{rg}'),
             self.check("properties.isScheduledForDeferredDelete", None)
         ])
 
+        # backup policy create
+        # az backup policy show -g ase-rg-ccy -v ase-rsv-grs -n DailyPolicy-ma2c99lv
+        self.kwargs['policy1_json'] = self.cmd('backup policy show -g {rg} -v {vault} -n {backup_policy}', checks=[
+            self.check('name', '{backup_policy}'),
+            self.check('resourceGroup', '{rg}')
+        ]).get_output_in_json()
+
+        self.kwargs['policy_json'] = json.dumps(self.kwargs['policy1_json'], separators=(',', ':')).replace('\'', '\\\'').replace('"', '\\"')
+
+        self.cmd("backup policy create -g {rg} -v {vault} --policy {policy_json} --backup-management-type AzureWorkload --workload-type SAPAseDatabase --name {policy_new}", checks=[
+            self.check('name', '{policy_new}'),
+            self.check('resourceGroup', '{rg}')
+        ])
+        
+        # backup policy delete
+        self.cmd('backup policy delete -g {rg} -v {vault} -n {policy_new}')
+
+        # az backup policy list -g ase-rg-ccy -v ase-rsv-grs --workload-type SAPAseDatabase
         self.cmd('backup policy list -g {rg} -v {vault}  --workload-type SAPAseDatabase', checks=[
-            self.check("length([?name == '{default}'])", 1),
-            self.check("length([?name == '{enhanced}'])", 1)
+            self.check("length([?name == '{backup_policy}'])", 1)
         ])
 
     def test_bkp_res_ase(self):
         self.kwargs.update({
             'rg': rg_ase,
-            'vault': vault_ase_crr,
+            'vault': vault_ase_restore,
             'backup_item_name_db1': backup_item_name_db1,
             'backup_item_name_db2': backup_item_name_db2,
-            # 'target_item_name': vm2_friendly_name,
             'vm5_friendly_name': vm5_friendly_name,
             'vm5_full_name': 'VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm5',
             'name' : vm5_friendly_name
@@ -106,12 +124,17 @@ class ASEBackupTests(ScenarioTest, unittest.TestCase):
 
     def test_registration_ase_container(self):
         self.kwargs.update({
-            'vault': vault_ase_olr,
-            'vm5_friendly_name': vm5_friendly_name,
+            'vault': vault_ase_reg,
+            'vm': reg_vm_friendly_name,
             'rg': rg_ase,
-            'id_ase': id_ase,
+            'reg_vm_id': reg_vm_id,
         })
 
-        self.cmd('backup container register -v {vault} -g {rg} --workload-type SAPAseDatabase --backup-management-type AzureWorkload --resource-id {id_ase}')
+        self.cmd('backup container register -v {vault} -g {rg} --workload-type SAPAseDatabase --backup-management-type AzureWorkload --resource-id {reg_vm_id}')
 
-        self.cmd('backup container unregister -v {vault} -g {rg} -c {vm5_friendly_name} --backup-management-type AzureWorkload -y')
+        # configure protection
+        # stop protection
+
+        # self.cmd('backup container unregister -v {vault} -g {rg} -c {vm} --backup-management-type AzureWorkload -y')
+        # az backup container unregister -v ase-rsv-ccy -g ase-rg-ccy -c ase-ccy-vm2 --backup-management-type AzureWorkload -y
+        # az backup container unregister -v ase-rsv-ccy -g ase-rg-ccy -c VMAppContainer;Compute;ase-rg-ccy;ase-ccy-vm2
