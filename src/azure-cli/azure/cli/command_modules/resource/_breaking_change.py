fff56accf5ff78710a411ab23c8df8c6d1dca759
Chris Stackhouse <cstack@microsoft.com>
2025-05-12 16:36:31 -0700

    Try to make style thing happy...

--- src/azure-cli/azure/cli/command_modules/resource/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/resource/_breaking_change.py
@@ -7,44 +7,44 @@ from azure.cli.core.breaking_change import register_other_breaking_change
 
 register_other_breaking_change(
     'policy assignment identity remove',
-    'Removing a user assigned identity will change in a future release of the resource commands. ' \
-        'It will require providing the --mi-user-assigned switch.')
+    'Removing a user assigned identity will change in a future release of the resource commands. '
+    'It will require providing the --mi-user-assigned switch.')
 register_other_breaking_change(
     'policy assignment identity assign',
-    'Replacing an existing identity will change in a future release of the resource commands. ' \
-        'It will require first removing the existing identity.')
+    'Replacing an existing identity will change in a future release of the resource commands. '
+    'It will require first removing the existing identity.')
 register_other_breaking_change(
     'policy assignment non-compliance-message create',
-    'The return value will change in a future release of the resource commands. ' \
-        'It will be the single created message object rather than the full array of message objects.')
+    'The return value will change in a future release of the resource commands. '
+    'It will be the single created message object rather than the full array of message objects.')
 register_other_breaking_change(
     'policy assignment delete',
-    'Behavior will change in a future release of the resource commands. ' \
-        'Bypassing the confirmation prompt will require providing the -y switch.')
+    'Behavior will change in a future release of the resource commands. '
+    'Bypassing the confirmation prompt will require providing the -y switch.')
 register_other_breaking_change(
     'policy assignment non-compliance-message delete',
-    'Behavior will change in a future release of the resource commands. ' \
-        'Bypassing the confirmation prompt will require providing the -y switch and ' \
-        'nothing will be returned instead of the remaining list of messages.')
+    'Behavior will change in a future release of the resource commands. '
+    'Bypassing the confirmation prompt will require providing the -y switch and '
+    'nothing will be returned instead of the remaining list of messages.')
 register_other_breaking_change(
     'policy definition delete',
-    'Behavior will change in a future release of the resource commands. ' \
-        'Bypassing the confirmation prompt will require providing the -y switch.')
+    'Behavior will change in a future release of the resource commands. '
+    'Bypassing the confirmation prompt will require providing the -y switch.')
 register_other_breaking_change(
     'policy exemption delete',
-    'Behavior will change in a future release of the resource commands. ' \
-        'Bypassing the confirmation prompt will require providing the -y switch.')
+    'Behavior will change in a future release of the resource commands. '
+    'Bypassing the confirmation prompt will require providing the -y switch.')
 register_other_breaking_change(
     'policy set-definition delete',
-    'Behavior will change in a future release of the resource commands. ' \
-        'Bypassing the confirmation prompt will require providing the -y switch.')
+    'Behavior will change in a future release of the resource commands. '
+    'Bypassing the confirmation prompt will require providing the -y switch.')
 register_other_breaking_change(
     'policy exemption create',
-    'Date format will change slightly in a future release of the resource commands. ' \
-        'New format is ISO-8601, e.g. 2025-08-05T00:45:13Z instead of 2025-08-05T00:45:13+00:00.',
+    'Date format will change slightly in a future release of the resource commands. '
+    'New format is ISO-8601, e.g. 2025-08-05T00:45:13Z instead of 2025-08-05T00:45:13+00:00.',
     '--expires-on')
 register_other_breaking_change(
     'policy exemption update',
-    'Date format will change slightly in a future release of the resource commands. ' \
-        'New format is ISO-8601, e.g. 2025-08-05T00:45:13Z instead of 2025-08-05T00:45:13+00:00.',
+    'Date format will change slightly in a future release of the resource commands. '
+    'New format is ISO-8601, e.g. 2025-08-05T00:45:13Z instead of 2025-08-05T00:45:13+00:00.',
     '--expires-on')
