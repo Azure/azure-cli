301a6ee7406546cae4472545b4a7337576ffe937
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2025-05-20 16:12:47 +0800

    {Compute} `az vm encryption`: Migrate to AAZ (#31383)

--- src/azure-cli/azure/cli/command_modules/vm/operations/vm.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/vm.py
@@ -6,9 +6,36 @@
 from knack.log import get_logger
 
 from azure.cli.core.aaz import AAZStrType
-from ..aaz.latest.vm import Show as _VMShow, ListSizes as _VMListSizes
+from ..aaz.latest.vm import Show as _VMShow, ListSizes as _VMListSizes, Update as _VMUpdate
 
 logger = get_logger(__name__)
+
+
+class VMUpdate(_VMUpdate):
+    class VirtualMachinesGet(_VMUpdate.VirtualMachinesGet):
+        # Override to solve key conflict of _schema_on_200.resources.Element.properties.type when deserializing
+        @classmethod
+        def _build_schema_on_200(cls):
+            schema = super()._build_schema_on_200()
+
+            del schema.resources.Element.properties._fields['type']
+            schema.resources.Element.properties.type = AAZStrType(
+                serialized_name="typePropertiesType",
+            )
+            return schema
+
+    def _output(self, *args, **kwargs):
+        from azure.cli.core.aaz import AAZUndefined, has_value
+
+        # Resolve flatten conflict
+        # When the type field conflicts, the type in inner layer is ignored and the outer layer is applied
+        if has_value(self.ctx.vars.instance.resources):
+            for resource in self.ctx.vars.instance.resources:
+                if has_value(resource.type):
+                    resource.type = AAZUndefined
+
+        result = self.deserialize_output(self.ctx.vars.instance, client_flatten=True)
+        return result
 
 
 class VMShow(_VMShow):
