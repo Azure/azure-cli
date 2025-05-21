b68e3e8771da34445b1fc00575f627a7068fb6e9
RuiJun Hu (MSFT) <v-ruih@microsoft.com>
2025-05-21 11:56:02 +0800

    nat gateway codegen

--- src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/nat/gateway/_wait.py
+++ src/azure-cli/azure/cli/command_modules/network/aaz/latest/network/nat/gateway/_wait.py
@@ -20,7 +20,7 @@ class Wait(AAZWaitCommand):
 
     _aaz_info = {
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.network/natgateways/{}", "2022-01-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.network/natgateways/{}", "2024-07-01"],
         ]
     }
 
@@ -116,7 +116,7 @@ class Wait(AAZWaitCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2022-01-01",
+                    "api-version", "2024-07-01",
                     required=True,
                 ),
             }
@@ -178,13 +178,23 @@ class Wait(AAZWaitCommand):
             properties.public_ip_addresses = AAZListType(
                 serialized_name="publicIpAddresses",
             )
+            properties.public_ip_addresses_v6 = AAZListType(
+                serialized_name="publicIpAddressesV6",
+            )
             properties.public_ip_prefixes = AAZListType(
                 serialized_name="publicIpPrefixes",
+            )
+            properties.public_ip_prefixes_v6 = AAZListType(
+                serialized_name="publicIpPrefixesV6",
             )
             properties.resource_guid = AAZStrType(
                 serialized_name="resourceGuid",
                 flags={"read_only": True},
             )
+            properties.source_virtual_network = AAZObjectType(
+                serialized_name="sourceVirtualNetwork",
+            )
+            _WaitHelper._build_schema_sub_resource_read(properties.source_virtual_network)
             properties.subnets = AAZListType(
                 flags={"read_only": True},
             )
@@ -193,9 +203,17 @@ class Wait(AAZWaitCommand):
             public_ip_addresses.Element = AAZObjectType()
             _WaitHelper._build_schema_sub_resource_read(public_ip_addresses.Element)
 
+            public_ip_addresses_v6 = cls._schema_on_200.properties.public_ip_addresses_v6
+            public_ip_addresses_v6.Element = AAZObjectType()
+            _WaitHelper._build_schema_sub_resource_read(public_ip_addresses_v6.Element)
+
             public_ip_prefixes = cls._schema_on_200.properties.public_ip_prefixes
             public_ip_prefixes.Element = AAZObjectType()
             _WaitHelper._build_schema_sub_resource_read(public_ip_prefixes.Element)
+
+            public_ip_prefixes_v6 = cls._schema_on_200.properties.public_ip_prefixes_v6
+            public_ip_prefixes_v6.Element = AAZObjectType()
+            _WaitHelper._build_schema_sub_resource_read(public_ip_prefixes_v6.Element)
 
             subnets = cls._schema_on_200.properties.subnets
             subnets.Element = AAZObjectType()
