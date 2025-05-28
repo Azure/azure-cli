95fdcac6c12247ef18d155953d584875530570e2
Yishi Wang <yishiwang@microsoft.com>
2025-05-28 17:35:00 +0800

    az cloud register: Compatible with more response formats from ARM endpoint API

--- src/azure-cli/azure/cli/command_modules/cloud/custom.py
+++ src/azure-cli/azure/cli/command_modules/cloud/custom.py
@@ -47,7 +47,13 @@ def _populate_from_metadata_endpoint(arm_endpoint, session=None):
         response = session.get(metadata_endpoint)
         if response.status_code == 200:
             metadata = response.json()
-            return _arm_to_cli_mapper(metadata)
+            if isinstance(metadata, dict):
+                return _arm_to_cli_mapper(metadata)
+            elif isinstance(metadata, list):
+                return _arm_to_cli_mapper(metadata[0])
+            else:
+                msg = 'Response body does not contain valid json. Response content: {}'.format(str(metadata))
+                raise CLIError(error_msg_fmt.format(msg))
         msg = 'Server returned status code {} for {}'.format(response.status_code, metadata_endpoint)
         raise CLIError(error_msg_fmt.format(msg))
     except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError) as err:
