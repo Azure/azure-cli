4517865d794842720c039ad8b99160d0d688bf15
Chang Lu <luchang@microsoft.com>
2025-05-29 13:06:53 -0700

    Add secret replacer to scrub secrets in test recordings.

--- src/azure-cli/azure/cli/command_modules/search/tests/latest/test_admin_key.py
+++ src/azure-cli/azure/cli/command_modules/search/tests/latest/test_admin_key.py
@@ -8,7 +8,11 @@ import unittest
 
 
 class AzureSearchAdminKeysTests(ScenarioTest):
-
+    def __init__(self, method_name):
+        super(AzureSearchAdminKeysTests, self).__init__(
+            method_name, recording_processors=[KeyReplacer()]
+        )
+        
     # https://vcrpy.readthedocs.io/en/latest/configuration.html#request-matching
     def setUp(self):
         self.vcr.match_on = ['scheme', 'method', 'path', 'query'] # not 'host', 'port'
