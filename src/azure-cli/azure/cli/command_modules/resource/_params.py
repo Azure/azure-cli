b27cb617597661153d0197f4b83271a08a7ce501
David Cho <chodavid@microsoft.com>
2025-06-19 11:11:00 -0400

    Fixing more linter errors

--- src/azure-cli/azure/cli/command_modules/resource/_params.py
+++ src/azure-cli/azure/cli/command_modules/resource/_params.py
@@ -535,7 +535,8 @@ def load_arguments(self, _):
         c.argument('skip_resource_name_params', action='store_true')
         c.argument('skip_all_params', action='store_true')
         c.argument('resource_ids', nargs='+', options_list='--resource-ids')
-        c.argument('output_format', options_list=['--output-format'])
+        c.argument('output_format', options_list=['--output-format'],
+                    help="The format of the exported template. Allowed values: 'json', 'arm', 'bicep'. Defaults to 'json'.")
 
     with self.argument_context('group create') as c:
         c.argument('rg_name', options_list=['--name', '--resource-group', '-n', '-g'],
