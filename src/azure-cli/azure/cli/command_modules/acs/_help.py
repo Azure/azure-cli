eab4ff6ec0bd67fdc2c109cce53b1c20c83857bc
skuchipudi295 <128435195+skuchipudi295@users.noreply.github.com>
2025-06-19 21:04:18 +0000

    fixing help info

--- src/azure-cli/azure/cli/command_modules/acs/_help.py
+++ src/azure-cli/azure/cli/command_modules/acs/_help.py
@@ -2585,7 +2585,7 @@ helps['aks machine list'] = """
          short-summary: Name of the agentpool of a managed cluster
    examples:
        - name: Get information about IP Addresses, Hostname, Availability Zones for all machines in an agentpool
-         text: az aks machine list --cluster-name <clusterName> --nodepool-name <apName>
+         text: az aks machine list  --resource-group <rg> --cluster-name <clusterName> --nodepool-name <apName>
 """
 helps['aks machine show'] = """
    type: command
@@ -2602,5 +2602,5 @@ helps['aks machine show'] = """
          short-summary: Name of the machine in the agentpool of a managed cluster
    exmaples:
        - name: Get IP Addresses, Hostname, Availability Zones for a specific machine in an agentpool
-         text: az aks machine show --cluster-name <clusterName> --nodepool-name <apName> --machine-name <machineName>
+         text: az aks machine show --resource-group <rg> --cluster-name <clusterName> --nodepool-name <apName> --machine-name <machineName>
 """
