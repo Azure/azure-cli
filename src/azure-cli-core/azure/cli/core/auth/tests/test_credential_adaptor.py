d04793c75f4dd9aa749e3e1ffc892d24e55875ed
jiasli <4003950+jiasli@users.noreply.github.com>
2025-06-26 17:47:43 +0800

    get-token-data

--- src/azure-cli-core/azure/cli/core/auth/tests/test_credential_adaptor.py
+++ src/azure-cli-core/azure/cli/core/auth/tests/test_credential_adaptor.py
@@ -11,11 +11,6 @@ from ..credential_adaptor import CredentialAdaptor
 
 
 MOCK_ACCESS_TOKEN = "mock_access_token"
-MOCK_DATA = {
-    'key_id': 'test',
-    'req_cnf': 'test',
-    'token_type': 'ssh-cert'
-}
 MOCK_CLAIMS = {"test_claims": "value2"}
 
 class MsalCredentialStub:
@@ -44,7 +39,7 @@ def _now_timestamp_mock():
 
 class TestCredentialAdaptor(unittest.TestCase):
 
-    @mock.patch('azure.cli.core.auth.util._now_timestamp', new=_now_timestamp_mock)
+    @mock.patch('azure.cli.core.auth.util.now_timestamp', new=_now_timestamp_mock)
     def test_get_token(self):
         msal_cred = MsalCredentialStub()
         sdk_cred = CredentialAdaptor(msal_cred)
@@ -56,15 +51,11 @@ class TestCredentialAdaptor(unittest.TestCase):
         assert access_token.token == MOCK_ACCESS_TOKEN
         assert access_token.expires_on == 1630920323
 
-        # Note that SDK doesn't support 'data'. This is a CLI-specific extension.
-        sdk_cred.get_token('https://management.core.windows.net//.default', data=MOCK_DATA)
-        assert msal_cred.acquire_token_kwargs['data'] == MOCK_DATA
-
         sdk_cred.get_token('https://management.core.windows.net//.default', claims=MOCK_CLAIMS)
         assert msal_cred.acquire_token_claims_challenge == MOCK_CLAIMS
 
 
-    @mock.patch('azure.cli.core.auth.util._now_timestamp', new=_now_timestamp_mock)
+    @mock.patch('azure.cli.core.auth.util.now_timestamp', new=_now_timestamp_mock)
     def test_get_token_info(self):
         msal_cred = MsalCredentialStub()
         sdk_cred = CredentialAdaptor(msal_cred)
@@ -77,10 +68,6 @@ class TestCredentialAdaptor(unittest.TestCase):
         assert access_token_info.token_type == 'Bearer'
 
         assert msal_cred.acquire_token_scopes == ['https://management.core.windows.net//.default']
-
-        # Note that SDK doesn't support 'data'. If 'data' were supported, it should be tested with:
-        sdk_cred.get_token_info('https://management.core.windows.net//.default', options={'data': MOCK_DATA})
-        assert msal_cred.acquire_token_kwargs['data'] == MOCK_DATA
 
         sdk_cred.get_token_info('https://management.core.windows.net//.default', options={'claims': MOCK_CLAIMS})
         assert msal_cred.acquire_token_claims_challenge == MOCK_CLAIMS
