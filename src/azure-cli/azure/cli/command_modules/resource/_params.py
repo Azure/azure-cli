5f689fc71cce114699133fdd580843c4132dae1b
David Cho <chodavid@microsoft.com>
2025-07-03 23:23:35 -0400

    Addressing comments

--- src/azure-cli/azure/cli/command_modules/resource/_params.py
+++ src/azure-cli/azure/cli/command_modules/resource/_params.py
@@ -535,7 +535,7 @@ def load_arguments(self, _):
         c.argument('skip_resource_name_params', action='store_true')
         c.argument('skip_all_params', action='store_true')
         c.argument('resource_ids', nargs='+', options_list='--resource-ids')
-        c.argument('export_format', options_list=['--export-format'], help="The format of the exported template. Allowed values: 'json', 'arm', 'bicep'. Defaults to 'json'.")
+        c.argument('export_format', help="The format of the exported template. Allowed values: 'json', 'arm', 'bicep'. Defaults to 'json'.")
 
     with self.argument_context('group create') as c:
         c.argument('rg_name', options_list=['--name', '--resource-group', '-n', '-g'],
