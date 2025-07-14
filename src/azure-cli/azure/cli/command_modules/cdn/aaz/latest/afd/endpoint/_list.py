88dcb4f582b94033faacfcc8d32a4dadf72fc4ff
Ptnan7 <148527582+Ptnan7@users.noreply.github.com>
2025-07-14 15:52:18 +1000

    {afd} Upgrade api version to 2025-06-01 (#31802)
    Co-authored-by: Jingnan Xu <jingnanxu@microsoft.com>

--- src/azure-cli/azure/cli/command_modules/cdn/aaz/latest/afd/endpoint/_list.py
+++ src/azure-cli/azure/cli/command_modules/cdn/aaz/latest/afd/endpoint/_list.py
@@ -22,9 +22,9 @@ class List(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2025-04-15",
+        "version": "2025-06-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.cdn/profiles/{}/afdendpoints", "2025-04-15"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.cdn/profiles/{}/afdendpoints", "2025-06-01"],
         ]
     }
 
@@ -126,7 +126,7 @@ class List(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2025-04-15",
+                    "api-version", "2025-06-01",
                     required=True,
                 ),
             }
