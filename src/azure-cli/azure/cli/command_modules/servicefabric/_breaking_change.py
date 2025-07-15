e2d229a1bae32d80623f32fe220a1058badabc74
Ivy Liu <iliu@microsoft.com>
2025-07-15 12:03:51 -0700

    azdev linter and style

--- src/azure-cli/azure/cli/command_modules/servicefabric/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/servicefabric/_breaking_change.py
@@ -4,4 +4,5 @@
 
 from azure.cli.core.breaking_change import register_other_breaking_change
 
-register_other_breaking_change('sf managed-application update', 'Options list has changed, run help command to see allowed options')
+register_other_breaking_change('sf managed-application update',
+                               'Options list has changed, run help command to see allowed options')
