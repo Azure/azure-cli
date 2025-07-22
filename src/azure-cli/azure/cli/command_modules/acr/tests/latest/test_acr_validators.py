50e0ef84fc52869dcb2efe305154d7ec4f6e69c1
trisavo-msft <141691062+trisavo-msft@users.noreply.github.com>
2025-07-22 12:26:33 -0500

    consolidate test

--- src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_acr_validators.py
+++ src/azure-cli/azure/cli/command_modules/acr/tests/latest/test_acr_validators.py
@@ -10,19 +10,7 @@ from azure.cli.core.azclierror import InvalidArgumentValueError
 from azure.cli.core.cloud import HARD_CODED_CLOUD_LIST
 from azure.cli.command_modules.acr._validators import validate_registry_name
 
-class AcrValidatorsTests(unittest.TestCase):
-    def test_registry_name_with_dnl_suffix(self):
-        acr_supported_clouds = [cloud for cloud in HARD_CODED_CLOUD_LIST if cloud.name != 'AzureGermanCloud']
-        for hard_coded_cloud in acr_supported_clouds:
-            namespace = SimpleNamespace(
-                    **{
-                        "registry_name": "myacr-dnlhash123",
-                    }
-                )
-            cmd = Mock(cli_ctx=Mock(cloud=hard_coded_cloud))
-            validate_registry_name(cmd, namespace)
-            self.assertEqual(namespace.registry_name, 'myacr')
-    
+class AcrValidatorsTests(unittest.TestCase):  
     def test_registry_name_with_dnl_suffix_loginserver(self):
         namespace = SimpleNamespace(
             **{
