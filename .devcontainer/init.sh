609aa17453ecf6462e71345ec99e47c854cbff8d
necusjz <necusjz@gmail.com>
2025-07-22 12:54:30 +1000

    chore: add user-friendly prompts

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -13,63 +13,66 @@ fi
 
 # Check `aaz-dev` is available or not
 if ! command -v aaz-dev &> /dev/null; then
-  GREEN="\033[0;32m"
-  YELLOW="\033[1;33m"
-  NC="\033[0m"  # no color
+    GREEN="\033[0;32m"
+    YELLOW="\033[0;33m"
+    NC="\033[0m"  # no color
 
-  set_or_add_remote() {
-      local REMOTE_NAME=$1
-      local REMOTE_URL=$2
-      local REPO_PATH="/workspaces/$REPO_NAME"
+    set_or_add_remote() {
+        local REMOTE_NAME=$1
+        local REMOTE_URL=$2
+        local REPO_PATH="/workspaces/$REPO_NAME"
 
-      git -C "$REPO_PATH" remote get-url "$REMOTE_NAME" &>/dev/null || git -C "$REPO_PATH" remote add "$REMOTE_NAME" "$REMOTE_URL"
-      git -C "$REPO_PATH" remote set-url "$REMOTE_NAME" "$REMOTE_URL"
-  }
+        git -C "$REPO_PATH" remote get-url "$REMOTE_NAME" &>/dev/null || git -C "$REPO_PATH" remote add "$REMOTE_NAME" "$REMOTE_URL"
+        git -C "$REPO_PATH" remote set-url "$REMOTE_NAME" "$REMOTE_URL"
+    }
 
-  setup_repo() {
-      local DIR_NAME="$1"
-      local DIR_PATH="/workspaces/$DIR_NAME"
-      local REPO="Azure/$DIR_NAME"
+    setup_repo() {
+        local DIR_NAME="$1"
+        local DIR_PATH="/workspaces/$DIR_NAME"
+        local REPO="Azure/$DIR_NAME"
 
-      if [ -d "$DIR_PATH" ]; then
-          echo -e "\n${YELLOW}($DIR_NAME) Pulling the latest changes from upstream...${NC}"
-          gh repo fork "$REPO" --clone=false
-      else
-          echo -e "\n${GREEN}($DIR_NAME) Forking and cloning the repository...${NC}"
-          gh repo fork "$REPO" --clone=true
-      fi
+        if [ -d "$DIR_PATH" ]; then
+            echo -e "\n${YELLOW}($DIR_NAME) Pulling the latest changes from upstream...${NC}"
+            gh repo fork "$REPO" --clone=false
+        else
+            echo -e "\n${GREEN}($DIR_NAME) Forking and cloning the repository...${NC}"
+            gh repo fork "$REPO" --clone -- --depth=1
+        fi
 
-      # `git` doesn't work well with private repository
-      if [ "$(gh repo view "$REPO" --json visibility --jq '.visibility')" == "PRIVATE" ]; then
-          cd "$DIR_PATH"
-          gh repo sync --source "$REPO"
-          cd /workspaces
-      else
-          DEFAULT_BRANCH=$(git -C "$DIR_PATH" remote show upstream | grep "HEAD branch" | awk '{print $NF}')
-          git -C "$DIR_PATH" pull -r upstream "$DEFAULT_BRANCH"
-      fi
-  }
+        # `git` doesn't work well with private repository
+        if [ "$(gh repo view "$REPO" --json visibility --jq '.visibility')" == "PRIVATE" ]; then
+            cd "$DIR_PATH"
+            gh repo sync --source "$REPO"
+            cd /workspaces
+        else
+            DEFAULT_BRANCH=$(git -C "$DIR_PATH" remote show upstream | grep "HEAD branch" | awk '{print $NF}')
+            git -C "$DIR_PATH" pull -r upstream "$DEFAULT_BRANCH"
+        fi
+    }
 
-  SECONDS=0
+    echo -e "\n${GREEN}Welcome to the dev environment setup wizard.${NC}\n"
 
-  REPO_NAME=$(basename "$GITHUB_REPOSITORY")
-  set_or_add_remote origin "https://github.com/$GITHUB_USER/$REPO_NAME.git"
-  set_or_add_remote upstream "https://github.com/Azure/$REPO_NAME.git"
+    SECONDS=0
 
-  uv pip install aaz-dev
+    REPO_NAME=$(basename "$GITHUB_REPOSITORY")
+    set_or_add_remote origin "https://github.com/$GITHUB_USER/$REPO_NAME.git"
+    set_or_add_remote upstream "https://github.com/Azure/$REPO_NAME.git"
 
-  # azdev repositories
-  setup_repo "azure-cli"
-  setup_repo "azure-cli-extensions"
+    uv pip install aaz-dev
 
-  azdev setup -c -r ./azure-cli-extensions
+    # `azdev` repositories
+    setup_repo "azure-cli"
+    setup_repo "azure-cli-extensions"
 
-  # aaz-dev repositories
-  setup_repo "aaz"
-  setup_repo "azure-rest-api-specs"
-  setup_repo "azure-rest-api-specs-pr"
+    azdev setup -c -r ./azure-cli-extensions
 
-  ELAPSED_TIME=$SECONDS
-  echo -e "\n${YELLOW}Elapsed time: $((ELAPSED_TIME / 60))m $((ELAPSED_TIME % 60))s.${NC}"
-  echo -e "\n${GREEN}Finished setup! Please launch the codegen tool via \`aaz-dev run -c azure-cli -e azure-cli-extensions -s azure-rest-api-specs -a aaz\`.${NC}\n"
+    # `aaz-dev` repositories
+    setup_repo "aaz"
+    setup_repo "azure-rest-api-specs"
+    setup_repo "azure-rest-api-specs-pr"
+
+    ELAPSED_TIME=$SECONDS
+    echo -e "\n${YELLOW}Elapsed time: $((ELAPSED_TIME / 60))m $((ELAPSED_TIME % 60))s.${NC}"
+    echo -e "\n${GREEN}Finished setup! Please launch the codegen tool via:${NC}"
+    echo -e "${GREEN}aaz-dev run -c azure-cli -e azure-cli-extensions -s azure-rest-api-specs -a aaz${NC}\n"
 fi
