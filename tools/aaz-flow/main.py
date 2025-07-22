edc11e8140a1b6f1a470c3f1d3eee229624c46dc
Julie Zhu <zhuyan@microsoft.com>
2025-07-22 12:16:02 +1000

    fix default env

--- tools/aaz-flow/main.py
+++ tools/aaz-flow/main.py
@@ -14,9 +14,9 @@ def generate_code(
 ):
     """Generate Azure CLI commands based on Swagger specifications.
     """
-    aaz_path = os.getenv("AAZ_PATH", "C:\\python\\project\\aaz")
-    cli_path = os.getenv("CLI_PATH", "C:\\python\\project\\azure-cli")
-    cli_extension_path = os.getenv("CLI_EXTENSION_PATH", "C:\\python\\project\\azure-cli-extensions")
+    aaz_path = os.getenv("AAZ_PATH", "/workspaces/aaz")
+    cli_path = os.getenv("CLI_PATH", "/workspaces/azure-cli")
+    cli_extension_path = os.getenv("CLI_EXTENSION_PATH", "/workspaces/azure-cli-extensions")
 
     return (
         f"Just run the following command in the terminal: 'aaz-dev cli generate-by-swagger-tag --aaz-path {aaz_path} --cli-path {cli_path} --cli-extension-path {cli_extension_path} --extension-or-module-name {extension_or_module_name} --swagger-module-path {swagger_module_path} --resource-provider {resource_provider} --swagger-tag {swagger_tag} --profile latest'",
