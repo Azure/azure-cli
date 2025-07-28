7f9d59ae03466a46a6c5f5b62cbaa31854a54daf
Elina Liu <wenxliu@microsoft.com>
2025-07-27 23:14:38 -0700

    Try to fix linter check

--- src/azure-cli/azure/cli/command_modules/compute_recommender/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/compute_recommender/_breaking_change.py
@@ -5,5 +5,5 @@
 
 from azure.cli.core.breaking_change import register_command_deprecate
 
-register_command_deprecate('compute-recommender spot-placement-recommender', redirect='az compute-recommender spot-placement-score', hide=True)
-# Warning Message: This command has been deprecated and will be removed in next breaking change release(2.67.0). Use `baz foo` instead.
+register_command_deprecate('compute-recommender spot-placement-recommender', 
+	redirect='az compute-recommender spot-placement-score', hide=True)
