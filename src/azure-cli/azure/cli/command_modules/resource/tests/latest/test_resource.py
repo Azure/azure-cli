371c3bad5d41712155c5694fed1eae0257cbbe0e
Kyle Albert <5498623+kalbert312@users.noreply.github.com>
2025-07-28 14:46:06 -0400

    Add live test for extensibility deployment in sub.

--- src/azure-cli/azure/cli/command_modules/resource/tests/latest/test_resource.py
+++ src/azure-cli/azure/cli/command_modules/resource/tests/latest/test_resource.py
@@ -1276,10 +1276,6 @@ class DeploymentTestsWithQueryString(LiveScenarioTest):
 
 
 class DeploymentTestAtSubscriptionScope(ScenarioTest):
-    def tearDown(self):
-        self.cmd('policy assignment delete -n location-lock')
-        self.cmd('policy definition delete -n policy2')
-        self.cmd('group delete -n cli_test_subscription_level_deployment --yes')
 
     @AllowLargeResponse(4096)
     def test_subscription_level_deployment(self):
@@ -1349,6 +1345,56 @@ class DeploymentTestAtSubscriptionScope(ScenarioTest):
             self.check('properties.provisioningState', 'Canceled')
         ])
 
+        self.cmd('policy assignment delete -n location-lock')
+        self.cmd('policy definition delete -n policy2')
+        self.cmd('group delete -n cli_test_subscription_level_deployment --yes')
+
+    @AllowLargeResponse(4096)
+    def test_sub_extensibility_deployment(self):
+        curr_dir = os.path.dirname(os.path.realpath(__file__))
+        self.kwargs.update(
+            {
+                'location': 'WestUS',
+                'tf': os.path.join(curr_dir, 'simple_extensibility_deploy.json').replace('\\', '\\\\'),
+                'params': os.path.join(curr_dir, 'simple_extensibility_deploy_params.json').replace('\\', '\\\\'),
+                'params_invalid': os.path.join(curr_dir, 'simple_extensibility_deploy_params_invalid.json').replace('\\', '\\\\'),
+                'dn': self.create_random_name('azure-cli-sub-extensibility-deployment', 60),
+                'dn2': self.create_random_name('azure-cli-sub-extensibility-deployment', 60),
+                'inlineExtConfigs': '{"parameters":{},"extensionConfigs":{"contoso":{"configOne":{"value":"cliParamValue"}}}}'.replace('"', '\\"'),
+            })
+
+        with self.assertRaises(CLIError) as err:
+            self.cmd(
+                'deployment sub validate -n {dn} --location {location} --template-file "{tf}" --parameters @"{params_invalid}" --no-prompt true')
+            self.assertTrue("Deployment template validation failed" in str(err.exception))
+
+        with self.assertRaises(CLIError) as err:
+            self.cmd(
+                'deployment sub create -n {dn} --location {location} --template-file "{tf}" --parameters @"{params_invalid}"')
+            self.assertTrue("Deployment template validation failed" in str(err.exception))
+
+        self.cmd(
+            'deployment sub validate -n {dn} --location {location} --template-file "{tf}" --parameters @"{params}"', checks=[
+                self.check('properties.provisioningState', 'Succeeded')
+            ])
+
+        self.cmd(
+            'deployment sub validate -n {dn2} --location {location} --template-file "{tf}" --parameters @"{params}" --parameters "{inlineExtConfigs}"',
+            checks=[
+                self.check('properties.provisioningState', 'Succeeded')
+            ])
+
+        self.cmd(
+            'deployment sub create -n {dn} --location {location} --template-file "{tf}" --parameters @"{params}"', checks=[
+                self.check('properties.provisioningState', 'Succeeded'),
+            ])
+
+        self.cmd(
+            'deployment sub create -n {dn2} --location {location} --template-file "{tf}" --parameters @"{params}" --parameters "{inlineExtConfigs}"',
+            checks=[
+                self.check('properties.provisioningState', 'Succeeded'),
+            ])
+
     @AllowLargeResponse(4096)
     def test_subscription_level_deployment_old_command(self):
         curr_dir = os.path.dirname(os.path.realpath(__file__))
@@ -1400,6 +1446,10 @@ class DeploymentTestAtSubscriptionScope(ScenarioTest):
         self.cmd('deployment show -n {dn2}', checks=[
             self.check('properties.provisioningState', 'Canceled')
         ])
+
+        self.cmd('policy assignment delete -n location-lock')
+        self.cmd('policy definition delete -n policy2')
+        self.cmd('group delete -n cli_test_subscription_level_deployment --yes')
 
 
 class DeploymentTestAtResourceGroup(ScenarioTest):
