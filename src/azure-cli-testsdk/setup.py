ce5dfdc038d6d61516d189676999948b9a18e7a3
Hang Lei <hanglei@microsoft.com>
2025-07-31 15:39:17 +0800

    Init

--- src/azure-cli-testsdk/setup.py
+++ src/azure-cli-testsdk/setup.py
@@ -19,6 +19,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
     'Programming Language :: Python :: 3.12',
+    'Programming Language :: Python :: 3.13',
     'License :: OSI Approved :: MIT License',
 ]
 
