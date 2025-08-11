8b5a18a4cb1651516335a9cda9ef3ab30949e207
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2025-08-11 14:10:16 +1000

    new changes

--- src/azure-cli/azure/cli/command_modules/vm/operations/vmss.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/vmss.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 # pylint: disable=no-self-use, line-too-long, protected-access, too-few-public-methods, unused-argument
+from azure.cli.core.aaz import has_value
 from knack.log import get_logger
 from ..aaz.latest.vmss import ListInstances as _VMSSListInstances
 from ..aaz.latest.vmss import Deallocate as _VMSSDeallocate
@@ -28,15 +29,8 @@ class VMSSListInstances(_VMSSListInstances):
 
 
 class VMSSDeallocate(_VMSSDeallocate):
-    def _output(self, *args, **kwargs):
-        from azure.cli.core.aaz import AAZUndefined, has_value
+    def pre_operations(self):
+        args = self.ctx.args
 
-        # Resolve flatten conflict
-        # When the type field conflicts, the type in inner layer is ignored and the outer layer is applied
-        if has_value(self.ctx.vars.instance.resources):
-            for resource in self.ctx.vars.instance.resources:
-                if has_value(resource.type):
-                    resource.type = AAZUndefined
-
-        result = self.deserialize_output(self.ctx.vars.instance, client_flatten=True)
-        return result
+        if has_value(args.instance_ids) and len(args.instance_ids) == 1 and not has_value(args.hibernate):
+            self.ctx.args.instance_ids = args.instance_ids[0]
