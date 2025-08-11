4c469215d565d57f0099b3cff0a99986a99be6c5
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2025-08-11 11:36:55 +1000

    vmss start migrate

--- src/azure-cli/azure/cli/command_modules/vm/operations/vmss.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/vmss.py
@@ -6,6 +6,8 @@
 from knack.log import get_logger
 
 from ..aaz.latest.vmss import ListInstances as _VMSSListInstances
+from ..aaz.latest.vmss import Start as _Start
+from azure.cli.core.aaz import has_value
 
 logger = get_logger(__name__)
 
@@ -25,3 +27,13 @@ class VMSSListInstances(_VMSSListInstances):
         result = self.deserialize_output(self.ctx.vars.instance.value, client_flatten=True)
         next_link = self.deserialize_output(self.ctx.vars.instance.next_link)
         return result, next_link
+
+
+class VMSSStart(_Start):
+
+    def pre_operations(self):
+        args = self.ctx.args
+
+        if not has_value(args.instance_ids):
+            # if instance id is not provide, override with '*'
+            args.instance_ids = ["*"]
