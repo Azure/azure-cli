334e16a52bf28b97421199142fdda856d1d56d5b
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2025-08-11 11:43:06 +1000

    vmss start migrate

--- src/azure-cli/azure/cli/command_modules/vm/operations/vmss.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/vmss.py
@@ -7,7 +7,7 @@ from knack.log import get_logger
 
 from ..aaz.latest.vmss import ListInstances as _VMSSListInstances
 from ..aaz.latest.vmss import Start as _Start
-from azure.cli.core.aaz import has_value
+
 
 logger = get_logger(__name__)
 
@@ -30,6 +30,7 @@ class VMSSListInstances(_VMSSListInstances):
 
 
 class VMSSStart(_Start):
+    from azure.cli.core.aaz import has_value
 
     def pre_operations(self):
         args = self.ctx.args
