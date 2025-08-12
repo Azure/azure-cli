6f70b9dad9723be3299da7528260ed65cb8fc2fc
Isaac Sarek Banner (HE/THEY) <isaacba@microsoft.com>
2025-08-12 11:20:58 -0700

    Add help message to create

--- src/azure-cli/azure/cli/command_modules/identity/_params.py
+++ src/azure-cli/azure/cli/command_modules/identity/_params.py
@@ -20,7 +20,7 @@ def load_arguments(self, _):
     with self.argument_context('identity create') as c:
         c.argument('location', get_location_type(self.cli_ctx), required=False)
         c.argument('tags', tags_type)
-        c.argument('isolation_scope', get_enum_type(['None', 'Regional']), required=False)
+        c.argument('isolation_scope', get_enum_type(['None', 'Regional']), required=False, help='Identity isolation scope, used to restrict identity assignment within an Azure region.')
 
     with self.argument_context('identity update') as c:
         c.argument('tags', tags_type)
