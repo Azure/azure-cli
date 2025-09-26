8c33ac910fefd05ab7812b63df29d9d7179a2e08
Naia Scott <nasc@microsoft.com>
2025-09-26 15:57:13 -0400

    lint

--- src/azure-cli/azure/cli/command_modules/rdbms/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/rdbms/_breaking_change.py
@@ -10,9 +10,9 @@ register_logic_breaking_change('postgres flexible-server create', 'Update defaul
                                detail='The default value will be changed from "17" to a '
                                'supported version based on regional capabilities.')
 register_argument_deprecate('postgres flexible-server create', '--create-default-database',
-                               message='Please use command group "postgres flexible-server db" for database creation.')
+                            message='Please use command group "postgres flexible-server db" for database creation.')
 register_argument_deprecate('postgres flexible-server create', '--database-name',
-                               message='Please use command group "postgres flexible-server db" for database creation.')
+                            message='Please use command group "postgres flexible-server db" for database creation.')
 register_other_breaking_change('postgres server',
                                message='Azure Database for PostgreSQL Single Server is deprecated. '
                                'Please migrate to Flexible Server for new deployments.')
