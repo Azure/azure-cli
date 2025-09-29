ca636528de7534f19e28287dfb1c82cce34f9791
Zhiyi Huang <17182306+calvinhzy@users.noreply.github.com>
2025-09-29 14:34:04 +0800

    some lint fix

--- src/azure-cli/azure/cli/command_modules/storage/_help.py
+++ src/azure-cli/azure/cli/command_modules/storage/_help.py
@@ -3202,9 +3202,7 @@ helps['storage file symbolic-link create'] = """
     examples:
       - name: Create a symbolic link to an NFS file specified by path.
         text: |
-            az storage file symbolic-link create --account-name MyAccount --share-name share --path link_path 
-            --target target_path --metadata meta1=value1 meta2=value2 --file-creation-time now 
-            --file-last-write-time now --owner 6 --group 7
+            az storage file symbolic-link create --account-name MyAccount --share-name share --path link_path --target target_path --metadata meta1=value1 meta2=value2 --file-creation-time now --file-last-write-time now --owner 6 --group 7
 """
 
 helps['storage file symbolic-link show'] = """
