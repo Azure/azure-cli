d9f86f9b3a4dd038a6b8e264a12c8f543d712191
necusjz <necusjz@gmail.com>
2025-10-09 16:30:33 +1100

    build: add hatchling configuration

--- tools/aaz-flow/src/aaz-flow/__init__.py
+++ tools/aaz-flow/src/aaz-flow/__init__.py
@@ -2,5 +2,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-# AAZ Flow package
+__version__ = "1.0.0"  # https://semver.org/#summary
