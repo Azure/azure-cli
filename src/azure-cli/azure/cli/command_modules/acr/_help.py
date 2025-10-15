d6184446ed17be9240be01d4e02a086ae1fa778a
Breanna Thompson <brethompson>
2025-10-14 19:32:02 -0700

    update py file

--- src/azure-cli/azure/cli/command_modules/acr/_help.py
+++ src/azure-cli/azure/cli/command_modules/acr/_help.py
@@ -898,6 +898,13 @@ examples:
   - name: Get the storage usage for an Azure Container Registry.
     text: >
         az acr show-usage -n myregistry
+name descriptions:
+  - name: Size: The current amount of storage used by the registry (in bytes). 
+      text: >
+        az acr show-usage -n myregistry --query "value[?name=='Size'] | [0]" 
+  - name: MaximumStorageCapacity: The maximum storage limit allowed for the registry based on its SKU (in bytes).
+      text: >
+        az acr show-usage -n myregistry --query "value[?name=='MaximumStorageCapacity'] | [0]" 
 """
 
 helps['acr task'] = """
