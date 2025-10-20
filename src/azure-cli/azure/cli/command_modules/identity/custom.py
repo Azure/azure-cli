e4f64be10d90a6ee1967ee38d7a8feaa151f4306
Mohamed Yilmaz Ibrahim <ibrahimmo@microsoft.com>
2025-10-20 16:03:25 -0700

    style error fix

--- src/azure-cli/azure/cli/command_modules/identity/custom.py
+++ src/azure-cli/azure/cli/command_modules/identity/custom.py
@@ -10,6 +10,7 @@ def list_user_assigned_identities(cmd, resource_group_name=None):
         return client.user_assigned_identities.list_by_resource_group(resource_group_name)
     return client.user_assigned_identities.list_by_subscription()
 
+
 def create_identity(client, resource_group_name, resource_name, location, tags=None, assignment_restriction=None):
     parameters = {}
     parameters['location'] = location
@@ -21,6 +22,7 @@ def create_identity(client, resource_group_name, resource_name, location, tags=N
                                    resource_name=resource_name,
                                    parameters=parameters)
 
+
 def update_identity(instance, tags=None, assignment_restriction=None):
     parameters = {}
     if tags is not None:
@@ -28,6 +30,7 @@ def update_identity(instance, tags=None, assignment_restriction=None):
     if assignment_restriction is not None:
         parameters['assignmentRestriction'] = assignment_restriction or instance.assignment_restriction
     return parameters
+
 
 def list_identity_resources(cmd, resource_group_name, resource_name):
     from azure.cli.command_modules.identity._client_factory import _msi_list_resources_client
