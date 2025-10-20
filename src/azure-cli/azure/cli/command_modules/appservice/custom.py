1c962a117c8addfeaf35f99cfe430b2a1d3564a8
Joseph Lin <joslin@microsoft.com>
2025-10-20 15:03:15 -0700

    Add missing zone_redundant property

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -4156,6 +4156,7 @@ has been deployed ".format(app_service_environment)
         "per_site_scaling": plan_def.per_site_scaling,
         "hosting_environment_profile": plan_def.hosting_environment_profile,
         "async_scaling_enabled": plan_def.async_scaling_enabled,
+        "zone_redundant": zone_redundant if zone_redundant else None,
         "is_custom_mode": is_custom_mode,
         "network": {
             "virtual_network_subnet_id": subnet_resource_id,
