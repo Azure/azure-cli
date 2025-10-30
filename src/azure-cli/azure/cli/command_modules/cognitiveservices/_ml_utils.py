c2c6e4207622fe4cff3cfd57d1e92ebf7a99cc34
CJ Ochs <carlochs@microsoft.com>
2025-10-30 15:54:29 +0000

    Add license headers

--- src/azure-cli/azure/cli/command_modules/cognitiveservices/_ml_utils.py
+++ src/azure-cli/azure/cli/command_modules/cognitiveservices/_ml_utils.py
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 from azure.mgmt.cognitiveservices.models import ConnectionCategory
 
 _ML_CONNECTION_TYPE_TO_COGNITIVE_SERVICES_CONNECTION_TYPE = {
