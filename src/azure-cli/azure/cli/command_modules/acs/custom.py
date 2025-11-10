1e7d5c7672928aa9b0b2d06e2b68c5db7a9908d7
FumingZhang <81607949+FumingZhang@users.noreply.github.com>
2025-11-10 13:48:58 +1100

    Update src/azure-cli/azure/cli/command_modules/acs/custom.py
    Co-authored-by: Yan Zhu <105691024+yanzhudd@users.noreply.github.com>

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -1286,7 +1286,7 @@ def aks_upgrade(cmd,
                 return None
             upgrade_all = True
         else:
-            msg = ("Since control-plane-only argument is specified, this will upgrade only the kubernetes version of the control plane to {}. "
+            msg = ("Since --control-plane-only parameter is specified, this will upgrade only the kubernetes version of the control plane to {}. "
                    "Kubernetes versions of the node pools will remain unchanged, "
                    "but node image version may be upgraded if there has been cluster config change that requires VM reimage. "
                    "Continue?").format(instance.kubernetes_version)
