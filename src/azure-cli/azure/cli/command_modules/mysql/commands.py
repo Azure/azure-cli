9b1419dba0d984e20fe1e157adad1d24c31ac93c
Haoran Hong <haoranhong@microsoft.com>
2025-11-20 16:27:32 +0800

    bug fix

--- src/azure-cli/azure/cli/command_modules/mysql/commands.py
+++ src/azure-cli/azure/cli/command_modules/mysql/commands.py
@@ -204,7 +204,7 @@ def load_command_table(self, _):
                             custom_command_type=mysql_custom,
                             client_factory=cf_mysql_flexible_export, 
                             is_preview=True,
-                            deprecate_info=self.deprecate(..., hide=True)) as g:
+                            deprecate_info=self.deprecate(hide=True)) as g:
         g.custom_command('create', 'flexible_server_export_create')
 
     with self.command_group('mysql flexible-server identity', mysql_flexible_servers_sdk,
