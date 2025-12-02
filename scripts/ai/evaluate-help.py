e9f81176dd199a05fb9931f8ba36e8b4ee4aa6fe
Ren Silva <resilv@microsoft.com>
2025-12-02 03:50:01 +0000

    added license headers to help analysers

--- scripts/ai/evaluate-help.py
+++ scripts/ai/evaluate-help.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 """
 Evaluate Azure CLI help documentation quality.
 
