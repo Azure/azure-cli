222dbe5957e8de1b1660fb367e85bc1dc187f1a6
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2025-12-04 11:18:50 +1100

    add warning msg to az sig image-version create/update

--- src/azure-cli/azure/cli/command_modules/vm/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/vm/_breaking_change.py
@@ -4,21 +4,17 @@
 # --------------------------------------------------------------------------------------------
 from azure.cli.core.breaking_change import register_default_value_breaking_change, register_other_breaking_change
 
-register_default_value_breaking_change(
-    command_name="vm create",
-    arg="--size",
-    current_default="Standard_DS1_v2",
-    new_default="Standard_D2s_v5",
-    target_version=None,
-)
+register_default_value_breaking_change(command_name='vm create',
+                                       arg='--size',
+                                       current_default='Standard_DS1_v2',
+                                       new_default='Standard_D2s_v5',
+                                       target_version=None)
 
-register_default_value_breaking_change(
-    command_name="vmss create",
-    arg="--vm-sku",
-    current_default="Standard_DS1_v2",
-    new_default="Standard_D2s_v5",
-    target_version=None,
-)
+register_default_value_breaking_change(command_name='vmss create',
+                                       arg='--vm-sku',
+                                       current_default='Standard_DS1_v2',
+                                       new_default='Standard_D2s_v5',
+                                       target_version=None)
 
 register_other_breaking_change(
     "sig image-version create",
