03b36c1417b88287ffa1e654deae37d449b34952
nvkhacker <63601005+nvkhacker@users.noreply.github.com>
2025-12-10 16:45:07 +0530

    Add test comment to custom.py

--- src/azure-cli/azure/cli/command_modules/policyinsights/custom.py
+++ src/azure-cli/azure/cli/command_modules/policyinsights/custom.py
@@ -2,6 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+# Test again
 
 from azure.mgmt.core.tools import is_valid_resource_id, resource_id
 
