d30b06e5b32aff12468538c5e7b657474c981ee2
nvkhacker <63601005+nvkhacker@users.noreply.github.com>
2025-12-10 16:39:40 +0530

    Add test comment in __init__.py

--- src/azure-cli/azure/cli/command_modules/policyinsights/__init__.py
+++ src/azure-cli/azure/cli/command_modules/policyinsights/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
+# Test
 # pylint: disable=unused-import
 
 from azure.cli.command_modules.policyinsights._help import helps
