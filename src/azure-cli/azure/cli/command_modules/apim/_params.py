604b8f3fc9d0797eaed0ed0123771ace304d6edf
Felipe Rezende <feliperezende.barbosa@gmail.com>
2025-12-22 10:58:18 -0300

    feat: added new subcommand for apim [backend]

--- src/azure-cli/azure/cli/command_modules/apim/_params.py
+++ src/azure-cli/azure/cli/command_modules/apim/_params.py
@@ -12,7 +12,7 @@ from azure.cli.core.commands.parameters import (get_enum_type,
                                                 resource_group_name_type,
                                                 get_three_state_flag)
 
-from azure.mgmt.apimanagement.models import (SkuType, VirtualNetworkType, Protocol, ApiType, ProductState)
+from azure.mgmt.apimanagement.models import (SkuType, VirtualNetworkType, Protocol, ApiType, ProductState, BackendProtocol)
 from azure.cli.command_modules.apim.actions import (TemplateParameter)
 
 
@@ -20,7 +20,7 @@ SKU_TYPES = SkuType
 VNET_TYPES = VirtualNetworkType
 API_PROTOCOLS = Protocol
 API_TYPES = ApiType
-
+BACKEND_PROTOCOLS = BackendProtocol
 
 class ImportFormat(Enum):
     Wadl = "Wadl"
@@ -51,6 +51,8 @@ def load_arguments(self, _):
                              help='API identifier. Must be unique in the current API Management service instance. Non-current revision has ;rev=n as a suffix where n is the revision number.')
     schema_id = CLIArgumentType(arg_group='Schema',
                                 help='Schema identifier. Must be unique in the current API Management service instance. Non-current revision has ;rev=n as a suffix where n is the revision number.')
+    backend_id = CLIArgumentType(arg_group='Backend',
+                                 help='Backend identifier. Must be unique in the current API Management service instance. Non-current revision has ;rev=n as a suffix where n is the revision number.')
 
     from azure.cli.core.commands.parameters import tags_type
     from azure.cli.core.commands.validators import get_default_location_from_resource_group
@@ -501,4 +503,16 @@ def load_arguments(self, _):
                    help='The name of the API Management service instance.')
         c.argument('api_id', arg_type=api_id)
         c.argument('resolver_id', help='Resolver identifier within a GraphQL API. Must be unique in the current API Management service instance.')
+        c.argument('if_match', help='ETag of the Entity.')
+
+    with self.argument_context('apim backend') as c:
+        c.argument('service_name', options_list=['--service-name', '-n'],
+                   help='The name of the API Management service instance.')
+        c.argument('backend_id', arg_type=backend_id, help='Identifier of the Backend.')
+        c.argument('url', help='Required. Backend service URL.')
+        c.argument('protocol', arg_type=get_enum_type(BACKEND_PROTOCOLS),
+                   help='Protocol used to communicate with the backend service. Possible values include: `http`, `soap`.')
+        c.argument('title', help='Backend title.')
+        c.argument('description', help='Description of the Backend. May include HTML formatting tags.')
+        c.argument('resource_id', help='Resource ID of the backend service.')
         c.argument('if_match', help='ETag of the Entity.')
