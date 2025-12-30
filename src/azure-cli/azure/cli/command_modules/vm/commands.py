d67c3c341ca5fbe43da53ac7f4c9602d54f94a2a
william051200 <williamng0512@gmail.com>
2025-12-30 14:35:42 +0800

    Removed unused parameter in commands.py

--- src/azure-cli/azure/cli/command_modules/vm/commands.py
+++ src/azure-cli/azure/cli/command_modules/vm/commands.py
@@ -283,7 +283,7 @@ def load_command_table(self, _):
         g.custom_command('remove', 'remove_template_error_handler', supports_local_cache=True)
         g.custom_show_command('show', 'show_template_error_handler', supports_local_cache=True)
 
-    with self.command_group('snapshot', compute_snapshot_sdk, operation_group='snapshots') as g:
+    with self.command_group('snapshot', operation_group='snapshots') as g:
         g.custom_command('create', 'create_snapshot', validator=process_snapshot_create_namespace, supports_no_wait=True)
         from .operations.snapshot import SnapshotUpdate
         self.command_table['snapshot update'] = SnapshotUpdate(loader=self)
