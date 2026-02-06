1f8f437c2234e631de46144b932e2b4c3666146f
ashutosh0x <ashutoshkumarsingh0x@gmail.com>
2026-02-06 16:56:56 +0530

    fix(vm): address security, reliability and path parsing feedback for 'az vm cp'

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -6825,21 +6825,58 @@ def list_vm_sizes(cmd, location):
 
 # endRegion
 
+def _is_windows_absolute_path(path):
+    """
+    Detect a Windows-style absolute path such as 'C:\\path\\to\\file' or 'D:/file.txt'.
+    This is a purely syntactic check and does not touch the filesystem.
+    """
+    return (
+        isinstance(path, str) and
+        len(path) >= 3 and
+        path[0].isalpha() and
+        path[1] == ':' and
+        path[2] in ('\\', '/')
+    )
+
+
 def _parse_vm_file_path(path):
+    # If there is no colon, this cannot be a VM path.
     if ':' not in path:
         return None
 
-    parts = path.split(':')
-    if len(parts) == 2:
+    # Do not treat Windows absolute paths like 'C:\\path\\to\\file' as VM paths.
+    if _is_windows_absolute_path(path):
+        return None
+
+    # VM path format is [resource-group-name:]vm-name:path-on-vm
+    # Only the first two colons separate components; any remaining colons belong to the path.
+    first_colon = path.find(':')
+    second_colon = path.find(':', first_colon + 1)
+
+    if first_colon == -1:
+        return None
+
+    if second_colon == -1:
         # vm-name:path
-        return None, parts[0], parts[1]
-    if len(parts) == 3:
-        # rg:vm-name:path
-        return parts[0], parts[1], parts[2]
-    return None
+        vm_name = path[:first_colon]
+        vm_path = path[first_colon + 1:]
+        if not vm_name or not vm_path:
+            return None
+        return None, vm_name, vm_path
+
+    # rg:vm-name:path...
+    rg_name = path[:first_colon]
+    vm_name = path[first_colon + 1:second_colon]
+    vm_path = path[second_colon + 1:]
+    if not rg_name or not vm_name or not vm_path:
+        return None
+    return rg_name, vm_name, vm_path
 
 
 def vm_cp(cmd, source, destination, storage_account=None, container_name='azvmcp'):
+    from azure.core.exceptions import ResourceExistsError
+    import shlex
+
     source_vm = _parse_vm_file_path(source)
     dest_vm = _parse_vm_file_path(destination)
 
@@ -6902,100 +6939,117 @@ def vm_cp(cmd, source, destination, storage_account=None, container_name='azvmcp
     container_client = blob_service_client.get_container_client(container_name)
     try:
         container_client.create_container()
-    except Exception: # Already exists or other error  # pylint: disable=broad-except
+    except ResourceExistsError:
         pass
 
     blob_name = str(uuid.uuid4())
-
     blob_client = container_client.get_blob_client(blob_name)
 
-    if dest_vm:
-        # UPLOAD: Local -> VM
-        rg, vm_name, vm_path = dest_vm
-        if not rg:
-            # find VM RG
-            client = _compute_client_factory(cmd.cli_ctx)
-            vms = client.virtual_machines.list_all()
-            vm = next((v for v in vms if v.name.lower() == vm_name.lower()), None)
-            rg = vm.id.split('/')[4]
+    try:
+        if dest_vm:
+            # UPLOAD: Local -> VM
+            rg, vm_name, vm_path = dest_vm
+            if not rg:
+                # find VM RG
+                client = _compute_client_factory(cmd.cli_ctx)
+                vms = client.virtual_machines.list_all()
+                vm = next((v for v in vms if v.name.lower() == vm_name.lower()), None)
+                rg = vm.id.split('/')[4]
 
-        logger.info("Uploading local file to bridge storage...")
-        upload_blob(cmd, blob_client, file_path=source)
+            logger.info("Uploading local file to bridge storage...")
+            upload_blob(cmd, blob_client, file_path=source)
 
-        # Get SAS for VM to download
-        sas_token = create_short_lived_blob_sas_v2(cmd, sa_name, container_name, blob_name, account_key=account_key)
-        blob_url = "https://{}.blob.{}/{}/{}?{}".format(sa_name, cmd.cli_ctx.cloud.suffixes.storage_endpoint, container_name, blob_name, sas_token)
+            # Get SAS for VM to download (2 hours expiry)
+            sas_token = create_short_lived_blob_sas_v2(cmd, sa_name, container_name, blob_name, account_key=account_key)
+            blob_url = "https://{}.blob.{}/{}/{}?{}".format(sa_name, cmd.cli_ctx.cloud.suffixes.storage_endpoint, container_name, blob_name, sas_token)
 
-        # VM run-command to download
-        # Check OS type
-        vm_obj = _compute_client_factory(cmd.cli_ctx).virtual_machines.get(rg, vm_name)
-        is_linux = vm_obj.storage_profile.os_disk.os_type.lower() == 'linux'
+            # VM run-command to download
+            # Check OS type
+            vm_obj = _compute_client_factory(cmd.cli_ctx).virtual_machines.get(rg, vm_name)
+            is_linux = vm_obj.storage_profile.os_disk.os_type.lower() == 'linux'
 
-        if is_linux:
-            script = "curl -L -o '{}' '{}'".format(vm_path, blob_url)
-            command_id = 'RunShellScript'
+            if is_linux:
+                script = "curl -L -f -s -S -o {} {}".format(shlex.quote(vm_path), shlex.quote(blob_url))
+                command_id = 'RunShellScript'
+            else:
+                # Escape single quotes for PowerShell
+                escaped_vm_path = vm_path.replace("'", "''")
+                escaped_blob_url = blob_url.replace("'", "''")
+                script = "Invoke-WebRequest -Uri '{}' -OutFile '{}'".format(escaped_blob_url, escaped_vm_path)
+                command_id = 'RunPowerShellScript'
+
+            logger.info("Executing download script in VM...")
+            from .aaz.latest.vm.run_command import Invoke
+            result = Invoke(cli_ctx=cmd.cli_ctx)(command_args={
+                'resource_group': rg,
+                'vm_name': vm_name,
+                'command_id': command_id,
+                'script': [script]
+            })
+            if result.get('value') and result['value'][0].get('message'):
+                message = result['value'][0]['message']
+                if 'failed' in message.lower() or 'error' in message.lower():
+                    raise CLIError("VM execution failed: {}".format(message))
+
         else:
-            script = "Invoke-WebRequest -Uri '{}' -OutFile '{}'".format(blob_url, vm_path)
-            command_id = 'RunPowerShellScript'
+            # DOWNLOAD: VM -> Local
+            rg, vm_name, vm_path = source_vm
+            if not rg:
+                # find VM RG
+                client = _compute_client_factory(cmd.cli_ctx)
+                vms = client.virtual_machines.list_all()
+                vm = next((v for v in vms if v.name.lower() == vm_name.lower()), None)
+                rg = vm.id.split('/')[4]
 
-        logger.info("Executing download script in VM...")
-        from .aaz.latest.vm.run_command import Invoke
-        Invoke(cli_ctx=cmd.cli_ctx)(command_args={
-            'resource_group': rg,
-            'vm_name': vm_name,
-            'command_id': command_id,
-            'script': [script]
-        })
+            # Get SAS with WRITE permission (2 hours expiry)
+            t_sas = cmd.get_models('_shared_access_signature#BlobSharedAccessSignature',
+                                   resource_type=ResourceType.DATA_STORAGE_BLOB)
+            t_blob_permissions = cmd.get_models('_models#BlobSasPermissions', resource_type=ResourceType.DATA_STORAGE_BLOB)
+            expiry = (datetime.utcnow() + timedelta(hours=2)).strftime('%Y-%m-%dT%H:%M:%SZ')
+            sas = t_sas(sa_name, account_key=account_key)
+            sas_token = sas.generate_blob(container_name, blob_name,
+                                          permission=t_blob_permissions(write=True),
+                                          expiry=expiry, protocol='https')
+            blob_url = "https://{}.blob.{}/{}/{}?{}".format(sa_name, cmd.cli_ctx.cloud.suffixes.storage_endpoint, container_name, blob_name, sas_token)
 
-        # Cleanup
-        logger.info("Cleaning up bridge storage...")
-        blob_client.delete_blob()
+            vm_obj = _compute_client_factory(cmd.cli_ctx).virtual_machines.get(rg, vm_name)
+            is_linux = vm_obj.storage_profile.os_disk.os_type.lower() == 'linux'
 
-    else:
-        # DOWNLOAD: VM -> Local
-        rg, vm_name, vm_path = source_vm
-        if not rg:
-             # find VM RG
-            client = _compute_client_factory(cmd.cli_ctx)
-            vms = client.virtual_machines.list_all()
-            vm = next((v for v in vms if v.name.lower() == vm_name.lower()), None)
-            rg = vm.id.split('/')[4]
+            if is_linux:
+                script = "curl -X PUT -T {} -H 'x-ms-blob-type: BlockBlob' {}".format(shlex.quote(vm_path), shlex.quote(blob_url))
+                command_id = 'RunShellScript'
+            else:
+                escaped_vm_path = vm_path.replace("'", "''")
+                escaped_blob_url = blob_url.replace("'", "''")
+                # Cross-version compatible Get-Content as suggested by Copilot
+                script = ("$body = if ($PSVersionTable.PSVersion.Major -ge 6) {{ Get-Content -Path '{}' -AsByteStream }} "
+                          "else {{ Get-Content -Path '{}' -Encoding Byte }}; "
+                          "Invoke-RestMethod -Uri '{}' -Method Put -Headers @{{'x-ms-blob-type'='BlockBlob'}} -Body $body").format(
+                              escaped_vm_path, escaped_vm_path, escaped_blob_url)
+                command_id = 'RunPowerShellScript'
 
-        # Get SAS with WRITE permission
-        t_sas = cmd.get_models('_shared_access_signature#BlobSharedAccessSignature',
-                               resource_type=ResourceType.DATA_STORAGE_BLOB)
-        t_blob_permissions = cmd.get_models('_models#BlobSasPermissions', resource_type=ResourceType.DATA_STORAGE_BLOB)
-        expiry = (datetime.utcnow() + timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SZ')
-        sas = t_sas(sa_name, account_key=account_key)
-        sas_token = sas.generate_blob(container_name, blob_name,
-                                      permission=t_blob_permissions(write=True),
-                                      expiry=expiry, protocol='https')
-        blob_url = "https://{}.blob.{}/{}/{}?{}".format(sa_name, cmd.cli_ctx.cloud.suffixes.storage_endpoint, container_name, blob_name, sas_token)
+            logger.info("Executing upload script in VM...")
+            from .aaz.latest.vm.run_command import Invoke
+            result = Invoke(cli_ctx=cmd.cli_ctx)(command_args={
+                'resource_group': rg,
+                'vm_name': vm_name,
+                'command_id': command_id,
+                'script': [script]
+            })
+            if result.get('value') and result['value'][0].get('message'):
+                message = result['value'][0]['message']
+                if 'failed' in message.lower() or 'error' in message.lower():
+                    raise CLIError("VM execution failed: {}".format(message))
 
-        vm_obj = _compute_client_factory(cmd.cli_ctx).virtual_machines.get(rg, vm_name)
-        is_linux = vm_obj.storage_profile.os_disk.os_type.lower() == 'linux'
+            logger.info("Downloading from bridge storage to local...")
+            download_blob(blob_client, file_path=destination)
 
-        if is_linux:
-            script = "curl -X PUT -T '{}' -H 'x-ms-blob-type: BlockBlob' '{}'".format(vm_path, blob_url)
-            command_id = 'RunShellScript'
-        else:
-            script = "$body = Get-Content -Path '{}' -Encoding Byte; Invoke-RestMethod -Uri '{}' -Method Put -Headers @{{'x-ms-blob-type'='BlockBlob'}} -Body $body".format(vm_path, blob_url)
-            command_id = 'RunPowerShellScript'
-
-        logger.info("Executing upload script in VM...")
-        from .aaz.latest.vm.run_command import Invoke
-        Invoke(cli_ctx=cmd.cli_ctx)(command_args={
-            'resource_group': rg,
-            'vm_name': vm_name,
-            'command_id': command_id,
-            'script': [script]
-        })
-
-        logger.info("Downloading from bridge storage to local...")
-        download_blob(blob_client, file_path=destination)
-
-        # Cleanup
-        logger.info("Cleaning up bridge storage...")
-        blob_client.delete_blob()
+    finally:
+        # Cleanup bridge storage
+        try:
+            logger.info("Cleaning up bridge storage...")
+            blob_client.delete_blob()
+        except Exception:  # pylint: disable=broad-except
+            pass
 
     return {"message": "File transfer successful."}
--- src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_cp_unit.py
+++ src/azure-cli/azure/cli/command_modules/vm/tests/latest/test_vm_cp_unit.py
@@ -10,9 +10,11 @@ from azure.cli.command_modules.vm.custom import _parse_vm_file_path, vm_cp
 class TestVmCp(unittest.TestCase):
 
     def test_parse_vm_file_path(self):
-        # Local path
+        # Local paths (non-VM)
         self.assertIsNone(_parse_vm_file_path("/path/to/file"))
         self.assertIsNone(_parse_vm_file_path("C:\\path\\to\\file"))
+        self.assertIsNone(_parse_vm_file_path("D:/path/to/file"))
+        self.assertIsNone(_parse_vm_file_path("justfile"))
         
         # VM path: vm:path
         self.assertEqual(_parse_vm_file_path("myvm:/tmp/file"), (None, "myvm", "/tmp/file"))
@@ -20,16 +22,17 @@ class TestVmCp(unittest.TestCase):
         # VM path: rg:vm:path
         self.assertEqual(_parse_vm_file_path("myrg:myvm:/tmp/file"), ("myrg", "myvm", "/tmp/file"))
         
-        # Edge cases
-        self.assertIsNone(_parse_vm_file_path("justfile"))
-        self.assertEqual(_parse_vm_file_path("vm:C:\\path"), (None, "vm", "C:\\path"))
-        self.assertEqual(_parse_vm_file_path("rg:vm:C:\\path"), ("rg", "vm", "C:\\path"))
+        # VM path with colons in the path component
+        self.assertEqual(_parse_vm_file_path("vm:C:\\remote\\path"), (None, "vm", "C:\\remote\\path"))
+        self.assertEqual(_parse_vm_file_path("rg:vm:C:\\remote\\path"), ("rg", "vm", "C:\\remote\\path"))
 
     @mock.patch('azure.cli.command_modules.vm.custom._compute_client_factory')
-    @mock.patch('azure.cli.command_modules.vm.custom.get_storage_client_factory')
+    @mock.patch('azure.cli.command_modules.vm.custom.cf_sa')
+    @mock.patch('azure.cli.command_modules.vm.custom.cf_sa_for_keys')
+    @mock.patch('azure.cli.command_modules.vm.custom.cf_blob_service')
     @mock.patch('azure.cli.command_modules.vm.custom.create_short_lived_blob_sas_v2')
     @mock.patch('azure.cli.command_modules.vm.custom.upload_blob')
-    def test_vm_cp_upload_basic(self, mock_upload, mock_sas, mock_storage_factory, mock_compute_factory):
+    def test_vm_cp_upload_basic(self, mock_upload, mock_sas, mock_blob_factory, mock_keys_factory, mock_sa_factory, mock_compute_factory):
         cmd = mock.MagicMock()
         cmd.cli_ctx.cloud.suffixes.storage_endpoint = 'core.windows.net'
         
@@ -39,31 +42,42 @@ class TestVmCp(unittest.TestCase):
         
         vm_obj = mock.MagicMock()
         vm_obj.storage_profile.os_disk.os_type.lower.return_value = 'linux'
+        vm_obj.id = "/subscriptions/sub/resourceGroups/myrg/providers/Microsoft.Compute/virtualMachines/myvm"
         mock_compute.virtual_machines.get.return_value = vm_obj
+        mock_compute.virtual_machines.list_all.return_value = [vm_obj]
         
-        # Mock storage client
-        mock_storage = mock.MagicMock()
-        mock_storage_factory.return_value = mock_storage
+        # Mock storage clients
+        mock_sa = mock.MagicMock()
+        mock_sa_factory.return_value = mock_sa
         
         sa = mock.MagicMock()
         sa.name = 'mystorage'
-        mock_storage.storage_accounts.list_by_resource_group.return_value = [sa]
+        sa.id = "/subscriptions/sub/resourceGroups/myrg/providers/Microsoft.Storage/storageAccounts/mystorage"
+        mock_sa.list.return_value = [sa]
         
+        mock_keys = mock.MagicMock()
+        mock_keys_factory.return_value = mock_keys
         key = mock.MagicMock()
         key.value = 'key1'
-        mock_storage.storage_accounts.list_keys.return_value.keys = [key]
+        mock_keys.list_keys.return_value.keys = [key]
         
-        # Mock blob client
-        with mock.patch('azure.cli.command_modules.vm.custom.BlobServiceClient') as mock_blob_service:
-            mock_container = mock.MagicMock()
-            mock_blob_service.from_connection_string.return_value.get_container_client.return_value = mock_container
-            
-            # Execute
-            vm_cp(cmd, source="local.txt", destination="myrg:myvm:/tmp/remote.txt")
+        # Mock blob service
+        mock_blob_service = mock.MagicMock()
+        mock_blob_factory.return_value = mock_blob_service
+        mock_container = mock.MagicMock()
+        mock_blob_service.get_container_client.return_value = mock_container
+        mock_blob = mock.MagicMock()
+        mock_container.get_blob_client.return_value = mock_blob
+        
+        # Execute
+        with mock.patch('azure.cli.command_modules.vm.custom.Invoke') as mock_invoke:
+            mock_invoke.return_value.return_value = {'value': [{'message': 'success'}]}
+            vm_cp(cmd, source="local.txt", destination="myvm:/tmp/remote.txt")
             
             # Verify
             mock_upload.assert_called_once()
-            mock_compute.virtual_machines.get.assert_called_with("myrg", "myvm")
+            mock_compute.virtual_machines.get.assert_called()
+            mock_blob.delete_blob.assert_called_once()
 
 if __name__ == '__main__':
     unittest.main()
