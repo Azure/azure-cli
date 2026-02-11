97767d173992bdd2de625e1ac955dbbf92aa93db
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2026-02-11 15:42:58 +1100

    enable_broker_on_mac flag change

--- src/azure-cli-core/azure/cli/core/_profile.py
+++ src/azure-cli-core/azure/cli/core/_profile.py
@@ -908,8 +908,8 @@ def _create_identity_instance(cli_ctx, authority, tenant_id=None, client_id=None
 
     # On Windows, use core.enable_broker_on_windows=false to disable broker (WAM) for authentication.
     enable_broker_on_windows = cli_ctx.config.getboolean('core', 'enable_broker_on_windows', fallback=True)
-    # On macOS, use core.enable_broker_on_mac=true to enable broker for authentication.
-    enable_broker_on_mac = cli_ctx.config.getboolean('core', 'enable_broker_on_mac', fallback=False)
+    # On macOS, use core.enable_broker_on_mac=false to disable broker for authentication.
+    enable_broker_on_mac = cli_ctx.config.getboolean('core', 'enable_broker_on_mac', fallback=True)
     from .telemetry import set_broker_info
     set_broker_info(enable_broker_on_windows, enable_broker_on_mac)
 
