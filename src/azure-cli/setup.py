53bcc4d7f75914d7a523afdd98a42217e64753b7
Naia Scott <nasc@microsoft.com>
2026-02-17 17:49:56 -0500

    update sdk

--- src/azure-cli/setup.py
+++ src/azure-cli/setup.py
@@ -100,7 +100,7 @@ DEPENDENCIES = [
     'azure-mgmt-msi~=7.1.0',
     'azure-mgmt-netapp~=10.1.0',
     'azure-mgmt-policyinsights==1.1.0b4',
-    'azure-mgmt-postgresqlflexibleservers==2.0.0',
+    'azure-mgmt-postgresqlflexibleservers==3.0.0b1',
     'azure-mgmt-privatedns~=1.0.0',
     'azure-mgmt-rdbms==10.2.0b17',
     'azure-mgmt-mysqlflexibleservers==1.1.0b2',
