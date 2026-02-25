cb775a45415a708ca220d1a37918257450ee8760
Alexandre Mougel <alex.mougle@proton.me>
2026-02-25 23:59:55 +0100

    Added unit tests in Cloud module

--- src/azure-cli/azure/cli/command_modules/cloud/tests/latest/test_cloud.py
+++ src/azure-cli/azure/cli/command_modules/cloud/tests/latest/test_cloud.py
@@ -43,6 +43,11 @@ class CloudTests(ScenarioTest):
         self.cmd('az cloud show -n AzureGermanCloud', checks=[self.check('isActive', True)])
 
     @serial_test()
+    def test_cloud_set_BleuCloud(self):
+        self.cmd('az cloud set -n BleuCloud')
+        self.cmd('az cloud show -n BleuCloud', checks=[self.check('isActive', True)])
+
+    @serial_test()
     def test_cloud_set_azurecloud(self):
         self.cmd('az cloud set -n azurecloud')
         self.cmd('az cloud show -n AzureCloud', checks=[self.check('isActive', True)])
@@ -61,6 +66,11 @@ class CloudTests(ScenarioTest):
     def test_cloud_set_azuregermancloud(self):
         self.cmd('az cloud set -n azuregermancloud')
         self.cmd('az cloud show -n AzureGermanCloud', checks=[self.check('isActive', True)])
+
+    @serial_test()
+    def test_cloud_set_bleucloud(self):
+        self.cmd('az cloud set -n bleucloud')
+        self.cmd('az cloud show -n BleuCloud', checks=[self.check('isActive', True)])
 
     @serial_test()
     def test_cloud_set_unregistered_cloud_name(self):
