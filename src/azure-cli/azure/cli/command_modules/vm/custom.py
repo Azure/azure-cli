87767e0ba7195ba09779a07543f94b4e10a02902
william051200 <williamng0512@gmail.com>
2026-03-06 10:27:12 +0800

    Update code

--- src/azure-cli/azure/cli/command_modules/vm/custom.py
+++ src/azure-cli/azure/cli/command_modules/vm/custom.py
@@ -6243,13 +6243,13 @@ def gallery_application_version_update(client,
                        gallery_application_version=gallery_application_version)
 
 
-def create_capacity_reservation_group(cmd, resource_group_name, capacity_reservation_group_name, location=None,
-                                      tags=None, zones=None, sharing_profile=None):
+def create_capacity_reservation_group(cmd, resource_group_name, capacity_reservation_group_name, location, tags=None,
+                                      zones=None, sharing_profile=None):
     from .aaz.latest.capacity.reservation.group import Create as CapacityReservationGroupCreate
     command_args = {
         'capacity_reservation_group_name': capacity_reservation_group_name,
         'resource_group': resource_group_name,
-        'location': location or _get_resource_group_location(cmd.cli_ctx, resource_group_name)
+        'location': location
     }
 
     if tags is not None:
