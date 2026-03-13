be85aa0ef158f6aa4100fafec300d3b5c4fa7711
Julie Zhu <zhuyan@microsoft.com>
2026-03-13 13:58:50 +1100

    set to static api versions

--- src/azure-cli/azure/cli/command_modules/vm/_vm_utils.py
+++ src/azure-cli/azure/cli/command_modules/vm/_vm_utils.py
@@ -60,8 +60,6 @@ def _resolve_api_version(cli_ctx, provider_namespace, resource_type, parent_path
     if not rt:
         raise CLIError('Resource type {} not found.'.format(resource_type_str))
     if len(rt) == 1 and rt[0].api_versions:
-        if rt[0].default_api_version:
-            return rt[0].default_api_version
         npv = [v for v in rt[0].api_versions if 'preview' not in v.lower()]
         return npv[0] if npv else rt[0].api_versions[0]
     raise CLIError(
@@ -96,7 +94,7 @@ def check_existence(cli_ctx, value, resource_group, provider_namespace, resource
         resource_type = id_parts.get('type', resource_type)
 
     api_version = _resolve_api_version(cli_ctx, provider_namespace, resource_type, parent_path)
-    if static_version:  # only for vnet
+    if static_version:
         api_version = static_version
 
     try:
