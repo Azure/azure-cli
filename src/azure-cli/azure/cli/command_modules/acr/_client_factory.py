278968f84e44327ee9a4c96285b98b4a331eb826
clarkporter <clarkporter@microsoft.com>
2026-03-17 23:26:15 +0000

    Implement ACR tasks service client and update related functions

--- src/azure-cli/azure/cli/command_modules/acr/_client_factory.py
+++ src/azure-cli/azure/cli/command_modules/acr/_client_factory.py
@@ -22,6 +22,12 @@ def get_acr_service_client(cli_ctx, api_version=None):
     return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_CONTAINERREGISTRY, api_version=api_version)
 
 
+def get_acr_tasks_service_client(cli_ctx, api_version=None):
+    """Returns the client for managing container registry tasks."""
+    from azure.cli.core.profiles import ResourceType
+    return get_mgmt_service_client(cli_ctx, ResourceType.MGMT_CONTAINERREGISTRYTASKS, api_version=api_version)
+
+
 # The function is used in Azure and Edge and hybrid profile is used to support the different API versions.
 def cf_acr_registries(cli_ctx, *_):
     return get_acr_service_client(cli_ctx).registries
@@ -40,7 +46,7 @@ def cf_acr_network_rules(cli_ctx, *_):
 
 
 def cf_acr_registries_tasks(cli_ctx, *_):
-    return get_acr_service_client(cli_ctx, api_version=VERSION_2025_03_01_PREVIEW).registries
+    return get_acr_tasks_service_client(cli_ctx).registries
 
 
 def cf_acr_replications(cli_ctx, *_):
@@ -56,15 +62,15 @@ def cf_acr_private_endpoint_connections(cli_ctx, *_):
 
 
 def cf_acr_tasks(cli_ctx, *_):
-    return get_acr_service_client(cli_ctx, VERSION_2025_03_01_PREVIEW).tasks
+    return get_acr_tasks_service_client(cli_ctx).tasks
 
 
 def cf_acr_taskruns(cli_ctx, *_):
-    return get_acr_service_client(cli_ctx, VERSION_2025_03_01_PREVIEW).task_runs
+    return get_acr_tasks_service_client(cli_ctx).task_runs
 
 
 def cf_acr_runs(cli_ctx, *_):
-    return get_acr_service_client(cli_ctx, VERSION_2025_03_01_PREVIEW).runs
+    return get_acr_tasks_service_client(cli_ctx).runs
 
 
 def cf_acr_scope_maps(cli_ctx, *_):
@@ -80,7 +86,7 @@ def cf_acr_token_credentials(cli_ctx, *_):
 
 
 def cf_acr_agentpool(cli_ctx, *_):
-    return get_acr_service_client(cli_ctx, VERSION_2025_03_01_PREVIEW).agent_pools
+    return get_acr_tasks_service_client(cli_ctx).agent_pools
 
 
 def cf_acr_connected_registries(cli_ctx, *_):
