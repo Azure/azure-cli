edb32866519d8dc3ebbf9d8f7b2245c4de68c938
clarkporter <clarkporter@microsoft.com>
2026-03-23 19:07:19 +0000

    Remove outdated ACR API versions and retain only the latest version 2026-01-01-preview

--- src/azure-cli-core/azure/cli/core/profiles/_shared.py
+++ src/azure-cli-core/azure/cli/core/profiles/_shared.py
@@ -229,16 +229,8 @@ AD_HOC_API_VERSIONS = {
         'app_service_certificate_orders': '2022-09-01'
     },
     ResourceType.MGMT_CONTAINERREGISTRY: {
-        # src/azure-cli/azure/cli/command_modules/acr/_client_factory.py:8
-        'VERSION_2019_05_01_PREVIEW': "2019-05-01-preview",
-        'VERSION_2019_06_01_PREVIEW': "2019-06-01-preview",
-        'VERSION_2020_11_01_PREVIEW': "2020-11-01-preview",
+        # src/azure-cli/azure/cli/command_modules/acr/_client_factory.py
         'VERSION_2021_08_01_PREVIEW': "2021-08-01-preview",
-        'VERSION_2022_02_01_PREVIEW': "2022-02-01-preview",
-        'VERSION_2023_11_01_PREVIEW': "2023-11-01-preview",
-        'VERSION_2024_11_01_PREVIEW': "2024-11-01-preview",
-        'VERSION_2025_03_01_PREVIEW': "2025-03-01-preview",
-        'VERSION_2025_04_01': "2025-04-01",
         'VERSION_2026_01_01_PREVIEW': "2026-01-01-preview"
     },
     ResourceType.MGMT_MSI: {
