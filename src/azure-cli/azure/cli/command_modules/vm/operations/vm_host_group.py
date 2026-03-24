6dcfef8bef1566a3f6e5f9a2effafdf48c81e647
william051200 <williamng0512@gmail.com>
2026-03-24 16:16:43 +0800

    Update code

--- src/azure-cli/azure/cli/command_modules/vm/operations/vm_host_group.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/vm_host_group.py
@@ -5,7 +5,8 @@
 # pylint: disable=no-self-use, line-too-long, protected-access, too-few-public-methods, unused-argument
 from knack.log import get_logger
 
-from ..aaz.latest.vm.host.group import Show as _VMHostGroupShow
+from azure.cli.core.aaz import AAZBoolArg, AAZStrArg
+from ..aaz.latest.vm.host.group import Show as _VMHostGroupShow, Update as _VMHostGroupUpdate
 
 logger = get_logger(__name__)
 
@@ -18,6 +19,31 @@ class VMHostGroupShow(_VMHostGroupShow):
         args_schema.expand._registered = False
 
         return args_schema
+
+
+class VMHostGroupUpdate(_VMHostGroupUpdate):
+    @classmethod
+    def _build_arguments_schema(cls, *args, **kwargs):
+        args_schema = super()._build_arguments_schema(*args, **kwargs)
+
+        args_schema.acquire_policy_token = AAZBoolArg(
+            options=["--acquire-policy-token"],
+            arg_group="Global Policy",
+            help="Acquiring an Azure Policy token automatically for this resource operation",
+            nullable=True,
+        )
+
+        args_schema.change_reference = AAZStrArg(
+            options=["--change-reference"],
+            arg_group="Global Policy",
+            help="The related change reference ID for this resource operation"
+        )
+
+        return args_schema
+
+    def pre_operations(self):
+        self.cli_ctx.data['_acquire_policy_token'] = self.ctx.args.acquire_policy_token
+        self.cli_ctx.data['_change_reference'] = self.ctx.args.change_reference
 
 
 def convert_show_result_to_snake_case(result):
