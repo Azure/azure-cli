ef587ad2181bc50dd60ec9aa8dfa3f343098f75d
Qinkai Wu <qinkaiwu@microsoft.com>
2026-03-25 16:11:05 +1100

    Add legacy fallback support

---
+++ src/azure-cli/azure/cli/command_modules/monitor/_legacy/grammar/metric_alert/__init__.py
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+# pylint: disable=unused-import
+from .MetricAlertConditionLexer import MetricAlertConditionLexer
+from .MetricAlertConditionParser import MetricAlertConditionParser
+from .MetricAlertConditionListener import MetricAlertConditionListener
+from .MetricAlertConditionValidator import MetricAlertConditionValidator
