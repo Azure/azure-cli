9986486e7d899f3722a2a5e197c516c3d39acac7
Fuming Zhang <fumingzhang@microsoft.com>
2026-04-09 15:48:43 +1000

    fix

--- src/azure-cli/azure/cli/command_modules/acs/custom.py
+++ src/azure-cli/azure/cli/command_modules/acs/custom.py
@@ -2161,8 +2161,8 @@ def aks_check_acr(cmd, client, resource_group_name, name, acr, node_name=None):
         # Get kubectl minor version
         kubectl_minor_version = -1
         try:
-            cmd = ["kubectl", "version", "-o", "json", "--kubeconfig", browse_path]
-            output = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            kubectl_cmd = ["kubectl", "version", "-o", "json", "--kubeconfig", browse_path]
+            output = subprocess.Popen(kubectl_cmd, stdout=subprocess.PIPE)
             jsonS, _ = output.communicate()
             kubectl_version = json.loads(jsonS)
             # Remove any non-numeric characters like + from minor version
