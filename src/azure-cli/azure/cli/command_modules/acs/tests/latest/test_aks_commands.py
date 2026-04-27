fdea7c392f32446d7cf341cf8f97c20386e9a7b4
Mauricio Ferrari <meece@MeeceSurface.localdomain>
2026-04-27 19:00:16 -0400

    fixing spacing

--- src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_aks_commands.py
+++ src/azure-cli/azure/cli/command_modules/acs/tests/latest/test_aks_commands.py
@@ -15143,6 +15143,8 @@ spec:
         # delete
         self.cmd(
             'aks delete -g {resource_group} -n {name} --yes --no-wait', checks=[self.is_empty()])
+
+    @AllowLargeResponse()
     @AKSCustomResourceGroupPreparer(
         random_name_length=17, name_prefix="clitest", location="centraluseuap"
     )
