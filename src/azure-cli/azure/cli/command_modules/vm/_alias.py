e4750cb567f650ec11db2988a6cc8d341f6015b1
william051200 <williamng0512@gmail.com>
2026-05-04 11:44:32 +0800

    Update vm alias with new aliases

--- src/azure-cli/azure/cli/command_modules/vm/_alias.py
+++ src/azure-cli/azure/cli/command_modules/vm/_alias.py
@@ -66,6 +66,27 @@ alias_json = """
             "sku":  "22_04-lts-gen2",
             "version":  "latest",
             "architecture": "x64"
+          },
+          "AzureLinux4": {
+            "publisher": "microsoftazurelinux",
+            "offer": "azurelinux-4",
+            "sku": "4",
+            "version": "latest",
+            "architecture": "x64"
+          },
+          "AzureLinux4Arm64": {
+            "publisher": "microsoftazurelinux",
+            "offer": "azurelinux-4",
+            "sku": "4-arm64",
+            "version": "latest",
+            "architecture": "Arm64"
+          },
+          "AzureLinux4Gen1": {
+            "publisher": "microsoftazurelinux",
+            "offer": "azurelinux-4",
+            "sku": "4-gen1",
+            "version": "latest",
+            "architecture": "x64"
           }
         },
         "Windows": {
