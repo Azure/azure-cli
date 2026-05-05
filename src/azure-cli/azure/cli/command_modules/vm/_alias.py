00c26b6e7bf56ca80528fe9867122ebe28f4991b
william051200 <williamng0512@gmail.com>
2026-05-06 06:59:08 +0800

    Updated alias sequence

--- src/azure-cli/azure/cli/command_modules/vm/_alias.py
+++ src/azure-cli/azure/cli/command_modules/vm/_alias.py
@@ -18,6 +18,27 @@ alias_json = """
       "type": "object",
       "value": {
         "Linux": {
+          "AzureLinux4": {
+            "publisher": "microsoftazurelinux",
+            "offer": "azurelinux-4",
+            "sku": "4",
+            "version": "latest",
+            "architecture": "x64"
+          },
+          "AzureLinux4Arm64": {
+            "publisher": "microsoftazurelinux",
+            "offer": "azurelinux-4",
+            "sku": "4-arm64",
+            "version": "latest",
+            "architecture": "Arm64"
+          },
+          "AzureLinux4Gen1": {
+            "publisher": "microsoftazurelinux",
+            "offer": "azurelinux-4",
+            "sku": "4-gen1",
+            "version": "latest",
+            "architecture": "x64"
+          },
           "CentOS85Gen2":  {
             "publisher":  "OpenLogic",
             "offer":  "CentOS",
@@ -65,27 +86,6 @@ alias_json = """
             "offer":  "0001-com-ubuntu-server-jammy",
             "sku":  "22_04-lts-gen2",
             "version":  "latest",
-            "architecture": "x64"
-          },
-          "AzureLinux4": {
-            "publisher": "microsoftazurelinux",
-            "offer": "azurelinux-4",
-            "sku": "4",
-            "version": "latest",
-            "architecture": "x64"
-          },
-          "AzureLinux4Arm64": {
-            "publisher": "microsoftazurelinux",
-            "offer": "azurelinux-4",
-            "sku": "4-arm64",
-            "version": "latest",
-            "architecture": "Arm64"
-          },
-          "AzureLinux4Gen1": {
-            "publisher": "microsoftazurelinux",
-            "offer": "azurelinux-4",
-            "sku": "4-gen1",
-            "version": "latest",
             "architecture": "x64"
           }
         },
