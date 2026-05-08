7b425db5fed454ff5d58ef1855642841a1d73731
Matthew Boentoro <mboentoro@microsoft.com>
2026-05-08 10:43:23 -0700

    [POSTGRESQL] BREAKING CHANGE: `az postgres flexible-server backup create`: Remove backup name requirement and implement automatic name generation for backups

--- src/azure-cli/azure/cli/command_modules/postgresql/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/postgresql/_breaking_change.py
@@ -53,11 +53,6 @@ register_command_group_deprecate(command_group='postgres flexible-server long-te
                                  message='Long term retention command group will be removed. '
                                  'For more information, open a support incident.')
 
-# Name of new backup no longer required in backup create command
-register_other_breaking_change('postgres flexible-server backup create',
-                               message='The argument for backup name will no longer be required '
-                               'in next breaking change release(2.86.0) scheduled for May 2026.')
-
 # LTR command argument changes
 register_other_breaking_change('postgres flexible-server long-term-retention',
                                message='The --backup-name/-b argument has been deprecated and will be removed '
