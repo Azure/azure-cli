3819b4943dadebc795a728ba201ca08af6f8091a
Nacho Alonso Portillo <ialonso@microsoft.com>
2026-05-19 16:51:03 +0200

    Remove obsolete test file for PostgreSQL flexible server long-term retention commands

--- src/azure-cli/azure/cli/command_modules/postgresql/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/postgresql/_breaking_change.py
@@ -2,7 +2,3 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-
-from azure.cli.core.breaking_change import (
-    register_argument_deprecate
-    )
