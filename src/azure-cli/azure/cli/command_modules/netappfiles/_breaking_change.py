30fec7a170c4bf76680b514f3662e4181dbef2ea
audunn <audunnb@gmail.com>
2026-05-26 09:39:24 +0000

    clean breaking change

--- src/azure-cli/azure/cli/command_modules/netappfiles/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/_breaking_change.py
@@ -2,18 +2,3 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
-from azure.cli.core.breaking_change import (
-    register_argument_deprecate,
-)
-register_argument_deprecate('netappfiles volume create', '--is-default-quota-enabled',
-                            redirect='netappfiles volume quota-rule')
-register_argument_deprecate('netappfiles volume update', '--is-default-quota-enabled',
-                            redirect='netappfiles volume quota-rule')
-register_argument_deprecate('netappfiles volume create', '--default-group-quota-in-ki-bs',
-                            redirect='netappfiles volume quota-rule')
-register_argument_deprecate('netappfiles volume update', '--default-group-quota-in-ki-bs',
-                            redirect='netappfiles volume quota-rule')
-register_argument_deprecate('netappfiles volume create', '--default-user-quota-in-ki-bs',
-                            redirect='netappfiles volume quota-rule')
-register_argument_deprecate('netappfiles volume update', '--default-user-quota-in-ki-bs',
-                            redirect='netappfiles volume quota-rule')
