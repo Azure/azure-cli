e70021314326e5d72da371bf68106500ac9bcbb1
Dylan Jones <dylan.jones@bjss.com>
2026-05-26 05:09:00 +0100

    [App Service] Fix #31394: `az functionapp deployment source config-zip`: Never ending loop on flex function app health check (#33388)

--- src/azure-cli/azure/cli/command_modules/appservice/custom.py
+++ src/azure-cli/azure/cli/command_modules/appservice/custom.py
@@ -824,6 +824,9 @@ def check_flex_app_after_deployment(cmd, resource_group_name, name):
                                 verify=not should_disable_connection_verify())
         if 200 <= response.status_code <= 299:
             break
+        if response.status_code == 403 and response.reason == 'Ip Forbidden':
+            return "Deployment was successful but health check failed due to IP restriction."
+        num_trials = num_trials + 1
 
     if response.status_code != 200:
         raise CLIError("Deployment was successful but the app appears to be unhealthy. Please "
