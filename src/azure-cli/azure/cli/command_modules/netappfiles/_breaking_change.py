d62067dc4c5b7ea24cae598f5546de5c0171fd2e
audunn <audunnb@gmail.com>
2026-05-26 17:07:28 +0000

    add subvolume breaking change notice

--- src/azure-cli/azure/cli/command_modules/netappfiles/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/_breaking_change.py
@@ -2,3 +2,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
+from azure.cli.core.breaking_change import (
+    register_command_group_deprecate,
+)
+register_command_group_deprecate('netappfiles subvolume', message='netappfiles subvolume command group is being deprecated '\
+                                 'and will be removed in a future release.')
