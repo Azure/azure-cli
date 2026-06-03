32496eda08ea26a954d388ecfe00301fd8f087fb
audunn <audunnb@gmail.com>
2026-06-03 12:25:10 +0000

    Style

--- src/azure-cli/azure/cli/command_modules/netappfiles/_breaking_change.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/_breaking_change.py
@@ -9,6 +9,8 @@ from azure.cli.core.breaking_change import (
 register_command_group_deprecate('netappfiles subvolume', message='netappfiles subvolume command group is '
                                  'being deprecated and will be removed in a future release.')
 
-register_argument_deprecate('netappfiles volume create', '--enable-subvolumes', message='--enable-subvolumes is being deprecated and will be removed in a future release.')
+register_argument_deprecate('netappfiles volume create', '--enable-subvolumes', message='--enable-subvolumes is '
+                            'being deprecated and will be removed in a future release.')
 
-register_argument_deprecate('netappfiles volume update', '--enable-subvolumes', message='--enable-subvolumes is being deprecated and will be removed in a future release.')
+register_argument_deprecate('netappfiles volume update', '--enable-subvolumes', message='--enable-subvolumes is '
+                            'being deprecated and will be removed in a future release.')
