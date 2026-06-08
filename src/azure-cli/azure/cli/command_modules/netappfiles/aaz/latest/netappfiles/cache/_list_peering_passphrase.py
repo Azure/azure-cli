432cdcbd08bb59db218884f6f6741f641a33daa8
audunn <audunnb@gmail.com>
2026-06-08 20:00:50 +0000

    Bump api-version to 2026-04-01

--- src/azure-cli/azure/cli/command_modules/netappfiles/aaz/latest/netappfiles/cache/_list_peering_passphrase.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/aaz/latest/netappfiles/cache/_list_peering_passphrase.py
@@ -22,9 +22,9 @@ class ListPeeringPassphrase(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2026-01-01",
+        "version": "2026-04-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.netapp/netappaccounts/{}/capacitypools/{}/caches/{}/listpeeringpassphrases", "2026-01-01"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.netapp/netappaccounts/{}/capacitypools/{}/caches/{}/listpeeringpassphrases", "2026-04-01"],
         ]
     }
 
@@ -153,7 +153,7 @@ class ListPeeringPassphrase(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2026-01-01",
+                    "api-version", "2026-04-01",
                     required=True,
                 ),
             }
--- src/azure-cli/azure/cli/command_modules/netappfiles/aaz/latest/netappfiles/volume/export_policy/_add.py
+++ src/azure-cli/azure/cli/command_modules/netappfiles/aaz/latest/netappfiles/volume/export_policy/_add.py
@@ -22,9 +22,9 @@ class Add(AAZCommand):
     """
 
     _aaz_info = {
-        "version": "2026-01-01",
+        "version": "2026-04-01",
         "resources": [
-            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.netapp/netappaccounts/{}/capacitypools/{}/volumes/{}", "2026-01-01", "properties.exportPolicy.rules[]"],
+            ["mgmt-plane", "/subscriptions/{}/resourcegroups/{}/providers/microsoft.netapp/netappaccounts/{}/capacitypools/{}/volumes/{}", "2026-04-01", "properties.exportPolicy.rules[]"],
         ]
     }
 
@@ -281,7 +281,7 @@ class Add(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2026-01-01",
+                    "api-version", "2026-04-01",
                     required=True,
                 ),
             }
@@ -388,7 +388,7 @@ class Add(AAZCommand):
         def query_parameters(self):
             parameters = {
                 **self.serialize_query_param(
-                    "api-version", "2026-01-01",
+                    "api-version", "2026-04-01",
                     required=True,
                 ),
             }
