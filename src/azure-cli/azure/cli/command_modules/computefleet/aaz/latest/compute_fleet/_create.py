9422ea89dff69ab7df2afa2cf2e3ff5dad642547
samudra <sabaner@microsoft.com>
2026-06-16 13:00:04 -0700

    Fix CI error

--- src/azure-cli/azure/cli/command_modules/computefleet/aaz/latest/compute_fleet/_create.py
+++ src/azure-cli/azure/cli/command_modules/computefleet/aaz/latest/compute_fleet/_create.py
@@ -978,7 +978,9 @@ class Create(AAZCommand):
         os_profile.admin_password = AAZPasswordArg(
             options=["admin-password"],
             help="Specifies the password of the administrator account. <br><br> **Minimum-length (Windows):** 8 characters <br><br> **Minimum-length (Linux):** 6 characters <br><br> **Max-length (Windows):** 123 characters <br><br> **Max-length (Linux):** 72 characters <br><br> **Complexity requirements:** 3 out of 4 conditions below need to be fulfilled <br> Has lower characters <br>Has upper characters <br> Has a digit <br> Has a special character (Regex match [\\W_]) <br><br> **Disallowed values:** \"abc@123\", \"P@$$w0rd\", \"P@ssw0rd\", \"P@ssword123\", \"Pa$$word\", \"pass@word1\", \"Password!\", \"Password1\", \"Password22\", \"iloveyou!\" <br><br> For resetting the password, see [How to reset the Remote Desktop service or its login password in a Windows VM](https://learn.microsoft.com/troubleshoot/azure/virtual-machines/reset-rdp) <br><br> For resetting root password, see [Manage users, SSH, and check or repair disks on Azure Linux VMs using the VMAccess Extension](https://learn.microsoft.com/troubleshoot/azure/virtual-machines/troubleshoot-ssh-connection)",
-            prompt={"cls": "AAZPromptPasswordInput", "kwargs": {"msg": "Please provide VMSS password:", "confirm": True}},
+            blank=AAZPromptPasswordInput(
+               msg="VM Admin Password",
+               confirm=True),
         )
         os_profile.admin_username = AAZStrArg(
             options=["admin-username"],
