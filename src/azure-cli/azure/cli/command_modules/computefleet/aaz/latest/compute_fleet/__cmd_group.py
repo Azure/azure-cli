5f7cee3291225007fc4977227213ad22ef69b4c4
samudra <sabaner@microsoft.com>
2026-06-17 10:24:30 -0700

    Set is_preview=True for command group

--- src/azure-cli/azure/cli/command_modules/computefleet/aaz/latest/compute_fleet/__cmd_group.py
+++ src/azure-cli/azure/cli/command_modules/computefleet/aaz/latest/compute_fleet/__cmd_group.py
@@ -13,6 +13,7 @@ from azure.cli.core.aaz import *
 
 @register_command_group(
     "compute-fleet",
+    is_preview=True,
 )
 class __CMDGroup(AAZCommandGroup):
     """Manage for Azure Compute Fleet
