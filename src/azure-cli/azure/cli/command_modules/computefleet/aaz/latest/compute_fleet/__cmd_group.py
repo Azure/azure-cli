48fe900e5703bb59213746498418dea070abed50
samudra <sabaner@microsoft.com>
2026-06-18 14:18:14 -0700

    Revert "Set is_preview=True for command group"
    This reverts commit 5f7cee3291225007fc4977227213ad22ef69b4c4.

--- src/azure-cli/azure/cli/command_modules/computefleet/aaz/latest/compute_fleet/__cmd_group.py
+++ src/azure-cli/azure/cli/command_modules/computefleet/aaz/latest/compute_fleet/__cmd_group.py
@@ -13,7 +13,6 @@ from azure.cli.core.aaz import *
 
 @register_command_group(
     "compute-fleet",
-    is_preview=True,
 )
 class __CMDGroup(AAZCommandGroup):
     """Manage for Azure Compute Fleet
