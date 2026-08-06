1452d5c56174e49194c3e16ed9557b13733d77b4
william <williamng0512@gmail.com>
2026-08-06 10:52:44 +0800

    Show force-deallocate param

--- src/azure-cli/azure/cli/command_modules/vm/operations/vm.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/vm.py
@@ -10,7 +10,7 @@ from knack.log import get_logger
 from azure.cli.core.aaz import AAZStrType
 from ..aaz.latest.vm import (Show as _VMShow, ListSizes as _VMListSizes, Patch as _VMPatch,
                              Update as _VMUpdate, Capture as _VMCapture, Create as _VMCreate,
-                             ListUsage as _VMListUsage, Deallocate as _VMDeallocate)
+                             ListUsage as _VMListUsage)
 from .._vm_utils import IdentityType
 
 logger = get_logger(__name__)
@@ -263,16 +263,6 @@ class VMListUsage(_VMListUsage):
             item['localName'] = item['name']['localizedValue']
 
         return result, next_link
-
-
-class VMDeallocate(_VMDeallocate):
-    @classmethod
-    def _build_arguments_schema(cls, *args, **kwargs):
-        args_schema = super()._build_arguments_schema(*args, **kwargs)
-
-        args_schema.force_deallocate._registered = False
-
-        return args_schema
 
 
 def convert_show_result_to_snake_case(result):
