aeb5e7ece69096c114ca592d0f206ec9b02aa9ad
william <williamng0512@gmail.com>
2026-09-24 15:18:36 +0800

    Update sig share wait command

--- src/azure-cli/azure/cli/command_modules/vm/operations/sig_share.py
+++ src/azure-cli/azure/cli/command_modules/vm/operations/sig_share.py
@@ -5,7 +5,7 @@
 # pylint: disable=no-self-use, line-too-long, protected-access, too-few-public-methods, unused-argument
 from knack.log import get_logger
 
-from azure.cli.core.aaz import register_command, AAZListArg, AAZStrArg
+from azure.cli.core.aaz import register_command, AAZListArg, AAZStrArg, has_value
 from azure.cli.core.azclierror import ArgumentUsageError
 from ..aaz.latest.sig.share import Update as _SigShareUpdate
 from ..aaz.latest.sig import Wait as _SigWait
@@ -203,6 +203,9 @@ class SigShareWait(_SigWait):
         az sig share wait --updated --resource-group MyResourceGroup --gallery-name Gallery
     """
 
+    _args_schema = None
+    _arguments_schema = None
+
     @classmethod
     def _build_arguments_schema(cls, *args, **kwargs):
         args_schema = super()._build_arguments_schema(*args, **kwargs)
@@ -223,6 +226,10 @@ class SigShareWait(_SigWait):
         args_schema.select._registered = False
 
         return args_schema
+
+    def pre_operations(self):
+        if not has_value(self.ctx.args.select):
+            self.ctx.args.select = 'Permissions'
 
     class GalleriesGet(_SigWait.GalleriesGet):
         @property
